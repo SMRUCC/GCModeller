f6855395039f2ca6f060804876ccc68dc9f805fa
xieguigang <xie.guigang@gmail.com>
2016-07-03 01:44:25 +0800

    fastq to fasta

---
+++ src/GCModeller/CLI_tools/RNA-seq/CLI/Tools.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports SMRUCC.genomics.SequenceModel.FASTA
+Imports SMRUCC.genomics.SequenceModel.Fastaq
+
+Partial Module CLI
+
+    <ExportAPI("/fq2fa", Usage:="/fq2fa /in <fastaq> [/out <fasta>]")>
+    Public Function Fq2fa(args As CommandLine) As Integer
+        Dim [in] As String = args("/in")
+        Dim out As String = args.GetValue("/out", [in].TrimFileExt & ".fasta")
+        Dim fastaq As FastaqFile = FastaqFile.Load([in])
+        Dim fasta As FastaFile = fastaq.ToFasta
+        Return fasta.Save(out, Encodings.ASCII)
+    End Function
+End Module
