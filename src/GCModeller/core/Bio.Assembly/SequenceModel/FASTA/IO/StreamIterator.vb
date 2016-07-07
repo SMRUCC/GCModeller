8e1abed5f233ab77479177669062e3592ecca551
xieguigang <xie.guigang@gmail.com>
2016-07-08 00:47:57 +0800

    unifying the nt source from file or directory

--- src/GCModeller/core/Bio.Assembly/SequenceModel/FASTA/IO/StreamIterator.vb
+++ src/GCModeller/core/Bio.Assembly/SequenceModel/FASTA/IO/StreamIterator.vb
@@ -1,32 +1,33 @@
 ﻿#Region "Microsoft.VisualBasic::f39b1bf760a437d63e32afe42598c913, ..\GCModeller\core\Bio.Assembly\SequenceModel\FASTA\IO\StreamIterator.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
-Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic
+Imports Microsoft.VisualBasic.ComponentModel
+Imports Microsoft.VisualBasic.Language.UnixBash
 
 Namespace SequenceModel.FASTA
 
@@ -92,6 +93,23 @@ Namespace SequenceModel.FASTA
 
             If Not temp.Count = 0 Then
                 Yield New FastaFile(temp)
+            End If
+        End Function
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="handle">File path or directory.</param>
+        ''' <returns></returns>
+        Public Shared Iterator Function SeqSource(handle As String, ParamArray ext As String()) As IEnumerable(Of FastaToken)
+            If handle.FileExists Then
+                Yield New StreamIterator(handle).ReadStream
+            Else
+                For Each file As String In ls - l - r - wildcards(ext) <= handle
+                    For Each nt As FastaToken In New StreamIterator(file).ReadStream
+                        Yield nt
+                    Next
+                Next
             End If
         End Function
     End Class
