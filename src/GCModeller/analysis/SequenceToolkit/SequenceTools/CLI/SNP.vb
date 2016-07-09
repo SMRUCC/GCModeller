6fea85871ba22ab2bd5c447afea9f9682398d605
xieguigang <xie.guigang@gmail.com>
2016-07-10 00:59:03 +0800

    calculate variation

--- src/GCModeller/analysis/SequenceToolkit/SequenceTools/CLI/SNP.vb
+++ src/GCModeller/analysis/SequenceToolkit/SequenceTools/CLI/SNP.vb
@@ -1,32 +1,34 @@
 ﻿#Region "Microsoft.VisualBasic::dab909649aa0dc21a54b9eab1f5211d1, ..\GCModeller\analysis\SequenceToolkit\SequenceTools\CLI\SNP.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
+Imports System.Text
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.DocumentFormat.Csv
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.genomics.Analysis.SequenceTools.SNP
 Imports SMRUCC.genomics.SequenceModel.FASTA
@@ -46,6 +48,15 @@ Partial Module Utilities
         Dim ref As Integer = args.GetInt32("/ref")
         Dim json As String = [in].TrimFileExt & ".SNPs.args.json"
         Return nt.ScanSNPs(ref, pure, monomorphic).GetJson.SaveTo(json)
+    End Function
+
+    <ExportAPI("/Time.Diffs", Usage:="/Time.Diffs /in <aln.fasta> [/out <out.csv>]")>
+    Public Function TimeDiffs(args As CommandLine) As Integer
+        Dim [in] As String = args - "/in"
+        Dim out As String = args.GetValue("/out", [in].TrimFileExt & ".time_variation.csv")
+        Dim result = New FastaFile([in]).GetSeqs.GroupByDate
+        Dim T = result.ToCsvDoc.Transpose
+        Return T.Save(out, Encoding.ASCII)
     End Function
 End Module
 
