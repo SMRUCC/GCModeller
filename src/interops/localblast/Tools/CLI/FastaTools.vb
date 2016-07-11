89ab2c18f7681ae2268e8ef0701512908ad22931
xieguigang <xie.guigang@gmail.com>
2016-07-12 00:05:06 +0800

    improvements on the cli helps

--- src/interops/localblast/Tools/CLI/FastaTools.vb
+++ src/interops/localblast/Tools/CLI/FastaTools.vb
@@ -9,23 +9,41 @@ Imports SMRUCC.genomics.SequenceModel.FASTA
 Partial Module CLI
 
     <ExportAPI("/Fasta.Filters",
-               Usage:="/Fasta.Filters /in <nt.fasta> /key <regex> [/out <out.fasta>]")>
+               Usage:="/Fasta.Filters /in <nt.fasta> /key <regex> [/out <out.fasta> /p]")>
+    <ParameterInfo("/p",
+                   True,
+                   AcceptTypes:={GetType(Boolean)},
+                   Description:="Using the parallel edition?? If GCModeller running in a 32bit environment, do not use this option.")>
     Public Function Filter(args As CommandLine) As Integer
         Dim [in] As String = args("/in")
         Dim key As String = args("/key")
         Dim out As String = args.GetValue("/out", [in].TrimFileExt & "-" & key.NormalizePathString.Replace(" ", "_") & ".fasta")
         Dim source As New StreamIterator([in])
+        Dim parallel As Boolean = args.GetBoolean("/p")
 
         Call "".SaveTo(out)
+
+        If parallel Then
+            Call "Using parallel edition!".__DEBUG_ECHO
+        End If
 
         Using file As New StreamWriter(New FileStream(out, FileMode.OpenOrCreate), Encoding.ASCII)
             Dim regex As New Regex(key, RegexICSng)
 
-            For Each block In LQuerySchedule.Where(source.ReadStream, Function(fa) regex.Match(fa.Title).Success)
-                For Each x In block
-                    Call file.WriteLine(x.GenerateDocument(-1))
+            If parallel Then
+                For Each block In LQuerySchedule.Where(source.ReadStream, Function(fa) regex.Match(fa.Title).Success)
+                    For Each x In block
+                        Call file.WriteLine(x.GenerateDocument(-1))
+                    Next
                 Next
-            Next
+            Else
+                For Each fa In source.ReadStream
+                    If regex.Match(fa.Title).Success Then
+                        Call file.WriteLine(fa.GenerateDocument(-1))
+                        Call fa.Title.__DEBUG_ECHO
+                    End If
+                Next
+            End If
         End Using
 
         Return 0
