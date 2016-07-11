7b4ce2256169e885948d3b2bd12e6c9a4eb83015
xieguigang <xie.guigang@gmail.com>
2016-07-11 17:44:35 +0800

    improvements on the exception helper

---
+++ src/interops/localblast/Tools/CLI/FastaTools.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Partial Module CLI
+
+    <ExportAPI("/Fasta.Filters",
+               Usage:="/Fasta.Filters")>
+    Public Function Filter(args As CommandLine) As Integer
+
+    End Function
+End Module
