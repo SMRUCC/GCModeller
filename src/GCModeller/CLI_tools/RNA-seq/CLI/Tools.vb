cae78796c8fa2b0dc6bbcd16e06ccceaeb52ab03
xieguigang <xie.guigang@gmail.com>
2016-07-15 19:40:12 +0800

    associate gi

--- src/GCModeller/CLI_tools/RNA-seq/CLI/Tools.vb
+++ src/GCModeller/CLI_tools/RNA-seq/CLI/Tools.vb
@@ -1,9 +1,13 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.DocumentFormat.Csv
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Language.UnixBash
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.genomics
 Imports SMRUCC.genomics.Analysis.Metagenome
 Imports SMRUCC.genomics.Analysis.Metagenome.BEBaC
@@ -106,16 +110,88 @@ Partial Module CLI
                 .Strand = readMaps.Strand.GetBriefCode,
                 .SequenceData = readMaps.QUAL,
                 .Complement = readMaps.QNAME,
-                .Ends =readMaps .FLAG 
+                .Ends = readMaps.FLAG
             }
         Next
 
         Dim maps As New Dictionary(Of String, String) From {
  _
             {NameOf(SimpleSegment.Complement), NameOf(AlignmentReads.QNAME)},
-            {NameOf(SimpleSegment.Ends), NameOf(AlignmentReads.FLAG)}
+            {NameOf(SimpleSegment.Ends), NameOf(AlignmentReads.FLAG)},
+            {NameOf(SimpleSegment.SequenceData), NameOf(AlignmentReads.QUAL)}
         }
 
+
+        Dim stat As NamedValue(Of Integer)() =
+            LinqAPI.Exec(Of NamedValue(Of Integer)) <= (From x As SimpleSegment
+                                                        In result
+                                                        Select x
+                                                        Group x By x.ID Into Count) _
+                   .Select(Function(x) New NamedValue(Of Integer)(x.ID, x.Count))
+        Dim statOut As String = out.TrimFileExt & ".stat.Csv"
+
+        Call New NamedValue(Of Integer)([in].BaseName, result.Count).Join(stat).SaveTo(statOut)
+
         Return result.SaveTo(out, maps:=maps)
+    End Function
+
+    <ExportAPI("/Associate.GI",
+               Usage:="/Associate.GI /in <list.Csv.DIR> /ref <nt.fasta> /out <out.DIR>")>
+    Public Function AssociateGI(args As CommandLine) As Integer
+        Dim [in] As String = args("/in")
+        Dim ref As String = args("/ref")
+        Dim EXPORT As String = args.GetValue("/out", [in].TrimDIR & "." & ref.BaseName & "/")
+        Dim hash = __buildHash(ref)
+
+        For Each file As String In ls - l - wildcards("*.Csv") <= [in]
+            Dim inputs = file.LoadCsv(Of NamedValue)
+            Dim out As String = EXPORT & "/" & file.BaseName & ".Csv"
+
+            For Each x In inputs
+                Dim gi As String = Regex.Match(x.Name, "gi\|\d+", RegexICSng).Value
+                gi = gi.Split("|"c).Last
+                If hash.ContainsKey(gi) Then
+                    x.Title = hash(gi)
+                End If
+            Next
+
+            Call inputs.SaveTo(out)
+        Next
+
+        Return 0
+    End Function
+
+    Public Class NamedValue
+        Public Property Name As String
+        Public Property x As String
+        Public Property Title As String
+    End Class
+
+    ''' <summary>
+    ''' {gi, title}
+    ''' </summary>
+    ''' <returns></returns>
+    Private Function __buildHash(nt As String) As Dictionary(Of String, String)
+        Dim source As New StreamIterator(nt)
+        Dim pairs = From x As FastaToken
+                    In source.ReadStream
+                    Let title As String = x.Title
+                    Let gi As String() = Regex.Matches(title, "gi\|\d+", RegexICSng).ToArray
+                    Select gi,
+                        title
+        Dim Groups = From p
+                     In (From x
+                         In pairs
+                         Let data = (From o As String
+                                     In x.gi
+                                     Select o,
+                                         x.title)
+                         Select data).MatrixAsIterator
+                     Select p
+                     Group By gi = p.o Into Group
+        Return Groups.ToDictionary(
+            Function(x) x.gi.Split("|"c).Last,
+            Function(x) x.Group.Select(
+            Function(o) o.title).JoinBy("; "))
     End Function
 End Module
