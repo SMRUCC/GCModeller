1edb83876af4e87d8ad609326fdfb44dccbb76e1
xieguigang <xie.guigang@gmail.com>
2016-07-17 19:05:12 +0800

    /Build_gi2taxi /in <gi2taxi.dmp> [/out <out.dat>]

--- src/GCModeller/CLI_tools/NCBI_tools/CLI.vb
+++ src/GCModeller/CLI_tools/NCBI_tools/CLI.vb
@@ -1,8 +1,14 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports SMRUCC.genomics.Assembly.NCBI
 
 Module CLI
 
+    <ExportAPI("/Build_gi2taxi",
+               Usage:="/Build_gi2taxi /in <gi2taxi.dmp> [/out <out.dat>]")>
     Public Function Build_gi2taxi(args As CommandLine) As Integer
-
+        Dim [in] As String = args("/in")
+        Dim out As String = args.GetValue("/out", [in].TrimFileExt & ".bin")
+        Return Taxonomy.Archive([in], out)
     End Function
 End Module
