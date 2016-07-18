81622013057e507daa502b5c3c360f58cc29a179
xieguigang <xie.guigang@gmail.com>
2016-07-19 04:33:01 +0800

    try implements a network viewer

---
+++ src/interops/visualize/Cytoscape/CLI_tool/Viewer/ViewerCanvas.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Windows.Forms
+Imports Microsoft.VisualBasic.DataVisualization.Network.Canvas
+
+Public Class ViewerCanvas
+
+    Dim WithEvents canvas As New Canvas With {
+        .Dock = DockStyle.Fill
+    }
+
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Controls.Add(canvas)
+        canvas.BringToFront()
+    End Sub
+End Class
