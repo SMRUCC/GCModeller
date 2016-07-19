7b8387dd6b7a35280c568ae6d795d30ea49bb1a5
xieguigang <xie.guigang@gmail.com>
2016-07-19 14:48:38 +0800

    improvements on fasta merge tools

--- src/GCModeller/analysis/SequenceToolkit/SequenceTools/CLI/FastaTools.vb
+++ src/GCModeller/analysis/SequenceToolkit/SequenceTools/CLI/FastaTools.vb
@@ -105,7 +105,7 @@ Partial Module Utilities
     End Function
 
     <ExportAPI("/Merge",
-               Usage:="/Merge /in <fasta.DIR> [/out <out.fasta> /trim /ext <*.fasta> /brief]",
+               Usage:="/Merge /in <fasta.DIR> [/out <out.fasta> /trim /unique /ext <*.fasta> /brief]",
                Info:="Only search for 1 level folder, dit not search receve.")>
     Public Function Merge(args As CommandLine.CommandLine) As Integer
         Dim inDIR As String = args("/in")
@@ -119,6 +119,19 @@ Partial Module Utilities
         Else
             fasta = FastaExportMethods.Merge(inDIR, ext, args.GetBoolean("/trim"), raw)
         End If
+
+        If args.GetBoolean("/unique") Then
+            Dim Groups = From f As FastaToken
+                         In fasta
+                         Let sid As String = f.Title.Split.First
+                         Select sid,
+                             f
+                         Group By sid Into Group
+            Dim uniques As IEnumerable(Of FastaToken) =
+                Groups.Select(Function(x) x.Group.First.f)
+            fasta = New FastaFile(uniques)
+        End If
+
         Return fasta.Save(out).CLICode
     End Function
 
