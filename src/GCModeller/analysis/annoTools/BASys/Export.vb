96f9927ae021bf8bb30186ee2373ce82831ae6ea
xieguigang <xie.guigang@gmail.com>
2016-07-21 23:02:42 +0800

    export cog

--- src/GCModeller/analysis/annoTools/BASys/Export.vb
+++ src/GCModeller/analysis/annoTools/BASys/Export.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Language
 Imports SMRUCC.genomics.Assembly.NCBI.GenBank.TabularFormat
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.RpsBLAST
 
@@ -16,6 +17,17 @@ Public Module Export
 
     <Extension>
     Public Function ExportCOG(proj As Project) As MyvaCOG()
-
+        Return LinqAPI.Exec(Of MyvaCOG) <=
+ _
+            From x As TableBrief
+            In proj.Briefs
+            Select New MyvaCOG With {
+                .COG = x.COG,
+                .QueryName = x.Accession,
+                .Description = x.Function,
+                .MyvaCOG = x.Gene,
+                .QueryLength = Math.Abs(x.End - x.Start),
+                .Length = Math.Abs(x.End - x.Start)
+            }
     End Function
 End Module
