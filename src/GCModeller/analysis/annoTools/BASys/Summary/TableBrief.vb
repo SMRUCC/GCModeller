dc954431bb8dad36413bf83f5bdae1df5c3e44d2
xieguigang <xie.guigang@gmail.com>
2016-07-21 16:08:45 +0800

    basys brief table

---
+++ src/GCModeller/analysis/annoTools/BASys/Summary/TableBrief.vb
@@ -0,0 +1,24 @@
+﻿
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+''' <summary>
+''' BASys Annotation Table for 1329830 
+''' </summary>
+Public Class TableBrief
+
+    Public Property Start As Integer
+    Public Property [End] As Integer
+    Public Property Strand As Char
+    Public Property Accession As String
+    Public Property Gene As String
+    Public Property COG As String
+    ''' <summary>
+    ''' Protein Function
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property [Function] As String
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
