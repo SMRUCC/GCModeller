6ed48afc63635875f93ba26c9ca0d9c8a0794cbe
xieguigang <xie.guigang@gmail.com>
2016-07-21 16:05:13 +0800

    summary

---
+++ src/GCModeller/analysis/annoTools/BASys/Summary.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+''' <summary>
+''' [index] BASys Annotation Summary
+''' </summary>
+Public Class Summary : Inherits ClassObject
+
+    ''' <summary>
+    ''' Chromosome Id
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property chrId As String
+    Public Property Length As Integer
+    ''' <summary>
+    ''' Gram Stain
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property GramStain As String
+    Public Property Topology As String
+    Public Property Genus As String
+    Public Property Species As String
+    Public Property Strain As String
+    ''' <summary>
+    ''' Number of Genes Identified
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property gIdentified As Integer
+    ''' <summary>
+    ''' Number of Genes Annotated
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property gAnnotated As Integer
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
