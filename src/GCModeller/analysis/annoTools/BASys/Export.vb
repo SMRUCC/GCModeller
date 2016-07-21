6f9090d4ae179cbc8057b0bfa3c6d9e77833ffd9
xieguigang <xie.guigang@gmail.com>
2016-07-21 22:48:39 +0800

    basys exports interface

---
+++ src/GCModeller/analysis/annoTools/BASys/Export.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank.TabularFormat
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.RpsBLAST
+
+Public Module Export
+
+    <Extension>
+    Public Function ExportPTT(proj As Project) As PTT
+
+    End Function
+
+    <Extension>
+    Public Function ExportGFF(proj As Project) As GFF
+
+    End Function
+
+    <Extension>
+    Public Function ExportCOG(proj As Project) As MyvaCOG()
+
+    End Function
+End Module
