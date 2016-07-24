bc368d5c83d3ebb9755950aa03d53dd68aff9e48
xieguigang <xie.guigang@gmail.com>
2016-07-25 00:14:58 +0800

    reconstruct for the BIOM project

---
+++ src/GCModeller/models/BIOM/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("The Biological Observation Matrix (BIOM) Format Project http://biom-format.org")>
+<Assembly: AssemblyDescription("The Biological Observation Matrix (BIOM) Format Project http://biom-format.org")>
+<Assembly: AssemblyCompany("http://biom-format.org")>
+<Assembly: AssemblyProduct("GCModeller")>
+<Assembly: AssemblyCopyright("Copyright © GPL3 2016")>
+<Assembly: AssemblyTrademark("biom-format")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5ae63b1e-2300-40de-951c-41648a9990f4")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
