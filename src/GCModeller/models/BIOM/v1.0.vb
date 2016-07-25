64ba93605a1eef215c4de5beec9302e3d7160427
xieguigang <xie.guigang@gmail.com>
2016-07-25 16:08:05 +0800

    add code comments on the fields

--- src/GCModeller/models/BIOM/v1.0.vb
+++ src/GCModeller/models/BIOM/v1.0.vb
@@ -14,6 +14,13 @@ Namespace v10
         Public Const Gene_table As String = "Gene table"
         Public Const Metabolite_table As String = "Metabolite table"
         Public Const Taxon_table As String = "Taxon table"
+
+        Public Const MatrixTypeSparse As String = "sparse"
+        Public Const MatrixTypeDense As String = "dense"
+
+        Public Const MatrixElTypeInt As String = "int"
+        Public Const MatrixElTypeFloat As String = "float"
+        Public Const MatrixElTypeUnicode As String = "unicode"
     End Class
 
     ''' <summary>
@@ -30,6 +37,12 @@ Namespace v10
         ''' </summary>
         ''' <returns></returns>
         Public Property id As String
+        ''' <summary>
+        ''' ``&lt;string>`` A free text field containing any information that you
+        ''' feel Is relevant (Or just feel Like sharing)
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property comment As String
         ''' <summary>
         ''' ``&lt;string>`` The name and version of the current biom format
         ''' </summary>
@@ -55,13 +68,73 @@ Namespace v10
         ''' </summary>
         ''' <returns></returns>
         Public Property type As String
+        ''' <summary>
+        ''' ``&lt;string>`` Package and revision that built the table
+        ''' </summary>
+        ''' <returns></returns>
         Public Property generated_by As String = "GCModeller"
+        ''' <summary>
+        ''' ``&lt;datetime>`` Date the table was built (ISO 8601 format)
+        ''' </summary>
+        ''' <returns></returns>
         Public Property [date] As Date
+        ''' <summary>
+        ''' ``&lt;string>`` Type of matrix data representation (a controlled vocabulary)
+        ''' Acceptable values : 
+        ''' 
+        ''' + "sparse" : only non-zero values are specified
+        ''' + "dense" : every element must be specified
+        ''' 
+        ''' </summary>
+        ''' <returns></returns>
         Public Property matrix_type As String
+        ''' <summary>
+        ''' Value type in matrix (a controlled vocabulary)
+        ''' Acceptable values : 
+        ''' 
+        ''' + "int"  Integer
+        ''' + "float" : floating point
+        ''' + "unicode" : unicode string
+        ''' 
+        ''' </summary>
+        ''' <returns></returns>
         Public Property matrix_element_type As String
+        ''' <summary>
+        ''' ``&lt;list of ints>``, the number of rows and number of columns in data
+        ''' </summary>
+        ''' <returns></returns>
         Public Property shape As Integer()
+        ''' <summary>
+        ''' ``&lt;list of lists>``, counts of observations by sample
+        ''' 
+        ''' If matrix_type Is "sparse", 
+        ''' 
+        ''' ```
+        ''' [[row, column, value],
+        '''  [row, column, value],
+        '''  ...]
+        ''' ```
+        '''  
+        ''' If matrix_type Is "dense",  
+        ''' 
+        ''' ```
+        ''' [[value, value, value, ...],
+        '''  [value, value, value, ...],
+        '''  ...]
+        ''' ```
+        ''' 
+        ''' </summary>
+        ''' <returns></returns>
         Public Property data As Integer()()
+        ''' <summary>
+        ''' ``&lt;list of objects>`` An ORDERED list of obj describing the rows
+        ''' </summary>
+        ''' <returns></returns>
         Public Property rows As row()
+        ''' <summary>
+        ''' ``&lt;list of objects>`` An ORDERED list of obj  describing the columns
+        ''' </summary>
+        ''' <returns></returns>
         Public Property columns As column()
 
         Public Overrides Function ToString() As String
