518258b5359aad164cbfd03cf1ea516fdf418162
xieguigang <xie.guigang@gmail.com>
2016-07-25 15:35:20 +0800

    reconstruct for v1.0 biom

--- src/GCModeller/models/BIOM/v1.0.vb
+++ src/GCModeller/models/BIOM/v1.0.vb
@@ -1,6 +1,38 @@
-﻿Namespace v10
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace v10
 
     Public Class Json
 
+        Public Property id As String
+        Public Property format As String
+        Public Property format_url As String
+        Public Property type As String
+        Public Property generated_by As String = "GCModeller"
+        Public Property [date] As String
+        Public Property matrix_type As String
+        Public Property matrix_element_type As String
+        Public Property shape As Integer()
+        Public Property data As Integer()()
+        Public Property rows As row()
+        Public Property columns As column()
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Class
+
+    Public Class row
+        Public Property id As String
+        Public Property metadata As meta
+    End Class
+
+    Public Class meta
+        Public Property taxonomy As String()
+    End Class
+
+    Public Class column
+        Public Property id As String
+        Public Property metadata As String
     End Class
 End Namespace
