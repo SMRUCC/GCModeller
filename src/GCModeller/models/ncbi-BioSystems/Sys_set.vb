d08c1423d4b264a4b26d2d5f05e954606e1d7a85
xieguigang <xie.guigang@gmail.com>
2016-07-30 15:52:45 +0800

    ncbi bio system

---
+++ src/GCModeller/models/ncbi-BioSystems/Sys_set.vb
@@ -0,0 +1,31 @@
+﻿''' <summary>
+''' 
+''' </summary>
+Public Class Sys_set
+    Public Property sysid As sysid
+    Public Property source As source
+    Public Property externalaccn As String
+    Public Property recordurl As String
+    Public Property names As String()
+    Public Property description As String
+
+End Class
+
+Public Class sysid
+    Public Property bsid As String
+    Public Property version As String
+End Class
+
+Public Class source
+
+    Public Property source As sourceInner
+
+    Public Class sourceInner
+        Public Property db As String
+        Public Property tag As tag
+    End Class
+End Class
+
+Public Class tag
+    Public Property id As String
+End Class
