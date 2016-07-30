c3090a620a4d46097bec1bd2fb8c2959f220dc5b
xieguigang <xie.guigang@gmail.com>
2016-07-31 00:56:30 +0800

    IDENTIFIER TABLE

---
+++ src/GCModeller/analysis/ProteinTools/Interactions.BioGRID/TabularFiles/IDENTIFIERS.vb
@@ -0,0 +1,7 @@
+﻿Public Class IDENTIFIERS
+    Public Property BIOGRID_ID As String
+    Public Property IDENTIFIER_VALUE As String
+    Public Property IDENTIFIER_TYPE As String
+    Public Property ORGANISM_OFFICIAL_NAME As String
+
+End Class
