dcc9355e6c46570807d73ae315b1d7f4d4728762
xieguigang <xie.guigang@gmail.com>
2016-08-11 18:02:30 +0800

    improvements on the plas system

--- src/GCModeller/sub-system/PLAS.NET/SSystem/My Project/Settings.Designer.vb
+++ src/GCModeller/sub-system/PLAS.NET/SSystem/My Project/Settings.Designer.vb
@@ -53,6 +53,18 @@ Namespace My
                 Return defaultInstance
             End Get
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property Setting() As String
+            Get
+                Return CType(Me("Setting"),String)
+            End Get
+            Set
+                Me("Setting") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
