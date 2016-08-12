a82b2de3579bc178610fb0236a5288e6f08c9fdf
xieguigang <xie.guigang@gmail.com>
2016-08-12 18:37:43 +0800

    add code comments

---
+++ src/GCModeller/models/Networks/psidev/XML/XmlCommon.vb
@@ -0,0 +1,135 @@
+﻿#Region "Microsoft.VisualBasic::32027627d069ded023ce3d6f1e8758ac, ..\GCModeller\data\ExternalDBSource\string-db\[xsd]net.sf.psidev.mi\XmlCommon.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.DocumentFormat.Csv.StorageProvider.Reflection
+
+Namespace XML
+
+    Public MustInherit Class DataItem
+
+        <XmlAttribute> Public Overridable Property id As Integer
+
+        Public Overrides Function ToString() As String
+            Return id
+        End Function
+    End Class
+
+    ''' <summary>
+    ''' Names for an object.
+    ''' </summary>
+    Public Class Names
+
+        ''' <summary>
+        ''' A short string, suitable to remember the object. Can be e.g. a gene name, the first author of a paper, etc.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property shortLabel As String
+        ''' <summary>
+        ''' A full, detailed name or description of the object. Can be e.g. the full title of a publication, or the scientific name of a species.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property fullName As String
+        Public Property [alias] As String
+
+        Public Overrides Function ToString() As String
+            Return String.Format("({0}) {1}", shortLabel, fullName)
+        End Function
+    End Class
+
+    ''' <summary>
+    ''' Crossreference to an external database. Crossreferences to literature databases, e.g. PubMed, should not be put into
+    ''' this Structure, but into the bibRef element where possible.
+    ''' </summary>
+    Public Class Xref
+
+        ''' <summary>
+        ''' Primary reference to an external database.
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlElement> Public Property primaryRef As Reference
+        ''' <summary>
+        ''' Further external objects describing the object.
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlElement> Public Property secondaryRef As Reference()
+
+        Public Overrides Function ToString() As String
+            Return primaryRef.ToString
+        End Function
+    End Class
+
+    ''' <summary>
+    ''' Refers to a unique object in an external database.
+    ''' </summary>
+    Public Class Reference : Inherits DataItem
+
+        ''' <summary>
+        ''' Primary identifier of the object in the external database, e.g. UniProt accession number.
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Overrides Property id As Integer
+
+        ''' <summary>
+        ''' Name of the external database. Taken from the controlled vocabulary of databases.
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Property db As String
+        ''' <summary>
+        ''' Accession number of the database in the database CV. This element is controlled by the PSI-MI controlled
+        ''' vocabulary "database citation", root term id MI0444.
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Property dbAc As String
+        ''' <summary>
+        ''' Reference type, e.g. "identity" if this reference referes to an identical object in the external database,
+        ''' Or "see-also" for additional information. Controlled by CV.
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Property refType As String
+        ''' <summary>
+        ''' Reference type accession number from the CV of reference types. This element is controlled by the PSI-MI
+        ''' controlled vocabulary "xref type", root term id MI:0353.
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Property refTypeAc As String
+        ''' <summary>
+        ''' Secondary identifier of the object in the external database, e.g. UniProt ID.
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Property secondary As String
+        ''' <summary>
+        ''' The version number of the object in the external database.
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Property version As String
+
+        Public Overrides Function ToString() As String
+            Return String.Format("{0}: {1}", db, id)
+        End Function
+    End Class
+End Namespace
