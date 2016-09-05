fa4d7ba72db6f8c2006a5aa1a6e79a9607b9c20c
xieguigang <xie.guigang@gmail.com>
2016-09-05 14:50:43 +0800

    Ontology Documentation

--- src/GCModeller/data/GO_gene-ontology/AnnotationFile/ToGo.vb
+++ src/GCModeller/data/GO_gene-ontology/AnnotationFile/ToGo.vb
@@ -1,31 +1,32 @@
 ﻿#Region "Microsoft.VisualBasic::ba617f8a182976f2d5739caefa1dba2f, ..\GCModeller\data\GO_gene-ontology\AnnotationFile\ToGo.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.Language
 
 ''' <summary>
 ''' The reference links between the Go database and other biological database.
@@ -51,14 +52,25 @@ Public Class ToGo
     ''' <remarks></remarks>
     Public Shared Function LoadDocument(Path As String, DbXrefHead As String) As ToGo()
         Dim OffSet As Integer = Len(DbXrefHead) + 2
-        Dim Document As String() = (From s As String In IO.File.ReadAllLines(Path) Where s.First <> "!"c Select Mid(s, OffSet)).ToArray
-        Dim LQuery = (From s As String In Document.AsParallel
-                      Let Tokens As String() = s.Split
-                      Let annotation As String = Regex.Match(s, "GO:.+;").Value
-                      Select New ToGo With {
-                          .DbXrefID = Tokens.First,
-                          .GO_ID = Tokens.Last,
-                          .FunctionAnnotation = annotation}).ToArray
+        Dim Document As String() = LinqAPI.Exec(Of String) <=
+ _
+            From s As String
+            In IO.File.ReadAllLines(Path)
+            Where s.First <> "!"c
+            Select Mid(s, OffSet)
+
+        Dim LQuery = LinqAPI.Exec(Of ToGo) <=
+ _
+            From s As String
+            In Document.AsParallel
+            Let Tokens As String() = s.Split
+            Let annotation As String = Regex.Match(s, "GO:.+;").Value
+            Select New ToGo With {
+                .DbXrefID = Tokens.First,
+                .GO_ID = Tokens.Last,
+                .FunctionAnnotation = annotation
+            }
+
         Return LQuery
     End Function
 End Class
--- src/GCModeller/data/GO_gene-ontology/README.md
+++ src/GCModeller/data/GO_gene-ontology/README.md
@@ -1,2 +1,21 @@
 # GO_gene-ontology
 Gene Ontology Parser for GCModeller
+
+The Gene Ontology (GO) project is a collaborative effort to address the need for consistent descriptions of gene products across databases. Founded in 1998, the project began as a collaboration between three model organism databases, FlyBase (Drosophila), the Saccharomyces Genome Database (SGD) and the Mouse Genome Database (MGD). The GO Consortium (GOC) has since grown to incorporate many databases, including several of the world's major repositories for plant, animal, and microbial genomes. The GO Contributors page lists all member organizations.
+
+The GO project has developed three structured ontologies that describe gene products in terms of their associated biological processes, cellular components and molecular functions in a species-independent manner. There are three separate aspects to this effort: first, the development and maintenance of the ontologies themselves; second, the annotation of gene products, which entails making associations between the ontologies and the genes and gene products in the collaborating databases; and third, the development of tools that facilitate the creation, maintenance and use of ontologies.
+
+The use of GO terms by collaborating databases facilitates uniform queries across all of them. Controlled vocabularies are structured so they can be queried at different levels; for example, users may query GO to find all gene products in the mouse genome that are involved in signal transduction, or zoom in on all receptor tyrosine kinases that have been annotated. This structure also allows annotators to assign properties to genes or gene products at different levels, depending on the depth of knowledge about that entity.
+
+Shared vocabularies are an important step towards unifying biological databases, but additional work is still necessary as knowledge changes, updates lag behind, and individual curators evaluate data differently. The GO aims to serve as a platform where curators can agree on stating how and why a specific term is used, and how to consistently apply it, for example, to establish relationships between gene products.
+
+#### The Scope of GO
+The following areas are outside the scope of GO, and terms in these domains will not appear in the ontologies:
+
++ Gene products: e.g. cytochrome c is not in the ontologies, but attributes of cytochrome c, such as oxidoreductase activity, are.
++ Processes, functions or components that are unique to mutants or diseases: e.g. oncogenesis is not a valid GO term, as "causing cancer" is the result of reprogrammed, not normal cells and thus it is not the normal function of a gene.
++ Attributes of sequence such as "intron" or "exon" parameters belong in a separate sequence ontology; see the Open Biological and Biomedical Ontologies website for more information.
++ Protein domains or structural features.
++ Protein-protein interactions.
++ Environment, evolution and expression.
++ Anatomical or histological features above the level of cellular components, including cell types.
