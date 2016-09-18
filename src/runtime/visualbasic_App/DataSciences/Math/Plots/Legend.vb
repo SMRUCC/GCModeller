a98930b7a98b35c2008053f8211aeca139cd49f3
xieguigang <xie.guigang@gmail.com>
2016-09-18 13:46:28 +0800

    css font for chart plot

---
+++ src/runtime/visualbasic_App/DataSciences/Math/Plots/Legend.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic.MarkupLanguage.CSS
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Class Legend
+
+    Public Property style As LegendStyles
+    Public Property title As String
+    Public Property color As String
+    ''' <summary>
+    ''' CSS expression, which can be parsing by <see cref="CSSFont"/> 
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property font As String
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
+
+Public Enum LegendStyles
+    Bar
+    Circle
+    SolidLine
+    DashLine
+    Diamond
+End Enum
