42c3eb03cd98a6007e1743d9a238b84a05d7c07d
xieguigang <xie.guigang@gmail.com>
2016-10-03 12:06:31 +0800

    localblast code improvements

--- src/interops/localblast/LocalBLAST/LocalBLAST/LocalBLAST/Program/RpsBLAST.vb
+++ src/interops/localblast/LocalBLAST/LocalBLAST/LocalBLAST/Program/RpsBLAST.vb
@@ -29,8 +29,8 @@ Namespace LocalBLAST.Programs
 
     Public NotInheritable Class RpsBLAST : Inherits NCBI.Extensions.LocalBLAST.InteropService.LocalBlastProgramGroup
 
-        Protected Friend ReadOnly _InternalExecutableMakeProfileDb As String
-        Protected Friend ReadOnly _InternalExecutableRpsBLAST As String
+        Protected Friend ReadOnly _MakeProfileDb As String
+        Protected Friend ReadOnly _RpsBLAST As String
 
         Public ReadOnly Property LastBLASTOutputFilePath As String
             Get
@@ -40,14 +40,14 @@ Namespace LocalBLAST.Programs
 
         Sub New(BLASTBin As String)
             Me._innerBLASTBinDIR = BLASTBin
-            Me._InternalExecutableMakeProfileDb = BLASTBin & "/makeprofiledb.exe"
-            Me._InternalExecutableRpsBLAST = BLASTBin & "/rpsblast.exe"
+            Me._MakeProfileDb = BLASTBin & "/makeprofiledb.exe"
+            Me._RpsBLAST = BLASTBin & "/rpsblast.exe"
         End Sub
 
         Public Function MakeProfileDb(pnList As String) As CommandLine.IORedirect
             Dim TargetAssembly As String = FileIO.FileSystem.GetParentPath(pnList) & "/makeprofiledb.exe"
             Dim Cmdl As CommandLine.IORedirect = New CommandLine.IORedirect(TargetAssembly, String.Format("-in ""{0}"" -dbtype rps", pnList))
-            Call FileIO.FileSystem.CopyFile(Me._InternalExecutableMakeProfileDb, TargetAssembly)
+            Call FileIO.FileSystem.CopyFile(Me._MakeProfileDb, TargetAssembly)
             Call Console.WriteLine("[RPSBLAST_MAKEPROFILEDB]{0}  ---> ""{1}""", vbCrLf, Cmdl.ToString)
 
             Return Cmdl
@@ -55,7 +55,7 @@ Namespace LocalBLAST.Programs
 
         Public Function Performance(FsaDb As String, rpsDb As String, Evalue As String, Output As String) As CommandLine.IORedirect
             Dim Argv As String = String.Format("-query ""{0}"" -evalue {1} -out ""{2}"" -db ""{3}""", FsaDb, Evalue, Output, rpsDb)
-            Dim Cmdl As CommandLine.IORedirect = New CommandLine.IORedirect(Me._InternalExecutableRpsBLAST, Argv)
+            Dim Cmdl As CommandLine.IORedirect = New CommandLine.IORedirect(Me._RpsBLAST, Argv)
             _InternalLastBLASTOutputFile = Output
             Call Console.WriteLine("[RPSBLAST]{0}  ---> ""{1}""", vbCrLf, Cmdl.ToString)
 
