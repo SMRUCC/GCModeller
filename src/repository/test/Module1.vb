fdd3bd1ab08af41108f6055d87cce252eb6db3a7
xieguigang <xie.guigang@gmail.com>
2016-10-04 21:41:21 +0800

    test index data writer

--- src/repository/test/Module1.vb
+++ src/repository/test/Module1.vb
@@ -4,9 +4,15 @@ Imports SMRUCC.genomics.Data.Repository.NCBI
 Module Module1
 
     Sub Main()
-        Dim cnn As New ConnectionUri With {.Database = "ncbi", .IPAddress = "127.0.0.1", .Password = "1234", .User = "root", .ServicesPort = 3306}
+        Dim cnn As New ConnectionUri With {
+            .Database = "ncbi",
+            .IPAddress = "127.0.0.1",
+            .Password = "1234",
+            .User = "root",
+            .ServicesPort = 3306
+        }
         Dim mysql As New MySQL(cnn)
 
-        Call mysql.[Imports]("G:\NCBI_nt\nt", "x:\")
+        Call mysql.[Imports]("D:\GCModeller\src\repository\data\test_virus_nt.fna", "D:\GCModeller\src\repository\data\DATA\")
     End Sub
 End Module
