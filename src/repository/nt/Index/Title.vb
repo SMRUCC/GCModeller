d8b8b57f1d9c1128adc110ee52badee62852a061
xieguigang <xie.guigang@gmail.com>
2016-10-05 13:03:53 +0800

    simple fasta file compare tool by compare the locis_tag

--- src/repository/nt/Index/Title.vb
+++ src/repository/nt/Index/Title.vb
@@ -1,4 +1,11 @@
-﻿Public Class TitleIndex
+﻿Imports Microsoft.VisualBasic.Data.IO
+Imports Microsoft.VisualBasic.Text
+Imports SMRUCC.genomics.Assembly.NCBI.SequenceDump
+Imports System.IO
+Imports System.Text
+Imports Microsoft.VisualBasic.Language
+
+Public Class TitleIndex
     Inherits IndexAbstract
 
 
@@ -13,10 +20,37 @@ End Class
 Public Class TitleWriter
     Inherits IndexAbstract
 
-
+    ReadOnly __titles As BinaryDataWriter
+    ReadOnly __index As BinaryDataWriter
 
     Public Sub New(DATA$, db$, uid$)
         MyBase.New(uid)
+
+        Dim path As New Value(Of String)
+        Dim file As FileStream
+
+        Call (path = $"{DATA}/headers/{db}/{uid}.dat").ParentPath.MkDIR
+
+        file = IO.File.OpenWrite(path)
+        __titles = New BinaryDataWriter(file, Encodings.ASCII)
+
+        Call (path = $"{DATA}/headers/index/{db}/{uid}.dat").ParentPath.MkDIR
+
+        file = IO.File.OpenWrite(path)
+        __index = New BinaryDataWriter(file, Encodings.ASCII)
     End Sub
 
+    ReadOnly __pointer&
+
+    ''' <summary>
+    ''' ``{gi,len,title}``
+    ''' </summary>
+    ''' <param name="header"></param>
+    ''' <returns></returns>
+    Public Function Write(header As NTheader) As Long
+        Dim start& = __pointer
+        Dim gi As Byte() = BitConverter.GetBytes(CLng(header.gi))
+        Dim title As Byte() = Encoding.ASCII.GetBytes(header.description)
+
+    End Function
 End Class
