6f1f199a3a1f46a7bcd21e34394479d67dfe72a8
xieguigang <xie.guigang@gmail.com>
2016-10-09 19:35:44 +0800

    unable to compile after configs

--- src/GCModeller/CLI_tools/S.M.A.R.T/CLI/Export.vb
+++ src/GCModeller/CLI_tools/S.M.A.R.T/CLI/Export.vb
@@ -1,43 +1,46 @@
 ﻿#Region "Microsoft.VisualBasic::5bb295f7fb8d2a4925058be1ea13b0f0, ..\GCModeller\CLI_tools\S.M.A.R.T\CLI\Export.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
-Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic
-Imports SMRUCC.genomics.SequenceModel
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Text
 Imports SMRUCC.genomics.Assembly
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST
+Imports SMRUCC.genomics.SequenceModel
+Imports SMRUCC.genomics.SequenceModel.FASTA
 
 Partial Module CLI
 
     ''' <summary>
     ''' 从目标蛋白质结构域数据库之中导出包含有目标关键词的序列数据
     ''' </summary>
-    ''' <param name="CommandLine"></param>
+    ''' <param name="args"></param>
     ''' <returns></returns>
     ''' <remarks></remarks>
     <ExportAPI("export", Info:="", Usage:="export -keyword <keyword_list> [-m <any/all>] -o <export_file> [-d <db_name> -casesense <T/F>]", Example:="")>
@@ -47,18 +50,18 @@ Partial Module CLI
     <ParameterInfo("-keyword",
         Description:="The keyword list will be use for the sequence record search, each keyword should seperated by comma character.",
         Example:="HTH,GGDEF,Clp,REC")>
-    Public Function Export(CommandLine As Microsoft.VisualBasic.CommandLine.CommandLine) As Integer
-        Dim Db As String = CommandLine("-d")
-        Dim Keywords As String() = CommandLine("-keyword").Split(CChar(","))
-        Dim Output As String = CommandLine("-o")
-        Dim CaseSense As CompareMethod = IIf(String.Equals(CommandLine("-casesense"), "T"), CompareMethod.Binary, CompareMethod.Text)
-        Dim IsMethodAny As Boolean = IIf(String.Equals(CommandLine("-m"), "any"), True, False)
+    Public Function Export(args As CommandLine) As Integer
+        Dim Db As String = args("-d")
+        Dim Keywords As String() = args("-keyword").Split(CChar(","))
+        Dim Output As String = args("-o")
+        Dim CaseSense As CompareMethod = IIf(String.Equals(args("-casesense"), "T"), CompareMethod.Binary, CompareMethod.Text)
+        Dim IsMethodAny As Boolean = IIf(String.Equals(args("-m"), "any"), True, False)
 
         Call FileIO.FileSystem.CreateDirectory(FileIO.FileSystem.GetParentPath(Output))
 
         If String.IsNullOrEmpty(Db) OrElse String.Equals(Db, "all") Then
 
-            Dim List As List(Of SMRUCC.genomics.SequenceModel.FASTA.FastaToken) = New List(Of FASTA.FastaToken)
+            Dim List As New List(Of FastaToken)
             For Each Db In New NCBI.CDD.Database("").Paths
                 Call List.AddRange(Export(Db, Keywords, CaseSense, IsMethodAny))
             Next
@@ -93,9 +96,9 @@ Partial Module CLI
     End Function
 
     <ExportAPI("convert", Usage:="convert -i <input_file> [-o <xml_file>]")>
-    Public Function Convert(CommandLine As Microsoft.VisualBasic.CommandLine.CommandLine) As Integer
-        Dim Log As String = CommandLine("-i")
-        Dim Saved As String = CommandLine("-o")
+    Public Function Convert(args As CommandLine) As Integer
+        Dim Log As String = args("-i")
+        Dim Saved As String = args("-o")
 
         Dim BlastLog As BLASTOutput.Standard.BLASTOutput = BLASTOutput.Standard.BLASTOutput.TryParse(Log)
         Call BlastLog.Save(Saved)
@@ -105,7 +108,7 @@ Partial Module CLI
     ''' <summary>
     ''' 解析BLAST日志文件中的标记号名称
     ''' </summary>
-    ''' <param name="CommandLine"></param>
+    ''' <param name="args"></param>
     ''' <returns></returns>
     ''' <remarks></remarks>
     <ExportAPI("grep", Info:="The gene id in the blast output log file are not well format for reading and program processing, so " &
@@ -139,10 +142,10 @@ Partial Module CLI
                      "   usage:   match <regular_expression>\n" &
                      "   Example: match .+[-]\d{5}",
         Example:="'tokens | 5';'match .+[-].+'")>
-    Public Function Grep(CommandLine As Microsoft.VisualBasic.CommandLine.CommandLine) As Integer
-        Dim GrepScriptQuery As Microsoft.VisualBasic.Text.TextGrepScriptEngine = Microsoft.VisualBasic.Text.TextGrepScriptEngine.Compile(CommandLine("-q"))
-        Dim GrepScriptHit As Microsoft.VisualBasic.Text.TextGrepScriptEngine = Microsoft.VisualBasic.Text.TextGrepScriptEngine.Compile(CommandLine("-h"))
-        Dim XmlFile As String = CommandLine("-i")
+    Public Function Grep(args As CommandLine) As Integer
+        Dim GrepScriptQuery As TextGrepScriptEngine = TextGrepScriptEngine.Compile(args("-q"))
+        Dim GrepScriptHit As TextGrepScriptEngine = TextGrepScriptEngine.Compile(args("-h"))
+        Dim XmlFile As String = args("-i")
 
         If String.IsNullOrEmpty(XmlFile) Then
             Return -1
