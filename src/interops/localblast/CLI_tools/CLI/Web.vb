fe155077ed998bf57a2dadab75ade4bb3c5590d2
xieguigang <xie.guigang@gmail.com>
2016-10-17 12:50:06 +0800

    export aligmment table from the ncbi web blast result

---
+++ src/interops/localblast/CLI_tools/CLI/Web.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.NCBIBlastResult
+
+Partial Module Web
+
+    <ExportAPI("/Export.AlignmentTable",
+               Usage:="/Export.AlignmentTable /in <alignment.txt> [/split /out <outDIR/file>]")>
+    <Group(CLIGrouping.WebTools)>
+    Public Function ExportWebAlignmentTable(args As CommandLine) As Integer
+        Dim [in] As String = args("/in")
+        Dim isSplit As Boolean = args.GetBoolean("/split")
+        Dim out As String = args.GetValue("/out", [in].TrimSuffix)
+        Dim tables As IEnumerable(Of AlignmentTable) =
+            [in].IterateTables
+
+        If isSplit Then
+            out = out & "-EXPORT/"
+
+            For Each table In tables
+                Call table.Hits _
+                    .SaveTo(out & "/" & table.Query & ".csv")
+            Next
+        Else
+            Return tables _
+                .Select(Function(x) x.Hits) _
+                .MatrixToList _
+                .SaveTo(out & ".csv") _
+                .CLICode
+        End If
+
+        Return 0
+    End Function
+End Module
