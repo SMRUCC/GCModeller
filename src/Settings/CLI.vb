e90610eccc96889db1acae45a2520767a496ea67
xieguigang <xie.guigang@gmail.com>
2016-10-21 01:57:36 +0800

    CLI grouping function description paragraphing

--- src/Settings/CLI.vb
+++ src/Settings/CLI.vb
@@ -60,7 +60,7 @@ Public Module CLI
                Usage:="var [varName]",
                Example:="")>
     <Argument("[VarName]", True,
-                   Description:="If this value is null, then the program will prints all of the variables in the gcmodeller config file or when the variable is presents in the database, only the config value of the specific variable will be display.")>
+              Description:="If this value is null, then the program will prints all of the variables in the gcmodeller config file or when the variable is presents in the database, only the config value of the specific variable will be display.")>
     Public Function Var(args As CommandLine) As Integer
         Using Settings = Global.GCModeller.Configuration.Settings.Session.ProfileData
             If args.Parameters.Length = 0 Then 'list all setting items
