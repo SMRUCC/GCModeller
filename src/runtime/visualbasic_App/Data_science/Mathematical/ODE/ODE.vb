6509e585cac3d34fa70a68653882ceedb2aad452
xieguigang <xie.guigang@gmail.com>
2016-10-23 15:51:21 +0800

    update runtime library readme doc

--- src/runtime/visualbasic_App/Data_science/Mathematical/ODE/ODE.vb
+++ src/runtime/visualbasic_App/Data_science/Mathematical/ODE/ODE.vb
@@ -1,37 +1,40 @@
 ﻿#Region "Microsoft.VisualBasic::38123610a59dc41d14c2a0d541e9ebe7, ..\visualbasic_App\Data_science\Mathematical\ODE\ODE.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.Ranges
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 ''' <summary>
 ''' Ordinary differential equation(ODE).(常微分方程的模型)
 ''' </summary>
 Public Class ODE
+    Implements sIdEnumerable
 
 #Region "Output results"
 
@@ -56,6 +59,14 @@ Public Class ODE
             Return _df(xi, yi)
         End Get
     End Property
+
+    Public ReadOnly Property xrange As DoubleRange
+        Get
+            Return New DoubleRange(x.First, x.Last)
+        End Get
+    End Property
+
+    Public Property Id As String Implements sIdEnumerable.Identifier
 
     Public Overrides Function ToString() As String
         Return x.GetJson & " --> " & y.GetJson
