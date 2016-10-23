554b260a623fd86f6a1203228ea664944c3f3d43
xieguigang <xie.guigang@gmail.com>
2016-10-24 01:57:32 +0800

    improvements on the 3d plot api

--- src/runtime/visualbasic_App/gr/Microsoft.VisualBasic.Imaging/Drawing3D/GDIDevice.vb
+++ src/runtime/visualbasic_App/gr/Microsoft.VisualBasic.Imaging/Drawing3D/GDIDevice.vb
@@ -1,33 +1,35 @@
-﻿#Region "Microsoft.VisualBasic::fe831b2e35a955c175751be200e64844, ..\visualbasic_App\gr\Microsoft.VisualBasic.Imaging\Drawing3D\GDIDevice.vb"
+﻿#Region "Microsoft.VisualBasic::2237640c37e7aaa21a547af928a3caf1, ..\visualbasic_App\gr\Microsoft.VisualBasic.Imaging\Drawing3D\GDIDevice.vb"
 
-' Author:
-' 
-'       asuka (amethyst.asuka@gcmodeller.org)
-'       xieguigang (xie.guigang@live.com)
-'       xie (genetics@smrucc.org)
-' 
-' Copyright (c) 2016 GPL3 Licensed
-' 
-' 
-' GNU GENERAL PUBLIC LICENSE (GPL3)
-' 
-' This program is free software: you can redistribute it and/or modify
-' it under the terms of the GNU General Public License as published by
-' the Free Software Foundation, either version 3 of the License, or
-' (at your option) any later version.
-' 
-' This program is distributed in the hope that it will be useful,
-' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-' GNU General Public License for more details.
-' 
-' You should have received a copy of the GNU General Public License
-' along with this program. If not, see <http://www.gnu.org/licenses/>.
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    '       xie (genetics@smrucc.org)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Drawing
 Imports System.Windows.Forms
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Parallel.Tasks
 
 Namespace Drawing3D
 
@@ -38,6 +40,29 @@ Namespace Drawing3D
 
         Protected WithEvents _animationLoop As Timer
         Protected camera As Camera
+        Protected models As New List(Of I3DModel)
+
+        Dim _rotationThread As New UpdateThread(
+            1000, Sub()
+                      SyncLock camera
+                          camera.angleX += 0.01
+                          camera.angleY += 0.01
+                          camera.angleZ += 0.01
+                      End SyncLock
+                  End Sub)
+
+        Public Property AutoRotation As Boolean
+            Get
+                Return _rotationThread.Running
+            End Get
+            Set(value As Boolean)
+                If value Then
+                    _rotationThread.Start()
+                Else
+                    _rotationThread.Stop()
+                End If
+            End Set
+        End Property
 
         ''' <summary>
         ''' Enable double-buffering to eliminate flickering.
@@ -46,7 +71,9 @@ Namespace Drawing3D
         ''' <param name="e"></param>
         Private Sub GDIDevice_Load(sender As Object, e As EventArgs) Handles Me.Load
             camera = New Camera With {
-                .angle = 0,
+                .angleX = 0,
+                .angleY = 0,
+                .angleZ = 0,
                 .fov = 256,
                 .screen = Size,
                 .ViewDistance = -40
@@ -93,8 +120,12 @@ Namespace Drawing3D
             Throw New Exception("Please Implements the control code at here.")
         End Sub
 
-        Protected Overridable Sub __updateGraphics(sender As Object, g As Graphics, region As Rectangle)
-            Throw New Exception("Please Implements the graphics updates code at here.")
+        Protected Overridable Sub __updateGraphics(sender As Object, ByRef g As Graphics, region As Rectangle)
+            Call g.Clear(Color.LightBlue)
+
+            For Each model As I3DModel In models
+                Call model.Copy(camera.Rotate(model)).Draw(g, camera)
+            Next
         End Sub
 
         Private Sub GDIDevice_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
@@ -112,31 +143,53 @@ Namespace Drawing3D
 
         End Sub
 
-        Dim rotate As Boolean
-        Dim angle!
+        Dim _rotate As Boolean
 
-        Public Event RotateCamera(angle!)
+        Public Event RotateCamera(angleX!, angleY!, angleZ!)
 
         Private Sub GDIDevice_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
-            rotate = True
+            _rotate = True
         End Sub
 
         Private Sub GDIDevice_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
-            If Not rotate Then
-                Return
+            If _rotate Then
+                camera.angleX += 1
+                camera.angleY += 1
+                camera.angleZ += 1
             End If
 
-            angle += 1
+            RaiseEvent RotateCamera(camera.angleX, camera.angleY, camera.angleZ)
+        End Sub
 
-            RaiseEvent RotateCamera(angle)
+        Public Sub RotateX(angle!)
+            camera.angleX = angle
+        End Sub
+
+        Public Sub RotateY(angle!)
+            camera.angleY = angle
+        End Sub
+
+        Public Sub RotateZ(angle!)
+            camera.angleZ = angle
+        End Sub
+
+        Public Sub Rotate(angle As Point3D)
+            camera.angleX = angle.X
+            camera.angleY = angle.Y
+            camera.angleZ = angle.Z
         End Sub
 
         Private Sub GDIDevice_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
-            rotate = False
+            _rotate = False
         End Sub
 
         Private Sub GDIDevice_Resize(sender As Object, e As EventArgs) Handles Me.Resize
             camera.screen = Size
+        End Sub
+
+        Private Sub GDIDevice_MouseWheel(sender As Object, e As MouseEventArgs) Handles Me.MouseWheel
+            Dim d% = Math.Sign(e.Delta)
+            camera.ViewDistance += d
         End Sub
     End Class
 End Namespace
--- src/runtime/visualbasic_App/gr/Microsoft.VisualBasic.Imaging/Drawing3D/Surface.vb
+++ src/runtime/visualbasic_App/gr/Microsoft.VisualBasic.Imaging/Drawing3D/Surface.vb
@@ -1,4 +1,4 @@
-﻿#Region "Microsoft.VisualBasic::a032ffe3db256e30ff2f69a03c92cf67, ..\visualbasic_App\gr\Microsoft.VisualBasic.Imaging\Drawing3D\Surface.vb"
+﻿#Region "Microsoft.VisualBasic::05e18b6fb7af3236820b0a323b12200e, ..\visualbasic_App\gr\Microsoft.VisualBasic.Imaging\Drawing3D\Surface.vb"
 
     ' Author:
     ' 
@@ -26,13 +26,79 @@
 
 #End Region
 
+Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports Microsoft.VisualBasic.Linq
+
 Namespace Drawing3D
 
+    Public Interface I3DModel : Inherits IEnumerable(Of Point3D)
+
+        Function Copy(data As IEnumerable(Of Point3D)) As I3DModel
+
+        Sub Draw(ByRef canvas As Graphics, camera As Camera)
+    End Interface
+
     Public Class Surface
+        Implements IEnumerable(Of Point3D)
+        Implements I3DModel
 
         ''' <summary>
-        ''' 请注意，在这里面的点都是有顺序分别的
+        ''' 请注意，在这里面的点都是有先后顺序分别的
         ''' </summary>
-        Protected _vertices() As Point3D
+        Public vertices() As Point3D
+        Public brush As Brush
+
+        Dim path As Point()
+
+        Public Sub Allocation()
+            path = New Point(vertices.Length - 1) {}
+        End Sub
+
+        Public Sub Draw(ByRef canvas As Graphics, camera As Camera) Implements I3DModel.Draw
+            SyncLock path
+                Dim polygon As New GraphicsPath
+
+                For Each pt As SeqValue(Of Point3D) In camera.Project(vertices).SeqIterator
+                    path(pt.i) = pt.obj.PointXY(camera.screen)
+                Next
+
+                Dim a As Point = path(0)
+                Dim b As Point
+
+                For i As Integer = 1 To path.Length - 1
+                    b = path(i)
+                    Call polygon.AddLine(a, b)
+                    a = b
+                Next
+
+                Call polygon.AddLine(a, path(0))
+                Call polygon.CloseFigure()
+#If DEBUG Then
+                Call canvas.DrawPath(Pens.Black, polygon)
+#End If
+                Call canvas.FillPath(brush, polygon)
+            End SyncLock
+        End Sub
+
+        Public Iterator Function GetEnumerator() As IEnumerator(Of Point3D) Implements IEnumerable(Of Point3D).GetEnumerator
+            For Each pt As Point3D In vertices
+                Yield pt
+            Next
+        End Function
+
+        Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+            Yield GetEnumerator()
+        End Function
+
+        Public Function Copy(data As IEnumerable(Of Point3D)) As I3DModel Implements I3DModel.Copy
+            Dim model As New Surface With {
+                .brush = brush,
+                .vertices = data.ToArray
+            }
+            Call model.Allocation()
+
+            Return model
+        End Function
     End Class
 End Namespace
