a0b551cfe1038f00a010e3a84dc7cd43b6e16f75
xieguigang <xie.guigang@gmail.com>
2016-11-05 09:21:11 +0800

    fix visualization tools

--- src/GCModeller/visualize/visualizeTools/ComparativeGenomics/DrawingDevice.vb
+++ src/GCModeller/visualize/visualizeTools/ComparativeGenomics/DrawingDevice.vb
@@ -1,31 +1,32 @@
 ﻿#Region "Microsoft.VisualBasic::e6adf157c4aabf207cf11ad41cc3dd32, ..\GCModeller\visualize\visualizeTools\ComparativeGenomics\DrawingDevice.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
+Imports System.Drawing.Drawing2D
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
 Imports Microsoft.VisualBasic.Imaging
 
@@ -105,7 +106,7 @@ Namespace ComparativeGenomics
             For Each Link In Model.Links
                 Dim r1 As Rectangle = RegionData1(Link.genome1)
                 Dim r2 As Rectangle = RegionData2(Link.genome2)
-                Dim drModel As New Drawing2D.GraphicsPath
+                Dim drModel As New GraphicsPath
 
                 Dim p1, p2, p3, p4 As Point
                 p1 = New Point(r1.Location.X, r1.Location.Y + r1.Height + 3)
