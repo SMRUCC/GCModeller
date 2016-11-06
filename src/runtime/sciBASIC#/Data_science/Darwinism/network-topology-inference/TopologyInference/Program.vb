82c37e4d0f048717f81e81885a0655d43285299a
xieguigang <xie.guigang@gmail.com>
2016-11-07 00:21:59 +0800

    bugs fixed in the data frame system and add custom parser in the csv

---
+++ src/runtime/sciBASIC#/Data_science/Darwinism/network-topology-inference/TopologyInference/Program.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.Text
+
+Module Program
+
+    Sub Main()
+        Dim network As New NetworkDemo
+        Dim result = network.Solve(12000, 0, 5)
+
+        Call result.DataFrame("#").Save("./network.csv", Encodings.ASCII)
+    End Sub
+End Module
---
+++ src/runtime/sciBASIC#/www/Microsoft.VisualBasic.NETProtocol/Test/Module1.vb
@@ -0,0 +1,8 @@
+﻿Module Module1
+
+    Sub Main()
+        For Each s In Microsoft.VisualBasic.Net.HTTP.WebExtensions.DownloadAllLinks("http://120.76.195.65/index.html", "x:\")
+            Call s.Warning
+        Next
+    End Sub
+End Module
