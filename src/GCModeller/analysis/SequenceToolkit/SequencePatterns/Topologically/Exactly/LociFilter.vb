96d5d27f4220726f9d766345835113a68f9e31ff
xieguigang <xie.guigang@gmail.com>
2016-11-13 23:47:32 +0800

    loci range selects tools

--- src/GCModeller/analysis/SequenceToolkit/SequencePatterns/Topologically/Exactly/LociFilter.vb
+++ src/GCModeller/analysis/SequenceToolkit/SequencePatterns/Topologically/Exactly/LociFilter.vb
@@ -1,38 +1,78 @@
 ﻿#Region "Microsoft.VisualBasic::55fdc3777910fb501e877ccb07f7c411, ..\GCModeller\analysis\SequenceToolkit\SequencePatterns\Topologically\Exactly\LociFilter.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.Ranges
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.genomics.Analysis.SequenceTools.SequencePatterns.Topologically
+Imports SMRUCC.genomics.ComponentModel.Loci.Abstract
 
 Public Module LociFilter
+
+    <Extension>
+    Public Iterator Function RangeSelects(Of T As ILoci)(range As IntRange, data As IEnumerable(Of NamedValue(Of T())), lociProvider As Func(Of T, Integer())) As IEnumerable(Of NamedValue(Of T()))
+        For Each part As NamedValue(Of T()) In data
+            Dim out As New List(Of T)
+
+            For Each x As T In part.x
+                If range.InsideAny(lociProvider(x)) Then
+                    Call out.Add(x)
+                End If
+            Next
+
+            Yield New NamedValue(Of T()) With {
+                .Name = part.Name,
+                .x = out
+            }
+        Next
+    End Function
+
+    <Extension>
+    Public Function RangeSelects(range As IntRange, data As IEnumerable(Of NamedValue(Of RepeatsView()))) As IEnumerable(Of NamedValue(Of RepeatsView()))
+        Return range.RangeSelects(data, Function(x) x.Locis.JoinIterates(x.Left).ToArray)
+    End Function
+
+    <Extension>
+    Public Function RangeSelects(range As IntRange, data As IEnumerable(Of NamedValue(Of RevRepeatsView()))) As IEnumerable(Of NamedValue(Of RevRepeatsView()))
+        Return range.RangeSelects(data, Function(x) x.Locis.JoinIterates(x.Left).JoinIterates(x.RevLocis).Distinct.ToArray)
+    End Function
+
+    <Extension>
+    Public Function RangeSelects(range As IntRange, data As IEnumerable(Of NamedValue(Of PalindromeLoci()))) As IEnumerable(Of NamedValue(Of PalindromeLoci()))
+        Return range.RangeSelects(data, Function(x) x.Start.Join({x.PalEnd, x.Mirror}).ToArray)
+    End Function
+
+    <Extension>
+    Public Function RangeSelects(range As IntRange, data As IEnumerable(Of NamedValue(Of ImperfectPalindrome()))) As IEnumerable(Of NamedValue(Of ImperfectPalindrome()))
+        Return range.RangeSelects(data, Function(x) {x.Left, x.Paloci})
+    End Function
 
     Public Enum Compares
         ''' <summary>
