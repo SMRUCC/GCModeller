99f8eb32f9011bfac6aba40823b627018c72ee17
xieguigang <xie.guigang@gmail.com>
2016-11-14 05:36:23 +0800

    try optimization for parallel on linux platform

--- src/GCModeller/CLI_tools/VirtualFootprint/CLI/Cluster.vb
+++ src/GCModeller/CLI_tools/VirtualFootprint/CLI/Cluster.vb
@@ -28,12 +28,22 @@ Partial Module CLI
     End Function
 
     Public Function BinaryKmeans(seq As FastaFile, Optional cutoff# = 0.65, Optional minW% = 6) As EntityLDM()
-        Dim LQuery = From a As FastaToken
-                     In seq.AsParallel
-                     Let Name As String = a.Title
+        ' 并行算法似乎會因爲内存資源的讀取問題而在linux平臺上面出現較高的系統CPU時間
+        ' 在這裏創建新對象來解決這個問題
+        Dim list As New List(Of KeyValuePair(Of FastaToken, FastaFile))
+
+        For Each x As FastaToken In seq
+            list.Add(New KeyValuePair(Of FastaToken, FastaFile)(
+                     x.Copy,
+                     TryCast(seq.Clone, FastaFile)))
+        Next
+
+        Dim LQuery = From a As KeyValuePair(Of FastaToken, FastaFile)
+                     In list.AsParallel  ' 在這裏使用clone而非直接使用原始的對象是爲了提升linux平臺上面的并行計算效率
+                     Let Name As String = a.Key.Title
                      Select New EntityLDM With {
                          .Name = Name,
-                         .Properties = a.Cluster(seq, cutoff, minW)
+                         .Properties = a.Key.Cluster(a.Value, cutoff, minW)
                      }
         Dim tree As EntityLDM() = LQuery.TreeCluster(True)
         Return tree
@@ -41,12 +51,14 @@ Partial Module CLI
 
     <Extension>
     Private Function Cluster(query As FastaToken, source As FastaFile, cutoff#, minW%) As Dictionary(Of String, Double)
+        Dim matrix As Blosum = Blosum.FromInnerBlosum62
         Dim LQuery = From b As FastaToken
                      In source
-                     Let sw As SmithWaterman = SmithWaterman.Align(query, b)
+                     Let sw As SmithWaterman = SmithWaterman.Align(query, b, matrix)
                      Let out As HSP = sw.GetOutput(cutoff, minW).Best
                      Select b.Title,
                          score = If(out Is Nothing, -10.0R, out.Score)
+
         Dim output As Dictionary(Of String, Double) =
             LQuery.ToDictionary(Function(x) x.Title,
                                 Function(x) x.score)
