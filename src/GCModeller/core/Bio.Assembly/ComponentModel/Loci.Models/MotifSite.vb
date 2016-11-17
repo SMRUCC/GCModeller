df712d42a17b213569ad2c8c68cb19151f76cce6
xieguigang <xie.guigang@gmail.com>
2016-11-17 20:11:28 +0800

    motif site model

---
+++ src/GCModeller/core/Bio.Assembly/ComponentModel/Loci.Models/MotifSite.vb
@@ -0,0 +1,9 @@
+﻿Namespace ComponentModel.Loci
+
+    Public Interface IMotifSite
+
+        Property Type As String
+        Property Name As String
+        Property Site As Location
+    End Interface
+End Namespace
