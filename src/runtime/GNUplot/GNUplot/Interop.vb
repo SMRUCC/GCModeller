453d068c4570385eea04aa46cb0567d2433d94a4
xieguigang <xie.guigang@gmail.com>
2016-11-21 04:03:39 +0800

    gpl3

--- src/runtime/GNUplot/GNUplot/Interop.vb
+++ src/runtime/GNUplot/GNUplot/Interop.vb
@@ -1,4 +1,32 @@
-﻿Imports System.Diagnostics
+﻿#Region "Microsoft.VisualBasic::b074aea1cbb13454449f47e2fb496896, ..\GNUplot\GNUplot\Interop.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    '       xie (genetics@smrucc.org)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports System.Diagnostics
 Imports System.IO
 
 ''' <summary>
@@ -108,3 +136,4 @@ Public Class Interop : Implements IDisposable
     End Sub
 #End Region
 End Class
+
--- src/runtime/GNUplot/Test/Program.vb
+++ src/runtime/GNUplot/Test/Program.vb
@@ -1,4 +1,32 @@
-﻿Module Program
+﻿#Region "Microsoft.VisualBasic::42f1b731579d3b88ae6b37c742d53294, ..\GNUplot\Test\Program.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    '       xie (genetics@smrucc.org)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Module Program
 
     Sub Main()
         ' Call plotArray()
@@ -88,3 +116,4 @@
         GNUplot.SPlot(X, Y, Z, "with points pointtype 8 lc rgb 'blue'")
     End Sub
 End Module
+
