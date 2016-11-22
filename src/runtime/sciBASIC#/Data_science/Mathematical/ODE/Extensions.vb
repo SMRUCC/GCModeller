10fcac05b2aab1ff63f9505018fe26e889bf1229
xieguigang <xie.guigang@gmail.com>
2016-11-23 02:06:45 +0800

    code tools for s-system nonlinear system simulations

--- src/runtime/sciBASIC#/Data_science/Mathematical/ODE/Extensions.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematical/ODE/Extensions.vb
@@ -12,9 +12,21 @@ Public Module Extensions
     ''' <returns></returns>
     <Extension> Public Function Pcc(df As ODEsOut) As DataSet()
         Dim out As New List(Of DataSet)
+        Dim vars$() = df.y.Keys.ToArray
 
         For Each var As NamedValue(Of Double()) In df
+            Dim x As New DataSet With {
+                .Identifier = var.Name,
+                .Properties = New Dictionary(Of String, Double)
+            }
 
+            For Each name$ In vars
+                Dim __pcc# = Correlations _
+                    .GetPearson(var.Value, df.y(name).Value)
+                x.Properties(name$) = __pcc
+            Next
+
+            out += x
         Next
 
         Return out
@@ -27,6 +39,23 @@ Public Module Extensions
     ''' <returns></returns>
     <Extension> Public Function SPcc(df As ODEsOut) As DataSet()
         Dim out As New List(Of DataSet)
+        Dim vars$() = df.y.Keys.ToArray
+
+        For Each var As NamedValue(Of Double()) In df
+            Dim x As New DataSet With {
+                .Identifier = var.Name,
+                .Properties = New Dictionary(Of String, Double)
+            }
+
+            For Each name$ In vars
+                Dim __spcc# = Correlations _
+                    .Spearman(var.Value, df.y(name).Value)
+                x.Properties(name$) = __spcc
+            Next
+
+            out += x
+        Next
+
         Return out
     End Function
 End Module
