15f73ddc912913decb70c5978e1fd42770f8c335
xieguigang <xie.guigang@gmail.com>
2016-11-26 18:48:28 +0800

    fix a fasta file encoding bugs, record this bugs note

--- src/GCModeller/CLI_tools/GCModeller/CLI/CLI.vb
+++ src/GCModeller/CLI_tools/GCModeller/CLI/CLI.vb
@@ -190,18 +190,22 @@ Public Module CLI
     ''' <param name="args"></param>
     ''' <returns></returns>
     ''' <remarks>
-    ''' ###### 2016-11-26: 合并fasta文件的时候会出现问题？
+    ''' ###### 2016-11-26: 合并fasta文件的时候会出现问题？ 已解决，是输出的文件的textEncoding的问题，fasta文件应该是ASCII的
     ''' ```
     ''' BLAST options error: D:\11.24\Aedes-Anopheles-Culex.fasta does not match input format type, default input type is FASTA
     ''' ```
     ''' </remarks>
     <ExportAPI("/Merge.Files",
-               Info:="Tools that works on the text files merged.",
+               Info:="Tools that works on the text files merge operation. This tool is usually used for merge of the fasta files into a larger fasta file.",
                Usage:="/Merge.Files /in <in.DIR> [/trim /ext <*.txt> /encoding <ascii> /out <out.txt>]")>
+    <Group(CLIGrouping.GCModellerAppTools)>
+    <Argument("/encoding", True, CLITypes.String,
+              AcceptTypes:={GetType(Encodings)},
+              Description:="Specific the output text file encoding value, default is ASCII encoding. Fasta file merge must be ASCII encoding output")>
     Public Function FileMerges(args As CommandLine) As Integer
         Dim [in] As String = args("/in")
         Dim ext As String = args.GetValue("/ext", "*.txt")
-        Dim out As String = args.GetValue("/out", [in].TrimDIR & ".txt")
+        Dim out As String = args.GetValue("/out", [in].TrimDIR & ext.Replace("*", ""))
         Dim exts = ext.Split(","c).ToArray(AddressOf Trim)
         Dim trimNull = args.GetBoolean("/trim")
         Dim encoding As String = args.GetValue("/encoding", "ascii")
