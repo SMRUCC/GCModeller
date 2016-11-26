3203434ca0d8d755b37eb996bff812ca0567ec7f
xieguigang <xie.guigang@gmail.com>
2016-11-26 21:12:47 +0800

    cytoscape tools for metagenome network

---
+++ src/Settings/Shared/Settings.Configuration/Session/Apps.vb
@@ -0,0 +1,19 @@
+﻿''' <summary>
+''' A list of GCModeller apps 
+''' </summary>
+Public Module Apps
+
+    Public ReadOnly Property NCBI_tools As String
+    Public ReadOnly Property localblast As String
+    Public ReadOnly Property MEME As String
+    Public ReadOnly Property KEGG_tools As String
+    Public ReadOnly Property seqtools As String
+
+    Sub New()
+        NCBI_tools = App.HOME & "/NCBI_tools.exe"
+        localblast = App.HOME & "/localblast.exe"
+        MEME = App.HOME & "/MEME.exe"
+        KEGG_tools = App.HOME & "/KEGG_tools.exe"
+        seqtools = App.HOME & $"/{NameOf(seqtools)}.exe"
+    End Sub
+End Module
--- src/interops/visualize/Cytoscape/CLI_tool/CLI/Metagenomics.vb
+++ src/interops/visualize/Cytoscape/CLI_tool/CLI/Metagenomics.vb
@@ -30,6 +30,7 @@ Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Text
 Imports SMRUCC.genomics.Assembly.NCBI.Taxonomy
@@ -40,6 +41,7 @@ Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput.BlastPlu
 Imports SMRUCC.genomics.Model.Network.BLAST.API
 Imports SMRUCC.genomics.Model.Network.BLAST.BBHAPI
 Imports SMRUCC.genomics.Model.Network.BLAST.LDM
+Imports SMRUCC.genomics.Model.Network.BLAST.Metagenome
 
 Partial Module CLI
 
@@ -163,21 +165,53 @@ Partial Module CLI
 
     <ExportAPI("/BLAST.Metagenome.SSU.Network",
                Info:="> Viral assemblage composition in Yellowstone acidic hot springs assessed by network analysis, DOI: 10.1038/ismej.2015.28",
-               Usage:="/BLAST.Metagenome.SSU.Network /net <blastn.self.txt> /tax <ssu-nt.blastnMaps.csv> /x2taxid <x2taxid.dmp> /taxonomy <ncbi_taxonomy:names,nodes> [/gi2taxid /identities <default:0.3> /coverage <default:0.3> /out <out-net.DIR>]")>
+               Usage:="/BLAST.Metagenome.SSU.Network /net <blastn.self.txt> /tax <ssu-nt.blastnMaps.csv> /x2taxid <x2taxid.dmp/DIR> /taxonomy <ncbi_taxonomy:names,nodes> [/gi2taxid /identities <default:0.3> /coverage <default:0.3> /out <out-net.DIR>]")>
     Public Function SSU_MetagenomeNetwork(args As CommandLine) As Integer
         Dim net$ = args("/net")
         Dim tax$ = args("/tax")
         Dim taxonomy$ = args("/taxonomy")
         Dim identities As Double = args.GetValue("/identities", 0.3)
         Dim coverage As Double = args.GetValue("/coverage", 0.3)
-        Dim out$ = args.GetValue("/out", net.TrimSuffix & "-" & tax.BaseName & "-metagenome-network/")
+        Dim EXPORT$ = args.GetValue("/out", net.TrimSuffix & "-" & tax.BaseName & "-metagenome-network/")
+        Dim out As New Value(Of String)
         Dim netdata As v228 = BlastPlus.Parser.ParsingSizeAuto(net)
         Dim taxdata As BlastnMapping() = tax.LoadCsv(Of BlastnMapping)
         Dim gi2taxid As Boolean = args.GetBoolean("/gi2taxid")
+        Dim x2taxid As String = args("/x2taxid")
         Dim xid$() = taxdata _
             .Select(Function(x) x.Reference) _
             .ToArray(TaxidMaps.GetParser(gi2taxid))
 
+        Call xid.FlushAllLines(out = EXPORT & "/reference_xid.txt")
 
+        ' subset database
+        Dim CLI$
+
+        If gi2taxid Then
+            CLI = $"/gi.Match /in {(+out).CLIPath} /gi2taxid {x2taxid.CLIPath} /out {(out = EXPORT & "/gi2taxid.txt").CLIPath}"
+        Else
+            CLI = $"/accid2taxid.Match /in {(+out).CLIPath} /acc2taxid {x2taxid.CLIPath} /gb_priority /out {(out = EXPORT & "/gi2taxid.txt").CLIPath}"
+        End If
+
+        Call New IORedirectFile(Apps.NCBI_tools, CLI).Run()
+
+        ' step1
+        Dim notFound As New List(Of String)
+        Dim ssuTax As IEnumerable(Of BlastnMapping) = taxdata.TaxonomyMaps(
+            x2taxid:=+out,
+            is_gi2taxid:=gi2taxid,
+            notFound:=notFound,
+            taxonomy:=New NcbiTaxonomyTree(taxonomy))
+
+        Call notFound.FlushAllLines(EXPORT & "/taxonomy_notfound.txt")
+
+        ' step2
+        Dim matrix As IEnumerable(Of DataSet) =
+            netdata.BuildMatrix(identities, coverage)
+
+        ' step3
+        Dim network As Network = BuildNetwork(matrix, ssuTax)
+
+        Return network.Save(EXPORT, Encodings.ASCII).CLICode
     End Function
 End Module
