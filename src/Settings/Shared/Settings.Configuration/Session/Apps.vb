02294acd93949d5967e2ed55e59028015b0cf55f
xieguigang <xie.guigang@gmail.com>
2016-11-26 21:24:17 +0800

    fix conflicts

--- src/Settings/Shared/Settings.Configuration/Session/Apps.vb
+++ src/Settings/Shared/Settings.Configuration/Session/Apps.vb
@@ -1,19 +1,19 @@
 ﻿''' <summary>
 ''' A list of GCModeller apps 
 ''' </summary>
-Public Module Apps
+Public NotInheritable Class Apps
 
-    Public ReadOnly Property NCBI_tools As String
-    Public ReadOnly Property localblast As String
-    Public ReadOnly Property MEME As String
-    Public ReadOnly Property KEGG_tools As String
-    Public ReadOnly Property seqtools As String
+    Public Shared ReadOnly Property NCBI_tools As String
+    Public Shared ReadOnly Property localblast As String
+    Public Shared ReadOnly Property MEME As String
+    Public Shared ReadOnly Property KEGG_tools As String
+    Public Shared ReadOnly Property seqtools As String
 
-    Sub New()
+    Shared Sub New()
         NCBI_tools = App.HOME & "/NCBI_tools.exe"
         localblast = App.HOME & "/localblast.exe"
         MEME = App.HOME & "/MEME.exe"
         KEGG_tools = App.HOME & "/KEGG_tools.exe"
         seqtools = App.HOME & $"/{NameOf(seqtools)}.exe"
     End Sub
-End Module
+End Class
