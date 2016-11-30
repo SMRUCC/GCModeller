30d7a73748dd8dd3de21e0c6fb6a1e19ec99d74b
xieguigang <xie.guigang@gmail.com>
2016-11-30 15:11:45 +0800

    improvements numeric parser

--- src/GCModeller/analysis/SequenceToolkit/SNP/SNP.vb
+++ src/GCModeller/analysis/SequenceToolkit/SNP/SNP.vb
@@ -1,70 +1,105 @@
 ﻿#Region "Microsoft.VisualBasic::2e51650288e373401b3379954147477c, ..\GCModeller\analysis\SequenceToolkit\SNP\SNP.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
-Imports System.Data.Linq.Mapping
+Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
 Imports Microsoft.VisualBasic.Language
 
 ''' <summary>
 ''' SNP site
 ''' </summary>
+''' <remarks>
+''' Min (original sequence)	Max (original sequence)	Change	Coverage	
+''' Polymorphism Type	Reference Nucleotide(s)	Variant Frequency	
+''' Variant Nucleotide(s)	Amino Acid Change	CDS	CDS Codon Number	
+''' CDS Interval	CDS Position	CDS Position Within Codon	Codon Change	
+''' gene	note	product	Protein Effect	protein_id	Variant Raw Frequency	Variant Sequences
+''' </remarks>
 Public Class SNP : Inherits ClassObject
 
-    <Column(Name:="Sequence Name")> Public Property Name As String
-    Public Property Left As Integer
-    Public Property Right As Integer
+    <Column("Sequence Name")>
+    Public Property Name As String
+    <Column("Min (original sequence)")> Public Property Left As Integer
+    <Column("Max (original sequence)")> Public Property Right As Integer
     Public Property Length As Integer
-    <Column(Name:="# Intervals")> Public Property Intervals As Integer
-    <Column(Name:="Length (with gaps)")> Public Property TotalLength As Integer
-    Public Property PolymorphismType As String
-    <Column(Name:="Variant Nucleotide(s)")> Public Property VariantNucleotides As String
-    Public Property Coverage As Double
-    Public Property StrandBias As Double
-    <Column(Name:="Strand-Bias >50% P-value")> Public Property SignificantStrandBias
-    Public Property VariantFrequency As Double
-    Public Property VariantRawFrequency As Double
-    Public Property VariantSequences As String
     Public Property Change As String
+    Public Property Coverage As Double
+
+    <Column("# Intervals")>
+    Public Property Intervals As Integer
+    <Column("Length (with gaps)")>
+    Public Property TotalLength As Integer
+    <Column("Polymorphism Type")>
+    Public Property PolymorphismType As String
+    <Column("Variant Nucleotide(s)")>
+    Public Property VariantNucleotides As String
+
+    Public Property StrandBias As Double
+    <Column("Strand-Bias >50% P-value")>
+    Public Property SignificantStrandBias
+    <Column("Variant Frequency")>
+    Public Property VariantFrequency As Double
+    <Column("Variant Raw Frequency")>
+    Public Property VariantRawFrequency As Double
+    <Collection("Variant Sequences", ", ")>
+    Public Property VariantSequences As String()
+
     Public Property ReferenceFrequency As Double
+    <Column("Reference Nucleotide(s)")>
     Public Property ReferenceNucleotides As String
+
+    Public Property gene As String
+    Public Property note As String
+    Public Property product As String
+    <Column("Protein Effect")>
+    Public Property ProteinEffect As String
+    Public Property protein_id As String
+
     ''' <summary>
     ''' 这个SNP位点可能引起的氨基酸序列上面的残基的变化
     ''' </summary>
     ''' <returns></returns>
+    <Column("Amino Acid Change")>
     Public Property AminoAcidChange As String
     Public Property CDS As String
-    <Column(Name:="CDS Codon Number")> Public Property CodonNumber As String
+    <Column("CDS Codon Number")>
+    Public Property CodonNumber As String
+    <Column("CDS Position")>
     Public Property CDSPosition As String
+    <Column("CDS Position Within Codon")>
     Public Property PositionWithinCodon As String
+    <Column("CDS Interval")>
+    Public Property CDSInterval As String
 
     ''' <summary>
     ''' 
     ''' </summary>
     ''' <returns></returns>
+    <Column("Codon Change")>
     Public Property CodonChange As String
 
 End Class
