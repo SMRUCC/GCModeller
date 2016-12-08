f1a61a156b60fa479edac9231773df1f6b1a1182
xieguigang <xie.guigang@gmail.com>
2016-12-08 13:43:03 +0800

    ga distribute computes bootstrapping

---
+++ src/runtime/sciBASIC.ComputingServices/Bootstrapping/GeneticAlgorithm/DistributeSupports.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic.Data.Bootstrapping.Darwinism.GAF
+Imports Microsoft.VisualBasic.Mathematical.Calculus
+
+Namespace Darwinism.GAF
+
+    ''' <summary>
+    ''' Supports for GA distribute computing
+    ''' </summary>
+    Public Module DistributeSupports
+
+
+        Public Function Fitness(v As ParameterVector, observation As ODEsOut, ignores$()) As Double
+
+        End Function
+    End Module
+End Namespace
