715314a24a7bbe2fa6c2c9f9ea27c68879eb23d4
xieguigang <xie.guigang@gmail.com>
2016-12-25 09:19:19 +0800

    improvements on the heatmap plot tools

--- src/runtime/sciBASIC#/Data_science/Mathematical/Plots/Heatmaps/HeatmapTable.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematical/Plots/Heatmaps/HeatmapTable.vb
@@ -14,16 +14,17 @@ Public Module HeatmapTable
     ''' 只能够用来表示两两变量之间的相关度
     ''' </summary>
     ''' <param name="triangularStyle">
-    ''' 是否上部分显示圆，下部分显示数值？默认是全部都显示圆圈
+    ''' 是否上部分显示圆，下部分显示数值？默认是本三角样式
+    ''' 圆的半径大小用来表示相关度的绝对值，颜色则是和普通的heatmap一样用来表示相关度的大小和方向
     ''' </param>
     ''' <returns></returns>
     Public Function Plot(data As IEnumerable(Of NamedValue(Of Dictionary(Of String, Double))),
-                         Optional mapLevels% = 100,
+                         Optional mapLevels% = 20,
                          Optional mapName$ = ColorMap.PatternJet,
                          Optional size As Size = Nothing,
                          Optional margin As Size = Nothing,
                          Optional bg$ = "white",
-                         Optional triangularStyle As Boolean = False,
+                         Optional triangularStyle As Boolean = True,
                          Optional fontStyle$ = CSSFont.Win10Normal,
                          Optional legendTitle$ = "Heatmap Color Legend",
                          Optional legendFont As Font = Nothing,
@@ -31,72 +32,50 @@ Public Module HeatmapTable
                          Optional max# = 1,
                          Optional mainTitle$ = "heatmap",
                          Optional titleFont As Font = Nothing,
-                         Optional drawGrid As Boolean = True,
+                         Optional drawGrid As Boolean = False,
                          Optional drawValueLabel As Boolean = True,
                          Optional valuelabelFont As Font = Nothing) As Bitmap
 
-        Dim colors As Color() = Designer.GetColors(mapName, mapLevels)
-        Dim font As Font = CSSFont.TryParse(fontStyle).GDIObject
-        Dim array As NamedValue(Of
-            Dictionary(Of String, Double))() = data.ToArray
-        Dim angle! = 45.0F
+        Return Heatmap.__plotInterval(
+            Sub(g, region, array, left, font, dw, levels, top, colors)
 
-        If margin.IsEmpty Then
-            Dim maxLabel As String = LinqAPI.DefaultFirst(Of String) <=
-                From x
-                In array
-                Select x.Name
-                Order By Name.Length Descending
-
-            Dim sz As Size = maxLabel.MeasureString(font)
-
-            margin = New Size(sz.Width * 1.5, sz.Width * 1.5)
-        End If
-
-        size = If(size.IsEmpty, New Size(2000, 1600), size)
-
-        Return GraphicsPlots(
-            size, margin,
-            bg$,
-            Sub(ByRef g, region)
-                Dim dw!? = CSng((size.Height - 2 * margin.Width) / array.Length)
-                Dim correl#() = array _
-                    .Select(Function(x) x.Value.Values) _
-                    .IteratesALL _
-                    .Join(min, max) _
-                    .Distinct _
-                    .ToArray
-                Dim lvs As Dictionary(Of Double, Integer) =
-                    correl _
-                    .GenerateMapping(mapLevels, offset:=0) _
-                    .SeqIterator _
-                    .ToDictionary(Function(x) correl(x.i),
-                                  Function(x) x.value)
-
-                Dim left! = margin.Width, top! = margin.Height
-                Dim blockSize As New SizeF(dw, dw)
+                ' 在绘制上三角的时候假设每一个对象的keys的顺序都是相同的
                 Dim keys$() = array(Scan0).Value.Keys.ToArray
-
-                If colors.IsNullOrEmpty Then
-                    colors = Designer.GetColors(mapName, mapLevels)
-                End If
+                Dim blockSize As New SizeF(dw, dw)  ' 每一个方格的大小
 
                 If valuelabelFont Is Nothing Then
                     valuelabelFont = New Font(FontFace.CambriaMath, 16, Drawing.FontStyle.Bold)
                 End If
 
-                For Each x As NamedValue(Of Dictionary(Of String, Double)) In array
-                    For Each key$ In keys
-                        Dim c# = x.Value(key)
-                        Dim level% = lvs(c#)  '  得到等级
-                        Dim color As Color = colors(
-                            If(level% > colors.Length - 1,
-                            colors.Length - 1,
-                            level))
-                        Dim rect As New RectangleF(New PointF(left, top), blockSize)
-                        Dim b As New SolidBrush(color)
+                margin = region.Margin
 
-                        Call g.FillRectangle(b, rect)
+                For Each x As SeqValue(Of NamedValue(Of Dictionary(Of String, Double))) In array.SeqIterator(offset:=1)  ' 在这里绘制具体的矩阵
+
+                    Dim i% = 1
+
+                    For Each key$ In keys
+                        Dim c# = (+x).Value(key)
+                        Dim rect As New RectangleF(New PointF(left, top), blockSize)
+                        Dim labelbrush As SolidBrush
+
+                        If triangularStyle AndAlso i <= x.i Then  ' 只显示数值，而不绘制圆圈
+                            labelbrush = Brushes.Black
+                        Else
+                            Dim level% = levels(c#)  '  得到等级
+                            Dim color As Color = colors(   ' 得到当前的方格的颜色
+                                If(level% > colors.Length - 1,
+                                colors.Length - 1,
+                                level))
+
+                            Dim b As New SolidBrush(color)
+                            Dim r As Single = Math.Abs(c) * dw / 2 ' 计算出半径的大小
+                            Dim d = dw / 2 - r
+
+                            r *= 2
+                            labelbrush = Brushes.White
+
+                            Call g.FillPie(b, rect.Left + d, rect.Top + d, r, r, 0, 360)
+                        End If
 
                         If drawGrid Then
                             Call g.DrawRectangles(Pens.WhiteSmoke, {rect})
@@ -105,62 +84,25 @@ Public Module HeatmapTable
                             key = c.FormatNumeric(2)
                             Dim ksz As SizeF = g.MeasureString(key, valuelabelFont)
                             Dim kpos As New PointF(rect.Left + (rect.Width - ksz.Width) / 2, rect.Top + (rect.Height - ksz.Height) / 2)
-                            Call g.DrawString(key, valuelabelFont, Brushes.White, kpos)
+                            Call g.DrawString(key, valuelabelFont, labelbrush, kpos)
                         End If
 
-                        left += dw!
+                        left.value += dw!
+                        i += 1
                     Next
 
-                    left = margin.Width
-                    top += dw!
+                    left.value = margin.Width
+                    top.value += dw!
 
-                    Dim sz As SizeF = g.MeasureString(x.Name, font)
-                    Dim y As Single = top - dw - (sz.Height - dw) / 2
-                    Dim lx As Single =
-                        margin.Width - sz.Width - margin.Width * 0.1
+                    Dim sz As SizeF = g.MeasureString((+x).Name, font)
+                    Dim y As Single = top.value - dw - (sz.Height - dw) / 2
+                    Dim lx! = margin.Width - sz.Width - margin.Width * 0.1
 
-                    Call g.DrawString(x.Name, font, Brushes.Black, New PointF(lx, y))
+                    Call g.DrawString((+x).Name, font, Brushes.Black, New PointF(lx, y))
                 Next
 
-                angle = -angle
-                left += dw / 2
-
-                For Each key$ In keys
-                    Dim sz = g.MeasureString(key$, font) ' 得到斜边的长度
-                    Dim dx! = sz.Width * Math.Cos(angle)
-                    Dim dy! = sz.Width * Math.Sin(angle)
-                    Call g.DrawString(key$, font, Brushes.Black, left - dx, top - dy, angle)
-                    left += dw
-                Next
-
-                ' Draw legends
-                Dim legend As Bitmap = colors.ColorMapLegend(
-                    haveUnmapped:=False,
-                    min:=Math.Round(correl.Min, 1),
-                    max:=Math.Round(correl.Max, 1),
-                    title:=legendTitle,
-                    titleFont:=legendFont)
-                Dim lsize As Size = legend.Size
-                Dim lmargin As Integer = size.Width - size.Height + margin.Width
-
-                left = size.Width - lmargin
-                top = size.Height / 3
-
-                Dim scale# = lmargin / lsize.Width
-                Dim lh% = CInt(scale * (size.Height * 2 / 3))
-
-                Call g.DrawImage(
-                    legend, CInt(left), CInt(top), lmargin, lh)
-
-                If titleFont Is Nothing Then
-                    titleFont = New Font(FontFace.BookmanOldStyle, 30, Drawing.FontStyle.Bold)
-                End If
-
-                Dim titleSize = g.MeasureString(mainTitle, titleFont)
-                Dim titlePosi As New PointF((left - titleSize.Width) / 2, (margin.Height - titleSize.Height) / 2)
-
-                Call g.DrawString(mainTitle, titleFont, Brushes.Black, titlePosi)
-
-            End Sub)
+            End Sub,
+            data.ToArray,,
+            mapLevels, mapName, size, margin, bg, fontStyle, legendTitle, legendFont, min, max, mainTitle, titleFont)
     End Function
 End Module
--- src/runtime/sciBASIC#/Data_science/Mathematical/Plots/Testing/Module1.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematical/Plots/Testing/Module1.vb
@@ -38,6 +38,7 @@ Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 Imports Microsoft.VisualBasic.Imaging.Drawing3D
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Mathematical.Correlations
 Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS
 Imports Microsoft.VisualBasic.Serialization.JSON
 
@@ -193,7 +194,23 @@ Module Module1
     .SaveAs("./3d-heatmap.png")
     End Sub
 
+    Public Sub heatmap2()
+
+        Dim data = LoadData("G:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematical\images\heatmap\Sample.csv", True)
+        Dim spcc = data.CorrelationMatrix(AddressOf Spearman)
+
+        Call HeatmapTable.Plot(spcc,) _
+            .SaveAs("G:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematical\images\heatmap\Sample.SPCC.png")
+        Call Heatmap.Plot(spcc, mapLevels:=25) _
+            .SaveAs("G:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematical\images\heatmap\Sample.heatmap.png")
+
+    End Sub
+
     Sub Main()
+        Call heatmap2()
+
+        Pause()
+
         Call d3heatmap()
         Pause()
         Call scatterHeatmapTest()
