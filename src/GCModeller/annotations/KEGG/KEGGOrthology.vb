754819ca150f5582f0eaed1614db2e0fb95628e6
xieguigang <xie.guigang@gmail.com>
2017-01-07 16:22:48 +0800

    noit sure for the annotation pipline

---
+++ src/GCModeller/annotations/KEGG/KEGGOrthology.vb
@@ -0,0 +1,8 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Public Class KEGGOrthology
+
+    Public Function CatalogProfiling() As NamedValue(Of Integer)()
+
+    End Function
+End Class
