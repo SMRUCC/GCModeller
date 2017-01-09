5b7d37ee41e22c9139366c0ba847000023b3dcb9
xieguigang <xie.guigang@gmail.com>
2017-01-10 00:26:13 +0800

    improvements on the repository model

---
+++ src/GCModeller/annotations/KEGG/PathwayMap.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+
+Public Module PathwayMapVisualize
+
+    <Extension>
+    Public Function BuildModel(ref As PathwayMap, reaction As IRepositoryRead(Of String, Reaction)) As Network
+
+    End Function
+End Module
