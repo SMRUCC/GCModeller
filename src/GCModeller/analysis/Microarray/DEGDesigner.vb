d8d6d70f6a35ec1550d61fa01ca6d121b24e856d
xieguigang <xie.guigang@gmail.com>
2017-01-11 18:17:41 +0800

    log2 designer

---
+++ src/GCModeller/analysis/Microarray/DEGDesigner.vb
@@ -0,0 +1,58 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.csv.DocumentStream
+Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Scripting
+Imports csv = Microsoft.VisualBasic.Data.csv.DocumentStream.File
+
+Public Module DEGDesigner
+
+    <Extension>
+    Public Iterator Function log2(data As IEnumerable(Of EntityObject), designers As Designer(), Optional label$ = Nothing) As IEnumerable(Of EntityObject)
+        For Each gene As EntityObject In data
+            For Each designer In designers
+                Dim tag$ = $"log2({designer.ToString})"
+                gene.Properties(tag) = designer.Log2(gene, label)
+            Next
+
+            Yield gene
+        Next
+    End Function
+
+    Public Function log2(path$, designers As Designer(), Optional label$ = Nothing) As csv
+        Dim genes As EntityObject() = EntityObject.LoadDataSet(path).ToArray
+        Dim out As EntityObject() = genes.log2(designers, label).ToArray
+        Dim csv As csv = out.ToCsvDoc
+        Return csv
+    End Function
+
+    Public Structure Designer
+
+        ''' <summary>
+        ''' 分子
+        ''' </summary>
+        Dim Experiment$
+        ''' <summary>
+        ''' 分母
+        ''' </summary>
+        Dim Control$
+
+        Public Overrides Function ToString() As String
+            Return $"{Experiment}/{Control}"
+        End Function
+
+        Public Function Log2(gene As EntityObject, Optional label$ = Nothing) As Double
+            Dim A#, B#
+
+            If label Is Nothing Then
+                A = gene(Experiment).ParseNumeric
+                B = gene(Control).ParseNumeric
+            Else
+                A = gene(label & "." & Experiment).ParseNumeric
+                B = gene(label & "." & Control).ParseNumeric
+            End If
+
+            Dim out As Double = Math.Log(A / B, 2)
+            Return out
+        End Function
+    End Structure
+End Module
--- src/GCModeller/annotations/Proteomics/Test.vb
+++ src/GCModeller/annotations/Proteomics/Test.vb
@@ -1,7 +1,30 @@
-﻿Module Test
+﻿Imports SMRUCC.genomics.Analysis.Microarray.DEGDesigner
+
+Module Test
 
     Sub Main()
-        Call GetProteinDefs("C:\Users\xieguigang\OneDrive\1.5\samples\proteinGroups_SK.csv")
+        ' Call GetProteinDefs("C:\Users\xieguigang\OneDrive\1.5\samples\proteinGroups_SK.csv")
+
+
+        Dim designer As Designer() = {
+            New Designer With {.Experiment = "24.C4", .Control = "24.A4"},
+            New Designer With {.Experiment = "24.C5", .Control = "24.A5"},
+            New Designer With {.Experiment = "24.C6", .Control = "24.A6"},
+            New Designer With {.Experiment = "72.C4", .Control = "72.A4"},
+            New Designer With {.Experiment = "72.C5", .Control = "72.A5"},
+            New Designer With {.Experiment = "72.C6", .Control = "72.A6"},
+            New Designer With {.Experiment = "72.A4", .Control = "24.A4"},
+            New Designer With {.Experiment = "72.A5", .Control = "24.A5"},
+            New Designer With {.Experiment = "72.A6", .Control = "24.A6"},
+            New Designer With {.Experiment = "72.C4", .Control = "24.C4"},
+            New Designer With {.Experiment = "72.C5", .Control = "24.C5"},
+            New Designer With {.Experiment = "72.C6", .Control = "24.C6"}
+        }
+
+        Call SMRUCC.genomics.Analysis.Microarray.DEGDesigner.log2("C:\Users\xieguigang\OneDrive\1.5\samples\proteinGroups_GL.csv", designer, "LFQ.intensity.GL").Save("C:\Users\xieguigang\OneDrive\1.5\samples\proteinGroups_GL-DEG-log2.csv")
+
+        Call SMRUCC.genomics.Analysis.Microarray.DEGDesigner.log2("C:\Users\xieguigang\OneDrive\1.5\samples\proteinGroups_SK.csv", designer, "LFQ.intensity.SK").Save("C:\Users\xieguigang\OneDrive\1.5\samples\proteinGroups_SK-DEG-log2.csv")
+
 
         Pause()
     End Sub
