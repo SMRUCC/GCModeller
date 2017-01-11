e6c37c8aa0627cfb19917c6aad167344c3940322
xieguigang <xie.guigang@gmail.com>
2017-01-12 03:12:28 +0800

    update runtime library docs

--- src/GCModeller/analysis/Microarray/DEGDesigner.vb
+++ src/GCModeller/analysis/Microarray/DEGDesigner.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.Data.csv.DocumentStream
 Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Data.csv.DocumentStream
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Mathematical.Statistics.Hypothesis
 Imports Microsoft.VisualBasic.Scripting
 Imports csv = Microsoft.VisualBasic.Data.csv.DocumentStream.File
 
@@ -20,9 +22,40 @@ Public Module DEGDesigner
 
     Public Function log2(path$, designers As Designer(), Optional label$ = Nothing) As csv
         Dim genes As EntityObject() = EntityObject.LoadDataSet(path).ToArray
-        Dim out As EntityObject() = genes.log2(designers, label).ToArray
+        Dim groups As Dictionary(Of String, Designer()) = designers _
+            .GroupBy(Function(x) x.GroupLabel) _
+            .ToDictionary(Function(k) k.Key,
+                          Function(repeats) repeats.ToArray)
+        Dim out As EntityObject() = genes _
+            .log2(designers, label) _
+            .StudentTtest(groups, label) _
+            .ToArray
         Dim csv As csv = out.ToCsvDoc
         Return csv
+    End Function
+
+    <Extension>
+    Public Iterator Function StudentTtest(data As IEnumerable(Of EntityObject), designers As Dictionary(Of String, Designer()), Optional label$ = Nothing) As IEnumerable(Of EntityObject)
+        For Each gene As EntityObject In data
+            For Each group In designers
+                Dim a#() = New Double(group.Value.Length - 1) {}
+                Dim b#() = New Double(group.Value.Length - 1) {}
+
+                For i As Integer = 0 To a.Length - 1
+                    Dim l = group.Value(i)
+                    Dim tag = l.GetLabel(label)
+
+                    a(i) = Val(gene(tag.exp))
+                    b(i) = Val(gene(tag.control))
+                Next
+
+                Dim result As TwoSampleResult = t.Test(a, b)
+
+                gene($"{group.Key}.P-value") = result.Pvalue
+            Next
+
+            Yield gene
+        Next
     End Function
 
     Public Structure Designer
@@ -36,21 +69,27 @@ Public Module DEGDesigner
         ''' </summary>
         Dim Control$
 
+        ''' <summary>
+        ''' 具有相同的这个属性的标签值的都是生物学重复
+        ''' </summary>
+        Dim GroupLabel$
+
         Public Overrides Function ToString() As String
             Return $"{Experiment}/{Control}"
         End Function
 
-        Public Function Log2(gene As EntityObject, Optional label$ = Nothing) As Double
-            Dim A#, B#
-
+        Public Function GetLabel(Optional label$ = Nothing) As (exp$, control$)
             If label Is Nothing Then
-                A = gene(Experiment).ParseNumeric
-                B = gene(Control).ParseNumeric
+                Return (Experiment, Control)
             Else
-                A = gene(label & "." & Experiment).ParseNumeric
-                B = gene(label & "." & Control).ParseNumeric
+                Return (label & "." & Experiment, label & "." & Control)
             End If
+        End Function
 
+        Public Function Log2(gene As EntityObject, Optional label$ = Nothing) As Double
+            Dim tag As (exp$, Control As String) = GetLabel(label)
+            Dim A# = gene(tag.exp).ParseNumeric
+            Dim B# = gene(tag.Control).ParseNumeric
             Dim out As Double = Math.Log(A / B, 2)
             Return out
         End Function
