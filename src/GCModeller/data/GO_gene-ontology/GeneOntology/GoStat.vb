a75f177b106a749e2a03dbfaae3492df3a9988cf
xieguigang <xie.guigang@gmail.com>
2017-01-14 03:39:06 +0800

    go stats

---
+++ src/GCModeller/data/GO_gene-ontology/GeneOntology/GoStat.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports SMRUCC.genomics.Data.GeneOntology.OBO
+
+''' <summary>
+''' Statics of the GO function catalog
+''' </summary>
+Public Module GoStat
+
+    ''' <summary>
+    ''' 计数统计
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function CountStat(Of gene)(genes As IEnumerable(Of gene), getGO As Func(Of gene, String), GO_terms As Dictionary(Of String, Term)) As Dictionary(Of String, NamedValue(Of Integer)())
+        Dim out As Dictionary(Of String, List(Of NamedValue(Of Integer))) =
+            Enums(Of Ontologies) _
+            .ToDictionary(Function(o) o.Description,
+                          Function(null) New List(Of NamedValue(Of Integer)))
+
+        For Each g As gene In genes
+            Dim goID As String = getGO(g)
+            Dim term As Term = GO_terms(goID)
+        Next
+
+        Return out.ToDictionary(
+            Function(x) x.Key,
+            Function(value) value.Value.ToArray)
+    End Function
+End Module
