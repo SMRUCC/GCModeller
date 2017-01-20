e0dfba9c11dfe836d0a11b724dbc5bc1ce1ceda3
xieguigang <xie.guigang@gmail.com>
2017-01-20 14:19:59 +0800

    color index model

---
+++ src/GCModeller/annotations/Proteomics/Module1.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic.Data.csv
+
+Module Module1
+
+    Sub Main()
+
+        ' 1. 总蛋白注释
+        'Call "C:\Users\xieguigang\OneDrive\1.17\2. annotations\uniprot.txt" _
+        '    .ReadAllLines _
+        '    .GenerateAnnotations("C:\Users\xieguigang\OneDrive\1.17\2. annotations\uniprot.xml") _
+        '    .Select(Function(x) x.Item1) _
+        '    .ToArray _
+        '    .SaveDataSet("C:\Users\xieguigang\OneDrive\1.17\2. annotations\sample-annotation.csv")
+
+        ' Pause()
+
+        ' 2. DEP注释
+        Call "C:\Users\xieguigang\OneDrive\1.17\4. analysis\C-T.txt" _
+            .ReadAllLines _
+            .GenerateAnnotations("C:\Users\xieguigang\OneDrive\1.17\2. annotations\uniprot.xml") _
+            .Select(Function(x) x.Item1) _
+            .ToArray _
+            .SaveDataSet("C:\Users\xieguigang\OneDrive\1.17\4. analysis\C-T.annotations.csv")
+
+        Call "C:\Users\xieguigang\OneDrive\1.17\4. analysis\WT-KO.txt" _
+         .ReadAllLines _
+         .GenerateAnnotations("C:\Users\xieguigang\OneDrive\1.17\2. annotations\uniprot.xml") _
+         .Select(Function(x) x.Item1) _
+         .ToArray _
+         .SaveDataSet("C:\Users\xieguigang\OneDrive\1.17\4. analysis\WT-KO.annotations.csv")
+
+        Pause()
+    End Sub
+End Module
