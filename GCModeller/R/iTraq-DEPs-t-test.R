2675b6eb916d943471efec446a066006af746052
xieguigang <xie.guigang@gmail.com>
2017-01-20 16:12:52 +0800

    improvements on the R script

--- GCModeller/R/iTraq-DEPs-t-test.R
+++ GCModeller/R/iTraq-DEPs-t-test.R
@@ -1,6 +1,6 @@
 library(tools) 
 
-logFC.test <- function(file) {
+logFC.test <- function(file, level = 1.5) {
 
 	data          = read.csv(file)
 	repeatsNumber = ncol(data) - 1          # 实验重复数
@@ -8,7 +8,7 @@ logFC.test <- function(file) {
 	index         = seq(2, repeatsNumber+1)
 	pvalue        = rep(0, nrow(data))
 	avgFC         = rep(0, nrow(data))
-		
+
 	# 对dataframe之中的每一行都进行计算
 	for(i in 1:(nrow(data))) {
 		
@@ -17,12 +17,16 @@ logFC.test <- function(file) {
 		v      = as.vector(as.matrix(row[index]))
 		valids = sum(!is.na(v))
 
-		if (valids > 1) {
+		if (valids > 0) {
+			v = v[!is.na(v)]
+			l = repeatsNumber - length(v)
+			# 使用1补齐NA
+			v = as.vector(append(v, rep(1, l)))  
 			avgFC[i] = mean(v, na.rm = TRUE)
 			avgFC[i]
 			v = log(v, 2)
 			# log2(FC) 结果和等长零向量做检验得到pvalue
-			pvalue[i] = t.test(v, ZERO, var.equal = FALSE)$p.value			
+			pvalue[i] = t.test(v, ZERO, var.equal = TRUE)$p.value			
 		} else {
 			avgFC[i] = NA
 			pvalue[i] = NA
@@ -32,7 +36,9 @@ logFC.test <- function(file) {
 	data["FC.avg"]  = avgFC
 	data["p.value"] = pvalue
 	# DEP 计算结果
-	data["is.DEP"] = ((avgFC >= 1.5 | avgFC <= 0.667) & (pvalue < 0.05))
+	
+	downLevel = 1 / level
+	data["is.DEP"] = ((avgFC >= level | avgFC <= downLevel) & (pvalue <= 0.05))
 	
 	DIR <- dirname(file)
 	DIR <- paste(DIR, file_path_sans_ext(basename(file)), sep="/")
