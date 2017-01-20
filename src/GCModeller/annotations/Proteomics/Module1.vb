9a7b0106bb010054bc326a623e11355d1eeadb2c
xieguigang <xie.guigang@gmail.com>
2017-01-20 17:11:35 +0800

    program rename

--- src/GCModeller/annotations/Proteomics/Module1.vb
+++ src/GCModeller/annotations/Proteomics/Module1.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.Data.csv
+Imports SMRUCC.genomics.Analysis.Microarray
 
 Module Module1
 
@@ -15,20 +16,26 @@ Module Module1
         ' Pause()
 
         ' 2. DEP注释
-        Call "C:\Users\xieguigang\OneDrive\1.17\4. analysis\C-T.txt" _
-            .ReadAllLines _
-            .GenerateAnnotations("C:\Users\xieguigang\OneDrive\1.17\2. annotations\uniprot.xml") _
-            .Select(Function(x) x.Item1) _
-            .ToArray _
-            .SaveDataSet("C:\Users\xieguigang\OneDrive\1.17\4. analysis\C-T.annotations.csv")
+        'Call "C:\Users\xieguigang\OneDrive\1.17\4. analysis\C-T.txt" _
+        '    .ReadAllLines _
+        '    .GenerateAnnotations("C:\Users\xieguigang\OneDrive\1.17\2. annotations\uniprot.xml") _
+        '    .Select(Function(x) x.Item1) _
+        '    .ToArray _
+        '    .SaveDataSet("C:\Users\xieguigang\OneDrive\1.17\4. analysis\C-T.annotations.csv")
 
-        Call "C:\Users\xieguigang\OneDrive\1.17\4. analysis\WT-KO.txt" _
-         .ReadAllLines _
-         .GenerateAnnotations("C:\Users\xieguigang\OneDrive\1.17\2. annotations\uniprot.xml") _
-         .Select(Function(x) x.Item1) _
-         .ToArray _
-         .SaveDataSet("C:\Users\xieguigang\OneDrive\1.17\4. analysis\WT-KO.annotations.csv")
+        'Call "C:\Users\xieguigang\OneDrive\1.17\4. analysis\WT-KO.txt" _
+        ' .ReadAllLines _
+        ' .GenerateAnnotations("C:\Users\xieguigang\OneDrive\1.17\2. annotations\uniprot.xml") _
+        ' .Select(Function(x) x.Item1) _
+        ' .ToArray _
+        ' .SaveDataSet("C:\Users\xieguigang\OneDrive\1.17\4. analysis\WT-KO.annotations.csv")
 
-        Pause()
+        'Pause()
+        ' 3. heatmap绘图
+
+        'Call DEGDesigner _
+        '    .MergeMatrix("C:\Users\xieguigang\OneDrive\1.17\3. DEP\heatmap", "*.csv", 1.25, 0.05, "FC.avg", 1 / 1.25, "p.value") _
+        '    .SaveDataSet("C:\Users\xieguigang\OneDrive\1.17\3. DEP\DEP.heatmap.csv", blank:=0)
+
     End Sub
 End Module
