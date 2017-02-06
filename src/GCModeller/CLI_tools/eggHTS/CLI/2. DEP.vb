f4cd314c153ce64f1ffd28f1e56e9a0aada55261
xieguigang <xie.guigang@gmail.com>
2017-02-06 20:25:20 +0800

    DEP uniprot id list tool

---
+++ src/GCModeller/CLI_tools/eggHTS/CLI/2. DEP.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.Analysis.HTS.Proteomics
+
+Partial Module CLI
+
+    <ExportAPI("/DEP.uniprot.list",
+               Usage:="/DEP.uniprot.list /DEP <log2-test.DEP.csv> /sample <sample.csv> [/out <out.txt>]")>
+    Public Function DEPUniprotIDlist(args As CommandLine) As Integer
+        Dim DEP As String = args("/DEP")
+        Dim sample As String = args("/sample")
+        Dim out As String = args.GetValue("/out", DEP.TrimSuffix & "-uniprot.ID.list.txt")
+        Dim DEP_data As IEnumerable(Of DEP) = EntityObject _
+            .LoadDataSet(Of DEP)(path:=DEP) _
+            .Where(Function(d) d.isDEP) _
+            .ToArray
+        Dim sampleData As Dictionary(Of String, String()) =
+            sample _
+            .LoadCsv(Of UniprotAnnotations) _
+            .GroupBy(Function(p) p.ORF) _
+            .ToDictionary(Function(p) p.Key,
+                          Function(g) g.ToArray(
+                          Function(p) p.ID))
+        Dim list$() = DEP_data _
+            .Select(Function(d) sampleData(d.ID)) _
+            .IteratesALL _
+            .Distinct _
+            .ToArray
+
+        Return list.SaveTo(out).CLICode
+    End Function
+End Module
---
+++ src/GCModeller/annotations/Proteomics/DEP.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Class DEP : Inherits EntityObject
+
+    <Column("FC.avg")> Public Property FCavg As Double
+    <Column("p.value")> Public Property pvalue As Double
+    <Column("is.DEP")> Public Property isDEP As Boolean
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
