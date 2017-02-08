8ef96138d0e4e9e2ed9661c8ca27cd7b590c46bb
xieguigang <xie.guigang@gmail.com>
2017-02-08 21:41:22 +0800

    context parser of the promoter region

--- src/interops/meme_suite/MEME/Workflows/PromoterParser/GenePromoterRegions.vb
+++ src/interops/meme_suite/MEME/Workflows/PromoterParser/GenePromoterRegions.vb
@@ -1,35 +1,9 @@
-﻿#Region "Microsoft.VisualBasic::8a57dcd99772df23552550c9446405c6, ..\interops\meme_suite\MEME\Workflows\PromoterParser\GenePromoterParser.vb"
-
-' Author:
-' 
-'       asuka (amethyst.asuka@gcmodeller.org)
-'       xieguigang (xie.guigang@live.com)
-'       xie (genetics@smrucc.org)
-' 
-' Copyright (c) 2016 GPL3 Licensed
-' 
-' 
-' GNU GENERAL PUBLIC LICENSE (GPL3)
-' 
-' This program is free software: you can redistribute it and/or modify
-' it under the terms of the GNU General Public License as published by
-' the Free Software Foundation, either version 3 of the License, or
-' (at your option) any later version.
-' 
-' This program is distributed in the hope that it will be useful,
-' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-' GNU General Public License for more details.
-' 
-' You should have received a copy of the GNU General Public License
-' along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-#End Region
-
+﻿Imports System.Text
 Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports Microsoft.VisualBasic.Data.csv.Extensions
-Imports Microsoft.VisualBasic.Linq.Extensions
+Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Extensions
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq.Extensions
 Imports Microsoft.VisualBasic.Mathematical
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Microsoft.VisualBasic.Text
@@ -37,83 +11,15 @@ Imports SMRUCC.genomics.Analysis.RNA_Seq.RTools
 Imports SMRUCC.genomics.Assembly.DOOR
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET
 Imports SMRUCC.genomics.Assembly.NCBI.GenBank.TabularFormat
-Imports SMRUCC.genomics.ComponentModel.Loci
+Imports SMRUCC.genomics.ContextModel
+Imports SMRUCC.genomics.ContextModel.GenePromoterParser
 Imports SMRUCC.genomics.SequenceModel
-Imports Microsoft.VisualBasic.Language
 
 Namespace Workflows.PromoterParser
 
-    ''' <summary>
-    ''' 直接从基因的启动子区选取序列数据以及外加操纵子的第一个基因的启动子序列
-    ''' </summary>
-    ''' <remarks></remarks>
-    ''' 
-    <[PackageNamespace]("Parser.Gene.Promoter", Publisher:="xie.guigang@gmail.com")>
-    Public Class GenePromoterParser : Inherits Workflows.PromoterParser.PromoterParser
-        Implements System.IDisposable
-
-        ''' <summary>
-        ''' 基因组的Fasta核酸序列
-        ''' </summary>
-        ''' <param name="Fasta"></param>
-        ''' <remarks></remarks>
-        Sub New(Fasta As FASTA.FastaToken, PTT As PTT)
-            Dim GenomeSeq As I_PolymerSequenceModel = Fasta
-
-            Promoter_100 = CreateObject(100, PTT, GenomeSeq)
-            Promoter_150 = CreateObject(150, PTT, GenomeSeq)
-            Promoter_200 = CreateObject(200, PTT, GenomeSeq)
-            Promoter_250 = CreateObject(250, PTT, GenomeSeq)
-            Promoter_300 = CreateObject(300, PTT, GenomeSeq)
-            Promoter_350 = CreateObject(350, PTT, GenomeSeq)
-            Promoter_400 = CreateObject(400, PTT, GenomeSeq)
-            Promoter_450 = CreateObject(450, PTT, GenomeSeq)
-            Promoter_500 = CreateObject(500, PTT, GenomeSeq)
-        End Sub
-
-        Sub New(genome As PTTDbLoader)
-            Call Me.New(genome.GenomeFasta, genome.ORF_PTT)
-        End Sub
-
-        ''' <summary>
-        ''' 解析出所有基因前面的序列片段
-        ''' </summary>
-        ''' <param name="Length"></param>
-        ''' <param name="PTT"></param>
-        ''' <param name="GenomeSeq"></param>
-        ''' <returns></returns>
-        Private Shared Function CreateObject(Length As Integer, PTT As PTT, GenomeSeq As I_PolymerSequenceModel) As Dictionary(Of String, FASTA.FastaToken)
-            Dim LQuery = (From gene As ComponentModels.GeneBrief
-                          In PTT.GeneObjects.AsParallel
-                          Select gene.Synonym,
-                              Promoter = GetFASTA(gene, GenomeSeq, Length)).ToArray
-            Dim DictData As Dictionary(Of String, FASTA.FastaToken) =
-                LQuery.ToDictionary(Function(obj) obj.Synonym,
-                                    Function(obj) obj.Promoter)
-            Return DictData
-        End Function
-
-        Private Shared Function GetFASTA(Gene As ComponentModels.GeneBrief, GenomeSeq As I_PolymerSequenceModel, SegmentLength As Integer) As FASTA.FastaToken
-            Dim Location As NucleotideLocation = Gene.Location
-
-            Call Location.Normalization()
-
-            If Location.Strand = Strands.Forward Then
-                Location = New NucleotideLocation(Location.Left - SegmentLength, Location.Left)  ' 正向序列是上游，无需额外处理
-            Else
-                Location = New NucleotideLocation(Location.Right, Location.Right + SegmentLength, ComplementStrand:=True)  '反向序列是下游，需要额外小心
-            End If
-
-            Dim PromoterFsa As FASTA.FastaToken = New FASTA.FastaToken With {
-                .Attributes = New String() {Gene.Synonym},
-                .SequenceData = GenomeSeq.CutSequenceLinear(Location).SequenceData
-            }
-
-            Return PromoterFsa
-        End Function
-
+    Public Module GenePromoterRegions
         <ExportAPI("Promoter.New", Info:="Create a new promoter sequence parser.")>
-        Public Shared Function CreateObject(Fasta As FASTA.FastaToken, PTT As PTT) As GenePromoterParser
+        Public Function CreateObject(Fasta As FASTA.FastaToken, PTT As PTT) As GenePromoterParser
             Return New GenePromoterParser(Fasta, PTT)
         End Function
 
@@ -123,7 +29,7 @@ Namespace Workflows.PromoterParser
         ''' <param name="locus">需要进行解析的基因的编号的列表</param>
         ''' 
         <ExportAPI("Locus.Promoters")>
-        Public Shared Sub ParsingList(Parser As GenePromoterParser, DOOR As String, locus As IEnumerable(Of String), EXPORT As String)
+        Public Sub ParsingList(Parser As GenePromoterParser, DOOR As String, locus As IEnumerable(Of String), EXPORT As String)
             Call ParsingList(Parser, DOOR_API.Load(DOOR), locus, EXPORT)
         End Sub
 
@@ -133,7 +39,7 @@ Namespace Workflows.PromoterParser
         ''' <param name="locus">需要进行解析的基因的编号的列表</param>
         ''' 
         <ExportAPI("Locus.Promoters")>
-        Public Shared Sub ParsingList(Parser As GenePromoterParser,
+        Public Sub ParsingList(Parser As GenePromoterParser,
                                       DOOR As DOOR,
                                       locus As IEnumerable(Of String),
                                       EXPORT As String,
@@ -155,15 +61,9 @@ Namespace Workflows.PromoterParser
                                     .Save(-1, Path, Encodings.ASCII)
                             End Sub
 
-            Call SaveFasta(Parser.Promoter_100, 100)
-            Call SaveFasta(Parser.Promoter_150, 150)
-            Call SaveFasta(Parser.Promoter_200, 200)
-            Call SaveFasta(Parser.Promoter_250, 250)
-            Call SaveFasta(Parser.Promoter_300, 300)
-            Call SaveFasta(Parser.Promoter_350, 350)
-            Call SaveFasta(Parser.Promoter_400, 400)
-            Call SaveFasta(Parser.Promoter_450, 450)
-            Call SaveFasta(Parser.Promoter_500, 500)
+            For Each l% In GenePromoterParser.PrefixLength
+                Call SaveFasta(Parser.GetRegionCollectionByLength(l), l)
+            Next
         End Sub
 
         ''' <summary>
@@ -172,7 +72,7 @@ Namespace Workflows.PromoterParser
         ''' <param name="modsDIR">包含有KEGG Modules的文件夹</param>
         ''' 
         <ExportAPI("KEGG_Modules.Promoters")>
-        Public Shared Sub ParsingKEGGModules(Parser As GenePromoterParser,
+        Public Sub ParsingKEGGModules(Parser As GenePromoterParser,
                                              DOOR As String,
                                              modsDIR As String,
                                              EXPORT As String,
@@ -192,15 +92,9 @@ Namespace Workflows.PromoterParser
                                         .Save(-1, Path, System.Text.Encoding.ASCII)
                                 End Sub
 
-                Call SaveFasta(Parser.Promoter_100, 100)
-                Call SaveFasta(Parser.Promoter_150, 150)
-                Call SaveFasta(Parser.Promoter_200, 200)
-                Call SaveFasta(Parser.Promoter_250, 250)
-                Call SaveFasta(Parser.Promoter_300, 300)
-                Call SaveFasta(Parser.Promoter_350, 350)
-                Call SaveFasta(Parser.Promoter_400, 400)
-                Call SaveFasta(Parser.Promoter_450, 450)
-                Call SaveFasta(Parser.Promoter_500, 500)
+                For Each l% In GenePromoterParser.PrefixLength
+                    Call SaveFasta(Parser.GetRegionCollectionByLength(l), l)
+                Next
 
                 Call Console.Write(".")
             Next
@@ -214,7 +108,7 @@ Namespace Workflows.PromoterParser
         ''' <param name="PathwaysDIR"></param>
         ''' <param name="EXPORT"></param>
         <ExportAPI("KEGG_Pathways.Promoters")>
-        Public Shared Sub ParsingKEGGPathways(Parser As GenePromoterParser,
+        Public Sub ParsingKEGGPathways(Parser As GenePromoterParser,
                                               DOOR As String,
                                               PathwaysDIR As String,
                                               EXPORT As String,
@@ -225,32 +119,26 @@ Namespace Workflows.PromoterParser
                    .ToArray(Function(xml) xml.LoadXml(Of bGetObject.Pathway))
             Dim DoorOperon = DOOR_API.Load(DOOR)
             Dim GetDOORUni As IGetLocusTag = ParserLocus.CreateMethod(DoorOperon, method)
-            Dim prefix As String = basename(EXPORT)
+            Dim prefix As String = BaseName(EXPORT)
 
             For Each [mod] As bGetObject.Pathway In Modules
                 Dim Genes As String() = (From gene As String In [mod].GetPathwayGenes Select GetDOORUni(gene)).IteratesALL.Distinct.ToArray
                 Dim SaveFasta = Sub(src As Dictionary(Of String, FASTA.FastaToken), len As Integer)
                                     Dim Path As String = $"{EXPORT}/{prefix}-{len}/{[mod].EntryId}.fasta"
                                     ' 由于会存在有RNA基因，所以这里需要额外注意一下
-                                    Call New FASTA.FastaFile(From id As String In Genes Where src.ContainsKey(id) Select src(id)).Save(-1, Path, System.Text.Encoding.ASCII)
+                                    Call New FASTA.FastaFile(From id As String In Genes Where src.ContainsKey(id) Select src(id)).Save(-1, Path, Encoding.ASCII)
                                 End Sub
 
-                Call SaveFasta(Parser.Promoter_100, 100)
-                Call SaveFasta(Parser.Promoter_150, 150)
-                Call SaveFasta(Parser.Promoter_200, 200)
-                Call SaveFasta(Parser.Promoter_250, 250)
-                Call SaveFasta(Parser.Promoter_300, 300)
-                Call SaveFasta(Parser.Promoter_350, 350)
-                Call SaveFasta(Parser.Promoter_400, 400)
-                Call SaveFasta(Parser.Promoter_450, 450)
-                Call SaveFasta(Parser.Promoter_500, 500)
+                For Each l% In GenePromoterParser.PrefixLength
+                    Call SaveFasta(Parser.GetRegionCollectionByLength(l), l)
+                Next
 
                 Call Console.Write(".")
             Next
         End Sub
 
         <ExportAPI("Read.Csv.DESeq")>
-        Public Shared Function LoadDESeqResult(path As String) As DESeq2.ResultData()
+        Public Function LoadDESeqResult(path As String) As DESeq2.ResultData()
             Return path.LoadCsv(Of DESeq2.ResultData)(False).ToArray
         End Function
 
@@ -262,7 +150,7 @@ Namespace Workflows.PromoterParser
         ''' <param name="EXPORT"></param>
         ''' <returns></returns>
         <ExportAPI("DESeq.Promoters")>
-        Public Shared Function DiffExpressionPromoters(Promoter As GenePromoterParser, DESeq As IEnumerable(Of DESeq2.ResultData),
+        Public Function DiffExpressionPromoters(Promoter As GenePromoterParser, DESeq As IEnumerable(Of DESeq2.ResultData),
                                                        <Parameter("Dir.Export")> Optional EXPORT As String = "./") As Boolean
             Dim Diff = (From GeneObject In DESeq.AsParallel Where Math.Abs(GeneObject.log2FoldChange) >= 2 Select GeneObject).ToArray
 
@@ -398,22 +286,22 @@ Namespace Workflows.PromoterParser
             End Function
         End Class
 
-        Private Shared Function AlwaysHighLevel(Gene As DESeq2.ResultData) As Boolean
+        Private Function AlwaysHighLevel(Gene As DESeq2.ResultData) As Boolean
             Dim LQuery = (From value In Gene.dataExpr0.Values Where value >= 6 Select 1).ToArray
             Return LQuery.Count = Gene.dataExpr0.Count
         End Function
 
-        Private Shared Function AlwaysLowLevel(Gene As DESeq2.ResultData) As Boolean
+        Private Function AlwaysLowLevel(Gene As DESeq2.ResultData) As Boolean
             Dim LQuery = (From value In Gene.dataExpr0.Values Where value = 2 Select 1).ToArray
             Return LQuery.Count = Gene.dataExpr0.Count
         End Function
 
-        Private Shared Function AlwaysUltraLowLevel(Gene As DESeq2.ResultData) As Boolean
+        Private Function AlwaysUltraLowLevel(Gene As DESeq2.ResultData) As Boolean
             Dim LQuery = (From value In Gene.dataExpr0.Values Where value = 1 Select 1).ToArray
             Return LQuery.Count = Gene.dataExpr0.Count
         End Function
 
-        Private Shared Function AlwaysNormalLevel(Gene As DESeq2.ResultData) As Boolean
+        Private Function AlwaysNormalLevel(Gene As DESeq2.ResultData) As Boolean
             Dim LQuery = (From value In Gene.dataExpr0.Values Where value < 6 AndAlso value > 2 Select 1).ToArray
             Return LQuery.Count = Gene.dataExpr0.Count
         End Function
@@ -423,7 +311,7 @@ Namespace Workflows.PromoterParser
         ''' </summary>
         ''' <param name="DESeq"></param>
         ''' <returns></returns>
-        Private Shared Function GenerateExpressionLevelMappings(DESeq As IEnumerable(Of DESeq2.ResultData)) As DESeq2.ResultData()
+        Private Function GenerateExpressionLevelMappings(DESeq As IEnumerable(Of DESeq2.ResultData)) As DESeq2.ResultData()
             Dim Experiments = DESeq.First.dataExpr0.Keys '从元数据属性之中得到实验条件的列表
 
             '对每一个实验条件得到映射值，请注意为了保持对象之间的一一对应关系，这里不可以再使用并行化拓展
@@ -448,73 +336,5 @@ Namespace Workflows.PromoterParser
 
             Return DESeq.ToArray
         End Function
-
-        Public Function GetSequenceById(lstId As IEnumerable(Of String), <Parameter("Len")> Length As Integer) As FASTA.FastaFile
-            Return GetSequenceById(Me, lstId, Length)
-        End Function
-
-        ''' <summary>
-        ''' 
-        ''' </summary>
-        ''' <param name="Promoter"></param>
-        ''' <param name="idList"></param>
-        ''' <param name="Length"></param>
-        ''' <returns></returns>
-        ''' <remarks></remarks>
-        <ExportAPI("Get.Sequence.By.Id")>
-        Public Shared Function GetSequenceById(Promoter As GenePromoterParser,
-                                               <Parameter("id.list", "The gene id list.")> idList As IEnumerable(Of String),
-                                               <Parameter("Len")> Optional Length As Integer = 150) As FASTA.FastaFile
-            If Not ContainsLength(Length) Then
-                Call $"The promoter region length {Length} is not valid, using default value is 150bp.".__DEBUG_ECHO
-                Length = 150
-            End If
-
-            Dim ListData As New List(Of String)(idList)
-
-            Select Case Length
-                Case 100
-                    Return CType((From Fasta In Promoter.Promoter_150.AsParallel Where ListData.IndexOf(Fasta.Key) > -1 Select Fasta.Value).ToArray, SMRUCC.genomics.SequenceModel.FASTA.FastaFile)
-                Case 150
-                    Return CType((From Fasta In Promoter.Promoter_150.AsParallel Where ListData.IndexOf(Fasta.Key) > -1 Select Fasta.Value).ToArray, SMRUCC.genomics.SequenceModel.FASTA.FastaFile)
-                Case 200
-                    Return CType((From Fasta In Promoter.Promoter_200.AsParallel Where ListData.IndexOf(Fasta.Key) > -1 Select Fasta.Value).ToArray, SMRUCC.genomics.SequenceModel.FASTA.FastaFile)
-                Case 250
-                    Return CType((From Fasta In Promoter.Promoter_250.AsParallel Where ListData.IndexOf(Fasta.Key) > -1 Select Fasta.Value).ToArray, SMRUCC.genomics.SequenceModel.FASTA.FastaFile)
-                Case 300
-                    Return CType((From Fasta In Promoter.Promoter_300.AsParallel Where ListData.IndexOf(Fasta.Key) > -1 Select Fasta.Value).ToArray, SMRUCC.genomics.SequenceModel.FASTA.FastaFile)
-                Case 350
-                    Return CType((From Fasta In Promoter.Promoter_350.AsParallel Where ListData.IndexOf(Fasta.Key) > -1 Select Fasta.Value).ToArray, SMRUCC.genomics.SequenceModel.FASTA.FastaFile)
-                Case 400
-                    Return CType((From Fasta In Promoter.Promoter_400.AsParallel Where ListData.IndexOf(Fasta.Key) > -1 Select Fasta.Value).ToArray, SMRUCC.genomics.SequenceModel.FASTA.FastaFile)
-                Case 450
-                    Return CType((From Fasta In Promoter.Promoter_450.AsParallel Where ListData.IndexOf(Fasta.Key) > -1 Select Fasta.Value).ToArray, SMRUCC.genomics.SequenceModel.FASTA.FastaFile)
-                Case 500
-                    Return CType((From Fasta In Promoter.Promoter_500.AsParallel Where ListData.IndexOf(Fasta.Key) > -1 Select Fasta.Value).ToArray, SMRUCC.genomics.SequenceModel.FASTA.FastaFile)
-                Case Else
-                    Throw New Exception
-            End Select
-        End Function
-
-        Private Shared Function ContainsLength(l As Integer) As Boolean
-            Return Array.IndexOf(PrefixLength, l) > -1
-        End Function
-
-#Region "IDisposable Support"
-        Private disposedValue As Boolean ' To detect redundant calls
-
-        ' IDisposable
-        Protected Overrides Sub Dispose(disposing As Boolean)
-            If Not Me.disposedValue Then
-                If disposing Then
-                    ' TODO: dispose managed state (managed objects).
-                End If
-
-                ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
-                ' TODO: set large fields to null.
-            End If
-            Me.disposedValue = True
-        End Sub
-#End Region
-    End Class
+    End Module
 End Namespace
