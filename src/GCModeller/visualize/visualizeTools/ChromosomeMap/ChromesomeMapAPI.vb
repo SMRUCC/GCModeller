28ce0e1cf16869dfef7a56bb2fa943e0519181c5
xieguigang <xie.guigang@gmail.com>
2017-02-08 17:58:03 +0800

    merge common code

--- src/GCModeller/visualize/visualizeTools/ChromosomeMap/ChromesomeMapAPI.vb
+++ src/GCModeller/visualize/visualizeTools/ChromosomeMap/ChromesomeMapAPI.vb
@@ -1,40 +1,40 @@
 ﻿#Region "Microsoft.VisualBasic::3bd2505dc1295233001cc7342b2060b7, ..\GCModeller\visualize\visualizeTools\ChromosomeMap\ChromesomeMapAPI.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
-Imports System.Drawing
-Imports System.Text
-Imports Microsoft.VisualBasic
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv.Extensions
-Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
 Imports Microsoft.VisualBasic.Extensions
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq.Extensions
+Imports Microsoft.VisualBasic.ListExtensions
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Oracle.Java.IO.Properties.Reflector
 Imports SMRUCC.genomics.Assembly.NCBI.GenBank.TabularFormat
@@ -44,7 +44,7 @@ Imports SMRUCC.genomics.Interops.NBCR.MEME_Suite.Analysis.GenomeMotifFootPrints
 Imports SMRUCC.genomics.Interops.NBCR.MEME_Suite.DocumentFormat
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application
 Imports SMRUCC.genomics.Model.Network.VirtualFootprint.DocumentFormat
-Imports SMRUCC.genomics.Visualize
+Imports SMRUCC.genomics.SequenceModel.FASTA
 Imports SMRUCC.genomics.Visualize.ChromosomeMap.DrawingModels
 
 Namespace ChromosomeMap
@@ -87,12 +87,17 @@ Namespace ChromosomeMap
         End Function
 
         <ExportAPI("Gene2Fasta", Info:="Convert the gene annotation data into a fasta sequence.")>
-        Public Function WriteGeneFasta(<Parameter("Genes.Anno")> data As Generic.IEnumerable(Of PlasmidAnnotation)) As SMRUCC.genomics.SequenceModel.FASTA.FastaFile
-            Return (From Gene As PlasmidAnnotation In data
-                    Where Not String.IsNullOrEmpty(Gene.GeneNA)
-                    Select New SMRUCC.genomics.SequenceModel.FASTA.FastaToken With {
-                        .Attributes = New String() {Gene.ORF_ID},
-                        .SequenceData = Gene.GeneNA}).ToArray
+        Public Function WriteGeneFasta(<Parameter("Genes.Anno")> data As IEnumerable(Of PlasmidAnnotation)) As FastaFile
+            Dim falist As IEnumerable(Of FastaToken) =
+                From gene As PlasmidAnnotation
+                In data
+                Where Not String.IsNullOrEmpty(gene.GeneNA)
+                Select New FastaToken With {
+                    .Attributes = New String() {gene.ORF_ID},
+                    .SequenceData = gene.GeneNA
+                }
+
+            Return New FastaFile(falist)
         End Function
 
         <ExportAPI("Read.Csv.PlasmidAnno", Info:="Reads the plasmid annotation data from a csv document.")>
@@ -100,14 +105,17 @@ Namespace ChromosomeMap
             Return path.LoadCsv(Of PlasmidAnnotation)(False).ToArray
         End Function
 
-        <ExportAPI("PTT.From.Plasmid", Info:="Generates the PTT genome data from the plasmid annotation data. NT.Src parameter is the file path of the original genome fasta source sequence.")>
-        Public Function get_Converted(Annotations As Generic.IEnumerable(Of PlasmidAnnotation),
-                                      <Parameter("NT.Src", "The nt fasta sequence of the plasmid whole genome.")> src As String) As SMRUCC.genomics.Assembly.NCBI.GenBank.TabularFormat.PTTDbLoader
-            Dim LoaderObject = SMRUCC.genomics.Assembly.NCBI.GenBank.TabularFormat.PTTDbLoader.CreateObject(Annotations, SMRUCC.genomics.SequenceModel.FASTA.FastaToken.Load(src))
-            Dim AnnoDict = Annotations.ToDictionary(Function(item) item.ORF_ID)
-            For Each Gene As GeneBrief In LoaderObject.Values
-                Gene.Gene = AnnoDict(Gene.Synonym).Gene_name
+        <ExportAPI("PTT.From.Plasmid",
+                   Info:="Generates the PTT genome data from the plasmid annotation data. NT.Src parameter is the file path of the original genome fasta source sequence.")>
+        Public Function get_Converted(Annotations As IEnumerable(Of PlasmidAnnotation),
+                                      <Parameter("NT.Src", "The nt fasta sequence of the plasmid whole genome.")> src As String) As PTTDbLoader
+            Dim LoaderObject = PTTDbLoader.CreateObject(Annotations, FastaToken.Load(src))
+            Dim AnnoDict = Annotations.ToDictionary(Function(ann) ann.ORF_ID)
+
+            For Each gene As GeneBrief In LoaderObject.Values
+                gene.Gene = AnnoDict(gene.Synonym).Gene_name
             Next
+
             Return LoaderObject
         End Function
 
@@ -126,9 +134,10 @@ Namespace ChromosomeMap
         ''' <returns></returns>
         ''' <remarks></remarks>
         ''' 
-        <ExportAPI("Add.Motif_Sites", Info:="Trim.Regulation: if TRUE, then only the motif site which has the regulator will be drawing on the map, else all of the motif site will be drawing on the maps.")>
+        <ExportAPI("Add.Motif_Sites",
+                   Info:="Trim.Regulation: if TRUE, then only the motif site which has the regulator will be drawing on the map, else all of the motif site will be drawing on the maps.")>
         Public Function AddMotifSites(model As ChromesomeDrawingModel,
-                                      data As Generic.IEnumerable(Of PredictedRegulationFootprint),
+                                      data As IEnumerable(Of PredictedRegulationFootprint),
                                       <Parameter("Trim.Regulation", "If the parameter is TRUE, then only the site data with regulation data that will be drawn.")>
                                       Optional onlyRegulations As Boolean = True) As ChromesomeDrawingModel
 
@@ -140,7 +149,7 @@ Namespace ChromosomeMap
             End If
 
             model = __addMotifSites(model, data:=(From footprint As PredictedRegulationFootprint
-                                                  In data.AsParallel
+                                                  In data
                                                   Select New KeyValuePair(Of String(), PredictedRegulationFootprint)({footprint.Regulator}, footprint)).ToArray)
             Return model
         End Function
@@ -153,15 +162,22 @@ Namespace ChromosomeMap
         ''' <returns></returns>
         <ExportAPI("Add.TSSs")>
         Public Function AddTSSs(Model As ChromesomeDrawingModel, <Parameter("TSSs.Sites")> Sites As IEnumerable(Of Transcript)) As ChromesomeDrawingModel
-            Dim TSSs = (From obj As Transcript In Sites.AsParallel
-                        Select New DrawingModels.TSSs With {
-                            .Left = obj.TSSs,
-                            .Comments = obj.Position,
-                            .Right = obj.TSSs + (CInt(obj.MappingLocation.Strand) * 10),
-                            .Strand = obj.MappingLocation.Strand,
-                            .SiteName = obj.TSS_ID,
-                            .Synonym = obj.Synonym}).ToArray
-            Model.TSSs = TSSs
+            With Model
+                Dim TSSs = LinqAPI.Exec(Of DrawingModels.TSSs) <=
+                    From t As Transcript
+                    In Sites
+                    Select New DrawingModels.TSSs With {
+                        .Left = t.TSSs,
+                        .Comments = t.Position,
+                        .Right = t.TSSs + (CInt(t.MappingLocation.Strand) * 10),
+                        .Strand = t.MappingLocation.Strand,
+                        .SiteName = t.TSS_ID,
+                        .Synonym = t.Synonym
+                    }
+
+                .TSSs = TSSs
+            End With
+
             Return Model
         End Function
 
@@ -171,8 +187,7 @@ Namespace ChromosomeMap
         End Function
 
         <ExportAPI("Add.Motif_Sites")>
-        Public Function AddMotifSites(model As ChromesomeDrawingModel,
-                                      data As Generic.IEnumerable(Of SMRUCC.genomics.ComponentModel.Loci.Loci)) As ChromesomeDrawingModel
+        Public Function AddMotifSites(model As ChromesomeDrawingModel, data As IEnumerable(Of SMRUCC.genomics.ComponentModel.Loci.Loci)) As ChromesomeDrawingModel
             Dim LQuery = (From loci As SMRUCC.genomics.ComponentModel.Loci.Loci
                           In data.AsParallel
                           Select site = New DrawingModels.MotifSite With {
@@ -189,8 +204,7 @@ Namespace ChromosomeMap
         End Function
 
         <ExportAPI("Add.Loci_Sites")>
-        Public Function AddLociSites(model As ChromesomeDrawingModel,
-                                     data As Generic.IEnumerable(Of SMRUCC.genomics.SequenceModel.NucleotideModels.SegmentObject)) As ChromesomeDrawingModel
+        Public Function AddLociSites(model As ChromesomeDrawingModel, data As IEnumerable(Of SMRUCC.genomics.SequenceModel.NucleotideModels.SegmentObject)) As ChromesomeDrawingModel
             Dim Locis = (From site As SMRUCC.genomics.SequenceModel.NucleotideModels.SegmentObject
                          In data
                          Select New DrawingModels.Loci With {
@@ -306,29 +320,30 @@ Namespace ChromosomeMap
         End Function
 
         ''' <summary>
-        ''' Image formats can be one of the value: jpg,bmp,emf,exif,gif,png,wmf,tiff
+        ''' Image formats can be one of the value: ``jpg,bmp,emf,exif,gif,png,wmf,tiff``
         ''' </summary>
         ''' <param name="res"><see cref="DrawingDevice.InvokeDrawing(ChromesomeDrawingModel)"/>函数所生成的绘图图形输出资源数据</param>
-        ''' <param name="Export">将要进行数据保存的文件夹</param>
-        ''' <param name="Format">Value variant in jpg,bmp,emf,exif,gif,png,wmf,tiff</param>
+        ''' <param name="EXPORT">将要进行数据保存的文件夹</param>
+        ''' <param name="Format">Value variant in ``jpg,bmp,emf,exif,gif,png,wmf,tiff``</param>
         ''' <returns></returns>
         <ExportAPI("Resource.Save")>
+        <Extension>
         Public Function SaveImage(<Parameter("Resource")> res As KeyValuePair(Of Imaging.ImageFormat, Bitmap()),
-                                  <Parameter("Dir.Export")> Export As String,
+                                  <Parameter("DIR.EXPORT")> EXPORT$,
                                   <Parameter("Image.Format", "Value variant in jpg,bmp,emf,exif,gif,png,wmf,tiff")>
                                   Optional Format As String = "") As Integer
             Dim i As Integer = 0
             Dim imageFormat As Imaging.ImageFormat =
                 If(String.IsNullOrEmpty(Format), Nothing, GetSaveImageFormat(Format))
 
-            Call FileIO.FileSystem.CreateDirectory(Export)
+            Call FileIO.FileSystem.CreateDirectory(EXPORT)
 
             If imageFormat Is Nothing Then
                 imageFormat = res.Key  ' 默认使用配置文件之中所设定的格式
             End If
 
             For Each Bitmap As Bitmap In res.Value
-                Call Bitmap.Save($"{Export}/ChromosomeMap_Drawing_data.resources__{i.MoveNext}.bmp", imageFormat)
+                Call Bitmap.Save($"{EXPORT}/ChromosomeMap_Drawing_data.resources__{i.MoveNext}.bmp", imageFormat)
             Next
             Return i
         End Function
