23074cd6b8d9ff78218ba09fed76d056553cf8f3
xieguigang <xie.guigang@gmail.com>
2017-02-09 17:40:39 +0800

    populate seeds from the pre-compiled seed file

--- src/GCModeller/analysis/SequenceToolkit/SequencePatterns/Topologically/Seeds/SeedBox.vb
+++ src/GCModeller/analysis/SequenceToolkit/SequencePatterns/Topologically/Seeds/SeedBox.vb
@@ -33,6 +33,32 @@ Namespace Topologically.Seeding
             Next
         End Function
 
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="seeds$">序列种子文件的文件路径</param>
+        ''' <param name="seq"></param>
+        ''' <param name="min%"></param>
+        ''' <param name="max%"></param>
+        ''' <returns></returns>
+        Public Shared Iterator Function PopulateSeedsFromSeedsFile(seeds$, seq As I_PolymerSequenceModel, min%, max%) As IEnumerable(Of Seed())
+            Dim data As SeedData = SeedData.Load(seeds)
+            Dim lg = From seed As String
+                     In data.Seeds
+                     Let len As Integer = seed.Length
+                     Where len >= min AndAlso len <= max
+                     Select len, seed
+                     Group By len Into Group
+                     Order By len Ascending
+
+            For Each pack In lg
+                Dim avaliable = pack.Group.Where(Function(s) seq.SequenceData.IndexOf(s.seed) > -1)
+                Dim out As Seed() = avaliable.ToArray(Function(s) New Seed(s.seed))
+
+                Yield out
+            Next
+        End Function
+
         Private Function __trimAvaliable(seeds As IEnumerable(Of String)) As List(Of String)
             Dim out As New List(Of String)(seeds.Where(Function(s) __seq.SequenceData.IndexOf(s) > -1))
             Return out
