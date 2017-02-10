d1e8d9e0b4cfc553a35cdc19fb60621ee044815e
xieguigang <xie.guigang@gmail.com>
2017-02-10 22:51:42 +0800

    invoke helper

---
+++ src/Settings/Shared/Settings.Configuration/Session/Apps/VennDiagram.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+
+Namespace GCModellerApps
+
+    Public Class VennDiagram : Inherits InteropService
+
+        Sub New(exe$)
+            MyBase._executableAssembly = exe
+        End Sub
+    End Class
+End Namespace
