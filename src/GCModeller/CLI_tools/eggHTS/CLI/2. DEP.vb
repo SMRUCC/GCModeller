889bec9aeb7614e9dc94ca5ddb673a52297c2c3e
xieguigang <xie.guigang@gmail.com>
2017-02-10 22:37:54 +0800

    tools couple by CLI

--- src/GCModeller/CLI_tools/eggHTS/CLI/2. DEP.vb
+++ src/GCModeller/CLI_tools/eggHTS/CLI/2. DEP.vb
@@ -4,6 +4,7 @@ Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.genomics.Analysis.HTS.Proteomics
+Imports SMRUCC.genomics.Analysis.Microarray
 
 Partial Module CLI
 
@@ -32,5 +33,20 @@ Partial Module CLI
             .ToArray
 
         Return list.SaveTo(out).CLICode
+    End Function
+
+    <ExportAPI("/DEP.venn", Usage:="/DEP.venn /data <Directory> [/FC.tag <FC.avg> /pvalue <p.value> /out <out.DIR>]")>
+    Public Function VennData(args As CommandLine) As Integer
+        Dim DIR$ = args("/data")
+        Dim FCtag$ = args.GetValue("/FC.tag", "FC.avg")
+        Dim pvalue$ = args.GetValue("/pvalue", "p.value")
+        Dim out As String = args.GetValue("/out", DIR.TrimDIR & ".venn/")
+
+        Call DEGDesigner _
+            .MergeMatrix(DIR, "*.csv", 1.5, 0.05, FCtag, 1 / 1.5, pvalue) _
+            .SaveDataSet(out & "/DEP.venn.csv")
+
+
+        Return 0
     End Function
 End Module
