78defca3ba4b54d2b7dfcece0da71e6c4adabd6f
xieguigang <xie.guigang@gmail.com>
2017-02-12 17:40:09 +0800

    plot data from genbank

--- src/GCModeller/CLI_tools/Map/CLI/CLI.vb
+++ src/GCModeller/CLI_tools/Map/CLI/CLI.vb
@@ -1,7 +1,9 @@
-﻿Imports Microsoft.VisualBasic.CommandLine
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv.Extensions
 Imports SMRUCC.genomics.Assembly.NCBI.GenBank
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank.TabularFormat
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.RpsBLAST
 Imports SMRUCC.genomics.Visualize
 Imports SMRUCC.genomics.Visualize.ChromosomeMap
@@ -9,9 +11,9 @@ Imports SMRUCC.genomics.Visualize.ChromosomeMap.DrawingModels
 
 Public Module CLI
 
-    <ExportAPI("--Drawing.ChromosomeMap",
+    <ExportAPI("--Draw.ChromosomeMap",
                Info:="Drawing the chromosomes map from the PTT object as the basically genome information source.",
-               Usage:="--Drawing.ChromosomeMap /ptt <genome.ptt> [/conf <config.inf> /out <dir.export> /COG <cog.csv>]")>
+               Usage:="--Draw.ChromosomeMap /ptt <genome.ptt> [/conf <config.inf> /out <dir.export> /COG <cog.csv>]")>
     <Argument("/COG", True, CLITypes.File,
               AcceptTypes:={GetType(MyvaCOG)},
               Description:="The gene object color definition, you can using this parameter to overrides the cog definition in the PTT file.")>
@@ -19,19 +21,25 @@ Public Module CLI
         Dim PTT = args.GetObject("/ptt", AddressOf TabularFormat.PTT.Load)
         Dim out As String = args.GetValue("/out", App.CurrentDirectory)
         Dim confInf As String = args.GetValue("/conf", out & "/config.inf")
+        Dim COG As String = args("/COG")
+
+        Return PTT.Draw(COG, confInf, out)
+    End Function
+
+    <Extension>
+    Private Function Draw(PTT As PTT, COG$, conf$, out$) As Integer
         Dim config As ChromosomeMap.Configurations
 
-        If Not confInf.FileExists Then
-Create:     config = ChromosomeMap.GetDefaultConfiguration(confInf)
+        If Not conf.FileExists(True) Then
+Create:     config = ChromosomeMap.GetDefaultConfiguration(conf)
         Else
             Try
-                config = ChromosomeMap.LoadConfig(confInf)
+                config = ChromosomeMap.LoadConfig(conf)
             Catch ex As Exception
                 GoTo Create
             End Try
         End If
 
-        Dim COG As String = args("/COG")
         Dim device As DrawingDevice = ChromosomeMap.CreateDevice(config)
         Dim model As ChromesomeDrawingModel = ChromosomeMap.FromPTT(PTT, config)
 
@@ -43,5 +51,17 @@ Create:     config = ChromosomeMap.GetDefaultConfiguration(confInf)
         Return device _
             .InvokeDrawing(model) _
             .SaveImage(out, "tiff")
+    End Function
+
+    <ExportAPI("--Draw.ChromosomeMap.genbank",
+               Usage:="--Draw.ChromosomeMap.genbank /gb <genome.gbk> [/conf <config.inf> /out <dir.export> /COG <cog.csv>]")>
+    Public Function DrawGenbank(args As CommandLine) As Integer
+        Dim gb As String = args("/gb")
+        Dim out As String = args.GetValue("/out", App.CurrentDirectory)
+        Dim confInf As String = args.GetValue("/conf", out & "/config.inf")
+        Dim COG As String = args("/COG")
+        Dim PTT As PTT = GBFF.File.Load(gb).GbkffExportToPTT
+
+        Return PTT.Draw(COG, confInf, out)
     End Function
 End Module
