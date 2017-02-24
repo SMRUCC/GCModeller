e6d6551398f7a005cc54ff770cf716af435474da
xieguigang <xie.guigang@gmail.com>
2017-02-25 00:43:33 +0800

    improvements on the converts the matrix to network

--- src/GCModeller/CLI_tools/PhenoTree/CLI/Clustering.vb
+++ src/GCModeller/CLI_tools/PhenoTree/CLI/Clustering.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Drawing
+﻿Imports System.Collections.Specialized
+Imports System.Drawing
 Imports System.IO
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
@@ -14,12 +15,13 @@ Imports SMRUCC.genomics.SequenceModel.FASTA
 Partial Module CLI
 
     <ExportAPI("/locis.clustering",
-               Usage:="/locis.clustering /in <locis.fasta> [/first.ID /method <NeedlemanWunsch> /colors <clusters> /clusters <20> /out <out.DIR>]")>
+               Usage:="/locis.clustering /in <locis.fasta> [/cut <0> /first.ID /method <NeedlemanWunsch> /colors <clusters> /clusters <20> /out <out.DIR>]")>
     Public Function LociClustering(args As CommandLine) As Integer
         Dim [in] As String = args("/in")
         Dim method$ = args.GetValue("/method", "NeedlemanWunsch")
         Dim expected% = args.GetValue("/clusters", 20)
-        Dim out$ = args.GetValue("/out", [in].TrimSuffix & $"-{method}_expected={expected}.clusters/")
+        Dim cut# = args.GetValue("/cut", 0R)
+        Dim out$ = args.GetValue("/out", [in].TrimSuffix & $"-{method}_expected={expected}_cut={cut}.clusters/")
         Dim fasta As FastaFile = FastaFile.LoadNucleotideData([in])
         Dim firstID As Boolean = args.GetBoolean("/first.ID")
 
@@ -27,20 +29,24 @@ Partial Module CLI
             Call fasta.FirstTokenID
         End If
 
-        Using log As StreamWriter = (out & "/alignment.txt").OpenWriter(Encodings.ASCII)
-            Dim matrix As DataSet() = fasta.SimilarityMatrix(out:=log)
-            Dim clusters As EntityLDM() = matrix.KMeans(expected)
-            Dim colors As Color() = Designer.GetColors(args <= "/colors", expected)
-            Dim clusterColors As Dictionary(Of String, Color) = clusters _
-                .Select(Function(x) x.Cluster) _
-                .Distinct _
-                .SeqIterator _
-                .ToDictionary(Function(cluster) +cluster,
-                              Function(color) colors(color))
+        Dim arguments As New NameValueCollection
 
-            Call clusters.SaveTo(out & "/clusters.csv")
-            Call clusters.ToNetwork(clusterColors).Save(out)
-        End Using
+        Call arguments.Add("cut", cut)
+
+        Dim matrix As DataSet() = fasta.SimilarityMatrix(arguments)
+        Dim clusters As EntityLDM() = matrix.KMeans(expected)
+        Dim colors As Color() = Designer.GetColors(args <= "/colors", expected)
+        Dim clusterColors As Dictionary(Of String, Color) = clusters _
+            .Select(Function(x) x.Cluster) _
+            .Distinct _
+            .SeqIterator _
+            .ToDictionary(Function(cluster) +cluster,
+                          Function(color) colors(color))
+
+        Call clusters.SaveTo(out & "/clusters.csv")
+        Call clusters.ToNetwork(
+            clusterColors,
+            cut:=Function(score) score > cut).Save(out)
 
         Return 0
     End Function
--- src/GCModeller/analysis/SequenceToolkit/ClusterMatrix/Matrix/NeedlemanWunsch.vb
+++ src/GCModeller/analysis/SequenceToolkit/ClusterMatrix/Matrix/NeedlemanWunsch.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.IO
 Imports System.Runtime.CompilerServices
+Imports System.Text
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting
@@ -10,31 +11,40 @@ Public Module Matrix
 
     <Extension>
     Public Function NeedlemanWunsch(locis As IEnumerable(Of FastaToken), Optional ByRef out As StreamWriter = Nothing) As DataSet()
-        Dim dev As StreamWriter = out
-
-        If dev Is Nothing Then
-            dev = App.NullDevice
-            out = dev
-        End If
-
         Dim buffer As FastaToken() = locis.ToArray
         Dim LQuery = From fa As SeqValue(Of FastaToken)
                      In buffer _
                          .SeqIterator _
                          .AsParallel
                      Select index = fa.i,
-                         vector = (+fa).__needlemanWunsch(buffer, dev)
+                         vector = (+fa).__needlemanWunsch(buffer)
                      Order By index Ascending
-        Dim result As DataSet() = LQuery _
+        Dim tuples = LQuery.Select(Function(v) v.vector).ToArray
+        Dim result As DataSet() = tuples _
             .Select(Function(d) d.vector) _
             .ToArray
+
+        If Not out Is Nothing Then
+            For Each query In tuples
+                Dim block As String = Encoding.ASCII.GetString(
+                    query.log _
+                    .BaseStream _
+                    .As(Of MemoryStream) _
+                    .ToArray)
+
+                Call out.WriteLine(query.title)
+                Call out.WriteLine(block)
+                Call out.WriteLine("---------------------------------------------------------------------")
+                Call out.WriteLine()
+            Next
+        End If
 
         Return result
     End Function
 
     <Extension>
-    Private Function __needlemanWunsch(query As FastaToken, buffer As FastaToken(), out As StreamWriter) As DataSet
-        Dim dev As StreamWriter = App.NullDevice(GetEncodings(out.Encoding))
+    Private Function __needlemanWunsch(query As FastaToken, buffer As FastaToken()) As (vector As DataSet, log As StreamWriter, title$)
+        Dim dev As StreamWriter = App.NullDevice(Encodings.ASCII)
         Dim score#
         Dim vector As New Dictionary(Of String, Double)
 
@@ -44,25 +54,19 @@ Public Module Matrix
             vector.Add(target.Title, score)
         Next
 
-        SyncLock out
+        Dim buf As Byte() = dev _
+            .BaseStream _
+            .As(Of MemoryStream) _
+            .ToArray
+        Dim block$ = dev.Encoding _
+            .GetString(buf)
 
-            Dim buf As Byte() = dev _
-                .BaseStream _
-                .As(Of MemoryStream) _
-                .ToArray
-            Dim block$ = dev.Encoding _
-                .GetString(buf)
+        Call query.Title.__DEBUG_ECHO
 
-            Call out.WriteLine(query.Title)
-            Call out.WriteLine(block)
-            Call out.WriteLine()
-
-            Call query.Title.__DEBUG_ECHO
-        End SyncLock
-
-        Return New DataSet With {
+        Return (vector:=New DataSet With {
             .ID = query.Title,
             .Properties = vector
-        }
+        }, Log:=dev,
+        title:=query.Title)
     End Function
 End Module
