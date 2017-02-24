d7131544245522fe8ce8b07db93a3889ea6fc8ca
xieguigang <xie.guigang@gmail.com>
2017-02-24 21:41:29 +0800

    locis clustering and creates network

--- src/GCModeller/CLI_tools/PhenoTree/CLI/Clustering.vb
+++ src/GCModeller/CLI_tools/PhenoTree/CLI/Clustering.vb
@@ -1,15 +1,19 @@
-﻿Imports Microsoft.VisualBasic.CommandLine
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.DataMining.KMeans
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.genomics.Analysis.SequenceTools.ClusterMatrix
 Imports SMRUCC.genomics.SequenceModel.FASTA
+Imports Microsoft.VisualBasic.Imaging
 
 Partial Module CLI
 
     <ExportAPI("/locis.clustering",
-               Usage:="/locis.clustering /in <locis.fasta> [/first.ID /method <NeedlemanWunsch> /clusters <20> /out <out.DIR>]")>
+               Usage:="/locis.clustering /in <locis.fasta> [/first.ID /method <NeedlemanWunsch> /colors <clusters> /clusters <20> /out <out.DIR>]")>
     Public Function LociClustering(args As CommandLine) As Integer
         Dim [in] As String = args("/in")
         Dim method$ = args.GetValue("/method", "NeedlemanWunsch")
@@ -24,8 +28,16 @@ Partial Module CLI
 
         Dim matrix As DataSet() = fasta.SimilarityMatrix
         Dim clusters As EntityLDM() = matrix.KMeans(expected)
+        Dim colors As Color() = Designer.GetColors(args <= "/colors", expected)
+        Dim clusterColors As Dictionary(Of String, Color) = clusters _
+            .Select(Function(x) x.Cluster) _
+            .Distinct _
+            .SeqIterator _
+            .ToDictionary(Function(cluster) +cluster,
+                          Function(color) colors(color))
 
         Call clusters.SaveTo(out & "/clusters.csv")
+        Call clusters.ToNetwork(clusterColors).Save(out)
 
         Return 0
     End Function
