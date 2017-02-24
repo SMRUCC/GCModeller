f81e5502fad5efd59dfe9808ab0e101eaaeedde3
xieguigang <xie.guigang@gmail.com>
2017-02-24 20:57:24 +0800

    kmeans result to network model

---
+++ src/GCModeller/CLI_tools/PhenoTree/CLI/Clustering.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.DataMining.KMeans
+Imports SMRUCC.genomics.Analysis.SequenceTools.ClusterMatrix
+Imports SMRUCC.genomics.SequenceModel.FASTA
+
+Partial Module CLI
+
+    <ExportAPI("/locis.clustering",
+               Usage:="/locis.clustering /in <locis.fasta> [/first.ID /method <NeedlemanWunsch> /clusters <20> /out <out.DIR>]")>
+    Public Function LociClustering(args As CommandLine) As Integer
+        Dim [in] As String = args("/in")
+        Dim method$ = args.GetValue("/method", "NeedlemanWunsch")
+        Dim expected% = args.GetValue("/clusters", 20)
+        Dim out$ = args.GetValue("/out", [in].TrimSuffix & $"-{method}_expected={expected}.clusters/")
+        Dim fasta As FastaFile = FastaFile.LoadNucleotideData([in])
+        Dim firstID As Boolean = args.GetBoolean("/first.ID")
+
+        If firstID Then
+            Call fasta.FirstTokenID
+        End If
+
+        Dim matrix As DataSet() = fasta.SimilarityMatrix
+        Dim clusters As EntityLDM() = matrix.KMeans(expected)
+
+        Call clusters.SaveTo(out & "/clusters.csv")
+
+        Return 0
+    End Function
+End Module
--- src/GCModeller/analysis/SequenceToolkit/ClusterMatrix/Matrix/SmithWaterman.vb
+++ src/GCModeller/analysis/SequenceToolkit/ClusterMatrix/Matrix/SmithWaterman.vb
@@ -1,3 +1,25 @@
-﻿Partial Module Matrix
+﻿Imports System.Collections.Specialized
+Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.csv.IO
+Imports SMRUCC.genomics.SequenceModel.FASTA
 
+Partial Module Matrix
+
+    <Extension>
+    Public Function SimilarityMatrix(source As FastaFile,
+                                     Optional args As NameValueCollection = Nothing,
+                                     Optional method As MatrixMethods = MatrixMethods.NeedlemanWunsch,
+                                     Optional ByRef out As StreamWriter = Nothing) As DataSet()
+        If args Is Nothing Then
+            args = New NameValueCollection
+        End If
+
+        Select Case method
+            Case MatrixMethods.NeedlemanWunsch
+                Return source.NeedlemanWunsch(out)
+            Case Else
+                Return source.NeedlemanWunsch(out)
+        End Select
+    End Function
 End Module
