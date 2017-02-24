c7f69d49b3985d14bb2bf7ce159975d62ccd782f
xieguigang <xie.guigang@gmail.com>
2017-02-24 16:59:23 +0800

    improvements on the code of the fasta sequnece parser

--- src/GCModeller/analysis/SequenceToolkit/ClusterMatrix/Clusters.vb
+++ src/GCModeller/analysis/SequenceToolkit/ClusterMatrix/Clusters.vb
@@ -1,3 +1,18 @@
-﻿Public Module Clusters
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.SequenceModel.FASTA
 
+Public Module Clusters
+
+    <Extension>
+    Public Sub FirstTokenID(ByRef source As FastaFile, Optional delimiter$ = "|")
+        If delimiter = FastaToken.DefaultHeaderDelimiter Then
+
+        End If
+
+        For Each f As FastaToken In source
+            f.Attributes = {
+                f.Attributes(Scan0)
+            }
+        Next
+    End Sub
 End Module
--- src/GCModeller/analysis/SequenceToolkit/ClusterMatrix/Matrix/DistanceMatrix.vb
+++ src/GCModeller/analysis/SequenceToolkit/ClusterMatrix/Matrix/DistanceMatrix.vb
@@ -1,11 +1,16 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports SMRUCC.genomics.SequenceModel.FASTA
 
 Public Module DistanceMatrix
 
     <Extension>
-    Public Function NeedlemanWunsch(locis As IEnumerable(Of FastaToken)) As DataSet()
+    Public Function NeedlemanWunsch(locis As IEnumerable(Of FastaToken), Optional out As StreamWriter = Nothing) As DataSet()
+        If out Is Nothing Then
+            out = App.NullDevice
+        End If
+
 
     End Function
 End Module
