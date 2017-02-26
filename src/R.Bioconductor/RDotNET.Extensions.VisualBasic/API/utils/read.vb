03784c1d4c0e2f443fc190704068ac83b1f95fd5
xieguigang <xie.guigang@gmail.com>
2017-02-26 17:53:31 +0800

    initialize for enricher in the clusterProfiler wrapper

---
+++ src/R.Bioconductor/RDotNET.Extensions.VisualBasic/API/utils/read.vb
@@ -0,0 +1,31 @@
+﻿Imports RDotNET.Extensions.VisualBasic.SymbolBuilder
+
+Namespace API.utils
+
+    Public Module read
+
+        Public Function csv(file$,
+                            Optional header As Boolean = True,
+                            Optional sep$ = ",",
+                            Optional quote$ = "\""",
+                            Optional dec$ = ".",
+                            Optional fill As Boolean = True,
+                            Optional commentChar$ = "") As String
+            Dim var$ = App.NextTempName
+
+            SyncLock R
+                With R
+                    .call = $"{var} <- read.csv({file}, 
+header = {Rbool(header)}, 
+sep = {Rstring(sep)}, 
+quote = {Rstring(quote)},
+dec = {Rstring(dec)}, 
+fill = {Rbool(fill)}, 
+comment.char = {Rstring(commentChar)});"
+                End With
+            End SyncLock
+
+            Return var
+        End Function
+    End Module
+End Namespace
---
+++ src/R.Bioconductor/RDotNet.Extensions.Bioinformatics/Declares/clusterProfiler/Enricher.vb
@@ -0,0 +1,29 @@
+﻿
+Imports RDotNET.Extensions.VisualBasic.API.utils
+Imports RDotNET.Extensions.VisualBasic.API.base
+Imports RDotNET.Extensions.VisualBasic
+
+Namespace clusterProfiler
+
+    Public Class Enricher
+
+        Public ReadOnly Property go2name As String
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="goBrief$">``go_brief.csv``</param>
+        Sub New(goBrief$, Optional goID$ = "goID", Optional name$ = "name")
+            Dim header$ = c({goID, name}, stringVector:=True)
+
+            goBrief = read.csv(goBrief)
+            go2name = App.NextTempName
+
+            SyncLock R
+                With R
+                    .call = $"{go2name} <- {goBrief}[, {header}]"
+                End With
+            End SyncLock
+        End Sub
+    End Class
+End Namespace
