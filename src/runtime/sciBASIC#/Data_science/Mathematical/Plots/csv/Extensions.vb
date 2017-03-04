1625cee6d05e134751ac6ddfe83a658ebc9a8098
xieguigang <xie.guigang@gmail.com>
2017-03-04 15:18:14 +0800

    create data from csv file

--- src/runtime/sciBASIC#/Data_science/Mathematical/Plots/csv/Extensions.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematical/Plots/csv/Extensions.vb
@@ -1,5 +1,8 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Linq
 
 Namespace csv
 
@@ -7,7 +10,23 @@ Namespace csv
 
         <Extension>
         Public Function ScatterSerials(csv As File, fieldX$, fieldY$, color$, Optional ptSize! = 5) As ChartPlots.SerialData
+            With DataFrame.CreateObject(csv)
+                Dim index As (X%, y%) = (.GetOrdinal(fieldX), .GetOrdinal(fieldY))
+                Dim columns = .Columns.ToArray
+                Dim X = columns(index.X)
+                Dim Y = columns(index.y)
+                Dim pts As PointF() = X _
+                    .SeqIterator _
+                    .ToArray(Function(xi) New PointF(xi.value, Y(xi)))
+                Dim points As PointData() = pts.ToArray(Function(pt) New PointData(pt))
 
+                Return New ChartPlots.SerialData With {
+                    .color = color.TranslateColor,
+                    .PointSize = ptSize,
+                    .title = $"Plot({fieldX}, {fieldY})",
+                    .pts = points
+                }
+            End With
         End Function
     End Module
 End Namespace
