e828e385db162961416e720d4f53452a48809adb
xieguigang <xie.guigang@gmail.com>
2017-03-05 09:20:24 +0800

    plot of the scatter heatmap

--- src/runtime/sciBASIC#/Data_science/Mathematical/Plots/Scatter/Heatmap.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematical/Plots/Scatter/Heatmap.vb
@@ -1,8 +1,7 @@
 ﻿Imports System.Drawing
 Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.Imaging.Drawing2D
-Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Mathematical
@@ -39,7 +38,8 @@ Partial Module Scatter
                                 Optional bg$ = "white",
                                 Optional xlabel$ = Nothing,
                                 Optional ylabel$ = Nothing,
-                                Optional legendTitle$ = Nothing) As Image
+                                Optional legendTitle$ = Nothing,
+                                Optional ptSize% = 8) As Image
 
         Dim colors As Color() = Designer.GetColors(colorSchema, n:=levels)
 
@@ -62,14 +62,17 @@ Partial Module Scatter
             Sub(ByRef g, rect)
                 Call g.__plotInternal(rect, data.ToArray, colors,
                                       fieldX, fieldY, valueField,
-                                      labelX:=xlabel, labelY:=ylabel)
+                                      labelX:=xlabel, labelY:=ylabel,
+                                      ptSize:=ptSize,
+                                      legendTitle:=legendTitle)
             End Sub)
     End Function
 
     <Extension>
     Private Sub __plotInternal(g As Graphics, rect As GraphicsRegion, data As DataSet(), colors As Color(),
-                               fieldX$, fieldY$, fieldValue$, 
-                               labelX$,labelY$)
+                               fieldX$, fieldY$, fieldValue$,
+                               labelX$, labelY$, legendTitle$,
+                               ptSize%)
 
         Dim points As (pt As PointF, value#)() = data.ToArray(
             Function(o) (New PointF(o(fieldX), o(fieldY)), o(fieldValue)))
@@ -79,20 +82,32 @@ Partial Module Scatter
             .SeqIterator _
             .Select(Function(p) (p.value.pt, p.value.value,seq:=levels (p))) _
             .GroupBy(Function (o)o.seq  )
+        Dim colorHelper = colors.MapHelper
         Dim serials As SerialData() = valueGroups _
             .Select(Function(o) New SerialData() With {
-                .color = colors(o.Key),
+                .color = colorHelper(o.Key),
                 .pts = o.Select(Function(x) New PointData(x.Item1)).ToArray,
-                .title = o.Key
+                .title = o.Key,
+                .PointSize = ptSize
             }) _
             .ToArray
-        Dim scatterPlotSize As New Size(width:=rect.Size.Width * 0.8, height:=rect.Size.Height)
+        Dim leftWidth% = rect.Size.Width * 0.9
+        Dim scatterPlotSize As New Size(width:=leftWidth, height:=rect.Size.Height)
         Dim left As Image = Scatter.Plot(
             serials, scatterPlotSize,
             Xlabel:=labelX, Ylabel:=labelY, drawLine:=False, showLegend:=False)
+        Dim legend As Image = Legends.ColorMapLegend(
+            designer:=colors,
+            title:=legendTitle,
+            min:=points.Min(Function(pt) pt.value),
+            max:=points.Max(Function(pt) pt.value),
+            lsize:=New Size(rect.Size.Width - leftWidth + rect.Padding.Right, rect.Size.Height * 0.7))
+
+        leftWidth -= (rect.Padding.Right)
 
         With g
             .DrawImageUnscaled(left, New Point)
+            .DrawImage(legend, leftWidth, CInt((rect.Size.Height - legend.Height) / 2))
         End With
     End Sub
 End Module
