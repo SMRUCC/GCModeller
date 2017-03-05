29df754728c752cb4279c7614ab14d43e1f32505
xieguigang <xie.guigang@gmail.com>
2017-03-05 16:57:01 +0800

    split IO code

---
+++ src/GCModeller/core/Bio.Assembly/Assembly/DOOR/IO.vb
@@ -0,0 +1,26 @@
+﻿Namespace Assembly.DOOR
+
+    ''' <summary>
+    ''' Parser and writer
+    ''' </summary>
+    Public Module IO
+
+        ''' <summary>
+        ''' 解析已经读取的文本行为DOOR操纵子集合对象
+        ''' </summary>
+        ''' <param name="data"></param>
+        ''' <param name="path"></param>
+        ''' <returns></returns>
+        Public Function DocParser(data As String(), path As String) As DOOR
+            Dim LQuery As OperonGene() = (From s_Line As String In data.Skip(1)
+                                          Where Not String.IsNullOrEmpty(s_Line)
+                                          Select OperonGene.TryParse(s_Line)).ToArray
+            Dim DOOR As DOOR = New DOOR With {
+                .Genes = LQuery,
+                .FilePath = path
+            }
+            DOOR.DOOROperonView = Assembly.DOOR.CreateOperonView(DOOR)
+            Return DOOR
+        End Function
+    End Module
+End Namespace
--- src/GCModeller/core/Bio.Assembly/Assembly/DOOR/Models/DOOR.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/DOOR/Models/DOOR.vb
@@ -1,44 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::86b06a7b94ebb1d510e86dabbf7f5eb8, ..\GCModeller\core\Bio.Assembly\Assembly\DOOR\DOOR.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Text
-Imports SMRUCC.genomics.ComponentModel.Loci
-Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
-Imports Microsoft.VisualBasic.Scripting.MetaData
-Imports Microsoft.VisualBasic
 Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Text
 
 Namespace Assembly.DOOR
 
     ''' <summary>
-    ''' DOOR: Database of prOkaryotic OpeRons.
+    ''' ###### DOOR: Database of prOkaryotic OpeRons.
+    ''' 
     ''' Door operon prediction data.(Door操纵子预测数据)
     ''' </summary>
     ''' <remarks></remarks>
@@ -68,7 +65,7 @@ Namespace Assembly.DOOR
         ''' <summary>
         ''' 查找不到目标基因对象则会返回空值
         ''' </summary>
-        ''' <param name="locusId"></param>
+        ''' <param name="locusId">基因编号</param>
         ''' <returns></returns>
         Default Public ReadOnly Property GetGene(locusId As String) As OperonGene
             Get
@@ -83,22 +80,35 @@ Namespace Assembly.DOOR
         ''' <summary>
         ''' 查看目标基因是否是其所处的操纵子的第一个基因
         ''' </summary>
-        ''' <param name="locus"></param>
+        ''' <param name="locus">目标基因的基因编号</param>
         ''' <returns></returns>
         Public Function IsOprPromoter(locus As String) As Boolean
             Dim g As OperonGene = _geneHash(locus)
             Dim opr As String = g.OperonID
             Dim DOOR As Operon = Me.DOOROperonView.GetOperon(opr)
+
             Return String.Equals(DOOR.InitialX.Synonym, locus, StringComparison.OrdinalIgnoreCase)
         End Function
 
+        ''' <summary>
+        ''' 是否能够在存在的基因数据之中找得到目标基因对象？
+        ''' </summary>
+        ''' <param name="GeneID"></param>
+        ''' <returns></returns>
         Public Function ContainsGene(GeneID As String) As Boolean
             Return _geneHash.ContainsKey(GeneID)
         End Function
 
+        ''' <summary>
+        ''' 根据基因编号来判断这两个基因是否是处在相同的一个操纵子之中的
+        ''' </summary>
+        ''' <param name="a"></param>
+        ''' <param name="b"></param>
+        ''' <returns></returns>
         Public Function SameOperon(a As String, b As String) As Boolean
-            Dim ga = Me(a)
-            Dim gb = Me(b)
+            Dim ga As OperonGene = Me(a)
+            Dim gb As OperonGene = Me(b)
+
             If ga Is Nothing OrElse gb Is Nothing Then
                 Return False
             End If
@@ -106,6 +116,11 @@ Namespace Assembly.DOOR
             Return String.Equals(ga.OperonID, gb.OperonID)
         End Function
 
+        ''' <summary>
+        ''' 根据操纵子编号来获取改操纵子之中的一系列的结构基因
+        ''' </summary>
+        ''' <param name="OperonId"></param>
+        ''' <returns></returns>
         Public Function [Select](OperonId As String) As OperonGene()
             Dim LQuery = LinqAPI.Exec(Of OperonGene) <=
  _
@@ -137,38 +152,6 @@ Namespace Assembly.DOOR
             Return DOOR_API.Load(Path)
         End Operator
 
-        Private Function __generate(sId As String, trim As Boolean) As String
-            Dim Genes As OperonGene() = Me.[Select](OperonId:=sId)
-
-            If trim Then
-                If Genes.Length < 2 Then
-                    Return ""
-                End If
-            End If
-
-            Dim sBuilder As StringBuilder = New StringBuilder(1024)
-            Dim LocationBuilder As StringBuilder = New StringBuilder(128)
-
-            For Each obj In Genes
-                Dim strand As String = If(
-                    obj.Location.Strand = Strands.Forward,
-                    "+; ",
-                    "-; ")
-
-                Call sBuilder.Append(obj.Synonym & "; ")
-                Call LocationBuilder.Append(strand)
-            Next
-            Call sBuilder.Remove(sBuilder.Length - 2, 2)
-            Call LocationBuilder.Remove(LocationBuilder.Length - 2, 2)
-
-            Dim strData As New StringBuilder(1024)
-            Call strData.Append(String.Format("{0},{1},", sId, Genes.Length))
-            Call strData.Append(LocationBuilder.ToString & ",")
-            Call strData.Append(sBuilder.ToString)
-
-            Return strData.ToString
-        End Function
-
         Public Shared Function CreateEmpty() As DOOR
             Return New DOOR With {
                 .Genes = New OperonGene() {},
@@ -178,51 +161,20 @@ Namespace Assembly.DOOR
             }
         End Function
 
-        Public Shared Function DocParser(data As String(), path As String) As DOOR
-            Dim LQuery As OperonGene() = (From s_Line As String In data.Skip(1)
-                                         Where Not String.IsNullOrEmpty(s_Line)
-                                         Select OperonGene.TryParse(s_Line)).ToArray
-            Dim DOOR As DOOR = New DOOR With {
-                .Genes = LQuery,
-                .FilePath = path
-            }
-            DOOR.DOOROperonView = Assembly.DOOR.CreateOperonView(DOOR)
-            Return DOOR
-        End Function
-
         ''' <summary>
         ''' 导出为一个Csv格式的文件
         ''' </summary>
-        ''' <param name="SavedPath"></param>
-        ''' <param name="Trim">是否将仅有一个基因的Operon对象进行去除</param>
+        ''' <param name="path"></param>
+        ''' <param name="trim">是否将仅有一个基因的Operon对象进行去除</param>
         ''' <returns></returns>
         ''' <remarks></remarks>
-        Public Function Export(SavedPath As String, Trim As Boolean) As Boolean
-            Dim OperonIds As String() = LinqAPI.Exec(Of String) <=
- _
-                From gene As OperonGene
-                In Genes
-                Select gene.OperonID
-                Distinct
-                Order By OperonID Ascending
-
-            Dim LQuery = LinqAPI.MakeList(Of String) <=
-                From OperonID As String
-                In OperonIds
-                Let row As String = __generate(OperonID, Trim)
-                Where Not String.IsNullOrEmpty(row)
-                Select row
-
-            Call LQuery.Insert(0, "OperonID,Counts,Direction,OperonGenes")
-
+        Public Function Export(path$, Optional trim As Boolean = False) As Boolean
             Try
-                Call LQuery.SaveTo(SavedPath)
+                Return csv(trim).SaveTo(path, Encodings.ASCII.CodePage)
             Catch ex As Exception
-                Call App.LogException(New Exception(SavedPath, ex))
+                Call App.LogException(New Exception(path, ex))
                 Return False
             End Try
-
-            Return True
         End Function
 
         Public Iterator Function GetEnumerator() As IEnumerator(Of Operon) Implements IEnumerable(Of Operon).GetEnumerator
