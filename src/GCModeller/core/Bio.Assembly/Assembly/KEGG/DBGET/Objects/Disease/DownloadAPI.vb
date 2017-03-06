6e00af53cff45ff5652d621b7680fc28021239bc
xieguigang <xie.guigang@gmail.com>
2017-03-06 21:58:44 +0800

    kegg disease parser api test successs

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/Objects/Disease/DownloadAPI.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/Objects/Disease/DownloadAPI.vb
@@ -1,7 +1,9 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.ComponentModel
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm.base
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Text.HtmlParser
 Imports SMRUCC.genomics.Assembly.KEGG.WebServices
 Imports SMRUCC.genomics.Assembly.KEGG.WebServices.InternalWebFormParsers
@@ -27,14 +29,55 @@ Namespace Assembly.KEGG.DBGET.bGetObject
                 .Pathway = PathwayWebParser.__parseHTML_ModuleList(html("Pathway").FirstOrDefault, LIST_TYPES.Pathway),
                 .Comment = html.GetText("Comment"),
                 .References = html.References,
-                .Markers = html("Marker").FirstOrDefault.MarkerList
+                .Markers = html("Marker").FirstOrDefault.MarkerList,
+                .Genes = html("Gene").FirstOrDefault.MarkerList,
+                .Drug = html("Drug").FirstOrDefault.__pairList(
+                    Function(s$)
+                        Dim id$ = Regex.Match(s, "\[.+?\]", RegexICSng) _
+                            .Value _
+                            .GetStackValue("[", "]")
+                        Return New KeyValuePair With {
+                            .Key = s,
+                            .Value = id
+                        }
+                    End Function),
+                .OtherDBs = html("Other DBs").FirstOrDefault.__otherDBs
             }
 
             Return dis
         End Function
 
         <Extension>
-        Private Function MarkerList(html$) As TripleKeyValuesPair()
+        Private Function __otherDBs(html$) As KeyValuePair()
+            Dim lines$() = html _
+                .DivInternals _
+                .ToArray(Function(s) s.StripHTMLTags(stripBlank:=True))
+            Dim slides = lines.SlideWindows(2, offset:=2)
+            Dim out As New List(Of KeyValuePair)
+
+            For Each s As SlideWindowHandle(Of String) In slides
+                out += New KeyValuePair With {
+                    .Key = s(Scan0).Trim(":"c),
+                    .Value = s(1)
+                }
+            Next
+
+            Return out
+        End Function
+
+        <Extension>
+        Private Function __pairList(html$, parser As Func(Of String, KeyValuePair)) As KeyValuePair()
+            Dim lines$() = html.DivInternals _
+                .FirstOrDefault _
+                .HtmlLines _
+                .Select(Function(s) s.StripHTMLTags(stripBlank:=True)) _
+                .Where(Function(s) Not s.StringEmpty) _
+                .ToArray
+            Dim out As KeyValuePair() = lines.ToArray(parser)
+            Return out
+        End Function
+
+        <Extension> Private Function MarkerList(html$) As TripleKeyValuesPair()
             Dim list As New List(Of TripleKeyValuesPair)
 
             html = html.DivInternals.FirstOrDefault
