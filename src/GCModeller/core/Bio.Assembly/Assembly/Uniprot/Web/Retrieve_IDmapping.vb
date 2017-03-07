afaa2056a4d7b52beba991c30718295b00dc1a54
谢桂纲 <xie.guigang@live.com>
2017-03-07 18:03:07 +0800

    ZERO return packages???

--- src/GCModeller/core/Bio.Assembly/Assembly/Uniprot/Web/Retrieve_IDmapping.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/Uniprot/Web/Retrieve_IDmapping.vb
@@ -1,32 +1,33 @@
 ﻿#Region "Microsoft.VisualBasic::e3dfb986b5834a64144d124ed1ceffbe, ..\GCModeller\core\Bio.Assembly\Assembly\Uniprot\Web\Retrieve_IDmapping.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Collections.Specialized
+Imports System.Text.RegularExpressions
 Imports System.Threading
 Imports Microsoft.VisualBasic.Text
 Imports Microsoft.VisualBasic.Text.HtmlParser
@@ -79,6 +80,17 @@ Namespace Assembly.Uniprot.Web
             Dim url$ = "http://www.uniprot.org/uploadlists/"
             Dim html As String = url.POST(args, "http://www.uniprot.org/uploadlists/",)
             Dim query$ = html.HTMLTitle.Split.First
+
+            ' 2017-3-7
+            ' 由于IE内核的版本不同的原因，所返回来的html文本会有些差异，所以对于query编号的解析会有些差异
+            ' 这里是处理win7老平台上面的query解析的操作
+            If query = "mapping" Then
+                query = Regex.Match(html, "new mappingResults\('.+?'\)", RegexICSng).Value
+                query = query.GetStackValue("(", ")")
+                query = query.Trim("'"c)
+                query = "yourlist:" & query
+            End If
+
             Dim uid$ = query.Split(":"c).Last
 
             Call Thread.Sleep(1000)
