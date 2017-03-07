946af7ebb8fdda9b892adedf77814bff4da26910
xieguigang <xie.guigang@gmail.com>
2017-03-07 22:20:10 +0800

    kegg disease model

---
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Medical/Disease.vb
@@ -0,0 +1,22 @@
+﻿Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
+
+Namespace Assembly.KEGG
+
+    Public Class Disease
+
+        Public Property Entry As String
+        Public Property Name As String
+        Public Property Description As String
+        Public Property Category As String
+        Public Property Genes As String()
+        Public Property Carcinogen As String
+        Public Property Markers As String()
+        Public Property Drugs As String()
+        Public Property Comments As String()
+        Public Property DbLinks As DBLink()
+        Public Property References As Reference()
+        Public Property Env_factors As String()
+
+    End Class
+End Namespace
