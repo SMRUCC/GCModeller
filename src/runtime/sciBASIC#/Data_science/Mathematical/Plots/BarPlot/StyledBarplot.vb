2e347d49022b6f32143957f44f618cc182be339c
xieguigang <xie.guigang@gmail.com>
2017-03-11 19:46:30 +0800

    fix some errors in the imaging parameters parser

--- src/runtime/sciBASIC#/Data_science/Mathematical/Plots/BarPlot/StyledBarplot.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematical/Plots/BarPlot/StyledBarplot.vb
@@ -1,10 +1,14 @@
 ﻿Imports System.Drawing
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Axis
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS
+Imports Microsoft.VisualBasic.Scripting
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace BarPlot
@@ -30,26 +34,73 @@ Namespace BarPlot
         ''' <param name="data">这个数据集之中是没有同组比较数据的</param>
         ''' <param name="size"></param>
         ''' <param name="padding$"></param>
+        ''' <param name="interval">The interval distance between the bars, percentage of the width</param>
         ''' <returns></returns>
         <Extension>
         Public Function Plot(data As IEnumerable(Of BarSerial),
-                             Optional size As Size = Nothing,
+                             Optional size$ = "1024,800",
                              Optional padding$ = g.DefaultPadding,
-                             Optional bg$ = "white") As Bitmap
+                             Optional bg$ = "white",
+                             Optional interval# = 0.085,
+                             Optional labelFont$ = CSSFont.Win10Normal,
+                             Optional shadowOffset% = 4) As Bitmap
 
             Return g.GraphicsPlots(
-                size, padding,
+                size.SizeParser, padding,
                 bg,
                 Sub(ByRef g, region)
                     Call data _
                         .ToArray _
-                        .__plotInternal(g, region)
+                        .__plotInternal(g, region,
+                                        interval:=interval * region.PlotRegion.Width,
+                                        labelFont:=labelFont,
+                                        shadowOffset:=shadowOffset)
                 End Sub)
         End Function
 
         <Extension>
-        Private Sub __plotInternal(data As BarSerial(), g As Graphics, region As GraphicsRegion)
+        Private Sub __plotInternal(data As BarSerial(),
+                                   g As Graphics, region As GraphicsRegion,
+                                   interval%,
+                                   labelFont$,
+                                   shadowOffset%)
 
+            Dim scaler As New Mapper(
+                range:=New Scaling(data.Select(Function(o) o.Value), horizontal:=False),
+                ignoreX:=True)
+            Dim bWidth% = (region.PlotRegion.Width - data.Length * interval) / data.Length
+            Dim bTop%
+            Dim hScaler As Func(Of Single, Single) =
+                scaler _
+                .YScaler(region.Size, region.Padding)
+            Dim bLeft% = region.Padding.Left + interval / 2
+            Dim bRECT As Rectangle   ' shadow rectangle
+            Dim barRECT As Rectangle
+            Dim label As Image
+            Dim labelLeft%
+
+            With region
+                Call g.DrawAxis(.Size, .Padding, scaler, showGrid:=True)
+            End With
+
+            For Each s As BarSerial In data
+                bTop = hScaler(s.Value)
+                bRECT = BarPlotAPI.Rectangle(bTop, bLeft, bLeft + bWidth, region.PlotRegion.Bottom)
+                barRECT = New Rectangle(bRECT.Location.OffSet2D(-2, -2), bRECT.Size)
+
+                ' Draw shadows
+                g.FillRectangle(Brushes.Gray, bRECT)
+                ' Draw bar
+                g.FillRectangle(s.Brush.GetBrush, barRECT)
+                ' Draw label
+                label = DrawLabel(s.Label, cssFont:=labelFont)
+                ' rotate -90
+                label = label.RotateImage(-90)
+                labelLeft = bLeft + (bWidth - label.Width) / 2
+                g.DrawImageUnscaled(label, New Point(labelLeft, region.Bottom + 20))
+
+                bLeft += interval + bWidth
+            Next
         End Sub
 
         ''' <summary>
