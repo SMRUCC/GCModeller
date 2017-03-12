0a20f48e861bcb54be7db08983d88cc553d120f6
xieguigang <xie.guigang@gmail.com>
2017-03-12 09:28:01 +0800

    move compounds html parser code to new api module

---
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/Objects/Pathway/Metabolites/Compound.vb
@@ -0,0 +1,156 @@
+﻿#Region "Microsoft.VisualBasic::c986bccdc44279ff1d6bc76b216e5791, ..\GCModeller\core\Bio.Assembly\Assembly\KEGG\DBGET\Objects\Pathway\Compound.vb"
+
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports System.Text.RegularExpressions
+Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Text.HtmlParser
+Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
+Imports SMRUCC.genomics.ComponentModel.EquaionModel
+
+Namespace Assembly.KEGG.DBGET.bGetObject
+
+    <XmlRoot("KEGG.Compound", Namespace:="http://www.kegg.jp/dbget-bin/www_bget?cpd:compound_id")>
+    Public Class Compound : Implements ICompoundObject
+
+        ''' <summary>
+        ''' KEGG compound ID: ``cpd:C\d+``
+        ''' </summary>
+        ''' <returns></returns>
+        Public Overridable Property Entry As String Implements ICompoundObject.Key, ICompoundObject.KEGG_cpd
+        ''' <summary>
+        ''' Name
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property CommonNames As String() Implements ICompoundObject.CommonNames
+        Public Property Formula As String
+        Public Property MolWeight As Double
+        Public Property ExactMass As Double
+
+        ''' <summary>
+        ''' The <see cref="Entry">compound</see> was involved in these reactions. (http://www.kegg.jp/dbget-bin/www_bget?rn:[KEGG_Reaction_ID])
+        ''' </summary>
+        ''' <value></value>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Property KEGG_reaction As String()
+        Public Property Pathway As String()
+        Public Property [Module] As String()
+        Public Property Remarks As String()
+
+        Dim _DBLinks As DBLinks
+        Public Property DbLinks As String() 'Implements MetaCyc.Schema.CompoundsMapping.ICompoundObject.DBLinks
+            Get
+                If _DBLinks Is Nothing Then
+                    Return New String() {}
+                End If
+                Return _DBLinks.DBLinks
+            End Get
+            Set(value As String())
+                _DBLinks = New DBLinks(value)
+            End Set
+        End Property
+
+        Sub New()
+        End Sub
+
+        Sub New(dblinks As DBLinks)
+            _DBLinks = dblinks
+        End Sub
+
+        Public Function GetDBLinkManager() As DBLinks
+            Return _DBLinks
+        End Function
+
+        ''' <summary>
+        ''' 下载代谢物的结构图
+        ''' </summary>
+        ''' <param name="save">所下载的结构图的保存文件路径</param>
+        Public Sub DownloadStructureImage(save As String)
+            Dim Url As String = String.Format("http://www.kegg.jp/Fig/compound/{0}.gif", Entry)
+            Call Url.DownloadFile(save)
+        End Sub
+
+        Public Function GetPathways() As NamedValue(Of String)()
+            Return __parseNamedData(Pathway)
+        End Function
+
+        Public Function GetModules() As NamedValue(Of String)()
+            Return __parseNamedData([Module])
+        End Function
+
+        Public Function GetDBLinks() As DBLink()
+            Return _DBLinks.DBLinkObjects.ToArray
+        End Function
+
+        Private Shared Function __parseNamedData(strData As String()) As NamedValue(Of String)()
+            Dim LQuery = LinqAPI.Exec(Of NamedValue(Of String)) <=
+ _
+                From s As String
+                In strData
+                Let Id As String = Regex.Match(s, "[.+?]").Value
+                Let value As String = s.Replace(Id, "").Trim
+                Select New NamedValue(Of String) With {
+                    .Name = Id,
+                    .Value = value
+                }
+
+            Return LQuery
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return String.Format("{0}: {1}", Entry, Me.Formula)
+        End Function
+
+        Public Property CHEBI As String() Implements ICompoundObject.CHEBI
+            Get
+                If _DBLinks.IsNullOrEmpty OrElse _DBLinks.CHEBI.IsNullOrEmpty Then
+                    Return Nothing
+                End If
+                Return (From item In _DBLinks.CHEBI Select item.Entry).ToArray
+            End Get
+            Set(value As String())
+                _DBLinks.AddEntry(New DBLink With {.DBName = "CHEBI", .Entry = value.First})
+            End Set
+        End Property
+
+        Public Property PUBCHEM As String Implements ICompoundObject.PUBCHEM
+            Get
+                If _DBLinks.IsNullOrEmpty OrElse _DBLinks.PUBCHEM Is Nothing Then
+                    Return ""
+                End If
+                Return _DBLinks.PUBCHEM.Entry
+            End Get
+            Set(value As String)
+                _DBLinks.AddEntry(New DBLink With {.DBName = "PUBCHEM", .Entry = value})
+            End Set
+        End Property
+    End Class
+End Namespace
---
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/Objects/Pathway/Metabolites/MetabolitesDBGet.vb
@@ -0,0 +1,130 @@
+﻿Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.ComponentModel
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Text.HtmlParser
+Imports SMRUCC.genomics.Assembly.KEGG.WebServices.InternalWebFormParsers
+Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
+
+Namespace Assembly.KEGG.DBGET.bGetObject
+
+    Public Module MetabolitesDBGet
+
+        Const URL = "http://www.kegg.jp/dbget-bin/www_bget?cpd:{0}"
+
+        Public Function Download(Id As String) As Compound
+            Return DownloadFrom(url:=String.Format(URL, Id))
+        End Function
+
+        Public Function DownloadFrom(url As String) As Compound
+            Dim html As New WebForm(url)
+            Dim links As DBLinks = GetDBLinks(html.GetValue("Other DBs").FirstOrDefault)
+            Dim cpd As New Compound(links) With {
+                .Entry = Regex.Match(html.GetValue("Entry").FirstOrDefault, "[GC]\d+").Value
+            }
+            cpd.CommonNames = GetCommonNames(html.GetValue("Name").FirstOrDefault())
+            cpd.Formula = html.GetValue("Formula").FirstOrDefault.Replace("<br>", "")
+            cpd.KEGG_reaction = GetReactionList(html.GetValue("Reaction").FirstOrDefault)
+            cpd.Pathway = (From s As KeyValuePair In WebForm.parseList(html.GetValue("Pathway").FirstOrDefault, "<a href="".*/kegg-bin/show_pathway\?.+?"">.+?</a>") Select String.Format("[{0}] {1}", s.Key, s.Value)).ToArray
+            cpd.Module = (From s As KeyValuePair In WebForm.parseList(html.GetValue("Module").FirstOrDefault, "<a href="".*/kegg-bin/show_module\?.+?"">.+?</a>") Select String.Format("[{0}] {1}", s.Key, s.Value)).ToArray
+            cpd.MolWeight = Val(html.GetValue("Mol weight").FirstOrDefault)
+
+            Return cpd
+        End Function
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="lstId"></param>
+        ''' <param name="EXPORT"></param>
+        ''' <returns>返回成功下载的对象的数目</returns>
+        ''' <remarks></remarks>
+        Public Function FetchTo(lstId As String(), EXPORT As String) As Integer
+            Dim i As Integer = 0
+
+            Call $"{lstId} go to download!".__DEBUG_ECHO
+
+            For Each Id As String In lstId
+                Dim path As String = String.Format("{0}/{1}.xml", EXPORT, Id)
+
+                If Not path.FileExists Then
+                    Dim CompoundData As Compound = Download(Id)
+
+                    If CompoundData Is Nothing Then
+#If DEBUG Then
+                        Call Console.WriteLine("{0} download failure!", Id)
+#End If
+                        Continue For
+                    End If
+                    i += 1
+                    Call CompoundData.GetXml.SaveTo(path)
+                End If
+            Next
+
+            Return i
+        End Function
+
+        Const FLAG As String = "[a-z0-9-_.]+"
+        Const REGEX_DBLINK As String = FLAG & ": (\s*<a href="".+?"">" & FLAG & "</a>\s*)+"
+
+        Friend Function GetDBLinks(strData As String) As DBLinks
+            If String.IsNullOrEmpty(strData) Then
+                Return Nothing
+            End If
+
+            Dim Tokens As String() = (From m As Match
+                                      In Regex.Matches(strData, REGEX_DBLINK, RegexOptions.IgnoreCase)
+                                      Select m.Value).ToArray
+            Dim LQuery As IEnumerable(Of DBLink()) = From s As String In Tokens Select TryParse(s)
+            Return New DBLinks(LQuery.IteratesALL)
+        End Function
+
+        Private Function TryParse(str As String) As DBLink()
+            Dim TempChunk As String() = Strings.Split(str, ": ")
+            Dim DBName As String = TempChunk.First
+            Dim Entries As String() = GetValues(TempChunk.Last)
+            Dim LQuery As String = (From prefixName As String
+                                    In ComponentModel.DBLinkBuilder.DBLinks.PrefixDB
+                                    Where InStr(DBName, prefixName, CompareMethod.Text) > 0
+                                    Select prefixName).FirstOrDefault
+
+            DBName = If(String.IsNullOrEmpty(LQuery), DBName, LQuery)
+
+            Return (From s As String
+                    In Entries
+                    Select New DBLink With {.DBName = DBName, .Entry = s}).ToArray
+        End Function
+
+        Private Function GetValues(str As String) As String()
+            Dim buf As String() =
+                Regex.Matches(str, "<a href="".+?"">.+?</a>") _
+               .ToArray(AddressOf HtmlStrips.GetValue)
+            Return buf
+        End Function
+
+        Friend Function GetReactionList(strData As String) As String()
+            If String.IsNullOrEmpty(strData) Then
+                Return New String() {}
+            End If
+
+            Dim buf As String() =
+                Regex.Matches(strData, "<a href="".+?"">.+?</a>", RegexOptions.Singleline) _
+               .ToArray(AddressOf HtmlStrips.GetValue)
+            Return buf
+        End Function
+
+        Friend Function GetCommonNames(strData As String) As String()
+            If String.IsNullOrEmpty(strData) Then
+                Return New String() {}
+            End If
+
+            Dim buf As String() = Strings.Split(strData, "<br>")
+            buf = (From s As String
+                   In buf
+                   Let strItem As String = s.Replace(";", "").Trim
+                   Where Not String.IsNullOrEmpty(strItem)
+                   Select strItem).ToArray
+
+            Return buf
+        End Function
+    End Module
+End Namespace
