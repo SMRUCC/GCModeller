975e01359b019eadf3e0df251f52007b6e90ec1b
谢桂纲 <xie.guigang@gcmodeller.org>
2017-03-15 14:29:35 +0800

    get KEGG pathway gene ID list

--- src/GCModeller/CLI_tools/KEGG/CLI/Pathways.vb
+++ src/GCModeller/CLI_tools/KEGG/CLI/Pathways.vb
@@ -1,34 +1,35 @@
 ﻿#Region "Microsoft.VisualBasic::105b0ca3e47c8a2d8d995380a553d473, ..\GCModeller\CLI_tools\KEGG\CLI\Pathways.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports SMRUCC.genomics.Assembly.KEGG.Archives.Xml
+Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
 
 Partial Module CLI
 
@@ -46,5 +47,14 @@ Partial Module CLI
             KEGGReactions:=reactions,
             speciesCode:=sp)
         Return model.SaveAsXml(out).CLICode
+    End Function
+
+    <ExportAPI("/Pathway.geneIDs",
+               Usage:="/Pathway.geneIDs /in <pathway.XML> [/out <out.list.txt>]")>
+    Public Function PathwayGeneList(args As CommandLine) As Integer
+        Dim in$ = args <= "/in"
+        Dim out As String = args.GetValue("/out", [in].TrimSuffix & ".geneIDs.txt")
+        Dim pathway As Pathway = [in].LoadXml(Of Pathway)
+        Return pathway.GetPathwayGenes.SaveTo(out).CLICode
     End Function
 End Module
