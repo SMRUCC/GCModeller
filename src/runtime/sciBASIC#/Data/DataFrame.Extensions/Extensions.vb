cdb7dc4a8291d8ca918403d94380f26f57705d2c
xieguigang <xie.guigang@gmail.com>
2017-03-28 19:37:50 +0800

    summary api for vb dataframe

--- src/runtime/sciBASIC#/Data/DataFrame.Extensions/Extensions.vb
+++ src/runtime/sciBASIC#/Data/DataFrame.Extensions/Extensions.vb
@@ -27,9 +27,8 @@
 #End Region
 
 Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.Data.csv.IO
-Imports Microsoft.VisualBasic.Data.csv.StorageProvider.ComponentModels
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Serialization
@@ -68,7 +67,8 @@ Public Module SchemasAPI
     ''' <typeparam name="T"></typeparam>
     ''' <param name="source"></param>
     ''' <param name="primary$">
-    ''' 如果没有指定主键域的话，会默认用元素在集合之中的index编号来作为<see cref="EntityObject.ID"/>的属性值
+    ''' 如果没有指定主键域的话，会默认用元素在集合之中的index编号来作为<see cref="EntityObject.ID"/>的属性值，
+    ''' 假若目标类型也继承了<see cref="INamedValue"/>则会自动读取这个属性作为主键
     ''' </param>
     ''' <returns></returns>
     ''' 
@@ -78,16 +78,21 @@ Public Module SchemasAPI
     End Function
 
     Public Function Summary(source As IEnumerable, type As Type, Optional primary$ = Nothing) As EntityObject()
-        Dim schema As SchemaProvider = SchemaProvider.CreateObject(type).CopyReadDataFromObject
+        Dim schema As [Class] = [Class].GetSchema(type)
         Dim getID As Func(Of SeqValue(Of Object), String)
         Dim out As New List(Of EntityObject)
+        Dim primaryField As Field
 
-        If primary.StringEmpty Then
-            Dim field = schema.GetField(primary)
+        If Not primary.StringEmpty Then
+            primaryField = schema.GetField(primary)
+        Else
+            primaryField = schema.PrimaryField
+        End If
 
-            schema.Remove(primary)
+        If Not primaryField Is Nothing Then
+            schema.Remove(primaryField.Name)
             getID = Function(o)
-                        Dim value As Object = field _
+                        Dim value As Object = primaryField _
                             .BindProperty _
                             .GetValue(+o)
                         Return Scripting.CStrSafe(value)
@@ -97,19 +102,62 @@ Public Module SchemasAPI
         End If
 
         For Each i As SeqValue(Of Object) In source.SeqIterator
-            Dim ID$ = getID(i)
-            Dim table As New Dictionary(Of String, String)
-
-            For Each field In schema
-                If DataFramework.IsPrimitive(field.BindProperty.PropertyType) Then
-                    table.Add(field.Name, field.GetValue(+i))
-                Else
-                    ' 递归进行下一级的展开
-
-                End If
-            Next
+            out += schema.Summary(
+                (+i),
+                stack:="",
+                fill:=New EntityObject With {
+                    .ID = getID(i),
+                    .Properties = New Dictionary(Of String, String)
+                })
         Next
 
         Return out
+    End Function
+
+    ''' <summary>
+    ''' 获取Schema之中的主键域
+    ''' </summary>
+    ''' <param name="schema"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function PrimaryField(schema As [Class]) As Field
+        Dim ilist As Type() = schema.Type.GetInterfaces
+
+        For Each [interface] As Type In {
+            GetType(INamedValue),
+            GetType(IReadOnlyId)
+        }
+            If ilist.IndexOf([interface]) > -1 Then
+                Dim map = schema.Type.GetInterfaceMap([interface])
+                Dim props = map.TargetMethods
+
+                ' 不能够查找出实现接口的属性？？？
+
+            End If
+        Next
+
+        Return Nothing
+    End Function
+
+    <Extension>
+    Public Function Summary(schema As [Class], o As Object, ByRef fill As EntityObject, stack$) As EntityObject
+        For Each prop As Field In schema
+            Dim name$ = prop.Name
+
+            If Not stack.StringEmpty Then
+                name = $"{stack}${name}"
+            End If
+
+            If Scripting.IsPrimitive(prop.Type) Then
+                Dim s$ = Scripting.CStrSafe(prop.GetValue(o))
+                fill.Properties.Add(name, s)
+            Else
+                ' 对于复杂类型，进行递归展开
+                Dim [sub] As Object = prop.GetValue(o)
+                prop.InnerClass.Summary([sub], fill, stack:=name)
+            End If
+        Next
+
+        Return fill
     End Function
 End Module
