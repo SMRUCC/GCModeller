fce4b797cebb3f1e805bfab2ecdd71314d678298
xieguigang <xie.guigang@gmail.com>
2017-03-29 22:22:25 +0800

    fixed for interpro

--- mysql/build.bat
+++ mysql/build.bat
@@ -1,12 +1,12 @@
 REM converts the mysql dataabase schema source code to visualbasic source code
 
-"Reflector" --reflects -o "../src/repository/Model_Repository/MySQL/" /namespace "MySQL.Tables" /split --language=visualbasic /sql "../src/repository/Model_Repository/MySQL/correlations.sql"
+REM "Reflector" --reflects -o "../src/repository/Model_Repository/MySQL/" /namespace "MySQL.Tables" /split --language=visualbasic /sql "../src/repository/Model_Repository/MySQL/correlations.sql"
 
 REM "Reflector" --reflects -o "../src/repository/nt/mysql/NCBI" /namespace "mysql.NCBI" /split --language=visualbasic /sql "../src/repository/ncbi.sql"
 
 REM "Reflector" --reflects -o "./KEGG_Schema/" /namespace "LocalMySQL" /split --language=visualbasic /sql "./jp_kegg2.sql"
 REM "Reflector" --reflects -o "./ExplorEnz/" /namespace "ExplorEnz.MySQL" /split --language=visualbasic /sql "./ExplorEnz.sql"
-REM "Reflector" --reflects -o "./interpro/" /namespace "Interpro.Tables" /split --language=visualbasic /sql "./interpro.sql"
+"Reflector" --reflects -o "../src/repository/DataMySql/Interpro/Tables/" /namespace "Interpro.Tables" /split --language=visualbasic /sql "../src/repository/DataMySql/Interpro/interpro.sql"
 REM "Reflector" --reflects -o "./uniprot/" /namespace "UniprotKB.MySQL.Tables" /split --language=visualbasic /sql "./uniprot.sql"
 REM "Reflector" --reflects -o "../src/GCModeller/data/ExternalDBSource/ChEBI/Tables/" /namespace "ChEBI.Tables" /split --language=visualbasic /sql "../src/GCModeller/data/ExternalDBSource/ChEBI/chebi.sql"
 REM "Reflector" --reflects -o "../src/GCModeller/data/ExternalDBSource/MetaCyc/MySQL/" /namespace "MetaCyc.MySQL" /split --language=visualbasic /sql "../src/GCModeller/data/ExternalDBSource/MetaCyc/bio_warehouse.sql"
