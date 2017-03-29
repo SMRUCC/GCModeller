60b204b47add9dd5eb3860dd0736cb77a2b50d1d
xieguigang <xie.guigang@gmail.com>
2017-03-29 22:01:12 +0800

    fix for nt database

--- mysql/build.bat
+++ mysql/build.bat
@@ -1,5 +1,7 @@
 REM converts the mysql dataabase schema source code to visualbasic source code
 
+"Reflector" --reflects -o "../src/repository/nt/mysql/NCBI" /namespace "mysql.NCBI" /split --language=visualbasic /sql "../src/repository/ncbi.sql"
+
 REM "Reflector" --reflects -o "./KEGG_Schema/" /namespace "mysql" /split --language=visualbasic /sql "./jp_kegg2.sql"
 REM "Reflector" --reflects -o "./ExplorEnz/" /namespace "ExplorEnz.MySQL" /split --language=visualbasic /sql "./ExplorEnz.sql"
 REM "Reflector" --reflects -o "./interpro/" /namespace "Interpro.Tables" /split --language=visualbasic /sql "./interpro.sql"
@@ -8,9 +10,9 @@ REM "Reflector" --reflects -o "../src/GCModeller/data/ExternalDBSource/ChEBI/Tab
 REM "Reflector" --reflects -o "../src/GCModeller/data/ExternalDBSource/MetaCyc/MySQL/" /namespace "MetaCyc.MySQL" /split --language=visualbasic /sql "../src/GCModeller/data/ExternalDBSource/MetaCyc/bio_warehouse.sql"
 REM "Reflector" --reflects -o "../src/GCModeller/data/GO_gene-ontology/GO_mysql/MySQL/Tables" /namespace "MySQL.Tables" /split --language=visualbasic /sql "../src/GCModeller/data/GO_gene-ontology/GO_mysql/MySQL/go.sql"
 
-"Reflector" --reflects -o "../src/GCModeller/data/Reactome/LocalMySQL/gk_current" /namespace "LocalMySQL.Tables.gk_current" /split --language=visualbasic /sql "./reactome/gk_current.sql"
-"Reflector" --reflects -o "../src/GCModeller/data/Reactome/LocalMySQL/gk_current_dn" /namespace "LocalMySQL.Tables.gk_current_dn" /split --language=visualbasic /sql "./reactome/gk_current_dn.sql"
-"Reflector" --reflects -o "../src/GCModeller/data/Reactome/LocalMySQL/gk_stable_ids" /namespace "LocalMySQL.Tables.gk_stable_ids" /split --language=visualbasic /sql "./reactome/gk_stable_ids.sql"
+REM "Reflector" --reflects -o "../src/GCModeller/data/Reactome/LocalMySQL/gk_current" /namespace "LocalMySQL.Tables.gk_current" /split --language=visualbasic /sql "./reactome/gk_current.sql"
+REM "Reflector" --reflects -o "../src/GCModeller/data/Reactome/LocalMySQL/gk_current_dn" /namespace "LocalMySQL.Tables.gk_current_dn" /split --language=visualbasic /sql "./reactome/gk_current_dn.sql"
+REM "Reflector" --reflects -o "../src/GCModeller/data/Reactome/LocalMySQL/gk_stable_ids" /namespace "LocalMySQL.Tables.gk_stable_ids" /split --language=visualbasic /sql "./reactome/gk_stable_ids.sql"
 
 REM copy ORM source code to project source code directory
 
