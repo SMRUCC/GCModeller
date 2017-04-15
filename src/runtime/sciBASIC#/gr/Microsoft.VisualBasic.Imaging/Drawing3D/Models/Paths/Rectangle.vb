dea2f224c8cadbc98e56bc5c72383732882edbfa
xieguigang <xie.guigang@gmail.com>
2017-04-15 20:43:33 +0800

    update 3d graphics engine

--- src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/Drawing3D/Models/Paths/Rectangle.vb
+++ src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/Drawing3D/Models/Paths/Rectangle.vb
@@ -1,17 +1,15 @@
 Imports Microsoft.VisualBasic.Imaging.Drawing3D
-Imports Microsoft.VisualBasic.Imaging.Drawing3D.IsoMetric
+Imports Microsoft.VisualBasic.Imaging.Drawing3D.Isometric
 
-Namespace Drawing3D.IsoMetric.Paths
-
+Namespace Drawing3D.Isometric.Paths
 
     ''' <summary>
     ''' Created by fabianterhorst on 01.04.17.
     ''' </summary>
-
     Public Class Rectangle
         Inherits Path3D
 
-        Public Sub New(ByVal origin As Point3D, ByVal width As Integer, ByVal height As Integer)
+        Public Sub New(origin As Point3D, width As Integer, height As Integer)
             MyBase.New()
             Push(origin)
             Push(New Point3D(origin.X + width, origin.Y, origin.Z))
@@ -19,5 +17,4 @@ Namespace Drawing3D.IsoMetric.Paths
             Push(New Point3D(origin.X, origin.Y + height, origin.Z))
         End Sub
     End Class
-
 End Namespace
