a4b4100245bdf9480f25da2c7d6d9e06a86b774f
xieguigang <xie.guigang@gmail.com>
2017-05-06 14:18:00 +0800

    CLI interface for blastn visualize

--- src/GCModeller/CLI_tools/Map/CLI/BlastAlignment.vb
+++ src/GCModeller/CLI_tools/Map/CLI/BlastAlignment.vb
@@ -11,6 +11,11 @@ Imports SMRUCC.genomics.Visualize.NCBIBlastResult
 
 Partial Module CLI
 
+    ''' <summary>
+    ''' 这个函数是从编译好的blast bbh xml结果之中绘图
+    ''' </summary>
+    ''' <param name="args"></param>
+    ''' <returns></returns>
     <ExportAPI("/Visual.BBH",
                Usage:="/Visual.BBH /in <bbh.Xml> /PTT <genome.PTT> /density <genomes.density.DIR> [/limits <sp-list.txt> /out <image.png>]")>
     <Argument("/PTT", False,
@@ -28,8 +33,7 @@ Partial Module CLI
             meta = meta.Take(limits)
         End If
 
-        Dim scores As Func(Of Analysis.Hit, Double) =
-            BBHMetaAPI.DensityScore(density, scale:=20)
+        Dim scores As Func(Of Analysis.Hit, Double) = BBHMetaAPI.DensityScore(density, scale:=20)
         Dim PTTdb As PTT = TabularFormat.PTT.Load(PTT)
         Dim table As AlignmentTable =
             BBHMetaAPI.DataParser(meta,
@@ -46,5 +50,21 @@ Partial Module CLI
                                                         IdentityNoColor:=False,
                                                         queryBrush:=densityQuery)
         Return res.SaveAs(out, ImageFormats.Png).CLICode
+    End Function
+
+    <ExportAPI("/Visualize.blastn.alignment",
+               Usage:="/Visualize.blastn.alignment /in <alignmentTable.txt> /PTT <genome.PTT> [/local /out <image.png>]",
+               Info:="Blastn result alignment visualization from the NCBI web blast.")>
+    <Argument("/local", Description:="The file for ``/in`` parameter is a local blastn output result file?")>
+    Public Function BlastnVisualizeWebResult(args As CommandLine) As Integer
+        Dim in$ = args <= "/in"
+        Dim PTT$ = args <= "/PTT"
+        Dim out$ = args.GetValue("/out", [in].TrimSuffix & ".blastn.visualize.png")
+        Dim local As Boolean = args.GetBoolean("/local")
+
+        If local Then
+        Else
+
+        End If
     End Function
 End Module
