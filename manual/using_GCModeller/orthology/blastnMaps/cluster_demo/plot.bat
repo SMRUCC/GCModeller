09d3998d8134a9d5b70d8044933560efbf3a577b
xieguigang <xie.guigang@gmail.com>
2017-05-07 23:41:23 +0800

    add cog data

--- manual/using_GCModeller/orthology/blastnMaps/cluster_demo/plot.bat
+++ manual/using_GCModeller/orthology/blastnMaps/cluster_demo/plot.bat
@@ -1,1 +1,3 @@
-mapplot /Visualize.blastn.alignment /in "./GYCCGKYD014-Alignment.txt" /genbank "G:\Xanthomonas_campestris_8004_uid15\genbank\CP000050.1.txt" /ORF.catagory "./Xcc8004.myva_COG.txt" /local /region "1418501,1465305"
+REM mapplot /Visualize.blastn.alignment /in "./GYCCGKYD014-Alignment.txt" /genbank "G:\Xanthomonas_campestris_8004_uid15\genbank\CP000050.1.txt" /ORF.catagory "./Xcc8004.myva.myva_COG.txt" /local /region "1418501,1465305"
+
+mapplot /Visualize.blastn.alignment /in "./blastn.tab" /genbank "G:\Xanthomonas_campestris_8004_uid15\genbank\CP000050.1.txt" /ORF.catagory "./Xcc8004.myva.myva_COG.txt" /region "1418501,1465305"
