9eba294cf20666195d3bb69695313122c7b07865
xieguigang <xie.guigang@gmail.com>
2017-05-07 23:01:02 +0800

    have bugs on the region selection

--- manual/using_GCModeller/orthology/blastnMaps/cluster_demo/plot.bat
+++ manual/using_GCModeller/orthology/blastnMaps/cluster_demo/plot.bat
@@ -1,1 +1,1 @@
-mapplot /Visualize.blastn.alignment /in "./GY5YZG8D015-Alignment.txt" /genbank "G:\Xanthomonas_campestris_8004_uid15\genbank\CP000050.1.txt" /ORF.catagory "./Xcc8004.myva_COG.txt" /local
+mapplot /Visualize.blastn.alignment /in "./GYCCGKYD014-Alignment.txt" /genbank "G:\Xanthomonas_campestris_8004_uid15\genbank\CP000050.1.txt" /ORF.catagory "./Xcc8004.myva_COG.txt" /local
