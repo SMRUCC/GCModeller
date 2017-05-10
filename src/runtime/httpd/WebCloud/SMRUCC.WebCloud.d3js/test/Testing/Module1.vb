c77077e49fa3d9fef85008924225d1185ddf4751
xieguigang <xie.guigang@gmail.com>
2017-05-10 21:40:06 +0800

    set cookies??

---
+++ src/runtime/httpd/WebCloud/SMRUCC.WebCloud.d3js/test/Testing/Module1.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
+
+Module Module1
+
+    Sub Main()
+        Dim json = SMRUCC.WebCloud.d3js.Network.htmlwidget.BuildData.BuildGraph("D:\GCModeller\src\runtime\httpd\WebCloud\SMRUCC.WebCloud.d3js\test\viewer.html")
+        Dim graph = json.CreateGraph
+        Call graph.doForceLayout
+        Call graph.Tabular.Save("./test")
+    End Sub
+End Module
