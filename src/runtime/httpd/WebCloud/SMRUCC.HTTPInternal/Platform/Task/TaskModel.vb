311c652775c388882b7a247c0cea12553185b422
xieguigang <xie.guigang@gmail.com>
2017-05-11 22:19:42 +0800

    graphics API improvements

--- src/runtime/httpd/WebCloud/SMRUCC.HTTPInternal/Platform/Task/TaskModel.vb
+++ src/runtime/httpd/WebCloud/SMRUCC.HTTPInternal/Platform/Task/TaskModel.vb
@@ -6,6 +6,12 @@
 
         Protected MustOverride Function contents() As String()
 
+        ''' <summary>
+        ''' Public interface for invoke this task
+        ''' </summary>
+        ''' <returns></returns>
+        Public MustOverride Function GetTask() As Action
+
         Public Function GetProgress() As TaskProgress
             Return New TaskProgress With {
                 .current = current,
