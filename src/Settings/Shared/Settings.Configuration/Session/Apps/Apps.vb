c205f75a67f0c3ed269132e967e4b0a2aa68b144
xieguigang <xie.guigang@gmail.com>
2017-05-13 21:06:04 +0800

    app path search test success

--- src/Settings/Shared/Settings.Configuration/Session/Apps/Apps.vb
+++ src/Settings/Shared/Settings.Configuration/Session/Apps/Apps.vb
@@ -9,7 +9,7 @@ Public NotInheritable Class Apps
 
     Public Shared ReadOnly Property NCBI_tools As GCModellerApps.NCBI_tools
     Public Shared ReadOnly Property localblast As GCModellerApps.localblast
-    Public Shared ReadOnly Property MEME As GCModellerApps.meme
+    Public Shared ReadOnly Property MEME As GCModellerApps.MEME
     Public Shared ReadOnly Property KEGG_tools As String
     Public Shared ReadOnly Property seqtools As GCModellerApps.seqtools
     Public Shared ReadOnly Property VennDiagram As GCModellerApps.VennDiagram
@@ -33,7 +33,7 @@ Public NotInheritable Class Apps
                 eggHTS = New GCModellerApps.eggHTS(HOME & "/eggHTS.exe")
                 NCBI_tools = New GCModellerApps.NCBI_tools(HOME & "/NCBI_tools.exe")
                 localblast = New GCModellerApps.localblast(HOME & "/localblast.exe")
-                MEME = New GCModellerApps.meme(HOME & "/MEME.exe")
+                MEME = New GCModellerApps.MEME(HOME & "/MEME.exe")
                 KEGG_tools = HOME & "/KEGG_tools.exe"
                 seqtools = New GCModellerApps.seqtools(HOME & $"/{NameOf(seqtools)}.exe")
                 VennDiagram = New GCModellerApps.VennDiagram(HOME & "/venn.exe")
