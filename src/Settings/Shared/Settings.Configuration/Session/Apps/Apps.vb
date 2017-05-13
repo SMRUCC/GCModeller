ff239bc4b67bfaaaa08bc1a91c7aa2c6ea5c1e6b
xieguigang <xie.guigang@gmail.com>
2017-05-13 21:01:26 +0800

    search App path

--- src/Settings/Shared/Settings.Configuration/Session/Apps/Apps.vb
+++ src/Settings/Shared/Settings.Configuration/Session/Apps/Apps.vb
@@ -1,4 +1,8 @@
-﻿''' <summary>
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.CommandLine.InteropService
+Imports Microsoft.VisualBasic.Linq
+
+''' <summary>
 ''' A list of GCModeller apps 
 ''' </summary>
 Public NotInheritable Class Apps
@@ -12,12 +16,54 @@ Public NotInheritable Class Apps
     Public Shared ReadOnly Property eggHTS As GCModellerApps.eggHTS
 
     Shared Sub New()
-        eggHTS = New GCModellerApps.eggHTS(App.HOME & "/eggHTS.exe")
-        NCBI_tools = New GCModellerApps.NCBI_tools(App.HOME & "/NCBI_tools.exe")
-        localblast = New GCModellerApps.localblast(App.HOME & "/localblast.exe")
-        MEME = New GCModellerApps.meme(App.HOME & "/MEME.exe")
-        KEGG_tools = App.HOME & "/KEGG_tools.exe"
-        seqtools = New GCModellerApps.seqtools(App.HOME & $"/{NameOf(seqtools)}.exe")
-        VennDiagram = New GCModellerApps.VennDiagram(App.HOME & "/venn.exe")
+        Dim list$() = {
+            App.HOME,
+            App.HOME & "/GCModeller/",
+            App.HOME & "/Apps/",
+            "C:\GCModeller\",
+            "C:\Program Files\GCModeller\",
+            "D:\GCModeller\",
+            "E:\GCModeller\",
+            "F:\GCModeller\",
+            "G:\GCModeller\"
+        } ' 枚举一些比较常用的路径
+
+        For Each HOME As String In list
+            If Apps.IsAppHome(HOME) Then
+                eggHTS = New GCModellerApps.eggHTS(HOME & "/eggHTS.exe")
+                NCBI_tools = New GCModellerApps.NCBI_tools(HOME & "/NCBI_tools.exe")
+                localblast = New GCModellerApps.localblast(HOME & "/localblast.exe")
+                MEME = New GCModellerApps.meme(HOME & "/MEME.exe")
+                KEGG_tools = HOME & "/KEGG_tools.exe"
+                seqtools = New GCModellerApps.seqtools(HOME & $"/{NameOf(seqtools)}.exe")
+                VennDiagram = New GCModellerApps.VennDiagram(HOME & "/venn.exe")
+
+                Exit For
+            End If
+        Next
     End Sub
+
+    ''' <summary>
+    ''' 目标文件夹是不是GCModeller Apps的应用程序组文件夹？
+    ''' </summary>
+    ''' <param name="DIR$"></param>
+    ''' <returns></returns>
+    Private Shared Function IsAppHome(DIR$) As Boolean
+        Dim Apps$() = GetType(Apps) _
+            .GetProperties(BindingFlags.Public Or BindingFlags.Static) _
+            .Where(Function(t) t.PropertyType.IsInheritsFrom(GetType(InteropService))) _
+            .Select(Function(t) t.PropertyType.GetMember("App")) _
+            .IteratesALL _
+            .Select(Function(m) DirectCast(m, FieldInfo).GetValue(Nothing)) _
+            .Select(Function(o) DirectCast(o, String)) _
+            .ToArray
+
+        For Each App$ In Apps.Select(Function(path$) DIR & "/" & path)
+            If App.FileExists(True) Then
+                Return True
+            End If
+        Next
+
+        Return False
+    End Function
 End Class
