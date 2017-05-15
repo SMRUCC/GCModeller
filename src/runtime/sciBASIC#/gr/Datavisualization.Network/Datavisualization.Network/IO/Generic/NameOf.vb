7d534ee4bc3aeda698bf1fcf33fa33da7b5ac366
谢桂纲 <xie.guigang@gcmodeller.org>
2017-05-15 11:53:13 +0800

    try improvements on the names references

--- src/runtime/sciBASIC#/gr/Datavisualization.Network/Datavisualization.Network/IO/Generic/NameOf.vb
+++ src/runtime/sciBASIC#/gr/Datavisualization.Network/Datavisualization.Network/IO/Generic/NameOf.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::d2699ac12a1c319767d9cd649ba94852, ..\sciBASIC#\gr\Datavisualization.Network\Datavisualization.Network\LDM\FileStream\NameOf.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -58,5 +58,6 @@ Namespace FileStream.Generic
         ''' <see cref="Node.NodeType"/>
         ''' </summary>
         Public Const REFLECTION_ID_MAPPING_NODETYPE As String = "nodeType"
+        Public Const REFLECTION_ID_MAPPING_DEGREE$ = "degree"
     End Module
 End Namespace
