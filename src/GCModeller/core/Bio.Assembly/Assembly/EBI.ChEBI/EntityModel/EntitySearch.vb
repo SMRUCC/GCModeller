6a1ffd58d2514711fcbe255ca37bb5da5838fa1e
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-02 13:51:47 +0800

    found by names

--- src/GCModeller/core/Bio.Assembly/Assembly/EBI.ChEBI/EntityModel/EntitySearch.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/EBI.ChEBI/EntityModel/EntitySearch.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Text.Levenshtein
 
 Namespace Assembly.EBI.ChEBI
@@ -30,14 +31,44 @@ Namespace Assembly.EBI.ChEBI
 
             Dim LQuery = From metabolite As ChEBIEntity
                          In source
-                         Where Not metabolite.Formulae Is Nothing AndAlso
-                             metabolite.Formulae.data.TextEquals(formula)
+                         Where (Not metabolite.Formulae Is Nothing AndAlso
+                             metabolite.Formulae.data.TextEquals(formula))
                          Let match = nameString Like LCase(metabolite.chebiAsciiName)
                          Where Not match Is Nothing AndAlso
                              match.MatchSimilarity >= 0.75
                          Select metabolite
 
             Return LQuery
+        End Function
+
+        <Extension>
+        Public Function SearchByNameEqualsAny(chebi As Dictionary(Of Long, ChEBIEntity), name$) As ChEBIEntity
+            For Each metabolite As ChEBIEntity In chebi.Values
+                If metabolite.NameEqualsAny(name) Then
+                    Return metabolite
+                End If
+            Next
+
+            Return Nothing
+        End Function
+
+        <Extension> Public Function NameEqualsAny(chebi As ChEBIEntity, name$) As Boolean
+            If chebi.chebiAsciiName.TextEquals(name) Then
+                Return True
+            Else
+                For Each s As Synonyms In chebi.Synonyms.SafeQuery
+                    If s.data.TextEquals(name) Then
+                        Return True
+                    End If
+                Next
+                For Each s As Synonyms In chebi.IupacNames.SafeQuery
+                    If s.data.TextEquals(name) Then
+                        Return True
+                    End If
+                Next
+            End If
+
+            Return False
         End Function
     End Module
 End Namespace
