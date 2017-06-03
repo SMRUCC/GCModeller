431ecb306d18840fd701463aaca0460e0ecd9544
xieguigang <xie.guigang@gmail.com>
2017-06-03 09:53:08 +0800

    update kegg mysql

---
+++ src/GCModeller/data/KEGG/Procedures/DumpUniprot.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module DumpUniprot
+
+    ''' <summary>
+    ''' 因为KEGG没有对外的批量数据下载接口，所以在这里使用uniprot的下载数据导入为KEGG之中的基因的数据
+    ''' </summary>
+    ''' <param name="XML$"></param>
+    ''' <returns></returns>
+    <Extension> Public Function ImportsUniprotProteins(XML$)
+
+    End Function
+End Module
