5c09d6ec2697636aee8d68aeae81cbd32352fe23
xieguigang <xie.guigang@gmail.com>
2017-06-03 13:25:23 +0800

    add otu table reader

---
+++ src/GCModeller/CLI_tools/meta-assmebly/CLI/02_Alpha_Diversity.vb
@@ -0,0 +1,16 @@
+﻿Imports System.ComponentModel
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Partial Module CLI
+
+    <ExportAPI("/Rank_Abundance")>
+    <Description("https://en.wikipedia.org/wiki/Rank_abundance_curve")>
+    <Usage("/Rank_Abundance /in <OTU.table.csv> [/schema <color schema, default=Rainbow> /out <out.DIR>]")>
+    <Group(CLIGroups.Alpha_Diversity)>
+    Public Function Rank_Abundance(args As CommandLine) As Integer
+        Dim in$ = args <= "/in"
+        Dim schema$ = args.GetValue("/schema", "Rainbow")
+        Dim out$ = args.GetValue("/out", [in].TrimSuffix & ".Rank_Abundance/")
+    End Function
+End Module
