19dea9bbef19bc306bc03ee7a3c8255a554f32b6
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-14 16:34:06 +0800

    add network module references

--- src/GCModeller/models/Networks/STRING/FunctionalEnrichmentPlot.vb
+++ src/GCModeller/models/Networks/STRING/FunctionalEnrichmentPlot.vb
@@ -1,3 +1,12 @@
-﻿Public Module FunctionalEnrichmentPlot
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.Assembly.Uniprot.XML
+Imports SMRUCC.genomics.Data.STRING
+Imports NetGraph = Microsoft.VisualBasic.Data.visualize.Network.FileStream.Network
 
+Public Module FunctionalEnrichmentPlot
+
+    <Extension>
+    Public Function BuildModel(interacts As IEnumerable(Of InteractExports), uniprot As UniprotXML) As NetGraph
+
+    End Function
 End Module
