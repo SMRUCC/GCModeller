a5d2ae788e0f500e3905f45967a2800b1c772c05
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-05 21:41:17 +0800

    try add new KEGG network graph API

---
+++ src/GCModeller/models/Networks/Network.Regulons/KEGGMap/PathwayMapNetwork.vb
@@ -0,0 +1,8 @@
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+
+Public Module PathwayMapNetwork
+
+    Public Function BuildModel(br08901$) As NetworkTables
+
+    End Function
+End Module
