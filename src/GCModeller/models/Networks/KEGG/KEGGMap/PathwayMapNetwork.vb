e317862d3d1aaf4d96885bb1d1f5e5a187f6dc95
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-12 16:05:40 +0800

    add KEGG compounds network

--- src/GCModeller/models/Networks/KEGG/KEGGMap/PathwayMapNetwork.vb
+++ src/GCModeller/models/Networks/KEGG/KEGGMap/PathwayMapNetwork.vb
@@ -12,6 +12,11 @@ Public Module PathwayMapNetwork
 
     Const delimiter$ = "|"
 
+    ''' <summary>
+    ''' 这个函数所产生的模型是以代谢途径为主体对象的
+    ''' </summary>
+    ''' <param name="br08901$"></param>
+    ''' <returns></returns>
     Public Function BuildModel(br08901$) As NetworkTables
         Dim nodes As New List(Of Node)
 
@@ -70,5 +75,70 @@ Public Module PathwayMapNetwork
         edges = edges(Which.IsTrue(ranks >= 3))
 
         Return New NetworkTables(edges, nodes)
+    End Function
+
+    ''' <summary>
+    ''' 这个网络模式是以代谢物为主题的
+    ''' </summary>
+    ''' <param name="br08901$"></param>
+    ''' <returns></returns>
+    Public Function BuildCompoundsNetwork(br08901$) As NetworkTables
+        Dim nodes As New Dictionary(Of Node)
+
+        For Each Xml As String In ls - l - r - "*.XML" <= br08901
+            Dim pathwayMap As PathwayMap = Xml.LoadXml(Of PathwayMap)
+
+            Call pathwayMap _
+                .KEGGCompound _
+                .SafeQuery _
+                .DoEach(Sub(cpd)
+                            If nodes.ContainsKey(cpd.Key) Then
+                                nodes(cpd.Key).NodeType &= "|" & pathwayMap.EntryId
+                            Else
+                                nodes += New Node With {
+                                    .ID = cpd.Key,
+                                    .NodeType = pathwayMap.EntryId,
+                                    .Properties = New Dictionary(Of String, String) From {
+                                        {"name", cpd.Value}
+                                    }
+                                }
+                            End If
+                        End Sub)
+        Next
+
+        Dim edges As New Dictionary(Of String, NetworkEdge)
+        Dim common As New Value(Of String())
+
+        For Each a As Node In nodes.Values
+            Dim pathways$() = a.NodeType _
+                .Split("|"c) _
+                .Where(Function(s) Not s.StringEmpty) _
+                .Distinct _
+                .ToArray
+
+            For Each b As Node In nodes.Values.Where(Function(x) x.ID <> a.ID)
+                Dim typeB$() = a.NodeType _
+                    .Split("|"c) _
+                    .Where(Function(s) Not s.StringEmpty) _
+                    .Distinct _
+                    .ToArray
+                Dim edge As New NetworkEdge With {
+                    .FromNode = a.ID,
+                    .ToNode = b.ID
+                }
+
+                If Not (common = pathways.Intersect(typeB).ToArray).IsNullOrEmpty Then
+                    With edge.GetNullDirectedGuid(True)
+                        If Not edges.ContainsKey(.ref) Then
+                            edges(.ref) = edge
+                            edge.value = common.value.Length
+                            edge.Interaction = common.value.JoinBy("|")
+                        End If
+                    End With
+                End If
+            Next
+        Next
+
+        Return New NetworkTables(nodes.Values, edges.Values)
     End Function
 End Module
