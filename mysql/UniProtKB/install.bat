c1f4aeeea3cb2b657b07328484a9d53a0d76b63e
xieguigang <xie.guigang@gcmodeller.org>
2017-09-01 00:07:21 +0800

    update install script

--- mysql/UniProtKB/install.bat
+++ mysql/UniProtKB/install.bat
@@ -1,2 +1,7 @@
-reflector --reflects /sql ./kb_go.sql /namespace "kb_go" /split /out "../../src/GCModeller/data/GO_gene-ontology/GO_mysql/kb_go"
- 
+@echo off
+
+REM install mysql to vb.net source code into the data project
+
+reflector --reflects /sql ./kb_go.sql /namespace "kb_go" /split
+xcopy "./kb_go/*.*" "../../src/GCModeller/data/GO_gene-ontology/GO_mysql/kb_go" /s /h /d /y /e /f /i
+RD /S /Q "./kb_go/"
