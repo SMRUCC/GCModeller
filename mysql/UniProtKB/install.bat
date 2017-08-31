536b598b0b0fbff8dfbce86096e3d5c3b9f143e5
xieguigang <xie.guigang@gcmodeller.org>
2017-09-01 00:03:31 +0800

    replace with new db definition

---
+++ mysql/UniProtKB/install.bat
@@ -0,0 +1,2 @@
+reflector --reflects /sql ./kb_go.sql /namespace "kb_go" /split /out "../../src/GCModeller/data/GO_gene-ontology/GO_mysql/kb_go"
+ 
---
+++ src/GCModeller/data/GO_gene-ontology/GO_mysql/DATA.vb
@@ -0,0 +1,3 @@
+﻿Public Module DATA
+
+End Module
