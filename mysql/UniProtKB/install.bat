5b827f43194ae49354965126d59a0a94c63aa632
xieguigang <xie.guigang@gcmodeller.org>
2017-09-02 10:16:51 +0800

    fix auto incremnent ID

--- mysql/UniProtKB/install.bat
+++ mysql/UniProtKB/install.bat
@@ -2,6 +2,6 @@
 
 REM install mysql to vb.net source code into the data project
 
-reflector --reflects /sql ./kb_go.sql /namespace "kb_go" /split
+reflector --reflects /sql ./kb_go.sql /namespace "kb_go" /split /auto_increment.disable
 xcopy "./kb_go/*.*" "../../src/GCModeller/data/GO_gene-ontology/GO_mysql/kb_go" /s /h /d /y /e /f /i
 RD /S /Q "./kb_go/"
