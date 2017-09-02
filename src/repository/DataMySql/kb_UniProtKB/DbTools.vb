e5ad15c4331c3f6187d7995cc89b68a30d27e028
xieguigang <xie.guigang@gcmodeller.org>
2017-09-03 03:11:20 +0800

    add mysql imports interface

--- src/repository/DataMySql/kb_UniProtKB/DbTools.vb
+++ src/repository/DataMySql/kb_UniProtKB/DbTools.vb
@@ -26,80 +26,16 @@
 
 '#End Region
 
-'Imports System.Data.SQLite.Linq
-'Imports System.Data.SQLite.Linq.Reflector
-'Imports Microsoft.VisualBasic
-'Imports SMRUCC.genomics.BioAssemblyExtensions
-'Imports SMRUCC.genomics.Data
-'Imports SMRUCC.genomics.Data.Model_Repository.DbFileSystemObject
-'Imports SMRUCC.genomics.Interops.NCBI.Extensions
+Imports Oracle.LinuxCompatibility.MySQL
+Imports SMRUCC.genomics.Assembly.Uniprot.XML
 
-'Namespace UniprotSprot
+Namespace kb_UniProtKB
 
-'    Public Class DbTools : Inherits AnnotationTool
+    Public Module MySqlImports
 
-'        Sub New(LocalBlast As LocalBLAST.InteropService.InteropService)
-'            Call MyBase.New(LocalBlast, DB:=Settings.Session.Initialize.RepositoryRoot & "/Uniprot/")
-'        End Sub
+        <extension>
+        Public Function ImportsUniProtKB(uniprot As IEnumerable(Of entry)) As Dictionary(Of String, SQLTable())
 
-'        ''' <summary>
-'        ''' 
-'        ''' </summary>
-'        ''' <param name="UniprotSprot"></param>
-'        ''' <param name="InstalledRoot">数据库将要安装的路径</param>
-'        ''' <returns></returns>
-'        ''' <remarks></remarks>
-'        Public Shared Function InstallDatabase(UniprotSprot As String, InstalledRoot As String) As Model_Repository.Uniprot()
-'            Dim UniprotData = (From Fasta In SMRUCC.genomics.Assembly.Uniprot.UniprotFasta.LoadFasta(UniprotSprot) Select Fasta Group By Fasta.OrgnsmSpName Into Group).ToArray
-'            Dim ChunkBuffer As New List(Of Model_Repository.Uniprot)
-
-'            Call FileIO.FileSystem.CreateDirectory(InstalledRoot)
-
-'            Dim Engine = Model_Repository.RepositoryEngine.SQLiteEngine
-'            Dim TableSchema As New TableSchema(type:=GetType(Model_Repository.Uniprot))
-
-'            If Engine.ExistsTable(TableSchema.TableName) Then
-'                Call Engine.DeleteTable(TableSchema.TableName)
-'            End If
-'            Call Engine.CreateTableFor(Of Model_Repository.Uniprot)()
-
-'            Dim LQuery = (From GroupedItem In UniprotData.AsParallel
-'                          Let Path As String = Function() As String
-'                                                   If Not String.IsNullOrEmpty(GroupedItem.OrgnsmSpName) Then
-'                                                       Return InstalledRoot & "/" & GroupedItem.OrgnsmSpName.First & "/" & GroupedItem.OrgnsmSpName.NormalizePathString() & ".fasta"
-'                                                   Else
-'                                                       Return InstalledRoot & "/UnknownSpecies.fasta"
-'                                                   End If
-'                                               End Function()
-'                          Let GroupedUniprot = GroupedItem.Group.ToArray
-'                          Let FastaData As SMRUCC.genomics.SequenceModel.FASTA.FastaToken() = (From item As SMRUCC.genomics.Assembly.Uniprot.UniprotFasta
-'                                                                                      In GroupedUniprot
-'                                                                                               Select New SMRUCC.genomics.SequenceModel.FASTA.FastaToken With
-'                                                                                                          {
-'                                                                                                              .Attributes = New String() {item.UniprotID},
-'                                                                                                              .SequenceData = item.SequenceData
-'                                                                                                          }).ToArray Select GroupedUniprot, Path, FastaData).ToArray
-
-'            For Each Source In LQuery
-'                Call CType(Source.FastaData, SMRUCC.genomics.SequenceModel.FASTA.FastaFile).Save(Source.Path)
-'                Dim Entries = (From item As SMRUCC.genomics.Assembly.Uniprot.UniprotFasta
-'                               In Source.GroupedUniprot
-'                               Select Model_Repository.Uniprot.CreateObject(item, SecurityString.GetFileHashString(Source.Path))).ToArray
-'                Call ChunkBuffer.AddRange(Entries)
-
-'                For Each Entry As Model_Repository.Uniprot In Entries
-'                    Call Engine.Insert(TableSchema, Entry)
-'                Next
-'                Call Console.WriteLine("[DEBUG] {0} data done!", Source.Path.ToFileURL)
-'            Next
-
-'            Call Console.WriteLine("[DEBUG] {0} entry was writers into the repository database.", ChunkBuffer.Count)
-
-'            Return ChunkBuffer.ToArray
-'        End Function
-
-'        Protected Overrides Function GetAnnotationSourceMeta() As ProteinDescriptionModel()
-'            Throw New NotImplementedException
-'        End Function
-'    End Class
-'End Namespace
+        End Function
+    End Module
+End Namespace
