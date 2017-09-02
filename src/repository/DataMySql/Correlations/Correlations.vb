718f33d7110c7064d92a52e6f1cd6236af6bdb56
xieguigang <xie.guigang@gcmodeller.org>
2017-09-03 03:06:09 +0800

    fix project reference

--- src/repository/DataMySql/Correlations/Correlations.vb
+++ src/repository/DataMySql/Correlations/Correlations.vb
@@ -1,141 +1,141 @@
-﻿#Region "Microsoft.VisualBasic::c20ab38b01b9a42b04d59915eab7344f, ..\GCModeller\analysis\annoTools\DataMySql\Correlations\Correlations.vb"
+﻿'#Region "Microsoft.VisualBasic::c20ab38b01b9a42b04d59915eab7344f, ..\GCModeller\analysis\annoTools\DataMySql\Correlations\Correlations.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+'    ' Author:
+'    ' 
+'    '       asuka (amethyst.asuka@gcmodeller.org)
+'    '       xieguigang (xie.guigang@live.com)
+'    '       xie (genetics@smrucc.org)
+'    ' 
+'    ' Copyright (c) 2016 GPL3 Licensed
+'    ' 
+'    ' 
+'    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+'    ' 
+'    ' This program is free software: you can redistribute it and/or modify
+'    ' it under the terms of the GNU General Public License as published by
+'    ' the Free Software Foundation, either version 3 of the License, or
+'    ' (at your option) any later version.
+'    ' 
+'    ' This program is distributed in the hope that it will be useful,
+'    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    ' GNU General Public License for more details.
+'    ' 
+'    ' You should have received a copy of the GNU General Public License
+'    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#End Region
+'#End Region
 
-Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports Microsoft.VisualBasic.Linq.Extensions
-Imports Microsoft.VisualBasic.Scripting.MetaData
-Imports SMRUCC.genomics.Analysis
-Imports SMRUCC.genomics.Analysis.RNA_Seq
-Imports SMRUCC.genomics.Analysis.RNA_Seq.RTools
-Imports SMRUCC.genomics.Analysis.RNA_Seq.RTools.WGCNA
-Imports SMRUCC.genomics.Data.Model_Repository.MySQL
-Imports SMRUCC.genomics.Data.Model_Repository.MySQL.Tables
+'Imports Microsoft.VisualBasic.CommandLine.Reflection
+'Imports Microsoft.VisualBasic.Linq.Extensions
+'Imports Microsoft.VisualBasic.Scripting.MetaData
+'Imports SMRUCC.genomics.Analysis
+'Imports SMRUCC.genomics.Analysis.RNA_Seq
+'Imports SMRUCC.genomics.Analysis.RNA_Seq.RTools
+'Imports SMRUCC.genomics.Analysis.RNA_Seq.RTools.WGCNA
+'Imports SMRUCC.genomics.Data.Model_Repository.MySQL
+'Imports SMRUCC.genomics.Data.Model_Repository.MySQL.Tables
 
-''' <summary>
-''' 基因表达相关性的数据库服务
-''' </summary>
-''' 
-Public Class Correlations
+'''' <summary>
+'''' 基因表达相关性的数据库服务
+'''' </summary>
+'''' 
+'Public Class Correlations
 
-    Public ReadOnly Property MySQL As Oracle.LinuxCompatibility.MySQL.MySQL
+'    Public ReadOnly Property MySQL As Oracle.LinuxCompatibility.MySQL.MySQL
 
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <param name="uri">一般情况下这个参数为空，程序会自动根据配置文件来查找数据源</param>
-    Sub New(Optional uri As Oracle.LinuxCompatibility.MySQL.ConnectionUri = Nothing)
-        MySQL = MySQLExtensions.GetMySQLClient(uri, "correlations")
-        MySQL.UriMySQL.TimeOut = 60 * 600
-    End Sub
+'    ''' <summary>
+'    ''' 
+'    ''' </summary>
+'    ''' <param name="uri">一般情况下这个参数为空，程序会自动根据配置文件来查找数据源</param>
+'    Sub New(Optional uri As Oracle.LinuxCompatibility.MySQL.ConnectionUri = Nothing)
+'        MySQL = MySQLExtensions.GetMySQLClient(uri, "correlations")
+'        MySQL.UriMySQL.TimeOut = 60 * 600
+'    End Sub
 
-    Public Overrides Function ToString() As String
-        Return MySQL.ToString
-    End Function
+'    Public Overrides Function ToString() As String
+'        Return MySQL.ToString
+'    End Function
 
-    ''' <summary>
-    ''' 无方向性的
-    ''' </summary>
-    ''' <param name="id1"></param>
-    ''' <param name="id2"></param>
-    ''' <returns></returns>
-    Public Function GetPcc(id1 As String, id2 As String) As Double
-        Dim correlation As xcb = GetCorrelation(id1, id2)
-        If correlation Is Nothing Then
-            Return 0
-        Else
-            Return correlation.pcc
-        End If
-    End Function
+'    ''' <summary>
+'    ''' 无方向性的
+'    ''' </summary>
+'    ''' <param name="id1"></param>
+'    ''' <param name="id2"></param>
+'    ''' <returns></returns>
+'    Public Function GetPcc(id1 As String, id2 As String) As Double
+'        Dim correlation As xcb = GetCorrelation(id1, id2)
+'        If correlation Is Nothing Then
+'            Return 0
+'        Else
+'            Return correlation.pcc
+'        End If
+'    End Function
 
-    Public Function GetCorrelation(id1 As String, id2 As String) As xcb
-        Return MySQL.ExecuteScalarAuto(Of xcb)($"(`g1_entity`='{id1}' and `g2_entity`='{id2}') or (`g1_entity`='{id2}' and `g2_entity`='{id1}')")
-    End Function
+'    Public Function GetCorrelation(id1 As String, id2 As String) As xcb
+'        Return MySQL.ExecuteScalarAuto(Of xcb)($"(`g1_entity`='{id1}' and `g2_entity`='{id2}') or (`g1_entity`='{id2}' and `g2_entity`='{id1}')")
+'    End Function
 
-    ''' <summary>
-    ''' 无方向性的
-    ''' </summary>
-    ''' <param name="id1"></param>
-    ''' <param name="id2"></param>
-    ''' <returns></returns>
-    Public Function GetSPcc(id1 As String, id2 As String) As Double
-        Dim correlation As xcb = GetCorrelation(id1, id2)
-        If correlation Is Nothing Then
-            Return 0
-        Else
-            Return correlation.spcc
-        End If
-    End Function
+'    ''' <summary>
+'    ''' 无方向性的
+'    ''' </summary>
+'    ''' <param name="id1"></param>
+'    ''' <param name="id2"></param>
+'    ''' <returns></returns>
+'    Public Function GetSPcc(id1 As String, id2 As String) As Double
+'        Dim correlation As xcb = GetCorrelation(id1, id2)
+'        If correlation Is Nothing Then
+'            Return 0
+'        Else
+'            Return correlation.spcc
+'        End If
+'    End Function
 
-    ''' <summary>
-    ''' 无方向性的
-    ''' </summary>
-    ''' <param name="id1"></param>
-    ''' <param name="id2"></param>
-    ''' <returns></returns>
-    Public Function GetWGCNAWeight(id1 As String, id2 As String) As Double
-        Dim correlation As xcb = GetCorrelation(id1, id2)
-        If correlation Is Nothing Then
-            Return 0
-        Else
-            Return correlation.wgcna_weight
-        End If
-    End Function
+'    ''' <summary>
+'    ''' 无方向性的
+'    ''' </summary>
+'    ''' <param name="id1"></param>
+'    ''' <param name="id2"></param>
+'    ''' <returns></returns>
+'    Public Function GetWGCNAWeight(id1 As String, id2 As String) As Double
+'        Dim correlation As xcb = GetCorrelation(id1, id2)
+'        If correlation Is Nothing Then
+'            Return 0
+'        Else
+'            Return correlation.wgcna_weight
+'        End If
+'    End Function
 
-    Public Function GetPccGreaterThan(id As String, cutoff As Double) As Dictionary(Of String, Double)
-        Dim SQL As String = String.Format(PCC_GREATER_THAN, id, cutoff)
-        Dim correlations = MySQL.Query(Of xcb)(SQL)
-        Dim dict = correlations.ToDictionary(
-            Function(paired) paired.GetConnectedNode(id),
-            elementSelector:=Function(paired) paired.pcc)
-        Return dict
-    End Function
+'    Public Function GetPccGreaterThan(id As String, cutoff As Double) As Dictionary(Of String, Double)
+'        Dim SQL As String = String.Format(PCC_GREATER_THAN, id, cutoff)
+'        Dim correlations = MySQL.Query(Of xcb)(SQL)
+'        Dim dict = correlations.ToDictionary(
+'            Function(paired) paired.GetConnectedNode(id),
+'            elementSelector:=Function(paired) paired.pcc)
+'        Return dict
+'    End Function
 
-    ''' <summary>
-    ''' <see cref="GetPccGreaterThan"/>不取绝对值，这个函数是取绝对值的
-    ''' </summary>
-    ''' <param name="id"></param>
-    ''' <param name="cutoff"></param>
-    ''' <returns></returns>
-    Public Function GetPccSignificantThan(id As String, cutoff As Double) As Dictionary(Of String, Double)
-        Dim SQL As String = String.Format(PCC_SIGNIFICANT_THAN, id, Math.Abs(cutoff))
-        Dim correlations = MySQL.Query(Of xcb)(SQL)
-        Dim dict = correlations.ToDictionary(
-            Function(paired) paired.GetConnectedNode(id),
-            elementSelector:=Function(paired) paired.pcc)
-        Return dict
-    End Function
+'    ''' <summary>
+'    ''' <see cref="GetPccGreaterThan"/>不取绝对值，这个函数是取绝对值的
+'    ''' </summary>
+'    ''' <param name="id"></param>
+'    ''' <param name="cutoff"></param>
+'    ''' <returns></returns>
+'    Public Function GetPccSignificantThan(id As String, cutoff As Double) As Dictionary(Of String, Double)
+'        Dim SQL As String = String.Format(PCC_SIGNIFICANT_THAN, id, Math.Abs(cutoff))
+'        Dim correlations = MySQL.Query(Of xcb)(SQL)
+'        Dim dict = correlations.ToDictionary(
+'            Function(paired) paired.GetConnectedNode(id),
+'            elementSelector:=Function(paired) paired.pcc)
+'        Return dict
+'    End Function
 
-    Public Function BuildHash(idList As String()) As Dictionary(Of String, Dictionary(Of String, xcb))
-        Dim SQL As String = "SELECT * FROM correlations.xcb where (g1_entity = '{0}' or g2_entity = '{0}');"
-        Dim LQuery = (From id As String
-                      In idList.AsParallel
-                      Select id,
-                          dataSet = MySQL.Query(Of xcb)(String.Format(SQL, id))).ToArray
+'    Public Function BuildHash(idList As String()) As Dictionary(Of String, Dictionary(Of String, xcb))
+'        Dim SQL As String = "SELECT * FROM correlations.xcb where (g1_entity = '{0}' or g2_entity = '{0}');"
+'        Dim LQuery = (From id As String
+'                      In idList.AsParallel
+'                      Select id,
+'                          dataSet = MySQL.Query(Of xcb)(String.Format(SQL, id))).ToArray
 
-    End Function
-End Class
+'    End Function
+'End Class
--- src/repository/DataMySql/Correlations/CorrsDbAPI.vb
+++ src/repository/DataMySql/Correlations/CorrsDbAPI.vb
@@ -1,165 +1,165 @@
-﻿#Region "Microsoft.VisualBasic::8857af36f4315f6c5da904c897307478, ..\GCModeller\analysis\annoTools\DataMySql\Correlations\CorrsDbAPI.vb"
+﻿'#Region "Microsoft.VisualBasic::8857af36f4315f6c5da904c897307478, ..\GCModeller\analysis\annoTools\DataMySql\Correlations\CorrsDbAPI.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+'    ' Author:
+'    ' 
+'    '       asuka (amethyst.asuka@gcmodeller.org)
+'    '       xieguigang (xie.guigang@live.com)
+'    '       xie (genetics@smrucc.org)
+'    ' 
+'    ' Copyright (c) 2016 GPL3 Licensed
+'    ' 
+'    ' 
+'    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+'    ' 
+'    ' This program is free software: you can redistribute it and/or modify
+'    ' it under the terms of the GNU General Public License as published by
+'    ' the Free Software Foundation, either version 3 of the License, or
+'    ' (at your option) any later version.
+'    ' 
+'    ' This program is distributed in the hope that it will be useful,
+'    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    ' GNU General Public License for more details.
+'    ' 
+'    ' You should have received a copy of the GNU General Public License
+'    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#End Region
+'#End Region
 
-Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic
-Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports Microsoft.VisualBasic.Data.csv
-Imports Microsoft.VisualBasic.Linq.Extensions
-Imports Microsoft.VisualBasic.Scripting.MetaData
-Imports Oracle.LinuxCompatibility.MySQL
-Imports SMRUCC.genomics.Analysis
-Imports SMRUCC.genomics.Analysis.RNA_Seq
-Imports SMRUCC.genomics.Analysis.RNA_Seq.RTools
-Imports SMRUCC.genomics.Analysis.RNA_Seq.RTools.WGCNA
-Imports SMRUCC.genomics.Analysis.RNA_Seq.WGCNA
-Imports SMRUCC.genomics.Data.Model_Repository.MySQL.Tables
+'Imports System.Runtime.CompilerServices
+'Imports Microsoft.VisualBasic
+'Imports Microsoft.VisualBasic.CommandLine.Reflection
+'Imports Microsoft.VisualBasic.Data.csv
+'Imports Microsoft.VisualBasic.Linq.Extensions
+'Imports Microsoft.VisualBasic.Scripting.MetaData
+'Imports Oracle.LinuxCompatibility.MySQL
+'Imports SMRUCC.genomics.Analysis
+'Imports SMRUCC.genomics.Analysis.RNA_Seq
+'Imports SMRUCC.genomics.Analysis.RNA_Seq.RTools
+'Imports SMRUCC.genomics.Analysis.RNA_Seq.RTools.WGCNA
+'Imports SMRUCC.genomics.Analysis.RNA_Seq.WGCNA
+'Imports SMRUCC.genomics.Data.Model_Repository.MySQL.Tables
 
-<Package("GCModeller.Gene.Correlations",
-                  Publisher:="xie.guigang@gcmodeller.org",
-                  Category:=APICategories.ResearchTools)>
-Public Module CorrsDbAPI
+'<Package("GCModeller.Gene.Correlations",
+'                  Publisher:="xie.guigang@gcmodeller.org",
+'                  Category:=APICategories.ResearchTools)>
+'Public Module CorrsDbAPI
 
-    Public Const PCC_SIGNIFICANT_THAN As String = "SELECT * FROM correlations.xcb where (g1_entity = '{0}' or g2_entity = '{0}') and abs(pcc) >= {1};"
-    Public Const PCC_GREATER_THAN As String = "SELECT * FROM correlations.xcb where (g1_entity = '{0}' or g2_entity = '{0}') and pcc >= {1};"
+'    Public Const PCC_SIGNIFICANT_THAN As String = "SELECT * FROM correlations.xcb where (g1_entity = '{0}' or g2_entity = '{0}') and abs(pcc) >= {1};"
+'    Public Const PCC_GREATER_THAN As String = "SELECT * FROM correlations.xcb where (g1_entity = '{0}' or g2_entity = '{0}') and pcc >= {1};"
 
-    <ExportAPI("DB.Init()")>
-    Public Function DBInit() As Correlations
-        Return New Correlations
-    End Function
+'    <ExportAPI("DB.Init()")>
+'    Public Function DBInit() As Correlations
+'        Return New Correlations
+'    End Function
 
-    <ExportAPI("Correlates")>
-    Public Function GetCorrelates(Db As Correlations, id1 As String, id2 As String) As xcb
-        Dim correlation As xcb = Db.GetCorrelation(id1, id2)
-        Return correlation
-    End Function
+'    <ExportAPI("Correlates")>
+'    Public Function GetCorrelates(Db As Correlations, id1 As String, id2 As String) As xcb
+'        Dim correlation As xcb = Db.GetCorrelation(id1, id2)
+'        Return correlation
+'    End Function
 
-    ''' <summary>
-    ''' 这个函数是自动计算WGCNA相关性的
-    ''' </summary>
-    ''' <param name="raw">第一列是基因的编号列表，其他的列都是基因的表达数据</param>
-    ''' <param name="uri"></param>
-    ''' <returns></returns>
-    ''' 
-    <ExportAPI("Write.Database")>
-    Public Function WriteDatabase(raw As IO.File, Optional uri As ConnectionUri = Nothing) As Boolean
-        Dim WGCNA As String = RTools.WGCNA.CallInvoke(raw.FilePath, raw.FilePath.ParentPath & "/annotations.csv")
-        Return WriteDatabase(raw, WGCNA, uri)
-    End Function
+'    ''' <summary>
+'    ''' 这个函数是自动计算WGCNA相关性的
+'    ''' </summary>
+'    ''' <param name="raw">第一列是基因的编号列表，其他的列都是基因的表达数据</param>
+'    ''' <param name="uri"></param>
+'    ''' <returns></returns>
+'    ''' 
+'    <ExportAPI("Write.Database")>
+'    Public Function WriteDatabase(raw As IO.File, Optional uri As ConnectionUri = Nothing) As Boolean
+'        Dim WGCNA As String = RTools.WGCNA.CallInvoke(raw.FilePath, raw.FilePath.ParentPath & "/annotations.csv")
+'        Return WriteDatabase(raw, WGCNA, uri)
+'    End Function
 
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <param name="raw">第一列是基因的编号列表，其他的列都是基因的表达数据</param>
-    ''' <param name="uri"></param>
-    ''' <returns></returns>
-    ''' <param name="WGCNA">WGCNA脚本所计算出来的Cytoscape的边文件</param>
-    <ExportAPI("Write.Database")>
-    Public Function WriteDatabase(raw As IO.File, WGCNA As String, Optional uri As ConnectionUri = Nothing) As Boolean
-        Dim WGCNA_MAT = FastImports(WGCNA)
-        Dim Samples = MatrixAPI.ToSamples(raw, True)
-        Dim PccMatrix = CreatePccMAT(Samples)
-        Dim sPccMAT = CreateSPccMAT(Samples)
+'    ''' <summary>
+'    ''' 
+'    ''' </summary>
+'    ''' <param name="raw">第一列是基因的编号列表，其他的列都是基因的表达数据</param>
+'    ''' <param name="uri"></param>
+'    ''' <returns></returns>
+'    ''' <param name="WGCNA">WGCNA脚本所计算出来的Cytoscape的边文件</param>
+'    <ExportAPI("Write.Database")>
+'    Public Function WriteDatabase(raw As IO.File, WGCNA As String, Optional uri As ConnectionUri = Nothing) As Boolean
+'        Dim WGCNA_MAT = FastImports(WGCNA)
+'        Dim Samples = MatrixAPI.ToSamples(raw, True)
+'        Dim PccMatrix = CreatePccMAT(Samples)
+'        Dim sPccMAT = CreateSPccMAT(Samples)
 
-        Try
-            Call __commits(GetMySQLClient(uri, DBName:="correlations"), WGCNA_MAT, PccMatrix, sPccMAT)
-        Catch ex As Exception
-            Call App.LogException(ex)
-            Return False
-        End Try
+'        Try
+'            Call __commits(GetMySQLClient(uri, DBName:="correlations"), WGCNA_MAT, PccMatrix, sPccMAT)
+'        Catch ex As Exception
+'            Call App.LogException(ex)
+'            Return False
+'        End Try
 
-        Return True
-    End Function
+'        Return True
+'    End Function
 
-    <ExportAPI("Write.Database")>
-    Public Function WriteDatabase(raw As String, WGCNA As String, Optional uri As ConnectionUri = Nothing) As Boolean
-        Return WriteDatabase(IO.File.Load(raw), WGCNA, uri)
-    End Function
+'    <ExportAPI("Write.Database")>
+'    Public Function WriteDatabase(raw As String, WGCNA As String, Optional uri As ConnectionUri = Nothing) As Boolean
+'        Return WriteDatabase(IO.File.Load(raw), WGCNA, uri)
+'    End Function
 
-    ''' <summary>
-    ''' 先排序在进行遍历，不需要再进行随机查找了，太耗费时间了
-    ''' </summary>
-    ''' <param name="MySQL"></param>
-    ''' <param name="WGCNA_MAT"></param>
-    ''' <param name="PccMatrix"></param>
-    ''' <param name="sPccMAT"></param>
-    Private Sub __commits(MySQL As MySQL, WGCNA_MAT As WGCNAWeight, PccMatrix As PccMatrix, sPccMAT As PccMatrix)
-        Call "Create data sets and commit transactions...".__DEBUG_ECHO
+'    ''' <summary>
+'    ''' 先排序在进行遍历，不需要再进行随机查找了，太耗费时间了
+'    ''' </summary>
+'    ''' <param name="MySQL"></param>
+'    ''' <param name="WGCNA_MAT"></param>
+'    ''' <param name="PccMatrix"></param>
+'    ''' <param name="sPccMAT"></param>
+'    Private Sub __commits(MySQL As MySQL, WGCNA_MAT As WGCNAWeight, PccMatrix As PccMatrix, sPccMAT As PccMatrix)
+'        Call "Create data sets and commit transactions...".__DEBUG_ECHO
 
-        For i As Integer = 0 To PccMatrix.lstGenes.Length - 1  ' pcc和spcc这两个对象的基因的排布顺序是一致的
-            Dim pcc = PccMatrix(i), spp = sPccMAT(i)
-            Dim g1 As String = pcc.locusId
-            Dim Transaction As New List(Of xcb)
+'        For i As Integer = 0 To PccMatrix.lstGenes.Length - 1  ' pcc和spcc这两个对象的基因的排布顺序是一致的
+'            Dim pcc = PccMatrix(i), spp = sPccMAT(i)
+'            Dim g1 As String = pcc.locusId
+'            Dim Transaction As New List(Of xcb)
 
-            For ii2 As Integer = 0 To PccMatrix.lstGenes.Length - 1  ' 由于在创建矩阵的时候没有打破一一对应的关系，所以这里可以使用遍历的方式来替代随机查询加快计算提交的过程
-                Dim g2 As String = PccMatrix.lstGenes(ii2)
-                Dim WGCNA = WGCNA_MAT.Find(g1, g2)
-                Dim weight As Double = If(WGCNA Is Nothing, 0, WGCNA.Weight)
-                Dim line As New xcb With {
-                    .g1_entity = g1,
-                    .g2_entity = g2,
-                    .pcc = pcc.Values(ii2),
-                    .spcc = spp.Values(ii2),
-                    .wgcna_weight = weight
-                }
-                Call Transaction.Add(line)
-            Next
+'            For ii2 As Integer = 0 To PccMatrix.lstGenes.Length - 1  ' 由于在创建矩阵的时候没有打破一一对应的关系，所以这里可以使用遍历的方式来替代随机查询加快计算提交的过程
+'                Dim g2 As String = PccMatrix.lstGenes(ii2)
+'                Dim WGCNA = WGCNA_MAT.Find(g1, g2)
+'                Dim weight As Double = If(WGCNA Is Nothing, 0, WGCNA.Weight)
+'                Dim line As New xcb With {
+'                    .g1_entity = g1,
+'                    .g2_entity = g2,
+'                    .pcc = pcc.Values(ii2),
+'                    .spcc = spp.Values(ii2),
+'                    .wgcna_weight = weight
+'                }
+'                Call Transaction.Add(line)
+'            Next
 
-            Dim exp As Exception = Nothing
-            Dim t As String = String.Join(vbCrLf, Transaction.ToArray(Function(ds) ds.GetInsertSQL))
+'            Dim exp As Exception = Nothing
+'            Dim t As String = String.Join(vbCrLf, Transaction.ToArray(Function(ds) ds.GetInsertSQL))
 
-            If Not MySQL.CommitTransaction(t, exp) Then
-                Call App.LogException(exp)
-                Call exp.PrintException
-            Else
-                Call $"[{g1}] Job Done!".__DEBUG_ECHO
-            End If
-        Next
+'            If Not MySQL.CommitTransaction(t, exp) Then
+'                Call App.LogException(exp)
+'                Call exp.PrintException
+'            Else
+'                Call $"[{g1}] Job Done!".__DEBUG_ECHO
+'            End If
+'        Next
 
-        Call "Job Done!".__DEBUG_ECHO
-    End Sub
+'        Call "Job Done!".__DEBUG_ECHO
+'    End Sub
 
-    ' 下面的代码是使用随机查找来得到数据的，但是效率非常低
+'    ' 下面的代码是使用随机查找来得到数据的，但是效率非常低
 
-    'Dim DataSet As Generic.IEnumerable(Of GCModeller.Workbench.DatabaseServices.Model_Repository.MySQL.Tables.xcb) =
-    '    PccMatrix.lstGenes.ToArray(
-    '        Function(p1) _
-    '            MySQL.CommitTransaction(
-    '                String.Join(vbCrLf, (
-    '                    From p2 As String
-    '                    In PccMatrix.lstGenes
-    '                    Select New SMRUCC.genomics.GCModeller.Workbench.DatabaseServices.Model_Repository.MySQL.Tables.xcb With {
-    '                        .pcc = PccMatrix.GetValue(p1, p2, Parallel:=False),
-    '                        .g1_entity = p1,
-    '                        .g2_entity = p2,
-    '                        .spcc = sPccMAT.GetValue(p1, p2, Parallel:=False),
-    '                        .wgcna_weight = WGCNA_MAT.GetValue(p1, p2, Parallel:=False)}).ToArray _
-    '                            .ToArray(Function(row) row.GetInsertSQL, Parallel:=False))), Parallel:=True).ToArray
+'    'Dim DataSet As Generic.IEnumerable(Of GCModeller.Workbench.DatabaseServices.Model_Repository.MySQL.Tables.xcb) =
+'    '    PccMatrix.lstGenes.ToArray(
+'    '        Function(p1) _
+'    '            MySQL.CommitTransaction(
+'    '                String.Join(vbCrLf, (
+'    '                    From p2 As String
+'    '                    In PccMatrix.lstGenes
+'    '                    Select New SMRUCC.genomics.GCModeller.Workbench.DatabaseServices.Model_Repository.MySQL.Tables.xcb With {
+'    '                        .pcc = PccMatrix.GetValue(p1, p2, Parallel:=False),
+'    '                        .g1_entity = p1,
+'    '                        .g2_entity = p2,
+'    '                        .spcc = sPccMAT.GetValue(p1, p2, Parallel:=False),
+'    '                        .wgcna_weight = WGCNA_MAT.GetValue(p1, p2, Parallel:=False)}).ToArray _
+'    '                            .ToArray(Function(row) row.GetInsertSQL, Parallel:=False))), Parallel:=True).ToArray
 
-    'Call "Job Done!".__DEBUG_ECHO
-End Module
+'    'Call "Job Done!".__DEBUG_ECHO
+'End Module
