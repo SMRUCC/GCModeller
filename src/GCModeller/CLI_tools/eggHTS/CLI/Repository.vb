db2a8fe34a7932596a1fb8d9031a4ef8a31d384e
xieguigang <xie.guigang@gcmodeller.org>
2017-09-03 12:39:08 +0800

    add cli tools for uniprot xml database imports

--- src/GCModeller/CLI_tools/eggHTS/CLI/Repository.vb
+++ src/GCModeller/CLI_tools/eggHTS/CLI/Repository.vb
@@ -1,7 +1,9 @@
 ﻿Imports System.ComponentModel
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports SMRUCC.genomics.Assembly.Uniprot.XML
 Imports SMRUCC.genomics.Data.GeneOntology.mysql
+Imports SMRUCC.genomics.Data.Repository.kb_UniProtKB
 Imports SMRUCC.genomics.foundation.OBO_Foundry
 
 Partial Module CLI
@@ -15,6 +17,18 @@ Partial Module CLI
 
         Return New OBOFile([in]) _
             .DumpMySQL(saveSQL:=out) _
+            .CLICode
+    End Function
+
+    <ExportAPI("/Imports.Uniprot.Xml")>
+    <Usage("/Imports.Uniprot.Xml /in <uniprot.xml> [/out <out.sql>]")>
+    Public Function DumpUniprot(args As CommandLine) As Integer
+        Dim in$ = args <= "/in"
+        Dim out$ = args.GetValue("/out", [in].TrimSuffix & ".kb_go.sql")
+        Dim proteins = UniProtXML.EnumerateEntries(path:=[in])
+
+        Return proteins _
+            .DumpMySQL(savedSQL:=out) _
             .CLICode
     End Function
 End Module
