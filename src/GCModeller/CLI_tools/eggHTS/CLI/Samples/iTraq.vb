107ee9a7c97e5bbdeca7881f60c114c45fdb24bf
xieguigang <xie.guigang@gcmodeller.org>
2017-09-13 10:58:18 +0800

    split data matrix based on the experimental designs

--- src/GCModeller/CLI_tools/eggHTS/CLI/Samples/iTraq.vb
+++ src/GCModeller/CLI_tools/eggHTS/CLI/Samples/iTraq.vb
@@ -1,12 +1,16 @@
-﻿Imports Microsoft.VisualBasic.CommandLine
+﻿Imports System.ComponentModel
+Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Language
 Imports SMRUCC.genomics.Analysis.HTS.Proteomics
+Imports SMRUCC.genomics.GCModeller.Workbench.ExperimentDesigner
 
 Partial Module CLI
 
-    <ExportAPI("/iTraq.Sign.Replacement")>
-    <Usage("/iTraq.Sign.Replacement /in <iTraq.data.csv> /symbols <signs.csv> [/out <out.DIR>]")>
+    <ExportAPI("/iTraq.Symbol.Replacement")>
+    <Usage("/iTraq.Symbol.Replacement /in <iTraq.data.csv> /symbols <symbols.csv> [/out <out.DIR>]")>
     <Argument("/in", False, CLITypes.File, PipelineTypes.std_in,
               AcceptTypes:={GetType(iTraqReader)},
               Description:="")>
@@ -25,6 +29,42 @@ Partial Module CLI
             Call .SymbolReplace(symbols) _
                  .ToArray _
                  .SaveTo(out & $"/{[in].BaseName}.sample.csv")
+        End With
+
+        Return 0
+    End Function
+
+    ''' <summary>
+    ''' 根据实验设计的信息进行矩阵的分组
+    ''' </summary>
+    ''' <param name="args"></param>
+    ''' <returns></returns>
+    ''' 
+    <ExportAPI("/iTraq.matrix.split")>
+    <Description("Split the raw matrix into different compare group based on the experimental designer information.")>
+    <Usage("/iTraq.matrix.split /in <matrix.csv> /sampleInfo <sampleInfo.csv> /designer <analysis.design.csv> [/out <out.Dir>]")>
+    Public Function iTraqAnalysisMatrixSplit(args As CommandLine) As Integer
+        Dim sampleInfo = (args <= "/sampleInfo").LoadCsv(Of SampleInfo)
+        Dim designer = (args <= "/designer").LoadCsv(Of ExperimentAnalysis)
+        Dim out$ = args.GetValue("/out", (args <= "/in").TrimSuffix & "-Groups/")
+        Dim matrix As DataSet() = DataSet.LoadDataSet(args <= "/in")
+
+        With sampleInfo.DataAnalysisDesign(analysis:=designer)
+
+            For Each group In .ref
+                Dim groupName = group.Key
+                Dim labels$() = group _
+                    .Value _
+                    .Select(Function(x) x.ToString) _
+                    .ToArray
+
+                Dim data = matrix _
+                    .Select(Function(x) x.SubSet(labels)) _
+                    .ToArray
+                Dim path$ = out & $"/{groupName.NormalizePathString(False)}.csv"
+
+                Call data.SaveTo(path)
+            Next
         End With
 
         Return 0
