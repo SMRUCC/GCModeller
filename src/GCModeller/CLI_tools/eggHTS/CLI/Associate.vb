43217fb187389a44265b8afaf6ff2601dc413ef2
xieguigang <xie.guigang@gcmodeller.org>
2017-09-28 21:49:24 +0800

    add sim similarity cal

--- src/GCModeller/CLI_tools/eggHTS/CLI/Associate.vb
+++ src/GCModeller/CLI_tools/eggHTS/CLI/Associate.vb
@@ -1,9 +1,12 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.DataMining
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Language.UnixBash
+Imports SMRUCC.genomics.Analysis.Microarray.KOBAS
 
 Partial Module CLI
 
@@ -19,5 +22,56 @@ Partial Module CLI
             Call .matrix.SaveTo(out & "/matrix.csv")
             Return .net.Save(out).CLICode
         End With
+    End Function
+
+    <ExportAPI("/KOBAS.similarity")>
+    <Usage("/KOBAS.Similarity /group1 <DIR> /group2 <DIR> [/fileName <default=output_run-Gene Ontology.csv> /out <out.DIR>]")>
+    Public Function KOBASSimilarity(args As CommandLine) As Integer
+        Dim group1$ = args <= "/group1"
+        Dim group2$ = args <= "/group2"
+        Dim out$ = args.GetValue("/out", group1.TrimDIR & "-" & group2.BaseName & "/")
+        Dim fileName$ = args.GetValue("/fileName", "output_run-Gene Ontology.csv")
+        Dim files1 = (ls - l - r - fileName <= group1) _
+            .Select(Function(file)
+                        Return file.LoadTerms(file.ParentPath.ParentDirName)
+                    End Function) _
+            .ToArray
+        Dim files2 = (ls - l - r - fileName <= group2) _
+            .Select(Function(file)
+                        Return file.LoadTerms(file.ParentPath.ParentDirName)
+                    End Function) _
+            .ToArray
+
+        Dim matrixA As New Dictionary(Of DataSet)
+        Dim matrixB As New Dictionary(Of DataSet)
+        Dim Sim As New List(Of DataSet)
+
+        For Each a In files1
+            Dim vector As New DataSet With {
+                .ID = $"[{group1.BaseName}] " & a.Name,
+                .Properties = New Dictionary(Of String, Double)
+            }
+
+            For Each b In files2
+                With Measure.Similarity(a, b)
+                    If Not matrixA.ContainsKey(.A.ID) Then
+                        matrixA += .A
+                    End If
+                    If Not matrixB.ContainsKey(.B.ID) Then
+                        matrixB += .B
+                    End If
+
+                    vector.Properties.Add($"[{group2.BaseName}] " & b.Name, .similarity)
+                End With
+            Next
+
+            Sim += vector
+        Next
+
+        Call matrixA.SaveTo(out & $"/{group1.BaseName}.csv")
+        Call matrixB.SaveTo(out & $"/{group2.BaseName}.csv")
+        Call Sim.SaveTo(out & $"/SimOf-{fileName}")
+
+        Return 0
     End Function
 End Module
