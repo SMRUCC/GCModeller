6b59131d2a737239cf3234cf9626fcc842f97bba
xieguigang <xie.guigang@gcmodeller.org>
2017-09-28 17:23:03 +0800

    add network builder

--- src/GCModeller/CLI_tools/eggHTS/CLI/Associate.vb
+++ src/GCModeller/CLI_tools/eggHTS/CLI/Associate.vb
@@ -1,4 +1,19 @@
-﻿Partial Module CLI
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.DataMining
+Imports Microsoft.VisualBasic.Language
 
+Partial Module CLI
 
+    <ExportAPI("/Network.PCC")>
+    <Usage("/Network.PCC /in <matrix.csv> [/cut <default=0.45> /out <out.DIR>]")>
+    Public Function PccNetwork(args As CommandLine) As Integer
+        Dim in$ = args <= "/in"
+        Dim cut# = args.GetValue("/cut", 0.45)
+        Dim out$ = (args <= "/out") Or ([in].TrimSuffix & ".PCC/").AsDefault
+        Dim matrix As DataSet() = DataSet.LoadDataSet([in]).ToArray
+        Dim network = CorrelationNetwork.BuildNetwork(matrix, cut)
+        Return network.Save(out).CLICode
+    End Function
 End Module
