c63e2027519f0d8c6005622d408554e539b25399
xieguigang <xie.guigang@gcmodeller.org>
2017-09-29 11:20:00 +0800

    conversion between string factor or numeric values

--- src/GCModeller/annotations/Proteomics/LabelFree/LabelFreeTtest.vb
+++ src/GCModeller/annotations/Proteomics/LabelFree/LabelFreeTtest.vb
@@ -1,8 +1,8 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.csv.IO
-Imports SMRUCC.genomics.GCModeller.Workbench.ExperimentDesigner
+Imports Microsoft.VisualBasic.Math
 Imports RDotNET.Extensions.VisualBasic.API
-Imports RServer = RDotNET.Extensions.VisualBasic.RSystem
+Imports SMRUCC.genomics.GCModeller.Workbench.ExperimentDesigner
 
 ''' <summary>
 ''' avg(A)/avg(B) = FC
@@ -29,11 +29,38 @@ Public Module LabelFreeTtest
         Dim experiment$() = sampleInfo.TakeGroup(analysis.Experimental).SampleNames
         Dim controls$() = sampleInfo.TakeGroup(analysis.Controls).SampleNames
         Dim allSamples$() = experiment.AsList + controls
-        Dim calc = data.Select(Function(protein)
-                                   Dim FC# = protein(experiment).Average / protein(controls).Average
-                                   Dim log2FC# = Math.Log(FC, newBase:=2)
-                                   Dim p_value# = stats.Ttest(protein(experiment), protein(controls), varEqual:=True).pvalue
-                                   Dim DEP As New DEP_iTraq With {.ID = protein.ID, .FCavg = FC, .log2FC = log2FC, .pvalue = pvalue, .Properties = protein.SubSet(allSamples).Properties.asstring}
-                               End Function)
+
+        ' calc the different expression proteins
+        Dim calc = data _
+            .Select(Function(protein)
+                        Dim FC# = protein(experiment).Average / protein(controls).Average
+                        Dim log2FC# = Math.Log(FC, newBase:=2)
+                        Dim p_value# = stats.Ttest(
+                            protein(experiment),
+                            protein(controls),
+                            varEqual:=True).pvalue
+
+                        Dim DEP As New DEP_iTraq With {
+                            .ID = protein.ID,
+                            .FCavg = FC,
+                            .log2FC = log2FC,
+                            .pvalue = pvalue,
+                            .Properties = protein _
+                                .SubSet(allSamples) _
+                                .Properties _
+                                .AsCharacter
+                        }
+
+                        Return DEP
+                    End Function) _
+            .ToArray
+
+        With calc.VectorShadows
+
+
+
+        End With
+
+        Return calc
     End Function
 End Module
