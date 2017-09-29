12d500d510858290d48f9056164b076f4d3467ea
xieguigang <xie.guigang@gcmodeller.org>
2017-09-29 17:58:32 +0800

    new VB runtime

--- src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/Drivers/CSS/RuntimeInvoker.vb
+++ src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/Drivers/CSS/RuntimeInvoker.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS.Render
+Imports VB = Microsoft.VisualBasic.Language.Runtime
 
 Namespace Driver.CSS
 
@@ -10,37 +12,31 @@ Namespace Driver.CSS
     ''' </summary>
     Public Module RuntimeInvoker
 
-        Public Class var
-            Public name$, value
-
-            Public Shared Operator =(var As var, value As Object) As var
-                var.value = value
-                Return var
-            End Operator
-
-            Public Shared Operator <>(var As var, value As Object) As var
-                Throw New NotImplementedException
-            End Operator
-        End Class
-
-        Public Structure ArgumentList
-
-            Default Public ReadOnly Property Argument(name$) As var
-                Get
-                    Return New var With {.name = name}
-                End Get
-            End Property
-        End Structure
-
-        Public Function RunPlot(driver As [Delegate], CSS As CssBlock, ParamArray args As var()) As GraphicsData
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="driver">绘图函数</param>
+        ''' <param name="CSS">
+        ''' 定义了该函数之中的CSS元素的样式值，假若没有某一个可选参数的值，则使用默认参数值；
+        ''' 或者参数值出现在了<paramref name="args"/>列表之中，则会被<paramref name="args"/>的值所覆盖
+        ''' </param>
+        ''' <param name="args">必须要包含有所有的必须参数，可选参数可以不包含在其中</param>
+        ''' <returns></returns>
+        ''' <remarks>
+        ''' 因为考虑到手动输入参数可能会出现大小写不匹配的问题，故而在这里会首先尝试使用字典查找，
+        ''' 没有找到键名的时候才会进行字符串大小写不敏感的字符串比较
+        ''' </remarks>
+        Public Function RunPlot(driver As [Delegate], CSS As CssBlock, ParamArray args As Argument()) As GraphicsData
             Dim type As MethodInfo = driver.Method
             Dim parameters = type.GetParameters
+            Dim values As Dictionary(Of String, Argument) = args.ToDictionary(Function(arg) arg.name)
 
         End Function
 
         Sub test()
 
-            With New ArgumentList
+            With New VB
+
                 RunPlot(Nothing, Nothing, !A = 99, !B = 123, !C = "dertfff")
             End With
         End Sub
