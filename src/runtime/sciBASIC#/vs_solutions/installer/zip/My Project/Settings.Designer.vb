bc9cf30cdf461bc78d6be9fa5b5894a1921640de
xieguigang <xie.guigang@gcmodeller.org>
2017-10-15 00:31:01 +0800

    fix for the installer project

--- src/runtime/sciBASIC#/vs_solutions/installer/zip/My Project/Settings.Designer.vb
+++ src/runtime/sciBASIC#/vs_solutions/installer/zip/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.Installer.My.MySettings
+        Friend ReadOnly Property Settings() As Global.zip.My.MySettings
             Get
-                Return Global.Installer.My.MySettings.Default
+                Return Global.zip.My.MySettings.Default
             End Get
         End Property
     End Module
