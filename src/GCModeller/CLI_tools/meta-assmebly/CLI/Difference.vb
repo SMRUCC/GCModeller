7350631f18325b02c53331d3ad0dd9233d40adb9
xieguigang <xie.guigang@gcmodeller.org>
2017-10-21 16:07:11 +0800

    group bar plot serials data

--- src/GCModeller/CLI_tools/meta-assmebly/CLI/Difference.vb
+++ src/GCModeller/CLI_tools/meta-assmebly/CLI/Difference.vb
@@ -153,7 +153,7 @@ Partial Module CLI
     End Function
 
     <ExportAPI("/Relative_abundance.barplot")>
-    <Usage("/Relative_abundance.barplot /in <dataset.csv> [/desc /asc /take <-1> /out <out.png>]")>
+    <Usage("/Relative_abundance.barplot /in <dataset.csv> [/group <sample_group.csv> /desc /asc /take <-1> /out <out.png>]")>
     <Argument("/desc", True, CLITypes.Boolean, Description:="")>
     <Argument("/asc", True, CLITypes.Boolean, Description:="")>
     <Argument("/take", True, CLITypes.Integer,
@@ -164,15 +164,30 @@ Partial Module CLI
         Dim out$ = (args <= "/out") Or $"{[in].TrimSuffix}.barplot.png".AsDefault
         Dim isDesc As Boolean = args.IsTrue("/desc")
         Dim isAsc As Boolean = args.IsTrue("/asc")
+        Dim sampleGroup = (args <= "/group").LoadCsv(Of SampleGroup)
 
         ' 如果/desc和/asc这两个开关都开启了的话，则会优先选择/desc，因为倒序的图样式会更好看一些
         If isDesc AndAlso isAsc Then
             Call "``/desc`` and ``/asc`` option are both open, ``/asc`` option will be disabled!".Warning
         End If
 
+        Dim groups = sampleGroup _
+            .GroupBy(Function(sample) sample.sample_group) _
+            .ToDictionary(Function(g) g.Key,
+                          Function(list)
+                              Return list _
+                                  .Select(Function(sample) sample.sample_name) _
+                                  .ToArray
+                          End Function)
         Dim data = BarPlotDataExtensions _
             .LoadDataSet([in]) _
             .Normalize
+
+        If groups.Count > 0 Then
+            data = data _
+                .GroupBy(groups) _
+                .Normalize
+        End If
 
         If isDesc Then
             data = data.Desc
--- src/workbench/ExperimentDesigner/SampleInfo.vb
+++ src/workbench/ExperimentDesigner/SampleInfo.vb
@@ -1,6 +1,7 @@
 ﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
 Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Language
 
 ''' <summary>
 ''' 一般而言，对于实验数据的分析而言，在进行数据存储的时候使用的是<see cref="ID"/>属性，而在进行数据可视化或者数据报告输出的时候，则是使用的<see cref="sample_name"/>属性作为显示的label
@@ -31,7 +32,12 @@ Imports Microsoft.VisualBasic.Data.csv
 
 End Class
 
-Public Class SampleGroup : Implements INamedValue
+''' <summary>
+''' 样品的分组信息
+''' </summary>
+Public Class SampleGroup
+    Implements INamedValue
+    Implements Value(Of String).IValueOf
 
     ''' <summary>
     ''' 显示名称，可能会含有一些奇怪的符号
@@ -42,7 +48,7 @@ Public Class SampleGroup : Implements INamedValue
     ''' 样品的实验设计分组信息
     ''' </summary>
     ''' <returns></returns>
-    Public Property sample_group As String
+    Public Property sample_group As String Implements Value(Of String).IValueOf.Value
 
     Public Overrides Function ToString() As String
         Return $"[{sample_group}] {sample_name}"
