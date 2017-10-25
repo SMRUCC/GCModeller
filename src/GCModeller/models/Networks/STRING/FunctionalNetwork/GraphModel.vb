784df4c7b90db433a50dd8659f428ce2b624c588
xieguigang <xie.guigang@gcmodeller.org>
2017-10-25 16:50:10 +0800

    fix a possible null exception

--- src/GCModeller/models/Networks/STRING/FunctionalNetwork/GraphModel.vb
+++ src/GCModeller/models/Networks/STRING/FunctionalNetwork/GraphModel.vb
@@ -1,11 +1,43 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Language
 Imports SMRUCC.genomics.Data.STRING
 
 Public Module GraphModel
 
     <Extension>
     Public Function CreateGraph(edges As IEnumerable(Of InteractExports), nodes As IEnumerable(Of Coordinates)) As NetworkGraph
+        Dim g As New NetworkGraph
+        Dim gEdges As New List(Of Edge)
+        Dim gNodes = nodes _
+            .Select(Function(n)
+                        Return New Node With {
+                            .ID = n.node,
+                            .Data = New NodeData With {
+                                .label = n.annotation,
+                                .Color = n.color.GetBrush,
+                                .origID = n.node,
+                                .initialPostion = New FDGVector2(n.x_position, n.y_position)
+                            }
+                        }
+                    End Function) _
+            .ToDictionary
 
+        For Each edge As InteractExports In edges
+            gEdges += New Edge With {
+                .Source = gNodes(edge.node1),
+                .Target = gNodes(edge.node2),
+                .Data = New EdgeData With {
+                    .weight = edge.combined_score
+                }
+            }
+        Next
+
+        g.nodes = gNodes.Values.AsList
+        g.edges = gEdges
+
+        Return g
     End Function
 End Module
