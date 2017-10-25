6579893a750afd938675560908d3c16197ac5c33
xieguigang <xie.guigang@gcmodeller.org>
2017-10-25 16:25:12 +0800

    code style improvements

--- src/GCModeller/models/Networks/STRING-network-test/test/Module1.vb
+++ src/GCModeller/models/Networks/STRING-network-test/test/Module1.vb
@@ -1,6 +1,13 @@
-﻿Module Module1
+﻿Imports Microsoft.VisualBasic.Data.csv
+Imports SMRUCC.genomics.Data.STRING
+
+Module Module1
 
     Sub Main()
+        Dim edges = InteractExports.ImportsTsv("D:\GCModeller\src\GCModeller\models\Networks\STRING-network-test\string_interactions.tsv")
+        Dim nodes = "D:\GCModeller\src\GCModeller\models\Networks\STRING-network-test\string_network_coordinates.txt".LoadTsv(Of Coordinates)
 
+
+        Pause()
     End Sub
 End Module
