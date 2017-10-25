b7172227098665d3d96499bda790d367ea616e06
xieguigang <xie.guigang@gcmodeller.org>
2017-10-25 16:14:57 +0800

    add xml code comments

--- src/GCModeller/data/STRING/tsv/Coordinates.vb
+++ src/GCModeller/data/STRING/tsv/Coordinates.vb
@@ -2,12 +2,13 @@
 Imports Microsoft.VisualBasic.Imaging.LayoutModel
 
 ''' <summary>
-''' Tsv table reader for string-db export result ``string_network_coordinates.txt``
+''' Tsv table reader for string-db export result ``string_network_coordinates.txt``.
+''' (这个表格文件定义的是网络的节点的位置和蛋白的注释信息)
 ''' </summary>
-Public Class Coordinates
-    Implements ILayoutCoordinate
+Public Class Coordinates : Implements ILayoutCoordinate
 
-    <Column("#node")> Public Property node As String Implements ILayoutCoordinate.ID
+    <Column("#node")>
+    Public Property node As String Implements ILayoutCoordinate.ID
     Public Property x_position As Double Implements ILayoutCoordinate.X
     Public Property y_position As Double Implements ILayoutCoordinate.Y
     Public Property color As String
