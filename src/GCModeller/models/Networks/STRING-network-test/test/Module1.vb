c61b3bab4634e492ca0af975a1479b18c6a12972
xieguigang <xie.guigang@gcmodeller.org>
2017-10-25 16:38:42 +0800

    add visualize test

--- src/GCModeller/models/Networks/STRING-network-test/test/Module1.vb
+++ src/GCModeller/models/Networks/STRING-network-test/test/Module1.vb
@@ -1,5 +1,7 @@
 ﻿Imports Microsoft.VisualBasic.Data.csv
 Imports SMRUCC.genomics.Data.STRING
+Imports SMRUCC.genomics.Model.Network.STRING
+Imports Microsoft.VisualBasic.Data.visualize.Network
 
 Module Module1
 
@@ -7,6 +9,7 @@ Module Module1
         Dim edges = InteractExports.ImportsTsv("D:\GCModeller\src\GCModeller\models\Networks\STRING-network-test\string_interactions.tsv")
         Dim nodes = "D:\GCModeller\src\GCModeller\models\Networks\STRING-network-test\string_network_coordinates.txt".LoadTsv(Of Coordinates)
 
+        Call GraphModel.CreateGraph(edges, nodes).DrawImage().Save("D:\GCModeller\src\GCModeller\models\Networks\STRING-network-test\string_interactions.png")
 
         Pause()
     End Sub
---
+++ src/GCModeller/models/Networks/STRING/FunctionalNetwork/GraphModel.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports SMRUCC.genomics.Data.STRING
+
+Public Module GraphModel
+
+    <Extension>
+    Public Function CreateGraph(edges As IEnumerable(Of InteractExports), nodes As IEnumerable(Of Coordinates)) As NetworkGraph
+
+    End Function
+End Module
