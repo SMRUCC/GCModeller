be0a478e42ede36edcf40f3714e7ff9685e105f2
xieguigang <xie.guigang@gcmodeller.org>
2017-10-26 16:38:55 +0800

    create chebi chemical data property from tsv table

---
+++ src/GCModeller/core/Bio.Assembly/Test/chebiTest.vb
@@ -0,0 +1,21 @@
+﻿Imports SMRUCC.genomics.Assembly.EBI.ChEBI.Database.IO.StreamProviders.Tsv
+Imports SMRUCC.genomics.Assembly.EBI.ChEBI.XML
+
+Module chebiTest
+
+    Sub Main()
+
+        Dim tables As New TSVTables("D:\smartnucl_integrative\DATA\ChEBI\tsv")
+
+        Dim alltypes = tables.GetChemicalData.Select(Function(c) c.TYPE).Distinct.JoinBy(";  ")
+
+        Dim prope = tables.GetChemicalData().CreateProperty
+
+
+        Dim model = EntityList.LoadDirectory("D:\smartnucl_integrative\DATA\ChEBI\chebi_cache\")
+        Dim list = model.AsList
+        Dim table = model.ToSearchModel
+
+        Call model.GetXml.SaveTo("./chebi-100.xml")
+    End Sub
+End Module
