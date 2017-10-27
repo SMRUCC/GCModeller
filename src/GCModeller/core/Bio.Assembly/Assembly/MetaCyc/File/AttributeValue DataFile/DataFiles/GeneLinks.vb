f18107de56b103d91482ce707f8624a210e5c5fd
xieguigang <xie.guigang@gcmodeller.org>
2017-10-27 16:36:05 +0800

    fix object reference error

--- src/GCModeller/core/Bio.Assembly/Assembly/MetaCyc/File/AttributeValue DataFile/DataFiles/GeneLinks.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/MetaCyc/File/AttributeValue DataFile/DataFiles/GeneLinks.vb
@@ -1,33 +1,34 @@
 ﻿#Region "Microsoft.VisualBasic::473e1a16e08040f118418244210e917c, ..\core\Bio.Assembly\Assembly\MetaCyc\File\AttributeValue DataFile\DataFiles\GeneLinks.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Text
 Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
 
 Namespace Assembly.MetaCyc.File.DataFiles.DataTabular
 
@@ -38,7 +39,7 @@ Namespace Assembly.MetaCyc.File.DataFiles.DataTabular
         Public Shared Widening Operator CType(Path As String) As GeneLinks
             Dim File As String() = Nothing
             Dim prop As [Property] = Nothing
-            Call FileReader.TabularParser(Path, prop, File, "").Assertion(Logging.MSG_TYPES.WRN)
+            Call FileReader.TabularParser(Path, prop, File, "").Assertion(MSG_TYPES.WRN)
             Dim LinkSource As IEnumerable(Of GeneLink) = From s As String
                                                          In File.AsParallel
                                                          Select CType(s, GeneLink)
