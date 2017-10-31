d56231dcf3176fb64a853acad655dae9595fbeff
xieguigang <xie.guigang@gcmodeller.org>
2017-10-31 16:23:29 +0800

    write memory for primitive type

--- src/R.Bioconductor/RDotNET.Extensions.VisualBasic/Extensions/Serialization/WriteMemoryInternal.vb
+++ src/R.Bioconductor/RDotNET.Extensions.VisualBasic/Extensions/Serialization/WriteMemoryInternal.vb
@@ -1,6 +1,62 @@
-﻿Imports RDotNET.Extensions.VisualBasic.SymbolBuilder
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports RDotNET.Extensions.VisualBasic.SymbolBuilder
 
 Module WriteMemoryInternal
+
+    ReadOnly numericWriter As MethodInfo = GetType(WriteMemoryInternal).GetMethod(NameOf(WriteNumeric), PublicShared)
+    ReadOnly numericsWriter As MethodInfo = GetType(WriteMemoryInternal).GetMethod(NameOf(WriteNumerics), PublicShared)
+
+    Public Sub WritePrimitive(var$, x As Object)
+        If x Is Nothing Then
+            Call var.WriteNothing
+        Else
+            Select Case x.GetType
+                Case GetType(String)
+                    Call WriteString(var, DirectCast(x, String))
+                Case GetType(Boolean)
+                    Call WriteLogical(var, DirectCast(x, Boolean))
+                Case Else
+                    Call numericWriter _
+                        .MakeGenericMethod(x.GetType) _
+                        .Invoke(Nothing, {x})
+            End Select
+        End If
+    End Sub
+
+    Public Sub WritePrimitive(var$, x As IEnumerable)
+        If x Is Nothing Then
+            Call var.WriteNothing
+        Else
+            Dim baseType As Type = x _
+                .GetType _
+                .GetTypeElement(strict:=False)
+
+            Select Case baseType
+                Case GetType(String)
+                    Call WriteString(var, DirectCast(x, IEnumerable(Of String)))
+                Case GetType(Boolean)
+                    Call WriteLogical(var, DirectCast(x, IEnumerable(Of Boolean)))
+                Case Else
+                    Call numericsWriter _
+                        .MakeGenericMethod(baseType) _
+                        .Invoke(Nothing, {x})
+            End Select
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 相当于进行变量申明
+    ''' </summary>
+    ''' <param name="var$"></param>
+    <Extension> Public Sub WriteNothing(var$)
+        SyncLock R
+            With R
+                .call = $"{var} <- NULL;"
+            End With
+        End SyncLock
+    End Sub
 
     Public Sub WriteNumeric(Of T As IComparable(Of T))(var$, x As T)
         SyncLock R
@@ -26,7 +82,7 @@ Module WriteMemoryInternal
         End SyncLock
     End Sub
 
-    Public Sub WriteNumeric(Of T As IComparable(Of T))(var$, x As IEnumerable(Of T))
+    Public Sub WriteNumerics(Of T As IComparable(Of T))(var$, x As IEnumerable(Of T))
         SyncLock R
             With R
                 .call = $"{var} <- c({x.JoinBy(", ")});"
