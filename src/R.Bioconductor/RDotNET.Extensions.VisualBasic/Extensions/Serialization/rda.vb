a7bd9807da76594fb360d9e681c86d8db250a638
xieguigang <xie.guigang@gcmodeller.org>
2017-10-31 16:56:54 +0800

    Write Any .NET object into R memory

--- src/R.Bioconductor/RDotNET.Extensions.VisualBasic/Extensions/Serialization/rda.vb
+++ src/R.Bioconductor/RDotNET.Extensions.VisualBasic/Extensions/Serialization/rda.vb
@@ -1,6 +1,12 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.csv.Extensions
 Imports Microsoft.VisualBasic.Emit.Delegates
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
+Imports RDotNET.Extensions.VisualBasic.API
+Imports RDotNET.Extensions.VisualBasic.SymbolBuilder
 
 ''' <summary>
 ''' + 如果是复杂类型，会被序列化为``list()``类型，
@@ -10,23 +16,99 @@ Imports Microsoft.VisualBasic.Emit.Delegates
 ''' </summary>
 Public Module rda
 
+    ''' <summary>
+    ''' Write Any .NET object into R memory
+    ''' </summary>
+    ''' <param name="obj"></param>
+    ''' <returns></returns>
     Public Function Push(obj As Object) As String
         Dim type As Type = obj.GetType
         Dim var$ = App.NextTempName
 
         If DataFramework.IsPrimitive(type) Then
-            SyncLock R
-                With R
-                    .call = ""
-                End With
-            End SyncLock
+            Call WriteMemoryInternal.WritePrimitive(var, obj)
         ElseIf type.ImplementInterface(GetType(IEnumerable)) Then
+            Dim base As Type = type.GetTypeElement(False)
 
+            If DataFramework.IsPrimitive(base) Then
+                ' 全部都是基础类型，则写为一个向量
+                Call WriteMemoryInternal.WritePrimitive(var, DirectCast(obj, IEnumerable))
+            Else
+                SyncLock R
+                    With R
+                        .call = $"{var} <- list();"
+                    End With
+                End SyncLock
+
+                ' 是非基础类型，如果是简单的非基础类型，则写为一个data.frame
+                ' 反之复杂的非基础类型写为一个list
+                If DataFramework.IscomplexType(base) Then
+                    ' write as list
+                    ' 如果实现了INamedValue接口，则使用key属性作为键名
+                    ' 反之使用索引号
+
+                    If base.ImplementInterface(GetType(INamedValue)) Then
+                        For Each x As Object In DirectCast(obj, IEnumerable)
+                            Dim key$ = DirectCast(x, INamedValue).Key
+
+                            SyncLock R
+                                With R
+                                    .call = $"{var}[[{Rstring(key)}]] <- {PushComplexObject(x)};"
+                                End With
+                            End SyncLock
+                        Next
+                    Else
+                        For Each x As SeqValue(Of Object) In DirectCast(obj, IEnumerable).SeqIterator
+                            Dim key$ = x.i + 1 ' R 之中的下标是从1开始的 
+
+                            SyncLock R
+                                With R
+                                    .call = $"{var}[[{key}]] <- {PushComplexObject(x)};"
+                                End With
+                            End SyncLock
+                        Next
+                    End If
+
+                Else
+                    ' write as dataframe
+                    With App.GetAppSysTempFile(, App.PID)
+                        Call DirectCast(obj, IEnumerable).SaveTable(.ref, type:=base)
+                        Return utils.read.csv(.ref)
+                    End With
+                End If
+            End If
         Else
-
-
-
+            Return PushComplexObject(obj)
         End If
+
+        Return var
+    End Function
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="obj">这个类型为单个的class或者structure类型</param>
+    ''' <returns></returns>
+    Public Function PushComplexObject(obj As Object) As String
+        If obj Is Nothing Then
+            With App.NextTempName
+                Call WriteMemoryInternal.WriteNothing(.ref)
+                Return .ref
+            End With
+        End If
+
+        Dim schema = obj.GetType.Schema(PropertyAccess.Readable, , True)
+        Dim var$ = App.NextTempName
+
+        SyncLock R
+            With R
+                .call = $"{var} <- list();"
+
+                For Each [property] As PropertyInfo In schema.Values
+                    .call = $"{var}[[{Rstring([property].Name)}]] <- {Push([property].GetValue(obj))}"
+                Next
+            End With
+        End SyncLock
 
         Return var
     End Function
