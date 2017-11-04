20b03b82b2629190bd490f02a92bc25f2e3232f6
xieguigang <xie.guigang@gcmodeller.org>
2017-11-04 21:27:30 +0800

    improvements on the data frame API

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Architecture.Framework/Text/Xml/Models/Vector.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Architecture.Framework/Text/Xml/Models/Vector.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Xml.Serialization
+﻿Imports System.Runtime.CompilerServices
+Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace Text.Xml.Models
@@ -8,9 +9,28 @@ Namespace Text.Xml.Models
         <XmlAttribute>
         Public Property Vector As Double()
 
+        Default Public Property Xi(i As Integer) As Double
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
+            Get
+                Return Vector(i)
+            End Get
+            Set(value As Double)
+                Vector(i) = value
+            End Set
+        End Property
+
+        ''' <summary>
+        ''' The vector length for counting the elements in <see cref="Vector"/> property.
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property Length As Integer
+            Get
+                Return Vector?.Length
+            End Get
+        End Property
+
         Public Overrides Function ToString() As String
             Return Me.GetJson
         End Function
     End Class
-
 End Namespace
