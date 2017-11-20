10497cd1d09bb1e9b582575b4a09ea4f606ff474
xieguigang <xie.guigang@gcmodeller.org>
2017-11-20 14:34:05 +0800

    add http get headers

--- src/workbench/Knowledge_base/Knowledge_base/PubMedServicesExtensions.vb
+++ src/workbench/Knowledge_base/Knowledge_base/PubMedServicesExtensions.vb
@@ -1,6 +1,17 @@
 ﻿Public Module PubMedServicesExtensions
 
-    ReadOnly tool_info As Dictionary(Of String, String)
+    ''' <summary>
+    ''' > https://www.ncbi.nlm.nih.gov/pmc/tools/developers/
+    ''' 
+    ''' Include two parameters that help to identify your service or application to our servers:
+    '''
+    ''' + ``tool`` should be the name Of the application, As a String value With no internal spaces, And
+    ''' + ``email`` should be the e-mail address Of the maintainer Of the tool, And should be a valid e-mail address.
+    ''' </summary>
+    ReadOnly tool_info As New Dictionary(Of String, String) From {
+        {"tool", "GCModeller-workbench-pubmed-repository"},
+        {"email", "xie.guigang@gcmodeller.org"}
+    }
 
     ''' <summary>
     ''' Example
@@ -13,7 +24,7 @@
     ''' <returns></returns>
     Public Function GetArticleInfo(term As String) As PubmedArticle
         Dim url$ = $"https://www.ncbi.nlm.nih.gov/pubmed/?term={term}&report=xml"
-        Dim html$ = url.GET()
+        Dim html$ = url.GET(headers:=tool_info)
         Dim xml$ = html _
             .GetBetween("<pre>", "</pre>") _
             .Replace("&lt;", "<") _
