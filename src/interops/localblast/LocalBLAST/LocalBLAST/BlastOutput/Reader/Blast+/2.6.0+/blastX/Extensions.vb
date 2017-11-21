226ee5eeabb7200eb7fe4e75feed44011b5b9084
xieguigang <xie.guigang@gcmodeller.org>
2017-11-21 09:26:54 +0800

    blastx sbh top best?

--- src/interops/localblast/LocalBLAST/LocalBLAST/BlastOutput/Reader/Blast+/2.6.0+/blastX/Extensions.vb
+++ src/interops/localblast/LocalBLAST/LocalBLAST/BlastOutput/Reader/Blast+/2.6.0+/blastX/Extensions.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 
 Namespace LocalBLAST.BLASTOutput.BlastPlus.BlastX
 
@@ -13,7 +14,38 @@ Namespace LocalBLAST.BLASTOutput.BlastPlus.BlastX
         ''' <param name="result"></param>
         ''' <returns></returns>
         <Extension> Public Function TopBest(result As IEnumerable(Of BlastXHit)) As BlastXHit()
+            ' 首先按照hits分组，然后再在hits分组中按照query分组，
+            ' 计算出得分最高的query作为top best
+            Dim hitsGroup = result.GroupBy(Function(hit) hit.HitName).ToArray
+            Dim top = hitsGroup _
+                .Select(Function(hits)
+                            Return hits _
+                                .GroupBy(Function(q) q.QueryName) _
+                                .OrderByDescending(Function(query)
+                                                       Return query _
+                                                           .Select(Function(q) q.SBHScore * q.hit_length) _
+                                                           .Sum
+                                                   End Function) _
+                                .First
+                        End Function) _
+                .Select(Function(g)
+                            Return New NamedValue(Of BlastXHit()) With {
+                                .Name = g.Key,
+                                .Value = g.ToArray
+                            }
+                        End Function) _
+                .ToArray
 
+            ' 将query得到的frame进行合并
+            Dim bestOne As BlastXHit() = top _
+                .Select(Function(q)
+                            Return q.Value _
+                                .OrderByDescending(Function(hit) hit.coverage) _
+                                .First
+                        End Function) _
+                .ToArray
+
+            Return bestOne
         End Function
     End Module
 End Namespace
