27433066b0848344a457e80b043055aec293005f
xieguigang <xie.guigang@gcmodeller.org>
2017-11-22 15:32:51 +0800

    add rda build test

--- src/R.Bioconductor/RDotNET.Extensions.GCModeller/test/Module1.vb
+++ src/R.Bioconductor/RDotNET.Extensions.GCModeller/test/Module1.vb
@@ -1,9 +1,16 @@
 ﻿Imports SMRUCC.genomics.Assembly.KEGG.WebServices
+Imports Microsoft.VisualBasic.Language.UnixBash
+Imports RDotNET.Extensions.GCModeller
 
 Module Module1
 
     Sub Main()
 
         Call MapDownloader.Downloads("./KEGGMaps/")
+
+
+        Call (ls - l - r - "*.XML" <= "./KEGGMaps/") _
+            .Select(AddressOf LoadXml(Of Map)) _
+            .SaveRda("./KEGG.rda")
     End Sub
 End Module
