87c8224ec235fe3b4aed1c5f251f0bcdef6d24f4
xieguigang <xie.guigang@gcmodeller.org>
2017-11-28 16:37:03 +0800

    removes shared canvas object

--- src/GCModeller/CLI_tools/KEGG/Tools/SystemDistribution.vb
+++ src/GCModeller/CLI_tools/KEGG/Tools/SystemDistribution.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::cf6c0bb7f9c85663f5b8c0f6cab720db, ..\CLI_tools\KEGG\Tools\SystemDistribution.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -32,6 +32,7 @@ Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
 Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
 
 <Package("Pathway.System.Distribution", Publisher:="xie.guigang@gmail.com")>
 Public Module SystemDistribution
@@ -47,35 +48,36 @@ Public Module SystemDistribution
                       Select sp, system, org = Organism(sp)
                       Group By org.Phylum Into Group).ToArray
 
-        Dim Device = New Size(MAT.Width * 100 + 2 * Margin, MAT.Count * 2 + 2 * Margin).CreateGDIDevice
-        Dim X As Integer, Y As Integer
-        Dim DrawingFont As New Font(FontFace.MicrosoftYaHei, 8)
-        Dim Size As SizeF = LQuery.First.Group.First.sp.MeasureString(DrawingFont)
+        Using device = New Size(MAT.Width * 100 + 2 * Margin, MAT.Count * 2 + 2 * Margin).CreateGDIDevice
+            Dim X As Integer, Y As Integer
+            Dim DrawingFont As New Font(FontFace.MicrosoftYaHei, 8)
+            Dim Size As SizeF = LQuery.First.Group.First.sp.MeasureSize(device, DrawingFont)
 
-        For Each row In LQuery
-            Dim Rect As Rectangle = New Rectangle(X, Y, Device.Width, Size.Height * row.Group.Count)
-            Call Device.Graphics.DrawRectangle(Pens.Black, Rect)
+            For Each row In LQuery
+                Dim Rect As Rectangle = New Rectangle(X, Y, device.Width, Size.Height * row.Group.Count)
+                Call device.Graphics.DrawRectangle(Pens.Black, Rect)
 
-            X = Margin
-            Y += 2
-
-            For Each sp In row.Group
-                Size = Device.Graphics.MeasureString(sp.sp, DrawingFont)
-                Call Device.Graphics.DrawString(sp.sp, DrawingFont, Brushes.Black, New Point(X, Y))
-
-                X += Size.Width
-
-                For Each sys In sp.system
-                    Dim Color As Color = If(sys = 0, Color.White, Color.Red)
-                    Call Device.Graphics.FillRectangle(New SolidBrush(Color), New Rectangle(New Point(X, Y), New Size(80, 2)))
-                    X += 90
-                Next
-
-                Y += Size.Height
                 X = Margin
-            Next
-        Next
+                Y += 2
 
-        Return Device.ImageResource
+                For Each sp In row.Group
+                    Size = device.Graphics.MeasureString(sp.sp, DrawingFont)
+                    Call device.Graphics.DrawString(sp.sp, DrawingFont, Brushes.Black, New Point(X, Y))
+
+                    X += Size.Width
+
+                    For Each sys In sp.system
+                        Dim Color As Color = If(sys = 0, Color.White, Color.Red)
+                        Call device.Graphics.FillRectangle(New SolidBrush(Color), New Rectangle(New Point(X, Y), New Size(80, 2)))
+                        X += 90
+                    Next
+
+                    Y += Size.Height
+                    X = Margin
+                Next
+            Next
+
+            Return device.ImageResource
+        End Using
     End Function
 End Module
