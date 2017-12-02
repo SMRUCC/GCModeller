73d0815ef324bca0bf3358be69907662596922c4
xieguigang <xie.guigang@gcmodeller.org>
2017-12-02 22:30:45 +0800

    get abstract information about the kb

--- src/GCModeller/CLI_tools/kb/CLI.vb
+++ src/GCModeller/CLI_tools/kb/CLI.vb
@@ -1,6 +1,11 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Language.UnixBash
+Imports Microsoft.VisualBasic.Serialization.JSON
+Imports Microsoft.VisualBasic.Text
 Imports Microsoft.VisualBasic.Webservices.Bing
+Imports Microsoft.VisualBasic.Webservices.Bing.Academic
+Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base
 
 Module CLI
 
@@ -8,7 +13,7 @@ Module CLI
     <Usage("/kb.build.query /term <term> [/pages <default=20> /out <out.directory>]")>
     Public Function BingAcademicQuery(args As CommandLine) As Integer
         Dim term$ = args <= "/term"
-        Dim out$ = args.GetValue("/out", App.CurrentDirectory & "/" & term.NormalizePathString)
+        Dim out$ = args("/out") Or (App.CurrentDirectory & "/" & term.NormalizePathString)
         Dim pages% = args.GetValue("/pages", 20)
 
         Call Academic.Build_KB(term, out, pages, flat:=False)
@@ -17,9 +22,17 @@ Module CLI
     End Function
 
     <ExportAPI("/kb.abstract")>
-    <Usage("/kb.abstract /in <kb.directory> [/out <out.xml>]")>
+    <Usage("/kb.abstract /in <kb.directory> [/out <out.txt>]")>
     Public Function GetKBAbstractInformation(args As CommandLine) As Integer
         Dim in$ = args <= "/in"
-        Dim out$ = args.GetValue("/out", [in].TrimDIR & ".abstract.xml")
+        Dim out$ = args("/out") Or $"{[in].TrimDIR}.abstract.xml"
+        Dim kb As IEnumerable(Of ArticleProfile) = (ls - l - r - "*.xml" <= [in]).Select(AddressOf LoadXml(Of ArticleProfile))
+        Dim abstract = kb.InformationAbstract
+        Dim abstractText$ = abstract.Keys.JoinBy(" ")
+        Dim scores = abstract.GetJson(indent:=True)
+
+        Return (abstractText & ASCII.LF & ASCII.LF & scores) _
+            .SaveTo(out, TextEncodings.UTF8WithoutBOM) _
+            .CLICode
     End Function
 End Module
