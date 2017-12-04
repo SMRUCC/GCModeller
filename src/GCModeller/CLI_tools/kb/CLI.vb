17834d88a7bad23baaad596f8e89e0f4172642ad
xieguigang <xie.guigang@gcmodeller.org>
2017-12-04 10:22:23 +0800

    improvements on the abstract function

--- src/GCModeller/CLI_tools/kb/CLI.vb
+++ src/GCModeller/CLI_tools/kb/CLI.vb
@@ -1,33 +1,34 @@
 ﻿#Region "Microsoft.VisualBasic::33d876b8b79180964ad6f271a20c041e, ..\GCModeller\CLI_tools\kb\CLI.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.NLP
 Imports Microsoft.VisualBasic.Language.UnixBash
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
@@ -50,17 +51,21 @@ Module CLI
     End Function
 
     <ExportAPI("/kb.abstract")>
-    <Usage("/kb.abstract /in <kb.directory> [/min.weight <default=0.05> /out <out.txt>]")>
+    <Usage("/kb.abstract /in <kb.directory> [/min.weight <default=0.05> /out <out.json>]")>
     Public Function GetKBAbstractInformation(args As CommandLine) As Integer
         Dim in$ = args <= "/in"
         Dim minWeight# = args.GetValue("/min.weight", 0.05)
-        Dim out$ = args("/out") Or $"{[in].TrimDIR}.abstract(min_weight={minWeight}).txt"
+        Dim out$ = args("/out") Or $"{[in].TrimDIR}.textgraph.weights.json"
         Dim kb As IEnumerable(Of ArticleProfile) = (ls - l - r - "*.xml" <= [in]).Select(AddressOf LoadXml(Of ArticleProfile))
-        Dim abstract = kb.InformationAbstract(minWeight:=minWeight)
-        Dim abstractText$ = abstract.Keys.JoinBy(" ")
-        Dim scores = abstract.GetJson(indent:=True)
+        Dim weights = kb.TextGraphWeights
+        Dim abstract = weights.AbstractFilter(minWeight:=minWeight)
+        Dim abstractText$ = abstract.Keys.JoinBy(ASCII.LF)
 
-        Return (abstractText & ASCII.LF & ASCII.LF & scores) _
+        Call (abstractText & ASCII.LF & ASCII.LF & abstract.GetJson(indent:=True)) _
+            .SaveTo(out.TrimSuffix & $".abstract(min_weight={minWeight}).txt")
+
+        Return weights _
+            .GetJson _
             .SaveTo(out, TextEncodings.UTF8WithoutBOM) _
             .CLICode
     End Function
