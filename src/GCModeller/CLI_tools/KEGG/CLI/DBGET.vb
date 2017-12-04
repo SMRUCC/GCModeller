1269e7dd37ecff91ddf6e764aa8a4329e7c45398
xieguigang <xie.guigang@gcmodeller.org>
2017-12-04 16:51:39 +0800

    batch download script

--- src/GCModeller/CLI_tools/KEGG/CLI/DBGET.vb
+++ src/GCModeller/CLI_tools/KEGG/CLI/DBGET.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::708329377a56401b31d8b16f6af70e33, ..\GCModeller\CLI_tools\KEGG\CLI\DBGET.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -39,6 +39,7 @@ Imports SMRUCC.genomics.Assembly.KEGG.DBGET
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.BriteHEntry
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.ReferenceMap
+Imports SMRUCC.genomics.Metagenomics
 Imports kegMap = SMRUCC.genomics.Assembly.KEGG.WebServices.MapDownloader
 
 Partial Module CLI
@@ -119,8 +120,19 @@ Partial Module CLI
         End If
 
         Dim codes = htext.GetBacteriaList
+        Dim EXPORT$
 
+        For Each code As Taxonomy In codes
+            EXPORT = code.scientificName.NormalizePathString
+            EXPORT = $"{out}/{EXPORT}/"
 
+            Apps.KEGG_tools.DownloadPathwayMaps(
+                _sp:=code.species,
+                _out:=EXPORT
+            )
+        Next
+
+        Return 0
     End Function
 
     <ExportAPI("/Download.Module.Maps",
