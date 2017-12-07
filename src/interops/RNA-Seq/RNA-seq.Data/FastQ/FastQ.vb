8453cf00d9ecb05d09fcae9eb5425a9cf010dff5
xieguigang <xie.guigang@gcmodeller.org>
2017-12-07 16:10:44 +0800

    add test

--- src/interops/RNA-Seq/RNA-seq.Data/FastQ/FastQ.vb
+++ src/interops/RNA-Seq/RNA-seq.Data/FastQ/FastQ.vb
@@ -1,31 +1,32 @@
 ﻿#Region "Microsoft.VisualBasic::43431bf845ed1aea9a1020acffad7314, ..\interops\RNA-Seq\RNA-seq.Data\FastQ\FastQ.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports SMRUCC.genomics.SequenceModel.FASTA
 
 Namespace Fastaq
@@ -69,6 +70,7 @@ Namespace Fastaq
         ''' <returns></returns>
         ''' <remarks></remarks>
         Public ReadOnly Property Title As String Implements IAbstractFastaToken.Title
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
                 Return SEQ_ID.Identifier
             End Get
