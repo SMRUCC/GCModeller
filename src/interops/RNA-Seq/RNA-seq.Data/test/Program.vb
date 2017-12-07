391bc570ec7c72485ac6d39f43ad616db126e7a9
xieguigang <xie.guigang@gcmodeller.org>
2017-12-07 17:44:36 +0800

    test success

--- src/interops/RNA-Seq/RNA-seq.Data/test/Program.vb
+++ src/interops/RNA-Seq/RNA-seq.Data/test/Program.vb
@@ -9,9 +9,14 @@ Module Program
         Call Stream _
             .ReadAllLines("F:\2017-12-6-16s_test\test\T1-1_combined_R1.fastq") _
             .TrimLowQuality _
-            .TrimShortReads(100) _
+            .TrimShortReads(200) _
             .WriteFastQ("F:\2017-12-6-16s_test\test\T1-1_combined_R1.trim.fastq")
 
+        Call Stream _
+            .ReadAllLines("F:\2017-12-6-16s_test\test\T1-1_combined_R2.fastq") _
+            .TrimLowQuality _
+            .TrimShortReads(200) _
+            .WriteFastQ("F:\2017-12-6-16s_test\test\T1-1_combined_R2.trim.fastq")
 
         Pause()
     End Sub
