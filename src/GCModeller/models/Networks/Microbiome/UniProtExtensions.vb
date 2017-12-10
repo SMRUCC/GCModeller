2687353c05053aead700cde54cbe6ff126c99513
xieguigang <xie.guigang@gcmodeller.org>
2017-12-10 14:02:55 +0800

    improvements on the uniprot taxonomy model data build

--- src/GCModeller/models/Networks/Microbiome/UniProtExtensions.vb
+++ src/GCModeller/models/Networks/Microbiome/UniProtExtensions.vb
@@ -1,12 +1,14 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
-Imports Microsoft.VisualBasic.Language.UnixBash
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Text
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
 Imports SMRUCC.genomics.Assembly.Uniprot.XML
 Imports KO = SMRUCC.genomics.Assembly.KEGG.DBGET.BriteHEntry.KOCatalog
+Imports r = System.Text.RegularExpressions.Regex
 Imports XmlProperty = Microsoft.VisualBasic.Text.Xml.Models.Property
 
 Public Module UniProtExtensions
@@ -30,24 +32,32 @@ Public Module UniProtExtensions
     <Extension> Public Function ScanUniProt(UniProtXml As IEnumerable(Of entry)) As TaxonomyRepository
         ' 因为在这里是处理一个非常大的UniProt注释数据库，所以需要首先做一次扫描
         ' 将需要提取的信息先放到缓存之中
-        Dim cache$ = UniProtXml.ScanInternal
+        Dim tmp$ = App.GetAppSysTempFile(, App.PID)
+        Dim cache = UniProtXml.ScanInternal(tmp)
         Dim model As TaxonomyRepository = ScanModels(cache)
         Return model
     End Function
 
-    Private Function ScanModels(cache As String) As TaxonomyRepository
+    Private Function ScanModels(cache As (KO_list$, taxonomy$)) As TaxonomyRepository
         Dim ko00000 = ko00000Provider()
         Dim list As New List(Of TaxonomyRef)
+        Dim organismKO As New Dictionary(Of String, List(Of String))
+
+        For Each line As String In cache.KO_list.IterateAllLines
+            With line.Split(ASCII.TAB)
+                If Not organismKO.ContainsKey(.First) Then
+                    Call organismKO.Add(.First, New List(Of String))
+                End If
+
+                Call organismKO(.First).Add(.Last)
+            End With
+        Next
 
         ' 之后再将信息提取出来整理为一个XML格式的信息库
-        For Each xml As String In ls - l - r - "*.Xml" <= cache
-            Dim organism As organism = xml.LoadXml(Of organism)
-            Dim KO$() = (xml.TrimSuffix & ".txt") _
-                .ReadAllLines _
-                .Distinct _
-                .Where(Function(s) Not s.StringEmpty) _
-                .OrderBy(Function(id) id) _
-                .ToArray
+        For Each xml As String In r.Split(cache.taxonomy.ReadAllText, blankPattern, RegexOptions.Singleline)
+            Dim organism As organism = xml.LoadFromXml(Of organism)
+            Dim taxon$ = organism.dbReference.id
+            Dim KO As IEnumerable(Of String) = If(organismKO.ContainsKey(taxon), organismKO(taxon), New List(Of String))
             Dim terms As XmlProperty() = KO _
                 .Select(Function(id)
                             If ko00000.ContainsKey(id) Then
@@ -68,7 +78,7 @@ Public Module UniProtExtensions
 
             list += New TaxonomyRef With {
                 .organism = organism,
-                .TaxonID = organism.dbReference.id,
+                .TaxonID = taxon,
                 .genome = New OrthologyTerms With {
                     .Terms = terms
                 }
@@ -80,53 +90,70 @@ Public Module UniProtExtensions
         }
     End Function
 
+    <Extension>
+    Private Iterator Function skipUntil(UniProtXml As IEnumerable(Of entry), acc$) As IEnumerable(Of entry)
+
+    End Function
+
+    Const KO_list$ = "KO.list"
+    Const Taxonomy_data$ = "taxonomy.txt"
+    Const blank$ = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    Const blankPattern$ = "\+{10,}"
+
     ''' <summary>
     ''' 函数返回来的是临时文件夹的路径位置
     ''' </summary>
     ''' <param name="UniProtXml"></param>
+    ''' <param name="tmp">工作区缓存文件夹</param>
     ''' <returns></returns>
     <Extension>
-    Private Function ScanInternal(UniProtXml As IEnumerable(Of entry)) As String
-        Dim tmp$ = App.GetAppSysTempFile(, App.PID)
+    Private Function ScanInternal(UniProtXml As IEnumerable(Of entry), tmp$) As (KO_list As String, taxonomy As String)
+        ' KO 缓存文件示例
+        ' 
+        ' taxonomy_id KO
+        ' taxonomy_id KO
+        ' taxonomy_id KO
+        '
+        ' 
+        Dim taxonomyIndex As New Dictionary(Of String, String)
 
-        For Each protein As entry In UniProtXml
-            Dim taxonomy = protein.organism
-            Dim lineage$ = taxonomy _
-                .lineage _
-                .taxonlist _
-                .Select(AddressOf NormalizePathString) _
-                .JoinBy("/") _
-                .MD5
-            Dim path$
+        Using KO As StreamWriter = $"{tmp}/{KO_list}".OpenWriter,
+            taxo As StreamWriter = $"{tmp}/{Taxonomy_data}".OpenWriter
 
-            With taxonomy
-                Dim name$ = $"[{ .dbReference.id}] { .scientificName.NormalizePathString}"
+            For Each protein As entry In UniProtXml _
+                .Where(Function(org)
+                           Return Not org.organism Is Nothing AndAlso
+                                  Not org.organism.dbReference Is Nothing
+                       End Function)
 
-                With .lineage.taxonlist
-                    path = $"{ .FirstOrDefault }/{ .ElementAtOrDefault(1)}/{lineage}"
-                    path = $"{tmp}/{path}/{name}.XML"
-                End With
-            End With
+                Dim taxonomy As organism = protein.organism
+                Dim taxonID$ = taxonomy.dbReference.id
 
-            Dim KO$() = protein.Xrefs _
-                .TryGetValue("KO") _
-                .SafeQuery _
-                .Select(Function(KEGG) KEGG.id) _
-                .ToArray
+                ' 如果已经存在index了，则不会写入taxo文件之中
+                If Not taxonomyIndex.ContainsKey(taxonID) Then
+                    Call taxo.WriteLine(taxonomy.GetXml)
+                    Call taxo.WriteLine(blank)
+                    Call taxonomyIndex.Add(taxonID, "null")
+                End If
 
-            If path.FileExists Then
-                ' 追加
-                ' 因为append并不会换行，所以在这里需要额外的追加一个LF换行
-                Call (KO _
-                    .JoinBy(ASCII.LF) & vbLf) _
-                    .SaveTo(path.TrimSuffix & ".txt", append:=True, throwEx:=False)
-            Else
-                ' 写入新的数据
-                Call taxonomy.GetXml.SaveTo(path)
-                Call KO.FlushAllLines(path.TrimSuffix & ".txt")
-            End If
-        Next
+                Dim KOlist$() = protein.Xrefs _
+                    .TryGetValue("KO") _
+                    .SafeQuery _
+                    .Select(Function(KEGG) KEGG.id) _
+                    .ToArray
 
-        Return tmp
+                If KOlist.Length > 0 Then
+                    Call KO.WriteLine(
+                        value:=KOlist _
+                            .Select(Function(id)
+                                        Return taxonID & vbTab & id
+                                    End Function) _
+                            .JoinBy(KO.NewLine)
+                    )
+                End If
+            Next
+        End Using
+
+        Return ($"{tmp}/{KO_list}", $"{tmp}/{Taxonomy_data}")
     End Function
 End Module
