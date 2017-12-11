9660440d1b434d29dac0ba08a6d65c6607114a25
xieguigang <xie.guigang@gcmodeller.org>
2017-12-11 14:10:47 +0800

    add mothur OTU fasta tool

---
+++ src/GCModeller/analysis/Metagenome/Metagenome/Mothur/SILVA_OTU.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Text
+Imports SMRUCC.genomics.SequenceModel.FASTA
+
+Public Module SILVA_OTU
+
+    <Extension>
+    Public Function ParseOTUrep(fasta As IEnumerable(Of FastaToken)) As Dictionary(Of String, NamedValue(Of Integer))
+        Dim table As New Dictionary(Of String, NamedValue(Of Integer))
+        Dim OTU$()
+
+        For Each kseq As FastaToken In fasta
+            With kseq.Title.Split(ASCII.TAB)
+                OTU = .Last.Split("|"c)
+                table(.First) = New NamedValue(Of Integer) With {
+                    .Name = OTU(0),
+                    .Value = Val(OTU(1))
+                }
+            End With
+        Next
+
+        Return table
+    End Function
+End Module
---
+++ src/interops/localblast/LocalBLAST/LocalBLAST/BlastOutput/Reader/Blast+/2.6.0+/blastn/BlastnOutputReader.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.Text
+
+Namespace LocalBLAST.BLASTOutput.BlastPlus
+
+    Public Module BlastnOutputReader
+
+        Public Iterator Function RunParser(path$, Optional encoding As Encodings = Encodings.UTF8) As IEnumerable(Of Query)
+
+        End Function
+    End Module
+End Namespace
