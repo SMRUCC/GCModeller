8e3414dae5f0c77593f1baf8d5e0f1614be0bbb2
xieguigang <xie.guigang@gcmodeller.org>
2017-12-11 17:32:51 +0800

    split source file

--- src/GCModeller/analysis/Metagenome/Metagenome/gast/gast_tools.vb
+++ src/GCModeller/analysis/Metagenome/Metagenome/gast/gast_tools.vb
@@ -30,12 +30,9 @@ Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.ComponentModel.Collection
-Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.csv
-Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
 Imports Microsoft.VisualBasic.Language
-Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
 Imports SMRUCC.genomics.Assembly
 Imports SMRUCC.genomics.Assembly.NCBI.Taxonomy
@@ -218,44 +215,4 @@ Namespace gast
 
         End Function
     End Module
-
-    ''' <summary>
-    ''' *.names
-    ''' </summary>
-    Public Class Names : Implements INamedValue
-
-        Public Property Unique As String Implements INamedValue.Key
-        <Ignored>
-        Public Property members As String()
-        Public Property NumOfSeqs As Integer
-        Public Property taxonomy As String
-        Public Property distance As Double
-        Public Property refs As String
-        <Meta>
-        Public Property Composition As Dictionary(Of String, String)
-
-        Public Overrides Function ToString() As String
-            Return Me.GetJson
-        End Function
-    End Class
-
-    Public Class gastOUT : Implements INamedValue
-
-        Public Property read_id As String Implements INamedValue.Key
-        Public Property taxonomy As String
-        Public Property distance As Double
-        Public Property rank As String
-        Public Property refssu_count As String
-        Public Property vote As String
-        Public Property minrank As String
-        Public Property taxa_counts As String
-        Public Property max_pcts As String
-        Public Property na_pcts As String
-        Public Property refhvr_ids As String
-        Public Property counts As Integer
-
-        Public Overrides Function ToString() As String
-            Return Me.GetJson
-        End Function
-    End Class
 End Namespace
---
+++ src/GCModeller/analysis/Metagenome/Metagenome/greengenes/Extensions.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports SMRUCC.genomics.Analysis.Metagenome.gast
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput.BlastPlus
+
+Namespace greengenes
+
+    Public Module Extensions
+
+        <Extension>
+        Public Iterator Function OTUgreengenesTaxonomy(blastn As IEnumerable(Of Query), OTUs As Dictionary(Of String, NamedValue(Of Integer)), taxonomy As Dictionary(Of String, otu_taxonomy)) As IEnumerable(Of gastOUT)
+            For Each query As Query In blastn
+                If Not OTUs.ContainsKey(query.QueryName) Then
+                    Continue For
+                End If
+
+                ' Create an array Of taxonomy objects For all the associated refssu_ids.
+                Dim hits = query _
+                    .SubjectHits _
+                    .Select(Function(h) taxonomy(h.Name)) _
+                    .Select(Function(h)
+                                Return New NamedValue(Of gast.Taxonomy) With {
+                                    .Name = h.ID,
+                                    .Value = New gast.Taxonomy(h.ToString)
+                                }
+                            End Function) _
+                    .ToArray
+            Next
+        End Function
+    End Module
+End Namespace
