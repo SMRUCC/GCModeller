f1269352c4f30ad106313432f04aa231843ac6f2
xieguigang <xie.guigang@gcmodeller.org>
2017-12-12 10:38:19 +0800

    build custom SILVA 16s reference database for bacteria

---
+++ src/GCModeller/analysis/Metagenome/Metagenome/SILVA/SILVABuild.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports SMRUCC.genomics.SequenceModel.FASTA
+
+Public Module SILVABuild
+
+    ''' <summary>
+    ''' 从SILVA序列库之中筛选出细菌或者古生菌的16s序列
+    ''' </summary>
+    ''' <param name="silva"></param>
+    ''' <returns></returns>
+    Public Iterator Function SILVABacteria(silva As IEnumerable(Of FastaToken)) As IEnumerable(Of FastaToken)
+        Dim title$
+        Dim header As NamedValue(Of String)
+
+        For Each seq As FastaToken In silva
+            title = seq.Title
+            header = title.GetTagValue(" ", trim:=True)
+
+            If InStr(header.Value, "Bacteria;", CompareMethod.Text) > 0 Then
+                Yield seq
+            ElseIf InStr(header.Value, "Archaea;", CompareMethod.Text) > 0 Then
+                Yield seq
+            Else
+                ' 不是细菌或者古生菌的16S序列
+                ' 跳过
+            End If
+        Next
+    End Function
+
+
+End Module
--- src/GCModeller/analysis/Metagenome/Metagenome/greengenes/Extensions.vb
+++ src/GCModeller/analysis/Metagenome/Metagenome/greengenes/Extensions.vb
@@ -7,6 +7,14 @@ Namespace greengenes
 
     Public Module Extensions
 
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="blastn"></param>
+        ''' <param name="OTUs"></param>
+        ''' <param name="taxonomy"></param>
+        ''' <returns></returns>
+        ''' <remarks>使用gast最多只能够注释到species，不能够具体的注释到某一个菌株？？</remarks>
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         <Extension>
         Public Function OTUgreengenesTaxonomy(blastn As IEnumerable(Of Query), OTUs As Dictionary(Of String, NamedValue(Of Integer)), taxonomy As Dictionary(Of String, otu_taxonomy)) As IEnumerable(Of gastOUT)
