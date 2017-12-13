0ab6bb54fa669a4607f455d497f28eebd2dbb88c
xieguigang <xie.guigang@gcmodeller.org>
2017-12-13 20:54:55 +0800

    add uniprot taxonomy reference data query API

--- src/GCModeller/models/Networks/Microbiome/TaxonomyRef.vb
+++ src/GCModeller/models/Networks/Microbiome/TaxonomyRef.vb
@@ -47,12 +47,16 @@ Public Class TaxonomyRef : Implements IKeyedEntity(Of String)
     End Function
 End Class
 
+''' <summary>
+''' 使用NCBI的物种编号作为主键的
+''' </summary>
 Public Class TaxonomyRepository : Implements IRepositoryRead(Of String, TaxonomyRef)
 
     Dim taxonIDtable As Dictionary(Of String, TaxonomyRef)
 
     <XmlElement("taxonomy")>
     Public Property Taxonomy As TaxonomyRef()
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Get
             Return taxonIDtable.Values.ToArray
         End Get
@@ -64,19 +68,34 @@ Public Class TaxonomyRepository : Implements IRepositoryRead(Of String, Taxonomy
         End Set
     End Property
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function Selects(range As Taxonomy) As IEnumerable(Of TaxonomyRef)
+        Return taxonIDtable _
+            .Values _
+            .SelectByTaxonomyRange(
+                ref:=range,
+                getTaxonomy:=Function(genome) genome.TaxonomyString,
+                getValue:=Function(genome) genome
+            )
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function Exists(key As String) As Boolean Implements IRepositoryRead(Of String, TaxonomyRef).Exists
-        Throw New NotImplementedException()
+        Return taxonIDtable.ContainsKey(key)
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetByKey(key As String) As TaxonomyRef Implements IRepositoryRead(Of String, TaxonomyRef).GetByKey
-        Throw New NotImplementedException()
+        Return taxonIDtable(key)
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetWhere(clause As Func(Of TaxonomyRef, Boolean)) As IReadOnlyDictionary(Of String, TaxonomyRef) Implements IRepositoryRead(Of String, TaxonomyRef).GetWhere
-        Throw New NotImplementedException()
+        Return taxonIDtable.Values.Where(clause).ToDictionary(Function(taxon) taxon.TaxonID)
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetAll() As IReadOnlyDictionary(Of String, TaxonomyRef) Implements IRepositoryRead(Of String, TaxonomyRef).GetAll
-        Throw New NotImplementedException()
+        Return New Dictionary(Of String, TaxonomyRef)(taxonIDtable)
     End Function
 End Class
