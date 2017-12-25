e66c95e2cbce50a9a6698b1c50bf73e718a7e42a
xieguigang <xie.guigang@gcmodeller.org>
2017-12-25 21:16:46 +0800

    improvements on the code style

---
+++ src/GCModeller/core/Bio.Assembly/test/TaxonomyTreeTest.vb
@@ -0,0 +1,11 @@
+﻿Imports SMRUCC.genomics.Assembly.NCBI.Taxonomy
+
+Module TaxonomyTreeTest
+    Sub Main()
+        Dim tree As New NcbiTaxonomyTree("T:\Resources\NCBI_taxonomy")
+
+        Dim nodes = tree.GetAscendantsWithRanksAndNames(526962, only_std_ranks:=True)
+
+        Pause()
+    End Sub
+End Module
