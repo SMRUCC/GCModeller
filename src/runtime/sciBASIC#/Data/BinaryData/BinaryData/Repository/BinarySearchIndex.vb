d877077caeaf3e16f42de20f0a9841e6c7c45c66
xieguigang <xie.guigang@gcmodeller.org>
2017-12-28 13:50:35 +0800

    add dev note

---
+++ src/runtime/sciBASIC#/Data/BinaryData/BinaryData/Repository/BinarySearchIndex.vb
@@ -0,0 +1,20 @@
+﻿''' <summary>
+''' 将二叉树序列化为二进制文件作为索引文件
+''' </summary>
+Public Class BinarySearchIndex
+
+End Class
+
+Public Class Index
+
+    ''' <summary>
+    ''' 索引关键词
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Key As String
+    ''' <summary>
+    ''' 在数据文件之中的偏移量
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Offset As Long
+End Class
