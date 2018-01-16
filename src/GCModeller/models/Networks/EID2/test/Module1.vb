fb995ed2b2f42ae9a88ca8c3677e54e7cb5515f4
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-17 02:31:39 +0800

    run test

--- src/GCModeller/models/Networks/EID2/test/Module1.vb
+++ src/GCModeller/models/Networks/EID2/test/Module1.vb
@@ -3,6 +3,8 @@
     Sub Main()
         Dim nnn = EID2.Interaction.BuildSharedPathogens(EID2.Interaction.Load("D:\OneDrive\2018-1-15\associations.csv", cargo:="vVirusNameCorrected", carrier:="hHostNameFinal"))
 
+        Call nnn.Save("D:\OneDrive\2018-1-15\net")
+
         Pause()
     End Sub
 
