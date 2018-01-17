aa5d8ee1501fd7c0bf0f094a1ef78933b5e50c39
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-17 21:47:31 +0800

    add cutoff

--- src/GCModeller/models/Networks/EID2/test/Module1.vb
+++ src/GCModeller/models/Networks/EID2/test/Module1.vb
@@ -3,7 +3,7 @@
     Sub Main()
         Dim nnn = EID2.Interaction.BuildSharedPathogens(
             EID2.Interaction.Load("D:\OneDrive\2018-1-15\associations.csv", cargo:="vVirusNameCorrected", carrier:="hHostNameFinal"),
-            cut:=1)
+            cut:=2)
 
         Call nnn.Save("D:\OneDrive\2018-1-15\net")
 
