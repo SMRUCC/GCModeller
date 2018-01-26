5f6219bc8edc3358f19ab2741a7fca35c6ba17ca
xieguigang <xie.guigang@gcmodeller.org>
2018-01-26 15:34:17 +0800

    add javascript helper

--- src/runtime/httpd/WebCloud/SMRUCC.WebCloud.highcharts/Javascript.vb
+++ src/runtime/httpd/WebCloud/SMRUCC.WebCloud.highcharts/Javascript.vb
@@ -1,32 +1,33 @@
 ﻿#Region "Microsoft.VisualBasic::16dcaeab0a827d50b8714d72412f2074, ..\httpd\WebCloud\SMRUCC.WebCloud.highcharts\Javascript.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports System.Text
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.WebCloud.highcharts.PieChart
 
@@ -57,5 +58,20 @@ Public Module Javascript
             .Select(Function(item) {CObj(item.Key), item.Value}) _
             .ToArray
     End Function
+
+    <Extension>
+    Public Sub WriteHighchartsHTML(template As StringBuilder, name$, javascript$, div$, Optional style$ = "height: 450px")
+        Dim html$ =
+            (<p>
+                 <div id=<%= div %> style=<%= style %>></div>
+                 <script type="text/javascript">
+                     <%= javascript %>
+                 </script>
+             </p>).ToString _
+                  .Replace("&lt;", "<") _
+                  .Replace("&gt;", ">")
+
+        Call template.Replace(name, html)
+    End Sub
 End Module
 
