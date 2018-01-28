8f1f74fcd88e7b36541b4016b177346f133131cb
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-28 21:28:45 +0800

    reversed map

--- src/interops/localblast/CLI_tools/CLI/BBH/UniProtTools.vb
+++ src/interops/localblast/CLI_tools/CLI/BBH/UniProtTools.vb
@@ -6,23 +6,23 @@ Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
 Partial Module CLI
 
     <ExportAPI("/UniProt.bbh.mappings")>
-    <Usage("/UniProt.bbh.mappings /in <bbh.csv> [/out <mappings.txt>]")>
+    <Usage("/UniProt.bbh.mappings /in <bbh.csv> [/reverse /out <mappings.txt>]")>
     Public Function UniProtBBHMapTable(args As CommandLine) As Integer
         Dim in$ = args <= "/in"
         Dim out$ = args("/out") Or $"{[in].TrimSuffix}.mappings.txt"
         Dim mappings = [in].LoadCsv(Of BiDirectionalBesthit)
+        Dim reversed As Boolean = args("/reverse")
         Dim table As Dictionary(Of String, String) = mappings _
+            .Where(Function(query)
+                       Return Not (query.HitName.StringEmpty OrElse query.HitName.TextEquals(HITS_NOT_FOUND))
+                   End Function) _
             .ToDictionary(Function(query)
                               Return query.QueryName
                           End Function,
                           Function(query)
-                              If query.HitName.StringEmpty OrElse query.HitName.TextEquals(HITS_NOT_FOUND) Then
-                                  Return ""
-                              Else
-                                  Return query.HitName.Split("|"c)(1)
-                              End If
+                              Return query.HitName.Split("|"c)(1)
                           End Function)
 
-        Return table.Tsv(out).CLICode
+        Return table.Tsv(out,, reversed:=reversed).CLICode
     End Function
 End Module
