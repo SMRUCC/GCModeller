14d9e9d73b95658a261ac3acc704ab527d6d8dff
谢桂纲 <xie.guigang@gcmodeller.org>
2018-02-08 00:00:44 +0800

    update mysqli runtime

---
+++ src/runtime/httpd/httpd/test.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.Net.Http
+Imports SMRUCC.WebCloud.HTTPInternal
+
+Module test
+
+    Sub Main()
+
+        Call FaviconZip.UnzipStream.FlushStream("./test.ico")
+
+        Pause()
+    End Sub
+End Module
