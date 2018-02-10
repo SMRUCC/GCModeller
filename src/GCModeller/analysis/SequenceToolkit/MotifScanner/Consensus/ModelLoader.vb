333953c172e411f6e02db9b3af30ab85113ba995
谢桂纲 <xie.guigang@gcmodeller.org>
2018-02-10 23:10:17 +0800

    Load kegg organism model

---
+++ src/GCModeller/analysis/SequenceToolkit/MotifScanner/Consensus/ModelLoader.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.Data
+Imports Microsoft.VisualBasic.Language.UnixBash
+
+Public Module ModelLoader
+
+    ''' <summary>
+    ''' Load kegg organism model
+    ''' </summary>
+    ''' <param name="repo"></param>
+    ''' <returns></returns>
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    <Extension>
+    Public Function LoadKEGGModels(repo As String) As Dictionary(Of String, OrganismModel)
+        Return (ls - l - r - "*.Xml" <= repo) _
+            .Select(AddressOf LoadXml(Of OrganismModel)) _
+            .ToDictionary(Function(org)
+                              Return org _
+                                  .organism _
+                                  .Taxonomy _
+                                  .GetTagValue(":", trim:=True) _
+                                  .Value
+                          End Function)
+    End Function
+End Module
