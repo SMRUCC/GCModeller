bd656101cb7cbebb2958dae25de01764649361e6
xieguigang <xie.guigang@gcmodeller.org>
2018-02-12 16:32:50 +0800

    fix for test

--- src/GCModeller/analysis/SequenceToolkit/MSA/test/Module1.vb
+++ src/GCModeller/analysis/SequenceToolkit/MSA/test/Module1.vb
@@ -12,7 +12,7 @@ CCTGTAGGAACAG".lTokens
         Dim matrix = "D:\GCModeller\src\GCModeller\analysis\SequenceToolkit\MSA\Matrix.txt".ReadAllLines.Select(Function(l) l.Replace(" "c, "").ToArray).ToArray
         Dim msa = seq.MultipleAlignment(matrix)
 
-        Call msa.SaveTo("./msa.txt")
+        Call msa.ToFasta.Save("./msa.txt")
         Call Console.WriteLine(msa)
 
         Pause()
