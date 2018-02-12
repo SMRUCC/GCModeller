5860970dad12160c5eca45b3075f309e4f1a3f25
xieguigang <xie.guigang@gcmodeller.org>
2018-02-12 15:08:53 +0800

    add test

---
+++ src/GCModeller/analysis/SequenceToolkit/MSA/Extensions.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.SequenceModel.FASTA
+
+Public Module Extensions
+
+    <Extension>
+    Public Function MultipleAlignment(input As IEnumerable(Of String)) As String()
+        Return input.Select(Function(seq) New FastaSeq With {.SequenceData = seq}).MultipleAlignment(0)
+    End Function
+End Module
---
+++ src/GCModeller/analysis/SequenceToolkit/MSA/test/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports SMRUCC.genomics.Analysis.SequenceTools.MSA
+
+Module Module1
+
+    Sub Main()
+        Dim seq$() = "GATGTGCCG
+GATGTGCAG
+CCGCTAGCAG
+CCTGCTGCAG
+CCTGTAGG".lTokens
+
+        Dim msa = seq.MultipleAlignment()
+
+        Pause()
+    End Sub
+End Module
