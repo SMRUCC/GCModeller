85a443fdb984f1f5cb431fcd54fe70232f82ae20
谢桂纲 <xie.guigang@gcmodeller.org>
2018-02-27 18:33:33 +0800

    fix for cli help format bugs

--- src/GCModeller/CLI_tools/eggHTS/CLI/Visualize/Clustering.vb
+++ src/GCModeller/CLI_tools/eggHTS/CLI/Visualize/Clustering.vb
@@ -1,41 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::131077a12cef3c7ca4e4b64c40f3c271, CLI_tools\eggHTS\CLI\Visualize\Clustering.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CLI
-    ' 
-    '     Function: DEPHeatmap3D, DEPKmeansScatter2D, MatrixClustering, PfamStringEnrichment
-    ' 
-    ' /********************************************************************************/
+' Module CLI
+' 
+'     Function: DEPHeatmap3D, DEPKmeansScatter2D, MatrixClustering, PfamStringEnrichment
+' 
+' /********************************************************************************/
 
 #End Region
 
