d77580ddde402c4801084c93adcdf95fc4a23e2a
谢桂纲 <xie.guigang@gcmodeller.org>
2018-03-03 17:43:39 +0800

    seeds filter not working???

--- src/GCModeller/analysis/SequenceToolkit/MotifScanner/Consensus/DATA.vb
+++ src/GCModeller/analysis/SequenceToolkit/MotifScanner/Consensus/DATA.vb
@@ -21,6 +21,11 @@ Public Class PopulatorParameter
     ''' </summary>
     ''' <returns></returns>
     Public Property seedingCutoff As Double
+    ''' <summary>
+    ''' [0, 1]，表示种子至少要在这个属性值所表示的百分比数量上的原始序列出现
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property seedOccurances As Double
     Public Property ScanMinW As Integer
     Public Property ScanCutoff As Double
 
@@ -34,7 +39,8 @@ Public Class PopulatorParameter
             .maxW = 14,
             .seedingCutoff = 0.9,
             .ScanCutoff = 0.6,
-            .ScanMinW = 6
+            .ScanMinW = 6,
+            .seedOccurances = 0.6
         }
     End Function
 
