673d946161c7c4614ae6f156f57746420c90bf01
谢桂纲 <xie.guigang@gcmodeller.org>
2018-03-10 23:35:04 +0800

    add models

---
+++ src/workbench/SeqFeature/SeqFeature/ASCIIViewer.vb
@@ -0,0 +1,27 @@
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
+
+Public Module ASCIIViewer
+
+    '                           ProtK 
+    'Ch_lo_Pn1.3_Pn2_ProtK_Therm| 
+    '           Glu_ProtK_Staph|| 
+    '            AspGluN_ProtK||| 
+    '  ArgC_Clost_Therm_Tryps|||| 
+    '        Glu_ProtK_Staph||||| 
+    '               AspGluN|||||| 
+
+    '                     ||||||| 
+    '                     SERVELAT
+    '                 1   --------   8
+
+    <Extension>
+    Public Sub DisplayOn(sites As IEnumerable(Of Site), seq$, Optional dev As TextWriter = Nothing)
+
+    End Sub
+End Module
+
+Public Class Site
+    Public Property Name As String
+    Public Property Left As Integer
+End Class
