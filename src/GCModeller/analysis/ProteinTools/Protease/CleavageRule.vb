803523a90d57eeb36e6baaf5d2d8939fe8dcb8b4
谢桂纲 <xie.guigang@gcmodeller.org>
2018-03-11 11:29:23 +0800

    CleavageRule model

---
+++ src/GCModeller/analysis/ProteinTools/Protease/CleavageRule.vb
@@ -0,0 +1,26 @@
+﻿Public Class CleavageRule
+
+    ''' <summary>
+    ''' FullName
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Name As String
+    ''' <summary>
+    ''' BriefName
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Protease As String
+
+#Region "in C-terminal direction"
+    Public Property P4 As String
+    Public Property P3 As String
+    Public Property P2 As String
+    Public Property P1 As String
+#End Region
+
+#Region "in C-terminal direction"
+    Public Property P1C As String
+    Public Property P2C As String
+#End Region
+
+End Class
