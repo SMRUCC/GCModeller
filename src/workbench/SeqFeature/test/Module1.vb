d89e2e4e3612d4dedcc1b7ab9c7548beab35f41e
谢桂纲 <xie.guigang@gcmodeller.org>
2018-03-11 12:19:52 +0800

    test success

--- src/workbench/SeqFeature/test/Module1.vb
+++ src/workbench/SeqFeature/test/Module1.vb
@@ -1,10 +1,13 @@
 ﻿Imports Microsoft.VisualBasic.Data.csv
 Imports SMRUCC.genomics.GCModeller.Workbench.SeqFeature
+Imports SMRUCC.genomics.SequenceModel.FASTA
 Imports SMRUCC.genomics.SequenceModel.Protease
 
 Module Module1
 
     Sub Main()
+
+        Call ruleTest()
 
         Call {New CleavageRule}.SaveTo("./CleavageRule.csv")
 
@@ -12,6 +15,19 @@ Module Module1
         Pause()
 
         Call viewerTest()
+    End Sub
+
+    Sub ruleTest()
+
+        Dim rules = "E:\GCModeller\src\GCModeller\analysis\ProteinTools\CleavageRule.csv".LoadCsv(Of CleavageRule)
+        Dim seq = New FastaSeq With {.SequenceData = "SERVELAT"}
+
+        Dim sites = seq.RunTest(proteases:=rules).ToArray
+
+        Call "output result".__DEBUG_ECHO
+        Call sites.DisplayOn(seq.SequenceData)
+
+        Pause()
     End Sub
 
     Sub viewerTest()
