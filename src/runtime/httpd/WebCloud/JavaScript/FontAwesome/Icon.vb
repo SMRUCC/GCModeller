ece7c006789f0176ac117bb233cade5ce8a877a5
xieguigang <xie.guigang@gcmodeller.org>
2018-03-21 11:29:27 +0800

    add font awsome library

---
+++ src/runtime/httpd/WebCloud/JavaScript/FontAwesome/Icon.vb
@@ -0,0 +1,33 @@
+﻿Imports System.ComponentModel
+Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging
+
+Public Class Icon
+
+    Public Property Style As Types
+    Public Property Name As String
+    Public Property Color As Color
+
+    Public ReadOnly Property Preview As String
+        Get
+            Return ToString()
+        End Get
+    End Property
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Overrides Function ToString() As String
+        If Color.IsEmpty Then
+            Return (<li class=<%= Style.Description & " " & Strings.LCase(Name) %>></li>).ToString
+        Else
+            Return (<li class=<%= Style.Description & " " & Strings.LCase(Name) %> style=<%= $"color:{Color.ToHtmlColor}" %>></li>).ToString
+        End If
+    End Function
+
+End Class
+
+Public Enum Types
+    <Description("fas")> Solid
+    <Description("far")> Regular
+    <Description("fal")> Light
+End Enum
