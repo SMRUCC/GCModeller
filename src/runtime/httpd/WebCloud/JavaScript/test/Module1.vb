2880dfd7089578d1d2b2be88ac6b7675391d58af
xieguigang <xie.guigang@gcmodeller.org>
2018-03-21 11:31:19 +0800

    config for test

--- src/runtime/httpd/WebCloud/JavaScript/test/Module1.vb
+++ src/runtime/httpd/WebCloud/JavaScript/test/Module1.vb
@@ -1,7 +1,12 @@
-﻿Module Module1
+﻿Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS.Parser
+
+Module Module1
 
     Sub Main()
+        Dim cssFile = "C:\Users\administrator\Desktop\fontawesome.css"
+        Dim css = CssParser.GetTagWithCSS(cssFile.ReadAllText)
 
+
+        Pause()
     End Sub
-
 End Module
