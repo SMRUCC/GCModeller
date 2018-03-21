c6c053035ea37008028e8ca8dc27db1c8b14f7de
xieguigang <xie.guigang@gcmodeller.org>
2018-03-21 11:38:58 +0800

    parse icons from css file

--- src/runtime/httpd/WebCloud/JavaScript/test/Module1.vb
+++ src/runtime/httpd/WebCloud/JavaScript/test/Module1.vb
@@ -4,8 +4,8 @@ Module Module1
 
     Sub Main()
         Dim cssFile = "C:\Users\administrator\Desktop\fontawesome.css"
-        Dim css = CssParser.GetTagWithCSS(cssFile.ReadAllText, selectorFilter:="\.fa[-]")
-
+        Dim css As CSSFile = CssParser.GetTagWithCSS(cssFile.ReadAllText, selectorFilter:="\.fa[-]")
+        Dim icons = css.Selectors.Values.Where(Function(s) s.HasProperty("content")).ToDictionary
 
         Pause()
     End Sub
