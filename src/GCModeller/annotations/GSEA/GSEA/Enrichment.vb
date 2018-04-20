4160c1c239f8200c97a7304810e1cd6960862612
谢桂纲 <xie.guigang@gcmodeller.org>
2018-04-20 20:08:01 +0800

    add GSEA models

---
+++ src/GCModeller/annotations/GSEA/GSEA/Enrichment.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module Enrichment
+
+    <Extension>
+    Public Function Enrichment(genome As Genome, list As IEnumerable(Of String)) As IEnumerable(Of EnrichmentResult)
+
+    End Function
+End Module
---
+++ src/GCModeller/annotations/GSEA/GSEA/EnrichmentResult.vb
@@ -0,0 +1,8 @@
+﻿Public Class EnrichmentResult
+
+    Public Property Term As String
+    Public Property Enriched As String()
+    Public Property Score As Double
+    Public Property Pvalue As Double
+
+End Class
