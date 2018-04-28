a5f75faa55516afa7077e1fdeff9f415627e5d6b
谢桂纲 <xie.guigang@gcmodeller.org>
2018-04-29 03:02:34 +0800

    add xml data model source comments

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/Map.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/Map.vb
@@ -1,50 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::f93a515dc3cafda5160bf9215123c580, core\Bio.Assembly\Assembly\KEGG\Web\Map\Map.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Map
-    ' 
-    '         Properties: Areas, ID, Name, PathwayImage, URL
-    ' 
-    '         Function: GetEntryInfo, GetImage, GetMembers, ParseHTML, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Map
+' 
+'         Properties: Areas, ID, Name, PathwayImage, URL
+' 
+'         Function: GetEntryInfo, GetImage, GetMembers, ParseHTML, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
 Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
@@ -60,7 +61,8 @@ Imports r = System.Text.RegularExpressions.Regex
 Namespace Assembly.KEGG.WebServices
 
     <XmlRoot("Map", [Namespace]:="http://GCModeller.org/core/KEGG/KGML_map.xsd")>
-    Public Class Map : Implements INamedValue
+    Public Class Map : Inherits XmlDataModel
+        Implements INamedValue
 
         <XmlAttribute>
         Public Property ID As String Implements IKeyedEntity(Of String).Key
@@ -147,7 +149,7 @@ Namespace Assembly.KEGG.WebServices
                 img = vbLf & img _
                     .LineTokens _
                     .Select(Function(s) New String(" ", 4) & s) _
-                    .JoinBy(ASCII.LF)
+                    .JoinBy(ASCII.LF) & vbLf
             End With
 
             Dim info As NamedValue(Of String) = GetEntryInfo(html)
