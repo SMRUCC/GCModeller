c04ec56dff3aeea4cc6e7086131e0afc1147db49
谢桂纲 <xie.guigang@gcmodeller.org>
2018-04-30 14:23:29 +0800

    create compound links

--- src/GCModeller/core/Bio.Repository/KEGG/ReactionRepository/ReactionRepository.vb
+++ src/GCModeller/core/Bio.Repository/KEGG/ReactionRepository/ReactionRepository.vb
@@ -1,50 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::18f2943118cf606493c2c32a5ebeedaf, core\Bio.Repository\KEGG\ReactionRepository\ReactionRepository.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class ReactionRepository
-    ' 
-    '     Properties: MetabolicNetwork
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: Enzymetic, Exists, GetAll, GetByKey, GetByKOMatch
-    '               GetWhere, ScanModel
-    ' 
-    ' /********************************************************************************/
+' Class ReactionRepository
+' 
+'     Properties: MetabolicNetwork
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: Enzymetic, Exists, GetAll, GetByKey, GetByKOMatch
+'               GetWhere, ScanModel
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Language.UnixBash
@@ -53,7 +54,8 @@ Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
 ''' <summary>
 ''' KEGG的参考代谢反应模型库，封装了对<see cref="Reaction"/>的对象查询操作
 ''' </summary>
-Public Class ReactionRepository : Implements IRepositoryRead(Of String, Reaction)
+Public Class ReactionRepository : Inherits XmlDataModel
+    Implements IRepositoryRead(Of String, Reaction)
 
     Dim table As Dictionary(Of String, Reaction)
 
