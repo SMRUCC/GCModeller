ee420a2687c146e02e5eaaeb4ff6e7785d821dde
xieguigang <xie.guigang@gcmodeller.org>
2018-05-09 09:54:28 +0800

    add test app

---
+++ src/runtime/httpd/httpd/test/TestWebApp.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports SMRUCC.WebCloud.HTTPInternal.AppEngine
+Imports SMRUCC.WebCloud.HTTPInternal.AppEngine.APIMethods
+Imports SMRUCC.WebCloud.HTTPInternal.AppEngine.APIMethods.Arguments
+Imports SMRUCC.WebCloud.HTTPInternal.Platform
+
+<[Namespace]("test")>
+Public Class ClassTestWebApp : Inherits WebApp
+
+    Public Sub New(main As PlatformEngine)
+        MyBase.New(main)
+    End Sub
+
+    <ExportAPI("/test/get.vbs")>
+    <[GET]>
+    Public Function testGET(request As HttpRequest, response As HttpResponse) As Integer
+        Dim args = request.URLParameters.ToDictionary
+
+        Call response.WriteJSON(args, indent:=True)
+
+        Return True
+    End Function
+End Class
