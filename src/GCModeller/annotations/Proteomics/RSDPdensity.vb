7daef918949ea4e6f3baf04ea6996c5075210b3f
谢桂纲 <xie.guigang@gcmodeller.org>
2018-06-04 00:54:12 +0800

    ??? Process is terminated due to StackOverflowException.

--- src/GCModeller/annotations/Proteomics/RSDPdensity.vb
+++ src/GCModeller/annotations/Proteomics/RSDPdensity.vb
@@ -76,7 +76,7 @@ Public Module RSDPdensity
     <Extension>
     Public Function RSDPdensity(points As IEnumerable(Of PointF),
                                 Optional size$ = "1800,1600",
-                                Optional padding$ = g.DefaultUltraLargePadding,
+                                Optional padding$ = g.DefaultLargerPadding,
                                 Optional bg$ = "white",
                                 Optional schema$ = "Jet",
                                 Optional RSD# = 1,
