5f0682a26993402af8422a398afdb66be341c41b
谢桂纲 <xie.guigang@gcmodeller.org>
2018-07-13 21:15:13 +0800

    fix reference errors

--- src/GCModeller/CLI_tools/GCModeller/CLI/DataVisualization.vb
+++ src/GCModeller/CLI_tools/GCModeller/CLI/DataVisualization.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::ca549929501f7ccca5a5a432af70a186, CLI_tools\GCModeller\CLI\DataVisualization.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CLI
-    ' 
-    '     Function: DrawClustalW, DrawMultipleAlignments, GetSubsetID, PlotGC, SelfMPAlignmentAsMAT
-    '               SelfOverviewAsMAT
-    ' 
-    ' /********************************************************************************/
+' Module CLI
+' 
+'     Function: DrawClustalW, DrawMultipleAlignments, GetSubsetID, PlotGC, SelfMPAlignmentAsMAT
+'               SelfOverviewAsMAT
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -57,8 +57,7 @@ Imports SMRUCC.genomics.Interops.Visualize.Phylip
 Imports SMRUCC.genomics.SequenceModel
 Imports SMRUCC.genomics.SequenceModel.FASTA
 Imports SMRUCC.genomics.Visualize
-Imports SMRUCC.genomics.Visualize.ComparativeAlignment
-Imports SMRUCC.genomics.Visualize.ComparativeGenomics.ModelAPI
+Imports SMRUCC.genomics.Visualize.SyntenyVisualize.ComparativeAlignment
 
 Partial Module CLI
 
--- src/GCModeller/CLI_tools/Map/CLI/BlastAlignment.vb
+++ src/GCModeller/CLI_tools/Map/CLI/BlastAlignment.vb
@@ -1,41 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::e37398d0aab4ffefd4590cdc8afbb187, CLI_tools\Map\CLI\BlastAlignment.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CLI
-    ' 
-    '     Function: BBHVisual, BlastnVisualizeWebResult
-    ' 
-    ' /********************************************************************************/
+' Module CLI
+' 
+'     Function: BBHVisual, BlastnVisualizeWebResult
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -58,8 +58,8 @@ Imports SMRUCC.genomics.Interops.NCBI.Extensions.NCBIBlastResult
 Imports SMRUCC.genomics.SequenceModel
 Imports SMRUCC.genomics.SequenceModel.FASTA
 Imports SMRUCC.genomics.Visualize
-Imports SMRUCC.genomics.Visualize.ComparativeGenomics.ModelAPI
 Imports SMRUCC.genomics.Visualize.NCBIBlastResult
+Imports SMRUCC.genomics.Visualize.SyntenyVisualize.ComparativeGenomics.ModelAPI
 
 Partial Module CLI
 
