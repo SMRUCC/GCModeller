c0ba7b13aae4a0bb53c9fb0b5ab352611cdd43bc
xieguigang <xie.guigang@gcmodeller.org>
2018-08-15 12:19:13 +0800

    why duplicated name?????

--- src/runtime/httpd/javascript/dotnet/dot-network/network.ts
+++ src/runtime/httpd/javascript/dotnet/dot-network/network.ts
@@ -1,85 +1,107 @@
 ﻿module network {
 
-    function readAttr(x: HTMLElement, name: string, Default: string): string {
-        return x.getAttribute(name) || Default;
+    export class dotCanvasSettings {
+        public l: number;
+        public z: number;
+        public o: number;
+        public c: string;
+        public n: number;
     }
 
-    function getTag(tagName: string): NodeListOf<Element> {
-        return document.getElementsByTagName(tagName);
+    function getTag(tagName: string): Element {
+        return document.getElementsByTagName(tagName)[0];
     }
 
-    function load(): networkCanvasSettings {
-        var nodes = getTag("script");
-        var n: number = nodes.length;
-        var last = nodes[n - 1];
-
-        return <networkCanvasSettings>{
-            l: n,
-            z: <number><any>readAttr(<HTMLElement>last, "zIndex", "-1"),
-            o: <number><any>readAttr(<HTMLElement>last, "opacity", "0.5"),
-            c: readAttr(<HTMLElement>last, "color", "0,0,0"),
-            n: <number><any>readAttr(<HTMLElement>last, "count", "99")
-        }
-    }
-
-    function k() {
-        r = u.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        n = u.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    function canvasResize() {
+        r = uCanvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        n = uCanvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     }
 
     function b() {
-        e.clearRect(0, 0, r, n);
-        var w = [f].concat(t);
-        var x, v, A, B, z, y; t.forEach(
+        var w = [f].concat(dots);
+        var x, v, A, B, z, y;
+
+        uContext.clearRect(0, 0, r, n);
+        dots.forEach(
             function (i) {
-                i.x += i.xa, i.y += i.ya, i.xa *= i.x > r || i.x < 0 ? -1 : 1, i.ya *= i.y > n || i.y < 0 ? -1 : 1, e.fillRect(i.x - 0.5, i.y - 0.5, 1, 1);
+                i.x += i.xa, i.y += i.ya, i.xa *= i.x > r || i.x < 0 ? -1 : 1, i.ya *= i.y > n || i.y < 0 ? -1 : 1, uContext.fillRect(i.x - 0.5, i.y - 0.5, 1, 1);
                 for (v = 0; v < w.length; v++) {
                     x = w[v];
                     if (i !== x && null !== x.x && null !== x.y) {
                         B = i.x - x.x, z = i.y - x.y, y = B * B + z * z;
-                        y < x.max && (x === f && y >= x.max / 2 && (i.x -= 0.03 * B, i.y -= 0.03 * z), A = (x.max - y) / x.max, e.beginPath(), e.lineWidth = A / 2, e.strokeStyle = "rgba(" + s.c + "," + (A + 0.2) + ")", e.moveTo(i.x, i.y), e.lineTo(x.x, x.y), e.stroke())
+                        y < x.max && (x === f && y >= x.max / 2 && (i.x -= 0.03 * B, i.y -= 0.03 * z), A = (x.max - y) / x.max, uContext.beginPath(), uContext.lineWidth = A / 2, uContext.strokeStyle = "rgba(" + setting.c + "," + (A + 0.2) + ")", uContext.moveTo(i.x, i.y), uContext.lineTo(x.x, x.y), uContext.stroke())
                     }
                 } w.splice(w.indexOf(i), 1)
-            }), m(b)
+            }), runFrame(b)
     }
 
-    var u = document.createElement("canvas");
-    var e = u.getContext("2d");
-    var f = {
+    var uCanvas: HTMLCanvasElement = document.createElement("canvas");
+    var uContext: CanvasRenderingContext2D = uCanvas.getContext("2d");
+    var f: dot = <dot>{
         x: null, y: null, max: 20000
     }
-    var r;
-    var t: dot[] = [];
-    var n;
-    var s: networkCanvasSettings = load();
-    var m = window.requestAnimationFrame || window.webkitRequestAnimationFrame || (<any>window).mozRequestAnimationFrame || (<any>window).oRequestAnimationFrame || (<any>window).msRequestAnimationFrame || function (i) {
-        window.setTimeout(i, 1000 / 45)
+    var r: number;
+    var dots: dot[] = [];
+    var n: number;
+    var setting: dotCanvasSettings;
+    var runFrame: (callback: FrameRequestCallback) => number
+
+    function defaultCallback(callback: FrameRequestCallback): number {
+        window.setTimeout(callback, 1000 / 45);
+        return 0;
     }
 
-    export function run() {
-        var c = "c_n" + s.l;
-        var a = Math.random;
+    function registerDevice() {
+        runFrame = window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            (<any>window).mozRequestAnimationFrame ||
+            (<any>window).oRequestAnimationFrame ||
+            (<any>window).msRequestAnimationFrame ||
+            defaultCallback;
 
-        u.id = c;
-        u.style.cssText = "position:fixed;top:0;left:0;z-index:" + s.z + ";opacity:" + s.o;
-        getTag("body")[0].appendChild(u);
-
-        k(), window.onresize = k;
-
+        window.onresize = canvasResize;
         window.onmousemove = function (i: MouseEvent) {
             i = i || <MouseEvent>window.event;
             f.x = i.clientX;
             f.y = i.clientY;
-        }
-
+        };
         window.onmouseout = function () {
             f.x = null;
             f.y = null;
         };
+    }
 
-        for (var p = 0; s.n > p; p++) {
-            var h = a() * r, g = a() * n, q = 2 * a() - 1, d = 2 * a() - 1;
-            t.push(<dot>{ x: h, y: g, xa: q, ya: d, max: 10000 })
+    export function run(settings: dotCanvasSettings = <dotCanvasSettings>{
+        l: n,
+        z: -1,
+        o: 0.9,
+        c: "0,104,183",
+        n: 399
+    }) {
+
+        var c = "c_n" + settings.l;
+
+        setting = settings;
+        uCanvas.id = c;
+        uCanvas.style.cssText = `position:fixed; top:0; left:0; z-index: ${settings.z}; opacity: ${settings.o}`;
+        getTag("body").appendChild(uCanvas);
+
+        canvasResize();
+        registerDevice();
+
+        for (var p: number = 0; settings.n > p; p++) {
+            var h = Math.random() * r;
+            var g = Math.random() * n;
+            var q = 2 * Math.random() - 1;
+            var d = 2 * Math.random() - 1;
+
+            dots.push(<dot>{
+                x: h,
+                y: g,
+                xa: q,
+                ya: d,
+                max: 10000
+            })
         }
 
         setTimeout(function () { b() }, 100)
@@ -92,14 +114,6 @@
         public ya: number;
         public max: number;
     }
-}
-
-class networkCanvasSettings {
-    public l: number;
-    public z: number;
-    public o: number;
-    public c: string;
-    public n: number;
 }
 
 network.run();
