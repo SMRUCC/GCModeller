6ae929183581d08e1930edf141c8dbebd6253f0e
谢桂纲 <xie.guigang@gcmodeller.org>
2018-10-05 22:16:48 +0800

    code style improvements of the tcp socket module

---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Serialization/JSON/AnonymousJSON.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Namespace Serialization.JSON
+
+    ''' <summary>
+    ''' Extension helpers for deal with the anonymous type
+    ''' </summary>
+    Public Module AnonymousJSONExtensions
+
+        <Extension>
+        Public Function GetJson(obj As String(,)) As String
+            With New Dictionary(Of String, String)
+                For Each prop As String() In obj.RowIterator
+                    Call .Add(prop(0), prop(1))
+                Next
+
+                Return .GetJson
+            End With
+        End Function
+
+        <Extension>
+        Public Function AnonymousJSON(Of T As Class)(obj As T) As String
+            Dim keys = obj.GetType.GetProperties(PublicProperty)
+
+            With New Dictionary(Of String, String)
+                For Each key As PropertyInfo In keys
+                    Call .Add(key.Name, key.GetValue(obj).ToString)
+                Next
+
+                Return .GetJson
+            End With
+        End Function
+    End Module
+End Namespace
