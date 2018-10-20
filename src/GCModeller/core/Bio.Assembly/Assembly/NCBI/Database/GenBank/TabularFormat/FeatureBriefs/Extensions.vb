e6a92aafadbf1deec2b5f7e7261fab1a8e764b18
谢桂纲 <xie.guigang@gcmodeller.org>
2018-10-20 16:01:29 +0800

    gff feature to gene brief model

--- src/GCModeller/core/Bio.Assembly/Assembly/NCBI/Database/GenBank/TabularFormat/FeatureBriefs/Extensions.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/NCBI/Database/GenBank/TabularFormat/FeatureBriefs/Extensions.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.genomics.Assembly.NCBI.GenBank.TabularFormat.ComponentModels
 
 Namespace Assembly.NCBI.GenBank.TabularFormat
@@ -11,9 +12,28 @@ Namespace Assembly.NCBI.GenBank.TabularFormat
             Return From gene In genes Select gene.COG
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         <Extension>
         Public Function AsGenes(features As IEnumerable(Of GFF.Feature)) As IEnumerable(Of GeneBrief)
+            Return features _
+                .SafeQuery _
+                .Select(Function(feature) feature.ToGeneBrief)
+        End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <Extension>
+        Public Function ToGeneBrief(feature As GFF.Feature) As GeneBrief
+            Return New GeneBrief With {
+                .Code = feature.ID,
+                .COG = feature.COG,
+                .Gene = feature.ID,
+                .IsORF = True,
+                .Length = feature.Length,
+                .Location = feature.Location,
+                .PID = feature.ProteinId,
+                .Product = feature.Product,
+                .Synonym = feature.Synonym
+            }
         End Function
     End Module
 End Namespace
