da12786bae6e0a97c6abce75c464ee7d39ea8d84
谢桂纲 <xie.guigang@gcmodeller.org>
2018-10-20 15:22:22 +0800

    update source file header

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/LocalRender.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/LocalRender.vb
@@ -1,47 +1,47 @@
-﻿#Region "Microsoft.VisualBasic::8235e23586ccf129a8b38d49f902ef3a, Bio.Assembly\Assembly\KEGG\Web\Map\LocalRender.vb"
+﻿#Region "Microsoft.VisualBasic::128ac365b631a83181feb9a9bfd633ae, Bio.Assembly\Assembly\KEGG\Web\Map\LocalRender.vb"
 
-' Author:
-' 
-'       asuka (amethyst.asuka@gcmodeller.org)
-'       xie (genetics@smrucc.org)
-'       xieguigang (xie.guigang@live.com)
-' 
-' Copyright (c) 2018 GPL3 Licensed
-' 
-' 
-' GNU GENERAL PUBLIC LICENSE (GPL3)
-' 
-' 
-' This program is free software: you can redistribute it and/or modify
-' it under the terms of the GNU General Public License as published by
-' the Free Software Foundation, either version 3 of the License, or
-' (at your option) any later version.
-' 
-' This program is distributed in the hope that it will be useful,
-' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-' GNU General Public License for more details.
-' 
-' You should have received a copy of the GNU General Public License
-' along with this program. If not, see <http://www.gnu.org/licenses/>.
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xie (genetics@smrucc.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2018 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-' /********************************************************************************/
+    ' /********************************************************************************/
 
-' Summaries:
+    ' Summaries:
 
-'     Class LocalRender
-' 
-'         Constructor: (+1 Overloads) Sub New
-' 
-'         Function: FromRepository, getAreas, GetEnumerator, GetTitle, IEnumerable_GetEnumerator
-'                   IteratesMapNames, (+2 Overloads) Rendering
-' 
-'         Sub: renderCompound, renderGenes
-' 
-' 
-' /********************************************************************************/
+    '     Class LocalRender
+    ' 
+    '         Constructor: (+1 Overloads) Sub New
+    ' 
+    '         Function: FromRepository, getAreas, GetEnumerator, GetTitle, IEnumerable_GetEnumerator
+    '                   IteratesMapNames, (+2 Overloads) Rendering
+    ' 
+    '         Sub: renderCompound, renderGenes
+    ' 
+    ' 
+    ' /********************************************************************************/
 
 #End Region
 
--- src/GCModeller/core/Bio.Repository/KEGG/KEGGOrthology/KEGGOrthology.vb
+++ src/GCModeller/core/Bio.Repository/KEGG/KEGGOrthology/KEGGOrthology.vb
@@ -1,48 +1,48 @@
-﻿#Region "Microsoft.VisualBasic::3f81e13543cbdbb0ddf67a323bc05ea1, Bio.Repository\KEGG\KEGGOrthology\KEGGOrthology.vb"
+﻿#Region "Microsoft.VisualBasic::32f4c1fc8a98385dfae945a759340965, Bio.Repository\KEGG\KEGGOrthology\KEGGOrthology.vb"
 
-' Author:
-' 
-'       asuka (amethyst.asuka@gcmodeller.org)
-'       xie (genetics@smrucc.org)
-'       xieguigang (xie.guigang@live.com)
-' 
-' Copyright (c) 2018 GPL3 Licensed
-' 
-' 
-' GNU GENERAL PUBLIC LICENSE (GPL3)
-' 
-' 
-' This program is free software: you can redistribute it and/or modify
-' it under the terms of the GNU General Public License as published by
-' the Free Software Foundation, either version 3 of the License, or
-' (at your option) any later version.
-' 
-' This program is distributed in the hope that it will be useful,
-' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-' GNU General Public License for more details.
-' 
-' You should have received a copy of the GNU General Public License
-' along with this program. If not, see <http://www.gnu.org/licenses/>.
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xie (genetics@smrucc.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2018 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-' /********************************************************************************/
+    ' /********************************************************************************/
 
-' Summaries:
+    ' Summaries:
 
-' Class KEGGOrthology
-' 
-'     Properties: Repository
-' 
-'     Constructor: (+1 Overloads) Sub New
-' 
-'     Function: __copy, __ko_genes_index, BuildLocus2KOIndex, EnumerateKO, FileCopy
-'               IndexSubMatch
-' 
-'     Sub: __locus2KO, BuildLocusIndex
-' 
-' /********************************************************************************/
+    ' Class KEGGOrthology
+    ' 
+    '     Properties: Repository
+    ' 
+    '     Constructor: (+1 Overloads) Sub New
+    ' 
+    '     Function: __copy, __ko_genes_index, BuildLocus2KOIndex, EnumerateKO, FileCopy
+    '               IndexSubMatch
+    ' 
+    '     Sub: __locus2KO, BuildLocusIndex
+    ' 
+    ' /********************************************************************************/
 
 #End Region
 
