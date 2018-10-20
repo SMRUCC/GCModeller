a2fb6c35e9da613260e85478ad1b6f287909ccff
谢桂纲 <xie.guigang@gcmodeller.org>
2018-10-20 15:03:04 +0800

    config projects

--- src/GCModeller/visualize/Synteny/Program.vb
+++ src/GCModeller/visualize/Synteny/Program.vb
@@ -1,50 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::e06210273e720553f60262bc083593c1, visualize\Synteny\Program.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Program
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: ExecuteFile, Main
-    ' 
-    ' /********************************************************************************/
+' Module Program
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: ExecuteFile, Main
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System
 Imports System.Drawing
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.Imaging
-Imports Microsoft.VisualBasic.Serialization
 Imports SMRUCC.genomics.Visualize
 Imports SMRUCC.genomics.Visualize.SyntenyVisualize
 
@@ -55,15 +53,16 @@ Module Program
     End Sub
 
     Public Function Main() As Integer
+        Return GetType(CLI).RunCLI(App.CommandLine, executeFile:=AddressOf ExecuteFile)
+    End Function
 
+    Private Sub test1()
         Dim list As Double() = "G:\5.14.circos\03.ZIKV_45_2015_updated_mafft_named.GCSkew.txt".ReadVector
         Dim img As New Bitmap(3000, 1000)
         Dim res = GCSkew.DrawingCurve(img, list, New Point(200, 850), New Size(2500, 800), GraphicTypes.Histogram)
 
         Call res.SaveAs("x:\dddd.png", ImageFormats.Png)
-
-        Return GetType(Program).RunCLI(App.CommandLine, executeFile:=AddressOf ExecuteFile)
-    End Function
+    End Sub
 
     Private Function ExecuteFile(file As String, args As CommandLine) As Integer
         Dim model As DrawingModel = ModelAPI.GetDrawsModel(file)
