566149f20d6520a09d3948a50d5a4dd8d8e2ab68
谢桂纲 <xie.guigang@gcmodeller.org>
2018-10-20 15:18:14 +0800

    load plot data

---
+++ src/GCModeller/core/Bio.Assembly/Assembly/NCBI/Database/GenBank/TabularFormat/FeatureBriefs/GFF/Extensions.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Assembly.NCBI.GenBank.TabularFormat.GFF
+
+    Public Module Extensions
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function Load(path As String) As GFFTable
+            Return GFFTable.LoadDocument(path)
+        End Function
+    End Module
+End Namespace
--- src/GCModeller/visualize/Synteny/CLI.vb
+++ src/GCModeller/visualize/Synteny/CLI.vb
@@ -1,3 +1,26 @@
-﻿Module CLI
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank.TabularFormat
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application
 
+Module CLI
+
+    ''' <summary>
+    ''' Plot of the blastn mapping result
+    ''' </summary>
+    ''' <param name="args"></param>
+    ''' <returns></returns>
+    <ExportAPI("/mapping.plot")>
+    <Usage("/mapping.plot /mapping <blastn_mapping.csv> /query <query.gff3> /ref <subject.gff3> [/out <Synteny.png>]")>
+    Public Function PlotMapping(args As CommandLine) As Integer
+        Dim in$ = args <= "/mapping"
+        Dim query$ = args <= "/query"
+        Dim ref$ = args <= "/ref"
+        Dim out$ = args("/out") Or $"{[in].TrimSuffix}.synteny.png"
+        Dim mappings = [in].LoadCsv(Of BlastnMapping).ToArray
+        Dim queryGff = GFF.Load(query)
+        Dim refGff = GFF.Load(ref)
+
+    End Function
 End Module
