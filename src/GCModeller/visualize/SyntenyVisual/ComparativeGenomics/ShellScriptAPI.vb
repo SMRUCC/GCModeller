e2d0e3ee669c55ddc083d1f6ba570dc16526e626
谢桂纲 <xie.guigang@gcmodeller.org>
2018-10-20 21:36:57 +0800

    try to fix for score

--- src/GCModeller/visualize/SyntenyVisual/ComparativeGenomics/ShellScriptAPI.vb
+++ src/GCModeller/visualize/SyntenyVisual/ComparativeGenomics/ShellScriptAPI.vb
@@ -166,6 +166,30 @@ Namespace ComparativeGenomics
         End Function
 
         <Extension>
+        Public Function LogScore(Of T)(data As IEnumerable(Of T), getScore As Func(Of T, Double)) As Vector
+            Dim raw = data _
+                .Select(Function(x)
+                            Dim score = getScore(x)
+
+                            If score = 0R Then
+                                Return Double.MinValue
+                            Else
+                                Return -Math.Log(score)
+                            End If
+                        End Function) _
+                .ToArray
+            Dim max = raw.Max + 1
+
+            For i As Integer = 0 To raw.Length - 1
+                If raw(i) <= Double.MinValue + 10 Then
+                    raw(i) = max
+                End If
+            Next
+
+            Return raw
+        End Function
+
+        <Extension>
         Public Function LinkFromBlastnMaps(model As DrawingModel,
                                            maps As IEnumerable(Of BlastnMapping),
                                            Optional grepOp As TextGrepScriptEngine = Nothing,
@@ -174,17 +198,7 @@ Namespace ComparativeGenomics
             Dim grep As TextGrepMethod = (grepOp Or TextGrepScriptEngine.DoNothing).PipelinePointer
             Dim colors As Color() = Designer.GetColors(ribbonColors, 30)
             Dim mapsVector = maps.ToArray
-            Dim scores As Vector =
-                mapsVector.Select(Function(d) d.Identities).AsVector '*
-            'mapsVector.Select(Function(d)
-            '                      ' evalue 可能是零
-            '                      If d.Evalue = 0R Then
-            '                          Return 300
-            '                      Else
-            '                          Return -Math.Log(d.Evalue)
-            '                      End If
-            '                  End Function) _
-            '          .AsVector
+            Dim scores As Vector = maps.LogScore(Function(m) m.Evalue)
             Dim colorIndex As New DoubleRange(scores)
             Dim indexRange As DoubleRange = {0, colors.Length - 1}
 
