1d5c427d1c925bd65e36badd66ba9f49062fa2f0
谢桂纲 <xie.guigang@gcmodeller.org>
2018-10-21 17:52:32 +0800

    add topbest control for myva COG output

--- src/interops/localblast/LocalBLAST/LocalBLAST/LocalBLAST/Application/COG/COGsUtils.vb
+++ src/interops/localblast/LocalBLAST/LocalBLAST/LocalBLAST/Application/COG/COGsUtils.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::0a47a5754fa054e08bfa64ec0ab45581, localblast\LocalBLAST\LocalBLAST\LocalBLAST\Application\COG\COGsUtils.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module COGsUtils
-    ' 
-    '         Function: (+2 Overloads) MyvaCOGCatalog
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module COGsUtils
+' 
+'         Function: (+2 Overloads) MyvaCOGCatalog
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -67,19 +67,21 @@ Namespace LocalBLAST.Application.RpsBLAST
                                        Optional coverage# = 0.5,
                                        Optional descriptParser As TextGrepMethod = Nothing,
                                        Optional ALL As Boolean = False) As MyvaCOG()
-            If blastp.FileExists Then
 
-                Dim outputRaw As v228 = Parser.TryParse(blastp)
-                Dim bbh As BestHit() = If(
-                    ALL,
-                    outputRaw.ExportAllBestHist(coverage, identities),
-                    outputRaw.ExportBestHit(coverage, identities))
-                Dim myvaCOG = bbh.MyvaCOGCatalog(whogXml, descriptParser)
-
-                Return myvaCOG
-            Else
-                Return Nothing
+            If Not blastp.FileExists Then
+                Return {}
             End If
+
+            Dim outputRaw As v228 = Parser.TryParse(blastp)
+            Dim bbh As BestHit()
+
+            If ALL Then
+                bbh = outputRaw.ExportAllBestHist(coverage, identities)
+            Else
+                bbh = outputRaw.ExportBestHit(coverage, identities)
+            End If
+
+            Return bbh.MyvaCOGCatalog(whogXml, descriptParser)
         End Function
 
         ''' <summary>
