ed570e5428ee3d88a3336dd5c75688be983534b1
谢桂纲 <xie.guigang@gcmodeller.org>
2018-10-21 14:45:16 +0800

    fix for drawer code

--- src/GCModeller/visualize/ChromosomeMap/RegionMap.vb
+++ src/GCModeller/visualize/ChromosomeMap/RegionMap.vb
@@ -2,6 +2,7 @@
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
 Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS
 Imports Microsoft.VisualBasic.Scripting.Runtime
 Imports SMRUCC.genomics.Visualize.ChromosomeMap.DrawingModels
 
@@ -14,7 +15,8 @@ Public Module RegionMap
                          Optional size$ = "5000,2000",
                          Optional padding$ = g.DefaultPadding,
                          Optional bg$ = "white",
-                         Optional geneShapeHeight% = 85) As GraphicsData
+                         Optional geneShapeHeight% = 85,
+                         Optional locusTagFontCSS$ = CSSFont.Win7Normal) As GraphicsData
 
         Dim nextLength%
         Dim ff As Boolean
@@ -24,31 +26,33 @@ Public Module RegionMap
         Dim startLength%
         Dim preRight#
         Dim level%
+        Dim locusTagFont As Font = CSSFont.TryParse(locusTagFontCSS)
         Dim plotInternal =
             Sub(ByRef g As IGraphics, region As GraphicsRegion)
-
                 Dim width = region.Width
+                Dim height = region.Height
+                Dim margin As Padding = region.Padding
 
                 For Each gene As SegmentObject In model.GeneObjects
                     If gene.Left > nextLength Then
                         ff = True  '这个变量确保能够正确的换行，不可以修改值以及顺序！~
 
                         If nextLength >= model.Size Then
-                            rightEnd = width - (nextLength - model.Size) * scaleFactor - 2 * margin
+                            rightEnd = width - (nextLength - model.Size) * scaleFactor - margin.Horizontal
                         End If
 
                         startLength = nextLength
                         nextLength = nextLength + unitLength
 
                         ' 每换一行则首先绘制突变数据
-                        Call drawChromosomeSites(Chr,
-                                                       _start_Length:=_Start_Length,
-                                                       FlagHeight:=FlagHeight,
-                                                       FlagLength:=FlagLength,
-                                                       GrDevice:=g,
-                                                       Height:=Height,
-                                                       NextLength:=nextLength,
-                                                       scale:=scaleFactor)
+                        'Call drawChromosomeSites(Chr,
+                        '                               _start_Length:=_Start_Length,
+                        '                               FlagHeight:=FlagHeight,
+                        '                               FlagLength:=FlagLength,
+                        '                               GrDevice:=g,
+                        '                               Height:=height,
+                        '                               NextLength:=nextLength,
+                        '                               scale:=scaleFactor)
                     End If
 
                     If gene.Left < preRight Then
@@ -61,12 +65,12 @@ Public Module RegionMap
 
                     gene.Height = geneShapeHeight
 
-                    Dim drawingLociLeft As Integer = (gene.Left - startLength) * scaleFactor + margin
+                    Dim drawingLociLeft As Integer = (gene.Left - startLength) * scaleFactor + margin.Left
                     Dim drawingSize = gene.Draw(g:=g,
-                                                    location:=New Point(drawingLociLeft, Height + 100 + level * 110),
+                                                    location:=New Point(drawingLociLeft, height + 100 + level * 110),
                                                     factor:=scaleFactor,
                                                     RightLimited:=rightEnd,
-                                                    conf:=Me.config)
+                                                    locusTagFont:=locusTagFont)
                 Next
 
             End Sub
