eb6dbee9ec7ca386c1c087913dd4f1e2152f8cfc
xieguigang <xie.guigang@gcmodeller.org>
2018-10-23 08:32:20 +0800

    add dev note

--- src/GCModeller/visualize/ChromosomeMap/Data/ReadsMap.vb
+++ src/GCModeller/visualize/ChromosomeMap/Data/ReadsMap.vb
@@ -246,7 +246,7 @@ Public Module ReadsMap
         Next
 
         '将位点还原为标签
-        Dim DipartsHistone = (From Loc In TSSPossibleLocation Select Loc.strand, Loc.Loci.UserTag, ID = Loc.ID, HisData = Reads(Loc.LociSequence, HistoneGram)).ToArray
+        Dim DipartsHistone = (From Loc In TSSPossibleLocation Select Loc.strand, Loc.Loci.Tag, ID = Loc.ID, HisData = Reads(Loc.LociSequence, HistoneGram)).ToArray
         '生成CSV文档
         Dim Df As New IO.File
         Dim OperonPromoterGene As String() = Nothing
@@ -327,7 +327,7 @@ Public Module ReadsMap
         Return New NucleotideLocation(
             GeneObject.Location.GetUpStreamLoci(CUInt(150)),
             GeneObject.Location.Start,
-            GeneObject.Location.Strand = Strands.Reverse) With {.UserTag = $"{GeneObject.Synonym} {GeneObject.Location.ToString}"}
+            GeneObject.Location.Strand = Strands.Reverse) With {.Tag = $"{GeneObject.Synonym} {GeneObject.Location.ToString}"}
     End Function
 
 End Module
