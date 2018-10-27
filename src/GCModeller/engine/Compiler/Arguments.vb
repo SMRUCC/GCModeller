ce1544ce78fa02684a1189eb05d672e4225b0f97
谢桂纲 <xie.guigang@gcmodeller.org>
2018-10-27 11:30:54 +0800

    repository data loader

--- src/GCModeller/engine/Compiler/Arguments.vb
+++ src/GCModeller/engine/Compiler/Arguments.vb
@@ -1,4 +1,5 @@
 ﻿Imports SMRUCC.genomics.Data
+Imports SMRUCC.genomics.Data.KEGG.Metabolism
 
 Public Class RepositoryArguments
 
@@ -8,6 +9,30 @@ Public Class RepositoryArguments
 
     Dim compoundsRepository As CompoundRepository
     Dim reactionsRepository As ReactionRepository
-    Dim pathwayRepository As 
+    Dim pathwayRepository As PathwayRepository
+
+    Public Function GetCompounds() As CompoundRepository
+        If compoundsRepository Is Nothing Then
+            compoundsRepository = KEGGCompounds.FetchCompoundRepository
+        End If
+
+        Return compoundsRepository
+    End Function
+
+    Public Function GetReactions() As ReactionRepository
+        If reactionsRepository Is Nothing Then
+            reactionsRepository = KEGGReactions.FetchReactionRepository
+        End If
+
+        Return reactionsRepository
+    End Function
+
+    Public Function GetPathways() As PathwayRepository
+        If pathwayRepository Is Nothing Then
+            pathwayRepository = KEGGPathway.FetchPathwayRepository
+        End If
+
+        Return pathwayRepository
+    End Function
 
 End Class
