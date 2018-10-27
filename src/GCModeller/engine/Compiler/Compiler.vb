6ffb4bf179d58c389b6e81b3851fa71197d4fb60
谢桂纲 <xie.guigang@gcmodeller.org>
2018-10-27 12:12:48 +0800

    build MetabolicProcess catalysis network

--- src/GCModeller/engine/Compiler/Compiler.vb
+++ src/GCModeller/engine/Compiler/Compiler.vb
@@ -1,41 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::d6450ea0ee552e8f4cf1194539c83a41, Compiler\Compiler.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Workflow
-    ' 
-    '     Function: AssemblingModel, BuildReactions, converts, GetCentralDogmas
-    ' 
-    ' /********************************************************************************/
+' Module Workflow
+' 
+'     Function: AssemblingModel, BuildReactions, converts, GetCentralDogmas
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -43,11 +43,12 @@ Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.ComponentModel.TagData
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.genomics.Assembly.NCBI.GenBank
 Imports SMRUCC.genomics.Assembly.NCBI.GenBank.GBFF.Keywords.FEATURES
 Imports SMRUCC.genomics.ComponentModel.EquaionModel.DefaultTypes
 Imports SMRUCC.genomics.Data
-Imports SMRUCC.genomics.Data.KEGG.Metabolism
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.Model
 Imports SMRUCC.genomics.Metagenomics
 
@@ -87,12 +88,39 @@ Public Module Workflow
 
     <Extension>
     Private Iterator Function createMetabolicProcess(KOfunction As Dictionary(Of String, String), reactions As ReactionRepository) As IEnumerable(Of Regulation)
+        Dim KOreactions = reactions _
+            .MetabolicNetwork _
+            .Where(Function(r)
+                       Return Not r.Orthology.Terms.IsNullOrEmpty
+                   End Function) _
+            .Select(Function(r)
+                        Return r.Orthology _
+                                .Terms _
+                                .Select(Function(KO)
+                                            Return (name:=KO.name, KOterm:=KO, flux:=r)
+                                        End Function)
+                    End Function) _
+            .IteratesALL _
+            .GroupBy(Function(KO) KO.name) _
+            .ToDictionary(Function(KO) KO.Key,
+                          Function(g)
+                              Return g.ToArray
+                          End Function)
+
         For Each enzyme In KOfunction
-            Yield New Regulation With {
-                .effects = 2,
-                .regulator = enzyme.Key,
-                .type = Processes.MetabolicProcess
-            }
+            Dim catalysis = KOreactions.TryGetValue(enzyme.Value)
+
+            For Each flux In catalysis.SafeQuery
+                Dim fluxName$ = flux.flux.CommonNames.ElementAtOrDefault(0) Or flux.flux.Definition.AsDefault
+
+                Yield New Regulation With {
+                    .effects = 2,
+                    .regulator = enzyme.Key,
+                    .type = Processes.MetabolicProcess,
+                    .name = fluxName,
+                    .process = flux.flux.ID
+                }
+            Next
         Next
     End Function
 
