c2e86a3515e81f18acbb3b9682afe05aa8f1b6d2
谢桂纲 <xie.guigang@gcmodeller.org>
2018-11-06 20:15:22 +0800

    RegPreciseRegulatorMatch

---
+++ src/GCModeller/data/RegulonDatabase/Regprecise/RegPreciseRegulatorMatch.vb
@@ -0,0 +1,31 @@
+﻿Namespace Regprecise
+
+    Public Class RegPreciseRegulatorMatch
+
+        ''' <summary>
+        ''' The genome query.(待注释的目标基因组的蛋白编号)
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Query As String
+
+        ''' <summary>
+        ''' The regprecise regulator name
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Regulator As String
+        Public Property Family As String
+        Public Property Description As String
+
+        ''' <summary>
+        ''' 相似度
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Identities As Double
+
+        ''' <summary>
+        ''' 这个<see cref="Regulator"/>所调控的位点集合
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property RegulonSites As String()
+    End Class
+End Namespace
