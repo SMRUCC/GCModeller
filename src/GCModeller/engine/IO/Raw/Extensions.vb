e7c4c9c5df8118688e06e12a74eb192fab22b648
谢桂纲 <xie.guigang@gcmodeller.org>
2018-11-08 20:43:26 +0800

    using netCDF for store simulation result data?

--- src/GCModeller/engine/IO/Raw/Extensions.vb
+++ src/GCModeller/engine/IO/Raw/Extensions.vb
@@ -1,46 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::832f0e49b32379e9553c875348974926, IO\Raw\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Extensions
-    ' 
-    '     Function: GetMatrix
-    ' 
-    ' /********************************************************************************/
+' Module Extensions
+' 
+'     Function: GetMatrix
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.MIME.application.netCDF
 
 Public Module Extensions
 
@@ -52,6 +53,15 @@ Public Module Extensions
                 .Properties = raw.Read(time, [module])
             }
         Next
+    End Function
+
+    ''' <summary>
+    ''' 读取netCDF格式的模拟计算结果数据文件
+    ''' </summary>
+    ''' <param name="cdf"></param>
+    ''' <returns></returns>
+    <Extension> Public Function LoadCDF(cdf As netCDFReader)
+
     End Function
 End Module
 
