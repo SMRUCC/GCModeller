dae80eec247555965f05cc663b59785ca9b77a06
谢桂纲 <xie.guigang@gcmodeller.org>
2018-11-11 01:15:36 +0800

    how to convert to network model?

---
+++ src/GCModeller/engine/Compiler/NetworkViz.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports SMRUCC.genomics.GCModeller.ModellingEngine.Model
+
+''' <summary>
+''' Export network visualize model for cytoscape software.
+''' </summary>
+Public Module NetworkViz
+
+    <Extension>
+    Public Function CreateGraph(cell As CellularModule) As NetworkTables
+
+    End Function
+End Module
