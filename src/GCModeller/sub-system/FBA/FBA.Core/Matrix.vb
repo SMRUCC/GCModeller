39cfad7c9fb607b0c3b46bdf295351cb0cfe1e25
谢桂纲 <xie.guigang@gcmodeller.org>
2018-11-14 05:07:34 +0800

    add basically model data

---
+++ src/GCModeller/sub-system/FBA/FBA.Core/Matrix.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Class Matrix
+
+    ''' <summary>
+    ''' 线性规划之中的变量名称，即``Xi``
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Compounds As String()
+    ''' <summary>
+    ''' 目标函数之中的计算目标，为Compounds之中的一部分
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Targets As String()
+
+    Public Property Matrix As Double()()
+
+    Public ReadOnly Property NumOfFlux As Integer
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Get
+            Return Matrix.Length
+        End Get
+    End Property
+
+    Public Function GetTargetCoefficients() As Double()
+        With Targets.Indexing
+            Return Compounds _
+                .Select(Function(name)
+                            If .IndexOf(name) > -1 Then
+                                Return 1.0
+                            Else
+                                Return 0.0
+                            End If
+                        End Function) _
+                .ToArray
+        End With
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function GetMatrix() As Double(,)
+        Return Matrix.ToMatrix
+    End Function
+
+End Class
