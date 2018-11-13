10449c070ee20bf62a1ac979144c5ba4ddbc4808
谢桂纲 <xie.guigang@gcmodeller.org>
2018-11-14 04:39:05 +0800

    config for projhect reference and namedspace

--- src/GCModeller/sub-system/FBA/FBA.Core/My Project/Resources.Designer.vb
+++ src/GCModeller/sub-system/FBA/FBA.Core/My Project/Resources.Designer.vb
@@ -39,7 +39,7 @@ Namespace My.Resources
         Friend ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
             Get
                 If Object.ReferenceEquals(resourceMan, Nothing) Then
-                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("FBA.Core.Resources", GetType(Resources).Assembly)
+                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("SMRUCC.genomics.Analysis.FBA.Core.Resources", GetType(Resources).Assembly)
                     resourceMan = temp
                 End If
                 Return resourceMan
