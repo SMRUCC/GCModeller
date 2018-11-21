e5ed6e4fc63105a6b22f37bb7ac23174f280dddd
xieguigang <xie.guigang@gcmodeller.org>
2018-11-21 16:59:39 +0800

    code style improvements of the csv load extensions

--- src/runtime/sciBASIC#/Data/DataFrame/Extensions/Extensions.vb
+++ src/runtime/sciBASIC#/Data/DataFrame/Extensions/Extensions.vb
@@ -1,52 +1,52 @@
 ﻿#Region "Microsoft.VisualBasic::ebd99919c4821c55c5ee7f393989dd2d, Data\DataFrame\Extensions\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Extensions
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    ' 
-    '     Function: (+4 Overloads) AsDataSource, AsLinq, (+3 Overloads) DataFrame, GetLocusMapName, IsEmptyTable
-    '               (+3 Overloads) LoadCsv, LoadDblVector, LoadStream, LoadTsv, SaveDataSet
-    '               (+2 Overloads) SaveTable, (+7 Overloads) SaveTo, TabExport, ToCsvDoc
-    ' 
-    '     Sub: Cable, ForEach
-    '     Structure __loadHelper
-    ' 
-    '         Function: LoadObject
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Module Extensions
+' 
+'     Constructor: (+1 Overloads) Sub New
+' 
+'     Function: (+4 Overloads) AsDataSource, AsLinq, (+3 Overloads) DataFrame, GetLocusMapName, IsEmptyTable
+'               (+3 Overloads) LoadCsv, LoadDblVector, LoadStream, LoadTsv, SaveDataSet
+'               (+2 Overloads) SaveTable, (+7 Overloads) SaveTo, TabExport, ToCsvDoc
+' 
+'     Sub: Cable, ForEach
+'     Structure __loadHelper
+' 
+'         Function: LoadObject
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -450,15 +450,23 @@ Public Module Extensions
                                                        Optional fast As Boolean = False,
                                                        Optional maps As NameMapping = Nothing,
                                                        Optional mute As Boolean = False) As List(Of T)
+        Dim buffer As List(Of T)
+        Dim fs$, ms&
 
-        Call "Start to load csv data....".__DEBUG_ECHO
-        Dim st = Stopwatch.StartNew
-        Dim bufs = Reflector.Load(Of T)(path, explicit, encoding, fast, maps)
-        Dim ms As Long = st.ElapsedMilliseconds
-        Dim fs As String = If(ms > 1000, (ms / 1000) & "sec", ms & "ms")
-        Call $"[CSV.Reflector::{GetType(T).FullName}]
-Load {bufs.Count} lines of data from ""{path.ToFileURL}""! ...................{fs}".__DEBUG_ECHO
-        Return bufs
+        Call "Start to load csv data....".__DEBUG_ECHO(mute:=mute)
+
+        With Stopwatch.StartNew
+            buffer = Reflector.Load(Of T)(path, explicit, encoding, fast, maps)
+            ms = .ElapsedMilliseconds
+            fs = If(ms > 1000, (ms / 1000) & "sec", ms & "ms")
+        End With
+
+        Dim type$ = GetType(T).FullName
+        Dim n% = buffer.Count
+
+        Call $"[CSV.Reflector::{type}]{vbLf}Load {n} lines of data from ""{path.ToFileURL}""! ...................{fs}".__DEBUG_ECHO(mute:=mute)
+
+        Return buffer
     End Function
 
     ''' <summary>
