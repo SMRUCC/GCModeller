b6c10a5bb3213fb3fa8cb7dea632654b63ca98e4
谢桂纲 <xie.guigang@gcmodeller.org>
2018-11-21 21:22:50 +0800

    add custom render color supports

--- src/GCModeller/CLI_tools/Solver.FBA/CLI/Visual.vb
+++ src/GCModeller/CLI_tools/Solver.FBA/CLI/Visual.vb
@@ -1,41 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::d0131d3dff81dea432954db75826f914, CLI_tools\Solver.FBA\CLI\Visual.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CLI
-    ' 
-    '     Function: Heatmap, ScaleHeatmap
-    ' 
-    ' /********************************************************************************/
+' Module CLI
+' 
+'     Function: Heatmap, ScaleHeatmap
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -43,6 +43,7 @@ Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
 Imports RDotNET.Extensions.Bioinformatics
@@ -51,6 +52,7 @@ Imports RDotNET.Extensions.VisualBasic.SymbolBuilder.packages.gplots
 Imports RDotNET.Extensions.VisualBasic.SymbolBuilder.packages.grDevices
 Imports RDotNET.Extensions.VisualBasic.SymbolBuilder.packages.utils.read.table
 Imports SMRUCC.genomics.Analysis.FBA_DP.Models.rFBA
+Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
 
 Partial Module CLI
 
@@ -99,5 +101,26 @@ Partial Module CLI
                         .ToDictionary(Function(key) key.Key,
                                       Function(v) v.Value * factor)}).ToArray
         Return data.SaveTo(out).CLICode
+    End Function
+
+    <ExportAPI("/visual.kegg.pathways")>
+    <Usage("/visual.kegg.pathways /model <virtualCell.GCMarkup> /maps <kegg_maps.repo.directory> [/plasmid.highlight <default=blue> /out <directory>]")>
+    Public Function VisualKEGGPathways(args As CommandLine) As Integer
+        Dim in$ = args <= "/model"
+        Dim maps$ = args <= "/maps"
+        Dim plasmidHighlight As String = Nothing
+        Dim out$ = args("/out") Or $"{[in].TrimSuffix}_visual.kegg.pathways/"
+
+        If args("/plasmid.highlight") Then
+            plasmidHighlight = args("/plasmid.highlight") Or "blue" _
+                .AsDefault(Function(val)
+                               With CStr(val)
+                                   Return .StringEmpty OrElse .TextEquals("True")
+                               End With
+                           End Function)
+        End If
+
+        Dim model As VirtualCell = [in].LoadXml(Of VirtualCell)
+
     End Function
 End Module
