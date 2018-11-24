aadb6308d347dfceeb3aba80c0f0f3e08a8139ec
xieguigang <xie.guigang@gcmodeller.org>
2018-11-24 17:41:13 +0800

    config projects refrence errors

--- src/GCModeller/visualize/ChromosomeMap/FootPrintMap/ColorProfiles.vb
+++ src/GCModeller/visualize/ChromosomeMap/FootPrintMap/ColorProfiles.vb
@@ -1,62 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::704a20f7049bc99813bf6232f52af217, ChromosomeMap\FootPrintMap\ColorProfiles.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class DrawingDevice
-    ' 
-    '         Properties: Color, GraphicDevice, Image
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: ToString
-    ' 
-    '         Sub: (+2 Overloads) Dispose
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class DrawingDevice
+' 
+'         Properties: Color, GraphicDevice, Image
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: ToString
+' 
+'         Sub: (+2 Overloads) Dispose
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Microsoft.VisualBasic.Linq.Extensions
-Imports Microsoft.VisualBasic
-Imports SMRUCC.genomics.Visualize
 Imports System.Drawing
+Imports SMRUCC.genomics.Visualize.GeneticClock
 
 Namespace ComponentModel
 
     Public Class DrawingDevice : Implements IDisposable
 
         Dim _GraphicDevice As Graphics, _ImageData As Bitmap
-        Dim _ColorProfiles As ColorProfiles
+        Dim _ColorProfiles As ColorProfile
 
         Public ReadOnly Property GraphicDevice As Graphics
             Get
