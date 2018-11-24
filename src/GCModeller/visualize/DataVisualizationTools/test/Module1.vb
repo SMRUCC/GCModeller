1da5dd274577836b56da1490e8fabcc7b47c99fc
xieguigang <xie.guigang@gcmodeller.org>
2018-11-24 17:12:51 +0800

    test success

--- src/GCModeller/visualize/DataVisualizationTools/test/Module1.vb
+++ src/GCModeller/visualize/DataVisualizationTools/test/Module1.vb
@@ -6,35 +6,23 @@ Imports SMRUCC.genomics.Visualize.NCBIBlastResult
 Module Module1
 
     Sub Main()
+        Dim rand As New Random
+        Dim max# = 1000
+        Dim getCategory = Function(i As Integer) As OrthologyProfile
+                              Return New OrthologyProfile With {
+                                  .Category = "category " & (i + 1),
+                                  .HomologyDegrees = {
+                                      New NamedValue(Of Color)("a", Color.Red, rand.NextDouble * max * 0.65),
+                                      New NamedValue(Of Color)("b", Color.LightBlue, rand.NextDouble * max),
+                                      New NamedValue(Of Color)("c", Color.SkyBlue, rand.NextDouble * max * 0.65),
+                                      New NamedValue(Of Color)("d", Color.Blue, rand.NextDouble * max),
+                                      New NamedValue(Of Color)("e", Color.DarkBlue, rand.NextDouble * max * 0.3),
+                                      New NamedValue(Of Color)("f", Color.Gray, rand.NextDouble * max * 0.5)
+                                  }
+                              }
+                          End Function
 
-        Dim profiles = {
-            New OrthologyProfile With {.Category = "category1", .HomologyDegrees = {
-                New NamedValue(Of Color)("a", Color.Red, 180),
-                New NamedValue(Of Color)("b", Color.LightBlue, 200),
-                New NamedValue(Of Color)("c", Color.SkyBlue, 100),
-                New NamedValue(Of Color)("d", Color.Blue, 260),
-                New NamedValue(Of Color)("e", Color.DarkBlue, 50),
-                New NamedValue(Of Color)("f", Color.Gray, 500)
-                }
-            },
-            New OrthologyProfile With {.Category = "category1 XXXXXXXXX", .HomologyDegrees = {
-                New NamedValue(Of Color)("a", Color.Red, 300),
-                New NamedValue(Of Color)("b", Color.LightBlue, 500),
-                New NamedValue(Of Color)("c", Color.SkyBlue, 190),
-                New NamedValue(Of Color)("d", Color.Blue, 90),
-                New NamedValue(Of Color)("e", Color.DarkBlue, 10),
-                New NamedValue(Of Color)("f", Color.Gray, 300)
-        }},
-         New OrthologyProfile With {.Category = "category1 XXXXXXXXX skjfhsdkjfshdjkf",
-            .HomologyDegrees = {
-                New NamedValue(Of Color)("a", Color.Red, 30),
-                New NamedValue(Of Color)("b", Color.LightBlue, 400),
-                New NamedValue(Of Color)("c", Color.SkyBlue, 390),
-                New NamedValue(Of Color)("d", Color.Blue, 290),
-                New NamedValue(Of Color)("e", Color.DarkBlue, 10),
-                New NamedValue(Of Color)("f", Color.Gray, 300)
-        }}
-        }
+        Dim profiles = 20.SeqRandom.Select(Function(null) getCategory(null)).ToArray
 
         Call profiles.Plot.AsGDIImage.SaveAs("./test.png")
 
