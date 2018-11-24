162cabc907cbe441b71bd601744ac46b69e07c36
xieguigang <xie.guigang@gcmodeller.org>
2018-11-24 16:44:26 +0800

    config for test

--- src/GCModeller/visualize/DataVisualizationTools/test/Module1.vb
+++ src/GCModeller/visualize/DataVisualizationTools/test/Module1.vb
@@ -1,7 +1,33 @@
-﻿Module Module1
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Imaging
+Imports SMRUCC.genomics.Visualize.NCBIBlastResult
+
+Module Module1
 
     Sub Main()
 
-    End Sub
+        Dim profiles = {
+            New OrthologyProfile With {.Category = "category1", .HomologyDegrees = {
+                New NamedValue(Of Color)(1, Color.Red, 180),
+                New NamedValue(Of Color)(10, Color.LightBlue, 200),
+                New NamedValue(Of Color)(20, Color.SkyBlue, 100),
+                New NamedValue(Of Color)(30, Color.Blue, 260),
+                New NamedValue(Of Color)(40, Color.DarkBlue, 50),
+                New NamedValue(Of Color)(100, Color.Gray, 500)
+                }
+            },
+            New OrthologyProfile With {.Category = "category1", .HomologyDegrees = {
+                New NamedValue(Of Color)(1, Color.Red, 300),
+                New NamedValue(Of Color)(10, Color.LightBlue, 500),
+                New NamedValue(Of Color)(20, Color.SkyBlue, 190),
+                New NamedValue(Of Color)(30, Color.Blue, 90),
+                New NamedValue(Of Color)(40, Color.DarkBlue, 10),
+                New NamedValue(Of Color)(100, Color.Gray, 300)
+        }}
+        }
 
+        Call profiles.Plot.AsGDIImage.SaveAs("./test.png")
+
+    End Sub
 End Module
