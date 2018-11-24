2e2e835bf6ef5d4e1ccf3df5ebd73e98067f7f87
xieguigang <xie.guigang@gcmodeller.org>
2018-11-24 17:07:40 +0800

    add axis ticks drawing

--- src/GCModeller/visualize/DataVisualizationTools/NCBIBlastResult/OrthologyProfiles.vb
+++ src/GCModeller/visualize/DataVisualizationTools/NCBIBlastResult/OrthologyProfiles.vb
@@ -8,6 +8,7 @@ Imports Microsoft.VisualBasic.Imaging.Driver
 Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS
 Imports Microsoft.VisualBasic.Scripting.Runtime
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH.Abstract
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
 
 Namespace NCBIBlastResult
 
@@ -67,7 +68,11 @@ Namespace NCBIBlastResult
                              Optional title$ = "Orthology Profiles",
                              Optional axisLabel$ = "Number of Orthology Genes",
                              Optional titleFontCSS$ = CSSFont.Win7VeryVeryLarge,
-                             Optional axisLabelFontCSS$ = CSSFont.Win7VeryLarge) As GraphicsData
+                             Optional axisLabelFontCSS$ = CSSFont.Win7VeryLarge,
+                             Optional axisTicksFontCSS$ = CSSFont.Win7Normal,
+                             Optional tick# = -1,
+                             Optional tickHeight% = 10,
+                             Optional tickStroke$ = Stroke.AxisStroke) As GraphicsData
 
             Dim labelFont As Font = CSSFont.TryParse(labelFontCSS)
             Dim profiles As OrthologyProfile() = profileData.ToArray
@@ -78,6 +83,13 @@ Namespace NCBIBlastResult
             Dim boxStroke As Pen = Stroke.TryParse(boxBorderStrokeCSS)
             Dim titleFont As Font = CSSFont.TryParse(titleFontCSS)
             Dim axisLabelFont As Font = CSSFont.TryParse(axisLabelFontCSS)
+            Dim ticks#() = CatalogProfiling.GetTicks(maxCount, tick)
+            Dim tickFont As Font = CSSFont.TryParse(axisTicksFontCSS)
+            Dim tickPen As Pen = Stroke.TryParse(tickStroke)
+
+            ' 将最大的统计数量设置为axis的最大值，可以让图表更加美观
+            maxCount = ticks.Max
+
             Dim plotInternal =
                 Sub(ByRef g As IGraphics, region As GraphicsRegion)
 
@@ -96,14 +108,15 @@ Namespace NCBIBlastResult
                     Dim boxHeight = totalHeight + boxStroke.Width * 2 + labelSize.Height
                     Dim boxLeft% = left + maxLabelWidth + 5
                     Dim pos As New Point(boxLeft, top)
+                    Dim box As New Rectangle(pos, New Size(boxWidth, boxHeight))
 
                     ' 绘制盒子边框
-                    Call g.DrawRectangle(boxStroke, New Rectangle(pos, New Size(boxWidth, boxHeight)))
+                    Call g.DrawRectangle(boxStroke, box)
 
                     ' 开始绘制每一个category的条形图
                     ' 这个条形图里面还包含有该分类之中的不同程度的同源结果
                     Dim x! = left
-                    Dim y! = top + labelSize.Height / 3
+                    Dim y! = top + labelSize.Height / 2
                     Dim barWidth!
                     Dim barRect As Rectangle
 
@@ -131,6 +144,22 @@ Namespace NCBIBlastResult
                         y = y + labelSize.Height + spacing
                     Next
 
+                    ' 绘制Ticks标签
+                    Dim tickSize As SizeF
+                    Dim anchor As PointF
+
+                    For Each tickVal As Double In ticks
+                        x = boxLeft + tickVal / maxCount * boxWidth
+                        tickSize = g.MeasureString(tickVal, tickFont)
+                        anchor = New PointF With {
+                            .X = x - tickSize.Width / 2,
+                            .Y = box.Bottom + spacing + 10
+                        }
+
+                        Call g.DrawLine(tickPen, New PointF(x, anchor.Y), New PointF(x, box.Bottom))
+                        Call g.DrawString(tickVal, tickFont, Brushes.Black, anchor)
+                    Next
+
                     ' 绘制标题和坐标轴的标签信息
                     ' 标题居中位置
                     labelSize = g.MeasureString(title, titleFont)
@@ -141,7 +170,7 @@ Namespace NCBIBlastResult
 
                     labelSize = g.MeasureString(axisLabel, axisLabelFont)
                     x = boxLeft + (boxWidth - labelSize.Width) / 2
-                    y = top + boxHeight + spacing
+                    y = top + boxHeight + spacing * 2 + tickSize.Height
 
                     Call g.DrawString(axisLabel, axisLabelFont, Brushes.Black, x, y)
 
