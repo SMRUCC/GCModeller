d452c3bcb515d98ecae37a5e5856c40fa7ed70e1
xieguigang <xie.guigang@gcmodeller.org>
2018-12-11 13:50:34 +0800

    add test

--- src/runtime/sciBASIC#/Data_science/Darwinism/GeneticAlgorithm/GAF_example/neuronTest.vb
+++ src/runtime/sciBASIC#/Data_science/Darwinism/GeneticAlgorithm/GAF_example/neuronTest.vb
@@ -1,6 +1,31 @@
-﻿Module neuronTest
+﻿Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork
+
+Module neuronTest
 
     Sub Main()
 
+        Dim test As New TrainingUtils(6, 9, 3)
+
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+        Call test.Add({0, 0, 0, 1, 1, 1}, {1, 0, 0})
+
+        Call test.Train()
+
+        Dim classes = test.NeuronNetwork.Compute(0, 0, 0, 1, 1, 1)
+
+        Pause()
     End Sub
 End Module
