ab49eee60e93bc3b4ae45643d3b891c424988f32
xieguigang <xie.guigang@gcmodeller.org>
2018-12-20 09:57:04 +0800

    add training cli

---
+++ src/runtime/sciBASIC#/CLI_tools/ANN/CLI.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork
+Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork.StoreProcedure
+
+Module CLI
+
+    <ExportAPI("/training")>
+    <Usage("/training /samples <sample_matrix.Xml> [/config <config.ini> /parallel /out <ANN.Xml>]")>
+    Public Function Train(args As CommandLine) As Integer
+        Dim in$ = args <= "/samples"
+        Dim parallel As Boolean = args("/parallel")
+        Dim out$ = args("/out") Or $"{[in].TrimSuffix}_ANN_trained.Xml"
+        Dim samples As DataSet = [in].LoadXml(Of DataSet)
+        Dim config As Config
+        Dim trainingHelper As New TrainingUtils(
+            samples.Size.Width, {},
+            samples.OutputSize,
+            config.learnRate,
+            config.momentum
+        )
+
+        For Each sample In samples.DataSamples
+            Call trainingHelper.Add(sample.status, sample.target)
+        Next
+
+        Call trainingHelper.Train()
+
+        Return trainingHelper _
+            .TakeSnapshot _
+            .GetXml _
+            .SaveTo(out) _
+            .CLICode
+    End Function
+End Module
