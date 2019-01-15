6fd2b53a0a1584c95bb83b25564bb310d1093da7
xieguigang <xie.guigang@gcmodeller.org>
2019-01-15 15:41:48 +0800

    create data matrix

--- src/GCModeller/annotations/Proteomics/LabelFree/FoldChangeMatrix.vb
+++ src/GCModeller/annotations/Proteomics/LabelFree/FoldChangeMatrix.vb
@@ -1,4 +1,6 @@
-﻿Imports Microsoft.VisualBasic.Data.csv.IO
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.csv.IO
 Imports SMRUCC.genomics.GCModeller.Workbench.ExperimentDesigner
 
 ''' <summary>
@@ -6,7 +8,51 @@ Imports SMRUCC.genomics.GCModeller.Workbench.ExperimentDesigner
 ''' </summary>
 Public Module FoldChangeMatrix
 
-    Public Iterator Function iTraqMatrix(rawMatrix As IEnumerable(Of DataSet), sampleInfo As SampleGroup(), analysis As AnalysisDesigner()) As IEnumerable(Of DataSet)
-        Dim sampleGroups = sampleInfo.GroupBy(Function(s) s.sample_group).ToArray
+    ''' <summary>
+    ''' 生成的matrix里面的foldchange结果是``experiment/controls``
+    ''' </summary>
+    ''' <param name="rawMatrix">原始的峰面积数据</param>
+    ''' <param name="analysis"></param>
+    ''' <returns></returns>
+    ''' 
+    <Extension>
+    Public Iterator Function iTraqMatrix(rawMatrix As IEnumerable(Of DataSet), analysis As (experiments As String(), controls As String())) As IEnumerable(Of DataSet)
+        For Each protein As DataSet In rawMatrix
+            Dim foldChanges As New Dictionary(Of String, Double)
+
+            For Each experiment As String In analysis.experiments
+                For Each control As String In analysis.controls
+                    foldChanges($"{experiment}/{control}") = protein(experiment) / protein(control)
+                Next
+            Next
+
+            Yield New DataSet With {
+                .ID = protein.ID,
+                .Properties = foldChanges
+            }
+        Next
+    End Function
+
+    Public Iterator Function iTraqMatrix(rawMatrix As IEnumerable(Of DataSet),
+                                         sampleInfo As SampleGroup(),
+                                         analysisDesigners As AnalysisDesigner()) As IEnumerable(Of NamedCollection(Of DataSet))
+        Dim groups = sampleInfo _
+            .GroupBy(Function(s) s.sample_group) _
+            .ToDictionary(Function(g) g.Key,
+                          Function(g) g.Select(Function(s) s.sample_name).ToArray)
+
+        With rawMatrix.ToArray
+            For Each designer As AnalysisDesigner In analysisDesigners
+                Dim controls$() = groups(designer.Controls)
+                Dim treatment$() = groups(designer.Treatment)
+                Dim matrix As DataSet() = .iTraqMatrix((treatment, controls))
+
+                Yield New NamedCollection(Of DataSet) With {
+                    .Name = designer.Title,
+                    .Value = matrix,
+                    .Description = designer.ToString
+                }
+            Next
+        End With
     End Function
 End Module
