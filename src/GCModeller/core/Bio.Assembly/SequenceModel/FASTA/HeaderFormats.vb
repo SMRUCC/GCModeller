70111aa6635a620c52b7115e474cd5c17a05c8bd
xieguigang <xie.guigang@gcmodeller.org>
2019-03-10 15:09:02 +0800

    move to fasta header helper module

---
+++ src/GCModeller/core/Bio.Assembly/SequenceModel/FASTA/HeaderFormats.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace SequenceModel.FASTA
+
+    ''' <summary>
+    ''' Fasta序列在不同的数据库之中的标题的格式的帮助函数模块
+    ''' </summary>
+    Public Module HeaderFormats
+
+        ''' <summary>
+        ''' 在这里移除序列编号之中的版本号
+        ''' </summary>
+        ''' <param name="accession">``XXXXX.1``</param>
+        ''' <returns></returns>
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function TrimAccessionVersion(accession As String) As String
+            Return accession.Split("."c)(Scan0)
+        End Function
+    End Module
+End Namespace
