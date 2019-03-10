1c7ba2c5a2e7b0f0d4d22ff547b2be1ac9981af2
xieguigang <xie.guigang@gcmodeller.org>
2019-03-10 17:11:42 +0800

    fix object reference errors

--- src/repository/Model_Repository/Tables/Uniprot.vb
+++ src/repository/Model_Repository/Tables/Uniprot.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::86da48461b4ebd9ffe8130f327b5d3c2, Model_Repository\Tables\Uniprot.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class Uniprot
-    ' 
-    '     Properties: COG
-    ' 
-    '     Function: CreateObject, GetPath
-    ' 
-    ' /********************************************************************************/
+' Class Uniprot
+' 
+'     Properties: COG
+' 
+'     Function: CreateObject, GetPath
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -66,18 +66,20 @@ Public Class Uniprot : Inherits DbFileSystemObject
         Return FilePath
     End Function
 
-    Public Shared Function CreateObject(Fasta As UniprotFasta, MD5 As String) As Uniprot
-        Dim Entry As New Uniprot With {
-            .Definition = Fasta.ProtName.MySqlEscaping,
-            .GeneName = Fasta.GN,
-            .LocusID = Fasta.EntryName,
+    Public Shared Function CreateObject(fasta As UniprotFasta, MD5 As String) As Uniprot
+        Dim header = fasta.UniProtHeader
+        Dim entry As New Uniprot With {
+            .Definition = header.ProtName.MySqlEscaping,
+            .GeneName = header.GN,
+            .LocusID = header.EntryName,
             .MD5Hash = MD5,
-            .OrganismSpecies = Fasta.OrgnsmSpName.MySqlEscaping,
-            .PE = Fasta.PE,
-            .SV = Fasta.SV,
-            .UniprotID = Fasta.UniprotID
+            .OrganismSpecies = header.OrgnsmSpName.MySqlEscaping,
+            .PE = header.PE,
+            .SV = header.SV,
+            .UniprotID = fasta.UniprotID
         }
-        Return Entry
+
+        Return entry
     End Function
 
     Public Property COG As String Implements DbFileSystemObject.ProteinDescriptionModel.COG
