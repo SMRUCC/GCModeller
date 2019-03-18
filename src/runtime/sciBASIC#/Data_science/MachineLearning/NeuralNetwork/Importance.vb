671b4ba90d5a6adbb9e4c3d0135eb3c797c24832
xieguigang <xie.guigang@gcmodeller.org>
2019-03-18 17:10:49 +0800

    neuron node improtance in ANN

--- src/runtime/sciBASIC#/Data_science/MachineLearning/NeuralNetwork/Importance.vb
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/NeuralNetwork/Importance.vb
@@ -1,8 +1,11 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.csv.IO
-Imports Microsoft.VisualBasic.Math.Quantile
-Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.Math.Quantile
 
 Namespace NeuralNetwork
 
@@ -32,6 +35,36 @@ Namespace NeuralNetwork
             ' 枚举每一个output
             For i As Integer = 0 To model.outputlayer.size - 1
 
+            Next
+        End Function
+
+        ''' <summary>
+        ''' 只计算输入节点和第一层隐藏层的节点之间的权重的和
+        ''' 
+        ''' > https://stats.stackexchange.com/questions/261008/deep-learning-how-do-i-know-which-variables-are-important
+        ''' </summary>
+        ''' <param name="model"></param>
+        ''' <returns>得到的是最原始的权重的和,可以除上最大的权重和来得到相对权重</returns>
+        <Extension>
+        Public Iterator Function SumWeight(model As StoreProcedure.NeuralNetwork, Optional factorNames$() = Nothing) As IEnumerable(Of NamedValue(Of Double))
+            Dim inputs$() = model.inputlayer.AsEnumerable.ToArray
+            Dim firstHidden = model.hiddenlayers _
+                .AsEnumerable _
+                .OrderBy(Function(l) Val(l.id)) _
+                .First _
+                .AsEnumerable _
+                .Indexing
+
+            factorNames = factorNames Or inputs.AsDefault
+
+            For Each input As SeqValue(Of String) In inputs.SeqIterator
+                Dim inputId As String = input.value
+                Dim allw = model.connections _
+                    .Where(Function(n) n.in = inputId AndAlso n.out.IsOneOfA(firstHidden)) _
+                    .Shadows!w _
+                    .Abs
+
+                Yield New NamedValue(Of Double)(factorNames(input), allw.Sum)
             Next
         End Function
     End Module
