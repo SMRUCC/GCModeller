273049f63e48eb987c671fd61531e04e3a034a35
xieguigang <xie.guigang@gcmodeller.org>
2019-03-19 10:20:23 +0800

    add memory test after reset environment

--- src/R.Bioconductor/RDotNET.Extensions.VisualBasic/test/ReSetREnvironmentTest.vb
+++ src/R.Bioconductor/RDotNET.Extensions.VisualBasic/test/ReSetREnvironmentTest.vb
@@ -15,11 +15,23 @@ Module ReSetREnvironmentTest
             Call base.print("Resulted data frame:", [string]:=True)
             Call base.print(!d)
 
+            Dim z As var = 9999.Replicate(100000).ToArray
+
+            Call utils.memory_size.__DEBUG_ECHO
+
             Call .Reset()
         End With
 
+        Call System.GC.Collect()
+
         With RServer.R
+
             Call base.print("After reset:", [string]:=True)
+            Call utils.memory_size.__DEBUG_ECHO
+
+            Pause()
+
+            ' This statement will cause exception as the environment have been reset
             Call base.print("d")
         End With
 
