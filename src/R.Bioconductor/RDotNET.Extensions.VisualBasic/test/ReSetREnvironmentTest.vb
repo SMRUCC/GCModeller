4ab2f33c6198bdfa490165b88c4e487b4da4d16f
xieguigang <xie.guigang@gcmodeller.org>
2019-03-19 09:29:20 +0800

    add dispose test

---
+++ src/R.Bioconductor/RDotNET.Extensions.VisualBasic/test/ReSetREnvironmentTest.vb
@@ -0,0 +1,23 @@
+﻿Imports RDotNET.Extensions.VisualBasic
+Imports RDotNET.Extensions.VisualBasic.API
+Imports RServer = RDotNET.Extensions.VisualBasic.RSystem
+
+Module ReSetREnvironmentTest
+
+    Sub Main()
+        Using RServer.R
+            With RServer.R
+                Dim x As var = 999
+                Dim y As var = False
+
+                !d = base.dataframe(!hello = x, !world = y)
+
+                Call base.print(!d)
+            End With
+        End Using
+
+
+
+        Pause()
+    End Sub
+End Module
