2b0dc516b9ed6c999df70229fe40f5b72652319e
谢桂纲 <xie.guigang@gcmodeller.org>
2019-03-25 18:13:23 +0800

    add iGEM part sequence model

---
+++ src/GCModeller/core/Bio.Assembly/Assembly/iGEM/PartSeq.vb
@@ -0,0 +1,15 @@
+﻿Imports SMRUCC.genomics.SequenceModel
+
+Namespace Assembly.iGEM
+
+    Public Class PartSeq : Implements IPolymerSequenceModel
+
+        Public Property PartName As String
+        Public Property Status As String
+        Public Property Id As String
+        Public Property Type As String
+        Public Property Description As String
+        Public Property SequenceData As String Implements IPolymerSequenceModel.SequenceData
+
+    End Class
+End Namespace
