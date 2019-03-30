dde0530ebd660e5e0ae4ae9a846c8d7cc0bb5817
谢桂纲 <xie.guigang@gcmodeller.org>
2019-03-30 17:53:26 +0800

    file removes

--- src/GCModeller/engine/Dynamics/My Project/AssemblyInfo.vb
+++ src/GCModeller/engine/Dynamics/My Project/AssemblyInfo.vb
@@ -2,34 +2,34 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
+' General Information about an assembly is controlled through the following
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("GCModeller.DynamicsCell")>
-<Assembly: AssemblyDescription("Dynamics cellular network visualization tools.")>
+<Assembly: AssemblyTitle("Dynamics")>
+<Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("GCModeller.DynamicsCell")>
-<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyProduct("Dynamics")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("aa04726d-2d25-42d7-bc64-3634b4c5a5b3")>
+<Assembly: Guid("c67708e1-f8fa-4120-abbb-f169a9fc090b")>
 
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
-'      Minor Version 
+'      Minor Version
 '      Build Number
 '      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers 
+' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
+' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
