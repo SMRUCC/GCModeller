a8bcec7ec6d311ff9a47b3a9c1e1e3c8772e6b7a
xieguigang <xie.guigang@gmail.com>
2019-04-05 20:44:02 +0800

    parse details

--- src/GCModeller/data/ExternalDBSource/DEG/Web/Module1.vb
+++ src/GCModeller/data/ExternalDBSource/DEG/Web/Module1.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports System.Threading
 Imports Microsoft.VisualBasic.Language.C
 Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
 
@@ -45,20 +46,65 @@ Module Module1
         Next
     End Function
 
+    Const detailsAPI As String = "http://origin.tubic.org/deg/public/index.php/information/bacteria/%s.html"
+
     <Extension>
-    Private Function parseDEGList(html As String) As IEnumerable(Of EssentialGene)
+    Private Iterator Function parseDEGList(html As String) As IEnumerable(Of EssentialGene)
         Dim table$ = html.GetTablesHTML.First
         Dim rows = table.GetRowsHTML
+
+        Call Thread.Sleep(1000)
 
         For Each row As String In rows
             Dim columns = row.GetColumnsHTML
 
-
+            Yield New EssentialGene With {
+                .ID = columns(1).StripHTMLTags,
+                .Name = columns(2),
+                .FunctionDescrib = columns(3),
+                .Organism = columns(4)
+            }.fillDetails
         Next
+    End Function
+
+    <Extension>
+    Private Function fillDetails(gene As EssentialGene) As EssentialGene
+        Dim html = sprintf(detailsAPI, gene.ID).GET
+        Dim table = html.GetTablesHTML.First
+        Dim rows = table.GetRowsHTML
+        Dim details = rows.Select(Function(r) r.GetColumnsHTML).ToDictionary(Function(c) c.First.Replace(" ", "_"), Function(c) c.Last)
+
+        With details
+            gene.RefSeq = !Gene_ref
+            gene.UniProt = !Uniprot_accession
+            gene.COG = !COG
+            gene.GO = !GO_annotation.Split
+            gene.FuncClass = !Function_Class
+            gene.Condition = !Condition
+            gene.Reference = !Reference
+            gene.Nt = !Nucleotide_sequence
+            gene.Aa = !Amino_acid_sequence
+        End With
+
+        Return gene
     End Function
 End Module
 
 Public Class EssentialGene
+    Public Property ID As String
+    Public Property Name As String
+    Public Property FunctionDescrib As String
+    Public Property Organism As String
+    Public Property geneRef As String
+    Public Property RefSeq As String
+    Public Property UniProt As String
+    Public Property COG As String
+    Public Property GO As String()
+    Public Property FuncClass As String
+    Public Property Reference As String
+    Public Property Condition As String
+    Public Property Nt As String
+    Public Property Aa As String
 
 End Class
 
