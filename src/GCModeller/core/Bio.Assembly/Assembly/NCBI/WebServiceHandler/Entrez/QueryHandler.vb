e0ad40f3d45862cb7e01d1973ac876d6935341da
xieguigang <xie.guigang@gmail.com>
2019-04-06 11:59:40 +0800

    download genbank file

--- src/GCModeller/core/Bio.Assembly/Assembly/NCBI/WebServiceHandler/Entrez/QueryHandler.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/NCBI/WebServiceHandler/Entrez/QueryHandler.vb
@@ -1,58 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::be38cdc49be9f459d596722acd8b4d08, Bio.Assembly\Assembly\NCBI\WebServiceHandler\Entrez\QueryHandler.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class QueryHandler
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: __startQuery, DownloadCurrentPage
-    '         Class Entry
-    ' 
-    '             Properties: AccessionId, Description, GI
-    ' 
-    '             Function: __buildQuery, __getTAG, (+2 Overloads) DownloadGBK, EntryParser
-    ' 
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class QueryHandler
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: __startQuery, DownloadCurrentPage
+'         Class Entry
+' 
+'             Properties: AccessionId, Description, GI
+' 
+'             Function: __buildQuery, __getTAG, (+2 Overloads) DownloadGBK, EntryParser
+' 
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Text
 Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
-Imports SMRUCC.genomics.Assembly.NCBI.GenBank
-Imports SMRUCC.genomics.Assembly.NCBI.GenBank.GBFF
 
 Namespace Assembly.NCBI.Entrez
 
@@ -105,60 +103,22 @@ Namespace Assembly.NCBI.Entrez
             Const SAVED_FILE As String = "[$SAVED_FILE]"
             Const ACCESSION_ID As String = "[$ACCESSION_ID]"
 
-            ''' <summary>
-            ''' The BioPerl is required for download the genbank file in this function.(本函数会尝试从NCBI服务器之上下载Genbank文件，这个方法的调用需要计算机之上安装有BioPerl)
-            ''' </summary>
-            ''' <param name="work">保存文件的临时文件夹</param>
-            ''' <returns></returns>
-            ''' <remarks></remarks>
-            Public Function DownloadGBK(work As String) As GBFF.File
-                Dim SavedGBK As String = DownloadGBK(work, Me.AccessionId)
-                Dim GBK As GBFF.File = Nothing
+            ' https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=gbwithparts&id=229599883&withparts=on&showgi=1
 
-                If SavedGBK.FileExists(True) Then
-                    GBK = GbkParser.Read(SavedGBK)
-                Else
-                    Call $"Genbank database file download for ""{Me.ToString}""; ACC:={AccessionId} is not successful.".Warning
-                End If
+            Const sviewerApi$ = "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=gbwithparts"
 
-                Return GBK
-            End Function
+            Public Sub Fetch(saveAs$, Optional full As Boolean = True, Optional showgi As Boolean = True)
+                Dim parameters As New Dictionary(Of String, String)
 
-            ''' <summary>
-            ''' Generates the perl process handle for download the genbank database file which was specific by the accessionID.
-            ''' (生成下载Genbank文件所需要的临时脚本文件)
-            ''' </summary>
-            ''' <param name="AccessionID">Gene locus_tag or genome accession id.</param>
-            ''' <param name="Work">Working directory</param>
-            ''' <param name="savedGBK">The downloaded GenBank file save location.</param>
-            ''' <param name="TempScript">Temp Script save location.</param>
-            ''' <returns></returns>
-            Private Shared Function __buildQuery(AccessionID As String, Work As String, ByRef savedGBK As String, ByRef TempScript As String) As Process
-                TempScript = $"{Work}/{Process.GetCurrentProcess.Id}_{Rnd()}_{AccessionID}.pl"
+                parameters!id = AccessionId
+                parameters!withparts = "on" Or "off".When(Not full)
+                parameters!showgi = 0 Or 1.When(showgi)
 
-                Dim p As New ProcessStartInfo("perl", TempScript)
-                Dim Script As New StringBuilder(My.Resources.GenBankQuery) 'Perl script template
+                Dim query$ = parameters.Select(Function(a) $"{a.Key}={a.Value}").JoinBy("&")
+                Dim url = $"{sviewerApi}&{query}"
 
-                savedGBK = $"{Work}/{AccessionID}.gbk"
-                savedGBK = FileIO.FileSystem.GetFileInfo(savedGBK).FullName.Replace("\", "/")
-
-                Call Script.Replace(ACCESSION_ID, AccessionID)
-                Call Script.Replace(SAVED_FILE, savedGBK)
-                Call Script.ToString.SaveTo(TempScript, Encoding.ASCII)
-
-                p.WindowStyle = ProcessWindowStyle.Hidden
-
-                Return New Process() With {.StartInfo = p}
-            End Function
-
-            Public Shared Function DownloadGBK(Work As String, AccessionID As String) As String
-                Dim SavedGBK As String = "", TempScript As String = ""
-
-                Call __buildQuery(AccessionID, Work, SavedGBK, TempScript).Invoke()
-                Call FileIO.FileSystem.DeleteFile(TempScript, FileIO.UIOption.OnlyErrorDialogs, FileIO.RecycleOption.DeletePermanently)
-
-                Return SavedGBK
-            End Function
+                Call url.DownloadFile(saveAs)
+            End Sub
 
             Public Shared Function EntryParser(str As String) As Entry
                 Dim EntryObj As Entry = New Entry
