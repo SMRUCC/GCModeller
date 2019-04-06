6a46f36e09ea7abf6ebfed47f5c03117e65450b6
xieguigang <xie.guigang@gmail.com>
2019-04-06 11:00:47 +0800

    run test

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/test/htmlParserTest.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/test/htmlParserTest.vb
@@ -1,8 +1,13 @@
-﻿Module htmlParserTest
+﻿Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
+
+Module htmlParserTest
 
     Sub Main()
         Dim testhtml = "D:\GCModeller\src\runtime\sciBASIC#\Microsoft.VisualBasic.Core\test\testhtml.txt".ReadAllText
 
-        Dim list = testhtml.
+        Dim list = testhtml.HtmlList
+
+
+        Pause()
     End Sub
 End Module
