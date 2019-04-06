9dd9713df70fb34dcece99472efb45c6ada7c2d0
xieguigang <xie.guigang@gmail.com>
2019-04-06 14:42:24 +0800

    add union sequence model

--- src/GCModeller/CLI_tools/S.M.A.R.T/CLI/CLI.vb
+++ src/GCModeller/CLI_tools/S.M.A.R.T/CLI/CLI.vb
@@ -1,51 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::a312225bfed4d6fd3cbe3f0a2f0a8ffc, CLI_tools\S.M.A.R.T\CLI\CLI.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CLI
-    ' 
-    '     Function: BuildCache, ExportPfamString
-    ' 
-    ' /********************************************************************************/
+' Module CLI
+' 
+'     Function: BuildCache, ExportPfamString
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Data.csv.IO.Linq
 Imports Microsoft.VisualBasic.Extensions
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Microsoft.VisualBasic.Terminal.STDIO
 Imports SMRUCC.genomics.Assembly.NCBI.CDD
+Imports SMRUCC.genomics.Data.DEG
+Imports SMRUCC.genomics.Data.DEG.Web
 Imports SMRUCC.genomics.Data.Xfam
 Imports SMRUCC.genomics.Interops
 Imports SMRUCC.genomics.Interops.NCBI.Extensions
@@ -87,7 +91,28 @@ Public Module CLI
     Public Function FetchDEG(args As CommandLine) As Integer
         Dim save$ = args("/save") Or "./essentialgenes/"
 
-        Call SMRUCC.genomics.Data.DEG.Web.WebParser.ParserWorkflow(save)
+        ' 下载数据
+        Call WebParser.ParserWorkflow(save)
+
+        ' 然后建立表格
+        Using ntTable As New WriteStream(Of SeqRef)($"{save}/nt.xls", tsv:=True),
+              prTable As New WriteStream(Of SeqRef)($"{save}/prot.xls", tsv:=True)
+
+            For Each genomeXml As String In save.EnumerateFiles
+                Dim genome As Genome = genomeXml.LoadXml(Of Genome)
+
+                For Each gene As EssentialGene In genome.AsEnumerable
+                    Dim ref As New SeqRef With {
+                        .fullName = gene.FunctionDescrib
+                    }
+
+                    ref.SequenceData = gene.Nt
+                    Call ntTable.Flush(ref)
+                    ref.SequenceData = gene.Aa
+                    Call prTable.Flush(ref)
+                Next
+            Next
+        End Using
 
         Return 0
     End Function
---
+++ src/GCModeller/data/ExternalDBSource/DEG/SeqRef.vb
@@ -0,0 +1,20 @@
+﻿Imports SMRUCC.genomics.SequenceModel
+
+Namespace DEG
+
+    ''' <summary>
+    ''' The sequence summary and reference data
+    ''' </summary>
+    Public Class SeqRef : Implements IPolymerSequenceModel
+
+        Public Property ID As String
+        Public Property Xref As String
+        Public Property geneName As String
+        Public Property isVirulence As Boolean
+        Public Property fullName As String
+        Public Property Organism As String
+        Public Property Reference As String
+        Public Property SequenceData As String Implements IPolymerSequenceModel.SequenceData
+
+    End Class
+End Namespace
