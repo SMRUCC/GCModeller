3775b9e973f6b858258ccf30695a98c19b19c52b
xieguigang <xie.guigang@gmail.com>
2019-04-07 14:06:27 +0800

    add namespace

--- src/GCModeller/engine/Dynamics/Core/Directions.vb
+++ src/GCModeller/engine/Dynamics/Core/Directions.vb
@@ -1,11 +1,14 @@
-﻿''' <summary>
-''' 反应过程的方向
-''' </summary>
-Public Enum Directions As Integer
+﻿Namespace Core
+
     ''' <summary>
-    ''' 反应过程将不会发生
+    ''' 反应过程的方向
     ''' </summary>
-    [stop] = 0
-    forward = 1
-    reverse = -1
-End Enum
+    Public Enum Directions As Integer
+        ''' <summary>
+        ''' 反应过程将不会发生
+        ''' </summary>
+        [stop] = 0
+        forward = 1
+        reverse = -1
+    End Enum
+End Namespace
