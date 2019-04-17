5cfabf7a4567c0876e8b0ed96c474450d9527cd9
xieguigang <xie.guigang@gcmodeller.org>
2019-04-17 13:56:09 +0800

    improvements of the xml data model comment info helper

---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Text/Xml/Linq/DataSetWriter.vb
@@ -0,0 +1,55 @@
+﻿Imports System.IO
+
+Namespace Text.Xml.Linq
+
+    ''' <summary>
+    ''' Write a very large dataset in Xml format
+    ''' </summary>
+    Public Class DataSetWriter(Of T) : Implements IDisposable
+
+        Dim file As StreamWriter
+
+        Sub New(file As String, Optional encoding As Encodings = Encodings.UTF8)
+            Me.file = file.OpenWriter(encoding)
+        End Sub
+
+        Public Sub Write(data As T)
+
+        End Sub
+
+#Region "IDisposable Support"
+        Private disposedValue As Boolean ' To detect redundant calls
+
+        ' IDisposable
+        Protected Overridable Sub Dispose(disposing As Boolean)
+            If Not disposedValue Then
+                If disposing Then
+                    ' TODO: dispose managed state (managed objects).
+                    Call file.Flush()
+                    Call file.Close()
+                    Call file.Dispose()
+                End If
+
+                ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+                ' TODO: set large fields to null.
+            End If
+            disposedValue = True
+        End Sub
+
+        ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+        'Protected Overrides Sub Finalize()
+        '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        '    Dispose(False)
+        '    MyBase.Finalize()
+        'End Sub
+
+        ' This code added by Visual Basic to correctly implement the disposable pattern.
+        Public Sub Dispose() Implements IDisposable.Dispose
+            ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+            Dispose(True)
+            ' TODO: uncomment the following line if Finalize() is overridden above.
+            ' GC.SuppressFinalize(Me)
+        End Sub
+#End Region
+    End Class
+End Namespace
