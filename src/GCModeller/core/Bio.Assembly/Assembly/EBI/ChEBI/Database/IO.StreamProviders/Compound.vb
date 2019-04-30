eb4a2d61d5767486a9ea3766b59c5a23c11e49bd
xieguigang <xie.guigang@gcmodeller.org>
2019-04-30 16:21:59 +0800

    code style improvements

--- src/GCModeller/core/Bio.Assembly/Assembly/EBI/ChEBI/Database/IO.StreamProviders/Compound.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/EBI/ChEBI/Database/IO.StreamProviders/Compound.vb
@@ -1,52 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::bc2b4d6ef3afe2f008ab61fea7f67746, Bio.Assembly\Assembly\EBI\ChEBI\Database\IO.StreamProviders\Compound.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Compound
-    ' 
-    '         Properties: CHEBI_ACCESSION, CREATED_BY, DEFINITION, ID, MODIFIED_ON
-    '                     NAME, PARENT_ID, SOURCE, STAR, STATUS
-    ' 
-    '         Function: Load, LoadTable, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Compound
+' 
+'         Properties: CHEBI_ACCESSION, CREATED_BY, DEFINITION, ID, MODIFIED_ON
+'                     NAME, PARENT_ID, SOURCE, STAR, STATUS
+' 
+'         Function: Load, LoadTable, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
-Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
 
@@ -76,34 +75,39 @@ Namespace Assembly.EBI.ChEBI.Database.IO.StreamProviders.Tsv
         Public Shared Function LoadTable(path$) As Dictionary(Of String, Compound)
             Dim data As Compound() = Load(path)
             Dim nameds As NamedValue(Of Compound)() = data _
-                .Select(Function(c) New NamedValue(Of Compound) With {
-                    .Name = c.CHEBI_ACCESSION.GetTagValue(":", trim:=True).Value,
-                    .Value = c
-                }).ToArray
+                .Select(Function(c)
+                            Return New NamedValue(Of Compound) With {
+                                .Name = c.CHEBI_ACCESSION.GetTagValue(":", trim:=True).Value,
+                                .Value = c
+                            }
+                        End Function) _
+                .ToArray
+
             Return nameds.ToDictionary(Function(k) k.Name, Function(v) v.Value)
         End Function
 
-        Public Shared Function Load(path$) As Compound()
+        Public Shared Iterator Function Load(path$) As IEnumerable(Of Compound)
             Dim index As Index(Of String) = path.TsvHeaders
-            Dim out As New List(Of Compound)
+            Dim compound As Compound
+            Dim tParts$()
 
             For Each line As String In path.IterateAllLines.Skip(1)
-                Dim t$() = line.Split(ASCII.TAB)
-                out += New Compound With {
-                    .CHEBI_ACCESSION = t(index(NameOf(.CHEBI_ACCESSION))),
-                    .CREATED_BY = t(index(NameOf(.CREATED_BY))),
-                    .DEFINITION = t(index(NameOf(.DEFINITION))),
-                    .ID = t(index(NameOf(.ID))),
-                    .MODIFIED_ON = t(index(NameOf(.MODIFIED_ON))),
-                    .NAME = t(index(NameOf(.NAME))),
-                    .PARENT_ID = t(index(NameOf(.PARENT_ID))),
-                    .SOURCE = t(index(NameOf(.SOURCE))),
-                    .STAR = t(index(NameOf(.STAR))),
-                    .STATUS = t(index(NameOf(.STATUS)))
+                tParts = line.Split(ASCII.TAB)
+                compound = New Compound With {
+                    .CHEBI_ACCESSION = tParts(index(NameOf(.CHEBI_ACCESSION))),
+                    .CREATED_BY = tParts(index(NameOf(.CREATED_BY))),
+                    .DEFINITION = tParts(index(NameOf(.DEFINITION))),
+                    .ID = tParts(index(NameOf(.ID))),
+                    .MODIFIED_ON = tParts(index(NameOf(.MODIFIED_ON))),
+                    .NAME = tParts(index(NameOf(.NAME))),
+                    .PARENT_ID = tParts(index(NameOf(.PARENT_ID))),
+                    .SOURCE = tParts(index(NameOf(.SOURCE))),
+                    .STAR = tParts(index(NameOf(.STAR))),
+                    .STATUS = tParts(index(NameOf(.STATUS)))
                 }
-            Next
 
-            Return out
+                Yield compound
+            Next
         End Function
     End Class
 End Namespace
--- src/GCModeller/core/Bio.Assembly/Assembly/EBI/ChEBI/EntityModel/EntitySummary.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/EBI/ChEBI/EntityModel/EntitySummary.vb
@@ -5,5 +5,20 @@
     ''' </summary>
     Public Class EntitySummary
 
+        Public Property chebiId As String
+        Public Property chebiAsciiName As String
+        Public Property smiles As String
+        Public Property charge As Integer
+        Public Property mass As Double
+        Public Property formulae As String
+        Public Property secondaryChebiIds As String()
+        Public Property kegg_Ids As String()
+        Public Property wikipedia As String
+        Public Property inchi As String
+        Public Property inchiKey As String
+        Public Property cas As String()
+        Public Property hmdb As String
+        Public Property biosamples As String()
+
     End Class
 End Namespace
