82434893ff416391d23f22350f826cd9070365da
xieguigang <xie.guigang@gcmodeller.org>
2019-05-14 16:32:58 +0800

    add gc helper

---
+++ src/R.Bioconductor/RDotNET.Extensions.VisualBasic/Server/GC.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Runtime.CompilerServices
+Imports RDotNET.Extensions.VisualBasic.API
+Imports RDotNET.Extensions.VisualBasic.SymbolBuilder
+
+Public Module GC
+
+    Dim objects As New List(Of String)
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Sub Add(name As String)
+        Call objects.Add(name)
+    End Sub
+
+    ''' <summary>
+    ''' 一次性的将R环境之中的通过<see cref="Add"/>方法所添加的对象进行删除
+    ''' </summary>
+    Public Sub [Do]()
+        Dim names = base.c(objects.ToArray, stringVector:=True)
+
+        Call base.rm(list:=names)
+        Call base.rm(list:=names.Rstring)
+        Call base.gc()
+        Call objects.Clear()
+    End Sub
+End Module
