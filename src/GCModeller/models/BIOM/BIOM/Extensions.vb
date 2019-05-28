1374d223551a65ae8ed9e5c0847855115be63c36
xieguigang <xie.guigang@gcmodeller.org>
2019-05-28 15:38:45 +0800

    add conversion on read auto helper

--- src/GCModeller/models/BIOM/BIOM/Extensions.vb
+++ src/GCModeller/models/BIOM/BIOM/Extensions.vb
@@ -2,6 +2,7 @@
 Imports Microsoft.VisualBasic.Scripting.Runtime
 Imports Microsoft.VisualBasic.Scripting.SymbolBuilder.VBLanguage
 Imports Microsoft.VisualBasic.Serialization.JSON
+Imports SMRUCC.genomics.foundation.BIOM.v10
 
 <HideModuleName> Public Module Extensions
 
@@ -9,21 +10,33 @@ Imports Microsoft.VisualBasic.Serialization.JSON
     ''' 自动根据文件拓展名以及hdf5文件版本来选择读取程序
     ''' </summary>
     ''' <param name="path"></param>
+    ''' <param name="denseMatrix">
+    ''' 当这个参数为真的时候，所返回的json对象之中的矩阵数据都会被强制的转换为全矩阵
+    ''' </param>
     ''' <returns></returns>
-    Public Function ReadAuto(path As String) As v10.Json(Of Double)
+    Public Function ReadAuto(path As String, Optional denseMatrix As Boolean = False) As v10.Json(Of Double)
+        Dim BIOM As v10.Json(Of Double)
+
         If path.ExtensionSuffix.TextEquals("json") Then
-            Return v10.FloatMatrix.LoadFile(path)
+            BIOM = v10.FloatMatrix.LoadFile(path)
         Else
             With BIOMVersion(path)
                 If .TextEquals("2.0") Then
-                    Return v20.ReadFile(path)
+                    BIOM = v20.ReadFile(path)
                 ElseIf .TextEquals("2.1") Then
-                    Return v21.ReadFile(path)
+                    BIOM = v21.ReadFile(path)
                 Else
                     Throw New NotSupportedException
                 End If
             End With
         End If
+
+        If denseMatrix AndAlso BIOM.RequiredConvertToDenseMatrix Then
+            BIOM.matrix_type = matrix_type.dense
+            BIOM.data = BIOM.data.ToDenseMatrix(BIOM.shape)
+        End If
+
+        Return BIOM
     End Function
 
     Public Function BIOMVersion(path As String) As String
