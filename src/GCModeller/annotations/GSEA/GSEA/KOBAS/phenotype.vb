57300dd652b0190e2f13566ab8219ef66fc61f45
xieguigang <xie.guigang@gcmodeller.org>
2019-05-30 11:31:15 +0800

    add phenotype data model

---
+++ src/GCModeller/annotations/GSEA/GSEA/KOBAS/phenotype.vb
@@ -0,0 +1,13 @@
+﻿Public Class phenotype
+
+    Public Property labels As String()
+    Public Property phnameA As String
+    Public Property phnameB As String
+    Public Property sample_num As Integer
+    Public Property sample0 As Integer
+    Public Property sample1 As Integer
+
+    Sub New(cls As CLS)
+
+    End Sub
+End Class
