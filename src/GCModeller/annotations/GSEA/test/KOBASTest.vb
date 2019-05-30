300b225a8a4d638982c944460a46c782b5461a19
xieguigang <xie.guigang@gcmodeller.org>
2019-05-30 10:41:45 +0800

    get hit matrix test success

--- src/GCModeller/annotations/GSEA/test/KOBASTest.vb
+++ src/GCModeller/annotations/GSEA/test/KOBASTest.vb
@@ -1,6 +1,17 @@
-﻿Module KOBASTest
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports SMRUCC.genomics.Analysis.HTS.GSEA
+
+Module KOBASTest
 
     Sub Main()
 
+        Dim genelist = {"A", "B", "C", "D", "E", "F", "G"}
+        Dim background As Index(Of String) = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"}
+
+        Dim name = {"test"}
+
+        Dim m = KOBAS_GSEA.get_hit_matrix(genelist, genelist.Length, name, {}, {background}, 3, 1000)
+
+        Pause()
     End Sub
 End Module
