7cb7c787d5188fe780dcfc330e6ef2af8c9c33a5
xieguigang <xie.guigang@gcmodeller.org>
2019-05-31 10:10:34 +0800

    enrichment test failure

--- src/GCModeller/annotations/GSEA/test/Module1.vb
+++ src/GCModeller/annotations/GSEA/test/Module1.vb
@@ -12,7 +12,7 @@ Imports SMRUCC.genomics.Data.GeneOntology.OBO
 Module Module1
 
     Sub Main()
-        Call buildFromKOBAS()
+        ' Call buildFromKOBAS()
         '  Call KEGGmodelBuildTest()
         ' Call modelBuildTest()
         Call enrichmentTest()
@@ -27,13 +27,14 @@ Module Module1
     End Sub
 
     Sub enrichmentTest()
-        Dim background = "E:\GCModeller\src\GCModeller\annotations\GSEA\xcb.Xml".LoadXml(Of Background)
-        Dim list$() = "E:\GCModeller\src\GCModeller\annotations\GSEA\xcb_TCS.txt" _
-            .IterateAllLines _
+        Dim folder = "D:\GCModeller\src\GCModeller\annotations\GSEA\data"
+
+        '  Dim background = $"{folder}\xcb.Xml".LoadXml(Of Background)
+        Dim list$() = $"{folder}\xcb_TCS.txt".IterateAllLines _
             .Select(Function(l) l.Split.First) _
             .ToArray
 
-        With UniProtXML.EnumerateEntries("E:\GCModeller\src\GCModeller\annotations\GSEA\uniprot-taxonomy%3A314565.xml") _
+        With UniProtXML.EnumerateEntries($"{folder}\uniprot-taxonomy_314565.xml") _
                        .Where(Function(prot) prot.Xrefs.ContainsKey("KEGG")) _
                        .ToDictionary(Function(prot)
                                          Return prot.Xrefs!KEGG.First.id.Split(":"c).Last
@@ -46,9 +47,9 @@ Module Module1
         ' Dim result = background.Enrichment(list).FDRCorrection.ToArray
 
         'Call result.SaveTo("./result.csv")
-        Dim result = "E:\GCModeller\src\GCModeller\annotations\GSEA\xcb_KO.Xml".LoadXml(Of Background).Enrichment(list).FDRCorrection.ToArray
+        Dim result = $"{folder}\xcb_KO.Xml".LoadXml(Of Background).Enrichment(list).FDRCorrection.ToArray
 
-        Call result.SaveTo("./result_KO.csv")
+        Call result.SaveTo($"{folder}/result_KO.csv")
     End Sub
 
     Sub modelBuildTest()
@@ -62,12 +63,14 @@ Module Module1
     End Sub
 
     Sub KEGGmodelBuildTest()
+        Dim folder$ = "D:\GCModeller\src\GCModeller\annotations\GSEA\data\"
+
         Dim kegg = (ls - l - r - "*.Xml" <= "D:\GCModeller-CAD-blueprint\KGML\maps").Select(AddressOf LoadXml(Of Map))
-        Dim uniprot = UniProtXML.EnumerateEntries("E:\GCModeller\src\GCModeller\annotations\GSEA\uniprot-taxonomy%3A314565.xml")
+        Dim uniprot = UniProtXML.EnumerateEntries($"{folder}\uniprot-taxonomy_314565.xml")
         Dim model As Background = GSEA.Imports.ImportsUniProt(uniprot, GSEA.UniProtGetKOTerms, define:=GSEA.KEGGClusters(kegg))
 
 
-        Call model.GetXml.SaveTo("E:\GCModeller\src\GCModeller\annotations\GSEA\xcb_KO.Xml")
+        Call model.GetXml.SaveTo($"{folder}\xcb_KO.Xml")
 
         Pause()
     End Sub
