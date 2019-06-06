2c8dfcd7fc6cc53c4d2063b23a6b2fd3010e036b
xieguigang <xie.guigang@gcmodeller.org>
2019-06-06 15:51:16 +0800

    ggi mask common operation

---
+++ src/GCModeller/annotations/GSEA/PFSNet/PFSNet/masked_ggi.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports SMRUCC.genomics.Analysis.PFSNet.DataStructure
+
+<HideModuleName> Module masked_ggi
+
+    <Extension>
+    Public Function ggi_mask(ggi As IEnumerable(Of GraphEdge), genelist As Index(Of String))
+        Return ggi _
+            .Select(Function(i)
+                        If i.g1 Like genelist AndAlso i.g2 Like genelist Then
+                            Return True
+                        Else
+                            Return False
+                        End If
+                    End Function) _
+            .ToArray
+    End Function
+End Module
