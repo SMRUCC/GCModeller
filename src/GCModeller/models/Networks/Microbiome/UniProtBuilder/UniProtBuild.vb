56c6c1739f81d253605e365611737b53541d9f36
xieguigang <xie.guigang@gcmodeller.org>
2019-06-10 15:32:30 +0800

    split uniprot builder code

--- src/GCModeller/models/Networks/Microbiome/UniProtBuilder/UniProtBuild.vb
+++ src/GCModeller/models/Networks/Microbiome/UniProtBuilder/UniProtBuild.vb
@@ -89,15 +89,6 @@ Public Module UniProtBuild
         Return model
     End Function
 
-    <Extension>
-    Public Sub CopyTo(cache As (KO_list$, taxonomy$, counts$), destination$)
-        With destination & "/"
-            Call cache.KO_list.FileCopy(.ByRef)
-            Call cache.taxonomy.FileCopy(.ByRef)
-            Call cache.counts.FileCopy(.ByRef)
-        End With
-    End Sub
-
     ''' <summary>
     ''' 从一个已经缓存有数据的文件夹之中构建模型
     ''' </summary>
@@ -107,6 +98,8 @@ Public Module UniProtBuild
     Public Function ScanModels(cache As String) As TaxonomyRepository
         Return (cache & "/" & KO_list, cache & "/" & Taxonomy_data, cache & "/" & gene_counts).ScanModels
     End Function
+
+    Const blankPattern$ = "\+{10,}\n"
 
     ''' <summary>
     ''' 
@@ -232,80 +225,4 @@ Public Module UniProtBuild
         Next
     End Function
 
-    Const KO_list$ = "KO.list"
-    Const Taxonomy_data$ = "taxonomy.txt"
-    Const gene_counts$ = "gene.counts"
-    Const blank$ = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    Const blankPattern$ = "\+{10,}\n"
-
-    ''' <summary>
-    ''' 函数返回来的是临时文件夹的路径位置
-    ''' </summary>
-    ''' <param name="UniProtXml"></param>
-    ''' <param name="tmp">工作区缓存文件夹</param>
-    ''' <returns></returns>
-    <Extension>
-    Private Function ScanInternal(UniProtXml As IEnumerable(Of entry), tmp$) As (KO_list$, taxonomy$, counts$)
-        ' KO 缓存文件示例
-        ' 
-        ' taxonomy_id KO
-        ' taxonomy_id KO
-        ' taxonomy_id KO
-        '
-        ' 
-        Dim taxonomyIndex As New Dictionary(Of String, String)
-
-        Using KO As StreamWriter = $"{tmp}/{KO_list}".OpenWriter,
-            taxon As StreamWriter = $"{tmp}/{Taxonomy_data}".OpenWriter,
-            counts As StreamWriter = $"{tmp}/{gene_counts}".OpenWriter
-
-            For Each protein As entry In UniProtXml _
-                .Where(Function(org)
-                           Return Not org.organism Is Nothing AndAlso
-                                  Not org.organism.dbReference Is Nothing
-                       End Function)
-
-                Dim taxonomy As organism = protein.organism
-                Dim taxonID$ = taxonomy.dbReference.id
-
-                ' 为了计算出覆盖度，需要知道基因组之中有多少个基因的记录
-                ' 在这里直接记录下物种的编号，在后面分组计数就可以了解基因组
-                ' 之中的基因的总数了
-                Call counts.WriteLine(taxonID)
-
-                ' 如果已经存在index了，则不会写入taxo文件之中
-                If Not taxonomyIndex.ContainsKey(taxonID) Then
-                    Call taxon.WriteLine(taxonomy.GetXml)
-                    Call taxon.WriteLine(blank)
-                    Call taxonomyIndex.Add(taxonID, "null")
-                End If
-
-                Dim KOlist$() = protein.Xrefs _
-                    .TryGetValue("KO") _
-                    .SafeQuery _
-                    .Select(Function(KEGG) KEGG.id) _
-                    .ToArray
-
-                If KOlist.Length > 0 Then
-                    Call KO.WriteLine(
-                        value:=KOlist _
-                            .Select(Function(id)
-                                        Return taxonID & vbTab & id
-                                    End Function) _
-                            .JoinBy(KO.NewLine)
-                    )
-                End If
-            Next
-
-            Call KO.Flush()
-            Call counts.Flush()
-            Call taxon.Flush()
-        End Using
-
-        Return (
-            $"{tmp}/{KO_list}",
-            $"{tmp}/{Taxonomy_data}",
-            $"{tmp}/{gene_counts}"
-        )
-    End Function
 End Module
