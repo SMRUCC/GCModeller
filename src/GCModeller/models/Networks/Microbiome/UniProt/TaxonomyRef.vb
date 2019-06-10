1b090f2aad7632b4f4ede2c0648bf56d3e70663d
xieguigang <xie.guigang@gcmodeller.org>
2019-06-10 14:16:10 +0800

    fix method reference errors

--- src/GCModeller/models/Networks/Microbiome/UniProt/TaxonomyRef.vb
+++ src/GCModeller/models/Networks/Microbiome/UniProt/TaxonomyRef.vb
@@ -1,49 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::36c177ae9bca4bf2fd550df54a82daf4, models\Networks\Microbiome\UniProt\TaxonomyRef.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class TaxonomyRef
-    ' 
-    '     Properties: Coverage, genome, KOTerms, organism, TaxonID
-    '                 TaxonomyString
-    ' 
-    '     Function: ToString
-    ' 
-    ' /********************************************************************************/
+' Class TaxonomyRef
+' 
+'     Properties: Coverage, genome, KOTerms, organism, TaxonID
+'                 TaxonomyString
+' 
+'     Function: ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
@@ -53,7 +54,8 @@ Imports SMRUCC.genomics.Metagenomics
 ''' <summary>
 ''' Combine the UniProt taxonomy information with the KEGG orthology reference.
 ''' </summary>
-Public Class TaxonomyRef : Implements IKeyedEntity(Of String)
+Public Class TaxonomyRef : Inherits XmlDataModel
+    Implements IKeyedEntity(Of String)
 
     ''' <summary>
     ''' The NCBI taxonomy id
--- src/GCModeller/models/Networks/Microbiome/UniProt/TaxonomyRepository.vb
+++ src/GCModeller/models/Networks/Microbiome/UniProt/TaxonomyRepository.vb
@@ -1,83 +1,97 @@
 ﻿#Region "Microsoft.VisualBasic::0d6beec1fe2218dd49f92d02743d8a39, models\Networks\Microbiome\UniProt\TaxonomyRepository.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class TaxonomyRepository
-    ' 
-    '     Properties: Taxonomy
-    ' 
-    '     Function: Exists, GetAll, GetByKey, GetWhere, Selects
-    '               TaxonomyGroup
-    ' 
-    ' /********************************************************************************/
+' Class TaxonomyRepository
+' 
+'     Properties: Taxonomy
+' 
+'     Function: Exists, GetAll, GetByKey, GetWhere, Selects
+'               TaxonomyGroup
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
-Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
 Imports SMRUCC.genomics.Analysis.Metagenome
 Imports SMRUCC.genomics.Metagenomics
 
 ''' <summary>
-''' 使用NCBI的物种编号作为主键的
+''' A taxonomy genome database which created based on the uniprot database.
+''' The reference data is key indexed by <see cref="TaxonomyRef.TaxonID"/>
+''' (使用NCBI的物种编号作为主键的)
 ''' </summary>
-Public Class TaxonomyRepository : Implements IRepositoryRead(Of String, TaxonomyRef)
+''' <remarks>
+''' #### 2019-06-10 因为应用于微生物组分析的话,因为微生物的物种数量很多,这个参考库往往很大
+''' 所以在这里不建议将物种参考数据都放在一个文件之中,这样子会造成数据的提取效率过低
+''' </remarks>
+Public Class TaxonomyRepository
+    Implements IFileReference
+    Implements IRepositoryRead(Of String, TaxonomyRef)
 
-    Dim taxonIDtable As Dictionary(Of String, TaxonomyRef)
+    ''' <summary>
+    ''' ``[taxonomy_id => lineage]``
+    ''' 
+    ''' 这个参考库应该是使用json格式保存的
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property taxonomy As Dictionary(Of String, Taxonomy)
 
-    <XmlElement("taxonomy")>
-    Public Property Taxonomy As TaxonomyRef()
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Get
-            Return taxonIDtable.Values.ToArray
-        End Get
-        Set(value As TaxonomyRef())
-            taxonIDtable = value _
-                .ToDictionary(Function(t)
-                                  Return t.TaxonID
-                              End Function)
-        End Set
-    End Property
+    ''' <summary>
+    ''' [taxonomy_id => reference]
+    ''' </summary>
+    Dim cache As Dictionary(Of String, TaxonomyRef)
+
+    ''' <summary>
+    ''' 用于查找给定的编号的物种参考数据信息
+    ''' </summary>
+    ''' <returns></returns>
+    Private Property base As String Implements IFileReference.FilePath
 
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function TaxonomyGroup(rank As TaxonomyRanks) As Dictionary(Of String, TaxonomyRef())
-        Return taxonIDtable _
-            .Values _
+        Return taxonomy _
             .GroupBy(Function(t)
-                         Return t.TaxonomyString.TaxonomyRankString(rank)
+                         Return t.Value.TaxonomyRankString(rank)
                      End Function) _
             .ToDictionary(Function(g) g.Key,
-                          Function(g) g.ToArray)
+                          Function(g)
+                              Return g.Keys _
+                                  .Select(AddressOf GetByKey) _
+                                  .ToArray
+                          End Function)
     End Function
 
     ' 2018-3-12
@@ -85,35 +99,76 @@ Public Class TaxonomyRepository : Implements IRepositoryRead(Of String, Taxonomy
 
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function Selects(range As Taxonomy) As IEnumerable(Of TaxonomyRef)
-        Return taxonIDtable _
-            .Values _
+        Return taxonomy _
             .SelectByTaxonomyRange(
                 ref:=range,
-                getTaxonomy:=Function(genome) genome.TaxonomyString,
-                getValue:=Function(genome) genome
-            )
+                getTaxonomy:=Function(genome) genome.Value,
+                getValue:=Function(genome) genome.Key
+            ).Select(Function(taxid)
+                         Return GetByKey(taxid)
+                     End Function)
     End Function
 
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function Exists(key As String) As Boolean Implements IRepositoryRead(Of String, TaxonomyRef).Exists
-        Return taxonIDtable.ContainsKey(key)
+        Return taxonomy.ContainsKey(key)
     End Function
 
+    ''' <summary>
+    ''' Get cache or load from file.(这个函数会利用缓存来加速数据的查找效率)
+    ''' </summary>
+    ''' <param name="key">NCBI taxonomy id</param>
+    ''' <returns></returns>
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetByKey(key As String) As TaxonomyRef Implements IRepositoryRead(Of String, TaxonomyRef).GetByKey
-        Return taxonIDtable(key)
+        Return cache.ComputeIfAbsent(key, lazyValue:=AddressOf LoadByTaxonomyId)
     End Function
 
+    ''' <summary>
+    ''' 这个函数不考虑Cache,直接从文件加载数据
+    ''' </summary>
+    ''' <param name="taxid"></param>
+    ''' <returns></returns>
+    Public Function LoadByTaxonomyId(taxid As String) As TaxonomyRef
+        Dim taxonomy As Taxonomy = Me.taxonomy(taxid)
+        Dim path As String = $"{StorageReference(taxonomy, relative:=False)}/{taxid}.Xml"
+
+        Return path.LoadXml(Of TaxonomyRef)
+    End Function
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="taxonomy"></param>
+    ''' <returns></returns>
+    Public Function StorageReference(taxonomy As Taxonomy, Optional relative As Boolean = True) As String
+        Dim part$ = taxonomy.ToArray.JoinBy("/").Trim("/"c, "_"c)
+        Dim groupName$ = $"/{part}"
+
+        If Not relative Then
+            groupName = $"{base.ParentPath}/{groupName}"
+        End If
+
+        Return groupName
+    End Function
+
+    ''' <summary>
+    ''' 并不推荐使用这个函数以及<see cref="GetAll()"/>函数
+    ''' </summary>
+    ''' <param name="clause"></param>
+    ''' <returns></returns>
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetWhere(clause As Func(Of TaxonomyRef, Boolean)) As IReadOnlyDictionary(Of String, TaxonomyRef) Implements IRepositoryRead(Of String, TaxonomyRef).GetWhere
-        Return taxonIDtable _
-            .Values _
-            .Where(clause) _
-            .ToDictionary(Function(taxon) taxon.TaxonID)
+        Return taxonomy.Keys.Select(Function(tax) (taxid:=tax, GetByKey(tax))) _
+            .Where(Function(tax)
+                       Return clause(tax.Item2)
+                   End Function) _
+            .ToDictionary(Function(taxon) taxon.taxid,
+                          Function(taxon) taxon.Item2)
     End Function
 
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetAll() As IReadOnlyDictionary(Of String, TaxonomyRef) Implements IRepositoryRead(Of String, TaxonomyRef).GetAll
-        Return New Dictionary(Of String, TaxonomyRef)(taxonIDtable)
+        Return taxonomy.Keys.ToDictionary(Function(taxid) taxid, Function(taxid) GetByKey(taxid))
     End Function
 End Class
