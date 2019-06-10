4df61f771318bf554468296391a88fd1dd214b67
xieguigang <xie.guigang@gcmodeller.org>
2019-06-10 08:36:11 +0800

    split source files

--- src/GCModeller/annotations/GSEA/GSEA/KnowledgeBase/Cluster.vb
+++ src/GCModeller/annotations/GSEA/GSEA/KnowledgeBase/Cluster.vb
@@ -49,7 +49,6 @@
 
 Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
-Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
@@ -94,24 +93,5 @@ Public Class Cluster : Implements INamedValue
 
     Public Overrides Function ToString() As String
         Return ID
-    End Function
-End Class
-
-''' <summary>
-''' 假设基因组是有许多个功能聚类的集合构成的
-''' </summary>
-<XmlRoot("background", [Namespace]:="http://gcmodeller.org/GSEA/background.xml")>
-Public Class Background : Inherits XmlDataModel
-    Implements INamedValue
-
-    Public Property name As String Implements IKeyedEntity(Of String).Key
-    Public Property comments As String
-    Public Property build As Date = Now
-
-    <XmlElement>
-    Public Property clusters As Cluster()
-
-    Public Overrides Function ToString() As String
-        Return name
     End Function
 End Class
