a1acfc79d34f2823fd90f6e9cec4845e4c708ed8
xieguigang <xie.guigang@gcmodeller.org>
2019-06-11 18:58:22 +0800

    ELIXIR Core Data Resources ?

---
+++ src/GCModeller/core/Bio.Assembly/Assembly/ELIXIR/NamespaceDoc.vb
@@ -0,0 +1,10 @@
+﻿Namespace Assembly.ELIXIR
+
+    ''' <summary>
+    ''' ## ELIXIR Core Data Resources
+    ''' 
+    ''' 
+    ''' </summary>
+    Module NamespaceDoc
+    End Module
+End Namespace
