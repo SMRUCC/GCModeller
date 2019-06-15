131a121cad637927b6cfa1bbd60f47981771e38d
xieguigang <xie.guigang@gcmodeller.org>
2019-06-15 12:03:17 +0800

    improvements on the decision tree data models

--- src/runtime/sciBASIC#/Data_science/DataMining/DataMining/DecisionTree/DataModels/DataModels.vb
+++ src/runtime/sciBASIC#/Data_science/DataMining/DataMining/DecisionTree/DataModels/DataModels.vb
@@ -3,7 +3,7 @@ Imports Microsoft.VisualBasic.DataMining.ComponentModel
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Serialization.JSON
 
-Namespace DecisionTree
+Namespace DecisionTree.Data
 
     ''' <summary>
     ''' A row in data table.(分类用的对象实例)
@@ -28,64 +28,12 @@ Namespace DecisionTree
             Return $"{decisions} ~ {entityVector.Take(Length - 1).GetJson}"
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Function Clone() As Object Implements ICloneable.Clone
             Return New Entity With {
                 .entityVector = entityVector.ToArray
             }
         End Function
-    End Class
-
-    Public Class DataTable
-
-        ''' <summary>
-        ''' Factor titles of <see cref="Entity.entityVector"/>
-        ''' </summary>
-        ''' <returns></returns>
-        Public Property headers As String()
-
-        ''' <summary>
-        ''' 分类结果的显示标题
-        ''' </summary>
-        ''' <returns></returns>
-        Public ReadOnly Property decisions As String
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Get
-                Return headers.Last
-            End Get
-        End Property
-
-        ''' <summary>
-        ''' Training set data
-        ''' </summary>
-        ''' <returns></returns>
-        Public Property rows As Entity()
-
-        ''' <summary>
-        ''' Get the property fields count
-        ''' </summary>
-        ''' <returns></returns>
-        Public ReadOnly Property columns As Integer
-            Get
-                Return headers.Length
-            End Get
-        End Property
-
-        Default Public ReadOnly Property GetRow(index As Integer) As Entity
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Get
-                Return rows(index)
-            End Get
-        End Property
-
-        Public Sub RemoveColumn(index As Integer)
-            headers = headers.Delete(index)
-            rows.DoEach(Sub(r) r.entityVector = r.entityVector.Delete(index))
-        End Sub
-
-        Public Overrides Function ToString() As String
-            Return headers.GetJson
-        End Function
-
     End Class
 
     Public Class ClassifyResult
