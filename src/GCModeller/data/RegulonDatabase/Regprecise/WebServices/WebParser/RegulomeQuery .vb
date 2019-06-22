0334047f0a44ca2dc91d9e61a453a508d38ea39b
xieguigang <xie.guigang@gcmodeller.org>
2019-06-22 14:38:29 +0800

    fix bugs in regprecise web query

--- src/GCModeller/data/RegulonDatabase/Regprecise/WebServices/WebParser/RegulomeQuery .vb
+++ src/GCModeller/data/RegulonDatabase/Regprecise/WebServices/WebParser/RegulomeQuery .vb
@@ -1,6 +1,5 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports System.Text.RegularExpressions
-Imports System.Threading
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
@@ -9,29 +8,53 @@ Imports r = System.Text.RegularExpressions.Regex
 
 Namespace Regprecise
 
-    Public Class RegulomeQuery : Inherits WebQuery(Of String)
+    Public Class RegulomeQuery : Inherits WebQueryModule(Of String)
 
         Public Sub New(<CallerMemberName>
                        Optional cache As String = Nothing,
                        Optional interval As Integer = -1,
-                       Optional offline As Boolean = False
-                   )
+                       Optional offline As Boolean = False)
 
-            MyBase.New(url:=AddressOf RegulomeQuery.url,
-                       contextGuid:=AddressOf GetsId,
-                       parser:=AddressOf ParseRegulon,
-                       prefix:=Nothing,
-                       cache:=cache,
-                       interval:=interval,
-                       offline:=offline
-                   )
+            MyBase.New(cache:=cache, interval:=interval, offline:=offline)
         End Sub
 
-        Private Shared Function url(entryHref As String) As String
-            Dim str$ = r.Match(entryHref, "href="".+?"">.+?</a>").Value
+        Protected Overrides Function doParseUrl(context As String) As String
+            Dim str$ = r.Match(context, "href="".+?"">.+?</a>").Value
             Dim strUrl$ = "http://regprecise.lbl.gov/RegPrecise/" & str.href
 
             Return strUrl
+        End Function
+
+        ''' <summary>
+        ''' 下载某一个基因组内所有预测的调控元的数据
+        ''' </summary>
+        ''' <returns></returns>
+        Protected Overrides Function doParseObject(html As String, schema As Type) As Object
+            Dim list$()
+            Dim regulators As New List(Of Regulator)
+            Dim str$
+            Dim regulatorQuery As New RegulatorQuery($"{cache}/regulators/", sleepInterval, offlineMode)
+
+            html$ = r _
+                .Match(html, "<table class=""stattbl"".+?</table>", RegexOptions.Singleline) _
+                .Match("<tbody>.+</tbody>", RegexOptions.Singleline)
+            list = r _
+                .Matches(html, "<tr .+?</tr>", RegexICSng) _
+                .ToArray
+
+            For i As Integer = 0 To list.Length - 1
+                str = list(i)
+                regulators += regulatorQuery.Query(Of Regulator)(str, ".txt")
+            Next
+
+            Return New Regulon With {
+                .regulators = regulators
+            }
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Protected Overrides Function doParseGuid(context As String) As String
+            Return GetsId(context)
         End Function
 
         <ExportAPI("Get.sId")>
@@ -44,34 +67,6 @@ Namespace Regprecise
                 Id = Mid(Id, 1, Len(Id) - 4)
                 Return Id
             End If
-        End Function
-
-        ''' <summary>
-        ''' 下载某一个基因组内所有预测的调控元的数据
-        ''' </summary>
-        ''' <returns></returns>
-        <ExportAPI("Regulon.Downloads")>
-        Private Shared Function ParseRegulon(html As String, null As Type) As Object
-            Dim list$()
-            Dim regulators As New List(Of Regulator)
-            Dim str$
-
-            html$ = r _
-                .Match(html, "<table class=""stattbl"".+?</table>", RegexOptions.Singleline) _
-                .Match("<tbody>.+</tbody>", RegexOptions.Singleline)
-            list = r _
-                .Matches(html, "<tr .+?</tr>", RegexICSng) _
-                .ToArray
-
-            For i As Integer = 0 To list.Length - 1
-                str = list(i)
-                regulators += Regulator.CreateObject(str)
-                Thread.Sleep(2000)
-            Next
-
-            Return New Regulon With {
-                .regulators = regulators
-            }
         End Function
     End Class
 End Namespace
