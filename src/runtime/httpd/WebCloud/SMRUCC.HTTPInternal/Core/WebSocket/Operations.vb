d19fa8c97479d2ce24b223bfe40516c28a4e0e50
xieguigang <xie.guigang@gcmodeller.org>
2019-06-28 23:11:48 +0800

    test websocket module success

---
+++ src/runtime/httpd/WebCloud/SMRUCC.HTTPInternal/Core/WebSocket/Operations.vb
@@ -0,0 +1,21 @@
+﻿Namespace Core.WebSocket
+
+    Public Enum Operations As Integer
+        ''' <summary>
+        ''' Text Data Sent From Client
+        ''' </summary>
+        TextRecieved = &H1
+        ''' <summary>
+        ''' Binary Data Sent From Client 
+        ''' </summary>
+        BinaryRecieved = &H2
+        ''' <summary>
+        ''' Ping Sent From Client 
+        ''' </summary>
+        Ping = &H9
+        ''' <summary>
+        ''' Pong Sent From Client 
+        ''' </summary>
+        Pong = &HA
+    End Enum
+End Namespace
