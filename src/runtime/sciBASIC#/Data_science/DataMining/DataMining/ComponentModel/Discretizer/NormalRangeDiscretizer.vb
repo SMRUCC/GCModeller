ec98bee1b9ae7fbd71e4411a62eb31fd3c09117a
xieguigang <xie.guigang@gcmodeller.org>
2019-06-28 15:23:50 +0800

    initialize from sample distribution

--- src/runtime/sciBASIC#/Data_science/DataMining/DataMining/ComponentModel/Discretizer/NormalRangeDiscretizer.vb
+++ src/runtime/sciBASIC#/Data_science/DataMining/DataMining/ComponentModel/Discretizer/NormalRangeDiscretizer.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Math.Distributions
 
 Namespace ComponentModel.Discretion
 
@@ -7,7 +8,7 @@ Namespace ComponentModel.Discretion
     ''' </summary>
     ''' <remarks>
     ''' 这种离散化处理方法比较适用于人工神经网络, 如果你的数据在归一化之后任然无法收敛
-    ''' 可以使用区间离散化来进行归一化
+    ''' 可以使用区间离散化来进行归一化, 通过减少数据特征来消除无法收敛的问题
     ''' </remarks>
     Public Class NormalRangeDiscretizer
 
@@ -18,6 +19,10 @@ Namespace ComponentModel.Discretion
 
         ReadOnly ranges As DoubleRange()
 
+        ''' <summary>
+        ''' 创建一个简易的正态分布区间
+        ''' </summary>
+        ''' <param name="sample"></param>
         Sub New(sample As IEnumerable(Of Double))
             With sample.ToArray
                 min = .Min
@@ -26,6 +31,18 @@ Namespace ComponentModel.Discretion
                 std = .StdError
             End With
 
+            ranges = doCreateRanges()
+        End Sub
+
+        Sub New(dist As SampleDistribution)
+            min = dist.min
+            max = dist.max
+            mean = dist.average
+            std = dist.stdErr
+            ranges = doCreateRanges()
+        End Sub
+
+        Private Function doCreateRanges() As DoubleRange()
             Dim normalLow As New DoubleRange(mean - std, mean)
             Dim normalHigh As New DoubleRange(mean + std, mean)
             Dim moderateLow As New DoubleRange(normalLow.Min, mean - 2 * std)
@@ -33,8 +50,8 @@ Namespace ComponentModel.Discretion
             Dim criticalLow As New DoubleRange(moderateLow.Min, mean - 3 * std)
             Dim criticalHigh As New DoubleRange(moderateHigh.Max, mean + 3 * std)
 
-            ranges = {criticalLow, moderateLow, normalLow, normalHigh, moderateHigh, criticalHigh}
-        End Sub
+            Return {criticalLow, moderateLow, normalLow, normalHigh, moderateHigh, criticalHigh}
+        End Function
 
         Public Function Normalize(x As Double) As Double
             If x < ranges(0).Min Then
---
+++ src/runtime/sciBASIC#/Data_science/DataMining/DataMining/ComponentModel/Normalizer/Normalizations.vb
@@ -0,0 +1,64 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Math.Distributions
+
+Namespace ComponentModel.Normalizer
+
+    Public Enum Methods
+        ''' <summary>
+        ''' 归一化到[0, 1]区间内
+        ''' </summary>
+        NormalScaler
+        ''' <summary>
+        ''' 直接 x / max 进行归一化, 当出现极值的时候, 此方法无效, 根据数据分布,可能会归一化到[0, 1] 或者 [-1, 1]区间内
+        ''' </summary>
+        RelativeScaler
+        ''' <summary>
+        ''' 通过对数据进行区间离散化来完成归一化
+        ''' </summary>
+        RangeDiscretizer
+    End Enum
+
+    Public Module Normalizations
+
+        ReadOnly normalRange As DoubleRange = {0, 1}
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function ScalerNormalize(samples As SampleDistribution, x#) As Double
+            Return samples.GetRange.ScaleMapping(x, normalRange)
+        End Function
+
+        ''' <summary>
+        ''' 正实数和负实数是分开进行归一化的
+        ''' </summary>
+        ''' <param name="samples"></param>
+        ''' <param name="x#"></param>
+        ''' <returns></returns>
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function RelativeNormalize(samples As SampleDistribution, x#) As Double
+            If x > 0 Then
+                If x > samples.max Then
+                    Return 1
+                Else
+                    Return x / samples.max
+                End If
+            ElseIf x = 0R Then
+                Return 0
+            Else
+                ' 负实数需要考察一下
+                If x < samples.min Then
+                    Return -1
+                ElseIf Samples.min >= 0 Then
+                    Return -1
+                Else
+                    Return x / Math.Abs(samples.min)
+                End If
+            End If
+        End Function
+
+        Public Function RangeDiscretizer(samples As SampleDistribution, x#) As Double
+
+        End Function
+
+    End Module
+End Namespace
