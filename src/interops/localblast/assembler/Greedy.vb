c372823cc86c1ffcb8cf39b43951afca1d6ab33c
xieguigang <xie.guigang@gcmodeller.org>
2019-07-05 14:45:08 +0800

    file renames

---
+++ src/interops/localblast/assembler/Greedy.vb
@@ -0,0 +1,53 @@
+﻿#Region "Microsoft.VisualBasic::487d8a87cdf100564981a99709d7a7a0, assembler\Class1.vb"
+
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+
+' /********************************************************************************/
+
+' Summaries:
+
+' Class Class1
+' 
+' 
+' 
+' /********************************************************************************/
+
+#End Region
+
+Public Module Greedy
+
+    ' Greedy algorithm
+    ' Given a Set Of sequence fragments the Object Is To find the shortest common supersequence.
+
+    ' Сalculate pairwise alignments Of all fragments.
+    ' Choose two fragments With the largest overlap.
+    ' Merge chosen fragments.
+    ' Repeat step 2 And 3 until only one fragment Is left.
+    ' The result need Not be an optimal solution To the problem.
+
+End Module
