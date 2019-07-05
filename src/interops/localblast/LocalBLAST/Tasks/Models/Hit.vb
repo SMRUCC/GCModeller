6c9e4a74f4652bc8be47866d68a7f32c95483264
xieguigang <xie.guigang@gcmodeller.org>
2019-07-05 14:52:18 +0800

    split source files

---
+++ src/interops/localblast/LocalBLAST/Tasks/Models/Hit.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+
+Namespace Tasks.Models
+
+    ''' <summary>
+    ''' 和Query的一个比对结果
+    ''' </summary>
+    Public Class Hit : Implements INamedValue
+
+        ''' <summary>
+        ''' <see cref="HitName"></see>所在的物种
+        ''' </summary>
+        ''' <value></value>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        <XmlAttribute> Public Property tag As String
+        ''' <summary>
+        ''' 和query蛋白质比对上的
+        ''' </summary>
+        ''' <value></value>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        <XmlAttribute> Public Property HitName As String Implements INamedValue.Key
+        <XmlAttribute> Public Property Identities As Double
+        <XmlAttribute> Public Property Positive As Double
+
+        Public Overrides Function ToString() As String
+            Return $"[{tag}] {HitName},    Identities:= {Identities};   Positive:= {Positive};"
+        End Function
+    End Class
+End Namespace
