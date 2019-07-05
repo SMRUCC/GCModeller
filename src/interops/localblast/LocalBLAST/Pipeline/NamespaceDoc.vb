501e47cabd32b5514f6ed08d2aa6c14444f1088a
xieguigang <xie.guigang@gcmodeller.org>
2019-07-05 13:55:38 +0800

    move COG annotation to pipeline module

---
+++ src/interops/localblast/LocalBLAST/Pipeline/NamespaceDoc.vb
@@ -0,0 +1,8 @@
+﻿Namespace Pipeline
+
+    ''' <summary>
+    ''' The functional annotation analysis pipeline based on the ncbi localblast+ toolkit
+    ''' </summary>
+    Module NamespaceDoc
+    End Module
+End Namespace
