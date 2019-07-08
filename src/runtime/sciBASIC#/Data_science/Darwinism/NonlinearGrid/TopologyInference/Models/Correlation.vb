9b785fb4781360789c55b000651e33dab918c7fe
xieguigang <xie.guigang@gcmodeller.org>
2019-07-08 17:52:43 +0800

    Evaluate the system dynamics

---
+++ src/runtime/sciBASIC#/Data_science/Darwinism/NonlinearGrid/TopologyInference/Models/Correlation.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+
+''' <summary>
+''' A linear correlation system
+''' </summary>
+Public Class Correlation
+
+    Public Property B As Vector
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function Evaluate(X As Vector) As Double
+        Return (B * X).Sum
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return B.ToString
+    End Function
+End Class
---
+++ src/runtime/sciBASIC#/Data_science/Darwinism/NonlinearGrid/TopologyInference/Models/System.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic.Math.LinearAlgebra
+
+''' <summary>
+''' The Nonlinear Grid Dynamics System
+''' </summary>
+Public Class GridSystem
+
+    Public Property A As Vector
+    Public Property C As Correlation()
+
+    ''' <summary>
+    ''' Evaluate the system dynamics
+    ''' </summary>
+    ''' <param name="X"></param>
+    ''' <returns></returns>
+    Public Function Evaluate(X As Vector) As Double
+        Dim C As Vector = Me.C.Select(Function(ci) ci.Evaluate(X)).AsVector
+        Dim fx As Vector = A * (X ^ C)
+        Dim result = fx.Sum
+
+        Return result
+    End Function
+End Class
