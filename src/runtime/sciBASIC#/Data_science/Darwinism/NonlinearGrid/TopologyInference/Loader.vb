20f642e46129711f9853ab6477893d8f42c4e5fb
xieguigang <xie.guigang@gcmodeller.org>
2019-07-09 16:40:00 +0800

    use ZERO for initialize entir system

--- src/runtime/sciBASIC#/Data_science/Darwinism/NonlinearGrid/TopologyInference/Loader.vb
+++ src/runtime/sciBASIC#/Data_science/Darwinism/NonlinearGrid/TopologyInference/Loader.vb
@@ -3,13 +3,18 @@ Imports Microsoft.VisualBasic.Math.LinearAlgebra
 
 Public Module Loader
 
+    ''' <summary>
+    ''' 因为累加效应在系统很庞大的时候可能会非常的大,所以在这里全部都是用零来进行初始化的
+    ''' </summary>
+    ''' <param name="width"></param>
+    ''' <returns></returns>
     Public Function EmptyGridSystem(width As Integer) As GridSystem
         Return New GridSystem With {
-            .A = Vector.rand(width),
+            .A = Vector.Zero(width),
             .C = width.SeqIterator _
                 .Select(Function(null)
                             Return New Correlation With {
-                                .B = Vector.rand(width)
+                                .B = Vector.Zero(width)
                             }
                         End Function) _
                 .ToArray
