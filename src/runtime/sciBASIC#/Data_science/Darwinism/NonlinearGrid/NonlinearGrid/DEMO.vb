92244940f2cbb2415dfad86620b3643051d1d12a
xieguigang <xie.guigang@gcmodeller.org>
2019-07-10 18:20:55 +0800

    run demo

--- src/runtime/sciBASIC#/Data_science/Darwinism/NonlinearGrid/NonlinearGrid/DEMO.vb
+++ src/runtime/sciBASIC#/Data_science/Darwinism/NonlinearGrid/NonlinearGrid/DEMO.vb
@@ -12,7 +12,7 @@ Module DEMO
         network += populate({0, 1, 1, 1, 1}, 50, 100)
         network += populate({0, 0, 0, 0, 1}, 500, 100)
 
-
+        Call Program.RunFitProcess(network, network.First.status.Length, "./test_demo.Xml", Nothing, 5000)
     End Sub
 
     Private Iterator Function populate([in] As Double(), out As Double, n As Integer) As IEnumerable(Of Sample)
