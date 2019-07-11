dff673f0094d5d390f93c87e56fd5683a1f6e6de
xieguigang <xie.guigang@gcmodeller.org>
2019-07-11 11:42:53 +0800

    create demo dataset

--- src/runtime/sciBASIC#/Data_science/Darwinism/NonlinearGrid/NonlinearGrid/DEMO.vb
+++ src/runtime/sciBASIC#/Data_science/Darwinism/NonlinearGrid/NonlinearGrid/DEMO.vb
@@ -1,18 +1,20 @@
-﻿Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork.StoreProcedure
-Imports Microsoft.VisualBasic.Language
+﻿Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.MachineLearning.Debugger
+Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork.StoreProcedure
 
 Module DEMO
 
     Sub Main()
         Dim network As New List(Of Sample)
 
-        network += populate({1, 1, 1, 1, 1, 99}, 0, 100)
-        network += populate({1, 1, 1, 1, 0, 99}, 100, 100)
-        network += populate({0, 0, 0, 0, 0, 1}, -50, 100)
-        network += populate({0, 1, 1, 1, 1, -100}, 50, 100)
-        network += populate({0, 0, 0, 0, 1, 10}, 500, 100)
+        network += populate({1, 1, 1, 1, 1, -10, 99}, 0, 100)
+        network += populate({1, 1, 1, 1, 0, -10, 99}, 100, 100)
+        network += populate({0, 0, 0, 0, 0, 50, 1}, -50, 100)
+        network += populate({0, 1, 1, 1, 1, 50, -100}, 50, 100)
+        network += populate({0, 0, 0, 0, 1, 100, 10}, 500, 100)
 
-        Call Program.RunFitProcess(network, network.First.status.Length, "./test_demo.Xml", Nothing, 5000)
+        ' Call Program.RunFitProcess(network, network.First.status.Length, "./test_demo.Xml", Nothing, 5000)
+        Call network.SampleSetCreator().GetXml.SaveTo("./DEMO_dataset.Xml")
     End Sub
 
     Private Iterator Function populate([in] As Double(), out As Double, n As Integer) As IEnumerable(Of Sample)
