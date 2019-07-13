c3042ca77d6d6e2c6fdf2828e9cf55c74228a52e
xieguigang <xie.guigang@gcmodeller.org>
2019-07-14 01:29:07 +0800

    render network layout progress as avi video

---
+++ src/runtime/sciBASIC#/gr/network-visualization/test/testRenderAVI.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Drawing
+Imports System.Windows.Forms
+Imports Microsoft.VisualBasic.Data.visualize.Network.Canvas
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
+
+Module testRenderAVI
+
+    Sub Main()
+        Dim graph = CytoscapeExportAsGraph(
+            App.HOME & "\Resources\xcb-main-Edges.csv",
+            App.HOME & "\Resources\xcb-main-Nodes.csv")
+
+        Dim video = AVI.DoRenderVideo(graph, {1024, 768}, drawFrames:=1024)
+
+        Call video.WriteBuffer("X:\network.avi")
+    End Sub
+End Module
