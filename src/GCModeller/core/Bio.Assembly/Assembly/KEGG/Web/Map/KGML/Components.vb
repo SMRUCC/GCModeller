3559b17639a326e268fbb2a5ab399bba0743587e
xieguigang <xie.guigang@gcmodeller.org>
2019-07-18 16:53:01 +0800

    split source files

---
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/KGML/Components.vb
@@ -0,0 +1,78 @@
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace Assembly.KEGG.WebServices.KGML
+
+    ''' <summary>
+    ''' Network edges
+    ''' </summary>
+    Public Class Link
+
+        <XmlAttribute> Public Property entry1 As String
+        <XmlAttribute> Public Property entry2 As String
+        <XmlAttribute> Public Property type As String
+
+    End Class
+
+    Public Class relation : Inherits Link
+        Public Property subtype As subtype
+    End Class
+
+    Public Class subtype
+        <XmlAttribute> Public Property name As String
+        <XmlAttribute> Public Property value As String
+    End Class
+
+    Public Class compound
+        <XmlAttribute> Public Property id As String
+        <XmlAttribute> Public Property name As String
+
+        Public Overrides Function ToString() As String
+            Return name
+        End Function
+    End Class
+
+    Public Class reaction : Inherits Link
+        <XmlElement("substrate")>
+        Public Property substrates As compound()
+        <XmlElement("product")>
+        Public Property products As compound()
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Class
+
+    ''' <summary>
+    ''' Network nodes
+    ''' </summary>
+    Public Class entry
+
+        <XmlAttribute> Public Property id As String
+        <XmlAttribute> Public Property name As String
+        <XmlAttribute> Public Property type As String
+        <XmlAttribute> Public Property link As String
+
+        Public Property graphics As graphics
+
+        Public Overrides Function ToString() As String
+            Return name
+        End Function
+    End Class
+
+    Public Class graphics
+
+        <XmlAttribute> Public Property name As String
+        <XmlAttribute> Public Property fgcolor As String
+        <XmlAttribute> Public Property bgcolor As String
+        <XmlAttribute> Public Property type As String
+        <XmlAttribute> Public Property x As String
+        <XmlAttribute> Public Property y As String
+        <XmlAttribute> Public Property width As String
+        <XmlAttribute> Public Property height As String
+
+        Public Overrides Function ToString() As String
+            Return name
+        End Function
+    End Class
+End Namespace
