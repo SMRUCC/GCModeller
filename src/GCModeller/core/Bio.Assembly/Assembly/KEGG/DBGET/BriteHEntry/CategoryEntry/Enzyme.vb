390fa7fc8cd537dc8881849f6f44aff768f334ee
xieguigang <xie.guigang@gcmodeller.org>
2019-07-18 16:07:53 +0800

    get external resource

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/BriteHEntry/CategoryEntry/Enzyme.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/BriteHEntry/CategoryEntry/Enzyme.vb
@@ -2,5 +2,18 @@
 
     Public Class EnzymeEntry
 
+        ''' <summary>
+        ''' 从卫星资源程序集之中加载数据库数据
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks>Load resource using <see cref="ResourcesSatellite"/></remarks>
+        Public Shared Function GetResource() As htext
+            Dim res$ = GetType(EnzymeEntry) _
+                .Assembly _
+                .ResourcesSatellite() _
+                .GetString("ko01000")
+            Dim htext As htext = htext.StreamParser(res)
+            Return htext
+        End Function
     End Class
 End Namespace
