cfee5fa20729bc9771cba230b2a37c88e4f3d7f9
xieguigang <xie.guigang@gcmodeller.org>
2019-07-19 13:43:52 +0800

    add relationship calculation between the EC numbers

---
+++ src/GCModeller/models/Networks/Microbiome/Membrane_transport/Membrane_transport.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+Imports SMRUCC.genomics.ComponentModel.Annotation
+Imports SMRUCC.genomics.Data
+
+Public Class Enzyme
+
+    Public Property KO As String
+    Public Property EC As ECNumber
+    Public Property name As String
+
+    Sub New(KO$, geneName$, EC$)
+        Me.KO = KO
+        Me.name = geneName
+        Me.EC = ECNumber.ValueParser(EC)
+    End Sub
+
+    Public Function Selects(repo As ReactionRepository) As Dictionary(Of String, Reaction)
+
+    End Function
+End Class
+
+Public Module Membrane_transport
+
+    <Extension>
+    Public Function BuildTransferNetwork(metagenome As IEnumerable(Of TaxonomyRef), reactions As ReactionRepository, enzymes As Enzyme()) As NetworkGraph
+
+    End Function
+End Module
