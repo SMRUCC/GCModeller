3a1a8425b970d699be9e466e3fcee2b47319b29b
xieguigang <xie.guigang@gcmodeller.org>
2019-07-20 14:21:23 +0800

    split source files

---
+++ src/GCModeller/models/Networks/Microbiome/Enzyme.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+Imports SMRUCC.genomics.ComponentModel.Annotation
+Imports SMRUCC.genomics.Data
+
+Public Class Enzyme
+
+    Public Property KO As String
+    Public Property EC As ECNumber
+    Public Property name As String
+
+    Sub New(KO$, geneName$, EC$)
+        Me.KO = KO
+        Me.name = geneName
+        Me.EC = ECNumber.ValueParser(EC)
+    End Sub
+
+    ''' <summary>
+    ''' Select enzymes
+    ''' </summary>
+    ''' <param name="repo"></param>
+    ''' <returns></returns>
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function Selects(repo As ReactionRepository) As IReadOnlyDictionary(Of String, Reaction)
+        Return repo _
+            .GetWhere(Function(r)
+                          Return r.Enzyme _
+                              .Any(Function(id)
+                                       Return Me.EC.Contains(id) OrElse ECNumber.ValueParser(id).Contains(EC)
+                                   End Function)
+                      End Function)
+    End Function
+End Class
--- src/GCModeller/models/Networks/Microbiome/Membrane_transport/Membrane_transport.vb
+++ src/GCModeller/models/Networks/Microbiome/Membrane_transport/Membrane_transport.vb
@@ -11,35 +11,6 @@ Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
 Imports SMRUCC.genomics.ComponentModel.Annotation
 Imports SMRUCC.genomics.Data
 
-Public Class Enzyme
-
-    Public Property KO As String
-    Public Property EC As ECNumber
-    Public Property name As String
-
-    Sub New(KO$, geneName$, EC$)
-        Me.KO = KO
-        Me.name = geneName
-        Me.EC = ECNumber.ValueParser(EC)
-    End Sub
-
-    ''' <summary>
-    ''' Select enzymes
-    ''' </summary>
-    ''' <param name="repo"></param>
-    ''' <returns></returns>
-    <MethodImpl(MethodImplOptions.AggressiveInlining)>
-    Public Function Selects(repo As ReactionRepository) As IReadOnlyDictionary(Of String, Reaction)
-        Return repo _
-            .GetWhere(Function(r)
-                          Return r.Enzyme _
-                              .Any(Function(id)
-                                       Return Me.EC.Contains(id) OrElse ECNumber.ValueParser(id).Contains(EC)
-                                   End Function)
-                      End Function)
-    End Function
-End Class
-
 Public Module Membrane_transport
 
     ReadOnly defaultIgnores As New [Default](Of Index(Of String))(
