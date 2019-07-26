dc3f8ffcb03c821be1cc616a4d83da1ed5d124b8
xieguigang <xie.guigang@gcmodeller.org>
2019-07-26 11:04:31 +0800

    fix syntax error

--- src/runtime/sciBASIC#/Data_science/Mathematica/CUDA/sciKernel/src/randf.rs
+++ src/runtime/sciBASIC#/Data_science/Mathematica/CUDA/sciKernel/src/randf.rs
@@ -8,12 +8,12 @@ const C: i64 = 453806245;
 const PI: f64 = std::f64::consts::PI;
 
 #[no_mangle]
-pub extern fn randn(m: i32, seed: i32) -> [f64] {
+pub extern fn randn(m: usize, seed: i32) -> [f64] {
     let mut gauss: [f64, m];
     let mut tmp1: f64;
     let mut u : [f64];
-    let mut k : i32;
-    let stop: i32;
+    let mut k : usize;
+    let stop: usize;
 
     if m % 2 == 0 {
         u = rand(m, seed);
@@ -24,23 +24,23 @@ pub extern fn randn(m: i32, seed: i32) -> [f64] {
     }
 
     while k < stop {
-        tmp1 = math.sqrt!(-2 * math.log!(1 - u(k)));
-        gauss(k) = tmp1 * math.cos!(2 * PI * u(k + 1));
-        gauss(k + 1) = tmp1 * math.sin!(2 * PI * u(k + 1));
+        tmp1 = math.sqrt!(-2 * math.log!(1 - u[k]));
+        gauss[k] = tmp1 * math.cos!(2 * PI * u[k + 1]);
+        gauss[k + 1] = tmp1 * math.sin!(2 * PI * u[k + 1]);
 
         k = k + 2;
     }
 
     if m % 2 == 0 {
-        tmp1 = math.sqrt!(-2 * math.log!(1 - u(m - 1)))
-        gauss(m - 1) = tmp1 * math.cos!(2 * PI * u(m))
+        tmp1 = math.sqrt!(-2 * math.log!(1 - u[m - 1]));
+        gauss[m - 1] = tmp1 * math.cos!(2 * PI * u[m]);
     }
 
     return gauss;
 }
 
 #[no_mangle]
-pub extern fn rand(m: i32, seed: i32) -> [f64] {
+pub extern fn rand(m: usize, seed: i32) -> [f64] {
     let mut goal : [f64, m];
     let mut x0 : i64 = seed as i64;
     let mut x1: i64;
