eb183158c336159667a9aecbe894d1cec38c4842
xieguigang <xie.guigang@gcmodeller.org>
2019-07-26 12:26:06 +0800

    improvements of the network layout pipeline function

--- src/runtime/sciBASIC#/CLI_tools/MLkit/render/CLI.vb
+++ src/runtime/sciBASIC#/CLI_tools/MLkit/render/CLI.vb
@@ -4,19 +4,24 @@ Imports Microsoft.VisualBasic.Data.visualize.Network
 Imports Microsoft.VisualBasic.Data.visualize.Network.Analysis
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
 Imports Microsoft.VisualBasic.Imaging.Driver
 
 Module CLI
 
     <ExportAPI("/network")>
-    <Usage("/network /model <network_tables.directory> [/size <default=5000,3000> /out <image.png/svg>]")>
+    <Usage("/network /model <network_tables.directory> [/size <default=5000,3000> /fd <arguments.ini> /out <image.png/svg>]")>
     Public Function VisualizeNetwork(args As CommandLine) As Integer
         Dim in$ = args("/model")
         Dim size$ = args("/size") Or "5000,3000"
         Dim out$ = args("/out") Or $"{[in].TrimDIR}/image.png"
+        Dim fdArgv As ForceDirectedArgs = Parameters.Load(args("/fd"), ForceDirectedArgs.DefaultNew)
         Dim model = NetworkTables.Load(DIR:=[in]).AnalysisDegrees
         Dim graph As NetworkGraph = model.CreateGraph
-        Dim image As GraphicsData = graph.DrawImage(canvasSize:=size)
+        Dim image As GraphicsData = graph _
+            .doRandomLayout _
+            .doForceLayout(fdArgv) _
+            .DrawImage(canvasSize:=size)
 
         Return image.Save(out).CLICode
     End Function
