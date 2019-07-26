2ea900959910f76254337551fc8de9848c8bfd9a
xieguigang <xie.guigang@gcmodeller.org>
2019-07-26 11:36:33 +0800

    tools for render network graph

---
+++ src/runtime/sciBASIC#/CLI_tools/MLkit/render/CLI.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.visualize.Network
+Imports Microsoft.VisualBasic.Data.visualize.Network.Analysis
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Imaging.Driver
+
+Module CLI
+
+    <ExportAPI("/network")>
+    <Usage("/network /model <network_tables.directory> [/size <default=5000,3000> /out <image.png/svg>]")>
+    Public Function VisualizeNetwork(args As CommandLine) As Integer
+        Dim in$ = args("/model")
+        Dim size$ = args("/size") Or "5000,3000"
+        Dim out$ = args("/out") Or $"{[in].TrimDIR}/image.png"
+        Dim model = NetworkTables.Load(DIR:=[in]).AnalysisDegrees
+        Dim graph As NetworkGraph = model.CreateGraph
+        Dim image As GraphicsData = graph.DrawImage(canvasSize:=size)
+
+        Return image.Save(out).CLICode
+    End Function
+End Module
