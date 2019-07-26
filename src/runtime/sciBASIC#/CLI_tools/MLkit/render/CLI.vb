3694d23b560cf51467df0a430f6c768ce3749cd8
xieguigang <xie.guigang@gcmodeller.org>
2019-07-26 18:05:22 +0800

    config node size

--- src/runtime/sciBASIC#/CLI_tools/MLkit/render/CLI.vb
+++ src/runtime/sciBASIC#/CLI_tools/MLkit/render/CLI.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.Drawing.Drawing2D
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Data.visualize.Network
 Imports Microsoft.VisualBasic.Data.visualize.Network.Analysis
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
@@ -13,7 +15,7 @@ Imports Microsoft.VisualBasic.Imaging.Driver
 Module CLI
 
     <ExportAPI("/network")>
-    <Usage("/network /model <network_tables.directory> [/size <default=5000,3000> /fd <arguments.ini> /out <image.png/svg>]")>
+    <Usage("/network /model <network_tables.directory> [/size <default=5000,3000> /node.size <fieldName=30,300> /fd <arguments.ini> /out <image.png/svg>]")>
     Public Function VisualizeNetwork(args As CommandLine) As Integer
         Dim in$ = args("/model")
         Dim size$ = args("/size") Or "5000,3000"
@@ -21,12 +23,28 @@ Module CLI
         Dim fdArgv As ForceDirectedArgs = Parameters.Load(args("/fd"), ForceDirectedArgs.DefaultNew)
         Dim model = NetworkTables.Load(DIR:=[in]).AnalysisDegrees
         Dim graph As NetworkGraph = model.CreateGraph(nodeColor:=Function(n) n!color.GetBrush)
+        Dim nodeSizeMapper As Func(Of Graph.Node, Single) = NodeStyles.NodeDegreeSize(graph.vertex, "30,300")
+
+        If Not args("/node.size").DefaultValue.StringEmpty Then
+            Dim config As NamedValue(Of String) = args("/node.size").DefaultValue.GetTagValue("=", trim:=True)
+            Dim radiusRange As DoubleRange = config.Value
+            Dim valueRange As DoubleRange = graph.vertex _
+                .Select(Function(node)
+                            Return Val(node.data(config.Name))
+                        End Function) _
+                .ToArray
+
+            nodeSizeMapper = Function(node) As Single
+                                 Return valueRange.ScaleMapping(node.data(config.Name), radiusRange)
+                             End Function
+        End If
+
         Dim image As GraphicsData = graph _
             .doRandomLayout _
             .doForceLayout(fdArgv, showProgress:=True) _
             .DrawImage(
                 canvasSize:=size,
-                nodeRadius:=NodeStyles.NodeDegreeSize(graph.vertex, "30,300"),
+                nodeRadius:=nodeSizeMapper,
                 fontSize:=NodeStyles.NodeDegreeSize(graph.vertex, "16,120"),
                 minLinkWidth:=20,
                 edgeDashTypes:=model.nodes _
