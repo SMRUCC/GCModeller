97e136230a3590c09eb26717b3e7288471cfd28d
xieguigang <xie.guigang@gcmodeller.org>
2019-07-26 11:02:15 +0800

    use macro for simulate .NET class library reference

--- src/runtime/sciBASIC#/Data_science/Mathematica/CUDA/sciKernel/src/randf.rs
+++ src/runtime/sciBASIC#/Data_science/Mathematica/CUDA/sciKernel/src/randf.rs
@@ -1,8 +1,11 @@
+include!("math_macro.rs");
+
 pub mod crandn;
 
 const Mlng : i64 =  2147483648;
 const namda : i64 = 314159269;
 const C: i64 = 453806245;
+const PI: f64 = std::f64::consts::PI;
 
 #[no_mangle]
 pub extern fn randn(m: i32, seed: i32) -> [f64] {
@@ -21,16 +24,16 @@ pub extern fn randn(m: i32, seed: i32) -> [f64] {
     }
 
     while k < stop {
-        tmp1 = Math.Sqrt(-2 * Math.Log(1 - u(k)));
-                gauss(k) = tmp1 * Math.Cos(2 * Math.PI * u(k + 1));
-                gauss(k + 1) = tmp1 * Math.Sin(2 * Math.PI * u(k + 1));
+        tmp1 = math.sqrt!(-2 * math.log!(1 - u(k)));
+        gauss(k) = tmp1 * math.cos!(2 * PI * u(k + 1));
+        gauss(k + 1) = tmp1 * math.sin!(2 * PI * u(k + 1));
 
-                k = k + 2;
+        k = k + 2;
     }
 
     if m % 2 == 0 {
-        tmp1 = Math.Sqrt(-2 * Math.Log(1 - u(m - 1)))
-            gauss(m - 1) = tmp1 * Math.Cos(2 * Math.PI * u(m))
+        tmp1 = math.sqrt!(-2 * math.log!(1 - u(m - 1)))
+        gauss(m - 1) = tmp1 * math.cos!(2 * PI * u(m))
     }
 
     return gauss;
