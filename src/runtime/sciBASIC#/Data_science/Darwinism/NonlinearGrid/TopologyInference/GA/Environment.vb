cb1f2446e0987fa154cb048733778b6181fc80dd
xieguigang <xie.guigang@gcmodeller.org>
2019-07-31 10:04:47 +0800

    lock trainingset for improvement the perfermance of the parallel computing

--- src/runtime/sciBASIC#/Data_science/Darwinism/NonlinearGrid/TopologyInference/GA/Environment.vb
+++ src/runtime/sciBASIC#/Data_science/Darwinism/NonlinearGrid/TopologyInference/GA/Environment.vb
@@ -54,24 +54,6 @@ Imports Microsoft.VisualBasic.MachineLearning.Darwinism.GAF
 Imports Microsoft.VisualBasic.MachineLearning.StoreProcedure
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
 
-Public Structure TrainingSet
-
-    Dim X As Vector
-    Dim Y As Double
-    Dim targetID As String
-
-    Sub New(sample As Sample)
-        Me.X = sample.status.vector
-        Me.Y = sample.target(Scan0)
-        Me.targetID = sample.ID
-    End Sub
-
-    Public Overrides Function ToString() As String
-        Return targetID
-    End Function
-
-End Structure
-
 Public Class Environment : Implements Fitness(Of Genome)
 
     Dim matrix As TrainingSet()
