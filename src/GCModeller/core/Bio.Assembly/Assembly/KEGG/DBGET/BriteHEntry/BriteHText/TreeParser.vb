da8d91c660cab446bb118f1ed5e45d3858189e67
xieguigang <xie.guigang@gcmodeller.org>
2019-08-05 10:34:17 +0800

    brite htext module

---
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/BriteHEntry/BriteHText/TreeParser.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Assembly.KEGG.DBGET.BriteHEntry
+
+    Module TreeParser
+
+        <Extension>
+        Public Function Deflate(htext As BriteHText) As IEnumerable(Of BriteTerm)
+
+        End Function
+    End Module
+End Namespace
