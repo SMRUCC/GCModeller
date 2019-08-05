6974f5c7a17bf356b4c850ab379b89a3d8455020
xieguigang <xie.guigang@gcmodeller.org>
2019-08-05 11:47:14 +0800

    protein family information

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/BriteHEntry/CategoryEntry/ProteinFamily.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/BriteHEntry/CategoryEntry/ProteinFamily.vb
@@ -8,5 +8,17 @@
         Public Shared Function Transporters() As BriteTerm()
             Return BriteTerm.GetInformation("https://www.kegg.jp/kegg-bin/get_htext?ko02000.keg", "K\d+")
         End Function
+
+        Public Shared Function SecretionSystem() As BriteTerm()
+            Return BriteTerm.GetInformation("https://www.kegg.jp/kegg-bin/get_htext?ko02044.keg", "K\d+")
+        End Function
+
+        Public Shared Function BacterialToxins() As BriteTerm()
+            Return BriteTerm.GetInformation("https://www.kegg.jp/kegg-bin/get_htext?ko02042.keg", "K\d+")
+        End Function
+
+        Public Shared Function Exosome() As BriteTerm()
+            Return BriteTerm.GetInformation("https://www.kegg.jp/kegg-bin/get_htext?ko04147.keg", "K\d+")
+        End Function
     End Class
 End Namespace
