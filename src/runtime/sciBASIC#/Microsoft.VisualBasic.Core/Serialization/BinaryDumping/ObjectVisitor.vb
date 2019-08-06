dd6dbc95186a44d2a629e5bcded928eb53024d13
xieguigang <xie.guigang@gcmodeller.org>
2019-08-06 13:46:02 +0800

    required a common vistor framework for do serialization

---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Serialization/BinaryDumping/ObjectVisitor.vb
@@ -0,0 +1,14 @@
+﻿Namespace Serialization.BinaryDumping
+
+    ''' <summary>
+    ''' A Common framework for visit a object
+    ''' </summary>
+    Public Class ObjectVisitor
+
+        ReadOnly type As Type
+
+        Sub New(type As Type)
+            Me.type = type
+        End Sub
+    End Class
+End Namespace
