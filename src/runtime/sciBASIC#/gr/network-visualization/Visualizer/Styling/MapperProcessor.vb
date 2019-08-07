3d8f2d07d294a1fec179524d43ffab3f94376412
xieguigang <xie.guigang@gcmodeller.org>
2019-08-07 16:12:01 +0800

    clone network model

--- src/runtime/sciBASIC#/gr/network-visualization/Visualizer/Styling/MapperProcessor.vb
+++ src/runtime/sciBASIC#/gr/network-visualization/Visualizer/Styling/MapperProcessor.vb
@@ -23,7 +23,21 @@ Namespace Styling
         ''' <returns></returns>
         Private Function copy(graph As NetworkGraph) As NetworkGraph
             Dim nodes As Node() = graph.vertex.Select(Function(n) n.Clone).ToArray
-            Dim edges As Edge() = graph.graphEdges.Select(Function(e) e.Clone).ToArray
+            Dim edges As Edge() = graph.graphEdges _
+                .Select(Function(e) e.Clone) _
+                .ToArray
+
+            ' 因为edge中的node对象还是原来的node对象
+            ' 所以在这里会需要进行重新赋值来解除对原始数据的引用
+            With nodes.ToDictionary(Function(n) n.label)
+                edges.DoEach(Sub(edge)
+                                 ' use the clone data
+                                 edge.U = .Item(edge.U.label)
+                                 edge.V = .Item(edge.V.label)
+                             End Sub)
+            End With
+
+            Return New NetworkGraph(nodes, edges)
         End Function
     End Module
 End Namespace
