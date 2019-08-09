0309ae63aaf69d7923a871c9a899ba0b499d542d
xieguigang <xie.guigang@gcmodeller.org>
2019-08-09 10:44:34 +0800

    improvements of the javascript object member enumerator

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/My/JavaScript/JavaScriptObject.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/My/JavaScript/JavaScriptObject.vb
@@ -65,12 +65,33 @@ Namespace My.JavaScript
 
     End Class
 
+    Public Enum MemberAccessorResult
+        ''' <summary>
+        ''' Member is not exists in current javascript object
+        ''' </summary>
+        Undefined
+        ''' <summary>
+        ''' IS a member property in this javascript object
+        ''' </summary>
+        ClassMemberProperty
+        ''' <summary>
+        ''' Is an extension property object this javascript object
+        ''' </summary>
+        ExtensionProperty
+    End Enum
+
     ''' <summary>
     ''' javascript object
     ''' </summary>
-    Public Class JavaScriptObject : Implements IEnumerable(Of String), IJavaScriptObjectAccessor
+    Public Class JavaScriptObject : Implements IEnumerable(Of String), IEnumerable(Of NamedValue(Of Object)), IJavaScriptObjectAccessor
 
         Dim members As New Dictionary(Of String, [Variant](Of BindProperty(Of DataFrameColumnAttribute), Object))
+
+        ''' <summary>
+        ''' This javascript object instance
+        ''' </summary>
+        ''' <returns></returns>
+        Protected ReadOnly Property this As JavaScriptObject = Me
 
         ''' <summary>
         ''' 只针对Public的属性或者字段有效
@@ -79,20 +100,7 @@ Namespace My.JavaScript
         ''' <returns></returns>
         Default Public Property Accessor(memberName As String) As Object Implements IJavaScriptObjectAccessor.Accessor
             Get
-                If members.ContainsKey(memberName) Then
-                    Dim value = members(memberName)
-
-                    If value Is Nothing Then
-                        Return Nothing
-                    ElseIf value Like GetType(BindProperty(Of DataFrameColumnAttribute)) Then
-                        Return value.TryCast(Of BindProperty(Of DataFrameColumnAttribute)).GetValue(Me)
-                    Else
-                        Return value.TryCast(Of Object)
-                    End If
-                Else
-                    ' Returns undefined in javascript
-                    Return Nothing
-                End If
+                Return GetMemberValue(memberName, Nothing)
             End Get
             Set(value As Object)
                 If members.ContainsKey(memberName) Then
@@ -126,6 +134,28 @@ Namespace My.JavaScript
             Next
         End Sub
 
+        Public Function GetMemberValue(memberName As String, ByRef access As MemberAccessorResult) As Object
+            If members.ContainsKey(memberName) Then
+                Dim value = members(memberName)
+
+                If value Is Nothing Then
+                    access = MemberAccessorResult.Undefined
+                    Return Nothing
+                ElseIf value Like GetType(BindProperty(Of DataFrameColumnAttribute)) Then
+                    access = MemberAccessorResult.ClassMemberProperty
+                    Return value.TryCast(Of BindProperty(Of DataFrameColumnAttribute)).GetValue(Me)
+                Else
+                    access = MemberAccessorResult.ExtensionProperty
+                    Return value.TryCast(Of Object)
+                End If
+            Else
+                ' Returns undefined in javascript
+                access = MemberAccessorResult.Undefined
+            End If
+
+            Return Nothing
+        End Function
+
         Public Shared Function GetDescription(jsObj As JavaScriptObject) As Dictionary(Of String, Descriptor)
             Dim desc As New Dictionary(Of String, Descriptor)
 
@@ -149,6 +179,24 @@ Namespace My.JavaScript
 
         Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
             Yield GetEnumerator()
+            Yield IEnumerable_GetEnumerator1()
+        End Function
+
+        Private Iterator Function IEnumerable_GetEnumerator1() As IEnumerator(Of NamedValue(Of Object)) Implements IEnumerable(Of NamedValue(Of Object)).GetEnumerator
+            Dim access As MemberAccessorResult = MemberAccessorResult.Undefined
+            Dim value As Object
+            Dim pop As NamedValue(Of Object)
+
+            For Each key As String In members.Keys
+                value = GetMemberValue(key, access)
+                pop = New NamedValue(Of Object) With {
+                    .Name = key,
+                    .Value = value,
+                    .Description = access.Description
+                }
+
+                Yield pop
+            Next
         End Function
     End Class
 End Namespace
