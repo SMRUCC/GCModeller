2b2e3659276d0d2c3735013518355596ff440194
xieguigang <xie.guigang@gcmodeller.org>
2019-08-20 14:50:34 +0800

    fix type reference errors

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/LinkDB/ParserHelper.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/LinkDB/ParserHelper.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::a949f99b337b94d3446f472780a05672, Bio.Assembly\Assembly\KEGG\DBGET\LinkDB\ParserHelper.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module ParserHelper
-    ' 
-    '         Function: GetRelationship, LinkDbEntries
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module ParserHelper
+' 
+'         Function: GetRelationship, LinkDbEntries
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -55,7 +55,7 @@ Namespace Assembly.KEGG.DBGET.LinkDB
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         <Extension>
-        Public Function LinkDbEntries(url As String, Optional cache$ = "./.kegg/linkdb/", Optional offline As Boolean = False) As IEnumerable(Of KeyValuePair)
+        Public Function LinkDbEntries(url As String, Optional cache$ = "./.kegg/linkdb/", Optional offline As Boolean = False) As IEnumerable(Of NamedValue)
             Return GenericParser.LinkDbEntries(url, cache, offline)
         End Function
 
