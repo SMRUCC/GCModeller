25e2ffa603af06999e5d852ba7168e709d61cd21
xieguigang <xie.guigang@gcmodeller.org>
2019-08-20 19:49:15 +0800

    code style improvements of the bubble plot

--- src/GCModeller/annotations/GO/EnrichPlot.vb
+++ src/GCModeller/annotations/GO/EnrichPlot.vb
@@ -68,17 +68,17 @@ Public Module EnrichPlot
     ''' </summary>
     ''' <param name="data">KOBAS富集计算分析的结果数据</param>
     ''' <param name="GO_terms">GO数据库</param>
-    ''' <param name="size$">输出的图像的大小</param>
-    ''' <param name="padding$">留白的大小</param>
-    ''' <param name="bg$">背景色</param>
-    ''' <param name="unenrichColor$">未被富集的go term的颜色，即那些pvalue值大于<paramref name="pvalue"/>参数值的go term的颜色，默认为浅灰色</param>
-    ''' <param name="pvalue#">pvalue阈值</param>
-    ''' <param name="legendFont$">legend的字体CSS</param>
-    ''' <param name="geneIDFont$">term标签的显示字体CSS</param>
+    ''' <param name="size">输出的图像的大小</param>
+    ''' <param name="padding">留白的大小</param>
+    ''' <param name="bg">背景色</param>
+    ''' <param name="unenrichColor">未被富集的go term的颜色，即那些pvalue值大于<paramref name="pvalue"/>参数值的go term的颜色，默认为浅灰色</param>
+    ''' <param name="pvalue">pvalue阈值</param>
+    ''' <param name="legendFont">legend的字体CSS</param>
+    ''' <param name="geneIDFont">term标签的显示字体CSS</param>
     ''' <param name="R$">term的半径大小的计算表达式</param>
-    ''' <param name="displays%">每一个GO的命名空间分类之下的显示标签label的term的数量，默认为每个命名空间显示10个term的标签</param>
-    ''' <param name="titleFontCSS$">标题字体的CSS字体</param>
-    ''' <param name="title$">Plot绘图的标题</param>
+    ''' <param name="displays">每一个GO的命名空间分类之下的显示标签label的term的数量，默认为每个命名空间显示10个term的标签</param>
+    ''' <param name="titleFontCSS">标题字体的CSS字体</param>
+    ''' <param name="title">Plot绘图的标题</param>
     ''' <returns></returns>
     <Extension>
     Public Function BubblePlot(data As IEnumerable(Of EnrichmentTerm),
@@ -86,7 +86,7 @@ Public Module EnrichPlot
                                Optional size$ = "1600,1200",
                                Optional padding$ = g.DefaultPadding,
                                Optional bg$ = "white",
-                               Optional unenrichColor$ = "gray", ' Optional enrichColorSchema$ = "Set1:c6",
+                               Optional unenrichColor$ = "gray",
                                Optional pvalue# = 0.01,
                                Optional legendFont$ = CSSFont.PlotSmallTitle,
                                Optional geneIDFont$ = CSSFont.Win10Normal,
@@ -99,10 +99,7 @@ Public Module EnrichPlot
         Dim enrichResult = data.EnrichResult(GO_terms)
         Dim unenrich As Color = unenrichColor.TranslateColor
         Dim math As New Expression
-        Dim calcR = Function(x#)
-                        Call math.SetVariable("x", x#)
-                        Return math.Evaluation(R)
-                    End Function
+        Dim calcR = Function(x#) math.SetVariable("x", x#).Evaluation(R)
 
         With New Dictionary(Of String, Color())
 
@@ -114,13 +111,14 @@ Public Module EnrichPlot
                 size.SizeParser, padding,
                 bg,
                 Sub(ByRef g, region)
-                    Call g.__plotInternal(
+                    Call g.plotInternal(
                         region, enrichResult, unenrich,
                         .ByRef, pvalue,
                         legendFont,
                         r:=calcR,
                         displays:=displays,
-                        showBubbleBorder:=bubbleBorder)
+                        showBubbleBorder:=bubbleBorder
+                    )
 
                     Dim titleFont As Font = CSSFont.TryParse(titleFontCSS).GDIObject
                     Dim fsize As SizeF = g.MeasureString(title, titleFont)
@@ -169,16 +167,16 @@ Public Module EnrichPlot
     ''' <param name="legendFontStyle$"></param>
     ''' <param name="r">点的半径大小的计算公式</param>
     <Extension>
-    Private Sub __plotInternal(g As IGraphics,
-                               region As GraphicsRegion,
-                               result As Dictionary(Of String, EnrichmentTerm()),
-                               unenrich As Color,
-                               enrichColors As Dictionary(Of String, Color()),
-                               pvalue#,
-                               legendFontStyle$,
-                               r As Func(Of Double, Double),
-                               displays%,
-                               showBubbleBorder As Boolean)
+    Private Sub plotInternal(g As IGraphics,
+                             region As GraphicsRegion,
+                             result As Dictionary(Of String, EnrichmentTerm()),
+                             unenrich As Color,
+                             enrichColors As Dictionary(Of String, Color()),
+                             pvalue#,
+                             legendFontStyle$,
+                             r As Func(Of Double, Double),
+                             displays%,
+                             showBubbleBorder As Boolean)
 
         Dim serials As SerialData() = result _
             .Keys _
@@ -213,20 +211,25 @@ Public Module EnrichPlot
             xAxis:="(0,1),tick=0.2",
             xlabel:="richFactor=(n/background)",
             ylabel:="-log<sub>10</sub>(p.value)",
-            bubbleBorder:=bubbleBorder)
+            bubbleBorder:=bubbleBorder
+        )
 
         Call g.DrawImageUnscaled(plot, New Point)
 
         Dim legends As Legend() = serials _
-            .Select(Function(s) New Legend With {
-                .color = s.color.RGBExpression,
-                .fontstyle = legendFontStyle,
-                .style = LegendStyles.Circle,
-                .title = s.title
-            }).ToArray
-        Dim ltopLeft As New Point(
-            plot.Width - 65,
-            region.Size.Height * 0.3)
+            .Select(Function(s)
+                        Return New Legend With {
+                            .color = s.color.RGBExpression,
+                            .fontstyle = legendFontStyle,
+                            .style = LegendStyles.Circle,
+                            .title = s.title
+                        }
+                    End Function) _
+            .ToArray
+        Dim ltopLeft As New Point With {
+            .X = plot.Width - 65,
+            .Y = region.Size.Height * 0.3
+        }
 
         Call g.DrawLegends(
             ltopLeft,
