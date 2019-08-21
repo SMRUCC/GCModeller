5768cfeb9674f6f06a0c9d949853998cf0e18e1c
xieguigang <xie.guigang@gcmodeller.org>
2019-08-21 16:22:47 +0800

    get overlap size between two sequence

--- src/GCModeller/core/Bio.Assembly/SequenceModel/Bits.vb
+++ src/GCModeller/core/Bio.Assembly/SequenceModel/Bits.vb
@@ -20,6 +20,50 @@ Namespace SequenceModel
             Me.title = title
         End Sub
 
+        ''' <summary>
+        ''' 注意这个函数并不是用于比较序列相似度, 而是精准的计算比较两条序列从头开始的重叠片段的大小
+        ''' </summary>
+        ''' <param name="another"></param>
+        ''' <returns></returns>
+        ''' <remarks>
+        ''' 主要是一个用于序列装配的帮助函数
+        ''' </remarks>
+        Public Function OverlapSize(another As Bits) As Integer
+            Dim first As Byte = another.bytes(Scan0)
+            Dim break As Boolean = False
+
+            For i As Integer = 0 To bytes.Length - 1
+                If bytes(i) = first Then
+                    ' 从这里开始往后面进行一一比较
+                    For j As Integer = 0 To another.bytes.Length - 1
+                        If i + j = bytes.Length Then
+                            ' 当前的序列已经到头了
+                            ' 说明已经找到了一个重叠区域
+                            ' 这个重叠区域的长度为j
+                            Return j
+                        End If
+                        If another.bytes(j) <> bytes(i + j) Then
+                            ' 中间有一个位点不一致
+                            ' 这个不是一个有效的重叠区域
+                            break = True
+                            Exit For
+                        End If
+                    Next
+
+                    If Not break Then
+                        ' another序列已经到头了
+                        ' 并且没有不一致的位点
+                        ' 说明存在一个有效的重叠区域
+                        ' 区域的长度为another序列的长度
+                        Return another.bytes.Length
+                    End If
+                End If
+            Next
+
+            ' 没有重叠片段
+            Return 0
+        End Function
+
         Public Overrides Function ToString() As String
             Return $"{seqType.Description} {title}"
         End Function
