8fa57319e2bbaad140cab6ad09fdfe54f9a87d2a
xieguigang <xie.guigang@gcmodeller.org>
2019-08-21 18:45:48 +0800

    fix bugs in overlap size calculatioon

--- src/interops/localblast/assembler/test/Module1.vb
+++ src/interops/localblast/assembler/test/Module1.vb
@@ -4,10 +4,15 @@ Imports SMRUCC.genomics.SequenceModel.FASTA
 Module Module1
 
     Sub Main()
-        Dim fa = "D:\biodeep\1_combined_R1.fasta"
-        Dim contigs = Greedy.DeNovoAssembly(FastaFile.LoadNucleotideData(fa), identity:=0.3, similar:=0.05).ToArray
+        'Dim fa = "D:\biodeep\1_combined_R1.fasta"
+        'Dim contigs = Greedy.DeNovoAssembly(FastaFile.LoadNucleotideData(fa), identity:=0.1, similar:=0.05).ToArray
 
-        Call New FastaFile(contigs).Save("D:\biodeep\1_combined_R1_contigs.fasta")
+        'Call New FastaFile(contigs).Save("D:\biodeep\1_combined_R1_contigs.fasta")
+
+        Dim fa = "E:\Resources\JCC1_combined_R1.fasta"
+        Dim contigs = Greedy.DeNovoAssembly(FastaFile.LoadNucleotideData(fa), identity:=0.1, similar:=0.05).ToArray
+
+        Call New FastaFile(contigs).Save("E:\Resources\JCC1_combined_R1_contigs.fasta")
 
         Pause()
     End Sub
