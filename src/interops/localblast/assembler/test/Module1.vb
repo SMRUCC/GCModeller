c039ec775fbe7a30410aee2fec274742fee3d87d
xieguigang <xie.guigang@gcmodeller.org>
2019-08-21 17:48:57 +0800

    how to config the assembler its threshold parameters?

--- src/interops/localblast/assembler/test/Module1.vb
+++ src/interops/localblast/assembler/test/Module1.vb
@@ -5,7 +5,7 @@ Module Module1
 
     Sub Main()
         Dim fa = "D:\biodeep\1_combined_R1.fasta"
-        Dim contigs = Greedy.DeNovoAssembly(FastaFile.LoadNucleotideData(fa)).ToArray
+        Dim contigs = Greedy.DeNovoAssembly(FastaFile.LoadNucleotideData(fa), identity:=0.3, similar:=0.05).ToArray
 
         Call New FastaFile(contigs).Save("D:\biodeep\1_combined_R1_contigs.fasta")
 
