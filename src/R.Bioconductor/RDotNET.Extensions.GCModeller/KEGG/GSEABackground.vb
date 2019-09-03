b856d86d975951b0e6b5f138967f974ef09e25aa
xieguigang <xie.guigang@gcmodeller.org>
2019-09-03 11:21:47 +0800

    save gsea background rda dataset

--- src/R.Bioconductor/RDotNET.Extensions.GCModeller/KEGG/GSEABackground.vb
+++ src/R.Bioconductor/RDotNET.Extensions.GCModeller/KEGG/GSEABackground.vb
@@ -1,24 +1,43 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Linq
 Imports RDotNET.Extensions.VisualBasic
 Imports RDotNET.Extensions.VisualBasic.API
 Imports SMRUCC.genomics.Assembly.KEGG
-Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
 Imports VisualBasicNet = Microsoft.VisualBasic.Language.Runtime
 
 Public Module GSEABackground
 
     ''' <summary>
-    ''' 从标准参考图数据中建立富集分析的背景模型
+    ''' 从标准参考图数据中建立通用的富集分析的背景模型
     ''' </summary>
     ''' <param name="maps"></param>
     ''' <returns></returns>
     ''' 
     <Extension>
-    Public Function SaveMetaboliteBackgroundRda(maps As IEnumerable(Of DBGET.bGetObject.PathwayMap), rdafile$) As Boolean
+    Public Function SaveBackgroundRda(maps As IEnumerable(Of DBGET.bGetObject.PathwayMap), rdafile$) As Boolean
         Dim backgroundSize%
         Dim terms As New List(Of String)
         Dim background As New var("GSEA.KEGGCompounds", "list()")
         Dim pathways As New var("list()")
+
+        With New VisualBasicNet
+            For Each map In maps
+                pathways(map.EntryId) = base.list(
+                    !id = map.EntryId,
+                    !name = map.name,
+                    !compounds = map.KEGGCompound _
+                        .Select(Function(c) c.name) _
+                        .DoCall(Function(list)
+                                    Return base.c(list, stringVector:=True)
+                                End Function),
+                    !ko = map.KEGGOrthology _
+                        .EntityList _
+                        .DoCall(Function(list)
+                                    Return base.c(list, stringVector:=True)
+                                End Function)
+                )
+            Next
+        End With
 
         With background
             !size = backgroundSize
