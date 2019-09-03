393576eb7f18fdb88d16f8ddf82b105093626ab9
xieguigang <xie.guigang@gcmodeller.org>
2019-09-03 09:37:50 +0800

    Create background model for KEGG pathway enrichment based on the kegg metabolites, used for LC-MS metabolism data analysis.

--- src/GCModeller/annotations/GSEA/GSEA.KnowledgeBase.Extensions/KEGGCompounds.vb
+++ src/GCModeller/annotations/GSEA/GSEA.KnowledgeBase.Extensions/KEGGCompounds.vb
@@ -1,4 +1,8 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject.Organism
 
 ''' <summary>
 ''' Create background model for KEGG pathway enrichment based on the kegg metabolites, used for LC-MS metabolism data analysis.
@@ -6,18 +10,48 @@
 Public Module KEGGCompounds
 
     ''' <summary>
-    ''' Create GSEA background model from bbh annotation result.
+    ''' Create GSEA background model from LC-MS metabolism analysis result.
     ''' </summary>
     ''' <param name="maps"></param>
-    ''' <param name="backgroundSize">
-    ''' The total number of genes in background genome. 
-    ''' </param>
     ''' <returns></returns>
     <Extension>
-    Public Function CreateBackground(maps As IEnumerable(Of map),
-                                     Optional backgroundSize% = -1,
-                                     Optional outputAll As Boolean = True,
-                                     Optional genomeName$ = "Unknown") As Background
+    Public Function CreateBackground(org As OrganismInfo, maps As IEnumerable(Of Pathway), Optional outputAll As Boolean = True) As Background
+        ' The total number of metabolites in background genome. 
+        Dim backgroundSize% = 0
+        Dim clusters As New List(Of Cluster)
 
+        For Each map As Pathway In maps
+            clusters += New Cluster With {
+                .description = map.description,
+                .ID = map.briteID,
+                .names = map.name,
+                .members = map.compound _
+                    .Select(Function(c)
+                                Return New BackgroundGene With {
+                                    .name = c.text,
+                                    .accessionID = c.name,
+                                    .[alias] = {c.name},
+                                    .locus_tag = c,
+                                    .term_id = {c.name}
+                                }
+                            End Function) _
+                    .ToArray
+            }
+        Next
+
+        backgroundSize = clusters _
+            .Select(Function(c) c.members) _
+            .IteratesALL _
+            .Select(Function(c) c.accessionID) _
+            .Distinct _
+            .Count
+
+        Return New Background With {
+            .build = Now,
+            .clusters = clusters,
+            .comments = "Background model apply for GSEA of LC-MS metabolism analysis, created by GCModeller.",
+            .name = org.FullName,
+            .size = backgroundSize
+        }
     End Function
 End Module
