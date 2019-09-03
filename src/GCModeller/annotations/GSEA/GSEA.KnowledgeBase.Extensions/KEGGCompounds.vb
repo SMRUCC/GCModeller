2efd3dec8fd3d511d5a7a64a69211e66e23dcc0d
xieguigang <xie.guigang@gcmodeller.org>
2019-09-03 09:01:45 +0800

    LC-MS metabolism pathway enrichment analysis?

---
+++ src/GCModeller/annotations/GSEA/GSEA.KnowledgeBase.Extensions/KEGGCompounds.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Runtime.CompilerServices
+
+''' <summary>
+''' Create background model for KEGG pathway enrichment based on the kegg metabolites, used for LC-MS metabolism data analysis.
+''' </summary>
+Public Module KEGGCompounds
+
+    ''' <summary>
+    ''' Create GSEA background model from bbh annotation result.
+    ''' </summary>
+    ''' <param name="maps"></param>
+    ''' <param name="backgroundSize">
+    ''' The total number of genes in background genome. 
+    ''' </param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function CreateBackground(maps As IEnumerable(Of map),
+                                     Optional backgroundSize% = -1,
+                                     Optional outputAll As Boolean = True,
+                                     Optional genomeName$ = "Unknown") As Background
+
+    End Function
+End Module
