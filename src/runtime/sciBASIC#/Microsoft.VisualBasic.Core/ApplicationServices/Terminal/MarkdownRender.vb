7279c8a6bbc9d84758cf695c3a13205371a20bfc
xieguigang <xie.guigang@gcmodeller.org>
2019-09-06 11:51:07 +0800

    render markdown on console screen?

---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/ApplicationServices/Terminal/MarkdownRender.vb
@@ -0,0 +1,12 @@
+﻿Namespace ApplicationServices.Terminal
+
+    ''' <summary>
+    ''' A simple markdown render on console
+    ''' </summary>
+    Public Module MarkdownRender
+
+        Public Sub Print(markdown As String)
+
+        End Sub
+    End Module
+End Namespace
