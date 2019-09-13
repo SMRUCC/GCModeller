22ed371e892e0dfd0cd2f30dbe06d1d44a895432
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-09-14 02:19:47 +0800

    add blastp parser interface

--- src/GCModeller/data/Xfam/Pfam/Pipeline/LocalBlast/BlastOutputParser.vb
+++ src/GCModeller/data/Xfam/Pfam/Pipeline/LocalBlast/BlastOutputParser.vb
@@ -1,6 +1,28 @@
-﻿Namespace Pipeline.LocalBlast
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput.BlastPlus
+
+Namespace Pipeline.LocalBlast
 
     Public Module BlastOutputParser
 
+        ''' <summary>
+        ''' PfamA as query, alignment with protein sequence as subjects
+        ''' </summary>
+        ''' <param name="query"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Iterator Function ParseDomainQuery(query As Query) As IEnumerable(Of PfamHit)
+
+        End Function
+
+        ''' <summary>
+        ''' The protein sequence as query input, alignment with pfamA domain sequence as subjects.
+        ''' </summary>
+        ''' <param name="query"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Iterator Function ParseProteinQuery(query As Query) As IEnumerable(Of PfamHit)
+
+        End Function
     End Module
 End Namespace
