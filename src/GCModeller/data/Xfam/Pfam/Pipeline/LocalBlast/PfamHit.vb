ab4d77513d3dc4e16e024481c58c34223e4c7707
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-09-14 01:14:17 +0800

    pfam hit

---
+++ src/GCModeller/data/Xfam/Pfam/Pipeline/LocalBlast/PfamHit.vb
@@ -0,0 +1,22 @@
+﻿Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
+
+Namespace Pipeline.LocalBlast
+
+    ''' <summary>
+    ''' A hit model of pfam domain annotation on query protein sequence.
+    ''' </summary>
+    Public Class PfamHit : Inherits BestHit
+
+        ''' <summary>
+        ''' The start position of this domain object on target sequence
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property start As Integer
+        ''' <summary>
+        ''' The end position of this domain object on target sequence.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property ends As Integer
+
+    End Class
+End Namespace
