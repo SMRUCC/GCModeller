f77d33e00104e84efe299e5a5154f66cd9e00008
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-09-14 00:44:53 +0800

    required improvements of the pfam annotation module

---
+++ src/GCModeller/data/GO_gene-ontology/GO_Annotation/Annotations.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.Data.csv
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.Pipeline
+
+Public Module Annotations
+
+    Public Function LoadSBHMaps(filepath As String) As IEnumerable(Of BestHit)
+        Return filepath.LoadCsv(Of BestHit)(skipWhile:=SkipHitNotFound)
+    End Function
+
+    Public Function PfamAssign()
+
+    End Function
+End Module
---
+++ src/interops/localblast/LocalBLAST/Pipeline/Extensions.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Namespace Pipeline
+
+    <HideModuleName> Public Module Extensions
+
+        ''' <summary>
+        ''' 在反序列化之前，使用这个过滤器过滤掉一些无效的hits来节省反序列的处理时间
+        ''' </summary>
+        ''' <returns></returns>
+        Public Function SkipHitNotFound() As NamedValue(Of Func(Of String, Boolean))
+            Return New NamedValue(Of Func(Of String, Boolean)) With {
+                .Name = "hit_name",
+                .Value = Function(colVal)
+                             ' 将所有的HITS_NOT_FOUND的行都跳过
+                             ' 这样子可以节省比较多的内存
+                             Return colVal = "HITS_NOT_FOUND"
+                         End Function
+            }
+        End Function
+    End Module
+End Namespace
