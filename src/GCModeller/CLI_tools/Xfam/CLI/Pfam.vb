f258e8a895773d92aa6ad576789f92af73e53af2
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-09-14 19:31:36 +0800

    add argument debug echo

--- src/GCModeller/CLI_tools/Xfam/CLI/Pfam.vb
+++ src/GCModeller/CLI_tools/Xfam/CLI/Pfam.vb
@@ -1,41 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::ca4d9eca6a1c4b3f25393d0c8e406b7c, CLI_tools\Xfam\CLI\Pfam.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CLI
-    ' 
-    '     Function: ExportHMMScan, ExportHMMSearch, ExportPfamHits, ExportUltraLarge, getPfam
-    ' 
-    ' /********************************************************************************/
+' Module CLI
+' 
+'     Function: ExportHMMScan, ExportHMMSearch, ExportPfamHits, ExportUltraLarge, getPfam
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -48,6 +48,7 @@ Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.csv.IO.Linq
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.genomics.Analysis.SequenceTools.Sanger.PfamHMMScan
 Imports SMRUCC.genomics.Analysis.SequenceTools.Sanger.PfamHMMScan.hmmscan
 Imports SMRUCC.genomics.Data.Xfam.Pfam
@@ -61,7 +62,7 @@ Imports Query = SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput.
 Partial Module CLI
 
     <ExportAPI("/Export.PfamHits")>
-    <Usage("/Export.PfamHits /in <blastp_vs_pfamA.txt> [/alt.direction /evalue <1e-5> /coverage <0.85> /identities <0.9> /out <pfamhits.csv>]")>
+    <Usage("/Export.PfamHits /in <blastp_vs_pfamA.txt> [/alt.direction /evalue <1e-5> /coverage <0.8> /identities <0.7> /out <pfamhits.csv>]")>
     <Argument("/out", True, CLITypes.File, PipelineTypes.std_out,
               AcceptTypes:={GetType(PfamHit)},
               Extensions:="*.csv",
@@ -84,13 +85,19 @@ Partial Module CLI
         Dim in$ = args <= "/in"
         Dim alt_direction As Boolean = args("/alt.direction")
         Dim evalue# = args("/evalue") Or 0.00001
-        Dim coverage# = args("/coverage") Or 0.85
-        Dim identities# = args("/identities") Or 0.9
+        Dim coverage# = args("/coverage") Or 0.8
+        Dim identities# = args("/identities") Or 0.7
         Dim i As i32 = 0
         Dim out$ = args("/out") Or $"{[in].TrimSuffix}.pfamhits.csv"
         Dim hits As PfamHit()
 
         Call $"Parse {[in]}...".__INFO_ECHO
+        Call New Dictionary(Of String, Double) From {
+            {NameOf(evalue), evalue},
+            {NameOf(coverage), coverage},
+            {NameOf(identities), identities}
+        }.GetJson _
+         .__DEBUG_ECHO
 
         Using pfamhits As New WriteStream(Of PfamHit)(out)
             For Each query As Query In BlastpOutputReader.RunParser(in$, fast:=False)
