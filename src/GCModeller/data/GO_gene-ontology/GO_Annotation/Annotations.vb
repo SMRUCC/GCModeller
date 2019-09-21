dea1b1055b6738f2d31ec103aa704fdd05cff552
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-09-21 14:07:55 +0800

    create go annotation interface

--- src/GCModeller/data/GO_gene-ontology/GO_Annotation/Annotations.vb
+++ src/GCModeller/data/GO_gene-ontology/GO_Annotation/Annotations.vb
@@ -1,55 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::ef36f2141674ae9feb5fc7fee35f8b2d, GO_gene-ontology\GO_Annotation\Annotations.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Annotations
-    ' 
-    '     Function: LoadSBHMaps, PfamAssign
-    ' 
-    ' /********************************************************************************/
+' Module Annotations
+' 
+'     Function: LoadSBHMaps, PfamAssign
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.csv
+Imports SMRUCC.genomics.Data.Xfam.Pfam.PfamString
+Imports SMRUCC.genomics.Data.Xfam.Pfam.Pipeline.LocalBlast
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.Pipeline
 
 Public Module Annotations
 
-    Public Function LoadSBHMaps(filepath As String) As IEnumerable(Of BestHit)
-        Return filepath.LoadCsv(Of BestHit)(skipWhile:=SkipHitNotFound)
+    <Extension>
+    Public Function PfamAssign(pfamhits As IEnumerable(Of PfamHit), pfam2GO As Dictionary(Of String, toGO))
+
     End Function
 
-    Public Function PfamAssign()
+    <Extension>
+    Public Function PfamAssign(pfamhits As IEnumerable(Of PfamString), pfam2GO As Dictionary(Of String, toGO))
 
     End Function
 End Module
--- src/GCModeller/data/GO_gene-ontology/GO_Annotation/NamespaceDoc.vb
+++ src/GCModeller/data/GO_gene-ontology/GO_Annotation/NamespaceDoc.vb
@@ -1,47 +1,46 @@
 ﻿#Region "Microsoft.VisualBasic::b7edee4c22392721d909b6c0e0687521, GO_gene-ontology\GO_Annotation\NamespaceDoc.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class NamespaceDoc
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Class NamespaceDoc
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 ''' <summary>
 ''' Go term annotation based on the Pfam/InterPro analysis.
 ''' </summary>
-Public Class NamespaceDoc
-
-End Class
+Module NamespaceDoc
+End Module
