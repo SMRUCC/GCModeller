054e33fdbacd41532bb16db92b7cbb3d24b4a4a4
xieguigang <xie.guigang@gcmodeller.org>
2019-09-30 09:17:01 +0800

    minvalue object?

---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Extensions/Math/StatisticsMathExtensions/Min.vb
@@ -0,0 +1,17 @@
+﻿Namespace Math.Statistics
+
+    Public Class Min
+
+        Public ReadOnly Property MinValue As Double
+
+        Sub New(value As Double)
+            MinValue = value
+        End Sub
+
+        Public Sub Add(sample As Double)
+            If sample < MinValue Then
+                _MinValue = sample
+            End If
+        End Sub
+    End Class
+End Namespace
