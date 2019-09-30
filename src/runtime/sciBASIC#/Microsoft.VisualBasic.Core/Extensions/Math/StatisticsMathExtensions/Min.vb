26c95be896ebad3097068bb8b5b6b5e0ae0b3af2
xieguigang <xie.guigang@gcmodeller.org>
2019-09-30 09:29:48 +0800

    minvalue

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Extensions/Math/StatisticsMathExtensions/Min.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Extensions/Math/StatisticsMathExtensions/Min.vb
@@ -1,17 +1,27 @@
 ﻿Namespace Math.Statistics
 
-    Public Class Min
+    Public Class Min : Inherits SampleObservation
 
         Public ReadOnly Property MinValue As Double
 
         Sub New(value As Double)
+            Call MyBase.New({value})
+            ' intialize value is the min value
             MinValue = value
         End Sub
 
-        Public Sub Add(sample As Double)
-            If sample < MinValue Then
-                _MinValue = sample
+        Protected Overrides Sub addObservation(observation As Double)
+            If observation < MinValue Then
+                _MinValue = observation
             End If
         End Sub
+
+        Protected Overrides Function getEigenvalue() As Double
+            Return MinValue
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return MinValue
+        End Function
     End Class
 End Namespace
