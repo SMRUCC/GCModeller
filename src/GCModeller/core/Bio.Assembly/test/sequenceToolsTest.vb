c1fcc44194e9718b9b9b246108b96cd3a2b58a42
xieguigang <xie.guigang@gcmodeller.org>
2019-10-26 10:00:47 +0800

    add test for cut sequence linear

--- src/GCModeller/core/Bio.Assembly/test/sequenceToolsTest.vb
+++ src/GCModeller/core/Bio.Assembly/test/sequenceToolsTest.vb
@@ -6,6 +6,10 @@ Module sequenceToolsTest
     Sub Main()
         Dim nt As New FastaSeq With {.Headers = {"ABC"}, .SequenceData = "abcdefghijklmnopqrstuvwxyz"}
         Dim cut = nt.CutSequenceLinear(New Location With {.left = 1, .right = 7})
+        Dim cut2 = nt.CutSequenceLinear(New Location With {.left = 1, .right = 25})
+        Dim cut3 = nt.CutSequenceLinear(New Location With {.left = 1, .right = 26})
+        Dim cut4 = nt.CutSequenceLinear(New Location With {.left = 1, .right = 30})
+        Dim cut5 = nt.CutSequenceLinear(New Location With {.left = 30, .right = 32})
 
         Pause()
     End Sub
