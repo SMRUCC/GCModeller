20e6de87c155d72de42f2c7c3968e4d417b69f83
xieguigang <xie.guigang@gcmodeller.org>
2019-10-28 15:14:10 +0800

    bar plot

--- src/GCModeller/annotations/GO/NamespaceCategoryPlots.vb
+++ src/GCModeller/annotations/GO/NamespaceCategoryPlots.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.ChartPlots.BarPlot
 Imports Microsoft.VisualBasic.Imaging.Driver
 Imports SMRUCC.genomics.Analysis.Microarray
 Imports SMRUCC.genomics.Data.GeneOntology.OBO
@@ -21,12 +22,32 @@ Public Module NamespaceCategoryPlots
                     Optional pvalue# = 0.05,
                     Optional size$ = "2200,2000",
                     Optional tick# = 1,
-                    Optional gray As Boolean = False,
-                    Optional labelRightAlignment As Boolean = False,
                     Optional usingCorrected As Boolean = False,
                     Optional top% = -1,
                     Optional colorSchema$ = "Set1:c6") As IEnumerable(Of NamedValue(Of GraphicsData))
 
+        Dim namespaceProfiles = data.CreateEnrichmentProfiles(GO_terms, usingCorrected, top, pvalue)
+        Dim image As GraphicsData
+        Dim namespaceTitle$
 
+        For Each [namespace] In namespaceProfiles
+            namespaceTitle = [namespace].Key
+            image = [namespace].Value.doSingleBarplot(namespaceTitle, size, tick, colorSchema)
+
+            Yield New NamedValue(Of GraphicsData) With {
+                .Name = [namespace].Key,
+                .Value = image
+            }
+        Next
     End Function
+
+    <Extension>
+    Private Function doSingleBarplot(profiles As NamedValue(Of Double)(), namespace$, size$, tick#, colorSchema$) As GraphicsData
+        Return LevelBarplot.Plot(
+            data:=profiles,
+            size:=size,
+            title:=$"Enrichment of {[namespace]}"
+        )
+    End Function
+
 End Module
---
+++ src/runtime/sciBASIC#/Data_science/Visualization/Plots/BarPlot/LevelBarplot.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS
+
+Namespace BarPlot
+
+    ''' <summary>
+    ''' 只针对单组数据的条形图绘制
+    ''' </summary>
+    Public Module LevelBarplot
+
+        Public Function Plot(data As NamedValue(Of Double)(),
+                             Optional size$ = "2000,1600",
+                             Optional margin$ = Resolution2K.PaddingWithRightLegend,
+                             Optional bg$ = "white",
+                             Optional title$ = "BarPlot",
+                             Optional titleFontCSS$ = CSSFont.Win7VeryLarge) As GraphicsData
+
+        End Function
+    End Module
+End Namespace
