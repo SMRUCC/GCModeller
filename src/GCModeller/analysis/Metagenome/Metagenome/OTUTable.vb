aaa8c9804e159c4c86343f678a8afd09f190c738
xieguigang <xie.guigang@gcmodeller.org>
2019-10-31 10:48:19 +0800

    improvements of the taxonomy object parser

--- src/GCModeller/analysis/Metagenome/Metagenome/OTUTable.vb
+++ src/GCModeller/analysis/Metagenome/Metagenome/OTUTable.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::b242c49b740ef306c8693b52165b8fac, analysis\Metagenome\Metagenome\OTUTable.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class OTUTable
-    ' 
-    '     Properties: ID, Taxonomy
-    ' 
-    '     Function: FromOTUData, LoadSample
-    ' 
-    ' /********************************************************************************/
+' Class OTUTable
+' 
+'     Properties: ID, Taxonomy
+' 
+'     Function: FromOTUData, LoadSample
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -52,6 +52,8 @@ Imports SMRUCC.genomics.Metagenomics
 
 ''' <summary>
 ''' 这个对象记录了当前的宏基因组实验之中的每一个OTU在样品之中的含量的多少
+''' 
+''' 这个对象的数据结构与<see cref="OTUData"/>类似, 二者可以做等价替换
 ''' </summary>
 Public Class OTUTable : Inherits DataSet
 
@@ -62,7 +64,9 @@ Public Class OTUTable : Inherits DataSet
     ''' OTU编号所对应的物种分类信息
     ''' </summary>
     ''' <returns></returns>
-    Public Property Taxonomy As Taxonomy
+    ''' 
+    <Column("taxonomy", GetType(BIOMTaxonomyParser))>
+    Public Property taxonomy As Taxonomy
 
     ''' <summary>
     ''' 这个函数会自动兼容csv或者tsv格式的
@@ -91,12 +95,12 @@ Public Class OTUTable : Inherits DataSet
         Return data _
             .SafeQuery _
             .Select(Function(d)
-                        Dim lineage As New Taxonomy(parser(d.Taxonomy))
+                        Dim lineage As New Taxonomy(parser(d.taxonomy))
 
                         Return New OTUTable With {
                             .ID = d.OTU,
-                            .Properties = d.Data.AsNumeric,
-                            .Taxonomy = lineage
+                            .Properties = d.data.AsNumeric,
+                            .taxonomy = lineage
                         }
                     End Function)
     End Function
--- src/GCModeller/core/Bio.Assembly/Metagenomics/BIOMTaxonomy.vb
+++ src/GCModeller/core/Bio.Assembly/Metagenomics/BIOMTaxonomy.vb
@@ -1,48 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::57c5f8d9860433d649b3d69d78571a31, Bio.Assembly\Metagenomics\BIOMTaxonomy.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module BIOMTaxonomy
-    ' 
-    '         Properties: BIOMPrefix, BIOMPrefixAlt, BriefParser, CompleteParser
-    '         Delegate Function
-    ' 
-    '             Constructor: (+1 Overloads) Sub New
-    '             Function: AsTaxonomy, FillLineageEmpty, TaxonomyFromString, (+2 Overloads) TaxonomyParser, TaxonomyParserAlt
-    '                       TaxonomyString
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module BIOMTaxonomy
+' 
+'         Properties: BIOMPrefix, BIOMPrefixAlt, BriefParser, CompleteParser
+'         Delegate Function
+' 
+'             Constructor: (+1 Overloads) Sub New
+'             Function: AsTaxonomy, FillLineageEmpty, TaxonomyFromString, (+2 Overloads) TaxonomyParser, TaxonomyParserAlt
+'                       TaxonomyString
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -51,9 +51,39 @@ Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language.Default
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Scripting.Runtime
 Imports SMRUCC.genomics.Assembly.NCBI.Taxonomy
 
 Namespace Metagenomics
+
+    ''' <summary>
+    ''' Parser and stringfier of <see cref="Taxonomy"/> object.
+    ''' </summary>
+    Public Class BIOMTaxonomyParser : Implements IParser
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="obj">
+        ''' Object value should be in data type <see cref="Taxonomy"/>
+        ''' </param>
+        ''' <returns></returns>
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overloads Function ToString(obj As Object) As String Implements IParser.ToString
+            Return DirectCast(obj, Taxonomy).ToString(BIOMstyle:=True)
+        End Function
+
+        ''' <summary>
+        ''' Create a <see cref="Taxonomy"/> object from parse taxonomy string
+        ''' </summary>
+        ''' <param name="content"></param>
+        ''' <returns></returns>
+        ''' 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function TryParse(content As String) As Object Implements IParser.TryParse
+            Return BIOMTaxonomy.TaxonomyParser(content).AsTaxonomy
+        End Function
+    End Class
 
     Public Module BIOMTaxonomy
 
