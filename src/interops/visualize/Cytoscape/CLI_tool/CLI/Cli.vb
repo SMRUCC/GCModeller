e0588e5f70874465c37bfe4b35ecdfa056a3affb
xieguigang <xie.guigang@gcmodeller.org>
2019-11-03 00:16:04 +0800

    fix type reference errors

--- src/interops/visualize/Cytoscape/CLI_tool/CLI/Cli.vb
+++ src/interops/visualize/Cytoscape/CLI_tool/CLI/Cli.vb
@@ -1,41 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::6429954dd232e7eb70e062bc57b3b95c, visualize\Cytoscape\CLI_tool\CLI\CLI.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CLI
-    ' 
-    '     Function: cyjsAsGraph, DrawingInvoke, getNetworkParser, getStyleParser
-    ' 
-    ' /********************************************************************************/
+' Module CLI
+' 
+'     Function: cyjsAsGraph, DrawingInvoke, getNetworkParser, getStyleParser
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -48,6 +48,7 @@ Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.genomics.Visualize.Cytoscape
 Imports SMRUCC.genomics.Visualize.Cytoscape.CytoscapeGraphView
 Imports SMRUCC.genomics.Visualize.Cytoscape.CytoscapeGraphView.XGMML
+Imports SMRUCC.genomics.Visualize.Cytoscape.CytoscapeGraphView.XGMML.File
 Imports SMRUCC.genomics.Visualize.Cytoscape.Visualization
 
 <Package("Cytoscape.CLI",
@@ -68,7 +69,7 @@ Imports SMRUCC.genomics.Visualize.Cytoscape.Visualization
         Dim Style As Visualization.VizMap =
             argvs.GetObject(Of VizMap)("/style", argvs.GetObject(Of Func(Of String, VizMap))("/style_parser", AddressOf getStyleParser))
 
-        Dim NetworkGraph = argvs.GetObject(Of Graph)("/network", argvs.GetObject(Of Func(Of String, Graph))("/parser", AddressOf getNetworkParser))
+        Dim NetworkGraph = argvs.GetObject(Of XGMMLgraph)("/network", argvs.GetObject(Of Func(Of String, XGMMLgraph))("/parser", AddressOf getNetworkParser))
         Dim res As Image = Nothing
 
         If Style Is Nothing Then
@@ -87,9 +88,9 @@ Imports SMRUCC.genomics.Visualize.Cytoscape.Visualization
         Return 0
     End Function
 
-    Private Function getNetworkParser(name As String) As Func(Of String, Graph)
+    Private Function getNetworkParser(name As String) As Func(Of String, XGMMLgraph)
         If String.Equals(name, "xgmml", StringComparison.OrdinalIgnoreCase) Then
-            Return Function(path As String) Graph.Load(path)
+            Return Function(path As String) RDFXml.Load(path)
         ElseIf String.Equals(name, "cyjs", StringComparison.OrdinalIgnoreCase) Then
             Return AddressOf CLI.cyjsAsGraph
         Else
@@ -98,7 +99,7 @@ Imports SMRUCC.genomics.Visualize.Cytoscape.Visualization
         End If
     End Function
 
-    Private Function cyjsAsGraph(cyjs As String) As Graph
+    Private Function cyjsAsGraph(cyjs As String) As XGMMLgraph
         Dim jsonText As String = IO.File.ReadAllText(cyjs)
         Dim json As Cyjs.Cyjs = (jsonText).LoadJSON(Of Cyjs.Cyjs)
         Return json.ToGraphModel
