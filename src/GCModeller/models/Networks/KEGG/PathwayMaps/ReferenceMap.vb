a6faeef5a2361761f11aa5a6668a82deadc5c408
xieguigang <xie.guigang@gcmodeller.org>
2019-11-03 10:17:58 +0800

    scan maps

---
+++ src/GCModeller/models/Networks/KEGG/PathwayMaps/ReferenceMap.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports SMRUCC.genomics.Assembly.KEGG.WebServices
+
+Namespace PathwayMaps
+
+    Public Module ReferenceMap
+
+        Public Function BuildNetworkModel(maps As IEnumerable(Of Map)) As NetworkTables
+
+        End Function
+    End Module
+End Namespace
---
+++ src/GCModeller/models/Networks/KEGG/PathwayMaps/ReferenceMapRender.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports SMRUCC.genomics.Visualize.Cytoscape.CytoscapeGraphView.XGMML.File
+
+Namespace PathwayMaps
+
+    Public Module ReferenceMapRender
+
+        ''' <summary>
+        ''' 将完成node和edge布局操作的网络模型进行渲染
+        ''' </summary>
+        ''' <param name="model"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function Render(model As XGMMLgraph) As GraphicsData
+
+        End Function
+    End Module
+End Namespace
