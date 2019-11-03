f8873a7d38e59c5ae76f25d2201120269056e10a
xieguigang <xie.guigang@gcmodeller.org>
2019-11-03 10:54:44 +0800

    fix a possible null reference error

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/Area.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/Area.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::2f17278cfff4ada48ac242f8c0cbf670, Bio.Assembly\Assembly\KEGG\Web\Map\Area.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Area
-    ' 
-    '         Properties: coords, href, IDVector, Names, Rectangle
-    '                     shape, title, Type
-    ' 
-    '         Function: Parse, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Area
+' 
+'         Properties: coords, href, IDVector, Names, Rectangle
+'                     shape, title, Type
+' 
+'         Function: Parse, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -145,19 +145,27 @@ Namespace Assembly.KEGG.WebServices
 
         Public ReadOnly Property Names As NamedValue(Of String)()
             Get
-                Dim t = title _
-                    .Split(","c) _
-                    .Select(AddressOf Trim) _
-                    .Select(Function(s)
-                                Dim name = s.GetTagValue(" ")
-                                Return New NamedValue(Of String) With {
-                                    .Name = name.Name,
-                                    .Value = name.Value.GetStackValue("(", ")")
-                                }
-                            End Function) _
-                    .ToArray
+                Dim terms As NamedValue(Of String)()
 
-                Return t
+                If title.StringEmpty Then
+                    Return {}
+                Else
+                    terms = title _
+                        .Split(","c) _
+                        .Select(AddressOf Trim) _
+                        .Select(Function(s)
+                                    Dim nameTerm = s.GetTagValue(" ")
+                                    Dim name$ = nameTerm.Value.GetStackValue("(", ")")
+
+                                    Return New NamedValue(Of String) With {
+                                        .Name = nameTerm.Name,
+                                        .Value = name
+                                    }
+                                End Function) _
+                        .ToArray
+                End If
+
+                Return terms
             End Get
         End Property
 
