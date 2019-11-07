848eced5284d1d0ef27325deacb7e48948ce36b2
xieguigang <xie.guigang@gcmodeller.org>
2019-11-07 14:19:21 +0800

    load reaction class data files

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/Objects/Pathway/ReactionClass.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/Objects/Pathway/ReactionClass.vb
@@ -1,6 +1,9 @@
 ﻿Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ComponentModel
+Imports Microsoft.VisualBasic.Terminal.ProgressBar
 Imports Microsoft.VisualBasic.Text.Xml.Models
+Imports Microsoft.VisualBasic.Language.UnixBash
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 
 Namespace Assembly.KEGG.DBGET.bGetObject
 
@@ -14,6 +17,27 @@ Namespace Assembly.KEGG.DBGET.bGetObject
         Public Property enzymes As NamedValue()
         Public Property pathways As NamedValue()
         Public Property orthology As NamedValue()
+
+        Public Shared Iterator Function ScanRepository(repository As String) As IEnumerable(Of ReactionClass)
+            Dim busy As New SwayBar
+            Dim message$
+            Dim [class] As ReactionClass
+            Dim loaded As New Index(Of String)
+
+            For Each xml As String In ls - l - r - "*.xml" <= repository
+                [class] = xml.LoadXml(Of ReactionClass)
+                message = [class].definition
+
+                Call busy.Step(message)
+
+                If Not [class].entryId Like loaded Then
+                    loaded.Add([class].entryId)
+
+                    ' return current file data
+                    Yield [class]
+                End If
+            Next
+        End Function
 
     End Class
 End Namespace
---
+++ src/GCModeller/core/Bio.Repository/KEGG/ReactionRepository/ReactionClassifier.vb
@@ -0,0 +1,18 @@
+﻿Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+
+Public Class ReactionClassifier
+
+    Dim classes As ReactionClass()
+    Dim reactionIndex As Dictionary(Of String, ReactionClass)
+
+    Private Function buildIndex() As ReactionClassifier
+
+    End Function
+
+    Public Shared Function FromRepository(directory As String) As ReactionClassifier
+        Return New ReactionClassifier With {
+            .classes = ReactionClass.ScanRepository(directory).ToArray
+        }.buildIndex
+    End Function
+
+End Class
