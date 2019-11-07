30a272279f922e260a0a146feadf20302ae5a2cd
xieguigang <xie.guigang@gcmodeller.org>
2019-11-07 11:39:26 +0800

    ReactionClass

---
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/BriteHEntry/CategoryEntry/ReactionClass.vb
@@ -0,0 +1,37 @@
+﻿
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+
+Namespace Assembly.KEGG.DBGET.BriteHEntry
+
+    Public Class ReactionClass : Implements INamedValue
+
+        ''' <summary>
+        ''' A
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property [class] As String
+        ''' <summary>
+        ''' B
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property subclass As String
+        ''' <summary>
+        ''' C
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property category As String
+        ''' <summary>
+        ''' D
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property ECNumber As String
+
+        ''' <summary>
+        ''' E: The KEGG RC number
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property RCNumber As String Implements IKeyedEntity(Of String).Key
+
+    End Class
+End Namespace
