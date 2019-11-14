3177928a4a7f31df94006f1882544f40974401f7
xieguigang <xie.guigang@gcmodeller.org>
2019-11-14 11:04:56 +0800

    wmf file writer driver

---
+++ src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/Drivers/Models/WmfData.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Drawing
+Imports System.IO
+
+Namespace Driver
+
+    Public Class WmfData : Inherits GraphicsData
+
+        Public Overrides ReadOnly Property Driver As Drivers
+            Get
+                Return Drivers.WMF
+            End Get
+        End Property
+
+        ReadOnly tmpfile As String = App.GetAppSysTempFile(".wmf", App.PID, RandomASCIIString(10, skipSymbols:=True))
+
+        Public Sub New(img As Object, size As Size)
+            MyBase.New(img, size)
+        End Sub
+
+        Public Overrides Function Save(path As String) As Boolean
+            Return tmpfile.FileCopy(path)
+        End Function
+
+        Public Overrides Function Save(out As Stream) As Boolean
+            Using reader As FileStream = tmpfile.Open(FileMode.Open, doClear:=False)
+                Call out.Seek(Scan0, SeekOrigin.Begin)
+                Call reader.CopyTo(out)
+            End Using
+
+            Return True
+        End Function
+    End Class
+End Namespace
