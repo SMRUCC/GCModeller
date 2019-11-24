523515fa5135675c701720504f3304d7f68362d0
xieguigang <xie.guigang@gcmodeller.org>
2019-11-24 20:25:47 +0800

    sequence toolkit plugin for R# language

---
+++ src/workbench/R#/seqtoolkit/Fasta.vb
@@ -0,0 +1,33 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Text
+Imports SMRUCC.genomics.SequenceModel.FASTA
+Imports SMRUCC.Rsharp.Runtime.Internal
+
+''' <summary>
+''' Fasta sequence toolkit
+''' </summary>
+''' 
+<Package("bioseq.fasta")>
+Module Fasta
+
+    Sub New()
+        Call printer.AttachConsoleFormatter(Of FastaSeq)(Function(seq)
+                                                             With DirectCast(seq, FastaSeq)
+                                                                 Return "> " & .Title & ASCII.CR & .SequenceData
+                                                             End With
+                                                         End Function)
+    End Sub
+
+    ''' <summary>
+    ''' Read a single fasta sequence file
+    ''' </summary>
+    ''' <param name="file">
+    ''' Just contains one sequence
+    ''' </param>
+    ''' <returns></returns>
+    <ExportAPI("read.seq")>
+    Public Function readSeq(file As String) As FastaSeq
+        Return FastaSeq.Load(file)
+    End Function
+End Module
