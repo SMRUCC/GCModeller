f3129a8a867c1d77a30ebd5202e6c525297ca455
xieguigang <xie.guigang@gcmodeller.org>
2019-11-25 15:25:58 +0800

    split source file

---
+++ src/GCModeller/engine/Dynamics/Engine/OmicsTuple.vb
@@ -0,0 +1,19 @@
+﻿Namespace Engine
+
+    Public Class OmicsTuple(Of T)
+
+        Public ReadOnly transcriptome As T
+        Public ReadOnly proteome As T
+        Public ReadOnly metabolome As T
+
+        Sub New(transcriptome As T, proteome As T, metabolome As T)
+            Me.transcriptome = transcriptome
+            Me.proteome = proteome
+            Me.metabolome = metabolome
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return GetType(T).Name
+        End Function
+    End Class
+End Namespace
