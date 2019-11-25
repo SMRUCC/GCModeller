60a8a473612b09eea489f21e93541840c52a6182
xieguigang <xie.guigang@gcmodeller.org>
2019-11-25 16:35:32 +0800

    add namespace docs

---
+++ src/GCModeller/engine/IO/Raw/GCModellerRaw/NamespaceDoc.vb
@@ -0,0 +1,8 @@
+﻿Namespace Raw
+
+    ''' <summary>
+    ''' The GCModeller Virtual Cell raw data file format and I/O 
+    ''' </summary>
+    Friend Class NamespaceDoc
+    End Class
+End Namespace
