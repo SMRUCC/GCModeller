d4eed5b1931fa2a3bb7ce50276f269c982c41815
xieguigang <xie.guigang@gcmodeller.org>
2019-11-25 17:06:13 +0800

    split source files

---
+++ src/GCModeller/engine/Dynamics/Engine/Loader/FluxLoader.vb
@@ -0,0 +1,23 @@
+﻿Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Core
+Imports SMRUCC.genomics.GCModeller.ModellingEngine.Model
+
+Namespace Engine.ModelLoader
+
+    Public MustInherit Class FluxLoader
+
+        Public ReadOnly Property MassTable As MassTable
+            Get
+                Return loader.massTable
+            End Get
+        End Property
+
+        Protected ReadOnly loader As Loader
+
+        Protected Sub New(loader As Loader)
+            Me.loader = loader
+        End Sub
+
+        Public MustOverride Function CreateFlux(cell As CellularModule) As IEnumerable(Of Channel)
+
+    End Class
+End Namespace
--- src/GCModeller/engine/Dynamics/Engine/Loader/Loader.vb
+++ src/GCModeller/engine/Dynamics/Engine/Loader/Loader.vb
@@ -47,24 +47,6 @@ Imports SMRUCC.genomics.GCModeller.ModellingEngine.Model
 
 Namespace Engine.ModelLoader
 
-    Public MustInherit Class FluxLoader
-
-        Public ReadOnly Property MassTable As MassTable
-            Get
-                Return loader.massTable
-            End Get
-        End Property
-
-        Protected ReadOnly loader As Loader
-
-        Protected Sub New(loader As Loader)
-            Me.loader = loader
-        End Sub
-
-        Public MustOverride Function CreateFlux(cell As CellularModule) As IEnumerable(Of Channel)
-
-    End Class
-
     ''' <summary>
     ''' Module loader
     ''' </summary>
