5e17bc084d56713758dc5e8d87bb107c23a5a03f
xieguigang <xie.guigang@gcmodeller.org>
2019-11-25 17:36:16 +0800

    storage writer for write GCModeller raw data file

---
+++ src/GCModeller/engine/IO/Raw/GCModellerRaw/StorageDriver.vb
@@ -0,0 +1,52 @@
+﻿
+Imports System.IO
+Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Engine.ModelLoader
+Imports SMRUCC.genomics.GCModeller.ModellingEngine.Model
+
+Namespace Raw
+
+    Public Class StorageDriver : Implements IDisposable
+
+        ReadOnly output As Writer
+
+        Sub New(output$, loader As Loader, model As CellularModule)
+            Me.output = New Writer(loader, model, output.Open(FileMode.OpenOrCreate, doClear:=True))
+        End Sub
+
+
+
+#Region "IDisposable Support"
+        Private disposedValue As Boolean ' To detect redundant calls
+
+        ' IDisposable
+        Protected Overridable Sub Dispose(disposing As Boolean)
+            If Not disposedValue Then
+                If disposing Then
+                    ' TODO: dispose managed state (managed objects).
+                    Call output.Dispose()
+                End If
+
+                ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+                ' TODO: set large fields to null.
+            End If
+            disposedValue = True
+        End Sub
+
+        ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+        'Protected Overrides Sub Finalize()
+        '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        '    Dispose(False)
+        '    MyBase.Finalize()
+        'End Sub
+
+        ' This code added by Visual Basic to correctly implement the disposable pattern.
+        Public Sub Dispose() Implements IDisposable.Dispose
+            ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+            Dispose(True)
+            ' TODO: uncomment the following line if Finalize() is overridden above.
+            ' GC.SuppressFinalize(Me)
+        End Sub
+#End Region
+
+    End Class
+End Namespace
