c0268b0bac0759d1b005a48864cd672dcb866b03
xieguigang <xie.guigang@gcmodeller.org>
2019-11-27 10:47:41 +0800

    add demo batch script

---
+++ src/GCModeller/engine/Rscript/test.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+R# ./run.R --in "S:\synthetic_biology\test_model\wildtype\model\Yersinia_pseudotuberculosis_IP_32953.GCMarkup" --out "S:/synthetic_biology/test_model/wildtype/model/test20191127/normals/"
