1662d7276c9bf3eaf7ebc1749b3ddd90bc65f6fd
xieguigang <xie.guigang@gcmodeller.org>
2019-11-28 11:30:54 +0800

    add xml docs

--- src/workbench/R#/fileformatkit/JSON.vb
+++ src/workbench/R#/fileformatkit/JSON.vb
@@ -6,6 +6,11 @@ Imports Microsoft.VisualBasic.Serialization.JSON
 <Package("JSON", Category:=APICategories.UtilityTools)>
 Module JSON
 
+    ''' <summary>
+    ''' Load R# list object from a given json file
+    ''' </summary>
+    ''' <param name="file"></param>
+    ''' <returns></returns>
     <ExportAPI("load.list")>
     Public Function LoadObjectList(file As String) As Dictionary(Of String, String)
         Return file.LoadJsonFile(Of Dictionary(Of String, String))
