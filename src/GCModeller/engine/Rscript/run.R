5e3ea6ac04d9d95664d920455c559025cadee74f
xieguigang <xie.guigang@gcmodeller.org>
2019-11-28 22:45:31 +0800

    Update run.R

--- src/GCModeller/engine/Rscript/run.R
+++ src/GCModeller/engine/Rscript/run.R
@@ -60,7 +60,7 @@ let run as function(i, deletions = NULL, exp.tag = tag.name) {
     engine = [vcell = vcell] 
         :> engine.load(
             inits            = inits, 
-            iterations       = 10, 
+            iterations       = 1000, 
             time_resolutions = 0.1, 
             deletions        = deletions
         ) 
