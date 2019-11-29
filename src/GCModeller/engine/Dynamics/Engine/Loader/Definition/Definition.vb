aca52cf632868c92e63c34d979a006e7b382c73d
xieguigang <xie.guigang@gcmodeller.org>
2019-11-29 09:29:18 +0800

    general compounds?

--- src/GCModeller/engine/Dynamics/Engine/Loader/Definition/Definition.vb
+++ src/GCModeller/engine/Dynamics/Engine/Loader/Definition/Definition.vb
@@ -45,6 +45,12 @@
 
 Namespace Engine.Definitions
 
+    Public Enum GeneralCompound
+        DNA
+        RNA
+        Protein
+    End Enum
+
     ''' <summary>
     ''' 因为物质编号可能会来自于不同的数据库，所以会需要使用这个对象将一些关键的物质映射为计算引擎所能够被识别的对象
     ''' </summary>
@@ -68,6 +74,8 @@ Namespace Engine.Definitions
 
         Public Property NucleicAcid As NucleicAcid
         Public Property AminoAcid As AminoAcid
+
+        Public Property GenericCompounds As Dictionary(Of String, GeneralCompound)
 #End Region
 
         ''' <summary>
@@ -124,7 +132,12 @@ Namespace Engine.Definitions
                 .Water = "C00001",
                 .NucleicAcid = ntBase,
                 .AminoAcid = aaResidue,
-                .status = initStatus
+                .status = initStatus,
+                .GenericCompounds = New Dictionary(Of String, GeneralCompound) From {
+                    {"C00017", GeneralCompound.Protein},
+                    {"C00039", GeneralCompound.DNA},
+                    {"C00046", GeneralCompound.RNA}
+                }
             }
         End Function
 
