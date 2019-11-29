08f8320e49177a760de0565d2037b9b7b7274a79
xieguigang <xie.guigang@gcmodeller.org>
2019-11-29 15:11:57 +0800

    fix type cast errors

--- src/GCModeller/engine/Rscript/foldchange.R
+++ src/GCModeller/engine/Rscript/foldchange.R
@@ -9,6 +9,16 @@ let sampleInfo <- [file = ?"--sampleInfo"]
 #     print(sample);
 # }
 
-let normal = sampleInfo :> first(x -> x$key == "normal");
+let control.label as string = "normal";
+let normal = sampleInfo :> first(x => x$key == control.label);
 
 print(normal);
+
+# split file test
+sampleInfo 
+:> which(x -> x$key != control.label) 
+:> projectAs(group -> group$group :> write.sampleinfo( file = `${?"--sampleInfo"}.group=${group$key}.csv`));
+
+for(group in sampleInfo :> which(x -> x$key != control.label)) {
+   # print(group);
+}
