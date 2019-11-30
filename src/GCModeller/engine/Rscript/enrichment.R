44dbd67d691a9f10d3fcb5335112fe6be95578c6
xieguigang <xie.guigang@gcmodeller.org>
2019-11-30 12:11:48 +0800

    GSEA analysis script

--- src/GCModeller/engine/Rscript/enrichment.R
+++ src/GCModeller/engine/Rscript/enrichment.R
@@ -3,21 +3,41 @@ imports "GCModeller" from "GCModeller_cli.dll";
 require(dataframe);
 
 let dem <- [?"--data"] :> read.dataframe(mode = "numeric");
+let background as string <- ?"--background";
+let out as string <- ?"--save";
 let cols <- dem :> dataset.colnames;
+let kit <- list(eggHTS = eggHTS() :> as.object, profiler = profiler() :> as.object);
 
 console::progressbar.pin.top();
 
-let partition as string;
+let [partition, fileName] as string;
 let data;
-let [up, down, all] as string;
+let [up, down] as string;
+let [GSEA.save, geneSet] as string;
+
+let doEnrichment as function(id, file) {
+    geneSet <- `${out}/${fileName}/${file}.txt`;
+    GSEA.save <- `${out}/${fileName}/${file}.GSEA.txt`;
+    id :> writeLines(file = geneSet);
+    
+    # do enrichment of the kegg metabolites
+    # /GSEA /background <clusters.XML> /geneSet <geneSet.txt> [/hide.progress /locus_tag /cluster_id <null, debug_used> /format <default=GCModeller> /out <out.csv>]
+    kit$profiler$EnrichmentTest(background, geneSet = geneSet, format = "KOBAS", hide_progress = TRUE, out = GSEA.save);
+    # /KEGG.enrichment.plot /in <enrichmentTerm.csv> [/gray /colors <default=Set1:c6> /label.right /pvalue <0.05> /tick 1 /size <2000,1600> /out <out.png>]
+    kit$eggHTS$KEGG_enrichment(GSEA.save, pvalue = "0.05", tick = "-1", size = "2300,2000" ); 
+}
 
 for(i in 1:length(cols) step 3) {
     # get foldchange value
     partition <- cols[i+1];
+    fileName <- cols[i];
     data <- dem :> dataset.project(cols = partition) :> as.object;
     up <- data :> which(x -> x$GetItemValue(partition) >= 0.1) :> projectAs(x -> x$ID);
     down <- data :> which(x -> x$GetItemValue(partition) <= neg(0.1)) :> projectAs(x -> x$ID);
-    all <- up << down;
+    
+    doEnrichment(up, "up");
+    doEnrichment(down, "down");
+    doEnrichment(up << down, "all_DEM");
 
     print(partition);
 }
--- src/workbench/R#/GCModeller_cli/CLI.vb
+++ src/workbench/R#/GCModeller_cli/CLI.vb
@@ -1,10 +1,17 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
 <Package("GCModeller")>
 Public Module CLI
 
+    <ExportAPI("profiler")>
     Public Function profiler() As GCModellerApps.Profiler
         Return GCModellerApps.Profiler.FromEnvironment(App.HOME)
+    End Function
+
+    <ExportAPI("eggHTS")>
+    Public Function eggHTS() As GCModellerApps.eggHTS
+        Return GCModellerApps.eggHTS.FromEnvironment(App.HOME)
     End Function
 End Module
