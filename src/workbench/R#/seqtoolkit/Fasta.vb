bee36cf34681e74ff4ae15422894edf96b8c59b9
xieguigang <xie.guigang@gcmodeller.org>
2019-11-30 19:57:38 +0800

    sequence logo helper

--- src/workbench/R#/seqtoolkit/Fasta.vb
+++ src/workbench/R#/seqtoolkit/Fasta.vb
@@ -1,6 +1,7 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Microsoft.VisualBasic.Text
+Imports SMRUCC.genomics.Analysis.SequenceTools.MSA
 Imports SMRUCC.genomics.SequenceModel.FASTA
 Imports SMRUCC.Rsharp.Runtime.Internal.ConsolePrinter
 
@@ -12,12 +13,25 @@ Imports SMRUCC.Rsharp.Runtime.Internal.ConsolePrinter
 Module Fasta
 
     Sub New()
-        Call printer.AttachConsoleFormatter(Of FastaSeq)(Function(seq)
-                                                             With DirectCast(seq, FastaSeq)
-                                                                 Return "> " & .Title & ASCII.LF & .SequenceData
-                                                             End With
-                                                         End Function)
+        Call printer.AttachConsoleFormatter(Of FastaSeq)(AddressOf viewFasta)
     End Sub
+
+    Private Function viewFasta(seq As Object) As String
+        If seq Is Nothing Then
+            Return "NULL"
+        End If
+
+        Select Case seq
+            Case GetType(FastaSeq)
+                With DirectCast(seq, FastaSeq)
+                    Return "> " & .Title & ASCII.LF & .SequenceData
+                End With
+            Case GetType(FastaFile)
+                Return DirectCast(seq, FastaFile).Select(Function(fa) "> " & fa.Title).JoinBy(vbCrLf)
+            Case Else
+                Throw New NotImplementedException
+        End Select
+    End Function
 
     ''' <summary>
     ''' Read a single fasta sequence file
@@ -29,5 +43,20 @@ Module Fasta
     <ExportAPI("read.seq")>
     Public Function readSeq(file As String) As FastaSeq
         Return FastaSeq.Load(file)
+    End Function
+
+    <ExportAPI("read.fasta")>
+    Public Function readFasta(file As String) As FastaFile
+        Return FastaFile.Read(file)
+    End Function
+
+    ''' <summary>
+    ''' Do multiple sequence alignment
+    ''' </summary>
+    ''' <param name="seqs"></param>
+    ''' <returns></returns>
+    <ExportAPI("MSA.of")>
+    Public Function MSA(seqs As FastaFile) As FastaFile
+        Return seqs.MultipleAlignment(ScoreMatrix.DefaultMatrix).ToFasta
     End Function
 End Module
---
+++ src/workbench/R#/seqtoolkit/sequenceLogo.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Analysis.SequenceTools.SequencePatterns.SequenceLogo
+Imports SMRUCC.genomics.SequenceModel.FASTA
+
+<Package("bioseq.sequenceLogo")>
+Module sequenceLogo
+
+    <ExportAPI("plot.seqLogo")>
+    Public Function DrawLogo(MSA As FastaFile, Optional title$ = "") As Image
+        Return DrawingDevice.DrawFrequency(MSA, title)
+    End Function
+End Module
