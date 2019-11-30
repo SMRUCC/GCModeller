3db19f0b5e6b57d370f00db39e324da19e0f746e
xieguigang <xie.guigang@gcmodeller.org>
2019-11-30 20:23:40 +0800

    demo cli script for create  sequence logo

---
+++ src/workbench/R#/demo/sequenceLogo.R
@@ -0,0 +1,23 @@
+# Demo script for create sequence logo based on the MSA alignment analysis
+# nt base frequency is created based on the MSA alignment operation.
+
+imports "bioseq.sequenceLogo" from "seqtoolkit.dll";
+imports "bioseq.fasta" from "seqtoolkit.dll";
+
+# script cli usage
+#
+# R# sequenceLogo.R --seq input.fasta --save output.png 
+#
+
+let seq.fasta as string = ?"--seq";
+let logo.png as string  = ?"--save";
+
+if (is.empty(logo.png)) {
+	logo.png <- `${seq.fasta}.logo.png`;
+}
+
+seq.fasta
+  :> read.fasta
+  :> MSA.of
+  :> plot.seqLogo
+  :> save.image( logo.png );
