c14d32d4b1bbcc7f5d5c4edbaa1b4cdc02aa5c32
xieguigang <xie.guigang@gcmodeller.org>
2019-12-01 09:22:32 +0800

    add demo data

--- src/workbench/typescript/Linq.ts/TsBuild/TsBuild/ModuleBuilder/ModuleParser.vb
+++ src/workbench/typescript/Linq.ts/TsBuild/TsBuild/ModuleBuilder/ModuleParser.vb
@@ -1,46 +1,4 @@
-﻿#Region "Microsoft.VisualBasic::d8018e7978b4c1981cf6e194495e9dfc, typescript\Linq.ts\TsBuild\TsBuild\ModuleBuilder\ModuleParser.vb"
-
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-
-
-    ' /********************************************************************************/
-
-    ' Summaries:
-
-    ' Class ModuleParser
-    ' 
-    '     Function: bufferEndWith, bufferEquals, bufferStartWith, getTextCode, ParseIndex
-    '               walkChar
-    ' 
-    ' /********************************************************************************/
-
-#End Region
-
-Imports Microsoft.VisualBasic.Emit.Marshal
+﻿Imports Microsoft.VisualBasic.Emit.Marshal
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Text
 
