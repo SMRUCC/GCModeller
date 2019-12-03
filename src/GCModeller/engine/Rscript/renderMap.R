e1ca0908e6fcec17650c4340cb7aa0b41d0b439a
xieguigang <xie.guigang@gcmodeller.org>
2019-12-03 22:02:40 +0800

    fix type conversion error

--- src/GCModeller/engine/Rscript/renderMap.R
+++ src/GCModeller/engine/Rscript/renderMap.R
@@ -3,7 +3,7 @@ require(dataframe);
 let enrichment <- [?"--enrichment"] 
 :> read.dataframe(mode = "character") 
 :> projectAs(as.object) 
-:> which(map -> map$"P-Value" <= 0.05)
+:> which(map -> as.numeric(map$"P-Value") <= 0.05)
 :> groupBy(map -> map$ID);
 
 if (length(enrichment) == 0) {
