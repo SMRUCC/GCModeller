2f8c07b3ce21d37c763c52215b1a43a52273bea5
xieguigang <xie.guigang@gcmodeller.org>
2019-12-03 20:45:53 +0800

    reander map script helper

---
+++ src/GCModeller/engine/Rscript/renderMap.R
@@ -0,0 +1,14 @@
+require(dataframe);
+
+let enrichment <- [?"--enrichment"] 
+:> read.dataframe(mode = "character") 
+:> projectAs(as.object) 
+:> which(map -> map$"P-Value" <= 0.05)
+:> groupBy(map -> map$ID);
+
+if (length(enrichment) == 0) {
+	return;
+} else {
+	print(`We get ${length(enrichment)} enriched pathway maps!`);
+	print(enrichment);
+}
