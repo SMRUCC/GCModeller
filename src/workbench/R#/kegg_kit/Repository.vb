06e39c2df244495e13ed47a27e66fe9c8a939e71
xieguigang <xie.guigang@gcmodeller.org>
2019-12-03 12:19:03 +0800

    kegg organism tools

--- src/workbench/R#/kegg_kit/Repository.vb
+++ src/workbench/R#/kegg_kit/Repository.vb
@@ -1,4 +1,6 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject.Organism
 Imports SMRUCC.genomics.Assembly.KEGG.WebServices
@@ -19,8 +21,22 @@ Public Module Repository
     <ExportAPI("fetch.kegg_organism")>
     Public Function FetchKEGGOrganism(Optional resource$ = "http://www.kegg.jp/kegg/catalog/org_list.html") As Prokaryote()
         Dim result As KEGGOrganism = EntryAPI.FromResource(resource)
-        Dim table As List(Of Prokaryote) = result.Prokaryote.AsList + result.Eukaryotes.Select(Function(x) New Prokaryote(x))
+        Dim eukaryotes As List(Of Prokaryote) = result.Eukaryotes _
+            .Select(Function(x)
+                        Return New Prokaryote(x)
+                    End Function) _
+            .AsList
 
-        Return table.ToArray
+        Return result.Prokaryote + eukaryotes
+    End Function
+
+    <ExportAPI("save.kegg_organism")>
+    Public Function SaveKEGGOrganism(organism As Prokaryote(), file$) As Boolean
+        Return organism.SaveTo(file)
+    End Function
+
+    <ExportAPI("read.kegg_organism")>
+    Public Function ReadKEGGOrganism(file As String) As Prokaryote()
+        Return file.LoadCsv(Of Prokaryote)
     End Function
 End Module
