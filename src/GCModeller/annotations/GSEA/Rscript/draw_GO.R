b1b822f468f47936e9b9821b5c8e1bc29d2cc242
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-12-08 17:35:17 +0800

    plot bubbles

--- src/GCModeller/annotations/GSEA/Rscript/draw_GO.R
+++ src/GCModeller/annotations/GSEA/Rscript/draw_GO.R
@@ -14,6 +14,29 @@ enrichment
 # /Go.enrichment.plot /in <enrichmentTerm.csv> [/bubble /r "log(x,1.5)" /Corrected /displays <default=10> /PlantRegMap /label.right /label.color.disable /label.maxlen <char_count, default=64> /colors <default=Set1:c6> /gray /pvalue <0.05> /size <2000,1600> /tick 1 /go <go.obo> /out <out.png>]
 eggHTS$GO_enrichmentPlot(
     in = KOBAS.convert,
-    go = go
+    go = go,
+    size = "2200,1900",
+    tick = 5,
+    displays = 20,
+    colors = "Set1:c6",
+    pvalue = 0.05,
+    label_maxlen = 72
 );
 
+let bubbles.out = `{dirname(enrichment)}/{basename(enrichment)}.bubbles.png`;
+
+# bubbles
+# /Go.enrichment.plot /in <enrichmentTerm.csv> [/bubble /r "log(x,1.5)" /Corrected /displays <default=10> /PlantRegMap /label.right /label.color.disable /label.maxlen <char_count, default=64> /colors <default=Set1:c6> /gray /pvalue <0.05> /size <2000,1600> /tick 1 /go <go.obo> /out <out.png>]
+eggHTS$GO_enrichmentPlot(
+    in = KOBAS.convert,
+    go = go,
+    size = "2200,1900",
+    tick = 5,
+    displays = 20,
+    colors = "Set1:c6",
+    pvalue = 0.05,
+    label_maxlen = 72,
+    out = bubbles.out,
+    bubble = TRUE,
+    r = "log(x, 1.25)"
+);
