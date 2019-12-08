8b66edd3a6ab003f9891bcff2ea6e0ce36c81f72
xieguigang <xie.guigang@gcmodeller.org>
2019-12-08 18:44:32 +0800

    update cli sourcefile

--- src/Settings/Shared/InternalApps_CLI/Apps/venn.vb
+++ src/Settings/Shared/InternalApps_CLI/Apps/venn.vb
@@ -11,11 +11,11 @@ Imports Microsoft.VisualBasic.ApplicationServices
 '  // 
 '  // SMRUCC genomics GCModeller Programs Profiles Manager
 '  // 
-'  // VERSION:   3.3277.7278.38403
-'  // ASSEMBLY:  Settings, Version=3.3277.7278.38403, Culture=neutral, PublicKeyToken=null
+'  // VERSION:   3.3277.7281.33691
+'  // ASSEMBLY:  Settings, Version=3.3277.7281.33691, Culture=neutral, PublicKeyToken=null
 '  // COPYRIGHT: Copyright © SMRUCC genomics. 2014
 '  // GUID:      a554d5f5-a2aa-46d6-8bbb-f7df46dbbe27
-'  // BUILT:     12/4/2019 8:55:34 AM
+'  // BUILT:     12/7/2019 6:18:30 AM
 '  // 
 ' 
 ' 
