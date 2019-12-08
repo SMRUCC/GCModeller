5c01777e9f66c61a1d04f18d9162e5f4c7957c6f
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-12-08 16:57:02 +0800

    do draw go enrichment output

--- src/GCModeller/annotations/GSEA/Rscript/draw_GO.R
+++ src/GCModeller/annotations/GSEA/Rscript/draw_GO.R
@@ -4,8 +4,16 @@ imports "GCModeller" from "GCModeller_cli.dll";
 let enrichment as string = ?"--enrichment";
 let go as string = ?"--go";
 let KOBAS.convert as string = `${dirname(enrichment)}/${basename(enrichment)}.KOBAS.csv`;
+let eggHTS = eggHTS() :> as.object;
 
 enrichment
 :> read.enrichment
 :> as.KOBAS_terms
-:> 
+:> write.enrichment(file = KOBAS.convert);
+
+# /Go.enrichment.plot /in <enrichmentTerm.csv> [/bubble /r "log(x,1.5)" /Corrected /displays <default=10> /PlantRegMap /label.right /label.color.disable /label.maxlen <char_count, default=64> /colors <default=Set1:c6> /gray /pvalue <0.05> /size <2000,1600> /tick 1 /go <go.obo> /out <out.png>]
+eggHTS$GO_enrichmentPlot(
+    in = KOBAS.convert,
+    go = go
+);
+
