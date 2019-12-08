c11c1e47c50a085dadcfa8d0c65cbdd73f8de4fe
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-12-08 14:03:58 +0800

    Go kit and GSEA analysis

---
+++ src/GCModeller/annotations/GSEA/Rscript/GSEA.R
@@ -0,0 +1,29 @@
+imports "gseakit.background" from "gseakit.dll";
+imports "GSEA" from "gseakit.dll";
+imports "gokit.file" from "gokit.dll";
+
+let background <- ?"--background" :> read.background;  
+let geneSet as string <- ?"--geneSet";
+let save.csv as string <- ?"--save" || `${dirname(geneSet)}/{basename(geneSet)}.enrichment.csv`;
+let go <- ?"--go" 
+
+geneSet <- readLines(geneSet);
+
+if (is.empty(go)) {
+    print("Do GSEA analysis with background:");
+} else {
+    go <- read.go_obo(go);
+    print("Do GO enrichment analysis with background:");
+}
+
+print(background);
+
+if (is.empty(go)) {
+    background
+    :> enrichment(geneSet)
+    :> write.enrichment(file = save.csv);
+} else {
+    background
+    :> enrichment.go(geneSet, go)
+    :> write.enrichment(file = save.csv);
+}
--- src/workbench/R#/gseakit/GSEA.vb
+++ src/workbench/R#/gseakit/GSEA.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Analysis.HTS.GSEA
 Imports SMRUCC.genomics.Data.GeneOntology
@@ -7,12 +8,22 @@ Imports SMRUCC.genomics.Data.GeneOntology
 Module GSEA
 
     <ExportAPI("enrichment")>
-    Public Function Enrichment(background As Background, geneSet$())
+    Public Function Enrichment(background As Background, geneSet$()) As EnrichmentResult()
 
     End Function
 
     <ExportAPI("enrichment.go")>
-    Public Function GOEnrichment(background As Background, geneSet$(), go As OBO.GO_OBO)
+    Public Function GOEnrichment(background As Background, geneSet$(), go As OBO.GO_OBO) As EnrichmentResult()
 
     End Function
+
+    <ExportAPI("write.enrichment")>
+    Public Function SaveEnrichment(enrichment As EnrichmentResult(), file$, Optional format$ = "GCModeller") As Boolean
+        If format = "GCModeller" Then
+            Return enrichment.SaveTo(file)
+        Else
+            Throw New NotImplementedException(format)
+        End If
+    End Function
 End Module
+
