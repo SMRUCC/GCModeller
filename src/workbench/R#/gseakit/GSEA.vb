1a1e0dff27a2ca0449e7f154b0e241384dedbd73
xieguigang <xie.guigang@gcmodeller.org>
2019-12-08 13:42:48 +0800

    GSEA api

---
+++ src/workbench/R#/gseakit/GSEA.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Analysis.HTS.GSEA
+Imports SMRUCC.genomics.Data.GeneOntology
+
+<Package("GSEA", Category:=APICategories.ResearchTools)>
+Module GSEA
+
+    <ExportAPI("enrichment")>
+    Public Function Enrichment(background As Background, geneSet$())
+
+    End Function
+
+    <ExportAPI("enrichment.go")>
+    Public Function GOEnrichment(background As Background, geneSet$(), go As OBO.GO_OBO)
+
+    End Function
+End Module
