c12999bd5246d2bbd37dd3fee9cb53476cea7bed
xieguigang <xie.guigang@gcmodeller.org>
2019-12-11 22:10:51 +0800

    sequence alignment helper

--- src/GCModeller/annotations/GSEA/Rscript/GSEA.R
+++ src/GCModeller/annotations/GSEA/Rscript/GSEA.R
@@ -2,6 +2,9 @@ imports "gseakit.background" from "gseakit.dll";
 imports "GSEA" from "gseakit.dll";
 imports "gokit.file" from "gokit.dll";
 
+# Get input value from commandlines arguments
+# background model
+# and target geneSet for GSEA analysis
 let background <- ?"--background" :> read.background;  
 let geneSet as string <- ?"--geneSet";
 let save.csv as string <- ?"--save" || `${dirname(geneSet)}/{basename(geneSet)}.enrichment.csv`;
@@ -18,6 +21,7 @@ if (is.empty(go)) {
 
 print(background);
 
+# do GSEA analysis
 if (is.empty(go)) {
     background
     :> enrichment(geneSet)
---
+++ src/workbench/R#/seqtoolkit/Blast.vb
@@ -0,0 +1,24 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.TagData
+Imports Microsoft.VisualBasic.DataMining.DynamicProgramming.NeedlemanWunsch
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Analysis.SequenceTools
+Imports SMRUCC.genomics.SequenceModel.FASTA
+
+<Package("bioseq.blast")>
+Module Blast
+
+    <ExportAPI("align.smith_waterman")>
+    Public Function doAlign(query As FastaSeq, ref As FastaSeq, Optional blosum As Blosum = Nothing) As SmithWaterman
+        Return SmithWaterman.Align(query, ref, blosum)
+    End Function
+
+    <ExportAPI("align.needleman_wunsch")>
+    Public Function RunGlobalNeedlemanWunsch(query As FastaSeq, ref As FastaSeq) As FactorValue(Of Double, GlobalAlign(Of Char)())
+        Dim score As Double = 0
+        Dim alignments = RunNeedlemanWunsch.RunAlign(query, ref, score).ToArray
+
+        Return (score, alignments)
+    End Function
+End Module
