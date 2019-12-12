2650a58f49d87b58b57fe822996c391bbc160c15
xieguigang <xie.guigang@gcmodeller.org>
2019-12-12 09:17:10 +0800

    fix namespace reference errors

--- src/GCModeller/core/Bio.Assembly/Assembly/ELIXIR/EBI/ChEBI/EntityModel/EntitySearch.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/ELIXIR/EBI/ChEBI/EntityModel/EntitySearch.vb
@@ -1,46 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::f88c90700134047be0eb2c14ebae263a, core\Bio.Assembly\Assembly\ELIXIR\EBI\ChEBI\EntityModel\EntitySearch.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module EntitySearch
-    ' 
-    '         Function: EnumerateNames, NameEqualsAny, SearchByNameAndFormula, SearchByNameEqualsAny
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module EntitySearch
+' 
+'         Function: EnumerateNames, NameEqualsAny, SearchByNameAndFormula, SearchByNameEqualsAny
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm.DynamicProgramming.Levenshtein
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
@@ -86,8 +87,8 @@ Namespace Assembly.ELIXIR.EBI.ChEBI
                              .ToArray
                          Where Not matches.IsNullOrEmpty
                          Select New NamedCollection(Of DistResult) With {
-                             .Name = metabolite.chebiId.Split(":"c).Last,
-                             .Value = matches
+                             .name = metabolite.chebiId.Split(":"c).Last,
+                             .value = matches
                          }
 
             Return LQuery
