1d8232668afae937254a1e2cb069472e414e2ab3
xieguigang <xie.guigang@gcmodeller.org>
2019-12-18 17:13:47 +0800

    NamespaceIgnorantXmlTextReader

---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Text/Xml/Linq/NamespaceIgnorantXmlTextReader.vb
@@ -0,0 +1,22 @@
+﻿Imports System.IO
+Imports System.Xml
+
+Namespace Text.Xml.Linq
+
+    ''' <summary>
+    ''' https://stackoverflow.com/questions/12590487/net-xml-deserialization-ignore-namespaces
+    ''' </summary>
+    Friend Class NamespaceIgnorantXmlTextReader
+        Inherits XmlTextReader
+
+        Public Overrides ReadOnly Property NamespaceURI As String
+            Get
+                Return ""
+            End Get
+        End Property
+
+        Public Sub New(stream As Stream)
+            Call MyBase.New(stream)
+        End Sub
+    End Class
+End Namespace
