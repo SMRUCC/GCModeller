ecf0d56fbf5b5d787b95b4b07a4cf6cd2f17ebe5
xieguigang <xie.guigang@gcmodeller.org>
2019-12-25 15:15:16 +0800

    kegg category profiles plot

---
+++ src/workbench/R#/visualkit/visualPlot.vb
@@ -0,0 +1,13 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("visualkit.plots")>
+Module visualPlot
+
+    <ExportAPI("kegg.category_profiles.plot")>
+    Public Function KEGGCategoryProfilePlots(profiles As Dictionary(Of String, Double)) As GraphicsData
+
+    End Function
+End Module
