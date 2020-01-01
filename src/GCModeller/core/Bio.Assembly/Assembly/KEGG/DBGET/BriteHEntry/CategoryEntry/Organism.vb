b7b517fa62c6005b8e95abe1fe5b0fad444beb8d
xieguigang <xie.guigang@gcmodeller.org>
2020-01-01 15:10:09 +0800

    fix method reference errors

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/BriteHEntry/CategoryEntry/Organism.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/BriteHEntry/CategoryEntry/Organism.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::091d94d9a57ba876f6d3d1721ca057ad, core\Bio.Assembly\Assembly\KEGG\DBGET\BriteHEntry\CategoryEntry\Organism.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module Organism
-    ' 
-    '         Function: (+2 Overloads) FillTaxonomyTable, GetBacteriaList, GetResource, IsSpeciesLevel
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module Organism
+' 
+'         Function: (+2 Overloads) FillTaxonomyTable, GetBacteriaList, GetResource, IsSpeciesLevel
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -73,11 +73,11 @@ Namespace Assembly.KEGG.DBGET.BriteHEntry
         Public Function GetBacteriaList(resource As htext) As Taxonomy()
             Dim bacteriaAll As BriteHText = resource _
                 .Hierarchical _
-                .CategoryItems _
-                .Select("Prokaryotes\s+\(\d+\)") _
+                .categoryItems _
+                .Takes("Prokaryotes\s+\(\d+\)") _
                 .First _
-                .CategoryItems _
-                .Select("Bacteria\s+\(\d+\)") _
+                .categoryItems _
+                .Takes("Bacteria\s+\(\d+\)") _
                 .First
             Dim table = bacteriaAll _
                 .EnumerateEntries _
@@ -98,11 +98,11 @@ Namespace Assembly.KEGG.DBGET.BriteHEntry
                 hc = code
 
                 Do While hc.CategoryLevel <> "/"c
-                    hc = hc.Parent
-                    levels(hc.CategoryLevel) = hc.ClassLabel
+                    hc = hc.parent
+                    levels(hc.CategoryLevel) = hc.classLabel
                 Loop
 
-                name = code.ClassLabel
+                name = code.classLabel
                 sp = name
                 sp = sp.Split.First
                 name = Mid(name, sp.Length + 1).Trim
