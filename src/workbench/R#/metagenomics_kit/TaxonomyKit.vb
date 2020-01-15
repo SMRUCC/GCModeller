b47e57913afe7ee3ad2836319133b814ba0c2a68
xieguigang <xie.guigang@gcmodeller.org>
2020-01-15 11:34:24 +0800

    metagenomics toolkit

--- src/workbench/R#/metagenomics_kit/TaxonomyKit.vb
+++ src/workbench/R#/metagenomics_kit/TaxonomyKit.vb
@@ -1,10 +1,12 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Assembly.NCBI.Taxonomy
 
 <Package("taxonomy_kit")>
 Module TaxonomyKit
 
+    <ExportAPI("Ncbi.taxonomy_tree")>
     Public Function LoadNcbiTaxonomyTree(repo$) As NcbiTaxonomyTree
         Return New NcbiTaxonomyTree(repo)
     End Function
--- src/workbench/R#/metagenomics_kit/metagenomicsKit.vb
+++ src/workbench/R#/metagenomics_kit/metagenomicsKit.vb
@@ -1,10 +1,14 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Assembly.NCBI.Taxonomy
+Imports SMRUCC.genomics.Model.Network.Microbiome
 
 <Package("metagenomics_kit")>
 Module metagenomicsKit
 
-    Public Function createEmptyCompoundOriginProfile()
-
+    <ExportAPI("compounds.origin.profile")>
+    Public Function createEmptyCompoundOriginProfile(taxonomy As NcbiTaxonomyTree, organism As String) As CompoundOrigins
+        Return CompoundOrigins.CreateEmptyCompoundsProfile(taxonomy, organism)
     End Function
 End Module
