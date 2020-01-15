b40c69b09f988af7b197b6b70cb88d8157e57bec
xieguigang <xie.guigang@gcmodeller.org>
2020-01-15 14:19:30 +0800

    filter invalid data

--- src/workbench/R#/demo/kegg/compoundOrigin_profiles.R
+++ src/workbench/R#/demo/kegg/compoundOrigin_profiles.R
@@ -1,10 +1,13 @@
 imports ["taxonomy_kit", "metagenomics_kit"] from "metagenomics_kit.dll";
 
+let dirs as string <- list.dirs("S:\biodeepdb\kegg\test", fullNames = TRUE, recursive = FALSE);
 let taxonomy.tree <- Ncbi.taxonomy_tree("S:\synthetic_biology\taxdmp");
 # let test_org as string = "S:\biodeepdb\kegg\enzymes\xcb";
 # let empty <- taxonomy.tree :> compounds.origin.profile(test_org);
 
-# print(json(empty)); 
+# print(json(empty));
+
+print(basename(dirs));
 
 let createProfiles as function(org) {
 	taxonomy.tree :> compounds.origin.profile(org);
@@ -13,12 +16,15 @@ let createProfiles as function(org) {
 let orgs <- list();
 let org;
 
-for(org.dir in list.dirs("S:\biodeepdb\kegg\enzymes")) {
+# "S:\biodeepdb\kegg\enzymes"
+for(org.dir in dirs :> which( d -> file.exists(`${d}/kegg_compounds.txt`) )) {
 	org <- createProfiles(org.dir);
 	orgs[[basename(org.dir)]] <- org;
 	
 	print(as.object(org)$taxonomy);
 }
 
-
+orgs 
+:> unlist(valueof compounds.origin.profile) 
+:> write.csv(file = "S:\biodeepdb\kegg\compounds.origins.csv");
 
