53615feef9c34992c5b9c86fe5fe561fb8e82de2
xieguigang <xie.guigang@gcmodeller.org>
2020-01-21 10:11:36 +0800

    improvements of the script for get id list data source

--- src/workbench/R#/demo/cytoscape/from_kegg_compounds.R
+++ src/workbench/R#/demo/cytoscape/from_kegg_compounds.R
@@ -7,9 +7,15 @@ let br08201 as string = ?"--br08201" || stop("No network connection data provide
 let id.list as string = ?"--id"      || stop("No node data provided!"); 
 let exports as string = ?"--save"    || `${dirname(id.list)}/${basename(id.list)}.network/`;
 
+if (lcase((file.info(id.list))["Extension"]) == ".json") {
+	id.list <- read.list(id.list, mode = "character", ofVector = TRUE) :> unlist(typeof = "string");
+} else {
+	id.list <- readLines(id.list);
+}
+
 br08201
 :> reactions.table()
-:> compounds.network( readLines(id.list) )
+:> compounds.network( id.list )
 :> save.network(file = exports, properties = ["common_name", "related"])
 ;
 
