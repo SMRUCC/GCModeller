352543bdd291c208b7e9817815fad488265d99ee
xieguigang <xie.guigang@gcmodeller.org>
2020-02-13 13:49:32 +0800

    improvements of the cdf data model

---
+++ src/GCModeller/models/BIOM/BIOM/v1.5.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic.Data.IO.netCDF
+Imports Microsoft.VisualBasic.Data.IO.netCDF.Components
+
+Namespace v15
+
+    ''' <summary>
+    ''' BIOM netCDF I/O
+    ''' </summary>
+    Public Module CDF
+
+        Public Function ReadFile(biom As String) As v10.BIOMDataSet(Of Double)
+            Using cdf As New netCDFReader(biom)
+                Dim attributes = cdf.globalAttributes.ToDictionary(Function(a) a.name, Function(a As attribute)
+                                                                                           Return a.
+                                                                                       End Function)
+            End Using
+        End Function
+    End Module
+
+End Namespace
+
