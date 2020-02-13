b6715afd6c5e16fa77c1319654ea20692dd8c24f
xieguigang <xie.guigang@gcmodeller.org>
2020-02-13 17:29:41 +0800

    v1.5 cdf format version

--- src/GCModeller/models/BIOM/BIOM/v1.5.vb
+++ src/GCModeller/models/BIOM/BIOM/v1.5.vb
@@ -1,5 +1,6 @@
 ﻿Imports Microsoft.VisualBasic.Data.IO.netCDF
 Imports Microsoft.VisualBasic.Data.IO.netCDF.Components
+Imports Microsoft.VisualBasic.Scripting.Runtime
 
 Namespace v15
 
@@ -10,9 +11,12 @@ Namespace v15
 
         Public Function ReadFile(biom As String) As v10.BIOMDataSet(Of Double)
             Using cdf As New netCDFReader(biom)
-                Dim attributes = cdf.globalAttributes.ToDictionary(Function(a) a.name, Function(a As attribute)
-                                                                                           Return a.
-                                                                                       End Function)
+                Dim attributes = cdf.globalAttributes _
+                    .ToDictionary(Function(a) a.name,
+                                  Function(a As attribute)
+                                      Return a.getObjectValue
+                                  End Function) _
+                    .AsCharacter
             End Using
         End Function
     End Module
