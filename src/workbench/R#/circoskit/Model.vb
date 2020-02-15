f5954f1c8681e32bdf64fd82c3d55786b7b4d48b
xieguigang <xie.guigang@gcmodeller.org>
2020-02-15 17:31:44 +0800

    improvements of the circos api

--- src/workbench/R#/circoskit/Model.vb
+++ src/workbench/R#/circoskit/Model.vb
@@ -1,7 +1,22 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Visualize.Circos
+Imports SMRUCC.genomics.Visualize.Circos.Configurations
 
 <Package("circos.model", Category:=APICategories.UtilityTools)>
 Module Model
 
+    ''' <summary>
+    ''' Invoke set the radius value of the ideogram circle.
+    ''' </summary>
+    ''' <param name="circos"></param>
+    ''' <param name="r"></param>
+    ''' <returns></returns>
+    <ExportAPI("Set.Ideogram.Radius")>
+    Public Function SetIdeogramRadius(circos As Circos, r As Double) As Circos
+        Dim idg As Ideogram = circos.GetIdeogram
+        Call CircosAPI.SetIdeogramRadius(idg, r)
+        Return circos
+    End Function
 End Module
