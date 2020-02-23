319fe9fd367710eb77c61e587e584e40f97906e2
xieguigang <xie.guigang@gcmodeller.org>
2020-02-24 03:21:10 +0800

    evaluate correlation

--- src/workbench/R#/proteomics_toolkit/labelFree.vb
+++ src/workbench/R#/proteomics_toolkit/labelFree.vb
@@ -3,6 +3,7 @@ Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Analysis.HTS.Proteomics.FoldChangeMatrix
+Imports Microsoft.VisualBasic.Math
 
 <Package("proteomics.labelfree")>
 Module labelFree
@@ -10,5 +11,20 @@ Module labelFree
     <ExportAPI("sample.normalize")>
     Public Function totalSumNormalize(data As DataSet(), Optional byMedianQuantile As Boolean = False, Optional samples As String() = Nothing) As DataSet()
         Return data.TotalSumNormalize(byMedianQuantile, samples).ToArray
+    End Function
+
+    <ExportAPI("sample.normalize.correlation")>
+    Public Function normalizationCorrelation(data As DataSet(), Optional samples As String() = Nothing) As Dictionary(Of String, Double)
+        Dim totalSum = data.TotalSumNormalize(byMedianQuantile:=False, samples:=samples).ToArray
+        Dim medianNor = data.TotalSumNormalize(byMedianQuantile:=True, samples:=samples).ToArray
+        Dim cor As Dictionary(Of String, Double) = totalSum.PropertyNames _
+            .ToDictionary(Function(name) name,
+                          Function(name)
+                              Dim ts As Double() = totalSum.Vector(name)
+                              Dim md As Double() = totalSum.Vector(name)
+
+                              Return Correlations.GetPearson(ts, md)
+                          End Function)
+        Return cor
     End Function
 End Module
