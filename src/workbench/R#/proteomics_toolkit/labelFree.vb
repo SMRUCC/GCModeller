05f61f6f6959ed864fc5ddb9925fc542391a1928
xieguigang <xie.guigang@gcmodeller.org>
2020-02-24 03:33:00 +0800

    sample.normalize.correlation

--- src/workbench/R#/proteomics_toolkit/labelFree.vb
+++ src/workbench/R#/proteomics_toolkit/labelFree.vb
@@ -1,9 +1,10 @@
 ﻿
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Math
+Imports Microsoft.VisualBasic.Math.Quantile
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Analysis.HTS.Proteomics.FoldChangeMatrix
-Imports Microsoft.VisualBasic.Math
 
 <Package("proteomics.labelfree")>
 Module labelFree
@@ -14,17 +15,50 @@ Module labelFree
     End Function
 
     <ExportAPI("sample.normalize.correlation")>
-    Public Function normalizationCorrelation(data As DataSet(), Optional samples As String() = Nothing) As Dictionary(Of String, Double)
-        Dim totalSum = data.TotalSumNormalize(byMedianQuantile:=False, samples:=samples).ToArray
-        Dim medianNor = data.TotalSumNormalize(byMedianQuantile:=True, samples:=samples).ToArray
-        Dim cor As Dictionary(Of String, Double) = totalSum.PropertyNames _
+    Public Function normalizationCorrelation(data As DataSet(), Optional samples As String() = Nothing) As DataSet()
+        Dim totalSum = data.TotalSumNormalize(byMedianQuantile:=False, samples:=samples).ToDictionary(Function(r) r.ID)
+        Dim medianNor = data.TotalSumNormalize(byMedianQuantile:=True, samples:=samples).ToDictionary(Function(r) r.ID)
+        Dim rownames As String() = totalSum.Keys.ToArray
+        Dim sampleNames As String() = totalSum.PropertyNames
+        Dim cor As Dictionary(Of String, Double) = sampleNames _
             .ToDictionary(Function(name) name,
                           Function(name)
-                              Dim ts As Double() = totalSum.Vector(name)
-                              Dim md As Double() = totalSum.Vector(name)
+                              Dim ts As Double() = rownames.Select(Function(id) totalSum(id)(name)).ToArray
+                              Dim md As Double() = rownames.Select(Function(id) medianNor(id)(name)).ToArray
 
                               Return Correlations.GetPearson(ts, md)
                           End Function)
-        Return cor
+        Dim output As New List(Of DataSet)
+        Dim aggregate As New DataSet With {.ID = "*Aggregate"}
+        Dim vector As Double()
+
+        For Each sample As String In sampleNames
+            vector = data.Vector(sample)
+            aggregate.Add($"{sample}.sum", vector.Sum)
+            aggregate.Add($"{sample}.median", vector.Quartile.Q2)
+        Next
+
+        output.Add(aggregate)
+        aggregate = New DataSet With {.ID = "*Correlation"}
+
+        For Each sample As String In sampleNames
+            aggregate.Add($"{sample}.sum", cor(sample))
+            aggregate.Add($"{sample}.median", cor(sample))
+        Next
+
+        output.Add(aggregate)
+
+        For Each id As String In rownames
+            aggregate = New DataSet With {.ID = id}
+
+            For Each sample As String In sampleNames
+                aggregate.Add($"{sample}.sum", totalSum(id)(sample))
+                aggregate.Add($"{sample}.median", medianNor(id)(sample))
+            Next
+
+            output.Add(aggregate)
+        Next
+
+        Return output.ToArray
     End Function
 End Module
