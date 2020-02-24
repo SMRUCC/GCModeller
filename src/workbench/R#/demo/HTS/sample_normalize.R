ce91fd15f953e09ecc41359172a9f4038ad6bb92
xieguigang <xie.guigang@gcmodeller.org>
2020-02-24 09:17:28 +0800

    impute for missing sample value data

--- src/workbench/R#/demo/HTS/sample_normalize.R
+++ src/workbench/R#/demo/HTS/sample_normalize.R
@@ -4,6 +4,7 @@ require(dataframe);
 
 ["\\192.168.1.239\linux\project\HT201702152001苏大附一国风\原始数据\proteinGroups.csv"] 
 :> read.dataframe(mode = "numeric")
+:> impute(byRow = TRUE, infer = "Min")
 :> sample.normalize.correlation
 :> write.csv(file = "\\192.168.1.239\linux\project\HT201702152001苏大附一国风\原始数据\proteinGroups.cor.csv")
 ;
