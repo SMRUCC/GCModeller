c456ddf2fd8baf92f689717e0208c3581e2e6f76
xieguigang <xie.guigang@gcmodeller.org>
2020-02-25 09:22:09 +0800

    correlation by sample group

--- src/workbench/R#/proteomics_toolkit/labelFree.vb
+++ src/workbench/R#/proteomics_toolkit/labelFree.vb
@@ -1,5 +1,7 @@
 ﻿
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.Quantile
@@ -15,15 +17,30 @@ Module labelFree
         Return data.TotalSumNormalize(byMedianQuantile, samples).ToArray
     End Function
 
+    <Extension>
+    Private Function aggregateByGroupLabels(data As IEnumerable(Of DataSet), groups As NamedCollection(Of String)()) As Dictionary(Of String, DataSet)
+        Return data.ToDictionary(Function(r) r.ID,
+                                 Function(r)
+                                     Return New DataSet With {
+                                        .ID = r.ID,
+                                        .Properties = groups _
+                                            .ToDictionary(Function(group) group.name,
+                                                          Function(group)
+                                                              Return r(group).Average
+                                                          End Function)
+                                     }
+                                 End Function)
+    End Function
+
     <ExportAPI("sample.normalize.correlation")>
     Public Function normalizationCorrelation(data As DataSet(), Optional samples As String() = Nothing) As DataSet()
-        Dim totalSum = data.TotalSumNormalize(byMedianQuantile:=False, samples:=samples)
-        Dim medianNor = data.TotalSumNormalize(byMedianQuantile:=True, samples:=samples)
-        Dim rownames As String() = totalSum.Keys.ToArray
-        Dim sampleNames As String() = totalSum.PropertyNames
+        Dim rownames As String() = data.Keys.ToArray
+        Dim sampleNames As String() = data.PropertyNames
         Dim groups = sampleNames.GuessPossibleGroups.ToArray
-        Dim groupsSum = totalSum
-        Dim cor As Dictionary(Of String, (pcc#, pvalue1#, pvalue2#)) = sampleNames _
+        Dim totalSum = data.TotalSumNormalize(byMedianQuantile:=False, samples:=samples).aggregateByGroupLabels(groups)
+        Dim medianNor = data.TotalSumNormalize(byMedianQuantile:=True, samples:=samples).aggregateByGroupLabels(groups)
+        Dim cor As Dictionary(Of String, (pcc#, pvalue1#, pvalue2#)) = groups _
+            .Keys _
             .ToDictionary(Function(name) name,
                           Function(name)
                               Dim ts As Double() = rownames.Select(Function(id) totalSum(id)(name)).ToArray
