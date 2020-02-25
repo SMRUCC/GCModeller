cf8876c1f2952499ab3770bf9750fb03f0c694b4
xieguigang <xie.guigang@gcmodeller.org>
2020-02-25 08:50:30 +0800

    SampleNames helper

---
+++ src/workbench/modules/ExperimentDesigner/SampleNames.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+''' <summary>
+''' Sample names helper
+''' </summary>
+Public Module SampleNames
+
+    ''' <summary>
+    ''' Guess all possible sample groups from the given name string collection.
+    ''' </summary>
+    ''' <param name="allSampleNames"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Iterator Function GuessPossibleGroups(allSampleNames As IEnumerable(Of String)) As IEnumerable(Of NamedCollection(Of String))
+
+    End Function
+End Module
