acd3d8120bdf69013643993ae9d211b15424ea7b
xieguigang <xie.guigang@gcmodeller.org>
2020-02-26 17:48:49 +0800

    Create sample_normalization.R
    run sample.normalize test

---
+++ src/workbench/R#/demo/HTS/sample_normalization.R
@@ -0,0 +1,10 @@
+imports ["proteomics.labelfree"] from "proteomics_toolkit.dll";
+
+require(dataframe);
+
+["\\192.168.1.239\linux\project\HT201702152001苏大附一国风\原始数据\proteinGroups.csv"] 
+:> read.dataframe(mode = "numeric")
+:> impute(byRow = TRUE, infer = "Average")
+:> sample.normalize()
+:> write.csv(file = "\\192.168.1.239\linux\project\HT201702152001苏大附一国风\原始数据\proteinGroups_norm.csv")
+;
