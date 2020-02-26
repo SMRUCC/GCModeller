885247be8600ae35a799b8a3fdc5af8640a0ee93
xieguigang <xie.guigang@gcmodeller.org>
2020-02-26 16:23:55 +0800

    Histogram Plot

--- src/workbench/R#/demo/HTS/RSD.barplot.R
+++ src/workbench/R#/demo/HTS/RSD.barplot.R
@@ -9,12 +9,12 @@ let raw = ["\\192.168.1.239\linux\project\HT201702152001苏大附一国风\原�
 ;
 
 let group_names as string = raw :> dataset.colnames;
-let data as double;
+let data = NULL;
 
 print("The given data contains data groups:");
 print(group_names);
 
 for(name in group_names) {
-    data <- raw :> dataset.vector(name);
+    data <- raw :> dataset.vector(name) :> hist(step = 0.1);
 
 }
