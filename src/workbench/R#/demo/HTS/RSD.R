dba0f35e60d0609c0aede29078ded3fe870c2d19
xieguigang <xie.guigang@gcmodeller.org>
2020-02-26 16:00:27 +0800

    calculate RSD values of the raw sample data

--- src/workbench/R#/demo/HTS/RSD.R
+++ src/workbench/R#/demo/HTS/RSD.R
@@ -6,9 +6,27 @@ require(dataframe);
 let raw = ["\\192.168.1.239\linux\project\HT201702152001苏大附一国风\原始数据\proteinGroups.csv"] 
 :> read.dataframe(mode = "numeric")
 ;
-let sample.names = names(raw[1]) :> groups;
+let sample.names = raw
+:> dataset.colnames
+:> guess.sample_groups
+;
 
 print("View of the sample group information:");
 str(sample.names);
 
-for
+let samples.RSD = lapply(sample.names, function(group) {
+	let RSD.vector = raw 
+	  :> dataset.project(group) 
+	  :> lapply(function(protein) {
+	       protein :> cells :> RSD;
+	  }, names = protein -> as.object(protein)$ID);
+	
+	RSD.vector;
+});
+
+str(samples.RSD);
+
+data.frame(samples.RSD)
+:> write.csv(file = "./test.csv")
+;
+
