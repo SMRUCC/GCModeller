dd5d8c1ec0f830810bb9ad6dd8dae46984fd6ec8
xieguigang <xie.guigang@gcmodeller.org>
2020-02-27 09:21:43 +0800

    write data

--- src/workbench/R#/demo/HTS/sample_normalization.R
+++ src/workbench/R#/demo/HTS/sample_normalization.R
@@ -16,6 +16,7 @@ let cor <- lapply(dataset.colnames(raw), function(col) {
 });
 
 norm :> write.csv(file = "\\192.168.1.239\linux\project\HT201702152001苏大附一国风\原始数据\proteinGroups_norm.csv");
+raw :> write.csv(file = "\\192.168.1.239\linux\project\HT201702152001苏大附一国风\原始数据\proteinGroups_impute_raw.csv");
 
 str(cor);
 
