d8c71fb8e7f8020c64c8d65ca4ec46f8289b3535
xieguigang <xie.guigang@gcmodeller.org>
2020-02-28 19:07:48 +0800

    create network

--- src/workbench/R#/demo/cytoscape/cor/proteomics_metabolism.R
+++ src/workbench/R#/demo/cytoscape/cor/proteomics_metabolism.R
@@ -1,15 +1,20 @@
 imports "cytoscape.kegg" from "cytoscape_toolkit.dll";
+imports "kegg.repository" from "kegg_kit.dll";
 
 require(dataframe);
+require(igraph);
 
 setwd(!script$dir);
 
-let proteins = read.csv("geneList.csv");
+let proteins            = read.csv("geneList.csv");
 let compounds as string = readLines("compounds.txt");
+let br08201 as string   = ["D:\biodeep\biodeepdb_v3\KEGG\br08201"];
+let exports as string   = "network/";
 
 print("We have kegg compounds list:");
 print(compounds);
-# print(head(proteins));
+print(colnames(proteins));
+print(`There are ${nrow(proteins)} enzymes in your data.`);
 
 let enzymeList = proteins[, "KO"] :> sapply(id -> strsplit(id, ";")[1]) :> as.character;
 let geneId = proteins[, "ID"];
@@ -24,3 +29,9 @@ enzymeList <- lapply(1:length(enzymeList), function(i) {
 
 print("And the enzymes that in gene expression:");
 str(enzymeList);
+
+br08201
+:> reactions.table()
+:> compounds.network( compounds , enzymes = enzymeList)
+:> save.network(file = exports, properties = ["common_name", "related"])
+;
