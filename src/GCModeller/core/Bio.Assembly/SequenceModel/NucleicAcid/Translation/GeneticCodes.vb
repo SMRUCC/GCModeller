86addd34e3dcbbe8ca4318e0fa07965ae5b3552b
xieguigang <xie.guigang@gcmodeller.org>
2020-03-02 21:16:34 +0800

    add codes

---
+++ src/GCModeller/core/Bio.Assembly/SequenceModel/NucleicAcid/Translation/GeneticCodes.vb
@@ -0,0 +1,26 @@
+﻿Namespace SequenceModel.NucleotideModels.Translation
+
+    ''' <summary>
+    ''' http://www.ncbi.nlm.nih.gov/Taxonomy/taxonomyhome.html/index.cgi?chapter=tgencodes#SG25
+    ''' </summary>
+    Public Enum GeneticCodes As Integer
+        StandardCode = 1
+        VertebrateMitochondrialCode = 2
+        YeastMitochondrialCode = 3
+        MoldProtozoanAndCoelenterateMitochondrialCodeAndMycoplasmaSpiroplasmaCode = 4
+        InvertebrateMitochondrialCode = 5
+        CiliateDasycladaceanAndHexamitaNuclearCode = 6
+        EchinodermAndFlatwormMitochondrialCode = 9
+        EuplotidNuclearCode = 10
+        BacterialArchaealAndPlantPlastidCode = 11
+        AlternativeYeastNuclearCode = 12
+        AscidianMitochondrialCode = 13
+        AlternativeFlatwormMitochondrialCode = 14
+        ChlorophyceanMitochondrialCode = 16
+        TrematodeMitochondrialCode = 21
+        ScenedesmusObliquusMitochondrialCode = 22
+        ThraustochytriumMitochondrialCode = 23
+        PterobranchiaMitochondrialCode = 24
+        CandidateDivisionSR1AndGracilibacteriaCode = 25
+    End Enum
+End Namespace
