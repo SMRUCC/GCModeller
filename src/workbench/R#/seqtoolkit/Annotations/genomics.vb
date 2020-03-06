2dbb67871ced9631a42190abbb6244507da997d0
xieguigang <xie.guigang@gcmodeller.org>
2020-03-06 21:39:58 +0800

    genomics annotation helper tools

---
+++ src/workbench/R#/seqtoolkit/Annotations/genomics.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank.TabularFormat
+Imports SMRUCC.genomics.ContextModel
+
+<Package("annotation.genomics", Category:=APICategories.ResearchTools, Publisher:="xie.guigang@gcmodeller.org")>
+Module genomics
+
+    <ExportAPI("read.gtf")>
+    Public Function readGtf(file As String) As PTT
+        Return Gtf.ParseFile(file)
+    End Function
+
+End Module
