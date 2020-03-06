1f0c547f6dde784bc602697073a3d4f0055aa521
xieguigang <xie.guigang@gcmodeller.org>
2020-03-06 23:00:26 +0800

    fix symbol name conflicts

--- src/workbench/R#/demo/regprecise/transcriptBindingRegion.R
+++ src/workbench/R#/demo/regprecise/transcriptBindingRegion.R
@@ -8,11 +8,11 @@ let genes = ["K:\20200226\IGV_data\genome.gtf"]
 let region = genes :> upstream(length = 250);
 let geneId = genes :> sapply(gene -> as.object(gene)$Synonym);
 let nt = ["K:\20200226\IGV_data\genome.fasta"] :> read.seq;
-let fasta = allocate(length(geneId));
+let region.fasta = allocate(length(geneId));
 
 for(i in 1:length(geneId)) {
-	fasta[i] <- nt :> cut_seq.linear(loci = region[i]);
-	as.object(fasta[i])$Headers = geneId;
+	region.fasta[i] <- nt :> cut_seq.linear(loci = region[i]);
+	as.object(region.fasta[i])$Headers = geneId;
 }
 
-fasta :> write.fasta(file = "K:\20200226\TRN\genomics\nt.fasta");
+region.fasta :> write.fasta(file = "K:\20200226\TRN\genomics\nt.fasta");
