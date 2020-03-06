301156a8a1a606899d54f8defd1cc50134e565e1
xieguigang <xie.guigang@gcmodeller.org>
2020-03-06 19:35:01 +0800

    TRN.builder

---
+++ src/workbench/R#/phenotype_kit/TRNBuilder.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Data.Regprecise
+
+<Package("TRN.builder")>
+Module TRNBuilder
+
+    <ExportAPI("read.regprecise")>
+    Public Function readRegPrecise(file As String) As TranscriptionFactors
+        Return file.LoadXml(Of TranscriptionFactors)
+    End Function
+End Module
