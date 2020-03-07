5e1e2742eac335a5ece80a7fcf5b4752849c3507
xieguigang <xie.guigang@gcmodeller.org>
2020-03-08 07:54:51 +0800

    do search of the TFBS sites

--- src/workbench/R#/demo/regprecise/search_sites.R
+++ src/workbench/R#/demo/regprecise/search_sites.R
@@ -2,9 +2,22 @@ imports ["bioseq.fasta", "bioseq.patterns"] from "seqtoolkit.dll";
 
 setwd(!script$dir);
 
-["PWM\AraC.json"]
-:> read.motifs
-:> motif.find_sites(target = read.fasta("K:\20200226\TRN\motifs\AraC.fasta")[1])
-:> as.fasta
-:> write.fasta("./sites.fasta")
-;
+print("Processing:");
+
+for(family.json in list.files("PWM", pattern = "*.json")) {
+	family.json 
+	:> read.motifs
+	:> motif.find_sites(target = read.fasta("K:\20200226\TRN\genomics\nt.fasta"))
+	:> as.fasta
+	:> write.fasta(`K:\20200226\TRN\genomics\search\TFBS\${basename(family.json)}.fasta`)
+	;
+	
+	print(basename(family.json));
+}
+
+# ["PWM\AraC.json"]
+# :> read.motifs
+# :> motif.find_sites(target = read.fasta("K:\20200226\TRN\motifs\AraC.fasta")[1])
+# :> as.fasta
+# :> write.fasta("./sites.fasta")
+# ;
