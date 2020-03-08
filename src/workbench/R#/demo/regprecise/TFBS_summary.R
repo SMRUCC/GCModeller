6a85e0a86bb457e3a8940b4bb9ec637569455798
xieguigang <xie.guigang@gcmodeller.org>
2020-03-08 09:44:01 +0800

    export TFBS site summary

---
+++ src/workbench/R#/demo/regprecise/TFBS_summary.R
@@ -0,0 +1,10 @@
+imports "bioseq.fasta" from "seqtoolkit.dll";
+
+list.files("K:\20200226\TRN\genomics\search\TFBS", pattern = "*.fasta")
+:> lapply(function(path) {
+	read.fasta(path) 
+	:> as.vector 
+	:> sapply(fa -> as.object(fa)$locus_tag) 
+	:> unique;
+}, names = path -> basename(path))
+:> save.list(file = "K:\20200226\TRN\genomics\search\TFBS.json");
