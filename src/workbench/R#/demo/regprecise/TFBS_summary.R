b66000bba6d8b3926b353aa3aa4a7f7bb6926ce5
xieguigang <xie.guigang@gcmodeller.org>
2020-03-08 11:07:48 +0800

    required a higher cutoff score

--- src/workbench/R#/demo/regprecise/TFBS_summary.R
+++ src/workbench/R#/demo/regprecise/TFBS_summary.R
@@ -5,6 +5,8 @@ list.files("K:\20200226\TRN\genomics\search\TFBS", pattern = "*.fasta")
 	read.fasta(path) 
 	:> as.vector 
 	:> sapply(fa -> as.object(fa)$locus_tag) 
-	:> unique;
+	:> unique
+	;
 }, names = path -> basename(path))
-:> save.list(file = "K:\20200226\TRN\genomics\search\TFBS.json");
+:> save.list(file = "K:\20200226\TRN\genomics\search\TFBS.json")
+;
