46c982fbd93ccdc037eb884d5eed8f82e9337361
xieguigang <xie.guigang@gcmodeller.org>
2020-03-08 20:28:56 +0800

    create regulation data

---
+++ src/workbench/R#/demo/regprecise/createFootprints.R
@@ -0,0 +1,11 @@
+imports "TRN.builder" from "phenotype_kit.dll";
+imports "annotation.workflow" from "seqtoolkit.dll";
+
+["K:\20200226\TRN\genomics\search\regulators.besthit.csv"] 
+:> open.stream(type = "SBH", ioRead = TRUE)
+:> regulation.footprint(
+	"K:\20200226\TRN\genomics\search\TFBS.summary.csv" :> read.footprints,
+	"P:\XCC\Regprecise\Db.Xml" :> read.regprecise
+)
+:> write.regulations(file = "K:\20200226\TRN\genomics\search\regulations.csv")
+;
--- src/workbench/R#/phenotype_kit/TRNBuilder.vb
+++ src/workbench/R#/phenotype_kit/TRNBuilder.vb
@@ -1,9 +1,13 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Data.csv.IO.Linq
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Data.Regprecise
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
+Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Package("TRN.builder")>
 Module TRNBuilder
@@ -11,6 +15,34 @@ Module TRNBuilder
     <ExportAPI("read.regprecise")>
     Public Function readRegPrecise(file As String) As TranscriptionFactors
         Return file.LoadXml(Of TranscriptionFactors)
+    End Function
+
+    <ExportAPI("read.footprints")>
+    Public Function readFootprintSites(file As String) As FootprintSite()
+        Return file.LoadCsv(Of FootprintSite)
+    End Function
+
+    <ExportAPI("write.regulations")>
+    Public Function writeRegulationFootprints(regulationFootprints As Object, file$, Optional env As Environment = Nothing) As Object
+        If regulationFootprints Is Nothing Then
+            Return Internal.debug.stop("no content data provides!", env)
+        ElseIf file.StringEmpty Then
+            Return Internal.debug.stop("no file write information provides!", env)
+        End If
+
+        If TypeOf regulationFootprints Is RegulationFootprint() Then
+            Return DirectCast(regulationFootprints, RegulationFootprint()).SaveTo(file)
+        ElseIf TypeOf regulationFootprints Is pipeline AndAlso DirectCast(regulationFootprints, pipeline).elementType Like GetType(RegulationFootprint) Then
+            Using writer As New WriteStream(Of RegulationFootprint)(file)
+                For Each edge As RegulationFootprint In DirectCast(regulationFootprints, pipeline).populates(Of RegulationFootprint)
+                    Call writer.Flush(edge)
+                Next
+            End Using
+
+            Return True
+        Else
+            Return Internal.debug.stop($"invalid data type for write: {regulationFootprints.GetType.FullName }", env)
+        End If
     End Function
 
     <ExportAPI("motif.raw")>
@@ -27,7 +59,31 @@ Module TRNBuilder
     End Function
 
     <ExportAPI("regulation.footprint")>
-    Public Function RegulationFootprint(regulators As BestHit(), motifLocis As FootprintSite(), regprecise As TranscriptionFactors) As RegulationFootprint()
+    Public Function RegulationFootprint(<RRawVectorArgument>
+                                        regulators As Object,
+                                        motifLocis As FootprintSite(),
+                                        regprecise As TranscriptionFactors,
+                                        Optional env As Environment = Nothing) As pipeline
+        If regulators Is Nothing Then
+            Return Nothing
+        End If
 
+        Dim regulatorMaps As BestHit()
+
+        If TypeOf regulators Is BestHit() Then
+            regulatorMaps = DirectCast(regulators, BestHit())
+        ElseIf TypeOf regulators Is pipeline AndAlso DirectCast(regulators, pipeline).elementType Like GetType(BestHit) Then
+            regulatorMaps = DirectCast(regulators, pipeline) _
+                .populates(Of BestHit) _
+                .ToArray
+        Else
+            Return Internal.debug.stop($"invalid regulator maps: '{regulators.GetType.FullName }'!", env)
+        End If
+
+        Return Iterator Function() As IEnumerable(Of RegulationFootprint)
+
+               End Function() _
+ _
+            .DoCall(AddressOf pipeline.CreateFromPopulator)
     End Function
 End Module
