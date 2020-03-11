393c3f7dc6619983aefa3393259d217f72fa2df7
xieguigang <xie.guigang@gcmodeller.org>
2020-03-11 12:36:34 +0800

    xml file model

---
+++ src/GCModeller/engine/IO/Raw/vcXML/frame.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Xml.Serialization
+
+Namespace vcXML
+
+    Public Class vcXMLFile
+        Public Property vcRun As vcRun
+        Public Property index As index
+        Public Property indexOffset As Long
+        Public Property sha1 As String
+    End Class
+
+    Public Class vcRun
+        Public Property frameCount As Integer
+    End Class
+
+    Public Class frame
+        <XmlAttribute> Public Property num As Integer
+        <XmlAttribute> Public Property frameTime As Double
+        <XmlAttribute> Public Property [module] As String
+
+        Public Property vector As vector
+    End Class
+
+    Public Class vector
+        <XmlAttribute> Public Property compressionType As String = "zlib"
+        <XmlAttribute> Public Property compressedLen As Integer
+        <XmlAttribute> Public Property precision As Integer = 64
+        <XmlAttribute> Public Property byteOrder As String = "network"
+        <XmlAttribute> Public Property contentType As String = "mass_profile"
+
+        <XmlText>
+        Public Property data As String
+    End Class
+
+    Public Class index
+
+        <XmlAttribute>
+        Public Property name As String
+        <XmlElement>
+        Public Property offsets As offset()
+    End Class
+
+    Public Class offset
+        <XmlAttribute>
+        Public Property id As String
+        <XmlText>
+        Public Property offset As Long
+    End Class
+End Namespace
