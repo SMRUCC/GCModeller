8729f4837cc206fe2ed393fa23e27710c4d03ccb
xieguigang <xie.guigang@gcmodeller.org>
2020-03-11 22:37:05 +0800

    parse offset index test success

--- src/GCModeller/engine/IO/Raw/vcXML/Reader.vb
+++ src/GCModeller/engine/IO/Raw/vcXML/Reader.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.IO
+Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Net.Http
 Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
 
 Namespace vcXML
@@ -50,14 +52,87 @@ Namespace vcXML
 
             hash = content.Match("<md5>.+</md5>", RegexICSng).GetValue
             fs.BaseStream.Seek(i, SeekOrigin.Begin)
-            content = fs.ReadLine
             index = New Dictionary(Of String, Dictionary(Of String, List(Of offset)))
+            entities = New Dictionary(Of String, Dictionary(Of String, String()))
 
+            Call parseFrameIndex(fs.ReadLine)
+            Call parseEntityIndex(fs.ReadLine)
+        End Sub
+
+        Private Sub parseEntityIndex(content As String)
+            Dim line As Value(Of String) = ""
+            Dim module$
+            Dim type$
+            Dim numOfFrames As Integer = content.attr("size").DoCall(AddressOf Integer.Parse)
+            Dim attrs As Dictionary(Of String, String)
+            Dim buffer As Byte()
+            Dim offset As offset
+            Dim tmpOffsets As New List(Of offset)
+
+            Do While numOfFrames > 0 AndAlso Not (line = fs.ReadLine).StringEmpty
+                attrs = line.Value _
+                   .TagAttributes _
+                   .ToDictionary(Function(a) a.Name,
+                                 Function(a)
+                                     Return a.Value
+                                 End Function)
+                [module] = attrs!module
+                [type] = attrs!content_type
+                offset = New offset With {
+                    .offset = line.Value _
+                        .GetValue _
+                        .DoCall(AddressOf Long.Parse),
+                    .content_type = type,
+                    .id = Integer.Parse(attrs!id),
+                    .[module] = [module]
+                }
+
+                tmpOffsets.Add(offset)
+                numOfFrames -= 1
+            Loop
+
+            For Each offset In tmpOffsets
+                [module] = offset.module
+                type = offset.content_type
+
+                If Not entities.ContainsKey([module]) Then
+                    entities.Add([module], New Dictionary(Of String, String()))
+                End If
+
+                fs.BaseStream.Position = offset.offset
+                fs.DiscardBufferedData()
+
+                buffer = fs.ReadLine.GetValue.DoCall(AddressOf decodeBuffer)
+
+                Using reader As New BinaryDataReader(New MemoryStream(buffer))
+                    Dim id As New List(Of String)
+
+                    Do While Not reader.EndOfStream
+                        id.Add(reader.ReadString(BinaryStringFormat.ByteLengthPrefix))
+                    Loop
+
+                    entities([module]).Add(type, id.ToArray)
+                End Using
+            Next
+        End Sub
+
+        Private Function decodeBuffer(data As String) As Byte()
+            Dim buffer As Byte() = data.Base64RawBytes.UnGzipStream.ToArray
+
+            If BitConverter.IsLittleEndian Then
+                Array.Reverse(buffer)
+            End If
+
+            Return buffer
+        End Function
+
+        Private Sub parseFrameIndex(content As String)
             Dim numOfFrames As Integer = content.attr("size").DoCall(AddressOf Integer.Parse)
             Dim module$
             Dim type$
             Dim attrs As Dictionary(Of String, String)
             Dim offset As offset
+            Dim line As Value(Of String) = ""
 
             Do While numOfFrames > 0 AndAlso Not (line = fs.ReadLine).StringEmpty
                 attrs = line.Value _
@@ -89,29 +164,6 @@ Namespace vcXML
             Loop
 
             fs.ReadLine()
-
-            content = fs.ReadLine
-            numOfFrames = content.attr("size").DoCall(AddressOf Integer.Parse)
-
-            Do While numOfFrames > 0 AndAlso Not (line = fs.ReadLine).StringEmpty
-                attrs = line.Value _
-                   .TagAttributes _
-                   .ToDictionary(Function(a) a.Name,
-                                 Function(a)
-                                     Return a.Value
-                                 End Function)
-                [module] = attrs!module
-                [type] = attrs!content_type
-
-                If Not index.ContainsKey([module]) Then
-                    index.Add([module], New Dictionary(Of String, List(Of offset)))
-                End If
-                If Not index([module]).ContainsKey(type) Then
-                    index([module]).Add(type, New List(Of offset))
-                End If
-
-                numOfFrames -= 1
-            Loop
         End Sub
 
 #Region "IDisposable Support"
