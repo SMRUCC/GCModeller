4e0ae8a0d4ed61bd1ae96058b494708985c7f52a
xieguigang <xie.guigang@gcmodeller.org>
2020-03-12 18:52:06 +0800

    fix of export matrix

--- src/GCModeller/engine/Rscript/dataMatrix.R
+++ src/GCModeller/engine/Rscript/dataMatrix.R
@@ -2,5 +2,5 @@ imports "vcellkit.rawXML" from "vcellkit.dll";
 
 list.files("S:\2020\union\3.VCell\result\raw", pattern = "*.vcXML")
 :> frame.matrix(tick = 499, metabolome = "mass_profile")
-:> write.csv(file = "S:\2020\union\3.VCell\result\controls.csv")
+:> write.csv(file = "S:\2020\union\3.VCell\result\samples.csv")
 ;
