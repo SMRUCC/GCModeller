870b8d1733a5368113c462b582a85595873cc9b3
xieguigang <xie.guigang@gcmodeller.org>
2020-03-16 11:09:34 +0800

    download reaction class data

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/WebQuery/ReactionClassQuery.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/WebQuery/ReactionClassQuery.vb
@@ -90,6 +90,12 @@ Namespace Assembly.KEGG.DBGET.WebQuery
 
     Public Module ReactionClassWebQuery
 
+        ''' <summary>
+        ''' The function returns a id list which failure to download its content data.
+        ''' </summary>
+        ''' <param name="export"></param>
+        ''' <param name="cache"></param>
+        ''' <returns></returns>
         Public Function DownloadReactionClass(export$, Optional cache$ = Nothing) As IEnumerable(Of String)
             Dim web As New ReactionClassQuery(cache Or $"{export}/.cache/".When(cache.StringEmpty))
             Dim numbers As BriteHEntry.ReactionClass() = BriteHEntry.ReactionClass _
@@ -106,9 +112,13 @@ Namespace Assembly.KEGG.DBGET.WebQuery
                     save = $"{export}/{number.GetPathComponents}/{number.RCNumber}.xml"
                     rcnumber = web.Query(Of ReactionClass)(number, ".html")
 
-                    Call rcnumber _
-                        .GetXml _
-                        .SaveTo(save)
+                    If rcnumber Is Nothing Then
+                        failures.Add(number.RCNumber)
+                    Else
+                        Call rcnumber _
+                            .GetXml _
+                            .SaveTo(save)
+                    End If
                 Next
             End Using
 
