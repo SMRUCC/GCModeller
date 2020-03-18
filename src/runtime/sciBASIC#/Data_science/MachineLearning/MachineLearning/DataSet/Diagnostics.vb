f946bd889b8cd176dfa2e0ecc2d7a3e164ae5f52
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-03-18 13:25:47 +0800

    check of the ML data samples

---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/MachineLearning/DataSet/Diagnostics.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
+Imports Microsoft.VisualBasic.Linq
+
+Namespace StoreProcedure
+
+    Public Module Diagnostics
+
+        <Extension>
+        Public Iterator Function CheckDataSet(data As DataSet) As IEnumerable(Of LogEntry)
+            Dim nSamples = data.DataSamples.size
+            Dim size As Size = data.Size
+
+            ' check for sample datas
+            For Each sample As Sample In data.DataSamples.AsEnumerable
+                If sample.status.Length <> size.Width Then
+                    Yield New LogEntry With {
+                        .message = $"{sample.ID} sample vector size is not equals to {size.Width}!",
+                        .[object] = sample.ID,
+                        .time = Now,
+                        .level = MSG_TYPES.ERR
+                    }
+                End If
+            Next
+        End Function
+    End Module
+End Namespace
