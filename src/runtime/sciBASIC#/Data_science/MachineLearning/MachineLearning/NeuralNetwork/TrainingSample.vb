fb5716122461bfaaa42aada8e1fbbf34c38284ea
xieguigang <xie.guigang@gcmodeller.org>
2020-03-18 14:48:24 +0800

    fix of the training set problem

---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/MachineLearning/NeuralNetwork/TrainingSample.vb
@@ -0,0 +1,20 @@
+﻿Namespace NeuralNetwork
+
+    Public Structure TrainingSample
+
+        Dim sampleID As String
+        Dim sample As Double()
+
+        ''' <summary>
+        ''' The output result.
+        ''' </summary>
+        Dim classify As Double()
+
+        Public ReadOnly Property isEmpty As Boolean
+            Get
+                Return sample.IsNullOrEmpty OrElse classify.IsNullOrEmpty
+            End Get
+        End Property
+
+    End Structure
+End Namespace
