7b295ac81466bf3bb96252c86449c1ace4900daf
xieguigang <xie.guigang@gcmodeller.org>
2020-03-19 09:19:37 +0800

    add dev notes about the framework runtime configuration

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/ApplicationServices/App.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/ApplicationServices/App.vb
@@ -477,15 +477,22 @@ Public Module App
     Dim m_joinedVariables As New Dictionary(Of NamedValue(Of String))
 
     ''' <summary>
-    ''' 添加参数到应用程序的环境变量之中
+    ''' add/update the environment variable in sciBASIC.NET framework.
+    ''' 
+    ''' (添加参数到应用程序的环境变量之中)
     ''' </summary>
-    ''' <param name="name">如果给定的当前这个参数名称存在于当前框架环境中，则会更新原来的值</param>
-    ''' <param name="value$"></param>
+    ''' <param name="name">
+    ''' if target variable symbol name is exists in the framework, 
+    ''' then the config value of the variable will be updated.
+    ''' or this function will add a new variable into the 
+    ''' environment.
+    ''' 
+    ''' (如果给定的当前这个参数名称存在于当前框架环境中，则会更新原来的值)</param>
+    ''' <param name="value"></param>
     Public Sub JoinVariable(name$, value$)
-        m_joinedVariables(name) =
-            New NamedValue(Of String) With {
-                .Name = name,
-                .Value = value
+        m_joinedVariables(name) = New NamedValue(Of String) With {
+            .Name = name,
+            .Value = value
         }
     End Sub
 
