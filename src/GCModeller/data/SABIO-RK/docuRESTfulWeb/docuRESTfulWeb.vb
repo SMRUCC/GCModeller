d390893fde957eacaeca007f283a20551d5107bf
xieguigang <xie.guigang@gcmodeller.org>
2020-03-20 21:25:17 +0800

    web api module

--- src/GCModeller/data/SABIO-RK/docuRESTfulWeb/docuRESTfulWeb.vb
+++ src/GCModeller/data/SABIO-RK/docuRESTfulWeb/docuRESTfulWeb.vb
@@ -1,3 +1,62 @@
-﻿Public Class docuRESTfulWeb
+﻿Imports System.Runtime.CompilerServices
+Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
+Imports SMRUCC.genomics.Data.SABIORK.SBML
 
-End Class
+Public Module docuRESTfulWeb
+
+    Public Const KEGG_QUERY_ENTRY As String = "http://sabiork.h-its.org/sabioRestWebServices/reactions/reactionIDs?q=KeggReactionID:"
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="IdList"></param>
+    ''' <param name="ExportDir"></param>
+    ''' <returns></returns>
+    ''' <remarks>
+    ''' 因为会存在非常多的废弃的id编号，所以这个函数应该会被废弃掉
+    ''' </remarks>
+    <ExportAPI("Query.KEGG")>
+    <Extension>
+    Public Iterator Function QueryUsing_KEGGId(IdList As String(), ExportDir As String) As IEnumerable(Of String)
+        For Each Id As String In IdList
+            Dim url As String = (KEGG_QUERY_ENTRY & Id)
+            Dim PageContent = url.GET
+            Dim Entries As String() = (From m As Match In Regex.Matches(PageContent, "<SabioReactionID>\d+</SabioReactionID>") Select m.Value.GetValue).ToArray
+
+            For Each Entry In Entries
+                Dim File = String.Format("{0}/{1}-{2}.sbml", ExportDir, Id, Entry)
+
+                url = SabiorkSBML.URL_SABIORK_KINETIC_LAWS_QUERY & Entry
+                Call url.GET.SaveTo(File)
+            Next
+        Next
+    End Function
+
+    <ExportAPI("SABIORK.Downloads")>
+    Public Function Download(Dir As String) As Integer
+        Dim c As Integer = 0
+
+        For i As Integer = FileIO.FileSystem.GetFiles(Dir).Count + 1 To Integer.MaxValue
+            Dim id As String = "kinlawids_" & i
+            Dim url As String = SabiorkSBML.URL_SABIORK_KINETIC_LAWS_QUERY & i
+            Dim File = String.Format("{0}/{1}.sbml", Dir, id)
+
+            Call url.GET.SaveTo(File)
+
+            If FileIO.FileSystem.GetFileInfo(File).Length < 100 Then
+                c += 1
+                If c > 1500 Then
+                    Exit For
+                End If
+            Else
+                c = 0
+            End If
+
+            Call Threading.Thread.Sleep(10)
+        Next
+
+        Return 0
+    End Function
+End Module
