aa1057d4dbdbf9a6129ad2339b5ab0398efa1e14
xieguigang <xie.guigang@gcmodeller.org>
2020-03-20 22:24:29 +0800

    modeller

---
+++ src/GCModeller/engine/vcellkit/Modeller/Modeller.vb
@@ -0,0 +1,27 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
+
+<Package("vcellkit.modeller")>
+Module Modeller
+
+    ''' <summary>
+    ''' apply the kinetics parameters from the sabio-rk database.
+    ''' </summary>
+    ''' <param name="vcell"></param>
+    ''' <returns></returns>
+    <ExportAPI("apply.kinetics")>
+    Public Function applyKinetics(vcell As VirtualCell) As VirtualCell
+
+    End Function
+
+    ''' <summary>
+    ''' read the virtual cell model file
+    ''' </summary>
+    ''' <param name="path"></param>
+    ''' <returns></returns>
+    <ExportAPI("read.vcell")>
+    Public Function LoadVirtualCell(path As String) As VirtualCell
+        Return path.LoadXml(Of VirtualCell)
+    End Function
+End Module
