64d8f17465c1f49b1cb134cfc1a2d9337e3a598b
xieguigang <xie.guigang@gcmodeller.org>
2020-04-02 20:55:51 +0800

    parse identifier url data

---
+++ src/GCModeller/data/SABIO-RK/SBML/SBMLInternalIndexer.vb
@@ -0,0 +1,23 @@
+﻿Imports SMRUCC.genomics.Model.SBML.Level3
+
+Namespace SBML
+
+    Public Class SBMLInternalIndexer
+
+        ReadOnly compartments As Dictionary(Of String, compartment)
+        ReadOnly species As Dictionary(Of String, species)
+
+        Sub New(sbml As XmlFile(Of SBMLReaction))
+            compartments = sbml.model.listOfCompartments.ToDictionary(Function(c) c.id)
+            species = sbml.model.listOfSpecies.ToDictionary(Function(c) c.id)
+        End Sub
+
+        Public Iterator Function getEnzymes(react As SBMLReaction) As IEnumerable(Of species)
+            If Not react.listOfModifiers.IsNullOrEmpty Then
+                For Each modifier In react.listOfModifiers
+                    Yield species(modifier.species)
+                Next
+            End If
+        End Function
+    End Class
+End Namespace
