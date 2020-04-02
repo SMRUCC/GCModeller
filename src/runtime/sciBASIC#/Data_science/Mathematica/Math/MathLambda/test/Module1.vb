a25f58f73ba09925ca846edcb4b01457fa76316a
xieguigang <xie.guigang@gcmodeller.org>
2020-04-02 22:20:04 +0800

    mathML parser

---
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/MathLambda/test/Module1.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.MIME.application.rdf_xml.MathML
+
+Module Module1
+
+    Sub Main()
+        Dim exp As BinaryExpression = BinaryExpression.FromMathML("E:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematica\Math\MathLambda\mathML.xml".ReadAllText)
+
+        Pause()
+    End Sub
+
+End Module
