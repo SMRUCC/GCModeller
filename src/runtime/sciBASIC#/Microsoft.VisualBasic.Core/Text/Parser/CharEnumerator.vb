b3ccf0467c5959c4357f1a7d44d51e2afad30f7e
xieguigang <xie.guigang@gcmodeller.org>
2020-04-03 10:05:02 +0800

    improvements of the text parser model

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Text/Parser/CharEnumerator.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Text/Parser/CharEnumerator.vb
@@ -55,16 +55,46 @@ Namespace Text.Parser
 
         ReadOnly buffer As New List(Of Char)
 
+        ''' <summary>
+        ''' get current size of the data in this char buffer object
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property Size As Integer
+            Get
+                Return buffer.Count
+            End Get
+        End Property
+
+        Public Sub Clear()
+            Call buffer.Clear()
+        End Sub
+
         Public Overrides Function ToString() As String
             Return buffer.CharString
         End Function
 
         Public Shared Widening Operator CType(c As Char) As CharBuffer
-            Dim buf As New CharBuffer
+            Return New CharBuffer + c
+        End Operator
+
+        Public Shared Operator +(buf As CharBuffer, c As Char) As CharBuffer
             buf.buffer.Add(c)
             Return buf
         End Operator
 
+        Public Shared Operator *(buf As CharBuffer, n As Integer) As CharBuffer
+            If n = 0 Then
+                buf.Clear()
+            Else
+                Dim template As Char() = buf.buffer.ToArray
+
+                For i As Integer = 1 To n - 1
+                    buf.buffer.AddRange(template)
+                Next
+            End If
+
+            Return buf
+        End Operator
 
     End Class
 
