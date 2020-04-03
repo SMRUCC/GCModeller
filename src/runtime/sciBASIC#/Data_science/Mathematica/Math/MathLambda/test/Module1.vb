dfe9639cfe102fecb663005a4591485f6c757ab2
xieguigang <xie.guigang@gcmodeller.org>
2020-04-03 13:54:33 +0800

    test success

--- src/runtime/sciBASIC#/Data_science/Mathematica/Math/MathLambda/test/Module1.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/MathLambda/test/Module1.vb
@@ -6,7 +6,10 @@ Module Module1
     Sub Main()
         Dim exp As LambdaExpression = LambdaExpression.FromMathML("D:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematica\Math\MathLambda\mathML.xml".ReadAllText)
         Dim func = Compiler.CreateLambda(exp)
-        Dim del = func.Compile
+        Dim del As Func(Of Double, Double, Double, Double, Double, Double, Double) = func.Compile()
+
+        Console.WriteLine(func)
+        Console.WriteLine(del(1, 0, 0.1, 0.01, 99, 10))
 
         Pause()
     End Sub
