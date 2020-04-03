d084b884b1f649228b3f0de517d483634420e500
xieguigang <xie.guigang@gcmodeller.org>
2020-04-03 11:41:26 +0800

    xml parser test success

--- src/runtime/sciBASIC#/mime/application%xml/XmlGeneric/XmlElement.vb
+++ src/runtime/sciBASIC#/mime/application%xml/XmlGeneric/XmlElement.vb
@@ -1,8 +1,17 @@
 ﻿Public Class XmlElement
 
     Public Property name As String
+    Public Property [namespace] As String
     Public Property attributes As Dictionary(Of String, String)
     Public Property elements As XmlElement()
     Public Property text As String
+
+    Public Overrides Function ToString() As String
+        Return $"{[namespace]}::{name}"
+    End Function
+
+    Public Shared Function ParseXmlText(xmlText As String) As XmlElement
+        Return XmlParser.ParseXml(xmlText)
+    End Function
 
 End Class
--- src/runtime/sciBASIC#/mime/application%xml/test/Module1.vb
+++ src/runtime/sciBASIC#/mime/application%xml/test/Module1.vb
@@ -1,7 +1,9 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim xml = Microsoft.VisualBasic.MIME.application.xml.XmlParser.ParseXml("D:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematica\Math\MathLambda\mathML.xml".ReadAllText)
 
+        Pause()
     End Sub
 
 End Module
