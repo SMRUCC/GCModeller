ed02974d67d4afa4dc150555897229c686c7d108
xieguigang <xie.guigang@gcmodeller.org>
2020-04-03 13:14:01 +0800

    failure to compile generated code

--- src/runtime/sciBASIC#/Data_science/Mathematica/Math/MathLambda/test/Module1.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/MathLambda/test/Module1.vb
@@ -6,6 +6,7 @@ Module Module1
     Sub Main()
         Dim exp As LambdaExpression = LambdaExpression.FromMathML("D:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematica\Math\MathLambda\mathML.xml".ReadAllText)
         Dim func = Compiler.CreateLambda(exp)
+        Dim del = func.Compile
 
         Pause()
     End Sub
