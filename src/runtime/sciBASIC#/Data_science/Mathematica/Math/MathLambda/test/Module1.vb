686a6dce2009ab4354fba066cf3dcadd17bb66fb
xieguigang <xie.guigang@gcmodeller.org>
2020-04-04 18:38:22 +0800

    math expression lambda compiler

--- src/runtime/sciBASIC#/Data_science/Mathematica/Math/MathLambda/test/Module1.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/MathLambda/test/Module1.vb
@@ -1,16 +1,29 @@
-﻿Imports Microsoft.VisualBasic.Math.Lambda
+﻿Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.Lambda
+Imports Microsoft.VisualBasic.Math.Scripting.MathExpression
 Imports Microsoft.VisualBasic.MIME.application.xml.MathML
 
 Module Module1
 
     Sub Main()
+        Call mathLambdaTest()
         Call complexExpressionTest()
+    End Sub
+
+    Sub mathLambdaTest()
+        Dim exp = New ExpressionTokenIcer("(x+y+z) ^ 2").GetTokens.ToArray.DoCall(AddressOf BuildExpression)
+        Dim lambda = ExpressionCompiler.CreateLambda({"x", "y", "z"}, exp)
+        Dim fun As Func(Of Double, Double, Double, Double) = lambda.Compile
+
+        Console.WriteLine(fun(1, 1, 1))
+
+        Pause()
     End Sub
 
     Sub complexExpressionTest()
         Dim test As String = "E:\GCModeller\src\runtime\sciBASIC#\mime\etc\kinetics2.xml"
         Dim exp As LambdaExpression = LambdaExpression.FromMathML(test.ReadAllText)
-        Dim func = Compiler.CreateLambda(exp)
+        Dim func = MathMLCompiler.CreateLambda(exp)
         Dim del = func.Compile()
 
         Console.WriteLine(func)
@@ -22,7 +35,7 @@ Module Module1
     Sub simpleExpressionTest()
         Dim test As String = "E:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematica\Math\MathLambda\mathML.xml"
         Dim exp As LambdaExpression = LambdaExpression.FromMathML(test.ReadAllText)
-        Dim func = Compiler.CreateLambda(exp)
+        Dim func = MathMLCompiler.CreateLambda(exp)
         Dim del As Func(Of Double, Double, Double, Double, Double, Double, Double) = func.Compile()
 
         Console.WriteLine(func)
