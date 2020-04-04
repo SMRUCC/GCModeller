04291be7a8b917ad9a2957cd877f582ecfb14825
xieguigang <xie.guigang@gcmodeller.org>
2020-04-04 18:56:05 +0800

    Update DisturbAPI.vb

--- src/GCModeller/sub-system/PLAS.NET/SSystem/System/Experiments/DisturbAPI.vb
+++ src/GCModeller/sub-system/PLAS.NET/SSystem/System/Experiments/DisturbAPI.vb
@@ -58,12 +58,15 @@ Namespace Kernel.ObjectModels
 
     Public Module DisturbAPI
 
-        Public ReadOnly Property Methods As IReadOnlyDictionary(Of Types, Func(Of Double, Double, Double)) =
-            New Dictionary(Of Types, Func(Of Double, Double, Double)) From {
+        Public ReadOnly Property Methods As IReadOnlyDictionary(Of Types, Func(Of Double, Double, Double))
+
+        Sub New()
+            Methods = New Dictionary(Of Types, Func(Of Double, Double, Double)) From {
  _
-            {Types.Increase, Function(x, d) x + d},
-            {Types.Decrease, Function(x, d) x - d},
-            {Types.ChangeTo, Function(x, d) x}
-        }
+                {Types.Increase, Function(x, d) x + d},
+                {Types.Decrease, Function(x, d) x - d},
+                {Types.ChangeTo, Function(x, d) x}
+            }
+        End Sub
     End Module
 End Namespace
