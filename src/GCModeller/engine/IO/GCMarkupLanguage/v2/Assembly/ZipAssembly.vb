534cca91fe8ad2fcd2bbfb880506e327f1d7528f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-04-06 12:58:16 +0800

    zip assembly for store model file?

---
+++ src/GCModeller/engine/IO/GCMarkupLanguage/v2/Assembly/ZipAssembly.vb
@@ -0,0 +1,11 @@
+﻿Namespace v2
+
+    ''' <summary>
+    ''' 将所有的模型数据都放置在同一个XML文件之中
+    ''' 会因为文件过大而难于调试
+    ''' 在这里进行元素的分块存储
+    ''' </summary>
+    Public Class ZipAssembly
+
+    End Class
+End Namespace
