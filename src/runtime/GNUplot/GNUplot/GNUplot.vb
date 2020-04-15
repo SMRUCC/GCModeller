9dd1a8203d8b9da7d7e73f4026967f68d381ff6d
xieguigang <xie.guigang@gcmodeller.org>
2020-04-15 20:49:41 +0800

    stdNum

--- src/runtime/GNUplot/GNUplot/GNUplot.vb
+++ src/runtime/GNUplot/GNUplot/GNUplot.vb
@@ -1,36 +1,35 @@
 ﻿#Region "Microsoft.VisualBasic::b3858db75c520c519e5d82a46c1aebcf, ..\GNUplot\GNUplot\GNUplot.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
-Imports System.Diagnostics
 Imports System.IO
 Imports System.Threading
-Imports Microsoft.VisualBasic
 Imports Microsoft.VisualBasic.Language
+Imports stdNum = System.Math
 
 ''' <summary>
 ''' Gnuplot is a portable command-line driven graphing utility for Linux, OS/2, MS Windows, OSX, VMS, and many other platforms. 
@@ -543,7 +542,7 @@ Public Module GNUplot
 
     Private Sub removeContourLabels()
         While contourLabelCount > 50000
-            __gnuplot.WriteLine("unset object " & contourLabelCount & ";unset label " & Math.Max(Interlocked.Decrement(contourLabelCount), contourLabelCount + 1))
+            __gnuplot.WriteLine("unset object " & contourLabelCount & ";unset label " & stdNum.Max(Interlocked.Decrement(contourLabelCount), contourLabelCount + 1))
         End While
     End Sub
 
