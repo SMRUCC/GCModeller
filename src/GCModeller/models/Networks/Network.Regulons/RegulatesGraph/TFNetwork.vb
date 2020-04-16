6573103cc5b3d51338b30a3a4a7e2599ef170013
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-04-16 18:42:34 +0800

    stdNum

--- src/GCModeller/models/Networks/Network.Regulons/RegulatesGraph/TFNetwork.vb
+++ src/GCModeller/models/Networks/Network.Regulons/RegulatesGraph/TFNetwork.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::190935173a49288a01f0d1121ad84530, Networks\Network.Regulons\RegulatesGraph\TFNetwork.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module NetAPI
-    ' 
-    '     Function: __netEdge, BuildNetwork
-    ' 
-    ' /********************************************************************************/
+' Module NetAPI
+' 
+'     Function: __netEdge, BuildNetwork
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.visualize.Network
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.genomics.Interops.NBCR.MEME_Suite.Analysis.GenomeMotifFootPrints
+Imports stdNum = System.Math
 
 Public Module NetAPI
 
@@ -104,10 +104,10 @@ Public Module NetAPI
             {"RegType", If(x.Pcc > 0, "Pos", "Neg")}
         }
         Return New NetworkEdge With {
-            .FromNode = x.Regulator,
-            .ToNode = x.ORF,
-            .value = Math.Abs(0.5 * (x.Pcc + x.sPcc)),
-            .Interaction = "Regulation",
+            .fromNode = x.Regulator,
+            .toNode = x.ORF,
+            .value = stdNum.Abs(0.5 * (x.Pcc + x.sPcc)),
+            .interaction = "Regulation",
             .Properties = prop
         }
     End Function
