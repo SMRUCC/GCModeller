41d8718aec0bfb9a42e1fcd75d84d57d619e3812
xieguigang <xie.guigang@gcmodeller.org>
2020-04-18 09:44:30 +0800

    compile lambda for kinetics engine in vcell engine

--- src/GCModeller/engine/Model/Models/Kinetics.vb
+++ src/GCModeller/engine/Model/Models/Kinetics.vb
@@ -1,9 +1,28 @@
-﻿Imports Microsoft.VisualBasic.Math.Scripting.MathExpression.Impl
+﻿Imports System.Linq.Expressions
+Imports Microsoft.VisualBasic.Math.Scripting.MathExpression
 
 Public Structure Kinetics
 
-    Dim formula As Expression
+    Dim formula As Impl.Expression
     Dim parameters As String()
     Dim paramVals As Object()
+
+    Public Function CompileLambda() As Func(Of Func(Of String, Double), Double)
+        Dim lambda As LambdaExpression = ExpressionCompiler.CreateLambda(parameters, formula)
+        Dim handler As [Delegate] = lambda.Compile
+        Dim vm = Me
+
+        Return Function(getVal As Func(Of String, Double)) As Double
+                   Dim vals = vm.paramVals.ToArray
+
+                   For i As Integer = 0 To vals.Length - 1
+                       If TypeOf vals(i) Is String Then
+                           vals(i) = getVal(vals(i))
+                       End If
+                   Next
+
+                   Return handler.DynamicInvoke(vals)
+               End Function
+    End Function
 
 End Structure
