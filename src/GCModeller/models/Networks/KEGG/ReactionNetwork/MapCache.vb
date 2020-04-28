568b8c69a3c9c3359c8f47c895c0c12870ee6811
xieguigang <xie.guigang@gcmodeller.org>
2020-04-28 19:05:01 +0800

    deal with the fast cache data for build network

--- src/GCModeller/models/Networks/KEGG/ReactionNetwork/MapCache.vb
+++ src/GCModeller/models/Networks/KEGG/ReactionNetwork/MapCache.vb
@@ -1,4 +1,5 @@
-﻿Imports stdNum = System.Math
+﻿Imports System.Text
+Imports stdNum = System.Math
 
 Namespace ReactionNetwork
 
@@ -12,12 +13,61 @@ Namespace ReactionNetwork
         Sub New()
         End Sub
 
+        Public Function FindPoints(c1$, c2$) As String()
+            Dim key = indexKey(c1, c2)
+
+            If index.ContainsKey(key) Then
+                Return index(key)
+            Else
+                Return {}
+            End If
+        End Function
+
+        Public Function FindAllPoints(compounds As IEnumerable(Of String)) As String()
+            Dim list = compounds.ToArray
+            Dim result As New List(Of String)
+
+            For Each a In list
+                For Each b In list
+                    result.AddRange(FindPoints(a, b))
+                Next
+            Next
+
+            Return result.Distinct.ToArray
+        End Function
+
         Private Shared Function indexKey(c1$, c2$) As String
             Dim i1 = Integer.Parse(c1.Substring(1))
             Dim i2 = Integer.Parse(c2.Substring(1))
             Dim key = stdNum.Min(i1, i2) & "|" & stdNum.Max(i1, i2)
 
             Return key
+        End Function
+
+        Public Function Text() As String
+            Dim sb As New StringBuilder
+
+            For Each index As KeyValuePair(Of String, String()) In Me.index
+                sb.AppendLine(index.Key & " " & index.Value.JoinBy(","))
+            Next
+
+            Return sb.ToString
+        End Function
+
+        Public Shared Function ParseText(text As String()) As MapCache
+            Dim cache As New Dictionary(Of String, String())
+            Dim values As String()
+            Dim key As String
+
+            For Each line As String In text
+                values = line.Split
+                key = values(Scan0)
+                values = values.Split(","c)
+
+                Call cache.Add(key, values)
+            Next
+
+            Return New MapCache With {.index = cache}
         End Function
 
         Public Shared Function CreateFromTable(rxn As IEnumerable(Of ReactionTable)) As MapCache
