aa50d2188a06d2753619603af579e1004ec16b51
xieguigang <xie.guigang@gcmodeller.org>
2020-05-04 00:05:18 +0800

    call Rweb backend

--- src/workbench/workbench/lib/Rconsole.js
+++ src/workbench/workbench/lib/Rconsole.js
@@ -1,51 +1,14 @@
 var shell;
 (function (shell) {
     function handle_command(command) {
-        // Conversational trivialities
-        var log_emoticon = function (face, rotate_direction) {
-            // top notch emotional mirroring (*basically* artificial general intelligence :P)
-            var span = document.createElement("span");
-            span.style.display = "inline-block";
-            span.style.transform = "rotate(" + (rotate_direction / 4) + "turn)";
-            span.style.cursor = "vertical-text";
-            span.style.fontSize = "1.3em";
-            span.innerText = face.replace(">", "?").replace("<", "?");
-            con.log(span);
-        };
-        if (command.match(/^((Well|So|Um|Uh),? )?(Hi|Hello|Hey|Greetings|Hola)/i)) {
-            con.log((command.match(/^[A-Z]/) ? "Hello" : "hello") + (command.match(/\.|!/) ? "." : ""));
-        }
-        else if (command.match(/^((Well|So|Um|Uh),? )?(What'?s up|Sup)/i)) {
-            con.log((command.match(/^[A-Z]/) ? "Not much" : "not much") + (command.match(/\?|!/) ? "." : ""));
-        }
-        else if (command.match(/^(>?[:;8X][-o ]?[O03PCDS\\/|()[\]{}])$/i)) {
-            log_emoticon(command, +1);
-        }
-        else if (command.match(/^([O03PCDS\\/|()[\]{}][-o ]?[:;8X]<?)$/i)) {
-            log_emoticon(command, -1);
-        }
-        else if (command.match(/^<3$/i)) {
-            con.log("?");
-            // Unhelp
-        }
-        else if (command.match(/^(!*\?+!*|(please |plz )?(((I )?(want|need)[sz]?|display|show( me)?|view) )?(the |some )?help|^(gimme|give me|lend me) ((the |some )?)help| a hand( here)?)/i)) { // overly comprehensive, much?
-            con.log("I could definitely help you if I wanted to.");
-        }
-        else {
-            var err;
-            try {
-                var result = eval(command);
-            }
-            catch (error) {
-                err = error;
-            }
-            if (err) {
-                con.error(err);
+        $ts.post("http://127.0.0.1:4752/exec", function (result) {
+            if (result.code == 0) {
+                con.log(result.info).classList.add("result");
             }
             else {
-                con.log(result).classList.add("result");
+                con.error(result.info);
             }
-        }
+        });
     }
     shell.handle_command = handle_command;
     ;
