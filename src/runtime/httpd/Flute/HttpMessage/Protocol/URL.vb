8ba151951436716ee3c346e799203732b0d9415c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-07 14:02:57 +0800

    url data model

--- src/runtime/httpd/Flute/HttpMessage/Protocol/URL.vb
+++ src/runtime/httpd/Flute/HttpMessage/Protocol/URL.vb
@@ -2,5 +2,23 @@
 
     Public Class URL
 
+        ''' <summary>
+        ''' the url query parameters
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property query As Dictionary(Of String, String)
+        Public Property path As String
+        Public Property hostName As String
+        Public Property port As Integer
+        Public Property protocol As String
+        ''' <summary>
+        ''' #....
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property hashcode As String
+
+        Sub New(url As String)
+
+        End Sub
     End Class
 End Namespace
