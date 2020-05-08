330ee6dd3939cf3b7140ed1cc9419379a44d6088
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-08 17:44:40 +0800

    fix of the test

--- src/GCModeller/engine/Dynamics/test/Module1.vb
+++ src/GCModeller/engine/Dynamics/test/Module1.vb
@@ -1,72 +1,69 @@
 ﻿#Region "Microsoft.VisualBasic::7c8c4ba7561b6f8d8db56e7bd464cde2, Dynamics\test\Module1.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Module1
-    ' 
-    '     Function: mass, reactions
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Module Module1
+' 
+'     Function: mass, reactions
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Dynamics.Debugger
 Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Data.visualize.Network
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Core
 
 Module Module1
 
     Sub Main()
         Dim massTable = mass.ToDictionary(Function(m) m.ID)
-        Dim envir As New Vessel With {
-            .MassEnvironment = massTable.Values.ToArray,
-            .Channels = reactions(massTable).ToArray
-        }
-
+        Dim envir As Vessel = New Vessel().load(massTable.Values).load(reactions(massTable))
         Dim snapshots As New List(Of DataSet)
         Dim flux As New List(Of DataSet)
+        Dim dynamics = envir.ContainerIterator(100000)
 
         Call envir.Initialize()
 
         For i As Integer = 0 To 100000
             flux += New DataSet With {
                 .ID = i,
-                .Properties = envir.ContainerIterator().ToDictionary.FlatTable
+                .Properties = .ToDictionary.FlatTable
             }
             snapshots += New DataSet With {
                 .ID = i,
@@ -109,68 +106,68 @@ Module Module1
         Yield New Channel(pop({"A", "B"}), pop({"C", "D"})) With {
             .bounds = {50, 50},
             .ID = "ABCD",
-            .forward = New Controls,
-            .reverse = New Controls With {.activation = pop({"B", "D"}).ToArray}}
+            .forward = New AdditiveControls,
+            .reverse = New AdditiveControls With {.activation = pop({"B", "D"}).ToArray}}
 
         Yield New Channel(pop({"E", "F"}), pop({"A", "G"})) With {
             .bounds = {50, 50},
             .ID = "EFAG",
-            .forward = New Controls,
-            .reverse = New Controls With {.activation = pop({"B"}).ToArray}
+            .forward = New AdditiveControls,
+            .reverse = New AdditiveControls With {.activation = pop({"B"}).ToArray}
         }
 
         Yield New Channel(pop({"B"}), pop({"A", "D"})) With {
             .bounds = {50, 50},
             .ID = "BAD",
-            .forward = New Controls With {.activation = pop({"C", "G", "B"}).ToArray},
-            .reverse = New Controls With {.activation = pop({"E"}).ToArray}
+            .forward = New AdditiveControls With {.activation = pop({"C", "G", "B"}).ToArray},
+            .reverse = New AdditiveControls With {.activation = pop({"E"}).ToArray}
         }
 
         Yield New Channel(pop({"G"}), pop({"E"})) With {
             .bounds = {50, 50},
             .ID = "GE",
-            .forward = New Controls With {.activation = pop({"F"}).ToArray}
+            .forward = New AdditiveControls With {.activation = pop({"F"}).ToArray}
         }
         Yield New Channel(pop({"E"}), pop({"G", "D", "C"})) With {
             .bounds = {50, 50},
             .ID = "EGDC",
-            .forward = New Controls With {.activation = pop({"E"}).ToArray},
-            .reverse = New Controls With {.activation = pop({"C", "D"}).ToArray}
+            .forward = New AdditiveControls With {.activation = pop({"E"}).ToArray},
+            .reverse = New AdditiveControls With {.activation = pop({"C", "D"}).ToArray}
         }
 
         Yield New Channel(pop({"B", "F"}), pop({"H"})) With {
             .bounds = {50, 50},
             .ID = "BFH",
-            .forward = New Controls With {.activation = pop({"B"}).ToArray},
-            .reverse = New Controls With {.activation = pop({"I", "D"}).ToArray}
+            .forward = New AdditiveControls With {.activation = pop({"B"}).ToArray},
+            .reverse = New AdditiveControls With {.activation = pop({"I", "D"}).ToArray}
         }
 
         Yield New Channel(pop({"D", "F"}), pop({"H"})) With {
             .bounds = {50, 50},
             .ID = "DFH",
-            .forward = New Controls With {.activation = pop({"B"}).ToArray},
-            .reverse = New Controls With {.activation = pop({"I", "D"}).ToArray}
+            .forward = New AdditiveControls With {.activation = pop({"B"}).ToArray},
+            .reverse = New AdditiveControls With {.activation = pop({"I", "D"}).ToArray}
         }
 
         Yield New Channel(pop({"I"}), pop({"G"})) With {
             .bounds = {50, 50},
             .ID = "IG",
-           .forward = New Controls With {.activation = pop({"B"}).ToArray},
-           .reverse = New Controls With {.activation = pop({"G", "D"}).ToArray}
+           .forward = New AdditiveControls With {.activation = pop({"B"}).ToArray},
+           .reverse = New AdditiveControls With {.activation = pop({"G", "D"}).ToArray}
        }
 
         Yield New Channel(pop({"H"}), pop({"I", "D"})) With {
             .bounds = {50, 50},
             .ID = "HID",
-           .forward = New Controls With {.activation = pop({"B", "H"}).ToArray},
-           .reverse = New Controls With {.activation = pop({"A"}).ToArray}
+           .forward = New AdditiveControls With {.activation = pop({"B", "H"}).ToArray},
+           .reverse = New AdditiveControls With {.activation = pop({"A"}).ToArray}
        }
 
         Yield New Channel(pop({"I", "D"}), pop({"J", "B"})) With {
             .bounds = {50, 50},
             .ID = "IDJB",
-           .forward = New Controls With {.activation = pop({"A"}).ToArray},
-           .reverse = New Controls With {.activation = pop({"C"}).ToArray}
+           .forward = New AdditiveControls With {.activation = pop({"A"}).ToArray},
+           .reverse = New AdditiveControls With {.activation = pop({"C"}).ToArray}
        }
     End Function
 End Module
--- src/GCModeller/engine/Dynamics/test/unitTest.vb
+++ src/GCModeller/engine/Dynamics/test/unitTest.vb
@@ -1,50 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::30604fae967ece186ae8d7c14986c4c0, Dynamics\test\unitTest.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module unitTest
-    ' 
-    '     Sub: loopTest, Main, singleDirection
-    ' 
-    ' /********************************************************************************/
+' Module unitTest
+' 
+'     Sub: loopTest, Main, singleDirection
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Dynamics.Debugger
 Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Data.visualize.Network
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Core
 
 Module unitTest
@@ -62,22 +62,24 @@ Module unitTest
         Dim reaction As New Channel({New Variable(a, 1)}, {New Variable(b, 2)}) With {
             .bounds = {10, 500},
             .ID = "a->b",
-            .forward = 300,
-            .reverse = New Controls With {.baseline = 0.05, .activation = {New Variable(b, 1)}}
+            .forward = CType(300, AdditiveControls),
+            .reverse = New AdditiveControls With {.baseline = 0.05, .activation = {New Variable(b, 1)}}
         }
 
-        Dim machine As New Vessel() With {.Channels = {reaction}, .MassEnvironment = {a, b}}
+        Dim machine As Vessel = New Vessel().load({a, b}).load({reaction})
 
         machine.Initialize()
 
         Dim snapshots As New List(Of DataSet)
         Dim flux As New List(Of DataSet)
-
+        Dim dynamics = machine.ContainerIterator(100000)
 
         For i As Integer = 0 To 100000
+            Call dynamics.Tick()
+
             flux += New DataSet With {
                 .ID = i,
-                .Properties = machine.ContainerIterator().ToDictionary.FlatTable
+                .Properties = machine. .ToDictionary.FlatTable
             }
             snapshots += New DataSet With {
                 .ID = i,
@@ -100,34 +102,34 @@ Module unitTest
         Dim reaction As New Channel({New Variable(a, 1)}, {New Variable(b, 2)}) With {
             .bounds = {10, 500},
             .ID = "a->b",
-            .forward = 300,
-            .reverse = New Controls With {.baseline = 0.05, .activation = {New Variable(b, 1)}}
+            .forward = CType(300, AdditiveControls),
+            .reverse = New AdditiveControls With {.baseline = 0.05, .activation = {New Variable(b, 1)}}
         }
         Dim reaction2 As New Channel({New Variable(b, 1)}, {New Variable(c, 2)}) With {
             .bounds = {10, 500},
             .ID = "b->c",
-            .forward = 300,
-            .reverse = New Controls With {.baseline = 0.05, .activation = {New Variable(a, 1)}}
+            .forward = CType(300, AdditiveControls),
+            .reverse = New AdditiveControls With {.baseline = 0.05, .activation = {New Variable(a, 1)}}
         }
         Dim reaction3 As New Channel({New Variable(c, 4)}, {New Variable(a, 1)}) With {
             .bounds = {10, 500},
             .ID = "c->a",
-            .forward = 300,
-            .reverse = New Controls With {.baseline = 0.05, .activation = {New Variable(a, 0.01)}}
+            .forward = CType(300, AdditiveControls),
+            .reverse = New AdditiveControls With {.baseline = 0.05, .activation = {New Variable(a, 0.01)}}
         }
 
-        Dim machine As New Vessel() With {.Channels = {reaction, reaction2, reaction3}, .MassEnvironment = {a, b, c}}
+        Dim machine As Vessel = New Vessel().load({reaction, reaction2, reaction3}).load({a, b, c})
 
         machine.Initialize()
 
         Dim snapshots As New List(Of DataSet)
         Dim flux As New List(Of DataSet)
-
+        Dim dynamics = machine.ContainerIterator(100000)
 
         For i As Integer = 0 To 100000
             flux += New DataSet With {
                 .ID = i,
-                .Properties = machine.ContainerIterator().ToDictionary.FlatTable
+                .Properties = .ToDictionary.FlatTable
             }
             snapshots += New DataSet With {
                 .ID = i,
