9795897bb8b088318264c287965b0b2481d4d1de
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-08 16:34:51 +0800

    split data model source

--- src/runtime/sciBASIC#/Data_science/Mathematica/Math/ODE/Dynamics/var.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/ODE/Dynamics/var.vb
@@ -1,54 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::b75acdaf4878b02e2be9709b990eee97, Data_science\Mathematica\Math\ODE\Dynamics\var.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class var
-    ' 
-    '         Properties: Index, Name, Value
-    ' 
-    '         Constructor: (+5 Overloads) Sub New
-    ' 
-    '         Function: Clone, Evaluate, ToString
-    ' 
-    '         Sub: Assign
-    ' 
-    '         Operators: (+2 Overloads) <>, (+2 Overloads) =
-    ' 
-    '     Interface Ivar
-    ' 
-    '         Properties: value
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class var
+' 
+'         Properties: Index, Name, Value
+' 
+'         Constructor: (+5 Overloads) Sub New
+' 
+'         Function: Clone, Evaluate, ToString
+' 
+'         Sub: Assign
+' 
+'         Operators: (+2 Overloads) <>, (+2 Overloads) =
+' 
+'     Interface Ivar
+' 
+'         Properties: value
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -71,28 +71,21 @@ Namespace Dynamics
         Public Property Name As String Implements INamedValue.Key
         Public Overrides Property Value As Double Implements Ivar.value
 
-        Friend deSolve As Func(Of Double)
-
         Public Shared ReadOnly type As Type = GetType(var)
 
         Sub New()
         End Sub
 
-        Sub New(name$, value#, Optional solve As Func(Of Double) = Nothing)
+        Sub New(name$, value#)
             With Me
                 .Name = name
                 .Value = value
-                .deSolve = solve
             End With
         End Sub
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Sub New(name As String)
             Me.Name = name
-        End Sub
-
-        Sub New(solve As Func(Of Double))
-            deSolve = solve
         End Sub
 
         ''' <summary>
@@ -107,10 +100,6 @@ Namespace Dynamics
             End With
         End Sub
 
-        Public Function Evaluate() As Double
-            Return deSolve()
-        End Function
-
         Public Overrides Function ToString() As String
             Return $"Dim [{Index}] {Name} As System.Double = {Value}"
         End Function
@@ -118,15 +107,6 @@ Namespace Dynamics
         Public Overloads Shared Operator =(var As var, x As Double) As var
             var.Value = x
             Return var
-        End Operator
-
-        Public Overloads Shared Operator =(var As var, equation As Func(Of Double)) As var
-            var.deSolve = equation
-            Return var
-        End Operator
-
-        Public Overloads Shared Operator <>(var As var, equation As Func(Of Double)) As var
-            Throw New NotImplementedException
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
@@ -139,7 +119,7 @@ Namespace Dynamics
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Function Clone() As Object Implements ICloneable.Clone
+        Public Overridable Function Clone() As Object Implements ICloneable.Clone
             Return New var(Me)
         End Function
 
@@ -147,6 +127,36 @@ Namespace Dynamics
         Private Sub Assign(address As Integer) Implements IAddress(Of Integer).Assign
             Index = address
         End Sub
+    End Class
+
+    Public Class NonlinearVar : Inherits var
+
+        Friend deSolve As Func(Of Double)
+
+        Sub New(solve As Func(Of Double))
+            deSolve = solve
+        End Sub
+
+        Public Function Evaluate() As Double
+            Return deSolve()
+        End Function
+
+        Public Overrides Function Clone() As Object
+            Return New NonlinearVar(deSolve) With {
+                .Index = Index,
+                .Name = Name,
+                .Value = Value
+            }
+        End Function
+
+        Public Overloads Shared Operator =(var As NonlinearVar, equation As Func(Of Double)) As var
+            var.deSolve = equation
+            Return var
+        End Operator
+
+        Public Overloads Shared Operator <>(var As NonlinearVar, equation As Func(Of Double)) As var
+            Throw New NotImplementedException
+        End Operator
     End Class
 
     Public Interface Ivar : Inherits INamedValue
