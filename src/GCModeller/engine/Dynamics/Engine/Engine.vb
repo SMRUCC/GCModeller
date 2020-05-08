bbe44f05437cec9275601dc2eab979a4db8229ec
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-08 17:15:52 +0800

    apply RK4 solver engine

--- src/GCModeller/engine/Dynamics/Engine/Engine.vb
+++ src/GCModeller/engine/Dynamics/Engine/Engine.vb
@@ -1,48 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::6f2534c2db3d1d0f529236b3ed4a3396, Dynamics\Engine\Engine.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Engine
-    ' 
-    '         Properties: debugView, dynamics, initials, model, snapshot
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: AttachBiologicalStorage, getCore, GetMass, LoadModel, Run
-    ' 
-    '         Sub: loopInternal, Reset
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Engine
+' 
+'         Properties: debugView, dynamics, initials, model, snapshot
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: AttachBiologicalStorage, getCore, GetMass, LoadModel, Run
+' 
+'         Sub: loopInternal, Reset
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -50,6 +50,7 @@ Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar
 Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.Calculus.Dynamics
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Core
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Engine.Definitions
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Engine.ModelLoader
@@ -200,12 +201,14 @@ Namespace Engine
 
         Private Sub loopInternal(tick As Action(Of Integer))
             Dim flux As Dictionary(Of String, Double)
+            Dim engine As SolverIterator = core.ContainerIterator(iterations)
 
             For i As Integer = 0 To iterations
-                flux = core _
-                    .ContainerIterator() _
-                    .ToDictionary _
-                    .FlatTable
+                'flux = core _
+                '    .ContainerIterator() _
+                '    .ToDictionary _
+                '    .FlatTable
+                Call engine.Tick()
 
                 _snapshot = (mass.GetMassValues, flux)
 
