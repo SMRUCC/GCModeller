1e2c1b3bb1412ae69c0eac154fea64fa86ef4022
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-11 16:29:14 +0800

    unsure of the null reference error on linux platform

--- src/workbench/R#/demo/kegg/renderMap_CLI_url.R
+++ src/workbench/R#/demo/kegg/renderMap_CLI_url.R
@@ -4,33 +4,43 @@ imports ["kegg.repository", "report.utils"] from "kegg_kit";
 #
 # R# ./renderMap_CLI_url.R --xls ./associate.xls --export ./output
 
-let data as string      = (?"--xls"    || stop("no source data provided!")) :> read.csv(tsv = TRUE);
+let data                = (?"--xls"    || stop("no source data provided!")) :> read.csv(tsv = TRUE);
 let outputdir as string =  ?"--export" || "./";
+let repo as string      =  ?"--repo"   || "/etc/GCModeller/kegg_maps.zip";
 let maps as string      = data[, "KEGG"];
 let urls as string      = data[, "Pathway_links"];
 
-using mapZip as open.zip("/etc/GCModeller/kegg_maps.zip") {
+str(data);
+
+using mapZip as open.zip(repo) {
+	let allMaps as string = as.object(mapZip)$ls;
 	let doRender as function(mapId, url) {
 		# parse parameters and re-format the 
 		# commandline argument inputs:
 		mapId <- str_pad(as.integer($"\d+"(mapId)), 5, "left", "0");
 		mapId <- `map${mapId}.XML`;
 		
-		# render result as html output
-		mapZip[[mapId]] 
-		:> loadMap 
-		:> as.object
-		:> keggMap.reportHtml(url)
-		:> writeLines(con = `${outputdir}/${basename(mapId)}.html`)
-		;
+		print(mapId);
+		
+		if (mapId in allMaps) {
+			# render result as html output
+			mapZip[[mapId]] 
+			:> loadMap 
+			:> as.object
+			:> keggMap.reportHtml(url)
+			:> writeLines(con = `${outputdir}/${basename(mapId)}.html`)
+			;
 
-		# just render image file 
-		mapZip[[mapId]]
-		:> loadMap 
-		:> as.object
-		:> keggMap.highlights(url)
-		:> save.graphics(file = `${outputdir}/${basename(mapId)}.png`)
-		;	
+			# just render image file 
+			mapZip[[mapId]]
+			:> loadMap 
+			:> as.object
+			:> keggMap.highlights(url)
+			:> save.graphics(file = `${outputdir}/${basename(mapId)}.png`)
+			;	
+		} else {
+			warning(`map ${mapId} is not exists in repository...`);
+		}	
 	}
 
 	for(i in 1:length(maps)) {	
