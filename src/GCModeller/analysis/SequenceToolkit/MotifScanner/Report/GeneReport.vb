fd8a0cc0ef0fa200e075914ead6ecff8117b4798
xieguigang <xie.guigang@gcmodeller.org>
2020-05-16 21:08:03 +0800

    add promoter search report model

---
+++ src/GCModeller/analysis/SequenceToolkit/MotifScanner/Report/GeneReport.vb
@@ -0,0 +1,36 @@
+﻿Public Class GeneReport
+
+    ''' <summary>
+    ''' the promoter region associated gene id
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property locus_tag As String
+    ''' <summary>
+    ''' the length of the input promoter region
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property length As Integer
+    Public Property threshold As Double
+    Public Property numOfPromoters As Integer
+    Public Property promoterPos As Integer
+    Public Property promoterPosLDF As Double
+    Public Property components As promoterPos()
+    Public Property tfBindingSites As TFBindingSite()
+
+End Class
+
+Public Class TFBindingSite
+    Public Property regulator As String
+    Public Property oligonucleotides As String
+    Public Property position As Integer
+    Public Property score As Double
+End Class
+
+Public Class promoterPos
+
+    Public Property type As String
+    Public Property pos As Integer
+    Public Property oligonucleotides As String
+    Public Property score As Double
+
+End Class
