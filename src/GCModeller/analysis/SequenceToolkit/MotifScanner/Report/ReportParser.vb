925eae7619d4c7984dfbaae3b6dac95feac02940
xieguigang <xie.guigang@gcmodeller.org>
2020-05-16 21:58:25 +0800

    report data parser

---
+++ src/GCModeller/analysis/SequenceToolkit/MotifScanner/Report/ReportParser.vb
@@ -0,0 +1,12 @@
+﻿Public Module ReportParser
+
+    Public Iterator Function ParseReport(text As String) As IEnumerable(Of GeneReport)
+        For Each section As String In text.StringSplit("^[>]", , RegexICMul)
+            Yield ParseSectionData(section)
+        Next
+    End Function
+
+    Private Function ParseSectionData(text As String) As GeneReport
+
+    End Function
+End Module
