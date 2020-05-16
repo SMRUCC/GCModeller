0bef5e9365db15e083454a154b3c7f3e99c43047
xieguigang <xie.guigang@gcmodeller.org>
2020-05-16 22:45:40 +0800

    script api for parse data models

--- src/GCModeller/analysis/SequenceToolkit/MotifScanner/Report/ReportParser.vb
+++ src/GCModeller/analysis/SequenceToolkit/MotifScanner/Report/ReportParser.vb
@@ -1,12 +1,18 @@
 ﻿Public Module ReportParser
 
     Public Iterator Function ParseReport(text As String) As IEnumerable(Of GeneReport)
-        For Each section As String In text.StringSplit("^[>]", , RegexICMul)
+        For Each section As String() In text _
+            .LineTokens _
+            .Split(
+                delimiter:=Function(l) l.FirstOrDefault = ">"c,
+                deliPosition:=DelimiterLocation.NextFirst
+            )
+
             Yield ParseSectionData(section)
         Next
     End Function
 
-    Private Function ParseSectionData(text As String) As GeneReport
+    Private Function ParseSectionData(lines As String()) As GeneReport
 
     End Function
 End Module
