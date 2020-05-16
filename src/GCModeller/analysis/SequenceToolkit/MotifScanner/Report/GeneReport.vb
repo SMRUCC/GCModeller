731e41b8129b24f6c7d0e90554d77b44956a1137
xieguigang <xie.guigang@gcmodeller.org>
2020-05-17 00:11:52 +0800

    improvements of the report xml output layout

--- src/GCModeller/analysis/SequenceToolkit/MotifScanner/Report/GeneReport.vb
+++ src/GCModeller/analysis/SequenceToolkit/MotifScanner/Report/GeneReport.vb
@@ -1,29 +1,31 @@
-﻿Public Class GeneReport
+﻿Imports System.Xml.Serialization
+
+Public Class GeneReport
 
     ''' <summary>
     ''' the promoter region associated gene id
     ''' </summary>
     ''' <returns></returns>
-    Public Property locus_tag As String
+    <XmlAttribute> Public Property locus_tag As String
     ''' <summary>
     ''' the length of the input promoter region
     ''' </summary>
     ''' <returns></returns>
     Public Property length As Integer
     Public Property threshold As Double
-    Public Property numOfPromoters As Integer
+    <XmlAttribute> Public Property numOfPromoters As Integer
     Public Property promoterPos As Integer
     Public Property promoterPosLDF As Double
-    Public Property components As PromoterComponent()
-    Public Property tfBindingSites As TFBindingSite()
+    <XmlElement> Public Property components As PromoterComponent()
+    <XmlElement> Public Property tfBindingSites As TFBindingSite()
 
 End Class
 
 Public Class TFBindingSite
-    Public Property regulator As String
-    Public Property oligonucleotides As String
-    Public Property position As Integer
-    Public Property score As Double
+    <XmlAttribute> Public Property regulator As String
+    <XmlText> Public Property oligonucleotides As String
+    <XmlAttribute> Public Property position As Integer
+    <XmlAttribute> Public Property score As Double
 
     Public Overrides Function ToString() As String
         Return $"       {regulator}:  {oligonucleotides} at position      {position} Score -  {score}"
@@ -32,10 +34,10 @@ End Class
 
 Public Class PromoterComponent
 
-    Public Property type As String
-    Public Property pos As Integer
-    Public Property oligonucleotides As String
-    Public Property score As Double
+    <XmlAttribute> Public Property type As String
+    <XmlAttribute> Public Property pos As Integer
+    <XmlText> Public Property oligonucleotides As String
+    <XmlAttribute> Public Property score As Double
 
     Public Overrides Function ToString() As String
         Return $" {type} at pos.     {pos} {oligonucleotides} Score    {score}"
--- src/workbench/R#/demo/regprecise/report.R
+++ src/workbench/R#/demo/regprecise/report.R
@@ -3,4 +3,6 @@ imports "TRN.builder" from "phenotype_kit";
 "K:\20200226\20200516_gbk\X101SC19112292-Z01-J001_result\res.txt"
 :> readText
 :> as.promoter.models
+:> xml
+:> cat
 ;
