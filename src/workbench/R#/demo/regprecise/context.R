0e64f97265236d1353a7c6221c1dad67bfd83f17
xieguigang <xie.guigang@gcmodeller.org>
2020-05-17 13:43:33 +0800

    Update context.R

--- src/workbench/R#/demo/regprecise/context.R
+++ src/workbench/R#/demo/regprecise/context.R
@@ -1,13 +1,13 @@
 imports "annotation.genomics_context" from "seqtoolkit";
-imports "annotation.genomics_context" from "seqtoolkit";
+imports "bioseq.fasta" from "seqtoolkit";
 
 let nt = read.seq("K:\20200226\20200516_gbk\Yersinia pseudotuberculosis (Pfeiffer) Smith and Thal.gbk");
 
 let debug as function(a, b) {
-	print(summary(context(a, 200), nt = nt));
-	print(summary(context(b, 200), nt = nt));
+	print(summary(context(a, 100), nt = nt));
+	print(summary(context(b, 100), nt = nt));
 
 	print(relationship(a, b));
 }
 
-debug(location(5000, 8000, "+"), location(4950, 5100, "+"));
+debug(location(5000, 5200, "+"), location(4950, 5100, "+"));
