eddab4b0b28858cead3af7bf2d3f57933494526b
xieguigang <xie.guigang@gcmodeller.org>
2020-05-17 14:20:08 +0800

    Update save_promoter2gbk.R

--- src/workbench/R#/demo/regprecise/save_promoter2gbk.R
+++ src/workbench/R#/demo/regprecise/save_promoter2gbk.R
@@ -4,7 +4,25 @@ imports ["annotation.genbank_kit", "annotation.workflow"] from "seqtoolkit";
 let regions = "K:\20200226\20200516_gbk\X101SC19112292-Z01-J001_result\res.txt"
 :> readText
 :> as.promoter.models
-:> lapply(a => a, names = as.object(a)$locus_tag)
+:> lapply(a => a, names = a => as.object(a)$locus_tag)
 ;
 
-str(regions);
+# str(regions);
+
+let mapping = "K:\20200226\TRN\1025_mapping.csv"
+:> open.stream(type = "Mapping", ioRead = TRUE)
+:> projectAs(as.object)
+:> groupBy(a -> a$Reads.Query)
+:> lapply(function(a) {
+	if (length(a) == 1) {
+		a[1]$Reference;
+	} else {
+		let ref = sapply(a, x -> x$Reference);
+		let scores = sapply(a, x -> x$"Score(bits)");
+		
+		ref[which.max(scores)];
+	}
+	
+}, names = g -> g$key);
+
+str(mapping);
