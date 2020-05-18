1b75a11514cd9371b8998fc15e03a009948d03dc
xieguigang <xie.guigang@gcmodeller.org>
2020-05-18 19:32:04 +0800

    fix id mapping

--- src/workbench/R#/demo/regprecise/save_promoter2gbk.R
+++ src/workbench/R#/demo/regprecise/save_promoter2gbk.R
@@ -7,17 +7,17 @@ let regions = "K:\20200226\20200516_gbk\X101SC19112292-Z01-J001_result\res.txt"
 :> lapply(a => a, names = a => as.object(a)$locus_tag)
 ;
 
-# str(regions);
+print(names(regions));
 
 let mapping = "K:\20200226\TRN\1025_mapping.csv"
 :> open.stream(type = "Mapping", ioRead = TRUE)
 :> projectAs(as.object)
-:> groupBy(a -> a$Reads.Query)
+:> groupBy(a -> a$Reference)
 :> lapply(function(a) {
 	if (length(a) == 1) {
-		a[1]$Reference;
+		a[1]$Reads.Query;
 	} else {
-		let ref = sapply(a, x -> x$Reference);
+		let ref = sapply(a, x -> x$Reads.Query);
 		let scores = sapply(a, x -> x$"Score(bits)");
 		
 		ref[which.max(scores)];
@@ -35,7 +35,13 @@ let genes = templateGenbank :> enumerateFeatures :> projectAs(as.object) :> whic
 
 for(a in genes) {
 	let location = as.object(a$Location)$ContiguousRegion;
-	let id = a$Query("db_xref");
+	let id = mapping[[a$Query("db_xref")]];
+	let promoter = regions[[id]];
 	
-	print(id);
+	if (is.null(promoter)) {
+		next;
+	}
+	
+	# print(id);
+	print(promoter);
 }
