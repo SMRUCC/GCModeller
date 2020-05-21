e80378db1e704670a2e9342119f1789efdac686b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-21 13:35:34 +0800

    populate signal regions

--- src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/SignalProcessing/PeakFinding/Algorithm.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/SignalProcessing/PeakFinding/Algorithm.vb
@@ -1,7 +1,89 @@
-﻿Namespace PeakFinding
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm.base
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.Scripting
+Imports stdNum = System.Math
 
-    Public Class Algorithm
+Namespace PeakFinding
 
+    ''' <summary>
+    ''' 通过累加线的高度落差进行峰识别
+    ''' 
+    ''' 1. 首先计算出信号的累加线
+    ''' 2. 然后删除所有的坡度小于给定角度值的平行段
+    ''' 3. 剩下的片段就是信号峰对应的时间s区间
+    ''' </summary>
+    Public Class ElevationAlgorithm
+
+        ReadOnly cos_angle As Double
+        ReadOnly baseline_quantile As Double
+
+        Sub New(angle As Double, baselineQuantile As Double)
+            Me.cos_angle = stdNum.Cos(angle)
+            Me.baseline_quantile = baselineQuantile
+        End Sub
+
+        Public Iterator Function FindAllSignalPeaks(signals As IEnumerable(Of ITimeSignal)) As IEnumerable(Of SeqValue(Of ITimeSignal()))
+            Dim data As ITimeSignal() = signals.OrderBy(Function(t) t.time).ToArray
+            Dim baseline As Double = data.SignalBaseline(baseline_quantile)
+            Dim line As PointF() = data.AccumulateLine(baseline)
+            ' 计算出角度
+            Dim angles As PointF() = cos(line).ToArray
+            ' 删掉所有角度低于阈值的片段
+            ' 剩下所有递增的坡度片段
+            Dim slopes As SeqValue(Of PointF())() = filterByCosAngles(angles).ToArray
+            Dim rawSignals As IVector(Of ITimeSignal) = data.Shadows
+
+        End Function
+
+        Private Iterator Function filterByCosAngles(angles As PointF()) As IEnumerable(Of SeqValue(Of PointF()))
+            Dim buffer As New List(Of PointF)
+            Dim i As i32 = 0
+
+            For Each a As PointF In angles
+                If a.Y <= cos_angle Then
+                    If buffer > 0 Then
+                        Yield New SeqValue(Of PointF()) With {
+                            .i = ++i,
+                            .value = buffer.PopAll
+                        }
+                    End If
+                Else
+                    buffer += a
+                End If
+            Next
+
+            If buffer > 0 Then
+                Yield New SeqValue(Of PointF()) With {
+                    .i = ++i,
+                    .value = buffer.PopAll
+                }
+            End If
+        End Function
+
+        Private Iterator Function cos(line As PointF()) As IEnumerable(Of PointF)
+            Dim A As PointF
+            Dim B As PointF
+            Dim cosB, cosC As Double
+
+            For Each con As SlideWindow(Of PointF) In line.SlideWindows(winSize:=2, offset:=1)
+                If con.Length = 2 Then
+                    A = con.First
+                    B = con.Last
+                    cosB = B.X - A.X
+                    cosC = EuclideanDistance(New Double() {A.X, A.Y}, New Double() {B.X, B.Y})
+
+                    '      B
+                    '     /|
+                    '    / |
+                    '   /  |
+                    ' A ---- X
+
+                    Yield New PointF(A.X, cosB / cosC)
+                End If
+            Next
+        End Function
 
     End Class
 End Namespace
--- src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/SignalProcessing/PeakFinding/Implement.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/SignalProcessing/PeakFinding/Implement.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Drawing
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Math.Quantile
 
 Namespace PeakFinding
 
@@ -11,10 +12,16 @@ Namespace PeakFinding
         ' 则可以利用这个特性将色谱峰给识别出来
         ' 这个方法仅局限于色谱峰都是各自相互独立的情况之下
 
+        ''' <summary>
+        ''' 通过这个函数得到的累加线是一个单调递增曲线
+        ''' </summary>
+        ''' <param name="signals">应该是按照时间升序排序过了的</param>
+        ''' <param name="baseline"></param>
+        ''' <returns></returns>
         <Extension>
-        Public Function AccumulateLine(chromatogram As IEnumerable(Of ITimeSignal), baseline#) As PointF()
+        Public Function AccumulateLine(signals As ITimeSignal(), baseline As Double) As PointF()
             Dim accumulate#
-            Dim sumALL# = Aggregate t As ITimeSignal In chromatogram
+            Dim sumALL# = Aggregate t As ITimeSignal In signals
                           Let x As Double = t.intensity - baseline
                           Where x > 0
                           Into Sum(x)
@@ -24,13 +31,28 @@ Namespace PeakFinding
                     accumulate += If(into < 0, 0, into)
                     Return (accumulate / sumALL) * 100
                 End Function
-            Dim accumulates As PointF() = chromatogram _
+            Dim accumulates As PointF() = signals _
                 .Select(Function(tick)
                             Return New PointF(tick.time, ay(tick.intensity))
                         End Function) _
                 .ToArray
 
             Return accumulates
+        End Function
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="signals"></param>
+        ''' <param name="quantile">一般建议值为0.65</param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function SignalBaseline(signals As ITimeSignal(), quantile As Double) As Double
+            Dim allIntensity As Double() = signals.Select(Function(t) t.intensity).ToArray
+            Dim q As QuantileEstimationGK = allIntensity.GKQuantile
+            Dim baseline As Double = q.Query(quantile)
+
+            Return baseline
         End Function
     End Module
 End Namespace
