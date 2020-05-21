1c902ec2757a95df533822f043a49ccc5c6aed1d
xieguigang <xie.guigang@gcmodeller.org>
2020-05-21 21:48:10 +0800

    update script

--- src/workbench/workbench/index.js
+++ src/workbench/workbench/index.js
@@ -9,27 +9,27 @@ var workbench;
          * When splashscreen was shown.
          * @ignore
          */
-        let splashScreenTimestamp = 0;
+        var splashScreenTimestamp = 0;
         /**
          * Splashscreen is loaded and ready to show.
          * @ignore
          */
-        let splashScreenReady = false;
+        var splashScreenReady = false;
         /**
          * Main window has been loading for a min amount of time.
          * @ignore
          */
-        let slowStartup = false;
+        var slowStartup = false;
         /**
          * True when expected work is complete and we've closed splashscreen, else user prematurely closed splashscreen.
          * @ignore
          */
-        let done = false;
+        var done = false;
         /**
          * Show splashscreen if criteria are met.
          * @ignore
          */
-        const showSplash = () => {
+        var showSplash = function () {
             if (splashScreen && splashScreenReady && slowStartup) {
                 splashScreen.show();
                 splashScreenTimestamp = Date.now();
@@ -39,10 +39,10 @@ var workbench;
          * Close splashscreen / show main screen. Ensure screen is visible for a min amount of time.
          * @ignore
          */
-        const closeSplashScreen = (main, min) => {
+        var closeSplashScreen = function (main, min) {
             if (splashScreen) {
-                const timeout = min - (Date.now() - splashScreenTimestamp);
-                setTimeout(() => {
+                var timeout = min - (Date.now() - splashScreenTimestamp);
+                setTimeout(function () {
                     done = true;
                     if (splashScreen) {
                         splashScreen.isDestroyed() || splashScreen.close(); // Avoid `Error: Object has been destroyed` (#19)
@@ -56,14 +56,14 @@ var workbench;
          * The actual splashscreen browser window.
          * @ignore
          */
-        let splashScreen;
+        var splashScreen;
         /**
          * Initializes a splashscreen that will show/hide smartly (and handle show/hiding of main window).
          * @param config - Configures splashscren
          * @returns {BrowserWindow} the main browser window ready for loading
          */
-        view.initSplashScreen = (config) => {
-            const xConfig = {
+        view.initSplashScreen = function (config) {
+            var xConfig = {
                 windowOpts: config.windowOpts,
                 templateUrl: config.templateUrl,
                 splashScreenOpts: config.splashScreenOpts,
@@ -75,23 +75,23 @@ var workbench;
             xConfig.splashScreenOpts.center = true;
             xConfig.splashScreenOpts.show = false;
             xConfig.windowOpts.show = false;
-            const window = new BrowserWindow(xConfig.windowOpts);
+            var window = new BrowserWindow(xConfig.windowOpts);
             splashScreen = new BrowserWindow(xConfig.splashScreenOpts);
-            splashScreen.loadURL(`file://${xConfig.templateUrl}`);
-            xConfig.closeWindow && splashScreen.on("close", () => {
+            splashScreen.loadURL("file://" + xConfig.templateUrl);
+            xConfig.closeWindow && splashScreen.on("close", function () {
                 done || window.close();
             });
             // Splashscreen is fully loaded and ready to view.
-            splashScreen.webContents.on("did-finish-load", () => {
+            splashScreen.webContents.on("did-finish-load", function () {
                 splashScreenReady = true;
                 showSplash();
             });
             // Startup is taking enough time to show a splashscreen.
-            setTimeout(() => {
+            setTimeout(function () {
                 slowStartup = true;
                 showSplash();
             }, xConfig.delay);
-            window.webContents.on("did-finish-load", () => {
+            window.webContents.on("did-finish-load", function () {
                 closeSplashScreen(window, xConfig.minVisible);
             });
             return window;
@@ -103,7 +103,7 @@ var workbench;
          * @param config - Configures splashscren
          * @returns {DynamicSplashScreen} the main browser window and the created splashscreen
          */
-        view.initDynamicSplashScreen = (config) => {
+        view.initDynamicSplashScreen = function (config) {
             return {
                 main: view.initSplashScreen(config),
                 // initSplashScreen initializes splashscreen so this is a safe cast.
@@ -117,13 +117,14 @@ var workbench;
     var view;
     (function (view) {
         function renderAppMenu(template) {
-            const menu = Menu.buildFromTemplate(runRender(template));
+            var menu = Menu.buildFromTemplate(runRender(template));
             Menu.setApplicationMenu(menu);
             return menu;
         }
         view.renderAppMenu = renderAppMenu;
         function runRender(template) {
-            for (let item of template) {
+            for (var _i = 0, template_1 = template; _i < template_1.length; _i++) {
+                var item = template_1[_i];
                 renderMenuTemplate(item);
             }
             return template;
@@ -134,16 +135,16 @@ var workbench;
         function renderMenuTemplate(templ) {
             if (!(templ.submenu instanceof Menu)) {
                 templ.submenu
-                    .filter(sm => "click" in sm)
-                    .forEach(sm => {
+                    .filter(function (sm) { return "click" in sm; })
+                    .forEach(function (sm) {
                     var url = sm.click + "";
                     sm.click = function () {
                         require('electron').shell.openExternal(url);
                     };
                 });
                 templ.submenu
-                    .filter(sm => sm.label == "Quit")
-                    .forEach(sm => {
+                    .filter(function (sm) { return sm.label == "Quit"; })
+                    .forEach(function (sm) {
                     sm.click = function () {
                         app.quit();
                     };
@@ -159,7 +160,7 @@ var workbench;
     (function (view_1) {
         // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
         // 垃圾回收的时候，window对象将会自动的关闭
-        let windows = {};
+        var windows = {};
         function getMainWindow() {
             if (mainView in windows) {
                 return windows[mainView];
@@ -169,10 +170,14 @@ var workbench;
             }
         }
         view_1.getMainWindow = getMainWindow;
-        function createWindow(view, size = [800, 600], callback = null, lambda = false, debug = false) {
-            let invoke = function () {
+        function createWindow(view, size, callback, lambda, debug) {
+            if (size === void 0) { size = [800, 600]; }
+            if (callback === void 0) { callback = null; }
+            if (lambda === void 0) { lambda = false; }
+            if (debug === void 0) { debug = false; }
+            var invoke = function () {
                 // 创建浏览器窗口。
-                let win = new BrowserWindow({ width: size[0], height: size[1] });
+                var win = new BrowserWindow({ width: size[0], height: size[1] });
                 // 然后加载应用的 index.html。
                 win.loadFile(view);
                 if (debug) {
@@ -180,7 +185,7 @@ var workbench;
                     win.webContents.openDevTools();
                 }
                 // 当 window 被关闭，这个事件会被触发。
-                win.on('closed', () => {
+                win.on('closed', function () {
                     // 取消引用 window 对象，如果你的应用支持多窗口的话，
                     // 通常会把多个 window 对象存放在一个数组里面，
                     // 与此同时，你应该删除相应的元素。
@@ -202,21 +207,21 @@ var workbench;
     var Shell;
     (function (Shell) {
         function Rweb() {
-            let spawn = require("child_process").spawn;
-            let bat = spawn("Rweb", [
+            var spawn = require("child_process").spawn;
+            var bat = spawn("Rweb", [
                 "--session",
                 "--port", "7452",
                 "--workspace", "./Rweb",
                 "--show_error",
                 "--n_threads", "2"
             ]);
-            bat.stdout.on("data", (data) => {
+            bat.stdout.on("data", function (data) {
                 // Handle data...
             });
-            bat.stderr.on("data", (err) => {
+            bat.stderr.on("data", function (err) {
                 // Handle error...
             });
-            bat.on("exit", (code) => {
+            bat.on("exit", function (code) {
                 // Handle exit
             });
             return bat;
@@ -224,9 +229,9 @@ var workbench;
         Shell.Rweb = Rweb;
         function initialize() {
             // 导入Rstudio环境诊断组件
-            let request = require('request');
-            let script = "imports 'diagnostics' from 'Rstudio';";
-            request(`http://127.0.0.1:7452/exec?script=${encodeURIComponent(script)}`, function (error, response, body) {
+            var request = require('request');
+            var script = "imports 'diagnostics' from 'Rstudio';";
+            request("http://127.0.0.1:7452/exec?script=" + encodeURIComponent(script), function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     console.log(body);
                 }
@@ -238,7 +243,7 @@ var workbench;
 var workbench;
 (function (workbench) {
     function osd() {
-        let msg = new Notification({
+        var msg = new Notification({
             title: "Task Finish",
             body: "test task finished!"
         });
@@ -254,24 +259,24 @@ var workbench;
 /// <reference path="dev/osd.ts" />
 /// <reference path="node_modules/electron/electron.d.ts" />
 // load framework
-const { app, BrowserWindow, Menu, Notification } = require('electron');
+var _a = require('electron'), app = _a.app, BrowserWindow = _a.BrowserWindow, Menu = _a.Menu, Notification = _a.Notification;
 // import { app, BrowserWindow, Menu, Notification } from "electron";
 // import * as path from "path";
 // import * as url from "url";
 // const { path } = require('path');
-const mainView = require('path').join(__dirname, "views", "index.html");
+var mainView = require('path').join(__dirname, "views", "index.html");
 // start the R# backend environment
-const backend = workbench.Shell.Rweb();
-const defaultViewSize = [1440, 900];
+var backend = workbench.Shell.Rweb();
+var defaultViewSize = [1440, 900];
 // load internal app components
-let template = require("./menu.json");
-let menu = null;
-let mainWindow;
+var template = require("./menu.json");
+var menu = null;
+var mainWindow;
 // Electron 会在初始化后并准备
 // 创建浏览器窗口时，调用这个函数。
 // 部分 API 在 ready 事件触发后才能使用。
-app.on("ready", () => {
-    const windowOptions = {
+app.on("ready", function () {
+    var windowOptions = {
         width: defaultViewSize[0],
         height: defaultViewSize[1],
         show: false,
@@ -287,7 +292,7 @@ app.on("ready", () => {
             transparent: false,
         },
     });
-    mainWindow.loadURL(`file://${mainView}`);
+    mainWindow.loadURL("file://" + mainView);
     menu = workbench.view.renderAppMenu(template);
     // 在这个文件中，你可以续写应用剩下主进程代码。
     // 也可以拆分成几个文件，然后用 require 导入。
