4d3a003beb556ebad84e36b6222b08f6d7280ae6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-27 11:47:53 +0800

    create matrix from tabular data

--- src/runtime/sciBASIC#/Data_science/Mathematica/Math/DataFrame/Correlation/Builder.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/DataFrame/Correlation/Builder.vb
@@ -33,4 +33,58 @@ Public Module Builder
 
         Return New DistanceMatrix(keys.Indexing, matrix)
     End Function
+
+    <Extension>
+    Public Function FromTabular(Of DataSet As {INamedValue, DynamicPropertyBase(Of String)})(data As IEnumerable(Of DataSet),
+                                                                                             Optional item1$ = "A",
+                                                                                             Optional item2$ = "B",
+                                                                                             Optional correlation$ = "correlation") As DistanceMatrix
+        Dim allData As DataSet() = data.ToArray
+        Dim names As New List(Of String)
+        Dim hash As New Dictionary(Of String, Dictionary(Of String, Double))
+        Dim a, b As String
+        Dim c As Double
+
+        For Each row As DataSet In allData
+            a = row.Properties(item1)
+            b = row.Properties(item2)
+            c = row.Properties(correlation)
+
+            Call names.Add(a)
+            Call names.Add(b)
+
+            If Not hash.ContainsKey(a) Then
+                Call hash.Add(a, New Dictionary(Of String, Double))
+            End If
+            If Not hash.ContainsKey(b) Then
+                Call hash.Add(b, New Dictionary(Of String, Double))
+            End If
+            If Not hash(a).ContainsKey(b) Then
+                Call hash(a).Add(b, c)
+            End If
+            If Not hash(b).ContainsKey(a) Then
+                Call hash(b).Add(a, c)
+            End If
+        Next
+
+        With names.Distinct.Indexing
+            Return .DoCall(Function(index)
+                               Dim matrix As Double()() = index.Objects _
+                                  .Select(Function(name)
+                                              Return index.Objects _
+                                                  .Select(Function(name2)
+                                                              If hash(name).ContainsKey(name2) Then
+                                                                  Return hash(name)(name2)
+                                                              Else
+                                                                  Return 0
+                                                              End If
+                                                          End Function) _
+                                                  .ToArray
+                                          End Function) _
+                                  .ToArray
+
+                               Return New DistanceMatrix(index, matrix)
+                           End Function)
+        End With
+    End Function
 End Module
