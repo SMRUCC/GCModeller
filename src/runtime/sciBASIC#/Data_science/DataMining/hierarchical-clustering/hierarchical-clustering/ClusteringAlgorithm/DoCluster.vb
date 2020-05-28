4b1ba88af6b9b20713385092477bbff1fd7f5890
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-28 17:12:26 +0800

    code style improvements

--- src/runtime/sciBASIC#/Data_science/DataMining/hierarchical-clustering/hierarchical-clustering/ClusteringAlgorithm/DoCluster.vb
+++ src/runtime/sciBASIC#/Data_science/DataMining/hierarchical-clustering/hierarchical-clustering/ClusteringAlgorithm/DoCluster.vb
@@ -1,68 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::afe70abdb84d742297b6a4cdd2057bb0, Data_science\DataMining\hierarchical-clustering\hierarchical-clustering\DoCluster.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module DoCluster
-    ' 
-    '     Function: DistanceMatrix, RunCluster
-    ' 
-    ' /********************************************************************************/
+' Module DoCluster
+' 
+'     Function: DistanceMatrix, RunCluster
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.Data.csv.IO
-Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Language.Default
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.DataFrame
 
 Public Module DoCluster
 
-    <Extension>
-    Public Function DistanceMatrix(objects As IEnumerable(Of DataSet)) As Double()()
-        Dim list = objects.ToArray
-        Dim keys = list _
-            .Select(Function(obj) obj.Properties.Keys) _
-            .IteratesALL _
-            .Distinct _
-            .ToArray
-
-        Return list _
-            .Select(Function(x)
-                        Return list _
-                            .Select(Function(y) x.EuclideanDistance(y, keys)) _
-                            .ToArray
-                    End Function) _
-            .ToArray
-    End Function
+    Public ReadOnly Property DefaultClusteringAlgorithm As [Default](Of DefaultClusteringAlgorithm) = New DefaultClusteringAlgorithm
+    Public ReadOnly Property DefaultLinkageStrategy As [Default](Of LinkageStrategy) = New AverageLinkageStrategy()
 
     ''' <summary>
     ''' Run hierarchical clustering
@@ -72,23 +59,19 @@ Public Module DoCluster
     ''' <param name="linkageStrategy">Default is <see cref="AverageLinkageStrategy"/></param>
     ''' <returns></returns>
     <Extension>
-    Public Function RunCluster(objects As IEnumerable(Of DataSet),
+    Public Function RunCluster(Of DataSet As {INamedValue, DynamicPropertyBase(Of Double)})(objects As IEnumerable(Of DataSet),
                                Optional algorithm As ClusteringAlgorithm = Nothing,
                                Optional linkageStrategy As LinkageStrategy = Nothing) As Cluster
 
-        Dim list = objects.ToArray
-        Dim distances = list.DistanceMatrix
-        Dim names$() = list.Keys
+        Dim dist As DistanceMatrix = objects.Euclidean
+        Dim distances As Double()() = dist.PopulateRows.Select(Function(r) r.ToArray).ToArray
+
+        linkageStrategy = linkageStrategy Or DefaultLinkageStrategy
 
         ' with (algorithm or new DefaultClusteringAlgorithm as default) if algorithm is nothing
-        With algorithm Or New DefaultClusteringAlgorithm().AsDefault ' (Function(alg) alg Is Nothing)
-
+        With algorithm Or DefaultClusteringAlgorithm  ' (Function(alg) alg Is Nothing)
             ' using (linkageStrategy or new AverageLinkageStrategy as default) if linkageStrategy is nothing
-            Dim cluster As Cluster = .performClustering(
-                distances,
-                names,
-                linkageStrategy Or New AverageLinkageStrategy().AsDefault)
-
+            Dim cluster As Cluster = .performClustering(distances, dist.keys, linkageStrategy)
             Return cluster
         End With
     End Function
