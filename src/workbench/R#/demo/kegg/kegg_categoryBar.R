ae9b116e4b681c45c30964a3ea379398c1788f9d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-29 12:17:11 +0800

    improvements of the kegg profiles plot api

---
+++ src/workbench/R#/demo/kegg/kegg_categoryBar.R
@@ -0,0 +1,24 @@
+imports "visualkit.plots" from "visualkit";
+
+let raw = read.csv(?"pathway_results");
+
+print(head(raw));
+
+let KO = unlist( $"\d+"(as.character(raw[, "pathway"])));
+let p = -log( as.numeric(raw[, "Raw p"]), 10);
+
+# str(KO);
+
+# print(p);
+
+let profiles = lapply(1:length(KO), i -> p[i], names = i-> KO[i]);
+
+str(profiles);
+
+profiles 
+:> kegg.category_profiles.plot(
+	title = "KEGG pathway enrichment",
+	 axisTitle = "-log10(Raw p)"
+)
+:> save.graphics(file = "./barplot.png")
+;
--- src/workbench/R#/visualkit/visualPlot.vb
+++ src/workbench/R#/visualkit/visualPlot.vb
@@ -1,51 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::79a539985f9dd52d121d7b7d090bce1b, R#\visualkit\visualPlot.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module visualPlot
-    ' 
-    '     Function: KEGGCategoryProfilePlots
-    ' 
-    ' /********************************************************************************/
+' Module visualPlot
+' 
+'     Function: KEGGCategoryProfilePlots
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Imaging.Driver
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Assembly.KEGG.DBGET.BriteHEntry
 Imports SMRUCC.genomics.Visualize.CatalogProfiling
 Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Runtime.Interop
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 <Package("visualkit.plots")>
 Module visualPlot
@@ -77,23 +81,29 @@ Module visualPlot
         Dim profile As Dictionary(Of String, NamedValue(Of Double)())
 
         If TypeOf profiles Is Dictionary(Of String, Integer) Then
-            profile = profiles _
-                .KEGGCategoryProfiles _
-                .ToDictionary(Function(p) p.Key,
-                              Function(group)
-                                  Return group.Value _
-                                     .OrderByDescending(Function(t) t.Value) _
-                                     .Take(displays) _
-                                     .ToArray
-                              End Function)
+            profile = DirectCast(profiles, Dictionary(Of String, Integer)) _
+                .ToDictionary(Function(a) a.Key,
+                              Function(a)
+                                  Return CDbl(a.Value)
+                              End Function) _
+                .doKeggProfiles(displays)
         ElseIf TypeOf profiles Is Dictionary(Of String, NamedValue(Of Double)()) Then
             profile = DirectCast(profiles, Dictionary(Of String, NamedValue(Of Double)()))
+        ElseIf TypeOf profiles Is Dictionary(Of String, Double) Then
+            profile = DirectCast(profiles, Dictionary(Of String, Double)).doKeggProfiles(displays)
+        ElseIf TypeOf profiles Is list Then
+            profile = DirectCast(profiles, list).slots _
+                .ToDictionary(Function(a) a.Key,
+                              Function(a)
+                                  Return CDbl(REnv.asVector(Of Double)(a.Value).GetValue(Scan0))
+                              End Function) _
+                .doKeggProfiles(displays)
         Else
             Return Internal.debug.stop("invalid data type for plot kegg category profile plot!", env)
         End If
 
         Return profile.ProfilesPlot(title,
-            size:=size,
+            size:=InteropArgumentHelper.getSize(size),
             tick:=tick,
             axisTitle:=axisTitle,
             labelRightAlignment:=False,
@@ -102,6 +112,18 @@ Module visualPlot
         )
     End Function
 
+    <Extension>
+    Private Function doKeggProfiles(profiles As Dictionary(Of String, Double), displays%) As Dictionary(Of String, NamedValue(Of Double)())
+        Return profiles _
+            .KEGGCategoryProfiles _
+            .ToDictionary(Function(p) p.Key,
+                          Function(group)
+                              Return group.Value _
+                                  .OrderByDescending(Function(t) t.Value) _
+                                  .Take(displays) _
+                                  .ToArray
+                          End Function)
+    End Function
 
 End Module
 
