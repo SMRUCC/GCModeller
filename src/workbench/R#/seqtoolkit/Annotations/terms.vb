df5c4146a1c4c7994d26f07428804489894c92a2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-09 10:35:24 +0800

    I/O of the id mapping model

--- src/workbench/R#/seqtoolkit/Annotations/terms.vb
+++ src/workbench/R#/seqtoolkit/Annotations/terms.vb
@@ -1,47 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::fa305348b133420b3a28f1743e529893, R#\seqtoolkit\Annotations\terms.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module terms
-    ' 
-    '     Function: COGannotations, GOannotations, KOannotations, Pfamannotations
-    ' 
-    ' /********************************************************************************/
+' Module terms
+' 
+'     Function: COGannotations, GOannotations, KOannotations, Pfamannotations
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.ComponentModel
+Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.Pipeline
 Imports SMRUCC.Rsharp.Runtime
@@ -80,5 +82,14 @@ Module terms
 
     End Function
 
+    <ExportAPI("write.id_maps")>
+    Public Function saveIdMappings(maps As SecondaryIDSolver, file As String) As Boolean
+        Return maps.Save(path:=file)
+    End Function
+
+    <ExportAPI("read.id_maps")>
+    Public Function readIdMappings(file As String) As SecondaryIDSolver
+        Return DBLinkBuilder.LoadMappingText(file)
+    End Function
 End Module
 
