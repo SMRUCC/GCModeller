366334d20196214c5d01cfec887a5c25707029e4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-12 19:23:21 +0800

    object renames improvements

--- src/GCModeller/analysis/SequenceToolkit/SequencePatterns/DNAOrigami/ScaffoldOrthogonality.vb
+++ src/GCModeller/analysis/SequenceToolkit/SequencePatterns/DNAOrigami/ScaffoldOrthogonality.vb
@@ -1,4 +1,6 @@
-﻿Imports SMRUCC.genomics.SequenceModel.NucleotideModels
+﻿Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Language.Default
+Imports SMRUCC.genomics.SequenceModel.NucleotideModels
 
 Namespace DNAOrigami
 
@@ -20,39 +22,43 @@ Namespace DNAOrigami
         ''' <param name="sc2"></param>
         ''' <param name="project"></param>
         ''' <returns></returns>
-        Public Function check_ortho(sc1 As String, sc2 As String, project As Project) As Output
-            Dim count As Double = 0
-            Dim count_RC = If(project.get_rev_compl, 0, Double.NaN)
+        Public Function CheckOrtho(sc1 As String, sc2 As String, Optional project As Project = Nothing) As Output
+            Dim count As Double
+            Dim count_RC As Double
             Dim n_count As New List(Of Double)
             Dim n_count_RC As New List(Of Double)
-            Dim sc1_full As String
-            Dim sc2_full As String
+            Dim sc1Full As String
+            Dim sc2Full As String
 
+            Static defaultArguments As [Default](Of Project) = New Project()
+
+            project = project Or defaultArguments
+            count_RC = If(project.get_rev_compl, 0, Double.NaN)
             sc1 = sc1.ToUpper
             sc2 = sc2.ToUpper
 
-            With circularise_sc(project, sc1, sc2)
-                sc1_full = .Item1
-                sc2_full = .Item2
+            With circulariseScaffold(project, sc1, sc2)
+                sc1Full = .Item1
+                sc2Full = .Item2
             End With
 
             For i As Integer = 0 To sc1.Length - 1
                 Dim repeat_count = 0
                 Dim repeat_count_RC = 0
-                Dim sc1_seg = sc1_full.Substring(i, project.n)
+                Dim sc1_seg = sc1Full.Substring(i, project.n)
 
                 For j As Integer = 0 To sc2.Length - 1
-                    Dim sc2_seg = sc2_full.Substring(j, project.n)
+                    Dim sc2_seg = sc2Full.Substring(j, project.n)
 
-                    If is_match(sc1_seg, sc2_seg, project) Then
+                    If isMatch(sc1_seg, sc2_seg, project) Then
                         count += 1
                         repeat_count += 1
                     End If
 
                     If project.get_rev_compl Then
-                        Dim sc2_seg_RC = NucleicAcid.Complement(sc2_full.Substring(i, project.n).Reverse.CharString)
+                        Dim sc2_seg_RC = NucleicAcid.Complement(sc2Full.Substring(i, project.n).Reverse.CharString)
 
-                        If is_match(sc1_seg, sc2_seg_RC, project) Then
+                        If isMatch(sc1_seg, sc2_seg_RC, project) Then
                             count_RC += 1
                             repeat_count_RC += 1
                         End If
@@ -80,7 +86,7 @@ Namespace DNAOrigami
             }
         End Function
 
-        Private Function circularise_sc(project As Project, sc1 As String, sc2 As String) As (String, String)
+        Private Function circulariseScaffold(project As Project, sc1 As String, sc2 As String) As (String, String)
             If project.is_linear Then
                 Return (sc1, sc2)
             Else
@@ -91,7 +97,7 @@ Namespace DNAOrigami
             End If
         End Function
 
-        Private Function is_match(seg1 As String, seg2 As String, project As Project) As Boolean
+        Private Function isMatch(seg1 As String, seg2 As String, project As Project) As Boolean
             Return seg1 = seg2 And Len(seg1) = project.n
         End Function
     End Module
