f18c028c530775480d008357b155d0bc1ccd46d6
xieguigang <xie.guigang@gcmodeller.org>
2020-06-12 23:36:24 +0800

    pfsnet analysis api

---
+++ src/workbench/R#/phenotype_kit/PFSNetAnalysis.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Analysis.PFSNet
+Imports SMRUCC.genomics.Analysis.PFSNet.DataStructure
+
+<Package("PFSNet", Category:=APICategories.ResearchTools)>
+Module PFSNetAnalysis
+
+    ''' <summary>
+    ''' Finding consistent disease subnetworks using PFSNet
+    ''' </summary>
+    ''' <param name="expr1o"></param>
+    ''' <param name="expr2o"></param>
+    ''' <param name="ggi"></param>
+    ''' <param name="b"></param>
+    ''' <param name="t1"></param>
+    ''' <param name="t2"></param>
+    ''' <param name="n"></param>
+    ''' <returns></returns>
+    <ExportAPI("pfsnet")>
+    Public Function pfsnet(expr1o As DataFrameRow(), expr2o As DataFrameRow(), ggi As GraphEdge(),
+                           Optional b# = 0.5,
+                           Optional t1# = 0.95,
+                           Optional t2# = 0.85,
+                           Optional n% = 1000) As PFSNetResultOut
+
+        Return PFSNetAlgorithm.pfsnet(expr1o, expr2o, ggi, b, t1, t2, n)
+    End Function
+End Module
