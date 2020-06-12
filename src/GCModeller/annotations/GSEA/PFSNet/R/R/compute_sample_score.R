c9657a23f1079bbb6c8381187cc3344d3b312f5c
xieguigang <xie.guigang@gcmodeller.org>
2020-06-13 01:14:56 +0800

    Update compute_sample_score.R

--- src/GCModeller/annotations/GSEA/PFSNet/R/R/compute_sample_score.R
+++ src/GCModeller/annotations/GSEA/PFSNet/R/R/compute_sample_score.R
@@ -1,3 +1,25 @@
+
+#' compute nscore or pscore matrix
+#'
+compute_npscore <- function(ccs, w1matrix1, nscore = FALSE) {
+	sapply(ccs,function(x){
+        vertices<-get.vertex.attribute(x,name="name")
+        ws<-w1matrix1[vertices,,drop=FALSE]
+        ws[is.na(ws)]<-0
+        v<-c();
+		weight.n <- if (nscore) {
+			V(x)$weight;
+		} else {
+			V(x)$weight2;
+		}		
+		
+        for(i in 1:ncol(ws)){
+            v<-c(v, sum((weight.n * ws[,i])));
+        }
+        v
+    });
+}
+
 pfsnet.compute.sample.score<-function(ccs,ccs2,file1,file2,t1=0.95,t2=0.85){
     expr1o<-loaddata(file1)
     expr2o<-loaddata(file2)
@@ -5,55 +27,10 @@ pfsnet.compute.sample.score<-function(ccs,ccs2,file1,file2,t1=0.95,t2=0.85){
     w1matrix2<-computew1(expr2o,theta1=t1,theta2=t2)
     rownames(w1matrix1)<-rownames(expr1o)
     rownames(w1matrix2)<-rownames(expr2o)
-    pscore<-sapply(ccs,function(x){
-        vertices<-get.vertex.attribute(x,name="name")
-        ws<-w1matrix1[vertices,,drop=FALSE]
-        ws[is.na(ws)]<-0
-        v<-c()
-        for(i in 1:ncol(ws)){
-            v<-c(v,sum(
-                (V(x)$weight*ws[,i])
-            ))
-        }
-        v
-    })
-    nscore<-sapply(ccs,function(x){
-        vertices<-get.vertex.attribute(x,name="name")
-        ws<-w1matrix1[vertices,,drop=FALSE]
-        ws[is.na(ws)]<-0
-        v<-c()
-        for(i in 1:ncol(ws)){
-            v<-c(v,sum(
-                (V(x)$weight2*ws[,i])
-            ))
-        }
-        v
-    })
-    pscore2<-sapply(ccs2,function(x){
-        vertices<-get.vertex.attribute(x,name="name")
-        ws<-w1matrix2[vertices,,drop=FALSE]
-        ws[is.na(ws)]<-0
-        v<-c()
-        for(i in 1:ncol(ws)){
-            v<-c(v,sum(
-                (V(x)$weight*ws[,i])
-            ))
-        }
-        v
-    })
-    nscore2<-sapply(ccs2,function(x){
-        vertices<-get.vertex.attribute(x,name="name")
-        ws<-w1matrix2[vertices,,drop=FALSE]
-        ws[is.na(ws)]<-0
-        v<-c()
-        for(i in 1:ncol(ws)){
-            v<-c(v,sum(
-                (V(x)$weight2*ws[,i])
-            ))
-        }
-        v
-    })
+    pscore<-compute_npscore(ccs, w1matrix1);
+    nscore<-compute_npscore(ccs, w1matrix1, nscore = TRUE);
+    pscore2<-compute_npscore(ccs2,w1matrix2);
+    nscore2<-compute_npscore(ccs2,w1matrix2, nscore = TRUE);
+	
     list(pscore=as.data.frame(pscore),nscore=as.data.frame(nscore),pscore2=as.data.frame(pscore2),nscore2=as.data.frame(nscore2))
-
 }
-
