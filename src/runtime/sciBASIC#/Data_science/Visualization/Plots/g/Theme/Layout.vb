d3da717763c2913f76e80f39e8c9708ccd783e24
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-15 13:34:20 +0800

    layout dependency

--- src/runtime/sciBASIC#/Data_science/Visualization/Plots/g/Theme/Layout.vb
+++ src/runtime/sciBASIC#/Data_science/Visualization/Plots/g/Theme/Layout.vb
@@ -1,11 +1,31 @@
 ﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
 
 Namespace Graphic.Canvas
 
     Public MustInherit Class Layout
 
-        Public MustOverride Function GetLocation(canvas As GraphicsRegion) As PointF
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="canvas"></param>
+        ''' <param name="dependency">
+        ''' a list of object with its ``[top-left]`` layout information. 
+        ''' this objects table should always contains an ``canvas`` 
+        ''' object layout information.
+        ''' </param>
+        ''' <returns></returns>
+        Public MustOverride Function GetLocation(canvas As GraphicsRegion, dependency As Dictionary(Of String, RectangleF)) As PointF
+
+        Public Shared Function BlankDependency(canvas As GraphicsRegion) As Dictionary(Of String, RectangleF)
+            Dim rect As Rectangle = canvas.PlotRegion
+            Dim rectf As New RectangleF(rect.Location.PointF, rect.Size.SizeF)
+
+            Return New Dictionary(Of String, RectangleF) From {
+                {NameOf(canvas), rectf}
+            }
+        End Function
 
     End Class
 
@@ -17,7 +37,7 @@ Namespace Graphic.Canvas
         Public Property x As Double
         Public Property y As Double
 
-        Public Overrides Function GetLocation(canvas As GraphicsRegion) As PointF
+        Public Overrides Function GetLocation(canvas As GraphicsRegion, objects As Dictionary(Of String, RectangleF)) As PointF
             Return New PointF(x, y)
         End Function
     End Class
@@ -27,7 +47,7 @@ Namespace Graphic.Canvas
     ''' </summary>
     Public Class Relative : Inherits Layout
 
-        Public Overrides Function GetLocation(canvas As GraphicsRegion) As PointF
+        Public Overrides Function GetLocation(canvas As GraphicsRegion, objects As Dictionary(Of String, RectangleF)) As PointF
             Throw New NotImplementedException()
         End Function
     End Class
@@ -39,10 +59,17 @@ Namespace Graphic.Canvas
 
         Public Property x As Double
         Public Property y As Double
+        Public Property target As String = "canvas"
 
-        Public Overrides Function GetLocation(canvas As GraphicsRegion) As PointF
-            Dim x = Me.x * canvas.PlotRegion.Width
-            Dim y = canvas.PlotRegion.Height - Me.y * canvas.PlotRegion.Height
+        Public Overrides Function GetLocation(canvas As GraphicsRegion, objects As Dictionary(Of String, RectangleF)) As PointF
+            Dim rect As RectangleF = objects.TryGetValue(target)
+
+            If rect.IsEmpty Then
+                Throw New MissingPrimaryKeyException("missing layout dependency: " & target)
+            End If
+
+            Dim x = Me.x * rect.Width
+            Dim y = rect.Height - Me.y * rect.Height
 
             Return New PointF(x, y)
         End Function
