e4da88679f4372c2ff94f7c0b3fcf6fb45d0e7d6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-15 08:16:39 +0800

    config data visualize module

--- src/GCModeller/annotations/GSEA/PFSNet/PFSNet_visual/BubblePlot.vb
+++ src/GCModeller/annotations/GSEA/PFSNet/PFSNet_visual/BubblePlot.vb
@@ -1,6 +1,12 @@
-﻿''' <summary>
+﻿Imports Microsoft.VisualBasic.Imaging.Driver
+Imports SMRUCC.genomics.Analysis.PFSNet.DataStructure
+
+''' <summary>
 ''' the bubble plot for the PFSNnet result.
 ''' </summary>
 Public Module BubblePlot
 
+    Public Function Plot(data As PFSNetResultOut) As GraphicsData
+
+    End Function
 End Module
