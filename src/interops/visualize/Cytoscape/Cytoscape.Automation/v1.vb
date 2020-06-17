63a9d9264ad0b6bfcfbc8f62b041ad81ff7883cb
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-17 11:14:07 +0800

    cytoscape rest api for automation

---
+++ src/interops/visualize/Cytoscape/Cytoscape.Automation/v1.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Class v1
+
+    ReadOnly api$
+
+    Sub New(port As Integer, Optional host$ = "localhost")
+        api = $"http://{host}:{port}/v1"
+    End Sub
+
+    ''' <summary>
+    ''' GET list of layout algorithms
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function layouts() As String()
+        Dim url As String = $"{api}/apply/layouts"
+        Dim json As String = url.GET
+
+        Return json.LoadJSON(Of String())
+    End Function
+
+End Class
