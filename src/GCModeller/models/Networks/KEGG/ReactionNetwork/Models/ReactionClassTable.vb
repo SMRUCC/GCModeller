60bbd67da170e71e158246803e6a2ea71dbdc11e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-18 15:24:27 +0800

    simplify network by reaction class?

---
+++ src/GCModeller/models/Networks/KEGG/ReactionNetwork/Models/ReactionClassTable.vb
@@ -0,0 +1,12 @@
+﻿Namespace ReactionNetwork
+
+    Public Class ReactionClassTable
+
+        Public Property rId As String
+        Public Property category As String
+        Public Property [from] As String
+        Public Property [to] As String
+        Public Property define As String
+
+    End Class
+End Namespace
