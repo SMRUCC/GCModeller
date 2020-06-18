0feb8397047366c5ec1b7ea79f9f3e5407dd35a9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-18 17:17:25 +0800

    improvements of the sample matrix data model

--- src/GCModeller/analysis/HTS_matrix/Document.vb
+++ src/GCModeller/analysis/HTS_matrix/Document.vb
@@ -1,10 +1,23 @@
-﻿Imports Microsoft.VisualBasic.Text
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Text
 
 Public Module Document
 
-    Public Function LoadMatrixDocument(file As String) As Matrix
+    Public Function LoadMatrixDocument(file As String, excludes As Index(Of String)) As Matrix
         Dim text As String() = file.LineIterators.ToArray
         Dim sampleIds As String() = text(Scan0).Split(ASCII.TAB).Skip(1).ToArray
+        Dim takeIndex As Integer()
+
+        If excludes Is Nothing Then
+            takeIndex = sampleIds.Sequence.ToArray
+        Else
+            takeIndex = sampleIds _
+                .Select(Function(name, i) (i, Not name Like excludes)) _
+                .Where(Function(a) a.Item2 = True) _
+                .Select(Function(a) a.i) _
+                .ToArray
+        End If
+
         Dim matrix As DataFrameRow() = text _
             .Skip(1) _
             .Select(Function(line)
@@ -13,6 +26,12 @@ Public Module Document
                             .Skip(1) _
                             .Select(AddressOf Val) _
                             .ToArray
+
+                        If Not excludes Is Nothing Then
+                            data = takeIndex _
+                                .Select(Function(i) data(i)) _
+                                .ToArray
+                        End If
 
                         Return New DataFrameRow With {
                             .experiments = data,
@@ -23,7 +42,9 @@ Public Module Document
 
         Return New Matrix With {
             .expression = matrix,
-            .sampleID = sampleIds
+            .sampleID = takeIndex _
+                .Select(Function(i) sampleIds(i)) _
+                .ToArray
         }
     End Function
 End Module
---
+++ src/GCModeller/visualize/DataVisualizationExtensions/SampleColorBend.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
+Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+
+Public Module SampleColorBend
+
+    ''' <summary>
+    ''' 仅对行进行计算
+    ''' </summary>
+    ''' <param name="matrix"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Iterator Function GetColors(matrix As Matrix, Optional colorSet$ = "RdYlGn:c8", Optional levels As Integer = 25) As IEnumerable(Of NamedCollection(Of Color))
+        Dim colors As Color() = Designer.GetColors(colorSet, n:=levels)
+        Dim indexRange As DoubleRange = {0, colors.Length - 1}
+
+        For Each gene As DataFrameRow In matrix.expression
+            Dim levelRange As New DoubleRange(gene.experiments)
+            Dim index As Integer() = gene.experiments _
+                .Select(Function(a)
+                            Return levelRange.ScaleMapping(a, indexRange)
+                        End Function) _
+                .Select(Function(d) CInt(d)) _
+                .ToArray
+
+            Yield New NamedCollection(Of Color) With {
+                .name = gene.geneID,
+                .value = index _
+                    .Select(Function(i) colors(i)) _
+                    .ToArray
+            }
+        Next
+    End Function
+
+    Public Sub Draw(g As IGraphics, layout As RectangleF, Optional horizontal As Boolean = True）
+
+    End Sub
+End Module
