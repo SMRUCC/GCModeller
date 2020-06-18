57e050febf79ade6386ae710a42aa2a0afb662f8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-18 10:11:30 +0800

    cy tables

---
+++ src/interops/visualize/Cytoscape/Cytoscape/Session/cyTables.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.VisualBasic.Linq
+
+Namespace Session
+
+    Public Class cyTables : Implements Enumeration(Of virtualColumn)
+
+        Public Property virtualColumns As virtualColumn()
+
+        Public Iterator Function GenericEnumerator() As IEnumerator(Of virtualColumn) Implements Enumeration(Of virtualColumn).GenericEnumerator
+            For Each item In virtualColumns
+                Yield item
+            Next
+        End Function
+
+        Public Iterator Function GetEnumerator() As IEnumerator Implements Enumeration(Of virtualColumn).GetEnumerator
+            Yield GenericEnumerator()
+        End Function
+    End Class
+
+    Public Class virtualColumn
+
+        Public Property name As String
+        Public Property sourceColumn As String
+        Public Property sourceTable As String
+        Public Property sourceJoinKey As String
+        Public Property targetTable As String
+        Public Property targetJoinKey As String
+        Public Property immutable As Boolean
+
+    End Class
+End Namespace
