aee33677e1a827c0d108f1e5c06bb9da8245cd97
xieguigang <xie.guigang@gcmodeller.org>
2020-06-26 03:47:55 +0800

    create annotation data cache from a large uniprot database?

---
+++ src/GCModeller/core/Bio.Repository/UniProt/AnnotationCache.vb
@@ -0,0 +1,8 @@
+﻿Imports SMRUCC.genomics.Assembly.Uniprot.XML
+
+Public Module AnnotationCache
+
+    Public Function WritePtfCache(proteins As IEnumerable(Of entry))
+
+    End Function
+End Module
