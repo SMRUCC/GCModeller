04fa264a606320f3da5f535f3c6c43f5061a5c68
xieguigang <xie.guigang@gcmodeller.org>
2020-06-26 10:58:31 +0800

    write ptf cache

--- src/workbench/R#/seqtoolkit/Annotations/uniprot.vb
+++ src/workbench/R#/seqtoolkit/Annotations/uniprot.vb
@@ -1,50 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::8ce567e0cbb93a56cea3f83018b3677c, R#\seqtoolkit\Annotations\uniprot.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module uniprot
-    ' 
-    '     Function: getProteinSeq, openUniprotXmlAssembly
-    ' 
-    ' /********************************************************************************/
+' Module uniprot
+' 
+'     Function: getProteinSeq, openUniprotXmlAssembly
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.IO
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Assembly.Uniprot.XML
+Imports SMRUCC.genomics.Data.AnnotationCache
 Imports SMRUCC.genomics.SequenceModel.FASTA
 Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Runtime.Interop
 
@@ -76,10 +80,7 @@ Module uniprot
                                   Optional extractAll As Boolean = False,
                                   Optional env As Environment = Nothing) As pipeline
 
-        If uniprot Is Nothing Then
-            Return Nothing
-        End If
-
+        Dim source = getUniprotData(uniprot, env)
         Dim protFa = Iterator Function(prot As entry) As IEnumerable(Of FastaSeq)
                          If extractAll Then
                              For Each accid As String In prot.accessions
@@ -96,21 +97,68 @@ Module uniprot
                          End If
                      End Function
 
-        If TypeOf uniprot Is entry() Then
-            Return DirectCast(uniprot, entry()) _
+        If source Like GetType(Message) Then
+            Return source.TryCast(Of Message)
+        Else
+            Return source.TryCast(Of IEnumerable(Of entry)) _
                 .Select(Function(prot)
                             Return protFa(prot)
                         End Function) _
                 .IteratesALL _
                 .DoCall(AddressOf pipeline.CreateFromPopulator)
+        End If
+    End Function
+
+    Public Function getUniprotData(uniprot As Object, env As Environment) As [Variant](Of IEnumerable(Of entry), Message)
+        If uniprot Is Nothing Then
+            Return DirectCast(New entry() {}, IEnumerable(Of entry))
+        End If
+
+        If TypeOf uniprot Is entry() OrElse TypeOf uniprot Is IEnumerable(Of entry) Then
+            Return DirectCast(uniprot, IEnumerable(Of entry))
         ElseIf TypeOf uniprot Is pipeline AndAlso DirectCast(uniprot, pipeline).elementType Like GetType(entry) Then
-            Return DirectCast(uniprot, pipeline) _
-                .populates(Of entry) _
-                .Select(Function(prot) protFa(prot)) _
-                .IteratesALL _
-                .DoCall(AddressOf pipeline.CreateFromPopulator)
+            Return DirectCast(uniprot, pipeline).populates(Of entry)
+        ElseIf TypeOf uniprot Is vector AndAlso DirectCast(uniprot, vector).elementType Like GetType(entry) Then
+            Return DirectCast(uniprot, vector).data.AsObjectEnumerator(Of entry)
         Else
             Return Internal.debug.stop($"invalid data source input: {uniprot.GetType.FullName}!", env)
+        End If
+    End Function
+
+    <ExportAPI("cache.ptf")>
+    Public Function writePtfFile(<RRawVectorArgument>
+                                 uniprot As Object,
+                                 file As Object,
+                                 Optional cacheTaxonomy As Boolean = False,
+                                 Optional env As Environment = Nothing) As Object
+
+        Dim source = getUniprotData(uniprot, env)
+
+        If source Like GetType(Message) Then
+            Return source.TryCast(Of Message)
+        Else
+            Dim stream As StreamWriter
+
+            If file Is Nothing Then
+                Return Internal.debug.stop({"file output can not be nothing!"}, env)
+            ElseIf TypeOf file Is String Then
+                stream = DirectCast(file, String).OpenWriter
+            ElseIf TypeOf file Is Stream Then
+                stream = New StreamWriter(DirectCast(file, Stream)) With {.NewLine = True}
+            ElseIf TypeOf file Is StreamWriter Then
+                stream = DirectCast(file, StreamWriter)
+            Else
+                Return Internal.debug.stop(Message.InCompatibleType(GetType(Stream), file.GetType, env,, NameOf(file)), env)
+            End If
+
+            Call source.TryCast(Of IEnumerable(Of entry)).WritePtfCache(stream, cacheTaxonomy)
+            Call stream.Flush()
+
+            If TypeOf file Is String Then
+                Call stream.Close()
+            End If
+
+            Return Nothing
         End If
     End Function
 End Module
