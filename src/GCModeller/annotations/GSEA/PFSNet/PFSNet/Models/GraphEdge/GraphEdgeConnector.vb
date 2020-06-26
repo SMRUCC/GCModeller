798189409cc4084cbcf90209ad6f92331fe56b70
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-26 15:05:37 +0800

    build data for pfsnet

---
+++ src/GCModeller/annotations/GSEA/PFSNet/PFSNet/Models/GraphEdge/GraphEdgeConnector.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+Namespace DataStructure
+
+    Public Module GraphEdgeConnector
+
+        <Extension>
+        Public Iterator Function FromMetabolismNetwork(g As NetworkGraph, Optional pathwayId As String = "pathwayId") As IEnumerable(Of GraphEdge)
+            For Each edge As Edge In g.graphEdges
+                Yield New GraphEdge With {
+                    .g1 = edge.U.label,
+                    .g2 = edge.V.label,
+                    .pathwayID = edge.data(pathwayId)
+                }
+            Next
+        End Function
+    End Module
+End Namespace
