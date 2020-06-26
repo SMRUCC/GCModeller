f902fa581288858e376f07f6b1b6f35f33046b31
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-26 17:18:02 +0800

    create ggi network

---
+++ src/GCModeller/annotations/GSEA/PFSNet/PFSNet_visual/ggiBuilder.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.Analysis.PFSNet.DataStructure
+Imports SMRUCC.genomics.Assembly.KEGG.WebServices
+Imports SMRUCC.genomics.Model.Network.KEGG.ReactionNetwork
+
+Public Module ggiBuilder
+
+    <Extension>
+    Public Iterator Function ReferenceCompoundNetwork(maps As IEnumerable(Of Map), reactions As IEnumerable(Of ReactionTable)) As IEnumerable(Of GraphEdge)
+        Dim fluxIndex As Dictionary(Of String, ReactionTable) = reactions.ToDictionary(Function(r) r.entry)
+        Dim fluxEntries As String()
+        Dim compoundEntries As String()
+        Dim allId As String()
+
+        For Each map As Map In maps
+            allId = map.GetMembers
+            fluxEntries = allId.Where(Function(id) id.IsPattern("R\d+")).ToArray
+
+            For Each reaction As ReactionTable In fluxEntries.Select(Function(id) fluxIndex(id))
+                compoundEntries = reaction.substrates.AsList + reaction.products
+
+                For Each a As String In compoundEntries
+                    For Each b As String In compoundEntries.Where(Function(id) id <> a)
+                        Yield New GraphEdge With {
+                            .pathwayID = map.Name,
+                            .g1 = a,
+                            .g2 = b
+                        }
+                    Next
+                Next
+            Next
+        Next
+    End Function
+End Module
