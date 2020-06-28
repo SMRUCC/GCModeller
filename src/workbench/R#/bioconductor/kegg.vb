310af8cbc94faceaef7b0809d10c1533bcda5b73
xieguigang <xie.guigang@gcmodeller.org>
2020-06-28 13:21:59 +0800

    R language to R.NET to R# interop helper

---
+++ src/workbench/R#/bioconductor/kegg.vb
@@ -0,0 +1,24 @@
+﻿
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports RDotNET.Extensions.GCModeller
+Imports SMRUCC.genomics.Assembly.KEGG.WebServices
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("kegg")>
+Module kegg
+
+    Public Function writeKeggMaps(<RRawVectorArgument> maps As Object, saveRDS As String, Optional env As Environment = Nothing) As Message
+        Dim kegg As pipeline = pipeline.TryCreatePipeline(Of Map)(maps, env)
+
+        If kegg.isError Then
+            Return kegg.getError
+        Else
+            Call kegg.populates(Of Map).WriteMaps(saveRDS)
+        End If
+
+        Return Nothing
+    End Function
+End Module
