fed5c7483166094fcfd9dee52ef22ce9440aff9f
xieguigang <xie.guigang@gcmodeller.org>
2020-06-28 21:16:52 +0800

    kegg pathway reconstruction

--- src/GCModeller/annotations/KEGG/Reconstruction.vb
+++ src/GCModeller/annotations/KEGG/Reconstruction.vb
@@ -5,8 +5,28 @@ Imports Microsoft.VisualBasic.Text.Xml.Models
 Imports SMRUCC.genomics.Annotation.Ptf
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
 Imports SMRUCC.genomics.Assembly.KEGG.WebServices
+Imports SMRUCC.genomics.Model.Network.KEGG.ReactionNetwork
 
 Public Module Reconstruction
+
+    <Extension>
+    Public Function AssignCompounds(pathway As Pathway, reactions As Dictionary(Of String, ReactionTable())) As Pathway
+        Dim fluxInMap = pathway.modules _
+            .Where(Function(id) reactions.ContainsKey(id.name)) _
+            .Select(Function(id) reactions(id.name)) _
+            .IteratesALL _
+            .ToArray
+
+        pathway.compound = fluxInMap _
+            .Select(Function(rxn) rxn.substrates.AsList + rxn.products) _
+            .IteratesALL _
+            .Distinct _
+            .Select(Function(cid) New NamedValue With {.name = cid}) _
+            .ToArray
+        pathway.modules = Nothing
+
+        Return pathway
+    End Function
 
     <Extension>
     Public Iterator Function KEGGReconstruction(reference As IEnumerable(Of Map),
@@ -24,66 +44,82 @@ Public Module Reconstruction
                           Function(g)
                               Return g.Select(Function(i) i.g).ToArray
                           End Function)
+        Dim mapReconstruct As New Value(Of Pathway)
 
         For Each map As Map In reference
-            Dim all As Integer = 0
-            Dim hits As New List(Of ProteinAnnotation())
-            Dim objs As String()
-
-            For Each entity As Area In map.shapes
-                objs = entity.IDVector
-
-                If objs.Any(Function(id) id.IsPattern("K\d+")) Then
-                    Dim tmp As New List(Of ProteinAnnotation)
-
-                    For Each id As String In objs
-                        If KOindex.ContainsKey(id) Then
-                            tmp += KOindex(id)
-                        End If
-                    Next
-
-                    If tmp > 0 Then
-                        hits += tmp.ToArray
-                    End If
-
-                    all += 1
-                End If
-            Next
-
-            Dim coverage As Double = hits.Count / all
-            Dim proteins As ProteinAnnotation() = hits _
-                .IteratesALL _
-                .GroupBy(Function(g) g.geneId) _
-                .Select(Function(g) g.First) _
-                .ToArray
-
-            If coverage >= min_cov Then
-                Yield New Pathway With {
-                    .description = map.Name,
-                    .EntryId = map.id,
-                    .name = map.Name,
-                    .KOpathway = proteins _
-                        .Select(Function(prot)
-                                    Return prot.attributes("ko") _
-                                        .Select(Function(ko)
-                                                    Return New NamedValue With {
-                                                        .name = ko,
-                                                        .text = prot.description
-                                                    }
-                                                End Function)
-                                End Function) _
-                        .IteratesALL _
-                        .ToArray,
-                    .genes = proteins _
-                        .Select(Function(g)
-                                    Return New NamedValue With {
-                                        .name = g.geneId,
-                                        .text = g.description
-                                    }
-                                End Function) _
-                        .ToArray
-                }
+            If Not mapReconstruct = map.KEGGReconstruction(KOindex, min_cov) Is Nothing Then
+                Yield mapReconstruct
             End If
         Next
+    End Function
+
+    <Extension>
+    Private Function KEGGReconstruction(map As Map, KOindex As Dictionary(Of String, ProteinAnnotation()), min_cov#) As Pathway
+        Dim all As Integer = 0
+        Dim hits As New List(Of ProteinAnnotation())
+        Dim objs As String()
+        Dim idIndex As New List(Of String)
+
+        For Each entity As Area In map.shapes
+            objs = entity.IDVector
+
+            If objs.Any(Function(id) id.IsPattern("K\d+")) Then
+                Dim tmp As New List(Of ProteinAnnotation)
+
+                For Each id As String In objs
+                    If KOindex.ContainsKey(id) Then
+                        tmp += KOindex(id)
+                    End If
+                Next
+
+                If tmp > 0 Then
+                    idIndex += objs
+                    hits += tmp.ToArray
+                End If
+
+                all += 1
+            End If
+        Next
+
+        Dim coverage As Double = hits.Count / all
+        Dim proteins As ProteinAnnotation() = hits _
+            .IteratesALL _
+            .GroupBy(Function(g) g.geneId) _
+            .Select(Function(g) g.First) _
+            .ToArray
+
+        If coverage >= min_cov Then
+            Return New Pathway With {
+                .description = map.Name,
+                .EntryId = map.id,
+                .name = map.Name,
+                .KOpathway = proteins _
+                    .Select(Function(prot)
+                                Return prot.attributes("ko") _
+                                    .Select(Function(ko)
+                                                Return New NamedValue With {
+                                                    .name = ko,
+                                                    .text = prot.description
+                                                }
+                                            End Function)
+                            End Function) _
+                    .IteratesALL _
+                    .ToArray,
+                .genes = proteins _
+                    .Select(Function(g)
+                                Return New NamedValue With {
+                                    .name = g.geneId,
+                                    .text = g.description
+                                }
+                            End Function) _
+                    .ToArray,
+                .modules = idIndex _
+                    .Distinct _
+                    .Select(Function(id) New NamedValue With {.name = id}) _
+                    .ToArray
+            }
+        Else
+            Return Nothing
+        End If
     End Function
 End Module
