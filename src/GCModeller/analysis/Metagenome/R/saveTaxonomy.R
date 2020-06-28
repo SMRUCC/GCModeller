2411a3bd269a49500c0742bbd1c07ce1cb3f90ac
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-28 17:06:44 +0800

    Update saveTaxonomy.R

--- src/GCModeller/analysis/Metagenome/R/saveTaxonomy.R
+++ src/GCModeller/analysis/Metagenome/R/saveTaxonomy.R
@@ -3,5 +3,5 @@ imports "taxonomy_kit" from "metagenomics_kit";
 "F:/"
 :> Ncbi.taxonomy_tree
 :> as.data.frame
-:> write.csv(file = "F:/ncbi_taxonomy.csv")
+:> write.csv(file = "F:/ncbi_taxonomy.csv", row_name = FALSE)
 ;
