fc925fd59ddd26b666705bb75f88ba094ff5b3c9
xieguigang <xie.guigang@gcmodeller.org>
2020-06-28 10:13:19 +0800

    load ptf annotation cache data

--- src/workbench/R#/proteomics_toolkit/ptfKit.vb
+++ src/workbench/R#/proteomics_toolkit/ptfKit.vb
@@ -1,5 +1,11 @@
-﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Annotation.Ptf
+Imports SMRUCC.Rsharp
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 
 ''' <summary>
@@ -10,7 +16,30 @@ Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Module ptfKit
 
     <ExportAPI("load.ptf")>
-    Public Function loadPtf(file As Object) As pipeline
+    Public Function loadPtf(file As Object, Optional env As Environment = Nothing) As pipeline
+        Dim stream = GetFileStream(file, FileAccess.Read, env)
 
+        If stream Like GetType(Message) Then
+            Return stream.TryCast(Of Message)
+        End If
+
+        Dim tryClose = Sub()
+                           If TypeOf file Is String Then
+                               Try
+                                   Call stream.TryCast(Of Stream).Close()
+                               Catch ex As Exception
+
+                               End Try
+                           End If
+                       End Sub
+
+        Return PtfFile _
+            .ReadAnnotations(stream.TryCast(Of Stream)) _
+            .DoCall(Function(anno)
+                        Return pipeline.CreateFromPopulator(
+                            upstream:=anno,
+                            finalize:=tryClose
+                        )
+                    End Function)
     End Function
 End Module
