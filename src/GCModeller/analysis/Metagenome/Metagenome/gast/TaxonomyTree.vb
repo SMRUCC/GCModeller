d5ae76d8455e37804d6265e32dbd34a5b8b17b50
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-28 16:52:30 +0800

    fix of object reference errors

--- src/GCModeller/analysis/Metagenome/Metagenome/gast/TaxonomyTree.vb
+++ src/GCModeller/analysis/Metagenome/Metagenome/gast/TaxonomyTree.vb
@@ -1,48 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::3684f18c05c3ae6152d4cc5d3cde0a2d, analysis\Metagenome\Metagenome\gast\TaxonomyTree.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class TaxonomyTree
-    ' 
-    '         Properties: Childs, hits, Lineage, Parent, TreeRoot
-    ' 
-    '         Constructor: (+3 Overloads) Sub New
-    ' 
-    '         Function: BuildTree, ToString
-    ' 
-    '         Sub: Split
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class TaxonomyTree
+' 
+'         Properties: Childs, hits, Lineage, Parent, TreeRoot
+' 
+'         Constructor: (+3 Overloads) Sub New
+' 
+'         Function: BuildTree, ToString
+' 
+'         Sub: Split
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -107,7 +107,7 @@ Namespace gast
             Return $"{lineage} ({rank.ToString}={hits})"
         End Function
 
-        Shared ReadOnly DescRanks$() = NcbiTaxonomyTree.stdranks.objects.Reverse.ToArray
+        Shared ReadOnly DescRanks$() = NcbiTaxonomyTree.stdranks.Objects.Reverse.ToArray
 
         ''' <summary>
         ''' Alignment hits by blastn
