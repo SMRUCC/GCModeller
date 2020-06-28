b24d68add78edc834516bebe1c6906370012501c
xieguigang <xie.guigang@gcmodeller.org>
2020-06-28 08:46:24 +0800

    ptf toolkit

---
+++ src/workbench/R#/proteomics_toolkit/ptfKit.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+
+''' <summary>
+''' toolkit for handle ptf annotation data set
+''' </summary>
+''' 
+<Package("ptfKit")>
+Module ptfKit
+
+    <ExportAPI("load.ptf")>
+    Public Function loadPtf(file As Object) As pipeline
+
+    End Function
+End Module
