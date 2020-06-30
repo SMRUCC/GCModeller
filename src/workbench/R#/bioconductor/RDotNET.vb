55e5d0c6cf6ffbfd5291402aff73ac300164c3a3
xieguigang <xie.guigang@gcmodeller.org>
2020-06-30 09:21:58 +0800

    .NET to R envrionment object push

--- src/workbench/R#/bioconductor/RDotNET.vb
+++ src/workbench/R#/bioconductor/RDotNET.vb
@@ -1,18 +1,22 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports RDotNET.Extensions.VisualBasic.API
 
 <Package("R")>
 Module RDotNET
 
-    Public Function push()
-
-    End Function
-
-    Public Function saveRDS()
-
-    End Function
-
-    Public Function saveRDA()
-
+    ''' <summary>
+    ''' push any .NET object into R runtime environment
+    ''' </summary>
+    ''' <param name="any"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("push")>
+    Public Function push(any As Object, Optional env As Environment = Nothing) As String
+        If any Is Nothing Then
+            Return "NULL"
+        End If
     End Function
 End Module
