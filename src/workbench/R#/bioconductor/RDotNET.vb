a9c5cf5a8b25f63e5254d29fdce5531dd1ea5043
xieguigang <xie.guigang@gcmodeller.org>
2020-06-30 09:09:36 +0800

    R DotNET to R#?

---
+++ src/workbench/R#/bioconductor/RDotNET.vb
@@ -0,0 +1,18 @@
+﻿
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("R")>
+Module RDotNET
+
+    Public Function push()
+
+    End Function
+
+    Public Function saveRDS()
+
+    End Function
+
+    Public Function saveRDA()
+
+    End Function
+End Module
