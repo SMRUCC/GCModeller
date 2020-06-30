a640bdb2d9645da37620cdfd8fd5eed54667c4fb
xieguigang <xie.guigang@gcmodeller.org>
2020-06-30 17:04:23 +0800

    fix bugs of write vector into R memory

--- src/R.Bioconductor/RDotNET.Extensions.VisualBasic/Extensions/Serialization/WriteMemoryInternal.vb
+++ src/R.Bioconductor/RDotNET.Extensions.VisualBasic/Extensions/Serialization/WriteMemoryInternal.vb
@@ -1,48 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::d992dc0b27c6697b7868ba4dbf04d9b6, RDotNET.Extensions.VisualBasic\Extensions\Serialization\WriteMemoryInternal.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module WriteMemoryInternal
-    ' 
-    '     Sub: (+2 Overloads) WriteLogical, WriteNothing, WriteNumeric, WriteNumerics, (+2 Overloads) WritePrimitive
-    '          (+2 Overloads) WriteString
-    ' 
-    ' /********************************************************************************/
+' Module WriteMemoryInternal
+' 
+'     Sub: (+2 Overloads) WriteLogical, WriteNothing, WriteNumeric, WriteNumerics, (+2 Overloads) WritePrimitive
+'          (+2 Overloads) WriteString
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Reflection
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports RDotNET.Extensions.VisualBasic.API
 Imports RDotNET.Extensions.VisualBasic.SymbolBuilder
 
 Module WriteMemoryInternal
@@ -127,7 +128,7 @@ Module WriteMemoryInternal
     Public Sub WriteNumerics(Of T As IComparable(Of T))(var$, x As IEnumerable(Of T))
         SyncLock R
             With R
-                .call = $"{var} <- c({x.JoinBy(", ")});"
+                .call = $"{var} <- {base.c(x)};"
             End With
         End SyncLock
     End Sub
@@ -135,7 +136,7 @@ Module WriteMemoryInternal
     Public Sub WriteString(var$, s As IEnumerable(Of String))
         SyncLock R
             With R
-                .call = $"{var} <- c({s.Select(AddressOf Rstring).JoinBy(", ")});"
+                .call = $"{var} <- {base.c(s, stringVector:=True)};"
             End With
         End SyncLock
     End Sub
@@ -143,7 +144,7 @@ Module WriteMemoryInternal
     Public Sub WriteLogical(var$, b As IEnumerable(Of Boolean))
         SyncLock R
             With R
-                .call = $"{var} <- c({b.Select(Function(f) f.ToString.ToUpper).JoinBy(", ")});"
+                .call = $"{var} <- {base.c(b)};"
             End With
         End SyncLock
     End Sub
