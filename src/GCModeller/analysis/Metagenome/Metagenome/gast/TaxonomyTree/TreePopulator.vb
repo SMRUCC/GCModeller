018cdc2b2ecb356cce2824e6c0f92b58a1a9401c
xieguigang <xie.guigang@gcmodeller.org>
2020-06-30 13:15:15 +0800

    Calc taxonomy Consensus

---
+++ src/GCModeller/analysis/Metagenome/Metagenome/gast/TaxonomyTree/TreePopulator.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace gast
+
+    Module TreePopulator
+
+        <Extension>
+        Public Iterator Function PopulateTaxonomy(tree As TaxonomyTree) As IEnumerable(Of Taxonomy)
+
+        End Function
+    End Module
+End Namespace
