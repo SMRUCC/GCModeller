3fc2759428c38dea5d91716476612f93e44fd8bd
xieguigang <xie.guigang@gcmodeller.org>
2020-07-01 08:50:41 +0800

    test matrix

--- src/workbench/R#/demo/taxonomy/biomMatrixReader.R
+++ src/workbench/R#/demo/taxonomy/biomMatrixReader.R
@@ -2,4 +2,5 @@ imports "BIOM_kit" from "metagenomics_kit";
 
 setwd(!script$dir);
 
-read.matrix("biom\EP733790_K70_BS1D.otu_table.biom") :> as.data.frame :> print;
+print(read.matrix("biom\EP733790_K70_BS1D.otu_table.biom") :> as.data.frame);
+print(read.matrix("biom\hmqcp.otu_table.biom.json") :> as.data.frame);
