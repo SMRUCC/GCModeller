fd7af7b1c50e804682a4e073aafacc8f0bc22a85
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-07-02 09:01:34 +0800

    read biom tables in linq pipeline mode

--- src/workbench/R#/demo/taxonomy/unionTables.R
+++ src/workbench/R#/demo/taxonomy/unionTables.R
@@ -2,8 +2,9 @@ imports ["BIOM_kit", "taxonomy_kit"] from "metagenomics_kit";
 
 setwd(!script$dir);
 
-list.files("./", "*.biom")
-:> sapply(path -> read.matrix(path))
+list.files("./hmqcp", "*.biom")
+:> as.pipeline
+:> projectAs(path -> read.matrix(path))
 :> biom.union
 :> write.csv(file = "./16s_comunity.csv", row_names = FALSE)
 ;
