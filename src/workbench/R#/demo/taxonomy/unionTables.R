83eb46a9fd69e21b363a99d470ff7ff2ed582d32
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-07-02 09:39:27 +0800

    Update unionTables.R

--- src/workbench/R#/demo/taxonomy/unionTables.R
+++ src/workbench/R#/demo/taxonomy/unionTables.R
@@ -5,6 +5,8 @@ setwd(!script$dir);
 list.files("./hmqcp", "*.biom")
 :> as.pipeline
 :> projectAs(path -> read.matrix(path))
+:> ctype("biom.matrix")
 :> biom.union
 :> write.csv(file = "./16s_comunity.csv", row_names = FALSE)
 ;
+
