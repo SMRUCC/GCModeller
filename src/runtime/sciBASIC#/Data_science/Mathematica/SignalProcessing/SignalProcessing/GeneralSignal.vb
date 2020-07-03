0d578e942b36d5ca120cb722f79d87d79789ee48
xieguigang <xie.guigang@gcmodeller.org>
2020-07-03 15:23:56 +0800

    general signal model

---
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/SignalProcessing/GeneralSignal.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Text
+
+Public Class GeneralSignal
+
+    Public Property Measures As Double()
+    Public Property Strength As Double()
+
+    Public Property MeasureUnit As String
+    Public Property Description As String
+
+    Public Overrides Function ToString() As String
+        Return Description
+    End Function
+
+    Public Function GetText() As String
+        Dim sb As New StringBuilder
+
+        Return sb.ToString
+    End Function
+
+End Class
