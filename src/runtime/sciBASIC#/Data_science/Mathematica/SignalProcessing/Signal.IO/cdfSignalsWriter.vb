07bb25864c61171e0a486eeef72d80eeac37110f
xieguigang <xie.guigang@gcmodeller.org>
2020-07-04 17:02:44 +0800

    improvements of the cdf file layout for save general signal datas

--- src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/Signal.IO/cdfSignalsWriter.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/Signal.IO/cdfSignalsWriter.vb
@@ -17,7 +17,7 @@ Public Module cdfSignalsWriter
     ' in R script code when the signal data count is large
 
     <Extension>
-    Public Function WriteCDF(signals As IEnumerable(Of GeneralSignal), file As String, Optional description$ = Nothing, Optional dimension_prefix$ = "signalChunk_") As Boolean
+    Public Function WriteCDF(signals As IEnumerable(Of GeneralSignal), file As String, Optional description$ = Nothing) As Boolean
         Using cdffile As New CDFWriter(file)
             Call cdffile.Dimensions(Dimension.Double, Dimension.Float, Dimension.Integer, Dimension.Long, Dimension.Text(fixedChars:=1024))
             Call cdffile.GlobalAttributes(New attribute With {.name = "time", .type = CDFDataTypes.CHAR, .value = Now.ToString})
@@ -30,16 +30,51 @@ Public Module cdfSignalsWriter
 
             Dim package As GeneralSignal() = signals.ToArray
             Dim chunksize As New List(Of Integer)
+            Dim offsets As New List(Of Long)
+            Dim dataDimension As Dimension
+            Dim attrNames As New List(Of String)
+            Dim measures As New List(Of Double)
+            Dim signalDatas As New List(Of Double)
 
             Call cdffile.GlobalAttributes(New attribute With {.name = "signals", .type = CDFDataTypes.INT, .value = package.Length})
 
             For Each attr As NamedValue(Of CDFData) In package.createAttributes()
+                dataDimension = New Dimension With {
+                    .name = "attribute_data: " & attr.Name,
+                    .size = attr.Value.Length
+                }
 
+                Call cdffile.AddVariable("meta:" & attr.Name, attr.Value, dataDimension)
+                Call attrNames.Add(attr.Name)
             Next
+
+            Call cdffile.GlobalAttributes(New attribute With {.name = "metadata", .type = CDFDataTypes.CHAR, .value = attrNames.AsEnumerable.GetJson})
+
+            Dim bufferOffset As Long = Scan0
+            Dim signal_guid As New CDFData With {.chars = signals.Select(Function(sig) sig.reference).GetJson}
+            Dim units As New CDFData With {.chars = signals.Select(Function(sig) sig.measureUnit).GetJson}
 
             For Each signal As GeneralSignal In signals
+                measures.AddRange(signal.Measures)
+                signalDatas.AddRange(signal.Strength)
+                offsets.Add(bufferOffset)
+                chunksize.Add(signal.Measures.Length)
 
+                bufferOffset += signal.Measures.Length
             Next
+
+            dataDimension = New Dimension With {.name = "data_chunks", .size = measures.Count}
+
+            Call cdffile.AddVariable("measure_buffer", New CDFData With {.numerics = measures.ToArray}, dataDimension)
+            Call cdffile.AddVariable("signal_buffer", New CDFData With {.numerics = signalDatas.ToArray}, dataDimension)
+            Call cdffile.AddVariable("chunk_size", New CDFData With {.integers = chunksize.ToArray}, dataDimension)
+            Call cdffile.AddVariable("buffer_offset", New CDFData With {.longs = offsets.ToArray}, dataDimension)
+
+            dataDimension = New Dimension With {.name = "guid_json_chars", .size = signal_guid.chars.Length}
+            cdffile.AddVariable("signal_guid", signal_guid, dataDimension)
+
+            dataDimension = New Dimension With {.name = "measure_unit_json_chars", .size = units.chars.Length}
+            cdffile.AddVariable("measure_unit", units, dataDimension)
         End Using
 
         Return True
