3186f2ad7e6bdc249c725fd7d52e9f2193e048bd
xieguigang <xie.guigang@gcmodeller.org>
2020-07-04 16:42:38 +0800

    create attributes variable for signal metadata

--- src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/Signal.IO/cdfSignalsWriter.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/Signal.IO/cdfSignalsWriter.vb
@@ -1,8 +1,11 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.IO.netCDF
 Imports Microsoft.VisualBasic.Data.IO.netCDF.Components
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math.SignalProcessing
+Imports Microsoft.VisualBasic.Net.Http
+Imports Microsoft.VisualBasic.Serialization.JSON
 
 ''' <summary>
 ''' cdf writer of the signals
@@ -27,12 +30,12 @@ Public Module cdfSignalsWriter
 
             Dim package As GeneralSignal() = signals.ToArray
             Dim chunksize As New List(Of Integer)
-            Dim attrs As New Dictionary(Of String, List(Of String))
 
             Call cdffile.GlobalAttributes(New attribute With {.name = "signals", .type = CDFDataTypes.INT, .value = package.Length})
 
+            For Each attr As NamedValue(Of CDFData) In package.createAttributes()
 
-
+            Next
 
             For Each signal As GeneralSignal In signals
 
@@ -43,22 +46,42 @@ Public Module cdfSignalsWriter
     End Function
 
     <Extension>
-    Private Function createAttributes(package As GeneralSignal()) As Dictionary(Of String, CDFData)
-        Dim attrs As New Dictionary(Of String, CDFData)
+    Private Iterator Function createAttributes(package As GeneralSignal()) As IEnumerable(Of NamedValue(Of CDFData))
         Dim allNames As String() = package.Select(Function(sig) sig.meta.Keys).IteratesALL.Distinct.ToArray
 
         For Each name As String In allNames
             Dim values As New List(Of String)
+            Dim data As CDFData
 
             For Each signal As GeneralSignal In package
-                values.Add(signal.meta.TryGetValue(name))
+                values.Add(signal.meta.TryGetValue(name, [default]:=""))
             Next
 
-            If values.All(Function(s) s.IsPattern("\d+")) Then
+            If values.All(Function(s) s Is Nothing OrElse s = "" OrElse s.IsPattern("\d+")) Then
+                Dim longs = values.Select(AddressOf Long.Parse).ToArray
 
+                If longs.All(Function(b) b <= 255 AndAlso b >= -255) Then
+                    data = New CDFData With {.byteStream = longs.Select(Function(l) CByte(l)).ToBase64String}
+                ElseIf longs.All(Function(s) s <= Short.MaxValue AndAlso s >= Short.MinValue) Then
+                    data = New CDFData With {.tiny_int = longs.Select(Function(l) CShort(l)).ToArray}
+                ElseIf longs.All(Function(i) i <= Integer.MaxValue AndAlso i >= Integer.MinValue) Then
+                    data = New CDFData With {.integers = longs.Select(Function(l) CInt(l)).ToArray}
+                Else
+                    data = New CDFData With {.longs = longs}
+                End If
+
+            ElseIf values.All(Function(s) s Is Nothing OrElse s = "" OrElse s.IsNumeric) Then
+                data = New CDFData With {.numerics = values.Select(AddressOf ParseDouble).ToArray}
+            ElseIf values.All(Function(s) s Is Nothing OrElse s = "" OrElse s.IsPattern("((true)|(false))", RegexICSng)) Then
+                data = New CDFData With {.flags = values.Select(AddressOf ParseBoolean).ToArray}
+            Else
+                data = New CDFData With {.chars = values.AsEnumerable.GetJson}
             End If
-        Next
 
-        Return attrs
+            Yield New NamedValue(Of CDFData) With {
+                .Name = name,
+                .Value = data
+            }
+        Next
     End Function
 End Module
