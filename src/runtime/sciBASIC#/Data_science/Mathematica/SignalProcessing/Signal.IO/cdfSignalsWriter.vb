41fff888c50c8039dba585579c7d7a92b176ef3d
xieguigang <xie.guigang@gcmodeller.org>
2020-07-04 17:37:59 +0800

    apply options

--- src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/Signal.IO/cdfSignalsWriter.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/Signal.IO/cdfSignalsWriter.vb
@@ -17,20 +17,18 @@ Public Module cdfSignalsWriter
     ''' </summary>
     ''' <param name="signals"></param>
     ''' <param name="file"></param>
-    ''' <param name="description$"></param>
-    ''' <param name="dimension_prefix$"></param>
+    ''' <param name="description"></param>
     ''' <param name="enableCDFExtension">enable netCDF extension type?</param>
     ''' <returns></returns>
     <Extension>
     Public Function WriteCDF(signals As IEnumerable(Of GeneralSignal), file As String,
                              Optional description$ = Nothing,
-                             Optional dimension_prefix$ = "signalChunk_",
                              Optional enableCDFExtension As Boolean = False) As Boolean
 
-    ' 20200704 redesign of the general signal cdf file storage layout:
-    ' the previous version of the signal data file is too slow that reading 
-    ' in R script code when the signal data count is large
-    
+        ' 20200704 redesign of the general signal cdf file storage layout:
+        ' the previous version of the signal data file is too slow that reading 
+        ' in R script code when the signal data count is large
+
         Using cdffile As New CDFWriter(file)
             Call cdffile.Dimensions(Dimension.Double, Dimension.Float, Dimension.Integer, Dimension.Long, Dimension.Text(fixedChars:=1024))
             Call cdffile.GlobalAttributes(New attribute With {.name = "time", .type = CDFDataTypes.CHAR, .value = Now.ToString})
@@ -51,7 +49,7 @@ Public Module cdfSignalsWriter
 
             Call cdffile.GlobalAttributes(New attribute With {.name = "signals", .type = CDFDataTypes.INT, .value = package.Length})
 
-            For Each attr As NamedValue(Of CDFData) In package.createAttributes()
+            For Each attr As NamedValue(Of CDFData) In package.createAttributes(enableCDFExtension)
                 dataDimension = New Dimension With {
                     .name = "attribute_data: " & attr.Name,
                     .size = attr.Value.Length
@@ -81,7 +79,10 @@ Public Module cdfSignalsWriter
             Call cdffile.AddVariable("measure_buffer", New CDFData With {.numerics = measures.ToArray}, dataDimension)
             Call cdffile.AddVariable("signal_buffer", New CDFData With {.numerics = signalDatas.ToArray}, dataDimension)
             Call cdffile.AddVariable("chunk_size", New CDFData With {.integers = chunksize.ToArray}, dataDimension)
-            Call cdffile.AddVariable("buffer_offset", New CDFData With {.longs = offsets.ToArray}, dataDimension)
+
+            If enableCDFExtension Then
+                Call cdffile.AddVariable("buffer_offset", New CDFData With {.longs = offsets.ToArray}, dataDimension)
+            End If
 
             dataDimension = New Dimension With {.name = "guid_json_chars", .size = signal_guid.chars.Length}
             cdffile.AddVariable("signal_guid", signal_guid, dataDimension)
@@ -94,7 +95,7 @@ Public Module cdfSignalsWriter
     End Function
 
     <Extension>
-    Private Iterator Function createAttributes(package As GeneralSignal()) As IEnumerable(Of NamedValue(Of CDFData))
+    Private Iterator Function createAttributes(package As GeneralSignal(), enableCDFExtension As Boolean) As IEnumerable(Of NamedValue(Of CDFData))
         Dim allNames As String() = package.Select(Function(sig) sig.meta.Keys).IteratesALL.Distinct.ToArray
 
         For Each name As String In allNames
@@ -114,14 +115,20 @@ Public Module cdfSignalsWriter
                     data = New CDFData With {.tiny_int = longs.Select(Function(l) CShort(l)).ToArray}
                 ElseIf longs.All(Function(i) i <= Integer.MaxValue AndAlso i >= Integer.MinValue) Then
                     data = New CDFData With {.integers = longs.Select(Function(l) CInt(l)).ToArray}
-                Else
+                ElseIf enableCDFExtension Then
                     data = New CDFData With {.longs = longs}
+                Else
+                    Throw New InvalidProgramException($"{GetType(Long).FullName} is not supports when option '{NameOf(enableCDFExtension)}' is not enable!")
                 End If
 
             ElseIf values.AsParallel.Select(Function(s) s Is Nothing OrElse s = "" OrElse s.IsNumeric).All(Function(t) t = True) Then
                 data = New CDFData With {.numerics = values.Select(AddressOf ParseDouble).ToArray}
             ElseIf values.AsParallel.Select(Function(s) s Is Nothing OrElse s = "" OrElse s.IsPattern("((true)|(false))", RegexICSng)).All(Function(t) t = True) Then
-                data = New CDFData With {.flags = values.Select(AddressOf ParseBoolean).ToArray}
+                If enableCDFExtension Then
+                    data = New CDFData With {.flags = values.Select(AddressOf ParseBoolean).ToArray}
+                Else
+                    data = New CDFData With {.tiny_int = values.Select(AddressOf ParseBoolean).Select(Function(f) CShort(If(f, 1, 0))).ToArray}
+                End If
             Else
                 data = New CDFData With {.chars = values.AsEnumerable.GetJson}
             End If
