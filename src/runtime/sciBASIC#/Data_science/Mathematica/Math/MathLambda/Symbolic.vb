f95f7cfa9526b1a8ae8ad053d6a53a6e73bb4c68
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-07-15 10:54:35 +0800

    how to simplify a symbolic expression?

---
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/MathLambda/Symbolic.vb
@@ -0,0 +1,20 @@
+﻿
+Imports Microsoft.VisualBasic.Math.Scripting.MathExpression.Impl
+
+''' <summary>
+''' symbolic computation engine
+''' </summary>
+Public Module Symbolic
+
+    Public Function Simplify(raw As Expression) As Expression
+        If Not TypeOf raw Is BinaryExpression Then
+            Return raw
+        Else
+            Dim bin As BinaryExpression = raw
+            Dim left = Simplify(bin.left)
+            Dim right = Simplify(bin.right)
+
+
+        End If
+    End Function
+End Module
