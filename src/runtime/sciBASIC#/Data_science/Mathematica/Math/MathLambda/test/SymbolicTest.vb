31c3c355a5cba83c4216b11d32da1ea170caded5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-07-15 11:35:45 +0800

    create a simple symbolic computation engine

---
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/MathLambda/test/SymbolicTest.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.Lambda
+Imports Microsoft.VisualBasic.Math.Scripting
+
+Module SymbolicTest
+
+    Sub Main()
+        Dim symbols = ScriptEngine.ParseExpression("(5+5) * (2*x + x / 5 + 1)").DoCall(AddressOf Symbolic.Simplify)
+
+        Console.WriteLine(symbols)
+
+        Pause()
+    End Sub
+End Module
