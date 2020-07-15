2d612941fe1385f26d0f7b304fff76ed984e40f1
xieguigang <xie.guigang@gcmodeller.org>
2020-07-15 08:38:57 +0800

    fix of null query context

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Net/HTTP/Web/WebQuery.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Net/HTTP/Web/WebQuery.vb
@@ -1,48 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::85f0858057db32d8783f73d508bfe512, Microsoft.VisualBasic.Core\Net\HTTP\Web\WebQuery.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class WebQuery
-    ' 
-    '         Properties: offlineMode
-    ' 
-    '         Constructor: (+3 Overloads) Sub New
-    ' 
-    '         Function: IsNullKey, (+2 Overloads) Query, QueryCacheText, queryText
-    ' 
-    '         Sub: runHttpGet
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class WebQuery
+' 
+'         Properties: offlineMode
+' 
+'         Constructor: (+3 Overloads) Sub New
+' 
+'         Function: IsNullKey, (+2 Overloads) Query, QueryCacheText, queryText
+' 
+'         Sub: runHttpGet
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -224,12 +224,18 @@ Namespace Net.Http
         End Function
 
         Public Function QueryCacheText(context As Context, Optional cacheType$ = ".xml", Optional ByRef hitCache As Boolean = False) As String
-            Dim result = queryText({context}, cacheType).First
-            Dim cache As String = result.cache.ReadAllText(throwEx:=False)
+            hitCache = False
 
-            hitCache = result.hitCache
+            If context Is Nothing Then
+                Return Nothing
+            Else
+                Dim result As (cache$, hitCache As Boolean) = queryText({context}, cacheType).First
+                Dim cache As String = result.cache.ReadAllText(throwEx:=False)
 
-            Return cache
+                hitCache = result.hitCache
+
+                Return cache
+            End If
         End Function
 
         ''' <summary>
