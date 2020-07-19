a4909fa95d517bfb37d0527be3ce2212b979a0c3
xieguigang <xie.guigang@gcmodeller.org>
2020-07-19 17:40:43 +0800

    how to map ncbi ftp url to cngb ftp utl?

--- src/workbench/R#/demo/cngb/all_bacterials.R
+++ src/workbench/R#/demo/cngb/all_bacterials.R
@@ -6,3 +6,8 @@ let prokaryote = fetch.kegg_organism(NULL, type = "prokaryote");
 
 write.csv(prokaryote, file = "./bacterials.csv", row_names = FALSE);
 
+for(genome in prokaryote :> projectAs(as.object)) {
+	let assembly as string = strsplit(gsub(genome$RefSeq, "ftp://ftp.ncbi.nlm.nih.gov/genomes/all/", ""), "/");
+	let assemblyName = `${assembly[1]}_${paste(assembly[2:4], "")}`;
+	let ftpURL = `ftp://ftp.cngb.org/pub/Assembly/${paste(assembly, "/")}/`;
+}
