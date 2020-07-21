6e4070ef7ab905733ae8dfd6cd72bc79914892fa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-07-21 16:36:45 +0800

    add IL source helper

---
+++ src/runtime/sciBASIC#/vs_solutions/dev/VisualStudio/IL/aa.vb
@@ -0,0 +1,223 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Reflection
+Imports System.Reflection.Emit
+Imports System.Threading
+
+Namespace SDILReader
+
+    ''' <summary>
+    ''' Parsing the IL of a Method Body
+    ''' 
+    ''' > https://www.codeproject.com/articles/14058/parsing-the-il-of-a-method-body
+    ''' </summary>
+    Public Class MethodBodyReader
+        Public instructions As List(Of ILInstruction) = Nothing
+        Protected il As Byte() = Nothing
+        Private mi As MethodInfo = Nothing
+
+#Region "il read methods"
+        Private Function ReadInt16(ByVal _il As Byte(), ByRef position As Integer) As Integer
+            Return il(Math.Min(Interlocked.Increment(position), position - 1)) Or il(Math.Min(Interlocked.Increment(position), position - 1)) << 8
+        End Function
+
+        Private Function ReadUInt16(ByVal _il As Byte(), ByRef position As Integer) As UShort
+            Return il(Math.Min(Interlocked.Increment(position), position - 1)) Or il(Math.Min(Interlocked.Increment(position), position - 1)) << 8
+        End Function
+
+        Private Function ReadInt32(ByVal _il As Byte(), ByRef position As Integer) As Integer
+            Return il(Math.Min(Interlocked.Increment(position), position - 1)) Or il(Math.Min(Interlocked.Increment(position), position - 1)) << 8 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H10 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H18
+        End Function
+
+        Private Function ReadInt64(ByVal _il As Byte(), ByRef position As Integer) As ULong
+            Return il(Math.Min(Interlocked.Increment(position), position - 1)) Or il(Math.Min(Interlocked.Increment(position), position - 1)) << 8 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H10 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H18 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H20 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H28 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H30 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H38
+        End Function
+
+        Private Function ReadDouble(ByVal _il As Byte(), ByRef position As Integer) As Double
+            Return il(Math.Min(Interlocked.Increment(position), position - 1)) Or il(Math.Min(Interlocked.Increment(position), position - 1)) << 8 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H10 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H18 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H20 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H28 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H30 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H38
+        End Function
+
+        Private Function ReadSByte(ByVal _il As Byte(), ByRef position As Integer) As SByte
+            Return il(Math.Min(Interlocked.Increment(position), position - 1))
+        End Function
+
+        Private Function ReadByte(ByVal _il As Byte(), ByRef position As Integer) As Byte
+            Return il(Math.Min(Interlocked.Increment(position), position - 1))
+        End Function
+
+        Private Function ReadSingle(ByVal _il As Byte(), ByRef position As Integer) As Single
+            Return il(Math.Min(Interlocked.Increment(position), position - 1)) Or il(Math.Min(Interlocked.Increment(position), position - 1)) << 8 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H10 Or il(Math.Min(Interlocked.Increment(position), position - 1)) << &H18
+        End Function
+#End Region
+
+        ''' <summary>
+        ''' Constructs the array of ILInstructions according to the IL byte code.
+        ''' </summary>
+        ''' <paramname="module"></param>
+        Private Sub ConstructInstructions(ByVal [module] As [Module])
+            Dim il = Me.il
+            Dim position = 0
+            instructions = New List(Of ILInstruction)()
+
+            While position < il.Length
+                Dim instruction As ILInstruction = New ILInstruction()
+
+                ' get the operation code of the current instruction
+                Dim code = OpCodes.Nop
+                Dim value As UShort = il(Math.Min(Interlocked.Increment(position), position - 1))
+
+                If value <> &HFE Then
+                    code = singleByteOpCodes(value)
+                Else
+                    value = il(Math.Min(Interlocked.Increment(position), position - 1))
+                    code = multiByteOpCodes(value)
+                    value = CUShort(value Or &HFE00)
+                End If
+
+                instruction.Code = code
+                instruction.Offset = position - 1
+                Dim metadataToken = 0
+                ' get the operand of the current operation
+                Select Case code.OperandType
+                    Case OperandType.InlineBrTarget
+                        metadataToken = ReadInt32(il, position)
+                        metadataToken += position
+                        instruction.Operand = metadataToken
+                    Case OperandType.InlineField
+                        metadataToken = ReadInt32(il, position)
+                        instruction.Operand = [module].ResolveField(metadataToken)
+                    Case OperandType.InlineMethod
+                        metadataToken = ReadInt32(il, position)
+
+                        Try
+                            instruction.Operand = [module].ResolveMethod(metadataToken)
+                        Catch
+                            instruction.Operand = [module].ResolveMember(metadataToken)
+                        End Try
+
+                    Case OperandType.InlineSig
+                        metadataToken = ReadInt32(il, position)
+                        instruction.Operand = [module].ResolveSignature(metadataToken)
+                    Case OperandType.InlineTok
+                        metadataToken = ReadInt32(il, position)
+
+                        Try
+                            instruction.Operand = [module].ResolveType(metadataToken)
+                        Catch
+                            ' SSS : see what to do here
+                        End Try
+
+                    Case OperandType.InlineType
+                        metadataToken = ReadInt32(il, position)
+                        ' now we call the ResolveType always using the generic attributes type in order
+                        ' to support decompilation of generic methods and classes
+
+                        ' thanks to the guys from code project who commented on this missing feature
+
+                        instruction.Operand = [module].ResolveType(metadataToken, mi.DeclaringType.GetGenericArguments(), mi.GetGenericArguments())
+                    Case OperandType.InlineI
+                        instruction.Operand = ReadInt32(il, position)
+                        Exit Select
+                    Case OperandType.InlineI8
+                        instruction.Operand = ReadInt64(il, position)
+                        Exit Select
+                    Case OperandType.InlineNone
+                        instruction.Operand = Nothing
+                        Exit Select
+                    Case OperandType.InlineR
+                        instruction.Operand = ReadDouble(il, position)
+                        Exit Select
+                    Case OperandType.InlineString
+                        metadataToken = ReadInt32(il, position)
+                        instruction.Operand = [module].ResolveString(metadataToken)
+                        Exit Select
+                    Case OperandType.InlineSwitch
+                        Dim count = ReadInt32(il, position)
+                        Dim casesAddresses = New Integer(count - 1) {}
+
+                        For i = 0 To count - 1
+                            casesAddresses(i) = ReadInt32(il, position)
+                        Next
+
+                        Dim cases = New Integer(count - 1) {}
+
+                        For i = 0 To count - 1
+                            cases(i) = position + casesAddresses(i)
+                        Next
+
+                        Exit Select
+                    Case OperandType.InlineVar
+                        instruction.Operand = ReadUInt16(il, position)
+                        Exit Select
+                    Case OperandType.ShortInlineBrTarget
+                        instruction.Operand = ReadSByte(il, position) + position
+                        Exit Select
+                    Case OperandType.ShortInlineI
+                        instruction.Operand = ReadSByte(il, position)
+                        Exit Select
+                    Case OperandType.ShortInlineR
+                        instruction.Operand = ReadSingle(il, position)
+                        Exit Select
+                    Case OperandType.ShortInlineVar
+                        instruction.Operand = ReadByte(il, position)
+                        Exit Select
+                    Case Else
+                        Throw New Exception("Unknown operand type.")
+                End Select
+
+                instructions.Add(instruction)
+            End While
+        End Sub
+
+        Public Function GetRefferencedOperand(ByVal [module] As [Module], ByVal metadataToken As Integer) As Object
+            Dim assemblyNames As AssemblyName() = [module].Assembly.GetReferencedAssemblies()
+
+            For i = 0 To assemblyNames.Length - 1
+                Dim modules As [Module]() = Assembly.Load(assemblyNames(i)).GetModules()
+
+                For j = 0 To modules.Length - 1
+
+                    Try
+                        Dim t = modules(j).ResolveType(metadataToken)
+                        Return t
+                    Catch
+                    End Try
+                Next
+            Next
+
+            Return Nothing
+            'System.Reflection.Assembly.Load(module.Assembly.GetReferencedAssemblies()[3]).GetModules()[0].ResolveType(metadataToken)
+
+        End Function
+        ''' <summary>
+        ''' Gets the IL code of the method
+        ''' </summary>
+        ''' <returns></returns>
+        Public Function GetBodyCode() As String
+            Dim result = ""
+
+            If instructions IsNot Nothing Then
+                For i = 0 To instructions.Count - 1
+                    result += instructions(i).GetCode() & Microsoft.VisualBasic.Constants.vbLf
+                Next
+            End If
+
+            Return result
+        End Function
+
+        ''' <summary>
+        ''' MethodBodyReader constructor
+        ''' </summary>
+        ''' <paramname="mi">
+        ''' The System.Reflection defined MethodInfo
+        ''' </param>
+        Public Sub New(ByVal mi As MethodInfo)
+            Me.mi = mi
+
+            If mi.GetMethodBody() IsNot Nothing Then
+                il = mi.GetMethodBody().GetILAsByteArray()
+                ConstructInstructions(mi.Module)
+            End If
+        End Sub
+    End Class
+End Namespace
---
+++ src/runtime/sciBASIC#/vs_solutions/dev/VisualStudio/IL/b.vb
@@ -0,0 +1,136 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Reflection.Emit
+
+Namespace SDILReader
+    Public Class ILInstruction
+        ' Fields
+        Private codeField As OpCode
+        Private operandField As Object
+        Private operandDataField As Byte()
+        Private offsetField As Integer
+
+        ' Properties
+        Public Property Code As OpCode
+            Get
+                Return codeField
+            End Get
+            Set(ByVal value As OpCode)
+                codeField = value
+            End Set
+        End Property
+
+        Public Property Operand As Object
+            Get
+                Return operandField
+            End Get
+            Set(ByVal value As Object)
+                operandField = value
+            End Set
+        End Property
+
+        Public Property OperandData As Byte()
+            Get
+                Return operandDataField
+            End Get
+            Set(ByVal value As Byte())
+                operandDataField = value
+            End Set
+        End Property
+
+        Public Property Offset As Integer
+            Get
+                Return offsetField
+            End Get
+            Set(ByVal value As Integer)
+                offsetField = value
+            End Set
+        End Property
+
+        ''' <summary>
+        ''' Returns a friendly strign representation of this instruction
+        ''' </summary>
+        ''' <returns></returns>
+        Public Function GetCode() As String
+            Dim result = ""
+            result += GetExpandedOffset(offsetField) & " : " & codeField.ToString
+
+            If operandField IsNot Nothing Then
+                Select Case codeField.OperandType
+                    Case OperandType.InlineField
+                        Dim fOperand = CType(operandField, FieldInfo)
+                        result += " " & ProcessSpecialTypes(fOperand.FieldType.ToString()) & " " & ProcessSpecialTypes(fOperand.ReflectedType.ToString()) & "::" & fOperand.Name & ""
+                    Case OperandType.InlineMethod
+
+                        Try
+                            Dim mOperand = CType(operandField, MethodInfo)
+                            result += " "
+                            If Not mOperand.IsStatic Then result += "instance "
+                            result += ProcessSpecialTypes(mOperand.ReturnType.ToString()) & " " & ProcessSpecialTypes(mOperand.ReflectedType.ToString()) & "::" & mOperand.Name & "()"
+                        Catch
+
+                            Try
+                                Dim mOperand = CType(operandField, ConstructorInfo)
+                                result += " "
+                                If Not mOperand.IsStatic Then result += "instance "
+                                result += "void " & ProcessSpecialTypes(mOperand.ReflectedType.ToString()) & "::" & mOperand.Name & "()"
+                            Catch
+                            End Try
+                        End Try
+
+                    Case OperandType.ShortInlineBrTarget, OperandType.InlineBrTarget
+                        result += " " & GetExpandedOffset(CInt(operandField))
+                    Case OperandType.InlineType
+                        result += " " & ProcessSpecialTypes(operandField.ToString())
+                    Case OperandType.InlineString
+
+                        If Equals(operandField.ToString(), Microsoft.VisualBasic.Constants.vbCrLf) Then
+                            result += " ""\r\n"""
+                        Else
+                            result += " """ & operandField.ToString() & """"
+                        End If
+
+                    Case OperandType.ShortInlineVar
+                        result += operandField.ToString()
+                    Case OperandType.InlineI, OperandType.InlineI8, OperandType.InlineR, OperandType.ShortInlineI, OperandType.ShortInlineR
+                        result += operandField.ToString()
+                    Case OperandType.InlineTok
+
+                        If TypeOf operandField Is Type Then
+                            result += CType(operandField, Type).FullName
+                        Else
+                            result += "not supported"
+                        End If
+
+                    Case Else
+                        result += "not supported"
+                End Select
+            End If
+
+            Return result
+        End Function
+
+        ''' <summary>
+        ''' Add enough zeros to a number as to be represented on 4 characters
+        ''' </summary>
+        ''' <paramname="offset">
+        ''' The number that must be represented on 4 characters
+        ''' </param>
+        ''' <returns>
+        ''' </returns>
+        Private Function GetExpandedOffset(ByVal offset As Long) As String
+            Dim result As String = offset.ToString()
+            Dim i = 0
+
+            While result.Length < 4
+                result = "0" & result
+                i += 1
+            End While
+
+            Return result
+        End Function
+
+        Public Sub New()
+        End Sub
+    End Class
+End Namespace
