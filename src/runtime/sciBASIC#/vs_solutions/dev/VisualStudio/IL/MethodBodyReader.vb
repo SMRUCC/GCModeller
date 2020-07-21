7dd3d4ac47164cbda660a4fe378a64749eb5fdff
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-07-21 17:47:01 +0800

    fix of il parser bugs

--- src/runtime/sciBASIC#/vs_solutions/dev/VisualStudio/IL/MethodBodyReader.vb
+++ src/runtime/sciBASIC#/vs_solutions/dev/VisualStudio/IL/MethodBodyReader.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Reflection
+﻿Imports System.IO
+Imports System.Reflection
 Imports System.Reflection.Emit
 Imports System.Threading
 Imports Microsoft.VisualBasic.Language
@@ -13,94 +14,97 @@ Namespace IL
     ''' </summary>
     Public Class MethodBodyReader
         Public instructions As List(Of ILInstruction) = Nothing
-        Protected il As Byte() = Nothing
+        Protected il As Stream
         Private mi As MethodInfo = Nothing
 
-#Region "il read methods"
-        Private Function ReadInt16(ByVal _il As Byte(), ByRef position As i32) As Integer
-            Return ((il(++position) Or (il(++position) << 8)))
-        End Function
+        '#Region "il read methods"
+        '        Private Function ReadInt16(ByVal _il As Byte(), ByRef position As i32) As Integer
+        '            Return ((il(++position) Or (il(++position) << 8)))
+        '        End Function
 
-        Private Function ReadUInt16(ByVal _il As Byte(), ByRef position As i32) As UShort
-            Return ((il(++position) Or (il(++position) << 8)))
-        End Function
+        '        Private Function ReadUInt16(ByVal _il As Byte(), ByRef position As i32) As UShort
+        '            Return ((il(++position) Or (il(++position) << 8)))
+        '        End Function
 
-        Private Function ReadInt32(ByVal _il As Byte(), ByRef position As i32) As Integer
-            Return (((il(++position) Or (il(++position) << 8)) Or (il(++position) << &H10)) Or (il(++position) << &H18))
-        End Function
+        '        Private Function ReadInt32(ByVal _il As Byte(), ByRef position As i32) As Integer
+        '            Return (((il(++position) Or (il(++position) << 8)) Or (il(++position) << &H10)) Or (il(++position) << &H18))
+        '        End Function
 
-        Private Function ReadInt64(ByVal _il As Byte(), ByRef position As i32) As ULong
-            Return (((il(++position) Or (il(++position) << 8)) Or (il(++position) << &H10)) Or (il(++position) << &H18) Or (il(++position) << &H20) Or (il(++position) << &H28) Or (il(++position) << &H30) Or (il(++position) << &H38))
-        End Function
+        '        Private Function ReadInt64(ByVal _il As Byte(), ByRef position As i32) As ULong
+        '            Return (((il(++position) Or (il(++position) << 8)) Or (il(++position) << &H10)) Or (il(++position) << &H18) Or (il(++position) << &H20) Or (il(++position) << &H28) Or (il(++position) << &H30) Or (il(++position) << &H38))
+        '        End Function
 
-        Private Function ReadDouble(ByVal _il As Byte(), ByRef position As i32) As Double
-            Return (((il(++position) Or (il(++position) << 8)) Or (il(++position) << &H10)) Or (il(++position) << &H18) Or (il(++position) << &H20) Or (il(++position) << &H28) Or (il(++position) << &H30) Or (il(++position) << &H38))
-        End Function
+        '        Private Function ReadDouble(ByVal _il As Byte(), ByRef position As i32) As Double
+        '            Return (((il(++position) Or (il(++position) << 8)) Or (il(++position) << &H10)) Or (il(++position) << &H18) Or (il(++position) << &H20) Or (il(++position) << &H28) Or (il(++position) << &H30) Or (il(++position) << &H38))
+        '        End Function
 
-        Private Function ReadSByte(ByVal _il As Byte(), ByRef position As i32) As SByte
-            Return il(++position)
-        End Function
+        '        Private Function ReadSByte(ByVal _il As Byte(), ByRef position As i32) As SByte
+        '            Return il(++position)
+        '        End Function
 
-        Private Function ReadByte(ByVal _il As Byte(), ByRef position As i32) As Byte
-            Return il(++position)
-        End Function
+        '        Private Function ReadByte(ByVal _il As Byte(), ByRef position As i32) As Byte
+        '            Return il(++position)
+        '        End Function
 
-        Private Function ReadSingle(ByVal _il As Byte(), ByRef position As i32) As Single
-            Return (((il(++position) Or (il(++position) << 8)) Or (il(++position) << &H10)) Or (il(++position) << &H18))
-        End Function
-#End Region
+        '        Private Function ReadSingle(ByVal _il As Byte(), ByRef position As i32) As Single
+        '            Return (((il(++position) Or (il(++position) << 8)) Or (il(++position) << &H10)) Or (il(++position) << &H18))
+        '        End Function
+        '#End Region
 
         ''' <summary>
         ''' Constructs the array of ILInstructions according to the IL byte code.
         ''' </summary>
         ''' <param name="module"></param>
         Private Sub ConstructInstructions(ByVal [module] As [Module])
-            Dim position As i32 = Scan0
+            ' Dim position As i32 = Scan0
+            Dim il As New BinaryReader(Me.il)
+
             instructions = New List(Of ILInstruction)()
 
-            While position < il.Length
+            While il.BaseStream.Position < il.BaseStream.Length   'position < il.Length
                 Dim instruction As New ILInstruction()
 
                 ' get the operation code of the current instruction
                 Dim code = OpCodes.Nop
-                Dim value As UShort = il(++position)
+                Dim value As UShort = il.ReadByte '(++position)
 
                 If value <> &HFE Then
                     code = singleByteOpCodes(value)
                 Else
-                    value = il(++position)
+                    value = il.ReadByte  ' il(++position)
                     code = multiByteOpCodes(value)
                     value = CUShort(value Or &HFE00)
                 End If
 
                 instruction.Code = code
-                instruction.Offset = CInt(position) - 1
+                instruction.Offset = il.BaseStream.Position - 1
 
                 Dim metadataToken = 0
 
                 ' get the operand of the current operation
                 Select Case code.OperandType
                     Case OperandType.InlineBrTarget
-                        metadataToken = ReadInt32(il, position)
-                        metadataToken += position
+                        metadataToken = il.ReadInt32 ' (il, position)
+                        metadataToken += il.BaseStream.Position  ' position
                         instruction.Operand = metadataToken
                     Case OperandType.InlineField
-                        metadataToken = ReadInt32(il, position)
+                        metadataToken = il.ReadInt32 ' (il, position)
                         instruction.Operand = [module].ResolveField(metadataToken)
                     Case OperandType.InlineMethod
-                        metadataToken = ReadInt32(il, position)
+                        metadataToken = il.ReadInt32 ' (il, position)
 
                         Try
                             instruction.Operand = [module].ResolveMethod(metadataToken)
                         Catch
-                            instruction.Operand = [module].ResolveMember(metadataToken)
+                            ' instruction.Operand = [module].ResolveMember(metadataToken)
+                            Continue While
                         End Try
 
                     Case OperandType.InlineSig
-                        metadataToken = ReadInt32(il, position)
+                        metadataToken = il.ReadInt32 ' (il, position)
                         instruction.Operand = [module].ResolveSignature(metadataToken)
                     Case OperandType.InlineTok
-                        metadataToken = ReadInt32(il, position)
+                        metadataToken = il.ReadInt32 ' (il, position)
 
                         Try
                             instruction.Operand = [module].ResolveType(metadataToken)
@@ -109,7 +113,7 @@ Namespace IL
                         End Try
 
                     Case OperandType.InlineType
-                        metadataToken = ReadInt32(il, position)
+                        metadataToken = il.ReadInt32 ' (il, position)
                         ' now we call the ResolveType always using the generic attributes type in order
                         ' to support decompilation of generic methods and classes
 
@@ -117,31 +121,31 @@ Namespace IL
 
                         instruction.Operand = [module].ResolveType(metadataToken, mi.DeclaringType.GetGenericArguments(), mi.GetGenericArguments())
                     Case OperandType.InlineI
-                        instruction.Operand = ReadInt32(il, position)
+                        instruction.Operand = il.ReadInt32' (il, position)
 
                     Case OperandType.InlineI8
-                        instruction.Operand = ReadInt64(il, position)
+                        instruction.Operand = il.ReadInt64' (il, position)
 
                     Case OperandType.InlineNone
                         instruction.Operand = Nothing
 
                     Case OperandType.InlineR
-                        instruction.Operand = ReadDouble(il, position)
+                        instruction.Operand = il.ReadDouble' (il, position)
 
                     Case OperandType.InlineString
-                        metadataToken = ReadInt32(il, position)
+                        metadataToken = il.ReadInt32 '(il, position)
                         instruction.Operand = [module].ResolveString(metadataToken)
 
                     Case OperandType.InlineSwitch
-                        Dim count = ReadInt32(il, position)
+                        Dim count = il.ReadInt32 ' (il, position)
                         Dim casesAddresses = New Integer(count - 1) {}
 
                         For i = 0 To count - 1
-                            casesAddresses(i) = ReadInt32(il, position)
+                            casesAddresses(i) = il.ReadInt32 ' (il, position)
                         Next
 
                         Dim cases = New Integer(count - 1) {}
-                        Dim position_i As Integer = position
+                        Dim position_i As Integer = il.BaseStream.Position
 
                         For i = 0 To count - 1
                             cases(i) = position_i + casesAddresses(i)
@@ -149,19 +153,19 @@ Namespace IL
 
 
                     Case OperandType.InlineVar
-                        instruction.Operand = ReadUInt16(il, position)
+                        instruction.Operand = il.ReadUInt16' (il, position)
 
                     Case OperandType.ShortInlineBrTarget
-                        instruction.Operand = ReadSByte(il, position) + position
+                        instruction.Operand = il.ReadSByte + il.BaseStream.Position  ' (il, position) + position
 
                     Case OperandType.ShortInlineI
-                        instruction.Operand = ReadSByte(il, position)
+                        instruction.Operand = il.ReadSByte'(il, position)
 
                     Case OperandType.ShortInlineR
-                        instruction.Operand = ReadSingle(il, position)
+                        instruction.Operand = il.ReadSingle' (il, position)
 
                     Case OperandType.ShortInlineVar
-                        instruction.Operand = ReadByte(il, position)
+                        instruction.Operand = il.ReadByte '(il, position)
 
                     Case Else
                         Throw New Exception("Unknown operand type.")
@@ -216,7 +220,7 @@ Namespace IL
             Me.mi = mi
 
             If mi.GetMethodBody() IsNot Nothing Then
-                il = mi.GetMethodBody().GetILAsByteArray()
+                il = New MemoryStream(mi.GetMethodBody().GetILAsByteArray())
                 ConstructInstructions(mi.Module)
             End If
         End Sub
