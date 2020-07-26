8b5a56bbb542f92105bb340e1bd37fbcae00195c
xieguigang <xie.guigang@gcmodeller.org>
2020-07-26 15:10:30 +0800

    improvements of the gene name parser rules

--- src/GCModeller/core/Bio.Assembly/Query/Term/geneName.vb
+++ src/GCModeller/core/Bio.Assembly/Query/Term/geneName.vb
@@ -2,13 +2,19 @@
 
     Public Module geneName
 
+        Const geneNamePattern As String = "[a-z][a-z0-9]{2,6}"
+
         Public Function TryParseGeneName(product As String) As String
             If product.TextEquals("conserved hypothetical protein") Then
                 Return ""
             End If
 
-            If product.IsPattern("[a-z][a-z0-9]{2,6} protein") Then
+            If product.IsPattern(geneNamePattern & " (protein|transposase)") Then
                 Return product.Split.First
+            ElseIf product.IsPattern(geneNamePattern) Then
+                Return product
+            ElseIf product.IsPattern(".+ protein " & geneNamePattern) Then
+                Return product.Split.Last
             Else
                 Return ""
             End If
