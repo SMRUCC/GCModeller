4983b3f34eeabc565de871a86ad18263b16475cb
xieguigang <xie.guigang@gcmodeller.org>
2020-07-26 14:46:27 +0800

    parse gene names

---
+++ src/GCModeller/core/Bio.Assembly/Query/Term/geneName.vb
@@ -0,0 +1,17 @@
+﻿Namespace Query
+
+    Public Module geneName
+
+        Public Function TryParseGeneName(product As String) As String
+            If product.TextEquals("conserved hypothetical protein") Then
+                Return ""
+            End If
+
+            If product.IsPattern("[a-z][a-z0-9]{2,6} protein") Then
+                Return product.Split.First
+            Else
+                Return ""
+            End If
+        End Function
+    End Module
+End Namespace
