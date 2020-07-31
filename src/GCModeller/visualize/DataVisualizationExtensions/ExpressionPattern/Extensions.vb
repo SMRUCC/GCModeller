40850fc253d3214c51561d1a8ae675d1b371191c
xieguigang <xie.guigang@gcmodeller.org>
2020-07-31 22:53:59 +0800

    plot extensions

---
+++ src/GCModeller/visualize/DataVisualizationExtensions/ExpressionPattern/Extensions.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.ChartPlots
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Scripting.Runtime
+Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+
+Public Module PatternPlotExtensions
+
+    Public Function DrawMatrix(raw As Matrix,
+                               Optional dim$ = "3,3",
+                               Optional size$ = "2400,2100",
+                               Optional padding$ = g.DefaultPadding,
+                               Optional bg$ = "white") As GraphicsData
+
+        Return ExpressionPattern.CMeansCluster(raw, dim$.SizeParser.ToArray).DrawMatrix(
+            size:=size,
+            padding:=padding,
+            bg:=bg
+        )
+    End Function
+
+    <Extension>
+    Public Function DrawMatrix(matrix As ExpressionPattern,
+                               Optional size$ = "2400,2100",
+                               Optional padding$ = g.DefaultPadding,
+                               Optional bg$ = "white",
+                               Optional title$ = "Expression Patterns",
+                               Optional xlab$ = "time groups",
+                               Optional ylab$ = "expression quantification") As GraphicsData
+
+        Dim theme As New Theme With {
+            .background = bg
+        }
+
+        Return New PatternPlot(theme) With {
+            .main = title,
+            .matrix = matrix,
+            .xlabel = xlab,
+            .ylabel = ylab
+        }.Plot(size, padding)
+    End Function
+End Module
--- src/GCModeller/visualize/DataVisualizationExtensions/ExpressionPattern/PatternPlot.vb
+++ src/GCModeller/visualize/DataVisualizationExtensions/ExpressionPattern/PatternPlot.vb
@@ -1,53 +1,13 @@
 ﻿Imports System.Drawing
-Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.ChartPlots
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
-Imports Microsoft.VisualBasic.Imaging.Driver
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
-Imports Microsoft.VisualBasic.Scripting.Runtime
 Imports SMRUCC.genomics.Analysis.HTS.DataFrame
 Imports DashStyle = System.Drawing.Drawing2D.DashStyle
-
-Public Module PatternPlotExtensions
-
-    Public Function DrawMatrix(raw As Matrix,
-                               Optional dim$ = "3,3",
-                               Optional size$ = "2400,2100",
-                               Optional padding$ = g.DefaultPadding,
-                               Optional bg$ = "white") As GraphicsData
-
-        Return ExpressionPattern.CMeansCluster(raw, dim$.SizeParser.ToArray).DrawMatrix(
-            size:=size,
-            padding:=padding,
-            bg:=bg
-        )
-    End Function
-
-    <Extension>
-    Public Function DrawMatrix(matrix As ExpressionPattern,
-                               Optional size$ = "2400,2100",
-                               Optional padding$ = g.DefaultPadding,
-                               Optional bg$ = "white",
-                               Optional title$ = "Expression Patterns",
-                               Optional xlab$ = "time groups",
-                               Optional ylab$ = "expression quantification") As GraphicsData
-
-        Dim theme As New Theme With {
-            .background = bg
-        }
-
-        Return New PatternPlot(theme) With {
-            .main = title,
-            .matrix = matrix,
-            .xlabel = xlab,
-            .ylabel = ylab
-        }.Plot(size, padding)
-    End Function
-End Module
 
 Public Class PatternPlot : Inherits Plot
 
