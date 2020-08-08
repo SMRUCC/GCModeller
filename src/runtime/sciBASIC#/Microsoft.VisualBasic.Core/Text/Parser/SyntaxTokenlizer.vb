96ad0e92844c575681c6babc2109c17922a0c497
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-08 14:08:51 +0800

    new command line string parser

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Text/Parser/SyntaxTokenlizer.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/Text/Parser/SyntaxTokenlizer.vb
@@ -3,10 +3,26 @@ Imports Microsoft.VisualBasic.Scripting.TokenIcer
 
 Namespace Text.Parser
 
-    Public MustInherit Class SyntaxTokenlizer(Of T As IComparable, Token As CodeToken(Of T))
+    Public MustInherit Class SyntaxTokenlizer(Of T As IComparable, SyntaxToken As CodeToken(Of T))
 
         Protected ReadOnly text As CharPtr
         Protected buffer As CharBuffer
+
+        Protected ReadOnly Property lastSplashEscape As Boolean
+            Get
+                If buffer = 0 Then
+                    Return False
+                ElseIf buffer = 1 Then
+                    If buffer = "\"c Then
+                        Return True
+                    Else
+                        Return False
+                    End If
+                Else
+                    Return buffer(-1) = "\"c AndAlso buffer(-2) <> "\"c
+                End If
+            End Get
+        End Property
 
         Sub New(text As [Variant](Of String, CharPtr))
             If text Like GetType(String) Then
@@ -16,21 +32,21 @@ Namespace Text.Parser
             End If
         End Sub
 
-        Public Overridable Iterator Function GetTokens(Of SyntaxToken As Token)() As IEnumerable(Of SyntaxToken)
+        Public Overridable Iterator Function GetTokens() As IEnumerable(Of SyntaxToken)
             Dim token As New Value(Of SyntaxToken)
 
             Do While text
-                If Not token = walkChar(Of SyntaxToken)(++text) Is Nothing Then
+                If Not token = walkChar(++text) Is Nothing Then
                     Yield CType(token, SyntaxToken)
                 End If
             Loop
 
             If buffer > 0 Then
-                Yield popOutToken(Of SyntaxToken)()
+                Yield popOutToken()
             End If
         End Function
 
-        Protected MustOverride Function walkChar(Of SyntaxToken As Token)(c As Char) As SyntaxToken
-        Protected MustOverride Function popOutToken(Of SyntaxToken As Token)() As SyntaxToken
+        Protected MustOverride Function walkChar(c As Char) As SyntaxToken
+        Protected MustOverride Function popOutToken() As SyntaxToken
     End Class
 End Namespace
