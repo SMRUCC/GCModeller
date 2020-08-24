8e3c5576b83e762113f6e7f6afd378da11a14691
xieguigang <xie.guigang@gcmodeller.org>
2020-08-24 11:35:49 +0800

    write bson buffer data

--- src/runtime/sciBASIC#/mime/application%json/Javascript/JsonObject.vb
+++ src/runtime/sciBASIC#/mime/application%json/Javascript/JsonObject.vb
@@ -1,52 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::6570ee0818aeee57aded826accc2b3e5, mime\application%json\Javascript\JsonObject.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class JsonObject
-    ' 
-    '         Function: BuildJsonString, ContainsElement, ContainsKey, CreateObject, GetEnumerator
-    '                   IEnumerable_GetEnumerator, Remove, ToString
-    ' 
-    '         Sub: (+2 Overloads) Add
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class JsonObject
+' 
+'         Function: BuildJsonString, ContainsElement, ContainsKey, CreateObject, GetEnumerator
+'                   IEnumerable_GetEnumerator, Remove, ToString
+' 
+'         Sub: (+2 Overloads) Add
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Text
+Imports System.IO
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
-Imports Microsoft.VisualBasic.Linq
 
 Namespace Javascript
 
@@ -89,6 +88,14 @@ Namespace Javascript
 
         Public Sub Add(key$, value As Object)
             Call array.Add(key, New JsonValue(value))
+        End Sub
+
+        ''' <summary>
+        ''' write bson buffer
+        ''' </summary>
+        ''' <param name="buffer"></param>
+        Public Sub WriteBuffer(buffer As FileStream)
+            Call BSON.WriteBuffer(Me, buffer)
         End Sub
 
         Public Function Remove(key As String) As Boolean
