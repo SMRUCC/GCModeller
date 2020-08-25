1322ea77f9c2a330e3be6367b691d745628e1baf
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-25 12:30:15 +0800

    improvements of the tree to graph epxort tools

--- src/runtime/sciBASIC#/gr/network-visualization/Datavisualization.Network/TreeAPI/TreeGraph.vb
+++ src/runtime/sciBASIC#/gr/network-visualization/Datavisualization.Network/TreeAPI/TreeGraph.vb
@@ -4,15 +4,15 @@ Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 
 Public NotInheritable Class TreeGraph(Of K, V)
 
-    Public Shared Function CreateGraph(tree As BinaryTree(Of K, V), uniqueId As Func(Of V, String), keyId As Func(Of K, String)) As NetworkGraph
+    Public Shared Function CreateGraph(tree As BinaryTree(Of K, V), uniqueId As Func(Of V, String), keyId As Func(Of K, String), Optional connectLeft As Boolean = False) As NetworkGraph
         Dim g As New NetworkGraph
         Dim root As Node = g.CreateNode(keyId(tree.Key), New NodeData With {.Properties = New Dictionary(Of String, String) From {{NamesOf.REFLECTION_ID_MAPPING_NODETYPE, keyId(tree.Key)}}})
 
-        Call appendGraph(g, root, tree, uniqueId, keyId)
+        Call appendGraph(g, root, tree, uniqueId, keyId, connectLeft)
         Return g
     End Function
 
-    Private Shared Sub appendGraph(g As NetworkGraph, center As Node, tree As BinaryTree(Of K, V), uniqueId As Func(Of V, String), keyId As Func(Of K, String))
+    Private Shared Sub appendGraph(g As NetworkGraph, center As Node, tree As BinaryTree(Of K, V), uniqueId As Func(Of V, String), keyId As Func(Of K, String), connectLeft As Boolean)
         Dim clusterId = keyId(tree.Key)
         Dim v As Node
         Dim guid As String
@@ -29,18 +29,18 @@ Public NotInheritable Class TreeGraph(Of K, V)
             g.CreateEdge(center, v)
         Next
 
-        If Not tree.Left Is Nothing Then
+        If connectLeft AndAlso Not tree.Left Is Nothing Then
             clusterId = keyId(tree.Left.Key)
             v = g.CreateNode(clusterId, New NodeData With {.Properties = New Dictionary(Of String, String) From {{NamesOf.REFLECTION_ID_MAPPING_NODETYPE, clusterId}}})
             g.CreateEdge(v, center)
-            appendGraph(g, v, tree.Left, uniqueId, keyId)
+            appendGraph(g, v, tree.Left, uniqueId, keyId, connectLeft)
         End If
 
         If Not tree.Right Is Nothing Then
             clusterId = keyId(tree.Right.Key)
             v = g.CreateNode(clusterId, New NodeData With {.Properties = New Dictionary(Of String, String) From {{NamesOf.REFLECTION_ID_MAPPING_NODETYPE, clusterId}}})
             g.CreateEdge(v, center)
-            appendGraph(g, v, tree.Right, uniqueId, keyId)
+            appendGraph(g, v, tree.Right, uniqueId, keyId, connectLeft)
         End If
     End Sub
 End Class
