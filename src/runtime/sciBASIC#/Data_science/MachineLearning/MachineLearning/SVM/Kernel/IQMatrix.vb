690045a4d6da60a702ea9e6cb3994a49f1cbd1d2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-28 09:04:18 +0800

    SVm kernel object

---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/MachineLearning/SVM/Kernel/IQMatrix.vb
@@ -0,0 +1,9 @@
+﻿
+Namespace SVM
+
+    Friend Interface IQMatrix
+        Function GetQ(ByVal column As Integer, ByVal len As Integer) As Single()
+        Function GetQD() As Double()
+        Sub SwapIndex(ByVal i As Integer, ByVal j As Integer)
+    End Interface
+End Namespace
--- src/runtime/sciBASIC#/Data_science/MachineLearning/MachineLearning/SVM/Kernel/Kernel.vb
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/MachineLearning/SVM/Kernel/Kernel.vb
@@ -1,12 +1,6 @@
-﻿Imports System
-Imports stdNum = System.Math
+﻿Imports stdNum = System.Math
 
 Namespace SVM
-    Friend Interface IQMatrix
-        Function GetQ(ByVal column As Integer, ByVal len As Integer) As Single()
-        Function GetQD() As Double()
-        Sub SwapIndex(ByVal i As Integer, ByVal j As Integer)
-    End Interface
 
     Friend MustInherit Class Kernel
         Implements IQMatrix
