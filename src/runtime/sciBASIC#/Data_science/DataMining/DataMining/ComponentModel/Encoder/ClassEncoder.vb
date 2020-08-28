0d82d9e5b6af2489939e96fd86fd8900f05ae8dd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-28 14:12:14 +0800

    improvements of save to file

--- src/runtime/sciBASIC#/Data_science/DataMining/DataMining/ComponentModel/Encoder/ClassEncoder.vb
+++ src/runtime/sciBASIC#/Data_science/DataMining/DataMining/ComponentModel/Encoder/ClassEncoder.vb
@@ -5,17 +5,38 @@ Namespace ComponentModel.Encoder
 
     Public Class ClassEncoder : Inherits NamedVector
 
-        Dim colors As Dictionary(Of String, ColorClass)
+        Dim m_colors As Dictionary(Of String, ColorClass)
 
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks>
+        ''' apply for save to file
+        ''' </remarks>
+        Public ReadOnly Property Colors As ColorClass()
+            Get
+                Return m_colors.Values.ToArray
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="color"></param>
+        ''' <returns></returns>
+        ''' <remarks>
+        ''' apply for load from file
+        ''' </remarks>
         Public Function AddClass(color As ColorClass) As ClassEncoder
-            colors.Add(color.name, color)
+            m_colors.Add(color.name, color)
             MyBase.Add(color.name, color.enumInt)
 
             Return Me
         End Function
 
         Public Function GetColor(value As Double) As ColorClass
-            Dim min = colors.Values _
+            Dim min = m_colors.Values _
                 .Select(Function(cls)
                             Return (ds:=stdNum.Abs(cls.enumInt - value), cls)
                         End Function) _
