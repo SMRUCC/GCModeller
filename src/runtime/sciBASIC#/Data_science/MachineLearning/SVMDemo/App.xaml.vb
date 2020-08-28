a32a8de275ad4eacb5f79cc8c7c1ac00f44a282e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-28 09:38:47 +0800

    add demo project source
    https://github.com/matajoh/svmnet

---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/SVMDemo/App.xaml.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Windows
+
+Namespace SVMDemo
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+    End Class
+End Namespace
---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/SVMDemo/DataPoint.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Windows
+
+Namespace SVMDemo
+    Public Class DataPoint
+        Public Property Position As Point
+        Public Property Label As Double
+    End Class
+End Namespace
