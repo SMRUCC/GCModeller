4af2c4ac8d0f6a2187faeaf7b3f807260ec947e7
xieguigang <xie.guigang@gcmodeller.org>
2020-09-11 08:54:06 +0800

    css environment?

---
+++ src/runtime/sciBASIC#/mime/text%html/HTML/CSS/CSSEnvirnment.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Drawing
+
+Namespace HTML.CSS
+
+    Public Class CSSEnvirnment
+
+        Public ReadOnly Property baseFont As Font
+
+        Sub New(basefont As Font)
+            Me.baseFont = basefont
+        End Sub
+
+    End Class
+End Namespace
