979d22e2debfa42cc97d3eb5513711cb59f7b176
xieguigang <xie.guigang@gcmodeller.org>
2020-09-11 19:00:16 +0800

    improvements of the css environment

--- src/runtime/sciBASIC#/mime/text%html/HTML/CSS/CSSEnvirnment.vb
+++ src/runtime/sciBASIC#/mime/text%html/HTML/CSS/CSSEnvirnment.vb
@@ -5,10 +5,19 @@ Namespace HTML.CSS
     Public Class CSSEnvirnment
 
         Public ReadOnly Property baseFont As Font
+        Public ReadOnly Property baseLine As Pen
 
-        Sub New(basefont As Font)
+        Sub New(basefont As Font, baseline As Pen)
             Me.baseFont = basefont
+            Me.baseLine = baseline
         End Sub
+
+        Public Function GetFontByScale(em As Single) As Font
+            Dim newSize As Single = em * baseFont.Size
+            Dim newFont As New Font(baseFont.FontFamily, newSize, baseFont.Style)
+
+            Return newFont
+        End Function
 
     End Class
 End Namespace
