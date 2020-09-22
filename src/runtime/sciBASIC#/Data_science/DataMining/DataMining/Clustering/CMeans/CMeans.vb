bf5f87781c74eeee0fbdf8a5896fb66fea020edd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-09-22 17:03:29 +0800

    renames symbol

--- src/runtime/sciBASIC#/Data_science/DataMining/DataMining/Clustering/CMeans/CMeans.vb
+++ src/runtime/sciBASIC#/Data_science/DataMining/DataMining/Clustering/CMeans/CMeans.vb
@@ -8,15 +8,51 @@ Imports Microsoft.VisualBasic.Language
 Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
 
 ''' <summary>
-''' the cmeans algorithm module
+''' ### the cmeans algorithm module
+''' 
+''' **Fuzzy clustering** (also referred to as **soft clustering**) is a form of clustering in which 
+''' each data point can belong to more than one cluster.
+'''
+''' Clustering Or cluster analysis involves assigning data points to clusters (also called buckets, 
+''' bins, Or classes), Or homogeneous classes, such that items in the same class Or cluster are as 
+''' similar as possible, while items belonging to different classes are as dissimilar as possible. 
+''' Clusters are identified via similarity measures. These similarity measures include distance, 
+''' connectivity, And intensity. Different similarity measures may be chosen based on the data Or 
+''' the application.
+''' 
+''' > https://en.wikipedia.org/wiki/Fuzzy_clustering
 ''' </summary>
+''' <remarks>
+''' Clustering problems have applications in **biology**, medicine, psychology, economics, and many other disciplines.
+'''
+''' ##### Bioinformatics
+''' 
+''' In the field of bioinformatics, clustering Is used for a number of applications. One use Is as 
+''' a pattern recognition technique to analyze gene expression data from microarrays Or other 
+''' technology. In this case, genes with similar expression patterns are grouped into the same cluster, 
+''' And different clusters display distinct, well-separated patterns of expression. Use of clustering 
+''' can provide insight into gene function And regulation. Because fuzzy clustering allows genes 
+''' to belong to more than one cluster, it allows for the identification of genes that are conditionally 
+''' co-regulated Or co-expressed. For example, one gene may be acted on by more than one Transcription 
+''' factor, And one gene may encode a protein that has more than one function. Thus, fuzzy clustering 
+''' Is more appropriate than hard clustering.
+''' </remarks>
 Public Module CMeans
-    Public Function CMeans(classCount As Integer, Values As IEnumerable(Of ClusterEntity)) As Classify()
-        Return CMeans(classCount, 2, 0.001, Values.ToArray)
+
+    ''' <summary>
+    ''' **Fuzzy clustering** (also referred to as **soft clustering**) is a form of clustering in which 
+    ''' each data point can belong to more than one cluster.
+    ''' </summary>
+    Public Function CMeans(classCount As Integer, entities As IEnumerable(Of ClusterEntity)) As Classify()
+        Return CMeans(classCount, 2, 0.001, entities.ToArray)
     End Function
 
-    Public Function CMeans(classCount As Integer, m As Double, Values As IEnumerable(Of ClusterEntity)) As Classify()
-        Return CMeans(classCount, m, 0.001, Values.ToArray)
+    ''' <summary>
+    ''' **Fuzzy clustering** (also referred to as **soft clustering**) is a form of clustering in which 
+    ''' each data point can belong to more than one cluster.
+    ''' </summary>
+    Public Function CMeans(classCount As Integer, m As Double, entities As IEnumerable(Of ClusterEntity)) As Classify()
+        Return CMeans(classCount, m, 0.001, entities.ToArray)
     End Function
 
     Private Iterator Function GetRandomMatrix(classCount As Integer, nsamples As Integer) As IEnumerable(Of Double())
@@ -35,21 +71,25 @@ Public Module CMeans
         Next
     End Function
 
-    Public Function CMeans(classCount As Integer, m As Double, diff As Double, Values As ClusterEntity(), Optional parallel As Boolean = True, Optional maxLoop As Integer = 10000) As Classify()
-        Dim u As Double()() = GetRandomMatrix(classCount, nsamples:=Values.Length).ToArray()
+    ''' <summary>
+    ''' **Fuzzy clustering** (also referred to as **soft clustering**) is a form of clustering in which 
+    ''' each data point can belong to more than one cluster.
+    ''' </summary>
+    Public Function CMeans(classCount As Integer, m As Double, threshold As Double, entities As ClusterEntity(), Optional parallel As Boolean = True, Optional maxLoop As Integer = 10000) As Classify()
+        Dim u As Double()() = GetRandomMatrix(classCount, nsamples:=entities.Length).ToArray()
         Dim _j As Double = -1
         Dim centers As Double()()
-        Dim width As Integer = Values(0).Length
+        Dim width As Integer = entities(0).Length
         Dim j_new As Double
         Dim membership_diff As Double
         Dim [loop] As i32 = Scan0
 
         While True
-            centers = GetCenters(classCount, m, u, Values, width).ToArray
-            j_new = J(m, u, centers, Values)
+            centers = GetCenters(classCount, m, u, entities, width).ToArray
+            j_new = J(m, u, centers, entities)
             membership_diff = Math.Abs(j_new - _j)
 
-            If _j <> -1 AndAlso membership_diff < diff Then
+            If _j <> -1 AndAlso membership_diff < threshold Then
                 Exit While
             Else
                 Call $"loop_{[loop]} membership_diff: |{j_new} - {_j}| = {membership_diff}".__DEBUG_ECHO
@@ -58,9 +98,9 @@ Public Module CMeans
             _j = j_new
 
             If parallel Then
-                Call u.updateMembershipParallel(Values, centers, classCount, m)
+                Call u.updateMembershipParallel(entities, centers, classCount, m)
             Else
-                Call u.updateMembership(Values, centers, classCount, m)
+                Call u.updateMembership(entities, centers, classCount, m)
             End If
 
             If ++[loop] > maxLoop Then
@@ -68,13 +108,13 @@ Public Module CMeans
             End If
         End While
 
-        Return Values.PopulateClusters(classCount, u)
+        Return entities.PopulateClusters(classCount, u)
     End Function
 
     <Extension>
-    Private Sub updateMembershipParallel(ByRef u As Double()(), Values As ClusterEntity(), centers As Double()(), classCount As Integer, m As Double)
+    Private Sub updateMembershipParallel(ByRef u As Double()(), entities As ClusterEntity(), centers As Double()(), classCount As Integer, m As Double)
         u = Enumerable.Range(0, u.Length) _
-            .Select(Function(i) (i, Val:=Values(i))) _
+            .Select(Function(i) (i, Val:=entities(i))) _
             .AsParallel _
             .Select(Function(obj)
                         Dim result As Double() = centers.scanRow(
@@ -113,7 +153,7 @@ Public Module CMeans
     End Function
 
     <Extension>
-    Private Sub updateMembership(u As Double()(), Values As ClusterEntity(), centers As Double()(), classCount As Integer, m As Double)
+    Private Sub updateMembership(u As Double()(), entities As ClusterEntity(), centers As Double()(), classCount As Integer, m As Double)
         For i As Integer = 0 To u.Length - 1
             Dim index As Integer = i
 
@@ -121,7 +161,7 @@ Public Module CMeans
                 Dim jIndex As Integer = j
                 Dim sumAll As Double = Aggregate x As Integer
                                        In Enumerable.Range(0, classCount)
-                                       Let a As Double = Math.Sqrt(Dist(Values(index), centers(jIndex))) / Math.Sqrt(Dist(Values(index), centers(x)))
+                                       Let a As Double = Math.Sqrt(Dist(entities(index), centers(jIndex))) / Math.Sqrt(Dist(entities(index), centers(x)))
                                        Let val As Double = a ^ (2 / (m - 1))
                                        Into Sum(val)
                 u(i)(j) = 1 / sumAll
@@ -167,14 +207,14 @@ Public Module CMeans
         Return result
     End Function
 
-    Public Iterator Function GetCenters(classCount As Integer, m As Double, u As Double()(), Values As ClusterEntity(), width As Integer) As IEnumerable(Of Double())
+    Public Iterator Function GetCenters(classCount As Integer, m As Double, u As Double()(), entities As ClusterEntity(), width As Integer) As IEnumerable(Of Double())
         For Each i As Integer In Enumerable.Range(0, classCount)
             Yield Enumerable.Range(0, width) _
                               .[Select](Function(x)
-                                            Dim sumAll = Aggregate j As Integer In Enumerable.Range(0, Values.Count)
-                                                   Let val As Double = Math.Pow(u(j)(i), m) * Values(j)(x)
+                                            Dim sumAll = Aggregate j As Integer In Enumerable.Range(0, entities.Count)
+                                                   Let val As Double = Math.Pow(u(j)(i), m) * entities(j)(x)
                                                    Into Sum(val)
-                                            Dim b = Aggregate j As Integer In Enumerable.Range(0, Values.Count)
+                                            Dim b = Aggregate j As Integer In Enumerable.Range(0, entities.Count)
                                                         Let val As Double = Math.Pow(u(j)(i), m)
                                                            Into Sum(val)
 
@@ -184,21 +224,21 @@ Public Module CMeans
         Next
     End Function
 
-    Public Function J(m As Double, u As Double()(), centers As Double()(), values As ClusterEntity()) As Double
+    Public Function J(m As Double, u As Double()(), centers As Double()(), entities As ClusterEntity()) As Double
         Return centers.Select(Function(x, i)
-                                  Return values.Select(Function(y, j1)
-                                                           Return (u(j1)(i) ^ m) * Dist(y, x)
-                                                       End Function).Sum()
+                                  Return entities.Select(Function(y, j1)
+                                                             Return (u(j1)(i) ^ m) * Dist(y, x)
+                                                         End Function).Sum()
                               End Function).Sum()
     End Function
 
     ''' <summary>
     ''' 在这里面只会计算结果值，并不会修改数据
     ''' </summary>
-    ''' <param name="value"></param>
+    ''' <param name="obj"></param>
     ''' <param name="center"></param>
     ''' <returns></returns>
-    Private Function Dist(value As ClusterEntity, center As Double()) As Double
-        Return value.entityVector.Select(Function(x, i) (x - center(i)) ^ 2).Sum()
+    Private Function Dist(obj As ClusterEntity, center As Double()) As Double
+        Return obj.entityVector.Select(Function(x, i) (x - center(i)) ^ 2).Sum()
     End Function
 End Module
--- src/runtime/sciBASIC#/Data_science/DataMining/DataMining/Clustering/FuzzyCMeans/Algorithm/FuzzyCMeansAlgorithm.vb
+++ src/runtime/sciBASIC#/Data_science/DataMining/DataMining/Clustering/FuzzyCMeans/Algorithm/FuzzyCMeansAlgorithm.vb
@@ -47,34 +47,6 @@ Imports stdNum = System.Math
 
 Namespace FuzzyCMeans
 
-    ''' <summary>
-    ''' **Fuzzy clustering** (also referred to as **soft clustering**) is a form of clustering in which 
-    ''' each data point can belong to more than one cluster.
-    '''
-    ''' Clustering Or cluster analysis involves assigning data points to clusters (also called buckets, 
-    ''' bins, Or classes), Or homogeneous classes, such that items in the same class Or cluster are as 
-    ''' similar as possible, while items belonging to different classes are as dissimilar as possible. 
-    ''' Clusters are identified via similarity measures. These similarity measures include distance, 
-    ''' connectivity, And intensity. Different similarity measures may be chosen based on the data Or 
-    ''' the application.
-    ''' 
-    ''' > https://en.wikipedia.org/wiki/Fuzzy_clustering
-    ''' </summary>
-    ''' <remarks>
-    ''' Clustering problems have applications in **biology**, medicine, psychology, economics, and many other disciplines.
-    '''
-    ''' ##### Bioinformatics
-    ''' 
-    ''' In the field of bioinformatics, clustering Is used for a number of applications. One use Is as 
-    ''' a pattern recognition technique to analyze gene expression data from microarrays Or other 
-    ''' technology. In this case, genes with similar expression patterns are grouped into the same cluster, 
-    ''' And different clusters display distinct, well-separated patterns of expression. Use of clustering 
-    ''' can provide insight into gene function And regulation. Because fuzzy clustering allows genes 
-    ''' to belong to more than one cluster, it allows for the identification of genes that are conditionally 
-    ''' co-regulated Or co-expressed. For example, one gene may be acted on by more than one Transcription 
-    ''' factor, And one gene may encode a protein that has more than one function. Thus, fuzzy clustering 
-    ''' Is more appropriate than hard clustering.
-    ''' </remarks>
     Public Module FuzzyCMeansAlgorithm
 
         ''' <summary>
