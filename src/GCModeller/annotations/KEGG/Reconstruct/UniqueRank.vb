6bf43173f8f6bdaca95f67904ade9197d9cbdb53
xieguigang <xie.guigang@gcmodeller.org>
2020-10-12 15:30:13 +0800

    pathway compound unique ranking

--- src/GCModeller/annotations/KEGG/Reconstruct/UniqueRank.vb
+++ src/GCModeller/annotations/KEGG/Reconstruct/UniqueRank.vb
@@ -1,7 +1,7 @@
 ﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
-Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Text.Xml.Models
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
 
 ''' <summary>
@@ -14,7 +14,7 @@ Public Module UniqueRank
     ''' </summary>
     ''' <param name="pathwayProfile"></param>
     ''' <returns></returns>
-    Public Function EvaluateUniqueRank(pathwayProfile As IEnumerable(Of Pathway)) As DataSet()
+    Public Iterator Function EvaluateUniqueRank(pathwayProfile As IEnumerable(Of Pathway)) As IEnumerable(Of DataSet)
         Dim maps As Pathway() = pathwayProfile.ToArray
         Dim allCompounds As String() = maps _
             .Select(Function(a) a.compound.Keys) _
@@ -32,6 +32,21 @@ Public Module UniqueRank
                                   .ToArray
                           End Function)
 
+        For Each pathway As Pathway In maps
+            Dim unique As New Dictionary(Of String, Double)
+            Dim total = Aggregate cpd As NamedValue
+                        In pathway.compound
+                        Let nmaps = occurs(cpd.name).Length
+                        Into Sum(nmaps)
 
+            For Each cpd As NamedValue In pathway.compound
+                unique(cpd.name) = 1 - occurs(cpd.name).Length / total
+            Next
+
+            Yield New DataSet With {
+                .ID = pathway.EntryId,
+                .Properties = unique
+            }
+        Next
     End Function
 End Module
