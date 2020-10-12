ffb4716c5219639c67456812d55e579710ee53e2
xieguigang <xie.guigang@gcmodeller.org>
2020-10-12 16:56:53 +0800

    evaluate unique rank matrix

--- src/GCModeller/annotations/KEGG/Reconstruct/UniqueRank.vb
+++ src/GCModeller/annotations/KEGG/Reconstruct/UniqueRank.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Text.Xml.Models
@@ -14,19 +15,38 @@ Public Module UniqueRank
     ''' </summary>
     ''' <param name="pathwayProfile"></param>
     ''' <returns></returns>
-    Public Iterator Function EvaluateUniqueRank(pathwayProfile As IEnumerable(Of Pathway)) As IEnumerable(Of DataSet)
+    ''' 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function EvaluateCompoundUniqueRank(pathwayProfile As IEnumerable(Of Pathway)) As IEnumerable(Of DataSet)
+        Return pathwayProfile.EvaluateUniqueRank(Function(map) map.compound.Keys.ToArray)
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function EvaluateEnzymeUniqueRank(pathwayProfile As IEnumerable(Of Pathway)) As IEnumerable(Of DataSet)
+        Return pathwayProfile.EvaluateUniqueRank(Function(map) map.genes.Keys.ToArray)
+    End Function
+
+    ''' <summary>
+    ''' the more pathway of one compound occurs in, the less unique rank of the compound it have
+    ''' </summary>
+    ''' <param name="pathwayProfile"></param>
+    ''' <returns></returns>
+    ''' 
+    <Extension>
+    Public Iterator Function EvaluateUniqueRank(pathwayProfile As IEnumerable(Of Pathway), getObjId As Func(Of Pathway, String())) As IEnumerable(Of DataSet)
         Dim maps As Pathway() = pathwayProfile.ToArray
         Dim allCompounds As String() = maps _
-            .Select(Function(a) a.compound.Keys) _
+            .Select(getObjId) _
             .IteratesALL _
             .Distinct _
             .ToArray
+        Dim mapIndex = maps.ToDictionary(Function(a) a.EntryId, Function(a) getObjId(a).Indexing)
         Dim occurs As Dictionary(Of String, String()) = allCompounds _
             .ToDictionary(Function(a) a,
                           Function(a)
                               Return maps _
                                   .Where(Function(p)
-                                             Return p.IsContainsCompound(a)
+                                             Return mapIndex(p.EntryId).IndexOf(a) > -1
                                          End Function) _
                                   .Select(Function(p) p.EntryId) _
                                   .ToArray
@@ -34,13 +54,13 @@ Public Module UniqueRank
 
         For Each pathway As Pathway In maps
             Dim unique As New Dictionary(Of String, Double)
-            Dim total = Aggregate cpd As NamedValue
-                        In pathway.compound
-                        Let nmaps = occurs(cpd.name).Length
+            Dim total = Aggregate cpd As String
+                        In getObjId(pathway)
+                        Let nmaps = occurs(cpd).Length
                         Into Sum(nmaps)
 
-            For Each cpd As NamedValue In pathway.compound
-                unique(cpd.name) = 1 - occurs(cpd.name).Length / total
+            For Each cpd As String In getObjId(pathway)
+                unique(cpd) = 1 - occurs(cpd).Length / total
             Next
 
             Yield New DataSet With {
