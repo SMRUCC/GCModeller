bbfea84ce0b68de1891e4f0ab18c752a5848613b
xieguigang <xie.guigang@gcmodeller.org>
2020-11-01 13:51:05 +0800

    add https://github.com/rdotnet/dynamic-interop-dll

---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/ApplicationServices/DynamicInterop/IDynamicLibraryLoader.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+
+Namespace ApplicationServices.DynamicInterop
+    ''' <summary>
+    ''' An interface definition to hide the platform specific aspects of library loading
+    ''' </summary>
+    ''' <remarks>There are probably projects 'out there' doing this already, but nothing 
+    ''' is obvious from a quick search. Re-consider again if you need more features.</remarks>
+    Public Interface IDynamicLibraryLoader
+        ''' <summary>
+        ''' Load a native library (DLL on windows, shared libraries on Linux and MacOS)
+        ''' </summary>
+        ''' <paramname="filename">The file name (short file name) of the library to load, e.g. R.dll on Windows</param>
+        ''' <returns></returns>
+        Function LoadLibrary(ByVal filename As String) As IntPtr
+
+        ''' <summary>
+        ''' Gets the last error message from the native API used to load the library.
+        ''' </summary>
+        ''' <returns></returns>
+        Function GetLastError() As String
+
+        ''' <summary>
+        ''' Unloads a library
+        ''' </summary>
+        ''' <paramname="handle">The pointer to the entry point of the library</param>
+        ''' <returns></returns>
+        Function FreeLibrary(ByVal handle As IntPtr) As Boolean
+
+        ''' <summary>
+        ''' Gets a pointer to the address of a native function in the specified loaded library
+        ''' </summary>
+        ''' <paramname="hModule">Handle of the module(library)</param>
+        ''' <paramname="lpProcName">The name of the function sought</param>
+        ''' <returns>Handle to the native function</returns>
+        Function GetFunctionAddress(ByVal hModule As IntPtr, ByVal lpProcName As String) As IntPtr
+    End Interface
+End Namespace
---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/ApplicationServices/DynamicInterop/WindowsLibraryLoader.vb
@@ -0,0 +1,72 @@
+﻿Imports System
+Imports System.ComponentModel
+Imports System.Runtime.ConstrainedExecution
+Imports System.Runtime.InteropServices
+Imports System.Security.Permissions
+Imports System.Text
+
+Namespace ApplicationServices.DynamicInterop
+    <SecurityPermission(SecurityAction.Demand, Flags:=SecurityPermissionFlag.UnmanagedCode)>
+    Friend Class WindowsLibraryLoader
+        Implements IDynamicLibraryLoader
+
+        Public Function LoadLibrary(ByVal filename As String) As IntPtr Implements IDynamicLibraryLoader.LoadLibrary
+            'new SecurityPermission(SecurityPermissionFlag.UnmanagedCode).Demand();
+            Dim handle = Win32.LoadLibrary(filename)
+
+            If handle = IntPtr.Zero Then
+                Dim [error] = New Win32Exception(Marshal.GetLastWin32Error()).Message
+                Console.WriteLine([error])
+            End If
+
+            Return handle
+        End Function
+
+        Public Function GetLastError() As String Implements IDynamicLibraryLoader.GetLastError
+            ' see for instance http://blogs.msdn.com/b/shawnfa/archive/2004/09/10/227995.aspx 
+            ' and http://blogs.msdn.com/b/adam_nathan/archive/2003/04/25/56643.aspx
+            ' TODO: does this work as expected with Mono+Windows stack?
+            Return New Win32Exception().Message
+        End Function
+
+        Public Function FreeLibrary(ByVal handle As IntPtr) As Boolean Implements IDynamicLibraryLoader.FreeLibrary
+            Return Win32.FreeLibrary(handle)
+        End Function
+
+        Public Function GetFunctionAddress(ByVal hModule As IntPtr, ByVal lpProcName As String) As IntPtr Implements IDynamicLibraryLoader.GetFunctionAddress
+            Return GetProcAddress(hModule, lpProcName)
+        End Function
+
+        Public Shared Function GetShortPath(ByVal path As String) As String
+            Dim shortPath = New StringBuilder(MaxPathLength)
+            GetShortPathName(path, shortPath, MaxPathLength)
+            Return shortPath.ToString()
+        End Function
+    End Class
+
+    Friend Module Win32
+        <DllImport("kernel32.dll", SetLastError:=True)>
+        Public Function LoadLibrary(
+        <MarshalAs(UnmanagedType.LPStr)> ByVal lpFileName As String) As IntPtr
+        End Function
+
+        <DllImport("kernel32.dll")>
+        <ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)>
+        <MarshalAs(UnmanagedType.Bool)>
+        Public Function FreeLibrary(ByVal hModule As IntPtr) As Boolean
+        End Function
+
+        <DllImport("kernel32.dll")>
+        Public Function GetProcAddress(ByVal hModule As IntPtr,
+        <MarshalAs(UnmanagedType.LPStr)> ByVal lpProcName As String) As IntPtr
+        End Function
+
+        Public Const MaxPathLength As Integer = 248 'MaxPath is 248. MaxFileName is 260.
+
+        <DllImport("kernel32.dll", CharSet:=CharSet.Auto)>
+        Public Function GetShortPathName(
+        <MarshalAs(UnmanagedType.LPTStr)> ByVal path As String,
+        <MarshalAs(UnmanagedType.LPTStr)> ByVal shortPath As StringBuilder, ByVal shortPathLength As Integer) As Integer
+        End Function
+    End Module
+End Namespace
