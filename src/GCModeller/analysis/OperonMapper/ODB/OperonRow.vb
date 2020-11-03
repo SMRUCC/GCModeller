3b43182641f02128af9b7d2860085dcac02ca268
xieguigang <xie.guigang@gcmodeller.org>
2020-11-03 18:50:50 +0800

    parse the internal ODB resource dataset

---
+++ src/GCModeller/analysis/OperonMapper/ODB/OperonRow.vb
@@ -0,0 +1,73 @@
+﻿#Region "Microsoft.VisualBasic::2e67bb6e4e5b8829ff378ffe4c371121, analysis\OperonMapper\Class1.vb"
+
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+
+' /********************************************************************************/
+
+' Summaries:
+
+' Class Class1
+' 
+' 
+' 
+' /********************************************************************************/
+
+#End Region
+
+Imports Microsoft.VisualBasic.Text
+
+''' <summary>
+''' A row of the data in ODB dataset ``known_operon.download.txt``
+''' </summary>
+Public Class OperonRow
+
+    Public Property koid As String
+    Public Property org As String
+    Public Property name As String
+    Public Property op As String()
+    Public Property definition As String
+    Public Property source As String
+
+    Public Shared Iterator Function LoadInternalResource() As IEnumerable(Of OperonRow)
+        For Each line As String In My.Resources.known_operon_download.LineTokens.Skip(1)
+            Dim tokens As String() = line.Split(ASCII.TAB)
+            Dim operon As New OperonRow With {
+                .koid = tokens(Scan0),
+                .org = tokens(1),
+                .name = tokens(2),
+                .op = tokens(3).StringSplit(","),
+                .definition = tokens(4),
+                .source = tokens(5)
+            }
+
+            Yield operon
+        Next
+    End Function
+
+End Class
+
