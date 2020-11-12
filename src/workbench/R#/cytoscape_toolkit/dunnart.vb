cf798235d1caf7579887fbd1f2610906a8741b7c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-11-12 11:00:45 +0800

    R# package api for create dunnart graph object

---
+++ src/workbench/R#/cytoscape_toolkit/dunnart.vb
@@ -0,0 +1,24 @@
+﻿
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+Imports SMRUCC.genomics.Model.Network.KEGG.Dunnart
+
+<Package("dunnart")>
+Module dunnart
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    <ExportAPI("as.graphObj")>
+    Public Function graphObject(network As NetworkGraph, Optional colorSet As String = "Paired:c12", Optional groupKey As String = "map") As GraphObject
+        Return network.FromNetwork(colorSet, groupKey)
+    End Function
+
+    <ExportAPI("network_map")>
+    Public Function CreateModel(template As NetworkGraph, maps As Pathway(),
+                                Optional desc As Boolean = False,
+                                Optional colorSet As String = "Paired:c12") As GraphObject
+        Return template.CreateModel(maps, desc, colorSet)
+    End Function
+End Module
