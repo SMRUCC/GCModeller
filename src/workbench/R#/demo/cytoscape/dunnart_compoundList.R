acf3fded5bd9b558c84b0eb38934fe834e87c843
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-11-12 11:41:35 +0800

    demo network create from a givne compound list

---
+++ src/workbench/R#/demo/cytoscape/dunnart_compoundList.R
@@ -0,0 +1,16 @@
+imports ["dunnart", "cytoscape.kegg"] from "cytoscape_toolkit";
+imports "kegg.repository" from "kegg_kit";
+
+setwd(!script$dir);
+
+let compounds = read.csv("doMSMSalignment.report1.csv")[, "KEGG"] :> as.character :> unique;
+let maps = read.KEGG_pathway("D:\biodeep\biodeep_pipeline\Biodeep_Rpackage\gsea\kegg\hsa");
+
+"D:\biodeep\biodeepdb_v3\KEGG\br08201.csv"
+:> reactions.table
+:> compounds.network(compounds = compounds[!(compounds in ["NULL", "NA"])])
+:> network_map(maps)
+:> as.graphObj
+:> json
+:> writeLines(con = "./demo.json")
+;
