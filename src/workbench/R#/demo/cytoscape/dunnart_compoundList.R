eba57a18baa0a665989b2a9ffbdaca41e7c2c1f9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-11-12 11:46:36 +0800

    run demo test success

--- src/workbench/R#/demo/cytoscape/dunnart_compoundList.R
+++ src/workbench/R#/demo/cytoscape/dunnart_compoundList.R
@@ -6,6 +6,8 @@ setwd(!script$dir);
 let compounds = read.csv("doMSMSalignment.report1.csv")[, "KEGG"] :> as.character :> unique;
 let maps = read.KEGG_pathway("D:\biodeep\biodeep_pipeline\Biodeep_Rpackage\gsea\kegg\hsa");
 
+print(compounds);
+
 "D:\biodeep\biodeepdb_v3\KEGG\br08201.csv"
 :> reactions.table
 :> compounds.network(compounds = compounds[!(compounds in ["NULL", "NA"])])
