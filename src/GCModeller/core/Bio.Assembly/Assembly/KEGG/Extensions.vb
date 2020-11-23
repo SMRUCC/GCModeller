0b86221e2a0064d7ce700ddacd3a29db7c5d20ca
xieguigang <xie.guigang@gcmodeller.org>
2020-11-23 21:39:00 +0800

    improvements of kegg enrichment plots api

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Extensions.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Extensions.vb
@@ -1,48 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::c091fbaf08e01fce06e6d26fa2e25269, core\Bio.Assembly\Assembly\KEGG\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module Extensions
-    ' 
-    '         Function: DirectGetChEBI, GetIDpairedList, GetPathwayBrite, Glycan2CompoundId, IDlistStrings
-    '                   LevelAKOStatics, RemarksTable, SingleID, (+2 Overloads) TheSameAs, ValidateEntryFormat
-    '         Interface IKEGGRemarks
-    ' 
-    '             Properties: Remarks
-    ' 
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module Extensions
+' 
+'         Function: DirectGetChEBI, GetIDpairedList, GetPathwayBrite, Glycan2CompoundId, IDlistStrings
+'                   LevelAKOStatics, RemarksTable, SingleID, (+2 Overloads) TheSameAs, ValidateEntryFormat
+'         Interface IKEGGRemarks
+' 
+'             Properties: Remarks
+' 
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -54,6 +54,7 @@ Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.BriteHEntry
+Imports SMRUCC.genomics.ComponentModel.Annotation
 
 Namespace Assembly.KEGG
 
@@ -254,6 +255,29 @@ Namespace Assembly.KEGG
         Public Function GetPathwayBrite(table As Dictionary(Of String, Pathway), ID$) As Pathway
             ID = Regex.Match(ID, "\d+").Value
             Return table.TryGetValue(ID)
+        End Function
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="profiles">[kegg_pathwayMapId -> value]</param>
+        ''' <param name="displays"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function DoKeggProfiles(profiles As Dictionary(Of String, Double), displays%) As CatalogProfiles
+            Dim data = profiles _
+                .KEGGCategoryProfiles _
+                .Where(Function(cls) Not cls.Value.IsNullOrEmpty) _
+                .ToDictionary(Function(p) p.Key,
+                              Function(group)
+                                  Return group.Value _
+                                      .OrderByDescending(Function(t) t.Value) _
+                                      .Take(displays) _
+                                      .ToArray
+                              End Function)
+            Dim profiler As New CatalogProfiles(data)
+
+            Return profiler
         End Function
 
         ''' <summary>
---
+++ src/workbench/R#/gseakit/Profiles.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Analysis.GO
+Imports SMRUCC.genomics.Analysis.Microarray.KOBAS
+Imports SMRUCC.genomics.Assembly.KEGG
+Imports SMRUCC.genomics.ComponentModel.Annotation
+Imports SMRUCC.genomics.Data.GeneOntology.OBO
+
+<Package("profiles")>
+Module profiles
+
+    ''' <summary>
+    ''' Create catalog profiles data for GO enrichment result its data visualization.
+    ''' </summary>
+    ''' <param name="enrichments"></param>
+    ''' <param name="goDb"></param>
+    ''' <param name="top">display the top n enriched GO terms.</param>
+    ''' <returns></returns>
+    <ExportAPI("GO.enrichment.profile")>
+    Public Function GOEnrichmentProfiles(enrichments As EnrichmentTerm(), goDb As GO_OBO, Optional top% = 10) As CatalogProfiles
+        Dim GO_terms = goDb.AsEnumerable.ToDictionary
+        ' 在这里是不进行筛选的
+        ' 筛选应该是发生在脚本之中
+        Dim profiles = enrichments.CreateEnrichmentProfiles(GO_terms, False, top, 1)
+
+        Return profiles
+    End Function
+
+    <ExportAPI("KEGG.enrichment.profile")>
+    Public Function KEGGEnrichmentProfiles(enrichments As EnrichmentTerm(), Optional top% = 10) As CatalogProfiles
+        Dim profiles As Dictionary(Of String, Double) = enrichments.ToDictionary(Function(a) a.ID, Function(a) -Math.Log10(a.Pvalue))
+        Dim result As CatalogProfiles = profiles.DoKeggProfiles(displays:=top)
+
+        Return result
+    End Function
+End Module
