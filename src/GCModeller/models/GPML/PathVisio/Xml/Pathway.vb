f81f29b2e2d0c20231364829a011bb50922383c8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-11-30 17:13:48 +0800

    split source files

---
+++ src/GCModeller/models/GPML/PathVisio/Xml/Pathway.vb
@@ -0,0 +1,82 @@
+﻿Imports System.Xml.Serialization
+
+Namespace GPML
+
+    Public Class Pathway
+
+        <XmlAttribute> Public Property Name As String
+        <XmlAttribute> Public Property Version As String
+        <XmlAttribute> Public Property Organism As String
+
+        Public Property Comment As Comment
+
+        <XmlElement>
+        Public Property BiopaxRef As String()
+
+    End Class
+
+    Public Class Comment
+
+        <XmlAttribute> Public Property Source As String
+        <XmlText> Public Property Text As String
+
+    End Class
+
+    Public Class Interaction
+
+        <XmlAttribute>
+        Public Property GraphId As String
+        Public Property BiopaxRef As String
+        Public Property Graphics As Graphics
+        Public Property Xref As Xref
+
+    End Class
+
+
+
+    Public Class PublicationXref
+
+        <XmlAttribute>
+        Public Property id As String
+    End Class
+
+    Public Class Group
+
+        <XmlAttribute> Public Property GroupId As String
+        <XmlAttribute> Public Property GraphId As String
+        <XmlAttribute> Public Property Style As String
+
+    End Class
+
+    Public Class Point
+
+        <XmlAttribute> Public Property X As Double
+        <XmlAttribute> Public Property Y As Double
+        <XmlAttribute> Public Property GraphRef As String
+        <XmlAttribute> Public Property RelX As Double
+        <XmlAttribute> Public Property RelY As Double
+        <XmlAttribute> Public Property ArrowHead As String
+
+    End Class
+
+    Public Class DataNode
+
+        <XmlAttribute> Public Property TextLabel As String
+        <XmlAttribute> Public Property GraphId As String
+        <XmlAttribute> Public Property Type As String
+        <XmlAttribute> Public Property GroupRef As String
+
+        Public Property Comment As Comment
+        Public Property Graphics As Graphics
+        Public Property Xref As Xref
+
+    End Class
+
+    Public Class Xref
+
+        <XmlAttribute> Public Property Database As String
+        <XmlAttribute> Public Property ID As String
+
+    End Class
+
+End Namespace
