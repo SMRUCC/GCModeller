fe48e3a0f60f9d7b45d05588ba759654c6216893
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-12 15:06:46 +0800

    reconstruct of the 3D scatter plot

--- src/runtime/sciBASIC#/Data_science/Visualization/Visualization/UMAP/Umap3D.vb
+++ src/runtime/sciBASIC#/Data_science/Visualization/Visualization/UMAP/Umap3D.vb
@@ -1,8 +1,11 @@
-﻿Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+Imports Microsoft.VisualBasic.Data.ChartPlots.Plot3D
 Imports Microsoft.VisualBasic.DataMining.UMAP
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
 Imports Microsoft.VisualBasic.Imaging.Drawing3D
+Imports Microsoft.VisualBasic.Linq
 
 Public Class Umap3D : Inherits UmapRender
 
@@ -12,5 +15,22 @@ Public Class Umap3D : Inherits UmapRender
 
     Protected Overrides Sub PlotInternal(ByRef g As IGraphics, canvas As GraphicsRegion)
         Dim embeddings As Point3D() = umap.GetPoint3D
+        Dim clusterSerials As New Dictionary(Of String, List(Of NamedValue(Of Point3D)))
+        Dim clusterName As String
+
+        For i As Integer = 0 To embeddings.Length - 1
+            clusterName = clusters(labels(i))
+
+            If Not clusterSerials.ContainsKey(clusterName) Then
+                clusterSerials.Add(clusterName, New List(Of NamedValue(Of Point3D)))
+            End If
+
+            Call New NamedValue(Of Point3D) With {
+                .Name = labels(i),
+                .Value = embeddings(i)
+            }.DoCall(AddressOf clusterSerials(clusterName).Add)
+        Next
+
+        Call clusterSerials.Select(Function(cluster) New Serial3D With {.Points = cluster.Value.ToArray}).Plot()
     End Sub
 End Class
