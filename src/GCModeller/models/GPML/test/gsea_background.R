972f170f403ec726b5851d6679c566822157c6e2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-14 13:19:58 +0800

    add lipidmaps

---
+++ src/GCModeller/models/GPML/test/gsea_background.R
@@ -0,0 +1,19 @@
+# Slenter DN, Kutmon M, Hanspers K, Riutta A, Windsor J, Nunes N, Mélius J, Cirillo E, Coort SL, Digles D, Ehrhart F, Giesbertz P, Kalafati M, Martens M, Miller R, Nishida K, Rieswijk L, Waagmeester A, Eijssen LMT, Evelo CT, Pico AR, Willighagen EL.
+# WikiPathways: a multifaceted pathway database bridging metabolomics to other omics research Nucleic Acids Research, (2017)
+# DOI: 10.1093/nar/gkx1064  PMCID: PMC5753270
+
+imports "PathVisio" from "cytoscape_toolkit";
+imports "background" from "gseakit";
+
+"D:\GCModeller\src\GCModeller\models\GPML\data"
+:> list.files(pattern = "*.gpml")
+:> lapply(function(filepath) {
+	
+	let pathway = as.object(read.gpml(filepath));
+	let lipids = pathway :> nodes.table;
+	
+	lipids = lipids[lipids[, "database"] == "LIPID MAPS", ];
+	lipids = lipids[lipids[, "dbref"] != "", ];
+
+	
+})
