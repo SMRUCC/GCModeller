658287d1bc2d7cbdfbc357e22d86e5dca3f264bd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-14 17:36:54 +0800

    create btree by data alignment

--- src/runtime/sciBASIC#/Data_science/DataMining/BinaryTree/Comparison.vb
+++ src/runtime/sciBASIC#/Data_science/DataMining/BinaryTree/Comparison.vb
@@ -1,37 +1,17 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Math.DataFrame
 
-Public Class Comparison
+Public Class Comparison : Inherits ComparisonProvider
 
     ReadOnly d As DistanceMatrix
-    ReadOnly equalsDbl As Double
-    ReadOnly gt As Double
 
     Sub New(d As DistanceMatrix, equals As Double, gt As Double)
+        MyBase.New(equals, gt)
         Me.d = d
-        Me.equalsDbl = equals
-        Me.gt = gt
     End Sub
 
-    Public Function Compares(x As String, y As String) As Integer
-        Dim similarity As Double = d(x, y)
-
-        If similarity >= equalsDbl Then
-            Return 0
-        ElseIf similarity >= gt Then
-            Return 1
-        Else
-            Return -1
-        End If
-    End Function
-
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
-    Public Function GetComparer() As Comparison(Of String)
-        Return Me
+    Protected Overrides Function GetSimilarity(x As String, y As String) As Double
+        Return d(x, y)
     End Function
-
-    <MethodImpl(MethodImplOptions.AggressiveInlining)>
-    Public Shared Narrowing Operator CType(c As Comparison) As Comparison(Of String)
-        Return New Comparison(Of String)(AddressOf c.Compares)
-    End Operator
 End Class
