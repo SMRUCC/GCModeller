e8cb92b1510d70f27998c9a471e74d6c96961acc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-20 11:16:17 +0800

    split source file modules

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/MapDownloader.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/MapDownloader.vb
@@ -50,7 +50,6 @@ Imports System.Runtime.CompilerServices
 Imports System.Threading
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar
 Imports Microsoft.VisualBasic.Language
-Imports Microsoft.VisualBasic.Net.Http
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.LinkDB
@@ -58,51 +57,6 @@ Imports PathwayEntry = SMRUCC.genomics.Assembly.KEGG.DBGET.BriteHEntry.Pathway
 Imports r = System.Text.RegularExpressions.Regex
 
 Namespace Assembly.KEGG.WebServices
-
-    Public Class MapQuery : Inherits WebQuery(Of PathwayEntry)
-
-        Public Sub New(briteFile$,
-                       <CallerMemberName>
-                       Optional cache As String = Nothing,
-                       Optional interval As Integer = -1,
-                       Optional offline As Boolean = False)
-
-            Call MyBase.New(
-                createUrl(briteFile),
-                AddressOf getID(briteFile).Invoke,
-                AddressOf ParseHtml,
-                prefix:=Nothing,
-                cache:=cache,
-                interval:=interval,
-                offline:=offline
-            )
-        End Sub
-
-        Friend Shared Function getID(briteFile As String) As Func(Of PathwayEntry, String)
-            Return Function(entry As PathwayEntry)
-                       If briteFile.StringEmpty Then
-                           Return "map" & entry.EntryId
-                       Else
-                           Dim s = entry.entry.text
-                           s = r.Match(s, "\[PATH:.+?\]", RegexICSng).Value
-                           s = s.GetStackValue("[", "]").Split(":"c).Last
-                           Return s
-                       End If
-                   End Function
-        End Function
-
-        Private Shared Function ParseHtml(html$, schema As Type) As Object
-            Return Map.ParseHTML(html)
-        End Function
-
-        Private Shared Function createUrl(briteFile As String) As Func(Of PathwayEntry, String)
-            Dim getID = MapQuery.getID(briteFile)
-
-            Return Function(entry)
-                       Return $"http://www.genome.jp/kegg-bin/show_pathway?{getID(entry)}"
-                   End Function
-        End Function
-    End Class
 
     ''' <summary>
     ''' KEGG reference map downloader API
