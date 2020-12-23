e7f850845c9fb216ae561730ae5ada6245cf40ca
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-23 12:32:10 +0800

    Update Extensions.vb

--- src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/Drawing3D/Models/Extensions.vb
+++ src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/Drawing3D/Models/Extensions.vb
@@ -46,10 +46,12 @@ Imports Microsoft.VisualBasic.Imaging.Drawing3D.Models.Isometric
 
 Namespace Drawing3D.Models
 
+    <HideModuleName>
     Public Module Extensions
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        <Extension> Public Function Model3D(path As Path3D, color As Color) As Surface
+        <Extension>
+        Public Function Model3D(path As Path3D, color As Color) As Surface
             Return New Surface With {
                 .brush = New SolidBrush(color),
                 .vertices = path.Points.ToArray
@@ -57,9 +59,9 @@ Namespace Drawing3D.Models
         End Function
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        <Extension> Public Function Model3D(shape As Shape3D, color As Color) As Surface()
-            Return shape _
-                .paths _
+        <Extension>
+        Public Function Model3D(shape As Shape3D, color As Color) As Surface()
+            Return shape.paths _
                 .Select(Function(path) path.Model3D(color)) _
                 .ToArray
         End Function
