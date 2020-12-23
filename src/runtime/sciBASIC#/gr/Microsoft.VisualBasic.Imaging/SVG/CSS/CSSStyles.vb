0dc2261c00dc9b8e4220a43b9ebe2b3a91774c19
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-23 12:38:51 +0800

    split source files

---
+++ src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/SVG/CSS/CSSStyles.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.MIME.Markup.HTML
+Imports Microsoft.VisualBasic.MIME.Markup.HTML.XmlMeta
+
+Namespace SVG.CSS
+
+    ''' <summary>
+    ''' 在这个SVG对象之中所定义的CSS样式数据
+    ''' </summary>
+    Public Class CSSStyles : Inherits Node
+
+        <XmlElement("linearGradient")>
+        Public Property linearGradients As linearGradient()
+        <XmlElement("radialGradient")>
+        Public Property radialGradients As radialGradient()
+        <XmlElement("style")>
+        Public Property styles As XmlMeta.CSS()
+        <XmlElement("filter")>
+        Public Property filters As Filter()
+
+    End Class
+End Namespace
