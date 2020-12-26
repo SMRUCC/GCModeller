04ebe7d0a13b6c469243f88a98a4211e9285ad62
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-26 10:20:09 +0800

    generic sequence and symbols

---
+++ src/runtime/sciBASIC#/Data_science/DataMining/DynamicProgramming/Generic/GenericSymbol.vb
@@ -0,0 +1,13 @@
+﻿Public Class GenericSymbol(Of T)
+
+    Friend ReadOnly m_equals As Func(Of T, T, Boolean)
+    Friend ReadOnly m_similarity As Func(Of T, T, Double)
+    Friend ReadOnly m_viewChar As Func(Of T, Char)
+
+    Sub New(equals As Func(Of T, T, Boolean), similarity As Func(Of T, T, Double), toChar As Func(Of T, Char))
+        Me.m_equals = equals
+        Me.m_similarity = similarity
+        Me.m_viewChar = toChar
+    End Sub
+
+End Class
