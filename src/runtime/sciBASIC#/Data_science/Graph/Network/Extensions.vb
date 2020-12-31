3eaf124c6dd47731a73688114008b41159862a37
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-31 17:31:28 +0800

    fix of type mis-match

--- src/runtime/sciBASIC#/Data_science/Graph/Network/Extensions.vb
+++ src/runtime/sciBASIC#/Data_science/Graph/Network/Extensions.vb
@@ -92,7 +92,7 @@ Namespace Network
         Public Function IteratesSubNetworks(Of Node As {New, Network.Node},
                                                U As {New, Edge(Of Node)},
                                                Graph As {New, NetworkGraph(Of Node, U)}
-                                            )(network As NetworkGraph(Of Node, U), Optional singleNodeAsGraph As Boolean = False) As IEnumerable(Of Graph)
+                                            )(network As NetworkGraph(Of Node, U), Optional singleNodeAsGraph As Boolean = False) As Graph()
 
             Return New SubNetworkComponents(Of Node, U, Graph)(network, singleNodeAsGraph).ToArray
         End Function
