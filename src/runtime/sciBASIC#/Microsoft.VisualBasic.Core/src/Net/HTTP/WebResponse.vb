169208878ac6ed7f71b3bf08bb182c2c0c92418b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-21 16:48:34 +0800

    htttp web response

---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/Net/HTTP/WebResponse.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Net
+
+Namespace Net.Http
+
+    Public Class WebResponseResult
+
+        Public Property html As String
+        Public Property headers As ResponseHeaders
+        Public Property timespan As Long
+        Public Property url As String
+
+    End Class
+
+    Public Class ResponseHeaders
+
+        Public Property headers As New Dictionary(Of HttpHeaderName, String)
+        Public Property customHeaders As New Dictionary(Of String, String)
+
+        Sub New(raw As WebHeaderCollection)
+            Dim header As HttpHeaderName
+
+            For Each key As String In raw.AllKeys
+                header = ParseHeaderName(key)
+
+                If header = HttpHeaderName.Unknown Then
+                    customHeaders(key) = raw.Get(key)
+                Else
+                    headers(header) = raw.Get(key)
+                End If
+            Next
+        End Sub
+
+    End Class
+End Namespace
