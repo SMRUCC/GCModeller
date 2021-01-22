469eb4d4a0a82a41d379f4f74fb0cf6ff93ff880
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-22 08:54:22 +0800

    fix of the http headers data model

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/Net/HTTP/WebResponse.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/Net/HTTP/WebResponse.vb
@@ -16,6 +16,8 @@ Namespace Net.Http
         Public Property headers As New Dictionary(Of HttpHeaderName, String)
         Public Property customHeaders As New Dictionary(Of String, String)
 
+        Dim stringIndex As New Dictionary(Of String, String)
+
         Sub New(raw As WebHeaderCollection)
             Dim header As HttpHeaderName
 
@@ -27,8 +29,17 @@ Namespace Net.Http
                 Else
                     headers(header) = raw.Get(key)
                 End If
+
+                stringIndex(key.ToLower) = raw.Get(key)
             Next
         End Sub
 
+        Public Function TryGetValue(header As HttpHeaderName) As String
+            Return headers.TryGetValue(header)
+        End Function
+
+        Public Function TryGetValue(header As String) As String
+            Return stringIndex.TryGetValue(Strings.LCase(header))
+        End Function
     End Class
 End Namespace
