5718bc74c68c018d3ee9736067c88b9344a1a2b1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-28 18:38:11 +0800

    run gene tree plot

--- src/GCModeller/annotations/WGCNA/test/Module1.vb
+++ src/GCModeller/annotations/WGCNA/test/Module1.vb
@@ -1,4 +1,5 @@
-﻿Imports SMRUCC.genomics.Analysis.HTS
+﻿Imports Microsoft.VisualBasic.DataMining.HierarchicalClustering
+Imports SMRUCC.genomics.Analysis.HTS
 Imports SMRUCC.genomics.Analysis.HTS.DataFrame
 
 Module Module1
@@ -6,6 +7,8 @@ Module Module1
     Sub Main()
         Dim raw As Matrix = Matrix.LoadData("D:\GCModeller\src\GCModeller\annotations\WGCNA\metabolome.csv")
         Dim out = WGCNA.Analysis.Run(raw)
+
+        Call out.hclust.Plot(layout:=Layouts.Horizon).Save("D:\GCModeller\src\GCModeller\annotations\WGCNA\metabolome.png")
 
         Pause()
     End Sub
