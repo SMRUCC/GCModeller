dfd036a13123f1c3206262af2e5995905a276a22
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-28 14:12:08 +0800

    TOM matrix

---
+++ src/GCModeller/annotations/WGCNA/WGCNA/Analysis.vb
@@ -0,0 +1,8 @@
+﻿Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+
+Public Module Analysis
+
+    Public Function Run(samples As Matrix)
+
+    End Function
+End Module
--- src/GCModeller/annotations/WGCNA/WGCNA/TOM.vb
+++ src/GCModeller/annotations/WGCNA/WGCNA/TOM.vb
@@ -1,5 +1,7 @@
-﻿
+﻿Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
+Imports stdNum = System.Math
+
 ''' <summary>
 ''' Category 2: Functions for module detection.
 ''' 
@@ -9,19 +11,40 @@ Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
 ''' </summary>
 Public Module TOM
 
+    ''' <summary>
+    ''' I矩阵
+    ''' </summary>
+    ''' <param name="A"></param>
+    ''' <returns></returns>
     Public Function Intermediate(A As GeneralMatrix) As GeneralMatrix
         Dim Iu As New GeneralMatrix(A.RowDimension, A.ColumnDimension)
         Dim x As Double()() = Iu.Array
         Dim m As Integer = A.RowDimension
         Dim n As Integer = A.ColumnDimension
-        Dim buffer As Double()() = A.Array
+        Dim alpha As Double()() = A.Array
 
-        For i As Integer = 0 To m - 1
-            For j As Integer = 0 To n - 1
-                x(i)(j) = buffer(i)(j)
+        For u As Integer = 0 To m - 1
+            For i As Integer = 0 To m - 1
+                For j As Integer = 0 To n - 1
+                    x(i)(j) += alpha(i)(u) * alpha(j)(u)
+                Next
             Next
         Next
 
         Return Iu
+    End Function
+
+    Public Function TOM(A As GeneralMatrix, K As Vector) As GeneralMatrix
+        Dim Imat As GeneralMatrix = Intermediate(A)
+        Dim W As New GeneralMatrix(A.RowDimension, A.ColumnDimension)
+        Dim wmat As Double()() = W.Array
+
+        For i As Integer = 0 To wmat.Length - 1
+            For j As Integer = 0 To wmat.Length - 1
+                wmat(i)(j) = (Imat(i, j) + A(i, j)) / (stdNum.Min(K(i), K(j)) + 1 - A(i, j))
+            Next
+        Next
+
+        Return W
     End Function
 End Module
