1aa51977255e3bc57e405101c00d95dc34d71bb9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-28 16:46:20 +0800

    Update Analysis.vb

--- src/GCModeller/annotations/WGCNA/WGCNA/Analysis.vb
+++ src/GCModeller/annotations/WGCNA/WGCNA/Analysis.vb
@@ -1,8 +1,10 @@
-﻿Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+﻿Imports Microsoft.VisualBasic.Math.DataFrame
+Imports SMRUCC.genomics.Analysis.HTS.DataFrame
 
 Public Module Analysis
 
     Public Function Run(samples As Matrix) As Result
+        Dim cor As CorrelationMatrix = samples.Correlation(Function(gene) gene.experiments)
 
     End Function
 End Module
