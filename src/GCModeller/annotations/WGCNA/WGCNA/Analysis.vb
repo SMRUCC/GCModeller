199263bc69ff0db86c5cc0ea570ce2c71c3dfa8a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-28 17:54:52 +0800

    create network result

--- src/GCModeller/annotations/WGCNA/WGCNA/Analysis.vb
+++ src/GCModeller/annotations/WGCNA/WGCNA/Analysis.vb
@@ -3,6 +3,9 @@ Imports SMRUCC.genomics.Analysis.HTS.DataFrame
 Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.DataMining.HierarchicalClustering
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 
 Public Module Analysis
 
@@ -13,7 +16,38 @@ Public Module Analysis
         Dim network As GeneralMatrix = cor.WeightedCorrelation(beta.Power, pvalue:=False)
         Dim K As New Vector(network.RowApply(AddressOf WeightedNetwork.sumK))
         Dim tomMat As GeneralMatrix = TOM.Matrix(network, K)
-        Dim dist As GeneralMatrix = 1 - tomMat
+        Dim dist As New DistanceMatrix(samples.expression.Keys, 1 - tomMat)
+        Dim alg As ClusteringAlgorithm = New DefaultClusteringAlgorithm With {.debug = True}
+        Dim matrix As Double()() = dist.PopulateRows _
+            .Select(Function(a) a.ToArray) _
+            .ToArray
+        Dim cluster As Cluster = alg.performClustering(matrix, dist.keys, New AverageLinkageStrategy)
 
+        Return New Result With {
+            .beta = beta,
+            .hclust = cluster,
+            .K = K,
+            .network = createGraph(network, samples),
+            .TOM = tomMat
+        }
+    End Function
+
+    Private Function createGraph(mat As GeneralMatrix, samples As Matrix) As NetworkGraph
+        Dim geneId As String() = samples.expression.Keys.ToArray
+        Dim g As New NetworkGraph
+
+        For Each id As String In geneId
+            Call g.CreateNode(id)
+        Next
+
+        For i As Integer = 0 To geneId.Length - 1
+            For j As Integer = 0 To geneId.Length - 1
+                If i <> j AndAlso mat(i, j) <> 0.0 Then
+                    Call g.AddEdge(geneId(i), geneId(j), weight:=mat(i, j))
+                End If
+            Next
+        Next
+
+        Return g
     End Function
 End Module
--- src/GCModeller/annotations/WGCNA/WGCNA/Result.vb
+++ src/GCModeller/annotations/WGCNA/WGCNA/Result.vb
@@ -1,3 +1,14 @@
-﻿Public Class Result
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.DataMining.HierarchicalClustering
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
+
+Public Class Result
+
+    Public Property beta As BetaTest
+    Public Property network As NetworkGraph
+    Public Property K As Vector
+    Public Property TOM As GeneralMatrix
+    Public Property hclust As Cluster
 
 End Class
