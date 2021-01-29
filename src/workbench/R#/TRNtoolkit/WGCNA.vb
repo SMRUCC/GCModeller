3d5b413ed7d6d32c0dfcad6b0d9b2f56f3abebd3
xieguigang <xie.guigang@gcmodeller.org>
2021-01-30 00:37:17 +0800

    combine TRN with WGCNA result

---
+++ src/workbench/R#/TRNtoolkit/WGCNA.vb
@@ -0,0 +1,6 @@
+﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("WGCNA")>
+Module WGCNA
+
+End Module
---
+++ src/workbench/R#/phenotype_kit/WGCNA.vb
@@ -0,0 +1,7 @@
+﻿
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("WGCNA")>
+Module WGCNA
+
+End Module
