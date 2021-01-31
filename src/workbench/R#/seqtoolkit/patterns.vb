66e802fd3957011631f50742d65198b81b15c9b9
xieguigang <xie.guigang@gcmodeller.org>
2021-01-31 19:52:48 +0800

    read motif sites

--- src/workbench/R#/seqtoolkit/patterns.vb
+++ src/workbench/R#/seqtoolkit/patterns.vb
@@ -1,49 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::6aeb0289de09c761e95dd80161b5431e, seqtoolkit\patterns.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module patterns
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: DrawLogo, FindMirrorPalindromes, GetMotifs, GetSeeds, matchSites
-    '               PalindromeToString, readMotifs, ScaffoldOrthogonality, viewSites
-    ' 
-    ' /********************************************************************************/
+' Module patterns
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: DrawLogo, FindMirrorPalindromes, GetMotifs, GetSeeds, matchSites
+'               PalindromeToString, readMotifs, ScaffoldOrthogonality, viewSites
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
 Imports System.Text
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Imaging.Driver
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
@@ -150,6 +151,11 @@ Module patterns
     <ExportAPI("read.motifs")>
     Public Function readMotifs(file As String) As SequenceMotif()
         Return file.LoadJSON(Of SequenceMotif())
+    End Function
+
+    <ExportAPI("read.scans")>
+    Public Function readSites(file As String) As MotifMatch()
+        Return file.LoadCsv(Of MotifMatch).ToArray
     End Function
 
     ''' <summary>
