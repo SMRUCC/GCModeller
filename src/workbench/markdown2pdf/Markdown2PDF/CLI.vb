e6ed7614b30479360bd8c150a7eb162698943223
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-02-19 15:51:11 +0800

    cli module for convert files

---
+++ src/workbench/markdown2pdf/Markdown2PDF/CLI.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.MIME.Markup.MarkDown
+Imports WkHtmlToPdf
+Imports WkHtmlToPdf.Arguments
+
+Module CLI
+
+    <ExportAPI("/glob")>
+    <Usage("/glob /src <dir of *.md> [/pdf <output.pdf>]")>
+    Public Function glob(args As CommandLine) As Integer
+        Dim src$ = args <= "/src"
+        Dim pdf$ = args("/pdf") Or $"{src.TrimDIR}.pdf"
+        Dim render As New MarkdownHTML()
+        Dim tmp As String = App.GetAppSysTempFile
+
+        Call src.FileCopy(tmp & "/")
+
+        Dim mdlist As String() = tmp _
+            .ListFiles("*.md") _
+            .OrderBy(Function(path)
+                         Return path.BaseName
+                     End Function) _
+            .ToArray
+
+        For Each file As String In mdlist
+            Call file _
+                .ReadAllText _
+                .DoCall(AddressOf render.Transform) _
+                .SaveTo(file.ChangeSuffix("html"))
+        Next
+
+        Dim srcDoc As New PdfDocument With {
+            .Url = mdlist _
+                .Select(Function(file)
+                            Return file.ChangeSuffix("html")
+                        End Function) _
+                .ToArray
+        }
+
+        Call PdfConvert.ConvertHtmlToPdf(
+            document:=srcDoc,
+            output:=New PdfOutput With {
+                .OutputFilePath = pdf
+            }
+        )
+
+        Return 0
+    End Function
+End Module
