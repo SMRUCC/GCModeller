595a3ee3daf08dab5aa9e09413bd62d41a23b02b
xieguigang <xie.guigang@gcmodeller.org>
2021-02-26 21:51:53 +0800

    kegg reaction data

---
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/DBGET/Objects/Pathway/Reaction/ReactionCompoundTransform.vb
@@ -0,0 +1,27 @@
+﻿
+Imports System.Xml.Serialization
+
+Namespace Assembly.KEGG.DBGET.bGetObject
+
+    ''' <summary>
+    ''' 反应左端代谢物在经过了代谢反应之后结果上的转换变化的结果（反应的右端）
+    ''' </summary>
+    Public Class ReactionCompoundTransform
+
+        ''' <summary>
+        ''' the kegg compound id
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Overridable Property from As String
+
+        ''' <summary>
+        ''' the kegg compound id
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Overridable Property [to] As String
+
+        Public Overrides Function ToString() As String
+            Return $"{from}->{[to]}"
+        End Function
+    End Class
+End Namespace
