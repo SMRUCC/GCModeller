6eb71eef8aef4677b337dc954eb7fdfb0ceac975
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-03-08 12:42:04 +0800

    add xml dev notes

--- src/runtime/sciBASIC#/Data/BinaryData/msgpack/Serialization/SchemaProvider.vb
+++ src/runtime/sciBASIC#/Data/BinaryData/msgpack/Serialization/SchemaProvider.vb
@@ -8,6 +8,15 @@ Namespace Serialization
         Function GetMembers() As IEnumerable(Of BindProperty(Of MessagePackMemberAttribute))
     End Interface
 
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <typeparam name="T"></typeparam>
+    ''' <remarks>
+    ''' 这个模块是为了处理元素类型定义信息和序列化代码调用模块之间没有实际的引用关系的情况
+    ''' 例如模块A没有引用messagepack模块，则没有办法添加<see cref="MessagePackMemberAttribute"/>
+    ''' 来完成序列化，则这个时候会需要通过这个模块来提供这样的映射关系
+    ''' </remarks>
     Public MustInherit Class SchemaProvider(Of T) : Implements ISchemaProvider
 
         Shared ReadOnly slotList As Dictionary(Of String, PropertyInfo) = DataFramework.Schema(Of T)(
