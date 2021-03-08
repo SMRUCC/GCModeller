d830afe7756b6f2359c54a1428737828c5206e7b
xieguigang <xie.guigang@gcmodeller.org>
2021-03-08 15:05:46 +0800

    split source file module

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/ApplicationServices/Debugger/Exception/VisualBasicAppException.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/ApplicationServices/Debugger/Exception/VisualBasicAppException.vb
@@ -85,28 +85,9 @@ Namespace ApplicationServices.Debugging
             MyBase.New("@" & calls, ex)
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Shared Function Creates(msg As String, calls As String) As VisualBasicAppException
             Return New VisualBasicAppException(New Exception(msg), calls)
         End Function
     End Class
-
-    <HideModuleName>
-    Public Module ExceptionExtensions
-
-        ''' <summary>
-        ''' 处理错误的工作逻辑的抽象接口
-        ''' </summary>
-        ''' <param name="ex">Socket的内部错误信息</param>
-        ''' <remarks></remarks>
-        Public Delegate Sub ExceptionHandler(ex As Exception)
-
-        ''' <summary>
-        ''' Just throw exception, but the exception contains more details information for the debugging
-        ''' </summary>
-        ''' <param name="msg$"></param>
-        ''' <returns></returns>
-        Public Function Fail(msg$, <CallerMemberName> Optional caller$ = "") As VisualBasicAppException
-            Return VisualBasicAppException.Creates(msg, caller)
-        End Function
-    End Module
 End Namespace
