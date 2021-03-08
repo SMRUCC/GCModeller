67c714a676e51c79154b59e6ac20a70a072d83d0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-03-08 10:39:23 +0800

    required of a schema model?

---
+++ src/repository/Bio.Repository/KEGG/MessagePack/KEGGCompoundPack.vb
@@ -0,0 +1,11 @@
+﻿Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+
+Namespace KEGG.Metabolism
+
+    ''' <summary>
+    ''' the schema of <see cref="Compound"/>
+    ''' </summary>
+    Public Class KEGGCompoundPack
+
+    End Class
+End Namespace
