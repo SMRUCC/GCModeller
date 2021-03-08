8348bbe15ffe7b35ad960b6d1677bb0af72c1f56
xieguigang <xie.guigang@gcmodeller.org>
2021-03-08 19:37:02 +0800

    improvements of the schema model

--- src/repository/Bio.Repository/test/Module1.vb
+++ src/repository/Bio.Repository/test/Module1.vb
@@ -1,13 +1,28 @@
-﻿Imports SMRUCC.genomics.Data
+﻿Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+Imports SMRUCC.genomics.Data
 Imports SMRUCC.genomics.Data.KEGG.Metabolism
 
 Module Module1
 
     Sub Main()
+        Call readTest()
+    End Sub
+
+    Sub writeTest()
         Dim list = CompoundRepository.ScanRepository("E:\biodeep\biodeepdb_v3\KEGG\KEGG_cpd").ToArray
 
         Using file = "E:\biodeep\biodeepdb_v3\KEGG\KEGG_cpd.repo".Open
             Call KEGGCompoundPack.WriteKeggDb(list, file)
         End Using
+    End Sub
+
+    Sub readTest()
+        Dim list As Compound()
+
+        Using file = "E:\biodeep\biodeepdb_v3\KEGG\KEGG_cpd.repo".Open(IO.FileMode.Open, doClear:=False, [readOnly]:=True)
+            list = KEGGCompoundPack.ReadKeggDb(file)
+        End Using
+
+        Pause()
     End Sub
 End Module
