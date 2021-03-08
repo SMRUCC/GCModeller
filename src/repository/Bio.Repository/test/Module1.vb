c1ef33e0467d230fad93688ffadc6403aa4e197e
xieguigang <xie.guigang@gcmodeller.org>
2021-03-08 19:05:37 +0800

    run db test

---
+++ src/repository/Bio.Repository/test/Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports SMRUCC.genomics.Data
+Imports SMRUCC.genomics.Data.KEGG.Metabolism
+
+Module Module1
+
+    Sub Main()
+        Dim list = CompoundRepository.ScanRepository("E:\biodeep\biodeepdb_v3\KEGG\KEGG_cpd").ToArray
+
+        Using file = "E:\biodeep\biodeepdb_v3\KEGG\KEGG_cpd.repo".Open
+            Call KEGGCompoundPack.WriteKeggDb(list, file)
+        End Using
+    End Sub
+End Module
