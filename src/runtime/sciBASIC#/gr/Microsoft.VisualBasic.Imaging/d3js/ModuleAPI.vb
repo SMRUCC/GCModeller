f96f36d48ba133f3af00e2183078f5273c76860d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-04-14 19:20:33 +0800

    Update ModuleAPI.vb

--- src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/d3js/ModuleAPI.vb
+++ src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/d3js/ModuleAPI.vb
@@ -50,6 +50,10 @@ Namespace d3js
 
     Public Module ModuleAPI
 
+        Public Function forcedirectedLabeler() As Forcedirected
+            Return New Forcedirected()
+        End Function
+
         ''' <summary>
         ''' A D3 plug-in for automatic label placement using simulated annealing that easily 
         ''' incorporates into existing D3 code, with syntax mirroring other D3 layouts.
