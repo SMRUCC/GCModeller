2a2b023b9913f4518b41d70cb76c40ce09ae5836
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-04-23 09:33:17 +0800

    add contour source code from blog https://blog.csdn.net/lweiyue/article/details/91490460

---
+++ src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/Drawing2D/Math2D/MarchingSquares/IntMeasureData.vb
@@ -0,0 +1,28 @@
+﻿Namespace Drawing2D.Math2D.MarchingSquares
+
+    Friend Structure IntMeasureData
+        Public Sub New(ByVal md As MeasureData, ByVal x_num As Integer, ByVal y_num As Integer)
+            X = CInt(md.X * x_num)
+
+            If X >= x_num Then
+                X = x_num - 1
+            End If
+
+            Y = CInt(md.Y * y_num)
+
+            If Y >= y_num Then
+                Y = y_num - 1
+            End If
+
+            Z = md.Z
+        End Sub
+
+        Public X As Integer
+        Public Y As Integer
+        Public Z As Single
+        ''' <summary>
+        ''' 数据插值
+        ''' </summary>
+    End Structure
+
+End Namespace
---
+++ src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/Drawing2D/Math2D/MarchingSquares/MeasureData.vb
@@ -0,0 +1,35 @@
+﻿Namespace Drawing2D.Math2D.MarchingSquares
+
+    ''' <summary>
+    ''' 测量数据
+    ''' </summary>
+    Public Structure MeasureData
+        ''' <summary>
+        ''' 初始化测量数据
+        ''' </summary>
+        ''' <paramname="x">坐标x</param>
+        ''' <paramname="y">坐标y</param>
+        ''' <paramname="z">高度</param>
+        Public Sub New(ByVal x As Single, ByVal y As Single, ByVal z As Single)
+            Me.X = x
+            Me.Y = y
+            Me.Z = z
+        End Sub
+
+        ''' <summary>
+        ''' 坐标X
+        ''' </summary>
+        Public X As Single
+
+        ''' <summary>
+        ''' 坐标Y
+        ''' </summary>
+        Public Y As Single
+
+        ''' <summary>
+        ''' 高度
+        ''' </summary>
+        Public Z As Single
+    End Structure
+
+End Namespace
