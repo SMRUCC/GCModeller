6f00ab9758e7e50f780ccff356112366a8dbdce4
xieguigang <xie.guigang@gcmodeller.org>
2021-05-05 15:45:50 +0800

    config solution projects

--- src/GCModeller/engine/vcellkit/Modeller/API.vb
+++ src/GCModeller/engine/vcellkit/Modeller/API.vb
@@ -1,67 +1,68 @@
 ﻿#Region "Microsoft.VisualBasic::f0e8fe752d589d3773422bb0a952116b, engine\CompilerServices\API.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module API
-    ' 
-    '     Function: __add_author, __add_email, __add_publication, __add_reversion, __add_URL
-    '               _set_DBLinks, _set_Name, _set_Title, set_SpeciesId, setComments
-    ' 
-    ' /********************************************************************************/
+' Module API
+' 
+'     Function: __add_author, __add_email, __add_publication, __add_reversion, __add_URL
+'               _set_DBLinks, _set_Name, _set_Title, set_SpeciesId, setComments
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Extensions
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.GCModeller.CompilerServices
 
 <Package("GCModeller.Property", Publisher:="GCModeller Virtual Cell System", Description:="")>
 Module API
 
     <ExportAPI("Write.Name")>
     Public Function _set_Name(model As ModelBaseType, value As String) As ModelBaseType
-        model.properties.Name = value
+        model.properties.name = value
         Return model
     End Function
 
     <ExportAPI("Author.Add")>
     Public Function __add_author(model As ModelBaseType, value As String) As ModelBaseType
-        model.properties.Authors.Add(value)
+        model.properties.authors.Add(value)
         Return model
     End Function
 
     <ExportAPI("Write.Comment")>
     Public Function setComments(model As ModelBaseType, value As String) As ModelBaseType
-        model.properties.Comment = value
+        model.properties.comment = value
         Return model
     End Function
 
