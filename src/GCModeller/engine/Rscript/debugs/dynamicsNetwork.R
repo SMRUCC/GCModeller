8e65101b1ad834f3e34fef5739da83519235b38a
xieguigang <xie.guigang@gcmodeller.org>
2021-05-12 20:46:24 +0800

    run dynamics engine test success

--- src/GCModeller/engine/Rscript/debugs/dynamicsNetwork.R
+++ src/GCModeller/engine/Rscript/debugs/dynamicsNetwork.R
@@ -7,8 +7,19 @@ const network = [
 
 ];
 
-network 
-:> test_network(a = 100, b = 1, c = 800, d = 1000) 
-:> as.object 
-:> do.call("Run")
-;
+using mass as auto(new dataset.driver(), `${dirname(@script)}/matrix/mass.csv`) {
+	using flux as auto(new dataset.driver(), `${dirname(@script)}/matrix/flux.csv`) {
+		network 
+		:> test_network(a = 100, b = 1, c = 800, d = 1000) 
+		:> flux.dynamics()
+		:> flux.load_driver(
+			mass = mass,
+			flux = flux
+		)
+		:> as.object 
+		:> do.call("Run")
+		;	
+	}
+}
+
+
