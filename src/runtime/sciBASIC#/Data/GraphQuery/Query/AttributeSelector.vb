6528b55ca87c9899abd1f22a982a6e2537228ebf
guigang xie <gg.xie@bionovogene.com>
2021-05-17 18:16:21 +0800

    improvements of the parser model

---
+++ src/runtime/sciBASIC#/Data/GraphQuery/Query/AttributeSelector.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic.MIME.Markup.HTML
+
+Public Class AttributeSelector : Inherits Parser
+
+    Protected Overrides Function ParseImpl(document As InnerPlantText, isArray As Boolean, env As Engine) As InnerPlantText
+        If isArray Then
+            Return New HtmlElement With {
+                .HtmlElements = DirectCast(document, HtmlElement)(parameters(Scan0)).Values _
+                    .Select(Function(a)
+                                Return New InnerPlantText With {
+                                    .InnerText = a
+                                }
+                            End Function) _
+                    .ToArray
+            }
+        Else
+            Return New InnerPlantText With {
+                .InnerText = DirectCast(document, HtmlElement)(parameters(Scan0)).Value
+            }
+        End If
+    End Function
+End Class
---
+++ src/runtime/sciBASIC#/Data/GraphQuery/Query/FunctionParser.vb
@@ -0,0 +1,8 @@
+﻿Imports Microsoft.VisualBasic.MIME.Markup.HTML
+
+Public Class FunctionParser : Inherits Parser
+
+    Protected Overrides Function ParseImpl(document As InnerPlantText, isArray As Boolean, env As Engine) As InnerPlantText
+        Return env.Execute(document, func, parameters, isArray)
+    End Function
+End Class
