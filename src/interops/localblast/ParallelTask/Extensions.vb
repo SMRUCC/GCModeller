e6d873c940ce66d39469350818dac1723646989c
xieguigang <xie.guigang@gcmodeller.org>
2021-05-22 16:09:37 +0800

    fix of symbol reference errors

--- src/interops/localblast/ParallelTask/Extensions.vb
+++ src/interops/localblast/ParallelTask/Extensions.vb
@@ -1,45 +1,46 @@
 ﻿#Region "Microsoft.VisualBasic::08ca4306ef9c10eeb28218a5710031aa, ParallelTask\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Extensions
-    ' 
-    '     Function: (+2 Overloads) BlastpSearch, IsAvailable
-    ' 
-    ' /********************************************************************************/
+' Module Extensions
+' 
+'     Function: (+2 Overloads) BlastpSearch, IsAvailable
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ApplicationServices
 Imports SMRUCC.genomics.Interops.NCBI.Extensions
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput
@@ -95,8 +96,8 @@ Public Module Extensions
             Return Nothing
         End If
 
-        Dim tmpQuery As String = App.GetAppSysTempFile
-        Dim tmpOut As String = App.GetAppSysTempFile
+        Dim tmpQuery As String = TempFileSystem.GetAppSysTempFile
+        Dim tmpOut As String = TempFileSystem.GetAppSysTempFile
 
         If Blastbin Is Nothing Then Blastbin = NCBILocalBlast.CreateSession
 
@@ -112,14 +113,14 @@ Public Module Extensions
                                              Optional evalue As String = "1e-3",
                                              Optional ByRef Blastbin As LocalBLAST.InteropService.InteropService = Nothing) As BlastPlus.v228
 
-        Dim tmpQuery As String = App.GetAppSysTempFile(, App.PID)
+        Dim tmpQuery As String = TempFileSystem.GetAppSysTempFile(, App.PID)
 
         If Blastbin Is Nothing Then Blastbin = NCBILocalBlast.CreateSession
 
         Call Query.Save(tmpQuery)
 
         Call Blastbin.FormatDb(Subject, Blastbin.MolTypeProtein).Start(True)
-        Call Blastbin.Blastp(tmpQuery, Subject, App.GetAppSysTempFile() & "/blastp.log", evalue).Start(True)
+        Call Blastbin.Blastp(tmpQuery, Subject, TempFileSystem.GetAppSysTempFile() & "/blastp.log", evalue).Start(True)
 
         Return Blastbin.GetLastLogFile
     End Function
