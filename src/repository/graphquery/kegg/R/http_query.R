68d1b8efdc0a319f71875dc401a6e0c853d23727
xieguigang <xie.guigang@gcmodeller.org>
2021-05-22 20:05:22 +0800

    code for run graphquery

--- src/repository/graphquery/kegg/R/http_query.R
+++ src/repository/graphquery/kegg/R/http_query.R
@@ -1,18 +1,23 @@
 imports ["Html", "http"] from "webKit";
+imports "graphquery" from "webKit";
 
 #' Run query with http cache
 #'
 const http_query as function(url, graphquery) {
 	getHtml(url)
-	
+	:> Html::parse
+	:> graphquery::query(graphquery, raw = TRUE)
+	;
 }
 
+#' Http get html or from cache
+#'
 const getHtml as function(url, interval = 5) {
 	const http.cache_dir as string = getOption("http.cache_dir") || stop("You should set of the 'http.cache_dir' option at first!");
 	
 	const cacheKey as string   = md5(url);
 	const prefix as string     = substr(cacheKey, 1, 2);
-	const cache_file as string = `${cacheDir}/${prefix}/${cacheKey}.html`;
+	const cache_file as string = `${http.cache_dir}/${prefix}/${cacheKey}.html`;
 	
 	if ((!file.exists(cache_file)) || (file.size(cache_file) <= 0)) {		
 		writeLines(con = cache_file) {
