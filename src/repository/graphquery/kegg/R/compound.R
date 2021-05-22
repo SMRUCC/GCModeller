cefc77e83ea6d2a29263046cebde039d2a5a26f3
xieguigang <xie.guigang@gcmodeller.org>
2021-05-22 23:11:47 +0800

    parse of the metabolic components

--- src/repository/graphquery/kegg/R/compound.R
+++ src/repository/graphquery/kegg/R/compound.R
@@ -1,15 +1,16 @@
 #' query data to gcmodeller object model 
 #'
 const kegg_compound as function(url) {
-	const keyValues = keyIndex(http_query(url, raw = FALSE));
-	const xref      = graphquery::query(document = Html::parse(keyValues$"Other DBs"), graphquery = get_graph("graphquery/fields/dbLinks.graphquery"));
-	const KCF_text  = graphquery::query(document = Html::parse(keyValues$"KCF data"),  graphquery = get_graph("graphquery/fields/KCFtext.graphquery"));
+	const keyValues  = keyIndex(http_query(url, raw = FALSE));
+	const xref       = graphquery::query(document = Html::parse(keyValues$"Other DBs"), graphquery = get_graph("graphquery/fields/dbLinks.graphquery"));
+	const KCF_text   = graphquery::query(document = Html::parse(keyValues$"KCF data"),  graphquery = get_graph("graphquery/fields/KCFtext.graphquery"));
+	const pathways   = graphquery::query(document = Html::parse(keyValues$"Pathway"),   graphquery = get_graph("graphquery/fields/pathway_item.graphquery"));
+	const modules    = graphquery::query(document = Html::parse(keyValues$"Module"),    graphquery = get_graph("graphquery/fields/pathway_item.graphquery"));
+	const reactionId = graphquery::query(document = Html::parse(keyValues$"Reaction"),  graphquery = get_graph("graphquery/fields/reactionLink.graphquery"));
 
 	print(names(keyValues));
 
-	print(KCF_text);
-
-	# print(json_encode(xref));
+print(reactionId);
 }
 
 
