903326f1a7fccb622ba810a3b2122f9a11dcbf7c
xieguigang <xie.guigang@gcmodeller.org>
2021-05-22 23:42:05 +0800

    parse simple text

--- src/repository/graphquery/kegg/R/compound.R
+++ src/repository/graphquery/kegg/R/compound.R
@@ -2,19 +2,22 @@
 #'
 const kegg_compound as function(url) {
 	const keyValues   = keyIndex(http_query(url, raw = FALSE));
-	const xref        = graphquery::query(document = Html::parse(keyValues$"Other DBs"), graphquery = get_graph("graphquery/fields/dbLinks.graphquery"));
-	const KCF_text    = graphquery::query(document = Html::parse(keyValues$"KCF data"),  graphquery = get_graph("graphquery/fields/KCFtext.graphquery"));
-	const pathways    = graphquery::query(document = Html::parse(keyValues$"Pathway"),   graphquery = get_graph("graphquery/fields/pathway_item.graphquery"));
-	const modules     = graphquery::query(document = Html::parse(keyValues$"Module"),    graphquery = get_graph("graphquery/fields/pathway_item.graphquery"));
-	const reactionId  = graphquery::query(document = Html::parse(keyValues$"Reaction"),  graphquery = get_graph("graphquery/fields/reactionLink.graphquery"));
-	const commonNames = graphquery::query(document = Html::parse(keyValues$"Name"),      graphquery = get_graph("graphquery/fields/commonNames.graphquery"));
-	const id          = graphquery::query(document = Html::parse(keyValues$"Entry"),     graphquery = get_graph("graphquery/fields/id.graphquery"));
-	const formula     = graphquery::query(document = Html::parse(keyValues$"Formula"),   graphquery = get_graph("graphquery/fields/id.graphquery"));
+	const xref        = graphquery::query(document = Html::parse(keyValues$"Other DBs"),  graphquery = get_graph("graphquery/fields/dbLinks.graphquery"));
+	const KCF_text    = graphquery::query(document = Html::parse(keyValues$"KCF data"),   graphquery = get_graph("graphquery/fields/KCFtext.graphquery"));
+	const pathways    = graphquery::query(document = Html::parse(keyValues$"Pathway"),    graphquery = get_graph("graphquery/fields/pathway_item.graphquery"));
+	const modules     = graphquery::query(document = Html::parse(keyValues$"Module"),     graphquery = get_graph("graphquery/fields/pathway_item.graphquery"));
+	const reactionId  = graphquery::query(document = Html::parse(keyValues$"Reaction"),   graphquery = get_graph("graphquery/fields/reactionLink.graphquery"));
+	const commonNames = graphquery::query(document = Html::parse(keyValues$"Name"),       graphquery = get_graph("graphquery/fields/commonNames.graphquery"));
+	const id          = graphquery::query(document = Html::parse(keyValues$"Entry"),      graphquery = get_graph("graphquery/fields/simpleText.graphquery"));
+	const formula     = graphquery::query(document = Html::parse(keyValues$"Formula"),    graphquery = get_graph("graphquery/fields/simpleText.graphquery"));
+	const exactMass   = graphquery::query(document = Html::parse(keyValues$"Exact mass"), graphquery = get_graph("graphquery/fields/simpleText.graphquery"));
+
 
 	print(names(keyValues));
 print(id);
 print(commonNames);
 print(formula);
+print(exactMass);
 }
 
 
