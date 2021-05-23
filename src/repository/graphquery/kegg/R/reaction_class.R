6cdb6bdcecb4fe78ea5d0e788cbc92a6c7495860
xieguigang <xie.guigang@gcmodeller.org>
2021-05-23 16:10:57 +0800

    parse reaction class data

---
+++ src/repository/graphquery/kegg/R/reaction_class.R
@@ -0,0 +1,39 @@
+imports "repository" from "kegg_kit";
+
+#'
+#'
+const kegg_reactionclass as function(url) {
+  # parse the page text
+  const keyValues = keyIndex(http_query(url, raw = FALSE));
+
+  const id         = graphquery::query(document = Html::parse(keyValues$"Entry"),         graphquery = get_graph("graphquery/fields/simpleText.graphquery"));
+  const info       = graphquery::query(document = Html::parse(keyValues$"Definition"),    graphquery = get_graph("graphquery/fields/text.graphquery"));
+  const reactionId = graphquery::query(document = Html::parse(keyValues$"Reaction"),      graphquery = get_graph("graphquery/fields/reactionLink.graphquery"));
+  const EC_idlist  = graphquery::query(document = Html::parse(keyValues$"Enzyme"),        graphquery = get_graph("graphquery/fields/reactionLink.graphquery"));
+  const pathways   = graphquery::query(document = Html::parse(keyValues$"Pathway"),       graphquery = get_graph("graphquery/fields/pathway_item.graphquery"));
+  const KO         = graphquery::query(document = Html::parse(keyValues$"Orthology"),     graphquery = get_graph("graphquery/fields/pathway_item.graphquery"));
+  const transforms = graphquery::query(document = Html::parse(keyValues$"Reactant pair"), graphquery = get_graph("graphquery/fields/reactionLink.graphquery"));
+  const rmodules   = graphquery::query(document = Html::parse(keyValues$"RModule"),       graphquery = get_graph("graphquery/fields/pathway_item.graphquery"));
+
+  repository::reaction_class(
+    id         = id,
+    definition = info,
+    reactions  = reactionId[reactionId == $"R\d+"],
+    enzyme     = EC_idlist[EC_idlist  == $"\d[.].+"],
+    pathways   = pathwayList(pathways),
+    KO         = pathwayList(KO),
+    transforms = compoundTransformPair(transforms),
+    rmodules   = pathwayList(rmodules)
+  );
+}
+
+const compoundTransformPair as function(transforms) {
+    const tuples = lapply(transforms, function(pair) {
+        strsplit(pair, "_");        
+    });
+
+    data.frame(
+        from = sapply(tuples, t -> t[1]),
+        to   = sapply(tuples, t -> t[2])
+    );
+}
