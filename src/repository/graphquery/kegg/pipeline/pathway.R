14a4a75e2e06a6ae07bfe8079bed079f1a876006
xieguigang <xie.guigang@gcmodeller.org>
2021-05-24 14:48:20 +0800

    Update pathway.R

--- src/repository/graphquery/kegg/pipeline/pathway.R
+++ src/repository/graphquery/kegg/pipeline/pathway.R
@@ -1,14 +1,19 @@
 # require(kegg_graphquery);
 
 const cache_dir = ?"--cache" || `${dirname(@script)}/.cache/`;
+const Tcode     = ?"--tcode" || "map";
 
 options(http.cache_dir = cache_dir);
 
-const maps = as.data.frame(pathway_category());
-const url = "https://www.kegg.jp/entry/ko%s";
-const img = "https://www.kegg.jp/kegg/pathway/ko/ko%s.png";
-const repoDir = enumeratePath(maps, "map");
-const id = maps[, "entry"];
+const maps    = as.data.frame(pathway_category());
+const url     = (if (Tcode == "map") {
+                    "https://www.kegg.jp/entry/ko%s";
+                } else {
+                    gsub("https://www.genome.jp/entry/pathway+%c%s", "%c", Tcode);
+                });
+const img     = "https://www.kegg.jp/kegg/pathway/ko/ko%s.png";
+const repoDir = enumeratePath(maps, Tcode);
+const id      = maps[, "entry"];
 
 print("get all kegg pathway maps:");
 str(maps);
