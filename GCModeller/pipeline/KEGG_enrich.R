ef1769bc62cabcf421eb80031401df4c080693cc
guigang xie <gg.xie@bionovogene.com>
2021-05-25 11:46:45 +0800

    add dev notes

--- GCModeller/pipeline/KEGG_enrich.R
+++ GCModeller/pipeline/KEGG_enrich.R
@@ -4,7 +4,7 @@ imports "visualPlot" from "visualkit";
 imports "brite" from "kegg_kit";
 
 # title: KEGG enrichment
-# 
+#
 # authors: xieguigang
 # description: KEGG enrichment commandline tool
 
@@ -18,7 +18,7 @@ const outputdir as string      = ?"--out"     || dirname(testId_input);
 
 # map gene id to uniprot id
 const toUniprot = uniprot_xml
-|> open.uniprot 
+|> open.uniprot
 |> id_unify(target = "gene:b\d+", id = readLines(testId_input))
 ;
 
@@ -26,31 +26,31 @@ const toUniprot = uniprot_xml
 const kegg_background = [kegg_onthology]
 |> brite::parse
 |> KO.background(
-	maps   = brite::parse("ko00001"), 
-	id_map = "b\d+"
+  maps   = brite::parse("ko00001"),
+  id_map = "b\d+"
 )
 ;
 
 print(kegg_background);
 
 write.enrichment(file = `${outputdir}/enrich.csv`, format = "KOBAS") {
-	const enrich = kegg_background
-	|> enrichment(toUniprot, showProgress = FALSE)
-	|> enrichment.FDR
-	|> as.KOBAS_terms
-	;
-	
-	bitmap(file = `${outputdir}/KEGG.png`) {
-		enrich 
-		|> which(i -> as.object(i)$CorrectedPvalue <= 0.05)
-		|> KEGG.enrichment.profile
-		|> category_profiles.plot(
-			title      = "KEGG Enrichment",
-            axis_title = "-log10(p-value)",
-			size       = [3300,2700]
-		)
-		;
-	}
-	
-	enrich;
+  const enrich = kegg_background
+  |> enrichment(toUniprot, showProgress = FALSE)
+  |> enrichment.FDR
+  |> as.KOBAS_terms
+  ;
+
+  bitmap(file = `${outputdir}/KEGG.png`) {
+    enrich
+    |> which(i -> as.object(i)$CorrectedPvalue <= 0.05)
+    |> KEGG.enrichment.profile
+    |> category_profiles.plot(
+      title      = "KEGG Enrichment",
+      axis_title = "-log10(p-value)",
+      size       = [3300,2700]
+    )
+    ;
+  }
+
+  enrich;
 }
---
+++ GCModeller/pipeline/README.md
@@ -0,0 +1,15 @@
+# Usage Note
+
+Run commandline query for script to view usage
+
+```bash
+R# ? /path/to/R#/script.R
+```
+
+Example as,
+
+```bash
+R# KEGG_enrich.R
+
+
+```
