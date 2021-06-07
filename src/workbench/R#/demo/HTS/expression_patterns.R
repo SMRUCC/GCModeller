3fd37ac5864afc96e8b8322ae58bf6e2ca70ab3b
xieguigang <xie.guigang@gcmodeller.org>
2021-06-07 22:22:12 +0800

    code style improvements of the demo code

--- src/workbench/R#/demo/HTS/expression_patterns.R
+++ src/workbench/R#/demo/HTS/expression_patterns.R
@@ -1,9 +1,7 @@
-imports "visualkit.plots" from "visualkit";
+imports "visualPlot" from "visualkit";
 imports ["geneExpression", "sampleInfo"] from "phenotype_kit";
 
-setwd(!script$dir);
-
-let expr0 = read.csv("http://www.biodeep.cn/demo/metabolome.csv", row_names = 1);
+const expr0 = read.csv("http://www.biodeep.cn/demo/metabolome.csv", row_names = 1);
 
 expr0[, "mz"] = NULL;
 expr0[, "rt"] = NULL;
@@ -11,27 +9,28 @@ expr0[, "rt"] = NULL;
 print("we have all sample labels:");
 print(colnames(expr0));
 
-let sampleinfo = guess.sample_groups(colnames(expr0), raw_list = FALSE);
+const sampleinfo = guess.sample_groups(colnames(expr0), raw_list = FALSE);
 
 print("a possible sample groups that parsed from the given sample labels:");
 print(sampleinfo);
 
-let patterns = expr0
-:> load.expr
-:> average(sampleinfo)
-:> relative
-:> expression.cmeans_pattern(dim = [3, 3], fuzzification = 5, threshold = 0.001)
-;
+bitmap(file = `${dirname(@script)}/patterns.png`) {
+	const patterns = expr0
+	|> load.expr
+	|> average(sampleinfo)
+	|> relative
+	|> expression.cmeans_pattern(dim = [3, 3], fuzzification = 5, threshold = 0.001)
+	;
 
-print("view patterns result:");
-print(patterns);
+	print("view patterns result:");
+	print(patterns);
 
-patterns
-:> plot.expression_patterns(size = [6000, 4500], colorSet = "Jet")
-:> save.graphics(file = "./patterns.png")
-;
+	patterns
+	:> cmeans_matrix
+	:> write.csv(file = "./patterns.csv")
+	;
 
-patterns
-:> cmeans_matrix
-:> write.csv(file = "./patterns.csv")
-;
+	patterns
+	:> plot.expression_patterns(size = [6000, 4500], colorSet = "Jet")
+	;
+}
