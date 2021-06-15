1e4793a135a48a8f628c6ded3036c374ed638163
xieguigang <xie.guigang@gcmodeller.org>
2021-06-16 00:35:08 +0800

    Update HttpDriver.vb

--- src/runtime/httpd/Flute/HttpDriver.vb
+++ src/runtime/httpd/Flute/HttpDriver.vb
@@ -1,25 +1,30 @@
 ﻿Imports System.Net.Sockets
+Imports System.Runtime.CompilerServices
 Imports Flute.Http.Core
+Imports Flute.Http.Core.Message
 
-Public Class HttpDriver : Inherits HttpServer
+Public Class HttpDriver
 
-    Public Sub New(port As Integer, Optional threads As Integer = -1)
-        MyBase.New(port, threads)
+    Dim responseHeader As New Dictionary(Of String, String)
+
+    Sub New()
     End Sub
 
-    Public Overrides Sub handleGETRequest(p As HttpProcessor)
-        Throw New NotImplementedException()
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Sub AddResponseHeader(header As String, value As String)
+        Call responseHeader.Add(header, value)
     End Sub
 
-    Public Overrides Sub handlePOSTRequest(p As HttpProcessor, inputData As String)
-        Throw New NotImplementedException()
-    End Sub
-
-    Public Overrides Sub handleOtherMethod(p As HttpProcessor)
-        Throw New NotImplementedException()
-    End Sub
-
-    Protected Overrides Function getHttpProcessor(client As TcpClient, bufferSize As Integer) As HttpProcessor
-        Return New HttpProcessor(client, Me, bufferSize)
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function GetSocket(port As Integer) As HttpSocket
+        Return New HttpSocket(
+            app:=AddressOf AppHandler,
+            port:=port
+        )
     End Function
+
+    Public Sub AppHandler(request As HttpRequest, response As HttpResponse)
+
+    End Sub
+
 End Class
