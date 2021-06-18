5299e209594036bc4161cb473f1aa6df3eb02721
xieguigang <xie.guigang@gcmodeller.org>
2021-06-18 10:00:05 +0800

    Create reaction_class.R

---
+++ src/repository/graphquery/kegg/pipeline/reaction_class.R
@@ -0,0 +1,33 @@
+require(kegg_graphquery);
+
+options(http.cache_dir = ?"--cache" || `${dirname(@script)}/.cache/`);
+
+# create resource url based on the organism Tcode exists or not
+const url = (
+    if (Tcode == "map") {
+        "https://www.kegg.jp/entry/ko%s";
+    } else {
+        gsub("https://www.genome.jp/entry/pathway+%c%s", "%c", Tcode);
+    }
+);
+const img     = "https://www.kegg.jp/kegg/pathway/ko/ko%s.png";
+const maps    = as.data.frame(pathway_category());
+const repoDir = enumeratePath(maps, Tcode);
+const id      = maps[, "entry"];
+
+print("get all kegg pathway maps:");
+str(maps);
+
+for(i in 1:nrow(maps)) {
+    const map = kegg_pathway(url = sprintf(url, id[i]));
+
+    if ((map != "") && (!is.null(map))) {
+
+        map
+        |> xml
+        |> writeLines(con = `${repoDir(i)}.XML`)
+        ;
+
+        # wget(sprintf(img, id[i]), `${repoDir(i)}.png`);
+    }     
+}
