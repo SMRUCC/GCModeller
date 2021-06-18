cb763e9451369d25eede8b574dc3274eaa15e05d
guigang xie <gg.xie@bionovogene.com>
2021-06-18 15:34:28 +0800

    query kegg compounds

--- src/repository/graphquery/kegg/pipeline/compounds.R
+++ src/repository/graphquery/kegg/pipeline/compounds.R
@@ -3,34 +3,26 @@ require(kegg_graphquery);
 options(http.cache_dir = ?"--cache" || `${dirname(@script)}/.cache/`);
 
 # create resource url based on the organism Tcode exists or not
-const url = (
-    if (Tcode == "map") {
-        "https://www.kegg.jp/pathway/map%s";
-    } else {
-        gsub("https://www.kegg.jp/pathway/%c%s", "%c", Tcode);
+const url     = "https://www.kegg.jp/dbget-bin/www_bget?cpd:%s";
+const runQuery as function(name, brite) {
+    const class   = as.data.frame(pathway_category());
+    const repoDir = enumeratePath(maps, Tcode);
+    const id      = maps[, "entry"];
+
+    print("get all kegg pathway maps:");
+    str(maps);
+
+    for(i in 1:nrow(maps)) {
+        const keg_compound = kegg_compound(url = sprintf(url, id[i]));
+
+        if ((keg_compound != "") && (!is.null(keg_compound))) {
+
+            keg_compound
+            |> xml
+            |> writeLines(con = `${name}/${repoDir(i)}.XML`)
+            ;
+        }
     }
-);
-
-const maps    = as.data.frame(pathway_category());
-const repoDir = enumeratePath(maps, Tcode);
-const id      = maps[, "entry"];
-
-print("get all kegg pathway maps:");
-str(maps);
-
-print("all map id list at here:");
-print(id);
-
-for(i in 1:nrow(maps)) {
-    const map = kegg_map(url = sprintf(url, id[i]));
-
-    if ((map != "") && (!is.null(map))) {
-
-        map
-        |> xml
-        |> writeLines(con = `${repoDir(i)}.XML`)
-        ;
-
-        # wget(sprintf(img, id[i]), `${repoDir(i)}.png`);
-    }     
 }
+
+
