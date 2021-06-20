2758bf8dfe66047682f05a79b70952bd25fa4a2d
xieguigang <xie.guigang@gcmodeller.org>
2021-06-20 13:29:56 +0800

    GaussBlur

---
+++ src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/test/imageSmoothTest.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Filters
+
+Module imageSmoothTest
+
+    Sub Main()
+        Dim bitmap1 = "E:\mzkit\DATA\test\imzML\s042_229_continuous_large.png".LoadImage
+        Dim bitmap2 = New GaussianSmooth(k:=250).Smooth(bitmap1)
+
+        Call bitmap2.SaveAs("E:\Resources\a.html.png")
+
+        Call GaussBlur.GaussBlur(bitmap1).SaveAs("E:\Resources\a.htm222l.png")
+    End Sub
+End Module
