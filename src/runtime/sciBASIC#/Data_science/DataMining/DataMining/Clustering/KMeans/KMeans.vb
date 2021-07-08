cd2bdfe8d7f9dcc818429ed52b00f2eeeecd357b
guigang xie <gg.xie@bionovogene.com>
2021-07-08 20:17:11 +0800

    fix of namespace reference errors

--- src/runtime/sciBASIC#/Data_science/DataMining/DataMining/Clustering/KMeans/KMeans.vb
+++ src/runtime/sciBASIC#/Data_science/DataMining/DataMining/Clustering/KMeans/KMeans.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::3816fb79787180b8115d5062c06f782e, Data_science\DataMining\DataMining\Clustering\KMeans\KMeans.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module KMeansAlgorithm
-    ' 
-    '         Function: (+2 Overloads) ClusterDataSet, ClusterMean, CrossOver, EuclideanDistance, ManhattanDistance
-    '                   means, minIndex
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module KMeansAlgorithm
+' 
+'         Function: (+2 Overloads) ClusterDataSet, ClusterMean, CrossOver, EuclideanDistance, ManhattanDistance
+'                   means, minIndex
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -46,6 +46,7 @@ Imports Microsoft.VisualBasic.DataMining.ComponentModel
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Linq.Extensions
+Imports Microsoft.VisualBasic.Math.Correlations
 Imports Microsoft.VisualBasic.Parallel.Linq
 Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
 Imports stdNum = System.Math
@@ -180,7 +181,7 @@ Namespace KMeans
                     End If
 
                     ' 假若上一次的迭代结果和这一次迭代的结果一样，则距离是0，得到了一个稳定的聚类结果
-                    If (EuclideanDistance(x.ClusterMean, y.ClusterMean)) = 0 Then
+                    If (DistanceMethods.EuclideanDistance(x.ClusterMean, y.ClusterMean)) = 0 Then
                         stableClustersCount += 1
                     End If
                 Next
