9bf6120df206fcc0d1f0a83489a71a6511d9b74d
xieguigang <xie.guigang@gcmodeller.org>
2021-07-13 10:44:44 +0800

    how to draw intersection data?

---
+++ src/GCModeller/visualize/DataVisualizationExtensions/CollectionSet/IntersectionData.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Linq
+
+Namespace CollectionSet
+
+    Public Class IntersectionData
+
+        Public Property groups As FactorGroup()
+
+        ''' <summary>
+        ''' get the labels of all collection set like ``a vs b``, etc
+        ''' </summary>
+        ''' <returns></returns>
+        Public Function GetAllCollectionTags() As String()
+            Return groups _
+                .Select(Function(d) d.data.Select(Function(t) t.name)) _
+                .IteratesALL _
+                .Distinct _
+                .ToArray
+        End Function
+
+    End Class
+
+    Public Class FactorGroup
+
+        Public Property factors As String
+        Public Property data As NamedCollection(Of String)()
+        Public Property color As Color
+
+    End Class
+End Namespace
---
+++ src/GCModeller/visualize/DataVisualizationExtensions/CollectionSet/IntersectionPlot.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.MIME.Html.CSS
+
+Namespace CollectionSet
+
+    ''' <summary>
+    ''' show venn intersection plot for more than 6 category 
+    ''' </summary>
+    Public Class IntersectionPlot : Inherits Plot
+
+        ReadOnly collections As IntersectionData
+
+        Public Sub New(data As IntersectionData, theme As Theme)
+            MyBase.New(theme)
+
+            Me.collections = data
+        End Sub
+
+        Protected Overrides Sub PlotInternal(ByRef g As IGraphics, canvas As GraphicsRegion)
+            Dim plotRect As Rectangle = canvas.PlotRegion
+            Dim labelFont As Font = CSSFont.TryParse(theme.axisLabelCSS)
+            Dim collectionSetLabels As String() = collections.GetAllCollectionTags
+            Dim maxLabelSize As SizeF = g.MeasureString(collectionSetLabels.MaxLengthString, labelFont)
+            Dim totalHeight As Double = collectionSetLabels.Length * (maxLabelSize.Height * 1.125)
+            Dim topBarPlot As New Rectangle(plotRect.Left, plotRect.Top, plotRect.Width, plotRect.Height - totalHeight)
+            Dim bottomIntersection As New Rectangle(plotRect.Left, plotRect.Bottom - totalHeight, plotRect.Width, totalHeight)
+            Dim barData As New List(Of NamedValue(Of Integer))
+
+            Call drawBottomIntersctionVisualize(g, collectionSetLabels, barData, layout:=bottomIntersection)
+        End Sub
+
+        Private Sub drawBottomIntersctionVisualize(g As IGraphics, collectionSetLabels As String(), barData As List(Of NamedValue(Of Integer)), layout As Rectangle)
+
+        End Sub
+
+        Private Sub drawLeftBarSet(g As IGraphics, rect As Rectangle)
+
+        End Sub
+
+        Private Sub drawTopBarPlot()
+
+        End Sub
+    End Class
+End Namespace
