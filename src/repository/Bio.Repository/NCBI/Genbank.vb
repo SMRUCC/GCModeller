917b92ac117c0cff2d3d65e43961477bf402d377
xieguigang <xie.guigang@gcmodeller.org>
2021-07-26 17:17:45 +0800

    add .net 5 project

--- src/repository/Bio.Repository/NCBI/Genbank.vb
+++ src/repository/Bio.Repository/NCBI/Genbank.vb
@@ -1,68 +1,71 @@
 ﻿#Region "Microsoft.VisualBasic::f5174cf30a7d0050983e0d9bd3fa95d5, core\Bio.Repository\NCBI\Genbank.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Installer
-    ' 
-    '     Function: BuildLocusHash, BuildNameHash, GetsiRNATargetSeqs, Install
-    ' 
-    ' Class Genbank
-    ' 
-    '     Properties: DIR
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    ' 
-    '     Function: __entryQuery, __query, AddNew, Exists, GetAll
-    '               GetByKey, GetWhere, (+3 Overloads) Query
-    ' 
-    '     Sub: AddOrUpdate, Delete
-    ' 
-    ' Class GeneInfo
-    ' 
-    '     Properties: [function], accId, locus_tag, name
-    ' 
-    '     Constructor: (+2 Overloads) Sub New
-    '     Function: NameEquals, ToString
-    ' 
-    ' Class GenbankIndex
-    ' 
-    '     Properties: AccId, definition, DIR, genome
-    ' 
-    '     Function: Gbk, ToString
-    ' 
-    ' /********************************************************************************/
+' Module Installer
+' 
+'     Function: BuildLocusHash, BuildNameHash, GetsiRNATargetSeqs, Install
+' 
+' Class Genbank
+' 
+'     Properties: DIR
+' 
+'     Constructor: (+1 Overloads) Sub New
+' 
+'     Function: __entryQuery, __query, AddNew, Exists, GetAll
+'               GetByKey, GetWhere, (+3 Overloads) Query
+' 
+'     Sub: AddOrUpdate, Delete
+' 
+' Class GeneInfo
+' 
+'     Properties: [function], accId, locus_tag, name
+' 
+'     Constructor: (+2 Overloads) Sub New
+'     Function: NameEquals, ToString
+' 
+' Class GenbankIndex
+' 
+'     Properties: AccId, definition, DIR, genome
+' 
+'     Function: Gbk, ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
+#If netcore5 Then
+Imports System.Data
+#End If
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Algorithm.DynamicProgramming.Levenshtein
 Imports Microsoft.VisualBasic.ComponentModel.Collection
