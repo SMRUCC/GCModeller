30594fbbf5e3ef7f1576b5d84c0933fe0b6c2e43
xieguigang <xie.guigang@gcmodeller.org>
2021-08-07 21:59:26 +0800

    add demo test data

---
+++ src/GCModeller/analysis/PhenoGraph/test/Program.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports SMRUCC.genomics.Analysis.Microarray.PhenoGraph
+
+Module Program
+
+    Sub Main()
+        Dim data As DataSet() = DataSet.LoadDataSet("E:\GCModeller\src\GCModeller\analysis\PhenoGraph\demo\GSE98734_Count_table.txt", tsv:=True).Transpose.ToArray
+        Dim graph As NetworkGraph = CommunityGraph.CreatePhenoGraph(data)
+
+
+        Pause()
+    End Sub
+
+End Module
