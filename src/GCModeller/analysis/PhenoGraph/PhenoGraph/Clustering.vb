b56e26ee4bd4f070d0510914f10538fdf989d0f8
xieguigang <xie.guigang@gcmodeller.org>
2021-08-07 08:13:50 +0800

    Rscript?

--- src/GCModeller/analysis/PhenoGraph/PhenoGraph/Clustering.vb
+++ src/GCModeller/analysis/PhenoGraph/PhenoGraph/Clustering.vb
@@ -1,6 +1,20 @@
-﻿Public Module Clustering
+﻿Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
 
-    Public Function CreatePhenoGraph(data As DataSet())
+Public Module Clustering
 
+    Public Function CreatePhenoGraph(data As DataSet(), Optional k As Integer = 30)
+
+    End Function
+
+    Public Function CreatePhenoGraph(data As GeneralMatrix, Optional k As Integer = 30)
+        If k < 1 Then
+            Throw New ArgumentException("k must be a positive integer!")
+        ElseIf k > data.RowDimension - 2 Then
+            Throw New ArgumentException("k must be smaller than the total number of points!")
+        End If
+
+        message("Run Rphenograph starts:", "\n",
+        "  -Input data of ", nrow(data), " rows and ", ncol(data), " columns", "\n",
+        "  -k is set to ", k)
     End Function
 End Module
