39ee87fa6269edc1692395511c7bbfb09eab49f1
guigang xie <gg.xie@bionovogene.com>
2021-09-02 18:55:38 +0800

    Create DualColorBar.vb

---
+++ src/runtime/sciBASIC#/Data_science/Visualization/Plots/g/Legends/ColorBar/DualColorBar.vb
@@ -0,0 +1,22 @@
+﻿
+Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging
+
+Namespace Graphic.Legend
+
+    Public Module DualColorBar
+
+        <Extension>
+        Public Sub DrawDualColorBar(canvas As IGraphics,
+                                    color1 As SolidBrush(),
+                                    color2 As SolidBrush(),
+                                    layout As Rectangle,
+                                    ticks As Double(),
+                                    axisPen As Pen,
+                                    tickPen As Pen)
+
+        End Sub
+
+    End Module
+End Namespace
