70a321accc4b9992b2afe234e7f5f1b0f52ca435
xieguigang <xie.guigang@gcmodeller.org>
2021-09-04 13:04:57 +0800

    add xgb training code

---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/xgboost/train/Metric.vb
@@ -0,0 +1,84 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports stdNum = System.Math
+
+Namespace train
+    Public Class Metric
+        Public Shared Function accuracy(pred As Double(), label As Double()) As Double
+            Dim hit = 0.0
+
+            For i = 0 To pred.Length - 1
+
+                If label(i) = 0 AndAlso pred(i) < 0.5 OrElse label(i) = 1 AndAlso pred(i) > 0.5 Then
+                    hit += 1
+                End If
+            Next
+
+            Return hit / pred.Length
+        End Function
+
+        Public Shared Function [error](pred As Double(), label As Double()) As Double
+            Return 1.0 - Metric.accuracy(pred, label)
+        End Function
+
+        Public Shared Function mean_square_error(pred As Double(), label As Double()) As Double
+            Dim sum = 0.0
+
+            For i = 0 To pred.Length - 1
+                sum += stdNum.Pow(pred(i) - label(i), 2.0)
+            Next
+
+            Return sum / pred.Length
+        End Function
+
+        Public Shared Function mean_absolute_error(pred As Double(), label As Double()) As Double
+            Dim sum = 0.0
+
+            For i = 0 To pred.Length - 1
+                sum += stdNum.Abs(pred(i) - label(i))
+            Next
+
+            Return sum / pred.Length
+        End Function
+
+        Public Shared Function auc(pred As Double(), label As Double()) As Double
+            Dim n_pos As Double = 0
+
+            For Each v In label
+                n_pos += v
+            Next
+
+            Dim n_neg = pred.Length - n_pos
+            Dim label_pred As Double()() = Mat(Of Double)(pred.Length, 2)
+
+            For i = 0 To pred.Length - 1
+                label_pred(i)(0) = label(i)
+                label_pred(i)(1) = pred(i)
+            Next
+
+            Array.Sort(label_pred, New Metric.ComparatorAnonymousInnerClass())
+            Dim accumulated_neg As Double = 0
+            Dim satisfied_pair As Double = 0
+
+            For i = 0 To label_pred.Length - 1
+
+                If label_pred(i)(0) = 1 Then
+                    satisfied_pair += accumulated_neg
+                Else
+                    accumulated_neg += 1
+                End If
+            Next
+
+            Return satisfied_pair / n_neg / n_pos
+        End Function
+
+        Private Class ComparatorAnonymousInnerClass : Implements IComparer(Of Double())
+
+            Public Sub New()
+            End Sub
+
+            Public Overridable Function compare(a As Double(), b As Double()) As Integer Implements IComparer(Of Double()).Compare
+                Return a(1).CompareTo(b(1))
+            End Function
+        End Class
+    End Class
+End Namespace
---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/xgboost/train/Sampling.vb
@@ -0,0 +1,66 @@
+﻿Imports Microsoft.VisualBasic.Language.Java.Arrays
+Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
+
+Namespace train
+    Public Class RowSampler
+        Public row_mask As List(Of Double?) = New List(Of Double?)()
+
+        Public Sub New(n As Integer, sampling_rate As Double)
+            For i = 0 To n - 1
+                row_mask.Add(If(randf.NextDouble <= sampling_rate, 1.0, 0.0))
+            Next
+        End Sub
+
+        Public Overridable Sub shuffle()
+            Dim rands = row_mask.Shuffles
+
+            row_mask.Clear()
+            row_mask.AddRange(rands)
+        End Sub
+    End Class
+
+    Public Class ColumnSampler
+        Private cols As List(Of Integer?) = New List(Of Integer?)()
+        Public col_selected As List(Of Integer?)
+        Private n_selected As Integer
+
+        Public Sub New(n As Integer, sampling_rate As Double)
+            For i = 0 To n - 1
+                cols.Add(i)
+            Next
+
+            n_selected = CInt(n * sampling_rate)
+            col_selected = cols.subList(0, n_selected)
+        End Sub
+
+        Public Overridable Sub shuffle()
+            Dim rands = cols.Shuffles
+
+            cols.Clear()
+            cols.AddRange(rands)
+            col_selected = cols.subList(0, n_selected)
+        End Sub
+    End Class
+
+    Public Class Sampling
+        Public Shared Sub Main(args As String())
+            'test case
+            Dim rs As RowSampler = New RowSampler(1000000, 0.8)
+            Console.WriteLine(rs.row_mask.subList(0, 20))
+            rs.shuffle()
+            Console.WriteLine(rs.row_mask.subList(0, 20))
+            Dim sum = 0
+
+            For Each v As Double In rs.row_mask
+                sum += CInt(v)
+            Next
+
+            Console.WriteLine(sum)
+            Dim cs As ColumnSampler = New ColumnSampler(1000, 0.6)
+            Console.WriteLine(cs.col_selected.subList(0, 20))
+            cs.shuffle()
+            Console.WriteLine(cs.col_selected.subList(0, 20))
+            Console.WriteLine(cs.col_selected.Count)
+        End Sub
+    End Class
+End Namespace
