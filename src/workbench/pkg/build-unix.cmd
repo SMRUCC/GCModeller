93735c2f8a2ca12000b5d964509bf568dab2a35f
guigang xie <gg.xie@bionovogene.com>
2021-09-06 19:48:17 +0800

    add .net5 projects

--- src/workbench/pkg/build-unix.cmd
+++ src/workbench/pkg/build-unix.cmd
@@ -1,7 +1,11 @@
 @echo off
 
-SET Rscript="D:\GCModeller\src\R-sharp\App\net5.0\Rscript.exe"
+SET Rscript="../../R-sharp\App\net5.0\Rscript.exe"
+SET R="../../R-sharp\App\net5.0\R#.exe"
 
 %Rscript% --build
+
+REM install local
+%R% --install.packages ../../workbench/GCModeller_1.1.0-beta.zip
 
 pause
