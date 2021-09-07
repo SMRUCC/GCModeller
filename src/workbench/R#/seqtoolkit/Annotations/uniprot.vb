38b63b3085922967ac919c37b694864bcadf15b2
guigang xie <gg.xie@bionovogene.com>
2021-09-07 09:46:47 +0800

    extract KO seq

--- src/workbench/R#/seqtoolkit/Annotations/uniprot.vb
+++ src/workbench/R#/seqtoolkit/Annotations/uniprot.vb
@@ -1,41 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::9b3597b69589873b196ad0ef1b4e669c, seqtoolkit\Annotations\uniprot.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module uniprot
-    ' 
-    '     Function: getProteinSeq, openUniprotXmlAssembly, writePtfFile
-    ' 
-    ' /********************************************************************************/
+' Module uniprot
+' 
+'     Function: getProteinSeq, openUniprotXmlAssembly, writePtfFile
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -127,7 +127,16 @@ Module uniprot
 
         Dim source = getUniprotData(uniprot, env)
         Dim protFa = Iterator Function(prot As entry) As IEnumerable(Of FastaSeq)
-                         If extractAll Then
+                         If KOseq Then
+                             Dim KO As dbReference = prot.KO
+
+                             If Not KO Is Nothing Then
+                                 Yield New FastaSeq With {
+                                    .Headers = {KO.id, prot.proteinFullName},
+                                    .SequenceData = prot.ProteinSequence
+                                 }
+                             End If
+                         ElseIf extractAll Then
                              For Each accid As String In prot.accessions
                                  Yield New FastaSeq With {
                                     .Headers = {accid},
