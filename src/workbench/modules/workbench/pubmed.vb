bf727d19fefce31ed173a841d9ee146938bba87d
guigang xie <gg.xie@bionovogene.com>
2021-09-13 17:37:38 +0800

    config for kb api

---
+++ src/workbench/modules/workbench/pubmed.vb
@@ -0,0 +1,13 @@
+
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("pubmed")>
+Module pubmed
+
+    <ExportAPI("query")>
+    Public Function QueryKeyword(keyword As String)
+
+    End Function
+
+End Module
