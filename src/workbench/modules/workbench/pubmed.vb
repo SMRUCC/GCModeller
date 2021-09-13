c5d360d34567af59309cb16018b3ae15dbaf92dc
guigang xie <gg.xie@bionovogene.com>
2021-09-13 17:51:41 +0800

    package api for query pubmed

--- src/workbench/modules/workbench/pubmed.vb
+++ src/workbench/modules/workbench/pubmed.vb
@@ -1,13 +1,20 @@
 
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base
+Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.PubMed
 
 <Package("pubmed")>
-Module pubmed
+Module pubmedTools
 
     <ExportAPI("query")>
-    Public Function QueryKeyword(keyword As String)
+    Public Function QueryKeyword(keyword As String, Optional pageSize As Integer = 2000) As String()
+        Return PubMed.QueryPubmed(term:=keyword, pageSize:=pageSize).ToArray
+    End Function
 
+    <ExportAPI("article")>
+    Public Function PubmedArticle(id As String) As PubmedArticle
+        Return PubMed.GetArticleInfo(term:=id)
     End Function
 
 End Module
