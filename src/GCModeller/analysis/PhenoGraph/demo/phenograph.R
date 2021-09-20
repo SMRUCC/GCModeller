346e756364a3ad6b0422746402c63615ca64b057
xieguigang <xie.guigang@gcmodeller.org>
2021-09-20 19:10:00 +0800

    Create phenograph.R

---
+++ src/GCModeller/analysis/PhenoGraph/demo/phenograph.R
@@ -0,0 +1,29 @@
+imports ["geneExpression", "phenograph"] from "phenotype_kit";
+
+require(igraph);
+require(charts);
+
+setwd(@dir);
+
+"HR2MSI mouse urinary bladder S096_top3.csv"
+|> load.expr
+|> phenograph(k = 200)
+|> save.network(file = "HR2MSI mouse urinary bladder S096_graph")
+;
+
+bitmap(file = `Rphenograph.png`) {
+	const data     = read.csv("HR2MSI mouse urinary bladder S096_graph/nodes.csv", row.names = NULL);
+	const ID       = lapply(data[, 1], function(px) as.numeric(strsplit(px, ",")));
+	const clusters = data[, "NodeType"];
+	const x        = sapply(ID, px -> px[1]);
+    const y        = sapply(ID, px -> px[2]);
+	
+	plot(x, y, 
+	    class      = clusters, 
+	    shape      = "Square", 
+	    colorSet   = "Set1:c8", 
+	    grid.fill  = "white", 
+	    point_size = 25, 
+	    reverse    = TRUE
+    );
+}
