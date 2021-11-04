b8b5d8244e8d8dd1f05ed7875a3f41908d00514e
xieguigang <xie.guigang@gcmodeller.org>
2021-11-04 15:45:36 +0800

    docs

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/URLEncoder.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/URLEncoder.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::f9e58f2d24c10379e1e7ced4a7205868, core\Bio.Assembly\Assembly\KEGG\Web\Map\URLEncoder.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module URLEncoder
-    ' 
-    '         Function: KEGGURLEncode, URLParser, URLParser1, URLParser2, URLParser3
-    '                   URLParser4, VisualizePathwayMap
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module URLEncoder
+' 
+'         Function: KEGGURLEncode, URLParser, URLParser1, URLParser2, URLParser3
+'                   URLParser4, VisualizePathwayMap
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
