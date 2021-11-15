04ab739a2755c3b6a9c599052f02dcba179dedc4
guigang xie <gg.xie@bionovogene.com>
2021-11-15 13:51:54 +0800

    build projects

--- src/workbench/pkg/R/zzz.R
+++ src/workbench/pkg/R/zzz.R
@@ -3,6 +3,6 @@ const .onLoad as function() {
     cat("\n\n");
     cat("  GCModeller: genomics CAD(Computer Assistant Design) Modeller System\n");
     cat("                                author by: xie.guigang@gcmodeller.org\n");
-    cat("                                  © 2021 | SMRUCC genomics · 中國 桂林\n")
+    cat("                                  © 2021 | SMRUCC genomics · 中國 桂林\n");
     cat("\n\n");
 }
