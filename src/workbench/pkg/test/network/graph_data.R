af6cb334e8b45c351ad0d745bd85bb7c4d4c0355
guigang xie <gg.xie@bionovogene.com>
2021-11-15 16:14:36 +0800

    update debug source

--- src/workbench/pkg/test/network/graph_data.R
+++ src/workbench/pkg/test/network/graph_data.R
@@ -1,13 +1,15 @@
 require(GCModeller);
 
 const pack = list();
-const enrichments = ["U:\项目以外内容\2021\研发\KYYF0006-空间代谢组学\20210813_Test_kidney\FULL MS_centriod_CHCA_20210819\2.0\20211115-1\MS1_analysis"]
+const enrichments = ["U:\项目以外内容\2021\研发\KYYF0006-空间代谢组学\20210813_Test_kidney\FULL MS_centriod_CHCA_20210819\20210922_400\MS1_analysis"]
 |> list.dirs(full.names = TRUE)
 |> sapply(dir -> `${dir}/visual/kegg/pathway_results.csv`)
 |> which(file.exists)
 |> lapply(function(file) {    
     const [compounds, Raw.p] = read.csv(file);
     const psize = -log10(Raw.p);    
+
+    print(file);
 
     for(i in 1:length(Raw.p)) {
         const cid = strsplit(compounds[i], "[+,;]|\s+");
