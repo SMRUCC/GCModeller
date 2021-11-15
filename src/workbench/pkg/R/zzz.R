a6f9c9bd9da85e46020c5ab9309f4b1d6995117c
guigang xie <gg.xie@bionovogene.com>
2021-11-15 15:50:12 +0800

    load score data

--- src/workbench/pkg/R/zzz.R
+++ src/workbench/pkg/R/zzz.R
@@ -4,6 +4,6 @@ const .onLoad as function() {
     cat("  GCModeller: genomics CAD(Computer Assistant Design) Modeller System\n");
     cat("                                author by: xie.guigang@gcmodeller.org\n");
     cat("\n");
-    cat("                            (c) 2021 | SMRUCC genomics - GuiLin China\n");
+    cat("                           (c) 2021 | SMRUCC genomics - GuiLin, China\n");
     cat("\n\n");
 }
--- src/workbench/pkg/test/network/graph.R
+++ src/workbench/pkg/test/network/graph.R
@@ -1,2 +1,28 @@
 require(GCModeller);
 
+const enrichments = ["U:\项目以外内容\2021\研发\KYYF0006-空间代谢组学\20210813_Test_kidney\FULL MS_centriod_CHCA_20210819\2.0\20211115-1\MS1_analysis"]
+|> list.dirs(full.names = TRUE)
+|> sapply(dir -> `${dir}/visual/kegg/pathway_results.csv`)
+|> which(file.exists)
+|> lapply(function(file) {    
+    const [compounds, Raw.p] = read.csv(file);
+    const psize = -log10(Raw.p);
+    const pack = list();
+
+    for(i in 1:length(Raw.p)) {
+        const cid = strsplit(compounds[i], "[+,;]|\s+");
+
+        for(id in cid) {
+            if (id in pack) {
+                pack[[id]] = pack[[id]] + psize[i];
+            } else {
+                pack[[id]] = psize[i];
+            }
+        }
+    }
+
+    pack;
+})
+;
+
+str(enrichments);
