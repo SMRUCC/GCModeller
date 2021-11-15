fdce103fbf7e694b7c82855a04e10e384752eb06
guigang xie <gg.xie@bionovogene.com>
2021-11-15 16:03:16 +0800

    fix of cast data from the data repository

--- src/repository/Bio.Repository/KEGG/ReactionRepository/CompoundRepository.vb
+++ src/repository/Bio.Repository/KEGG/ReactionRepository/CompoundRepository.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::cb5dbc6ff836aebcdf1c6de2c00ad96c, Bio.Repository\KEGG\ReactionRepository\CompoundRepository.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class CompoundRepository
-    ' 
-    '     Properties: Compounds
-    ' 
-    '     Constructor: (+2 Overloads) Sub New
-    '     Function: BuildIndexTable, Exists, GenericEnumerator, GetAll, GetByKey
-    '               GetEnumerator, GetNames, GetWhere, (+2 Overloads) ScanModels, (+2 Overloads) ScanRepository
-    ' 
-    ' Class CompoundIndex
-    ' 
-    '     Properties: DbTerms, Entity, ID, Index
-    ' 
-    '     Function: ToString
-    ' 
-    ' /********************************************************************************/
+' Class CompoundRepository
+' 
+'     Properties: Compounds
+' 
+'     Constructor: (+2 Overloads) Sub New
+'     Function: BuildIndexTable, Exists, GenericEnumerator, GetAll, GetByKey
+'               GetEnumerator, GetNames, GetWhere, (+2 Overloads) ScanModels, (+2 Overloads) ScanRepository
+' 
+' Class CompoundIndex
+' 
+'     Properties: DbTerms, Entity, ID, Index
+' 
+'     Function: ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -106,6 +106,11 @@ Public Class CompoundRepository : Inherits XmlDataModel
         Return compoundTable.ContainsKey(key)
     End Function
 
+    ''' <summary>
+    ''' returns nothing if the given key is not exists in the repository data.
+    ''' </summary>
+    ''' <param name="key"></param>
+    ''' <returns></returns>
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetByKey(key As String) As CompoundIndex Implements IRepositoryRead(Of String, CompoundIndex).GetByKey
         If compoundTable.ContainsKey(key) Then
@@ -238,4 +243,12 @@ Public Class CompoundIndex
     Public Overrides Function ToString() As String
         Return ID
     End Function
+
+    Public Shared Narrowing Operator CType(index As CompoundIndex) As Compound
+        If index Is Nothing Then
+            Return Nothing
+        Else
+            Return index.Entity
+        End If
+    End Operator
 End Class
--- src/workbench/R#/kegg_kit/network.vb
+++ src/workbench/R#/kegg_kit/network.vb
@@ -3,40 +3,33 @@ Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+Imports SMRUCC.genomics.Data
 Imports SMRUCC.genomics.Model.Network.KEGG.ReactionNetwork
 
 <Package("network")>
 Module network
 
     <ExportAPI("fromCompounds")>
-    Public Function fromCompoundId(compoundsId As String(), graph As Reaction(), Optional compounds As Compound() = Nothing) As NetworkGraph
+    Public Function fromCompoundId(compoundsId As String(), graph As Reaction(), Optional compounds As CompoundRepository = Nothing) As NetworkGraph
         ' BuildModel(br08901 As IEnumerable(Of ReactionTable), compounds As IEnumerable(Of NamedValue(Of String)),
         Dim template As ReactionTable() = ReactionTable.Load(graph)
         Dim cid As NamedValue(Of String)()
 
-        If compounds.IsNullOrEmpty Then
+        If compounds Is Nothing Then
             cid = compoundsId _
                 .Select(Function(c)
                             Return New NamedValue(Of String)(c, c, c)
                         End Function) _
                 .ToArray
         Else
-            With compounds _
-                .GroupBy(Function(c) c.entry) _
-                .ToDictionary(Function(c) c.Key,
-                              Function(list)
-                                  Return list.First
-                              End Function)
+            cid = compoundsId _
+                .Select(Function(c)
+                            Dim model As Compound = compounds.GetByKey(c)
+                            Dim name As String = If(model Is Nothing, c, If(model.commonNames.FirstOrDefault, c))
 
-                cid = compoundsId _
-                    .Select(Function(c)
-                                Dim model As Compound = .TryGetValue(c)
-                                Dim name As String = If(model Is Nothing, c, If(model.commonNames.FirstOrDefault, c))
-
-                                Return New NamedValue(Of String)(c, name, c)
-                            End Function) _
-                    .ToArray
-            End With
+                            Return New NamedValue(Of String)(c, name, c)
+                        End Function) _
+                .ToArray
         End If
 
         Return template.BuildModel(
