6f897ea96f971a73abd35d4eb45f532d9883a606
xieguigang <xie.guigang@gcmodeller.org>
2021-11-21 01:36:45 +0800

    Update Program.vb

--- src/GCModeller/analysis/STdeconvolve/test/Program.vb
+++ src/GCModeller/analysis/STdeconvolve/test/Program.vb
@@ -6,6 +6,8 @@ Module Program
     Sub Main(args As String())
         Dim STdataset As Matrix = Matrix.LoadData("E:\GCModeller\src\GCModeller\analysis\PhenoGraph\demo\HR2MSI mouse urinary bladder S096_top3.csv")
         Dim corpus As Corpus = STdataset.CreateSpatialDocuments
+        Dim result = corpus.LDAModelling(13).Deconvolve(corpus)
+
 
         Pause()
     End Sub
