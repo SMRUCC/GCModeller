f23ac694e80f8dd7609d85f91607ea6dfef837bb
xieguigang <xie.guigang@gcmodeller.org>
2021-11-21 05:13:34 +0800

    dmp matrix

--- src/GCModeller/analysis/SingleCell/STdeconvolve/test/Program.vb
+++ src/GCModeller/analysis/SingleCell/STdeconvolve/test/Program.vb
@@ -5,6 +5,8 @@ Imports SMRUCC.genomics.Analysis.SingleCell.STdeconvolve
 
 Module Program
     Sub Main(args As String())
+        Call dumpMatrix()
+
         Dim STdataset As Matrix = Matrix.LoadData("E:\GCModeller\src\GCModeller\analysis\SingleCell\demo\HR2MSI mouse urinary bladder S096_top3.csv")
         Dim corpus As STCorpus = STdataset.CreateSpatialDocuments
         Dim result = corpus.LDAModelling(13).Deconvolve(corpus)
@@ -12,5 +14,12 @@ Module Program
         Call result.GetJson.SaveTo("E:\GCModeller\src\GCModeller\analysis\SingleCell\STdeconvolve\demo\HR2MSI mouse urinary bladder S096_top3.json")
 
         Pause()
+    End Sub
+
+    Sub dumpMatrix()
+        Dim data = "E:\GCModeller\src\GCModeller\analysis\SingleCell\STdeconvolve\demo\HR2MSI mouse urinary bladder S096_top3.json".LoadJSON(Of Deconvolve)
+
+        Call data.theta.SaveMatrix("E:\GCModeller\src\GCModeller\analysis\SingleCell\STdeconvolve\demo\HR2MSI mouse urinary bladder S096_Deconvolve.csv")
+        Call Pause()
     End Sub
 End Module
