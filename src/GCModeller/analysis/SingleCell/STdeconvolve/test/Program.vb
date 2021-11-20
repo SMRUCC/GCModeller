a62f6d0a279caa0fc56c2cabf633de59323960d7
xieguigang <xie.guigang@gcmodeller.org>
2021-11-21 03:46:29 +0800

    fix of project reference errors

--- src/GCModeller/analysis/SingleCell/STdeconvolve/test/Program.vb
+++ src/GCModeller/analysis/SingleCell/STdeconvolve/test/Program.vb
@@ -5,11 +5,11 @@ Imports SMRUCC.genomics.Analysis.SingleCell.STdeconvolve
 
 Module Program
     Sub Main(args As String())
-        Dim STdataset As Matrix = Matrix.LoadData("E:\GCModeller\src\GCModeller\analysis\PhenoGraph\demo\HR2MSI mouse urinary bladder S096_top3.csv")
+        Dim STdataset As Matrix = Matrix.LoadData("E:\GCModeller\src\GCModeller\analysis\SingleCell\demo\HR2MSI mouse urinary bladder S096_top3.csv")
         Dim corpus As STCorpus = STdataset.CreateSpatialDocuments
         Dim result = corpus.LDAModelling(13).Deconvolve(corpus)
 
-        Call result.GetJson.SaveTo("E:\GCModeller\src\GCModeller\analysis\STdeconvolve\demo\HR2MSI mouse urinary bladder S096_top3.json")
+        Call result.GetJson.SaveTo("E:\GCModeller\src\GCModeller\analysis\SingleCell\STdeconvolve\demo\HR2MSI mouse urinary bladder S096_top3.json")
 
         Pause()
     End Sub
