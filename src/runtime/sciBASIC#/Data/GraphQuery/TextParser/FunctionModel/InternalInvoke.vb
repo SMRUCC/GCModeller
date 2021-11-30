21785c4fed3895a0760c84b4bb592725a4269eae
guigang xie <gg.xie@bionovogene.com>
2021-11-30 15:23:56 +0800

    split source file model

---
+++ src/runtime/sciBASIC#/Data/GraphQuery/TextParser/FunctionModel/InternalInvoke.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.MIME.Html.Document
+
+Namespace TextParser
+
+    Public Class InternalInvoke : Inherits ParserFunction
+
+        Public Property name As String
+        Public Property method As MethodInfo
+
+        Public Overrides Function GetToken(document As InnerPlantText, parameters As String(), isArray As Boolean) As InnerPlantText
+            Return method.Invoke(Nothing, {document, parameters, isArray})
+        End Function
+    End Class
+End Namespace
