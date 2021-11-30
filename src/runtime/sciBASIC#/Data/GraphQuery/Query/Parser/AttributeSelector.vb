60b8027ed009799f10a59ec39b8020c466f1ced1
guigang xie <gg.xie@bionovogene.com>
2021-11-30 12:15:27 +0800

    fix of the possible empty collection problem

--- src/runtime/sciBASIC#/Data/GraphQuery/Query/Parser/AttributeSelector.vb
+++ src/runtime/sciBASIC#/Data/GraphQuery/Query/Parser/AttributeSelector.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::c31e500ea7db8cdf9e9fe85eff0ceea9, Data\GraphQuery\Query\Parser\AttributeSelector.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class AttributeSelector
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: ParseImpl
-    ' 
-    ' /********************************************************************************/
+' Class AttributeSelector
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: ParseImpl
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -51,7 +51,9 @@ Public Class AttributeSelector : Inherits Parser
     Protected Overrides Function ParseImpl(document As InnerPlantText, isArray As Boolean, env As Engine) As InnerPlantText
         If TypeOf document Is HtmlElement AndAlso DirectCast(document, HtmlElement).TagName.StringEmpty Then
             If Not isArray Then
-                document = DirectCast(document, HtmlElement).HtmlElements.First
+                If DirectCast(document, HtmlElement).HtmlElements.Count > 0 Then
+                    document = DirectCast(document, HtmlElement).HtmlElements.First
+                End If
             End If
         End If
 
