c1e5116827948de2cf5afbf2f56438758a3e12a5
guigang xie <gg.xie@bionovogene.com>
2021-12-14 18:00:02 +0800

    create taxonomy tree

--- src/GCModeller/analysis/Metagenome/Metagenome/Mothur/MothurRankTree.vb
+++ src/GCModeller/analysis/Metagenome/Metagenome/Mothur/MothurRankTree.vb
@@ -1,5 +1,6 @@
 ﻿Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.GraphTheory
+Imports Microsoft.VisualBasic.Language
 
 Public Class MothurRankTree : Inherits Tree(Of MothurData)
 
@@ -9,7 +10,29 @@ Public Class MothurRankTree : Inherits Tree(Of MothurData)
 
     Public Shared Function LoadTaxonomySummary(file As String) As MothurRankTree
         Dim rows As MothurData() = file.LoadTsv(Of MothurData).ToArray
+        Dim i As i32 = Scan0
+        Dim root As New MothurRankTree With {
+            .Data = rows(Scan0),
+            .ID = ++i,
+            .label = .Data.rankID,
+            .Childs = New Dictionary(Of String, Tree(Of MothurData))
+        }
+        Dim taxonNode As MothurRankTree = root
+        Dim parent As New Dictionary(Of String, MothurRankTree)
 
+        Call parent.Add(root.label, root)
+
+        For Each row As MothurData In rows.Skip(1)
+            taxonNode = New MothurRankTree With {
+                .Childs = New Dictionary(Of String, Tree(Of MothurData)),
+                .Data = row,
+                .ID = ++i,
+                .label = row.rankID
+            }
+            taxonNode.Parent = parent(row.parentID).Add(taxonNode)
+        Next
+
+        Return root
     End Function
 
 End Class
@@ -22,5 +45,18 @@ Public Class MothurData
     Public Property daughterlevels As Integer
     Public Property total As Integer
     Public Property samples As Dictionary(Of String, Integer)
+
+    Public ReadOnly Property parentID As String
+        Get
+            Dim t As String() = rankID.Split("."c)
+            Dim parent As String = t.Take(t.Length - 1).JoinBy(".")
+
+            Return parent
+        End Get
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return $"[{rankID}] ({total}){taxon}"
+    End Function
 
 End Class
