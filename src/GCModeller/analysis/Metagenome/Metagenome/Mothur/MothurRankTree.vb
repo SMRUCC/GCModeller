9131a35a785a2affe422ae84294dbd72f92ff0e1
guigang xie <gg.xie@bionovogene.com>
2021-12-14 17:41:24 +0800

    how to parse tree data ?

---
+++ src/GCModeller/analysis/Metagenome/Metagenome/Mothur/MothurRankTree.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Data.GraphTheory
+
+Public Class MothurRankTree : Inherits Tree(Of MothurData)
+
+    Public Function GetOTUTable() As OTUTable()
+
+    End Function
+
+    Public Shared Function LoadTaxonomySummary(file As String) As MothurRankTree
+        Dim rows As MothurData() = file.LoadTsv(Of MothurData).ToArray
+
+    End Function
+
+End Class
+
+Public Class MothurData
+
+    Public Property taxlevel As Integer
+    Public Property rankID As String
+    Public Property taxon As String
+    Public Property daughterlevels As Integer
+    Public Property total As Integer
+    Public Property samples As Dictionary(Of String, Integer)
+
+End Class
--- src/workbench/R#/metagenomics_kit/TaxonomyKit.vb
+++ src/workbench/R#/metagenomics_kit/TaxonomyKit.vb
@@ -1,50 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::dafdb55ec4b6163a67db333dc9e04ad2, R#\metagenomics_kit\TaxonomyKit.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module TaxonomyKit
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: buildTree, Consensus, filterLambda, Filters, Lineage
-    '               lineageTable, LoadNcbiTaxonomyTree, printTaxonomy, RangeFilter, TaxonomyBIOMString
-    '               uniqueTaxonomy
-    ' 
-    ' /********************************************************************************/
+' Module TaxonomyKit
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: buildTree, Consensus, filterLambda, Filters, Lineage
+'               lineageTable, LoadNcbiTaxonomyTree, printTaxonomy, RangeFilter, TaxonomyBIOMString
+'               uniqueTaxonomy
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.ComponentModel.Ranges
+Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Analysis.Metagenome
@@ -56,6 +57,7 @@ Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports REnv = SMRUCC.Rsharp.Runtime.Internal.ConsolePrinter
 Imports Taxonomy = SMRUCC.genomics.Metagenomics.Taxonomy
+Imports rdataframe = SMRUCC.Rsharp.Runtime.Internal.Object.dataframe
 
 ''' <summary>
 ''' toolkit for process ncbi taxonomy tree data
@@ -65,10 +67,32 @@ Module TaxonomyKit
 
     Sub New()
         REnv.AttachConsoleFormatter(Of Taxonomy)(AddressOf printTaxonomy)
+
         Internal.Object.Converts.addHandler(GetType(NcbiTaxonomyTree), AddressOf lineageTable)
+        Internal.Object.Converts.addHandler(GetType(OTUTable()), AddressOf getOTUDataframe)
     End Sub
 
-    Private Function lineageTable(x As Object, args As list, env As Environment) As dataframe
+    Private Function getOTUDataframe(table As OTUTable(), args As list, env As Environment) As rdataframe
+        Dim OTU_num As String() = table.Select(Function(r) r.ID).ToArray
+        Dim taxonomy As String() = table.Select(Function(r) r.taxonomy.ToString).ToArray
+        Dim sampleNames As String() = table.PropertyNames
+        Dim matrix As New rdataframe With {
+            .columns = New Dictionary(Of String, Array) From {
+                {"OTU_num", OTU_num},
+                {"taxonomy", taxonomy}
+            }
+        }
+
+        For Each name As String In sampleNames
+            matrix.columns(name) = table _
+                .Select(Function(r) r(name)) _
+                .ToArray
+        Next
+
+        Return matrix
+    End Function
+
+    Private Function lineageTable(x As Object, args As list, env As Environment) As rdataframe
         Dim tree As NcbiTaxonomyTree = DirectCast(x, NcbiTaxonomyTree)
         Dim taxonomy = tree.Taxonomy.Keys _
             .Select(Function(id)
@@ -90,7 +114,7 @@ Module TaxonomyKit
         Dim genus As Array = taxonomy.Select(Function(t) t.genus).ToArray
         Dim species As Array = taxonomy.Select(Function(t) t.species).ToArray
 
-        Return New dataframe With {
+        Return New rdataframe With {
             .columns = New Dictionary(Of String, Array) From {
                 {NameOf(ncbi_taxid), ncbi_taxid},
                 {NameOf(kingdom), kingdom},
@@ -257,5 +281,20 @@ Module TaxonomyKit
             .PopulateTaxonomy(level) _
             .Select(Function(t) DirectCast(t, Taxonomy)) _
             .ToArray
+    End Function
+
+    ''' <summary>
+    ''' Parse the result output from mothur command ``summary.tax``. 
+    ''' </summary>
+    ''' <param name="file"></param>
+    ''' <returns></returns>
+    <ExportAPI("read.mothurTree")>
+    Public Function loadMothurTree(file As String) As MothurRankTree
+        Return MothurRankTree.LoadTaxonomySummary(file)
+    End Function
+
+    <ExportAPI("as.OTU_table")>
+    Public Function asOTUTable(tree As MothurRankTree) As OTUTable()
+        Return tree.GetOTUTable
     End Function
 End Module
