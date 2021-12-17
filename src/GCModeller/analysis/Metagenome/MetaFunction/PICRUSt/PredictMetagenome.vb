fc0c0c770ef6dd7cffc788153a26c18901f78beb
xieguigang <xie.guigang@gcmodeller.org>
2021-12-17 22:39:21 +0800

    stoere PICRUSt ko_13_5_precalculated table in taxonomy tree binary file?

---
+++ src/GCModeller/analysis/Metagenome/MetaFunction/PICRUSt/PredictMetagenome.vb
@@ -0,0 +1,36 @@
+﻿Imports SMRUCC.genomics.Metagenomics
+
+Namespace PICRUSt
+
+    Public Module PredictMetagenome
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="OTUtable"></param>
+        ''' <param name="precalculated">
+        ''' ko_13_5_precalculated.tab
+        ''' </param>
+        ''' <returns></returns>
+        ''' <remarks>
+        ''' For metagenome prediction, PICRUSt takes an input OTU table 
+        ''' that contains identifiers that match tips from the marker 
+        ''' gene (e.g., Greengenes identifiers) with corresponding 
+        ''' abundances for each of those OTUs across one or more samples. 
+        ''' First, PICRUSt normalizes the OTU table by the 16S copy 
+        ''' number predictions so that OTU abundances more accurately 
+        ''' reflect the true abundances of the underlying organisms. 
+        ''' The metagenome is then predicted by looking up the precalculated 
+        ''' genome content for each OTU, multiplying the normalized 
+        ''' OTU abundance by each KO abundance in the genome and 
+        ''' summing these KO abundances together per sample. The 
+        ''' prediction yields a table of KO abundances for each 
+        ''' metagenome sample in the OTU table. For optional organism
+        ''' specific predictions, the per-organism abundances are 
+        ''' retained and annotated for each KO.
+        ''' </remarks>
+        Public Function PredictMetagenome(OTUtable As OTUData(), precalculated As ko_13_5_precalculated) As OTUData()
+
+        End Function
+    End Module
+End Namespace
---
+++ src/GCModeller/analysis/Metagenome/MetaFunction/PICRUSt/ko_13_5_precalculated.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Data.GraphTheory
+Imports SMRUCC.genomics.Metagenomics
+
+Namespace PICRUSt
+
+    Public Class ko_13_5_precalculated : Inherits Tree(Of DataSet)
+
+        Public Property taxonomy As Taxonomy
+
+    End Class
+End Namespace
