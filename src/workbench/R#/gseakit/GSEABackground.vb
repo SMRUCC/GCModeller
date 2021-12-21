53ff693a02877dce9e3e99f7fab92537d6f4c35b
guigang xie <gg.xie@bionovogene.com>
2021-12-21 16:57:33 +0800

    improvements for the KO reference gsea background model handler

--- src/workbench/R#/gseakit/GSEABackground.vb
+++ src/workbench/R#/gseakit/GSEABackground.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::4ef9cdd0bb54ecaac9a311982ce558a6, R#\gseakit\GSEABackground.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module GSEABackground
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: assembleBackground, ClusterIntersections, CreateCluster, createGene, CreateKOBackground
-    '               KOTable, PrintBackground, ReadBackground, WriteBackground
-    ' 
-    ' /********************************************************************************/
+' Module GSEABackground
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: assembleBackground, ClusterIntersections, CreateCluster, createGene, CreateKOBackground
+'               KOTable, PrintBackground, ReadBackground, WriteBackground
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -194,43 +194,59 @@ Public Module GSEABackground
         Return background
     End Function
 
+    <Extension>
+    Private Function KO_category(category As BriteHText) As IEnumerable(Of Cluster)
+        Return category.categoryItems _
+            .SafeQuery _
+            .Select(Function(subtype)
+                        Return subtype.categoryItems _
+                            .SafeQuery _
+                            .Select(Function(pathway)
+                                        Return New Cluster With {
+                                            .ID = "map" & pathway.entryID,
+                                            .description = pathway _
+                                                .ToString _
+                                                .Replace("[BR:ko]", "") _
+                                                .Replace("[PATH:ko]", "") _
+                                                .Trim,
+                                            .names = pathway.description _
+                                                .Replace("[BR:ko]", "") _
+                                                .Replace("[PATH:ko]", "") _
+                                                .Trim,
+                                            .members = pathway.categoryItems _
+                                                .SafeQuery _
+                                                .Select(Function(ko)
+                                                            Return New BackgroundGene With {
+                                                                .accessionID = ko.entryID,
+                                                                .[alias] = {ko.entryID},
+                                                                .locus_tag = New NamedValue With {.name = ko.entryID, .text = ko.description},
+                                                                .name = ko.description,
+                                                                .term_id = {ko.entryID}
+                                                            }
+                                                        End Function) _
+                                                .ToArray
+                                        }
+                                    End Function)
+                    End Function) _
+            .IteratesALL
+    End Function
+
     <ExportAPI("KO_reference")>
     Public Function CreateKOReference() As Background
         Dim ko00001 = htext.ko00001.Hierarchical.categoryItems
-        Dim clusters = ko00001 _
+        Dim clusters As Cluster() = ko00001 _
             .Select(Function(category)
-                        Return category.categoryItems _
-                            .SafeQuery _
-                            .Select(Function(subtype)
-                                        Return subtype.categoryItems _
-                                            .SafeQuery _
-                                            .Select(Function(pathway)
-                                                        Return New Cluster With {
-                                                            .ID = "map" & pathway.entryID,
-                                                            .description = pathway.ToString,
-                                                            .names = pathway.description,
-                                                            .members = pathway.categoryItems _
-                                                                .SafeQuery _
-                                                                .Select(Function(ko)
-                                                                            Return New BackgroundGene With {
-                                                                                .accessionID = ko.entryID,
-                                                                                .[alias] = {ko.entryID},
-                                                                                .locus_tag = New NamedValue With {.name = ko.entryID, .text = ko.description},
-                                                                                .name = ko.description,
-                                                                                .term_id = {ko.entryID}
-                                                                            }
-                                                                        End Function) _
-                                                                .ToArray
-                                                        }
-                                                    End Function)
-                                    End Function)
+                        Return category.KO_category
                     End Function) _
-            .IteratesALL _
             .IteratesALL _
             .ToArray
 
         Return New Background With {
-            .clusters = clusters
+            .clusters = clusters,
+            .build = Now,
+            .comments = "The KEGG orthology reference pathway model",
+            .id = NameOf(ko00001),
+            .name = "KEGG Orthology (KO)"
         }
     End Function
 
