4ce078844315631f65cb43ca95163fc3c80d5082
xieguigang <xie.guigang@gcmodeller.org>
2022-01-09 05:01:02 +0800

    config script for run gsva test

--- src/GCModeller/annotations/GSEA/data/HTS/gsva.py
+++ src/GCModeller/annotations/GSEA/data/HTS/gsva.py
@@ -2,11 +2,38 @@ import GCModeller
 
 from gseakit import GSVA 
 from visualkit import visualPlot
+from phenotype_kit import geneExpression
 
 expr0    = read.csv(`${@dir}/metabolome.csv`, row.names = 1)
 metainfo = read.csv(`${@dir}/metainfo.csv`, row.names = None)
 metaSet  = read.csv(`${@dir}/kegg_enrichment.xls`, row.names = None, check.names = False, tsv = True)
+metaSet[, "names"] = None
+
+metainfo[, "KEGG"] = unique.names(metainfo[, "KEGG"])
+metainfo = as.list(metainfo, byrow = True)
+names(metainfo, sapply(metainfo, r -> r$KEGG))
+metainfo = lapply(metainfo, r -> r$name)
 
 print(expr0, max.print = 13)
 print(metaSet, max.print = 13)
-print(metainfo, max.print = 13)
+# str(metainfo)
+
+def getCluster(term):
+    
+    kegg_id = strsplit(term$compounds, ";")
+    names = sapply(kegg_id, id -> metainfo[[id]])
+    
+    return names
+    
+
+metaSet = lapply(as.list(metaSet, byrow = True), r -> getCluster(term = r), names = r -> r$term)
+
+# print("view of the background dataset:")
+# str(metaSet)
+
+scores = GSVA::gsva(expr0, metaSet)
+
+print("preview of your gsva score matrix:")
+print(as.data.frame(scores), max.print = 13)
+
+write.expr_matrix(scores, file = `${@dir}/gsva.csv`, id = "pathway Name")
