c050fe3df6f245ebd90f1467afd924e5fc33f36d
xieguigang <xie.guigang@gcmodeller.org>
2022-01-09 04:28:13 +0800

    read data

--- src/GCModeller/annotations/GSEA/data/HTS/gsva.py
+++ src/GCModeller/annotations/GSEA/data/HTS/gsva.py
@@ -1,2 +1,9 @@
 import GCModeller
+
+from gseakit import GSVA 
 from visualkit import visualPlot
+
+expr0    = read.csv(`${@dir}/metabolome.csv`, row.names = 1)
+metainfo = read.csv(`${@dir}/metainfo.csv`, row.names = None)
+
+print(expr0, max.print = 13)
