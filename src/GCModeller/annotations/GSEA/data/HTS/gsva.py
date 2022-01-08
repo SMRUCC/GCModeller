a37207e330c255424a4a4f683e4de9f4276f3c35
xieguigang <xie.guigang@gcmodeller.org>
2022-01-09 04:33:00 +0800

    test read data

--- src/GCModeller/annotations/GSEA/data/HTS/gsva.py
+++ src/GCModeller/annotations/GSEA/data/HTS/gsva.py
@@ -5,5 +5,8 @@ from visualkit import visualPlot
 
 expr0    = read.csv(`${@dir}/metabolome.csv`, row.names = 1)
 metainfo = read.csv(`${@dir}/metainfo.csv`, row.names = None)
+metaSet  = read.csv(`${@dir}/kegg_enrichment.xls`, row.names = None, check.names = False, tsv = True)
 
 print(expr0, max.print = 13)
+print(metaSet, max.print = 13)
+print(metainfo, max.print = 13)
