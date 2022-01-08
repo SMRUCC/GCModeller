2d20e36208af2d6276a140edce765de696de9fb5
xieguigang <xie.guigang@gcmodeller.org>
2022-01-09 05:28:04 +0800

    negative value?

--- src/GCModeller/annotations/GSEA/data/HTS/gsva.py
+++ src/GCModeller/annotations/GSEA/data/HTS/gsva.py
@@ -25,15 +25,59 @@ def getCluster(term):
     
     return names
     
+def run_gsva():
 
-metaSet = lapply(as.list(metaSet, byrow = True), r -> getCluster(term = r), names = r -> r$term)
+    metaSet = lapply(as.list(metaSet, byrow = True), r -> getCluster(term = r), names = r -> r$term)
 
-# print("view of the background dataset:")
-# str(metaSet)
+    # print("view of the background dataset:")
+    # str(metaSet)
 
-scores = GSVA::gsva(expr0, metaSet)
+    scores = GSVA::gsva(expr0, metaSet)
 
-print("preview of your gsva score matrix:")
-print(as.data.frame(scores), max.print = 13)
+    print("preview of your gsva score matrix:")
+    print(as.data.frame(scores), max.print = 13)
 
-write.expr_matrix(scores, file = `${@dir}/gsva.csv`, id = "pathway Name")
+    write.expr_matrix(scores, file = `${@dir}/gsva.csv`, id = "pathway Name")
+
+    return as.data.frame(scores)
+
+gsva_score = run_gsva()
+
+# test group diff
+A01 = ["A01.1", "A01.2", "A01.3", "A01.4", "A01.5", "A01.6"]
+CK  = ["CK1", "CK2", "CK3", "CK4", "CK5", "CK6"]
+
+A01       = gsva_score[, A01]
+CK        = gsva_score[, CK]
+pathNames = rownames(gsva_score)
+
+def diff(name, g1, g2):
+    
+    mean1  = mean(g1)
+    mean2  = mean(g2)
+    sd1    = sd(g1)
+    sd2    = sd(g2)
+    t      = as.object(t.test(g1, g2))
+    pvalue = t$Pvalue
+    t      = t$TestValue
+    
+    return (name, mean1, mean2, sd1, sd2, t, pvalue)    
+    
+compares   = lapply(1:nrow(gsva_score), i -> diff(pathNames[i], unlist(as.list(A01[i, ])), unlist(as.list(CK[i, ]))))
+foldchange = sapply(compares, i -> (i$mean1) / (i$mean2))
+log2fc     = log(foldchange, 2)
+m1         = sapply(compares, i -> i$mean1)
+m2         = sapply(compares, i -> i$mean2)
+sd1        = sapply(compares, i -> i$sd1)
+sd2        = sapply(compares, i -> i$sd2)
+t          = sapply(compares, i -> i$t)
+pvalue     = sapply(compares, i -> i$pvalue)
+pathName   = sapply(compares, i -> i$name)
+
+gsva_diff = data.frame(pathNames = pathName, mean_A01 = m1, mean_CK = m2, sd_A01 = sd1, sd_CK = sd2, foldchange = foldchange, log2fc = log2fc, t = t, pvalue = pvalue)
+
+print(gsva_diff, max.print = 13)
+
+write.csv(gsva_diff, file = `${@dir}/A01_vs_CK_gsva.csv`, row.names = False)
+
+
