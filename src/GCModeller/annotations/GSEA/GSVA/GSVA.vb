ae2f5913c751744a571a0a4e1721f637f9a4ddf9
xieguigang <xie.guigang@gcmodeller.org>
2022-01-09 06:24:19 +0800

    split source file module

--- src/GCModeller/annotations/GSEA/GSVA/GSVA.vb
+++ src/GCModeller/annotations/GSEA/GSVA/GSVA.vb
@@ -237,65 +237,6 @@ Public Module GSVA
 
         Return gene_density
     End Function
-
-    Private Function filterGeneSets(mapped_gset_idx_list As Dictionary(Of String, String()), min_sz As Integer, max_sz As Integer) As Dictionary(Of String, String())
-        Return mapped_gset_idx_list _
-            .Where(Function(d)
-                       Return d.Value.Length >= min_sz AndAlso d.Value.Length <= max_sz
-                   End Function) _
-            .ToDictionary
-    End Function
-
-    ''' <summary>
-    ''' filter out genes with constant expression values
-    ''' </summary>
-    ''' <param name="expr"></param>
-    ''' <param name="method"></param>
-    ''' <returns></returns>
-    Public Function filterFeatures(expr As Matrix, method As Methods) As Matrix
-        Dim sdGenes As Vector = expr.rowSds.Values.AsVector
-
-        sdGenes(sdGenes < 0.0000000001) = Vector.Zero
-
-        If sdGenes.Any(Function(x) x = 0.0 OrElse x.IsNaNImaginary) Then
-            Call $"{(sdGenes = 0.0 OrElse sdGenes.IsNaNImaginary).Sum} genes with constant expression values throuhgout the samples.".Warning
-
-            If method <> Methods.ssgsea Then
-                Call "Since argument method!='ssgsea', genes with constant expression values are discarded.".Warning
-
-                expr = expr(sdGenes > 0 & Not sdGenes.IsNaNImaginary)
-            End If
-        End If
-
-        If expr.size < 2 Then
-            Throw New InvalidProgramException("Less than two genes in the input assay object")
-        Else
-            Return expr
-        End If
-    End Function
-
-    ''' <summary>
-    ''' maps gene sets content in 'gsets' to 'features', where 'gsets'
-    ''' Is a 'list' object with character string vectors as elements,
-    ''' And 'features' is a character string vector object. it assumes
-    ''' features In both input objects follow the same nomenclature
-    ''' </summary>
-    ''' <param name="gsets"></param>
-    ''' <param name="features"></param>
-    ''' <returns></returns>
-    Public Function mapGeneSetsToFeatures(gsets As Background, features As String()) As Dictionary(Of String, String())
-        Dim mapdgenesets = gsets.clusters _
-            .ToDictionary(Function(c) c.ID,
-                          Function(c)
-                              Return c.Intersect(features).ToArray
-                          End Function)
-
-        If mapdgenesets.Values.IteratesALL.Count = 0 Then
-            Throw New InvalidProgramException("No identifiers in the gene sets could be matched to the identifiers in the expression data.")
-        Else
-            Return mapdgenesets
-        End If
-    End Function
 End Module
 
 
--- src/GCModeller/annotations/GSEA/data/HTS/gsva.py
+++ src/GCModeller/annotations/GSEA/data/HTS/gsva.py
@@ -14,8 +14,8 @@ metainfo = as.list(metainfo, byrow = True)
 names(metainfo, sapply(metainfo, r -> r$KEGG))
 metainfo = lapply(metainfo, r -> r$name)
 
-print(expr0, max.print = 13)
-print(metaSet, max.print = 13)
+# print(expr0, max.print = 13)
+# print(metaSet, max.print = 13)
 # str(metainfo)
 
 def getCluster(term):
@@ -39,8 +39,8 @@ def run_gsva():
 
     scores = GSVA::gsva(expr0, metaSet)
 
-    print("preview of your gsva score matrix:")
-    print(as.data.frame(scores), max.print = 13)
+    # print("preview of your gsva score matrix:")
+    # print(as.data.frame(scores), max.print = 13)
 
     write.expr_matrix(scores, file = `${@dir}/gsva.csv`, id = "pathway Name")
 
@@ -50,18 +50,10 @@ def run_gsva():
     
         print(`normalize score: ${i}`)
         scores[, i] = normalization(scores[, i])
-
+    
     return scores
 
 gsva_score = run_gsva()
-
-# test group diff
-A01 = ["A01.1", "A01.2", "A01.3", "A01.4", "A01.5", "A01.6"]
-CK  = ["CK1", "CK2", "CK3", "CK4", "CK5", "CK6"]
-
-A01       = gsva_score[, A01]
-CK        = gsva_score[, CK]
-pathNames = rownames(gsva_score)
 
 def diff(name, g1, g2):
     
@@ -75,21 +67,34 @@ def diff(name, g1, g2):
     
     return (name, mean1, mean2, sd1, sd2, t, pvalue)    
     
-compares   = lapply(1:nrow(gsva_score), i -> diff(pathNames[i], unlist(as.list(A01[i, ])), unlist(as.list(CK[i, ]))))
-foldchange = sapply(compares, i -> (i$mean1) / (i$mean2))
-log2fc     = log(foldchange, 2)
-m1         = sapply(compares, i -> i$mean1)
-m2         = sapply(compares, i -> i$mean2)
-sd1        = sapply(compares, i -> i$sd1)
-sd2        = sapply(compares, i -> i$sd2)
-t          = sapply(compares, i -> i$t)
-pvalue     = sapply(compares, i -> i$pvalue)
-pathName   = sapply(compares, i -> i$name)
+def run_diff(gsva_score) :
+    
+    print("run group diff between A01 and CK...")
 
-gsva_diff = data.frame(pathNames = pathName, mean_A01 = m1, mean_CK = m2, sd_A01 = sd1, sd_CK = sd2, foldchange = foldchange, log2fc = log2fc, t = t, pvalue = pvalue)
+    # test group diff
+    A01 = ["A01.1", "A01.2", "A01.3", "A01.4", "A01.5", "A01.6"]
+    CK  = ["CK1", "CK2", "CK3", "CK4", "CK5", "CK6"]
 
+    A01       = gsva_score[, A01]
+    CK        = gsva_score[, CK]
+    pathNames = rownames(gsva_score)
+        
+    compares   = lapply(1:nrow(gsva_score), i -> diff(pathNames[i], unlist(as.list(A01[i, ])), unlist(as.list(CK[i, ]))))
+    foldchange = sapply(compares, i -> (i$mean1) / (i$mean2))
+    log2fc     = log(foldchange, 2)
+    m1         = sapply(compares, i -> i$mean1)
+    m2         = sapply(compares, i -> i$mean2)
+    sd1        = sapply(compares, i -> i$sd1)
+    sd2        = sapply(compares, i -> i$sd2)
+    t          = sapply(compares, i -> i$t)
+    pvalue     = sapply(compares, i -> i$pvalue)
+    pathName   = sapply(compares, i -> i$name)
+
+    return data.frame(pathNames = pathName, mean_A01 = m1, mean_CK = m2, sd_A01 = sd1, sd_CK = sd2, foldchange = foldchange, log2fc = log2fc, t = t, pvalue = pvalue)
+
+print(" ~done!")
+gsva_diff = run_diff(gsva_score)
 print(gsva_diff, max.print = 13)
-
 write.csv(gsva_diff, file = `${@dir}/A01_vs_CK_gsva.csv`, row.names = False)
 
 
