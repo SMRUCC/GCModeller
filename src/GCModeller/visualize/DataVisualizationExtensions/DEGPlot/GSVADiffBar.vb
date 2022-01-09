0c049ef2d8382e0c05025bce98f1668bfccea3fa
xieguigang <xie.guigang@gcmodeller.org>
2022-01-09 18:58:45 +0800

    draw x axis

--- src/GCModeller/visualize/DataVisualizationExtensions/DEGPlot/GSVADiffBar.vb
+++ src/GCModeller/visualize/DataVisualizationExtensions/DEGPlot/GSVADiffBar.vb
@@ -1,7 +1,12 @@
-﻿Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.Ranges
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Axis
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.MIME.Html
+Imports stdNum = System.Math
 
 Public Class GSVADiffBar : Inherits Plot
 
@@ -14,6 +19,23 @@ Public Class GSVADiffBar : Inherits Plot
     End Sub
 
     Protected Overrides Sub PlotInternal(ByRef g As IGraphics, canvas As GraphicsRegion)
+        Dim rect As Rectangle = canvas.PlotRegion
+        Dim stroke As New Pen(Brushes.Black, 3)
+        Dim t As Double() = diff _
+            .Select(Function(d) stdNum.Abs(d.t)) _
+            .Where(Function(n) Not n.IsNaNImaginary) _
+            .Max _
+            .SymmetricalRange _
+            .CreateAxisTicks(ticks:=5)
+        Dim scaleX = d3js.scale _
+            .linear() _
+            .domain(t) _
+            .range(integers:={rect.Left, rect.Right})
+        Dim axisPen As Pen = CSS.Stroke.TryParse(theme.axisStroke).GDIObject
+        Dim axisTickStroke As Pen = CSS.Stroke.TryParse(theme.axisTickStroke).GDIObject
+
+        Call g.DrawRectangle(stroke, rect)
+        Call g.DrawX(axisPen, $"t-value of GSVA score",)
 
     End Sub
 End Class
