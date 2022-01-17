dd9ef4a49f58730d1bbcc18f245af599a3437ee5
xieguigang <xie.guigang@gcmodeller.org>
2022-01-17 21:53:16 +0800

    get data table result

--- src/GCModeller/engine/Rscript/test2/kinetics.py
+++ src/GCModeller/engine/Rscript/test2/kinetics.py
@@ -1,4 +1,5 @@
 import GCModeller
+import ggplot
 
 from vcellkit import modeller
 
@@ -6,11 +7,23 @@ def eval(s_content):
 
     return kinetics("(Vmax * S) / (Km + S)", Vmax = 10, S = "s", Km = 2).kinetics_lambda().eval_lambda(s = s_content)
 
+contents = []
+rate = []
 
-for target in 0:10 step 0.25:
+for target in 0:30 step 0.25:
     result = eval(target)
+    contents = append(contents, target)
+    rate = append(rate, result)
 
-    print(`target [${target}] kinetics is ${result}`)
+    if target % 6 == 0:
+        print(`target '${target}' kinetics is ${toString(result, format = "F4")}`)
 
+data = data.frame(contents = contents, kinetics_rate = rate)
 
+cat("\n\n")
 
+print(data, max.print = 10)
+
+bitmap(file = `${@dir}/kinetics_rate.png`, size = [1600,1200]):
+
+    
