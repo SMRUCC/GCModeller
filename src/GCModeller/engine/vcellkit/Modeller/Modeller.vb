4abb49a9d588b8a8ef324bf1564d5030d76bddc3
xieguigang <xie.guigang@gcmodeller.org>
2022-01-31 15:11:19 +0800

    update kegg enzymes list

--- src/GCModeller/engine/vcellkit/Modeller/Modeller.vb
+++ src/GCModeller/engine/vcellkit/Modeller/Modeller.vb
@@ -81,8 +81,10 @@ Module vcellModeller
     ''' </summary>
     ''' <param name="export$"></param>
     <ExportAPI("cacheOf.enzyme_kinetics")>
-    Public Sub createKineticsDbCache(Optional export$ = "./")
-        Call htext.GetInternalResource("ko01000").QueryByECNumbers(export).ToArray
+    Public Sub createKineticsDbCache(Optional export$ = "./", Optional ko01000 As String = "ko01000")
+        Call htext.GetInternalResource(ko01000) _
+            .QueryByECNumbers(export) _
+            .ToArray
     End Sub
 
     ''' <summary>
