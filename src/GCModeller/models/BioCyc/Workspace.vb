bb84d603bc8df3074844e87442cbb36cde3c3dd6
xieguigang <xie.guigang@gcmodeller.org>
2022-02-02 01:51:17 +0800

    Update Workspace.vb

--- src/GCModeller/models/BioCyc/Workspace.vb
+++ src/GCModeller/models/BioCyc/Workspace.vb
@@ -1,25 +1,49 @@
 ﻿Imports System.IO
+Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
 
 Public Class Workspace
 
     ReadOnly dir As String
 
-    Dim m_reactions As AttrDataCollection(Of reactions)
+    Dim m_reactions As Lazy(Of AttrDataCollection(Of reactions))
+    Dim m_pathways As Lazy(Of AttrDataCollection(Of pathways))
 
     Public ReadOnly Property reactions As AttrDataCollection(Of reactions)
         Get
-            If m_reactions Is Nothing Then
-                Using file As Stream = "".Open()
+            Return m_reactions.Value
+        End Get
+    End Property
 
-                End Using
-                m_reactions = AttrDataCollection(Of reactions).LoadFile()
-            End If
+    Public ReadOnly Property pathways As AttrDataCollection(Of pathways)
+        Get
+            Return m_pathways.Value
         End Get
     End Property
 
     Sub New(dir As String)
         Me.dir = dir.GetDirectoryFullPath
+
+        m_reactions = New Lazy(Of AttrDataCollection(Of reactions))(Function() openFile(Of reactions)())
+        m_pathways = New Lazy(Of AttrDataCollection(Of pathways))(Function() openFile(Of pathways)())
     End Sub
+
+    Private Function openFile(Of T As Model)() As AttrDataCollection(Of T)
+        Using file As Stream = $"{dir}/{getFileName(Of T)()}".Open(FileMode.OpenOrCreate, doClear:=False, [readOnly]:=True)
+            Return AttrDataCollection(Of T).LoadFile(file)
+        End Using
+    End Function
+
+    Private Shared Function getFileName(Of T As Model)() As String
+        Dim attrs As Object() = GetType(T).GetCustomAttributes(inherit:=True).ToArray
+        Dim ref = From attr As Object In attrs Where TypeOf attr Is XrefAttribute
+        Dim fileName As XrefAttribute = ref.FirstOrDefault
+
+        If fileName Is Nothing Then
+            Throw New MissingFieldException
+        Else
+            Return fileName.Name
+        End If
+    End Function
 
     Public Overrides Function ToString() As String
         Return dir
