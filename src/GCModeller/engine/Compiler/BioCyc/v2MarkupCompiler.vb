2d56c3dd4d0fd41f919be94fbc23f8ad4f3a4d05
xieguigang <xie.guigang@gcmodeller.org>
2022-02-02 02:00:38 +0800

    config for package release

--- src/GCModeller/engine/Compiler/BioCyc/v2MarkupCompiler.vb
+++ src/GCModeller/engine/Compiler/BioCyc/v2MarkupCompiler.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine
+Imports SMRUCC.genomics.Data.BioCyc
 Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
 Imports SMRUCC.genomics.GCModeller.CompilerServices
 
@@ -6,8 +7,14 @@ Namespace BioCyc
 
     Public Class v2MarkupCompiler : Inherits Compiler(Of VirtualCell)
 
+        ReadOnly biocyc As Workspace
+
+        Sub New(biocyc As Workspace)
+            Me.biocyc = biocyc
+        End Sub
+
         Protected Overrides Function CompileImpl(args As CommandLine) As Integer
-            Throw New NotImplementedException()
+
         End Function
     End Class
 End Namespace
