3984af223cd60c337f96fdab692806198c8e3d1e
xieguigang <xie.guigang@gcmodeller.org>
2022-02-02 01:54:36 +0800

    add biocyc compiler module

---
+++ src/GCModeller/engine/Compiler/BioCyc/v2MarkupCompiler.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
+Imports SMRUCC.genomics.GCModeller.CompilerServices
+
+Namespace BioCyc
+
+    Public Class v2MarkupCompiler : Inherits Compiler(Of VirtualCell)
+
+        Protected Overrides Function CompileImpl(args As CommandLine) As Integer
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
