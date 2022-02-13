0bba3472d5bd3e2f2fd3f11bfee10d14e9913e10
xieguigang <xie.guigang@gcmodeller.org>
2022-02-13 19:03:42 +0800

    plot heatmap

---
+++ src/GCModeller/visualize/DataVisualizationExtensions/CatalogProfiling/CatalogHeatMap.vb
@@ -0,0 +1,32 @@
+﻿
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+Imports SMRUCC.genomics.ComponentModel.Annotation
+
+Namespace CatalogProfiling
+
+    Public Class CatalogHeatMap : Inherits Plot
+
+        ReadOnly profile As CatalogProfiles
+        ReadOnly matrix As Matrix
+
+        Public Sub New(profile As CatalogProfiles, matrix As Matrix, theme As Theme)
+            MyBase.New(theme)
+
+            Me.profile = profile
+            Me.matrix = matrix
+        End Sub
+
+        ''' <summary>
+        ''' heatmap是按照代谢途径分块绘制的
+        ''' </summary>
+        ''' <param name="g"></param>
+        ''' <param name="canvas"></param>
+        Protected Overrides Sub PlotInternal(ByRef g As IGraphics, canvas As GraphicsRegion)
+            Throw New NotImplementedException()
+        End Sub
+    End Class
+End Namespace
