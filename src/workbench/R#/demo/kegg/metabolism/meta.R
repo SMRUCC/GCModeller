56c3392de7a50f364497f865b035413e6fc015b4
xieguigang <xie.guigang@gcmodeller.org>
2022-02-19 05:24:59 +0800

    run search test

--- src/workbench/R#/demo/kegg/metabolism/meta.R
+++ src/workbench/R#/demo/kegg/metabolism/meta.R
@@ -3,11 +3,16 @@ require(GCModeller);
 setwd(@dir);
 
 data = read.csv("mummichog_matched_compound_all.csv", row.names = NULL);
-kegg = kegg_compounds();
+kegg = kegg_compounds(TRUE)
+|> lapply(function(c) {
+	names = as.object(c)$commonNames;
+	names[1];
+	
+}, names = c -> as.object(c)$entry);
 
 print(data, max.print = 13);
 
-print(kegg);
+str(kegg);
 
 data[, "Query.Mass"] = as.character(data[, "Query.Mass"]);
 
@@ -19,8 +24,22 @@ data = lapply(data, function(mass) {
 	delta = sapply(mass, x -> x$Mass.Diff);
 	i = order(delta)[1];
 	hit = mass[i];
+	hit$name = kegg[[hit$Matched.Compound]];
 	
 	hit;
 });
 
-str(data)
+str(data);
+
+data = data.frame(
+	mz = sapply(data, x-> x$Query.Mass),
+	name = sapply(data, x-> x$name),
+	type = sapply(data, x-> x$Matched.Form)
+
+);
+
+
+print(data);
+
+write.csv(data, file = "./kegg_names.csv", row.names = FALSE);
+
