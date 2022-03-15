47b2943ce278838a33c703c36af52459c07884cf
guigang xie <gg.xie@bionovogene.com>
2022-03-15 11:00:03 +0800

    config for run algorithm test

--- src/runtime/sciBASIC#/gr/network-visualization/Datavisualization.Network/test/LouvainTest.vb
+++ src/runtime/sciBASIC#/gr/network-visualization/Datavisualization.Network/test/LouvainTest.vb
@@ -47,6 +47,37 @@ Imports Microsoft.VisualBasic.Serialization.JSON
 
 Module LouvainTest
 
+    Const source As String = "D:\GCModeller\src\runtime\sciBASIC#\Data_science\algorithms\Louvain\facebook_combined.txt"
+
+    Sub analysis2()
+        Dim links As String()() = source.ReadAllLines.Skip(1).Select(Function(str) Strings.Trim(str).StringSplit("\s+")).ToArray
+        Dim g As New NetworkGraph
+
+        ' build network via links
+        For Each line As String() In links
+            If g.GetElementByID(line(0)) Is Nothing Then
+                g.CreateNode(line(0))
+            End If
+            If g.GetElementByID(line(1)) Is Nothing Then
+                g.CreateNode(line(1))
+            End If
+
+            g.CreateEdge(g.GetElementByID(line(0)), g.GetElementByID(line(1)), 1)
+        Next
+
+        ' the original network with communities labeled
+        Dim clusters As NetworkGraph = Communities.AnalysisUnweighted(g)
+
+        Call Console.WriteLine(Communities.Modularity(clusters))
+        Call Console.WriteLine(Communities.Community(g).GetJson(indent:=True))
+
+        Call clusters _
+            .Tabular _
+            .Save("D:\GCModeller\src\runtime\sciBASIC#\Data_science\algorithms\UnweightedFastUnfolding\facebook_combined_graph")
+
+        Pause()
+    End Sub
+
     Sub Main()
 
         Dim links As String()() = "E:\GCModeller\src\runtime\sciBASIC#\Data_science\algorithms\Louvain\facebook_combined.txt".ReadAllLines.Skip(1).Select(Function(str) Strings.Trim(str).StringSplit("\s+")).ToArray
