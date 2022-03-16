b46ec44051318d2a1122c08fef0a88c513c49b6b
xieguigang <xie.guigang@gcmodeller.org>
2022-03-16 11:37:52 +0800

    improvements of the deque debug view

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/ComponentModel/DataStructures/Deque/Deque.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/ComponentModel/DataStructures/Deque/Deque.vb
@@ -1,48 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::802129f8ed49dcb64229b2536bd47bc2, Microsoft.VisualBasic.Core\src\ComponentModel\DataStructures\Deque\Deque.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Deque
-    ' 
-    '         Properties: Count, First, IsReadOnly, Last, version
-    ' 
-    '         Function: Contains, GetEnumerator, GetEnumerator1, IndexOf, Peek
-    '                   Remove, RemoveHead, RemoveTail, Reverse
-    ' 
-    '         Sub: Add, AddHead, Clear, CopyTo, CopyToReversed
-    '              Insert, RemoveAt
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Deque
+' 
+'         Properties: Count, First, IsReadOnly, Last, version
+' 
+'         Function: Contains, GetEnumerator, GetEnumerator1, IndexOf, Peek
+'                   Remove, RemoveHead, RemoveTail, Reverse
+' 
+'         Sub: Add, AddHead, Clear, CopyTo, CopyToReversed
+'              Insert, RemoveAt
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -170,6 +170,10 @@ Namespace ComponentModel.Collection.Deque
             data.Clear()
             version += 1
         End Sub
+
+        Public Overrides Function ToString() As String
+            Return $"first: {First} --- {Count} element --- last: {Last}"
+        End Function
 
         ''' <summary>
         ''' Determines whether an element is in the Deque(Of T).
