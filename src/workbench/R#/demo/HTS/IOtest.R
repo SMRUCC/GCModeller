488b050af4312e0723f50d48483e0e85e3bb60ac
xieguigang <xie.guigang@gcmodeller.org>
2022-04-09 19:14:55 +0800

    the io test result is not working as expetced

--- src/workbench/R#/demo/HTS/IOtest.R
+++ src/workbench/R#/demo/HTS/IOtest.R
@@ -5,7 +5,7 @@ imports ["geneExpression", "sampleInfo"] from "phenotype_kit";
 
 @profile {
 
-	x = load.expr("C:\GSM3067190_06hpf.csv");
+	x = load.expr0("C:\GSM3067190_06hpf.HTS");
 	x = NULL;
 	
 }
@@ -14,9 +14,11 @@ print(" ~~done!");
 profile = profiler.fetch() |> as.data.frame();
 print(profile, max.print = 13);
 
+gc();
+
 @profile {
 
-	x = load.expr0("C:\GSM3067190_06hpf.HTS");
+	x = load.expr("C:\GSM3067190_06hpf.csv");
 	x = NULL;
 	
 }
