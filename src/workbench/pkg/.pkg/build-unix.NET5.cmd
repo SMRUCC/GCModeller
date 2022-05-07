9cbc18d80162044708ea4ef9a8b407f8a8a8faca
guigang xie <gg.xie@bionovogene.com>
2022-05-07 17:18:07 +0800

    build .net 5 package

--- src/workbench/pkg/.pkg/build-unix.NET5.cmd
+++ src/workbench/pkg/.pkg/build-unix.NET5.cmd
@@ -4,9 +4,9 @@ SET R_HOME=../../../R-sharp\App\net5.0
 SET Rscript="%R_HOME%/Rscript.exe"
 SET R="%R_HOME%/R#.exe"
 
-%Rscript% --build /src ../ /save ./GCModeller.NET5.zip
+%Rscript% --build /src ../ /save ./GCModeller-net5.0.zip
 
 REM install local
-%R% --install.packages ./GCModeller.NET5.zip
+%R% --install.packages ./GCModeller-net5.0.zip
 
 pause
