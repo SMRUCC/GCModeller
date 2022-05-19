bbf54908986ca038e8dcf98b6962bab0a9de8cbc
guigang xie <gg.xie@bionovogene.com>
2022-05-19 10:59:35 +0800

    removes debug echo

--- src/repository/graphquery/kegg/R/compound.R
+++ src/repository/graphquery/kegg/R/compound.R
@@ -8,8 +8,8 @@ const kegg_compound as function(url) {
   # parse the page text
   const keyValues = keyIndex(http_query(url, raw = FALSE));
 
-  print(keyValues);
-  stop();
+  # str(keyValues);
+  # stop();
 
   # parse fields
   const xref        = graphquery::query(document = Html::parse(keyValues$"Other DBs"),  graphquery = get_graph("graphquery/fields/dbLinks.graphquery"));
