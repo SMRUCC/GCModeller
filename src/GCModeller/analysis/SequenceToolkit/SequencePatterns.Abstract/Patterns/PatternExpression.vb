741c2115a4f2b013cd674aef6336c3c23f1f15f6
xieguigang <xie.guigang@gcmodeller.org>
2022-06-08 19:34:11 +0800

    code style improvements

--- src/GCModeller/analysis/SequenceToolkit/SequencePatterns.Abstract/Patterns/PatternExpression.vb
+++ src/GCModeller/analysis/SequenceToolkit/SequencePatterns.Abstract/Patterns/PatternExpression.vb
@@ -1,55 +1,52 @@
 ﻿#Region "Microsoft.VisualBasic::9fb8dc33b82fb56fe530d19686077ee9, analysis\SequenceToolkit\SequencePatterns.Abstract\Patterns\PatternExpression.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class PatternExpression
-    ' 
-    '         Properties: Identifier, Motif, RangeExpr
-    ' 
-    '         Function: Match, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class PatternExpression
+' 
+'         Properties: Identifier, Motif, RangeExpr
+' 
+'         Function: Match, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Text
 Imports System.Text.RegularExpressions
-Imports System.Xml.Serialization
-Imports SMRUCC.genomics.SequenceModel
-Imports SMRUCC.genomics.SequenceModel.NucleotideModels
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.Linq
-Imports Microsoft.VisualBasic.Scripting.TokenIcer
+Imports SMRUCC.genomics.SequenceModel
+Imports SMRUCC.genomics.SequenceModel.NucleotideModels
 
 Namespace Motif.Patterns
 
@@ -67,8 +64,8 @@ Namespace Motif.Patterns
             End Get
             Set(value As Residue())
                 __motif = value
-                __regex = New Regex(String.Join("", value.Select(Function(x) x.Regex).ToArray))
-                __rc = New Regex(String.Join("", value.Reverse.Select(Function(x) x.GetComplement.Regex).ToArray))
+                __regex = New Regex(value.Select(Function(x) x.Regex).JoinBy(""))
+                __rc = New Regex(value.Reverse.Select(Function(x) x.GetComplement.Regex).JoinBy(""))
             End Set
         End Property
 
