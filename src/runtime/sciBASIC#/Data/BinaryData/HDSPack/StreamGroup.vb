285c4327dbc69d285de8554aab46ede07354bc57
guigang xie <gg.xie@bionovogene.com>
2022-06-27 15:53:32 +0800

    code for filesystem operation

--- src/runtime/sciBASIC#/Data/BinaryData/HDSPack/StreamGroup.vb
+++ src/runtime/sciBASIC#/Data/BinaryData/HDSPack/StreamGroup.vb
@@ -4,16 +4,102 @@ Public Class StreamGroup : Inherits StreamObject
 
     Public ReadOnly Property tree As Dictionary(Of String, StreamObject)
 
-    Public Function GetDataBlock(filepath As FilePath) As StreamBlock
+    Sub New()
+    End Sub
 
+    ''' <summary>
+    ''' create a new file tree
+    ''' </summary>
+    ''' <param name="directory">
+    ''' the folder directory path
+    ''' </param>
+    Sub New(directory As String)
+        Call Me.New(directory.Split("/"c))
+    End Sub
+
+    ''' <summary>
+    ''' create a new file tree
+    ''' </summary>
+    ''' <param name="dirs"></param>
+    Sub New(dirs As IEnumerable(Of String))
+        Call MyBase.New(New FilePath(dirs, isDir:=True, isAbs:=True))
+        ' create a new empty folder tree
+        tree = New Dictionary(Of String, StreamObject)
+    End Sub
+
+    Public Function GetDataBlock(filepath As FilePath) As StreamBlock
+        Return VisitBlock(filepath)
     End Function
 
     Public Function AddDataBlock(filepath As FilePath) As StreamBlock
         Dim createNew As New StreamBlock(filepath)
-
         ' then add current file block to the tree
+        Dim dir As FilePath = filepath.ParentDirectory
+        Dim dirBlock As StreamGroup = VisitBlock(dir)
+
+        If dirBlock Is Nothing Then
+            ' no dir tree
+            dirBlock = AddDataGroup(filepath)
+        End If
+
+        Call dirBlock.tree.Add(filepath.FileName, createNew)
 
         Return createNew
+    End Function
+
+    Public Function AddDataGroup(filepath As FilePath) As StreamGroup
+        Dim tree As Dictionary(Of String, StreamObject) = Me.tree
+        Dim file As StreamObject
+        Dim names As String() = filepath.Components
+        Dim name As String
+
+        For i As Integer = 0 To names.Length - 1
+            name = names(i)
+
+            If Not tree.ContainsKey(name) Then
+                tree.Add(name, New StreamGroup(filepath.Components.Take(i)))
+                tree = DirectCast(tree(name), StreamGroup).tree
+            Else
+                file = tree(name)
+
+                If TypeOf file Is StreamGroup Then
+                    tree = DirectCast(file, StreamGroup).tree
+                Else
+                    ' required a folder(file group)
+                    ' but a file is exists?
+                    Throw New InvalidProgramException($"a folder can not be created at location '{names.Take(i).JoinBy("/")}' due to the reason of there is a file whith the same name on the target location!")
+                End If
+            End If
+        Next
+
+        Return tree(filepath.Components.Last)
+    End Function
+
+    Private Function VisitBlock(filepath As FilePath) As StreamObject
+        Dim tree As Dictionary(Of String, StreamObject) = Me.tree
+        Dim file As StreamObject
+        Dim names As String() = filepath.Components
+        Dim name As String
+
+        For i As Integer = 0 To names.Length - 1
+            name = names(i)
+
+            If Not tree.ContainsKey(name) Then
+                Return Nothing
+            Else
+                file = tree(name)
+
+                If TypeOf file Is StreamGroup Then
+                    tree = DirectCast(file, StreamGroup).tree
+                ElseIf i <> names.Length - 1 Then
+                    ' required a folder(file group)
+                    ' but a file is exists?
+                    Return Nothing
+                End If
+            End If
+        Next
+
+        Return tree(filepath.Components.Last)
     End Function
 
     ''' <summary>
@@ -25,29 +111,10 @@ Public Class StreamGroup : Inherits StreamObject
     ''' </param>
     ''' <returns></returns>
     Public Function BlockExists(filepath As FilePath) As Boolean
-        Dim tree As Dictionary(Of String, StreamObject) = Me.tree
-        Dim file As StreamObject
-        Dim names As String() = filepath.Components
-        Dim name As String
+        Return Not VisitBlock(filepath) Is Nothing
+    End Function
 
-        For i As Integer = 0 To names.Length - 1
-            name = names(i)
-
-            If Not tree.ContainsKey(name) Then
-                Return False
-            Else
-                file = tree(name)
-
-                If TypeOf file Is StreamGroup Then
-                    tree = DirectCast(file, StreamGroup).tree
-                ElseIf i <> names.Length - 1 Then
-                    ' required a folder(file group)
-                    ' but a file is exists?
-                    Return False
-                End If
-            End If
-        Next
-
-        Return True
+    Public Shared Function CreateRootTree() As StreamGroup
+        Return New StreamGroup("/")
     End Function
 End Class
--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/Extensions/IO/Path/FilePath.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/Extensions/IO/Path/FilePath.vb
@@ -18,6 +18,22 @@
             End Get
         End Property
 
+        Public ReadOnly Property ParentDirectory As FilePath
+            Get
+                If Components.Length - 1 Then
+                    Return New FilePath("/")
+                Else
+                    Return New FilePath(Components.Take(Components.Length - 1), IsDirectory, IsAbsolutePath)
+                End If
+            End Get
+        End Property
+
+        Public Sub New(tokens As IEnumerable(Of String), isDir As Boolean, isAbs As Boolean)
+            Components = (From name As String In tokens Where Not name.StringEmpty).ToArray
+            IsDirectory = isDir
+            IsAbsolutePath = isAbs
+        End Sub
+
         Sub New(filepath As String)
             If filepath.EndsWith("/"c) OrElse filepath.EndsWith("\"c) Then
                 IsDirectory = True
