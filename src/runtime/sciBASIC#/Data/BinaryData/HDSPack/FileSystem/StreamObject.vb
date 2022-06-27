ecf3778b3e125fe0dd7420ae5ec985e51a84f392
guigang xie <gg.xie@bionovogene.com>
2022-06-27 20:10:44 +0800

    add attribute data

--- src/runtime/sciBASIC#/Data/BinaryData/HDSPack/FileSystem/StreamObject.vb
+++ src/runtime/sciBASIC#/Data/BinaryData/HDSPack/FileSystem/StreamObject.vb
@@ -1,23 +1,48 @@
 ﻿Imports Microsoft.VisualBasic.FileIO.Path
+Imports Microsoft.VisualBasic.Linq
+Imports any = Microsoft.VisualBasic.Scripting
 
-Public MustInherit Class StreamObject
+Namespace FileSystem
 
-    Public ReadOnly Property referencePath As FilePath
-    Public ReadOnly Property fileName As String
-        Get
-            Return referencePath.FileName
-        End Get
-    End Property
+    ''' <summary>
+    ''' the abstract type of the file or directory object
+    ''' </summary>
+    Public MustInherit Class StreamObject
 
-    Sub New(path As FilePath)
-        referencePath = path
-    End Sub
+        Public ReadOnly Property referencePath As FilePath
+        Public ReadOnly Property fileName As String
+            Get
+                Return referencePath.FileName
+            End Get
+        End Property
 
-    Sub New()
-    End Sub
+        ''' <summary>
+        ''' comments about this file object
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property description As String
+        Public Property attributes As New Dictionary(Of String, Object)
 
-    Public Overrides Function ToString() As String
-        Return referencePath.ToString
-    End Function
+        Sub New(path As FilePath)
+            referencePath = path
+        End Sub
 
-End Class
+        Sub New()
+        End Sub
+
+        Public Sub AddAttributes(attrs As Dictionary(Of String, Object))
+            For Each item As KeyValuePair(Of String, Object) In attrs.SafeQuery
+                If item.Key = NameOf(description) Then
+                    description = any.ToString(item.Value)
+                Else
+                    _attributes(item.Key) = item.Value
+                End If
+            Next
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return referencePath.ToString
+        End Function
+
+    End Class
+End Namespace
