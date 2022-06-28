930bba8f3835b1f309dbfed3b335c0b883d74bae
xieguigang <xie.guigang@gcmodeller.org>
2022-06-28 13:24:04 +0800

    fix of the namespace reference error

--- src/Settings/Shared/Settings.FileSystem/FileSystem.vb
+++ src/Settings/Shared/Settings.FileSystem/FileSystem.vb
@@ -1,55 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::1f363922baccdc82916c5ae39e48cb96, Shared\Settings.FileSystem\FileSystem.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module FileSystem
-    ' 
-    ' 
-    '     Module FileSystem
-    ' 
-    '         Properties: CDD, COGs, Correlations, GO, InterproXml
-    '                     IsNullOrEmpty, KEGGFamilies, MotifLDM, RegpreciseBBH, RegPreciseRegulatorFasta
-    '                     RegpreciseRoot, Regulations, RepositoryRoot
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: GetLocalblast, (+2 Overloads) GetMotifLDM, GetPfamDb, GetR_HOME, GetRegpreciseBBH
-    '                   GetRegpreciseRoot, GetRegulations, GetRepositoryRoot, IsRepositoryNullOrEmpty, TryGetSource
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module FileSystem
+' 
+' 
+'     Module FileSystem
+' 
+'         Properties: CDD, COGs, Correlations, GO, InterproXml
+'                     IsNullOrEmpty, KEGGFamilies, MotifLDM, RegpreciseBBH, RegPreciseRegulatorFasta
+'                     RegpreciseRoot, Regulations, RepositoryRoot
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: GetLocalblast, (+2 Overloads) GetMotifLDM, GetPfamDb, GetR_HOME, GetRegpreciseBBH
+'                   GetRegpreciseRoot, GetRegulations, GetRepositoryRoot, IsRepositoryNullOrEmpty, TryGetSource
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports Microsoft.VisualBasic.ApplicationServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.Repository
 Imports Microsoft.VisualBasic.FileIO.Path
