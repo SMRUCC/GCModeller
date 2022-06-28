75b0b23ab2421db9895b27552cbe0dc73d98cf52
guigang xie <gg.xie@bionovogene.com>
2022-06-28 10:48:31 +0800

    show HDS fs tree and add global attribute data impementation

--- src/runtime/sciBASIC#/Data/BinaryData/HDSPack/Debugger.vb
+++ src/runtime/sciBASIC#/Data/BinaryData/HDSPack/Debugger.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
 Imports Microsoft.VisualBasic.Linq
 
@@ -23,4 +24,27 @@ Public Module Debugger
             End If
         Next
     End Function
+
+    ''' <summary>
+    ''' a linux tree liked command for show structure 
+    ''' inside of the HDS stream pack file.
+    ''' </summary>
+    ''' <param name="dir"></param>
+    ''' <param name="text"></param>
+    <Extension>
+    Public Sub Tree(dir As StreamGroup, text As TextWriter, Optional indent As Integer = 0)
+        text.WriteLine(dir.ToString)
+        TreeInternal(dir, text, indent)
+    End Sub
+
+    Private Sub TreeInternal(dir As StreamGroup, text As TextWriter, indent As Integer)
+        For Each file As StreamObject In dir.files
+            If TypeOf file Is StreamBlock Then
+                Call text.WriteLine($"{New String(" ", indent)}|-" & file.ToString)
+            Else
+                Call text.WriteLine($"{New String(" ", indent)}|-" & file.ToString)
+                Call TreeInternal(dir:=file, text, indent + 1)
+            End If
+        Next
+    End Sub
 End Module
