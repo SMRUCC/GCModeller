8b3da786c76e67f4e2831ff2cb24801c5c61397d
guigang xie <gg.xie@bionovogene.com>
2022-06-29 13:40:39 +0800

    write text extension method

---
+++ src/runtime/sciBASIC#/Data/BinaryData/HDSPack/Extensions.vb
@@ -0,0 +1,27 @@
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
+Imports Microsoft.VisualBasic.Text
+
+<HideModuleName>
+Public Module Extensions
+
+    <Extension>
+    Public Function WriteText(pack As StreamPack,
+                              text As String,
+                              fileName As String,
+                              Optional encoding As Encodings = Encodings.UTF8) As Boolean
+
+        Dim buffer As Stream = pack.OpenBlock(fileName)
+        Dim bin As New StreamWriter(buffer, encoding.CodePage)
+
+        Call bin.WriteLine(text)
+        Call bin.Flush()
+
+        If TypeOf buffer Is StreamBuffer Then
+            Call buffer.Dispose()
+        End If
+
+        Return True
+    End Function
+End Module
