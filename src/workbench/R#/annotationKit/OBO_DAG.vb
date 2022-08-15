4f4ab68a8788b1b62260011ffe9263fdb7ca744f
guigang xie <xie.guigang@gcmodeller.org>
2022-08-16 04:24:22 +0800

    go

---
+++ src/workbench/R#/annotationKit/OBO_DAG.vb
@@ -0,0 +1,13 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Data.GeneOntology.OBO
+
+<Package("OBO")>
+Module OBO_DAG
+
+    <ExportAPI("read.obo")>
+    Public Function readOboDAG(path As String) As GO_OBO
+        Return GO_OBO.LoadDocument(path)
+    End Function
+End Module
