abc79ba3f34722c253dd260c6df9a00be779f6c1
xieguigang <xie.guigang@gcmodeller.org>
2022-09-21 01:04:07 +0800

    Update obo_test.R

--- src/workbench/R#/demo/go/obo_test.R
+++ src/workbench/R#/demo/go/obo_test.R
@@ -1,8 +1,8 @@
 imports "OBO" from "annotationKit";
 
-obo = OBO::read.obo("K:\Downloads\chebi.obo");
+obo = OBO::read.obo("E:\mzkit\src\mzkit\rstudio\data\chebi.obo");
 obo 
 |> filter.is_obsolete() 
 |> filter_properties(["monoisotopicmass","inchikey","inchi","smiles"])
-|> write.obo( "K:\Downloads\chebi_lite.obo",  excludes = ["synonym", "relationship","alt_id","xref","subset"])
+|> write.obo( "E:\mzkit\src\mzkit\rstudio\data\chebi_lite.obo",  excludes = ["synonym", "relationship","alt_id","xref","subset"])
 ;
