988703f3fef43488a30997e686f2971f197b6a44
guigang xie <xie.guigang@gcmodeller.org>
2022-09-30 16:57:27 +0800

    PTF annotation module

--- src/workbench/R#/seqtoolkit/Annotations/uniprot.vb
+++ src/workbench/R#/seqtoolkit/Annotations/uniprot.vb
@@ -275,43 +275,6 @@ Module uniprot
         End If
     End Function
 
-    <ExportAPI("cache.ptf")>
-    Public Function writePtfFile(<RRawVectorArgument>
-                                 uniprot As Object,
-                                 file As Object,
-                                 Optional cacheTaxonomy As Boolean = False,
-                                 Optional env As Environment = Nothing) As Object
-
-        Dim source = getUniprotData(uniprot, env)
-
-        If source Like GetType(Message) Then
-            Return source.TryCast(Of Message)
-        Else
-            Dim stream As StreamWriter
-
-            If file Is Nothing Then
-                Return Internal.debug.stop({"file output can not be nothing!"}, env)
-            ElseIf TypeOf file Is String Then
-                stream = DirectCast(file, String).OpenWriter
-            ElseIf TypeOf file Is Stream Then
-                stream = New StreamWriter(DirectCast(file, Stream)) With {.NewLine = True}
-            ElseIf TypeOf file Is StreamWriter Then
-                stream = DirectCast(file, StreamWriter)
-            Else
-                Return Internal.debug.stop(Message.InCompatibleType(GetType(Stream), file.GetType, env,, NameOf(file)), env)
-            End If
-
-            Call source.TryCast(Of IEnumerable(Of entry)).WritePtfCache(stream, cacheTaxonomy)
-            Call stream.Flush()
-
-            If TypeOf file Is String Then
-                Call stream.Close()
-            End If
-
-            Return Nothing
-        End If
-    End Function
-
     ''' <summary>
     ''' id unify mapping
     ''' </summary>
