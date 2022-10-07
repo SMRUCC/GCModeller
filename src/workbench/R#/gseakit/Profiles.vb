94b647008b327b6aa5d79a9792a397daa332a0f8
xieguigang <xie.guigang@gcmodeller.org>
2022-10-07 23:14:35 +0800

    try to fix of the type cast error

--- src/workbench/R#/gseakit/Profiles.vb
+++ src/workbench/R#/gseakit/Profiles.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::48a3d7e956aa95a38690f989801ad933, R#\gseakit\Profiles.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 38
-    '    Code Lines: 24
-    ' Comment Lines: 9
-    '   Blank Lines: 5
-    '     File Size: 1.65 KB
+' Summaries:
 
 
-    ' Module profiles
-    ' 
-    '     Function: GOEnrichmentProfiles, KEGGEnrichmentProfiles
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 38
+'    Code Lines: 24
+' Comment Lines: 9
+'   Blank Lines: 5
+'     File Size: 1.65 KB
+
+
+' Module profiles
+' 
+'     Function: GOEnrichmentProfiles, KEGGEnrichmentProfiles
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -55,10 +55,15 @@ Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Analysis.GO
+Imports SMRUCC.genomics.Analysis.HTS.GSEA
 Imports SMRUCC.genomics.Analysis.Microarray.KOBAS
 Imports SMRUCC.genomics.Assembly.KEGG
 Imports SMRUCC.genomics.ComponentModel.Annotation
 Imports SMRUCC.genomics.Data.GeneOntology.OBO
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Interop
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 <Package("profiles")>
 Module profiles
@@ -89,13 +94,32 @@ Module profiles
     End Function
 
     <ExportAPI("no_catagory_profile")>
-    Public Function NoCatagoryProfile(enrichments As EnrichmentTerm(), name As String, Optional top% = 30) As CatalogProfiles
-        Dim profiles As NamedValue(Of Double)() = enrichments _
-            .OrderBy(Function(a) a.Pvalue) _
-            .Take(top) _
-            .Select(Function(a)
-                        Return New NamedValue(Of Double)(a.ID, -Math.Log10(a.Pvalue), a.Term)
-                    End Function)
+    <RApiReturn(GetType(CatalogProfiles))>
+    Public Function NoCatagoryProfile(enrichments As Array, name As String,
+                                      Optional top% = 30,
+                                      Optional env As Environment = Nothing) As Object
+
+        Dim profiles As NamedValue(Of Double)()
+
+        enrichments = REnv.TryCastGenericArray(enrichments, env)
+
+        If TypeOf enrichments Is EnrichmentTerm() Then
+            profiles = DirectCast(enrichments, EnrichmentTerm()) _
+                .OrderBy(Function(a) a.Pvalue) _
+                .Take(top) _
+                .Select(Function(a)
+                            Return New NamedValue(Of Double)(a.ID, -Math.Log10(a.Pvalue), a.Term)
+                        End Function)
+        ElseIf TypeOf enrichments Is EnrichmentResult() Then
+            profiles = DirectCast(enrichments, EnrichmentResult()) _
+                .OrderBy(Function(a) a.pvalue) _
+                .Take(top) _
+                .Select(Function(a)
+                            Return New NamedValue(Of Double)(a.term, -Math.Log10(a.pvalue), a.name)
+                        End Function)
+        Else
+            Return Message.InCompatibleType(GetType(EnrichmentTerm), enrichments.GetType, env)
+        End If
 
         Return New CatalogProfiles With {
             .catalogs = New Dictionary(Of String, CatalogProfile) From {
