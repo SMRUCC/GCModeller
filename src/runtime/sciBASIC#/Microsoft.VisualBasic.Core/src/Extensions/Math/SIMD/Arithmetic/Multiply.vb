a886e2f809eb8fa73f3d3fbbbb24b1629e7dd525
guigang xie <xie.guigang@gcmodeller.org>
2022-10-17 10:01:07 +0800

    add operator base supports

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/Extensions/Math/SIMD/Arithmetic/Multiply.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/Extensions/Math/SIMD/Arithmetic/Multiply.vb
@@ -9,5 +9,24 @@ Namespace Math.SIMD
 
     Public Class Multiply
 
+        Public Shared Function f64_scalar_op_multiply_f64(v1 As Double, v2 As Double()) As Double()
+            Dim result As Double() = New Double(v2.Length - 1) {}
+
+            For i As Integer = 0 To v2.Length - 1
+                result(i) = v1 * v2(i)
+            Next
+
+            Return result
+        End Function
+
+        Public Shared Function f64_op_multiply_f64(v1 As Double(), v2 As Double()) As Double()
+            Dim result As Double() = New Double(v1.Length - 1) {}
+
+            For i As Integer = 0 To v1.Length - 1
+                result(i) = v1(i) * v2(i)
+            Next
+
+            Return result
+        End Function
     End Class
 End Namespace
