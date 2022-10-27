dafa2a5d6469d4860fbfc1c05860327b5a890a9c
xieguigang <xie.guigang@gcmodeller.org>
2022-10-27 21:32:27 +0800

    fix of namespace reference error

--- src/repository/Bio.Repository/KEGG/ReactionRepository/RepositoryExtensions.vb
+++ src/repository/Bio.Repository/KEGG/ReactionRepository/RepositoryExtensions.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::04adec1c15b1ca149f1bf3dcfef9b637, Bio.Repository\KEGG\ReactionRepository\RepositoryExtensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module RepositoryExtensions
-    ' 
-    '         Function: FetchCompoundRepository, FetchPathwayRepository, FetchReactionRepository
-    '         Class TermKeys
-    ' 
-    '             Constructor: (+1 Overloads) Sub New
-    ' 
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module RepositoryExtensions
+' 
+'         Function: FetchCompoundRepository, FetchPathwayRepository, FetchReactionRepository
+'         Class TermKeys
+' 
+'             Constructor: (+1 Overloads) Sub New
+' 
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-#If netcore5 Then
+#If NETCOREAPP Then
 Imports System.Data
 #End If
 Imports System.Runtime.CompilerServices
