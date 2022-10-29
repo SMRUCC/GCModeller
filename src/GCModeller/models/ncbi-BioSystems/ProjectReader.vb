bd8c5087957cb8aa8e2214b9fe8896447f82fa9d
xieguigang <xie.guigang@gcmodeller.org>
2022-10-29 11:36:54 +0800

    project reader for populate protein sequence?

--- src/GCModeller/models/ncbi-BioSystems/ProjectReader.vb
+++ src/GCModeller/models/ncbi-BioSystems/ProjectReader.vb
@@ -1,3 +1,14 @@
-﻿Public Class ProjectReader
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
+Imports SMRUCC.genomics.Data
 
+Public Class ProjectReader
+
+    Dim buffer As StreamPack
+    Dim proteins As PtfReader
+
+    Sub New(stream As Stream)
+        buffer = New StreamPack(stream)
+        proteins = New PtfReader(buffer)
+    End Sub
 End Class
