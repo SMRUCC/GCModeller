6dc698a103e2b680c7a630101d904c307ca17e0d
xieguigang <xie.guigang@gcmodeller.org>
2022-10-30 13:02:20 +0800

    fix for the compiler error

--- src/interops/localblast/LocalBLAST/LocalBLAST/LocalBLAST/InteropService/InteropService.vb
+++ src/interops/localblast/LocalBLAST/LocalBLAST/LocalBLAST/InteropService/InteropService.vb
@@ -1,71 +1,68 @@
 ﻿#Region "Microsoft.VisualBasic::da2bd9811b2777e90b096dc1f4a284dd, localblast\LocalBLAST\LocalBLAST\LocalBLAST\InteropService\InteropService.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class LocalBlastProgramGroup
-    ' 
-    '         Properties: BlastBin, NumThreads, Version
-    ' 
-    '         Function: ToString
-    ' 
-    '         Sub: (+2 Overloads) Dispose
-    ' 
-    '     Class InteropService
-    ' 
-    ' 
-    '         Enum AlignmentViewOptions
-    ' 
-    '             ASNBinary, ASNText, FlatQueryAnchored, FlatQueryAnchoredNoIdentities, FlatQueryAnchoredWithIdentities
-    '             Pairwise, QueryAnchored, QueryAnchoredNoIdentities, QueryAnchoredShowingIdentities, Tabular
-    '             TabularWithCommentLines, XML
-    ' 
-    ' 
-    ' 
-    '         Delegate Function
-    ' 
-    '             Properties: LastBLASTOutputFilePath
-    ' 
-    '             Constructor: (+1 Overloads) Sub New
-    '             Function: TryInvoke
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class LocalBlastProgramGroup
+' 
+'         Properties: BlastBin, NumThreads, Version
+' 
+'         Function: ToString
+' 
+'         Sub: (+2 Overloads) Dispose
+' 
+'     Class InteropService
+' 
+' 
+'         Enum AlignmentViewOptions
+' 
+'             ASNBinary, ASNText, FlatQueryAnchored, FlatQueryAnchoredNoIdentities, FlatQueryAnchoredWithIdentities
+'             Pairwise, QueryAnchored, QueryAnchoredNoIdentities, QueryAnchoredShowingIdentities, Tabular
+'             TabularWithCommentLines, XML
+' 
+' 
+' 
+'         Delegate Function
+' 
+'             Properties: LastBLASTOutputFilePath
+' 
+'             Constructor: (+1 Overloads) Sub New
+'             Function: TryInvoke
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-#If netcore5 = 1 Then
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
-#End If
-
 Imports Microsoft.VisualBasic.CommandLine
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput
 Imports CLI = Microsoft.VisualBasic.CommandLine.InteropService.InteropService
