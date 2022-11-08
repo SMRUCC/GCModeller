ae1792ab3148194d5cbc590dc54ea8537706850c
xieguigang <xie.guigang@gcmodeller.org>
2022-11-08 22:30:21 +0800

    enzyme model?

---
+++ src/GCModeller/models/ncbi-BioSystems/Enzyme.vb
@@ -0,0 +1,8 @@
+﻿Imports SMRUCC.genomics.Data
+
+Public Class Enzyme
+
+    Public Property EC_Number As String
+    Public Property protein_id As String
+    Public Property reactions As New List(Of Rhea.Reaction)
+End Class
