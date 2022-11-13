b8c1d8d25d2b79652d2e2649fd0c20d3613a87e2
xieguigang <xie.guigang@gcmodeller.org>
2022-11-13 11:06:53 +0800

    set default kineticis

--- src/GCModeller/data/SABIO-RK/SBML/MathMLParser.vb
+++ src/GCModeller/data/SABIO-RK/SBML/MathMLParser.vb
@@ -1,57 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::b254b435c382c44545396f33beddb7e1, GCModeller\data\SABIO-RK\SBML\MathMLParser.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 43
-    '    Code Lines: 35
-    ' Comment Lines: 0
-    '   Blank Lines: 8
-    '     File Size: 1.65 KB
+' Summaries:
 
 
-    '     Module MathMLParser
-    ' 
-    '         Function: ParseMathML
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 43
+'    Code Lines: 35
+' Comment Lines: 0
+'   Blank Lines: 8
+'     File Size: 1.65 KB
+
+
+'     Module MathMLParser
+' 
+'         Function: ParseMathML
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.MIME.application.xml
+Imports Microsoft.VisualBasic.MIME.application.xml.MathML
 Imports Microsoft.VisualBasic.Text
 Imports r = System.Text.RegularExpressions.Regex
 
@@ -91,5 +92,34 @@ Namespace SBML
                 }
             Next
         End Function
+
+        ' Km, kcat, E
+        ' V = Kcat[E]t[S] / ( KM + [S])
+        Public Function DefaultKineticis() As LambdaExpression
+            Dim exp As MathExpression = New BinaryExpression With {
+                .[operator] = "/",
+                .applyleft = New BinaryExpression With {
+                    .[operator] = "*",
+                    .applyleft = New BinaryExpression With {
+                        .[operator] = "*",
+                        .applyleft = New SymbolExpression("kcat"),
+                        .applyright = New SymbolExpression("E")
+                    },
+                    .applyright = New SymbolExpression("S")
+                },
+                .applyright = New BinaryExpression With {
+                    .[operator] = "+",
+                    .applyleft = New SymbolExpression("Km"),
+                    .applyright = New SymbolExpression("S")
+                }
+            }
+
+            Return New LambdaExpression With {
+                .parameters = {"Km", "kcat", "E", "S"},
+                .lambda = exp
+            }
+        End Function
+
+
     End Module
 End Namespace
