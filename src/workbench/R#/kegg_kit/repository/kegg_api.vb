ddc1e57133672e89a7e977fe9e7bf7f963fa630a
guigang xie <xie.guigang@gcmodeller.org>
2022-11-17 09:59:36 +0800

    get kegg object by id

--- src/workbench/R#/kegg_kit/repository/kegg_api.vb
+++ src/workbench/R#/kegg_kit/repository/kegg_api.vb
@@ -10,6 +10,11 @@ Public Module kegg_api
 
     <ExportAPI("listing")>
     Public Function listing(database As String, Optional [option] As String = Nothing) As Object
-        Return New KEGGApi().List(database, [option])
+        Return KEGGApi.List(database, [option])
+    End Function
+
+    <ExportAPI("get")>
+    Public Function [get](id As String) As String
+        Return KEGGApi.GetObject(id)
     End Function
 End Module
