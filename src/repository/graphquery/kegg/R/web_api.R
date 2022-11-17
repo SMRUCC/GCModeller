7b5f04c90215ed92378af86d0c68eb47fd6ccadd
guigang xie <xie.guigang@gcmodeller.org>
2022-11-17 10:28:02 +0800

    implements the cache proxy

--- src/repository/graphquery/kegg/R/web_api.R
+++ src/repository/graphquery/kegg/R/web_api.R
@@ -1,7 +1,7 @@
 imports "kegg_api" from "kegg_kit";
 
-const list_pathway = function(org = ["ko", "map", "hsa"]) {
-    const list_data = kegg_api::listing("pathway", org[1] || "map");
+const list_pathway = function(org = ["ko", "map", "hsa"], cache = NULL) {
+    const list_data = kegg_api::listing("pathway", org[1] || "map", cache = cache);
     const idlist as string = names(list_data) 
     |> sapply(x -> strsplit(x, ":", fixed = TRUE)[2])
     ;
--- src/repository/graphquery/kegg/pipeline/pathway.R
+++ src/repository/graphquery/kegg/pipeline/pathway.R
@@ -6,10 +6,11 @@ package_utils::attach("D:\\GCModeller\\src\\repository\\graphquery\\kegg");
 
 require(kegg_graphquery);
 require(HDS);
+require(GCModeller);
 
 const cache_dir = [?"--cache" || stop("No data cahce file!")] |> http::http.cache();
-const Tcode     = ?"--tcode" || "map";
-const pathways  = list_pathway(Tcode);
+const Tcode     =  ?"--tcode" || "map";
+const pathways  = list_pathway(Tcode, cache = cache_dir);
 
 for(name in names(pathways)) {
     print(`${name}: ${pathways[[name]]}`);
