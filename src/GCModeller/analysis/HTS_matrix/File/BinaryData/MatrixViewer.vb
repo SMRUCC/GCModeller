daaa9c96a72d4d7aeef6583d9f182182b352bedf
guigang xie <xie.guigang@gcmodeller.org>
2022-12-13 16:11:27 +0800

    make improvements of the matrix viewer model

--- src/GCModeller/analysis/HTS_matrix/File/BinaryData/MatrixViewer.vb
+++ src/GCModeller/analysis/HTS_matrix/File/BinaryData/MatrixViewer.vb
@@ -26,7 +26,23 @@ Public MustInherit Class MatrixViewer
     End Property
 
     Public MustOverride Function GetSampleOrdinal(sampleID As String) As Integer
+    ''' <summary>
+    ''' get gene expression across all sample data
+    ''' </summary>
+    ''' <param name="geneID">a specific gene target</param>
+    ''' <returns>
+    ''' expression data is aligned with the <see cref="SampleIDs"/>
+    ''' </returns>
     Public MustOverride Function GetGeneExpression(geneID As String) As Double()
+    ''' <summary>
+    ''' get a set of gene expression across a specific sample data
+    ''' </summary>
+    ''' <param name="geneID">a set of target gene</param>
+    ''' <param name="sampleOrdinal">the order index of the specific sample data</param>
+    ''' <returns>
+    ''' expression data is aligned with the <paramref name="geneID"/> set.
+    ''' </returns>
+    Public MustOverride Function GetGeneExpression(geneID As String(), sampleOrdinal As Integer) As Double()
 
     Public Overrides Function ToString() As String
         Dim size = Me.Size
