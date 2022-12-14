6677021516856383405c0951f6f0bb91de2a41b8
guigang xie <xie.guigang@gcmodeller.org>
2022-12-14 11:25:47 +0800

    merge multiple batches data directly

--- src/workbench/R#/phenotype_kit/geneExpression.vb
+++ src/workbench/R#/phenotype_kit/geneExpression.vb
@@ -1145,46 +1145,7 @@ Module geneExpression
         ElseIf samples.Length = 1 Then
             Return samples(Scan0)
         Else
-            Return mergeMultiple(samples)
+            Return MergeMultipleHTSMatrix(samples)
         End If
     End Function
-
-    Private Function mergeMultiple(multiple As Matrix()) As Matrix
-        Dim matrix As Matrix = multiple(Scan0)
-        Dim geneIndex = matrix.expression.ToDictionary(Function(g) g.geneID)
-        Dim sampleList As New List(Of String)(matrix.sampleID)
-
-        For Each append As Matrix In multiple.Skip(1)
-            For Each gene As DataFrameRow In append.expression
-                Dim v As Double() = New Double(sampleList.Count + append.sampleID.Length - 1) {}
-                Dim a As Double()
-                Dim b As Double() = gene.experiments
-
-                If geneIndex.ContainsKey(gene.geneID) Then
-                    a = geneIndex(gene.geneID).experiments
-                Else
-                    a = New Double(sampleList.Count - 1) {}
-                End If
-
-                Call Array.ConstrainedCopy(a, Scan0, v, Scan0, a.Length)
-                Call Array.ConstrainedCopy(b, Scan0, v, a.Length, b.Length)
-
-                geneIndex(gene.geneID) = New DataFrameRow With {
-                    .experiments = v,
-                    .geneID = gene.geneID
-                }
-            Next
-
-            Call sampleList.AddRange(append.sampleID)
-        Next
-
-        Return New Matrix With {
-            .expression = geneIndex.Values.ToArray,
-            .sampleID = sampleList.ToArray,
-            .tag = multiple _
-                .Select(Function(m) m.tag) _
-                .JoinBy("+")
-        }
-    End Function
-
 End Module
