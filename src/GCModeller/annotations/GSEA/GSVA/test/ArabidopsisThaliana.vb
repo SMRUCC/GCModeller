ff87c09d15bacf718ce7e13124d990661fe11278
xieguigang <xie.guigang@gcmodeller.org>
2023-01-29 00:06:19 +0800

    how to load kegg background model?

--- src/GCModeller/annotations/GSEA/GSVA/test/ArabidopsisThaliana.vb
+++ src/GCModeller/annotations/GSEA/GSVA/test/ArabidopsisThaliana.vb
@@ -1,6 +1,26 @@
-﻿Public Module ArabidopsisThalianaTest
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
+Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+Imports SMRUCC.genomics.Analysis.HTS.GSEA
+
+Public Module ArabidopsisThalianaTest
+
+    Dim base As String = "E:\GCModeller\src\workbench\R#\demo\HTS\GSVA"
 
     Sub Main()
-
+        Dim expr As Matrix = Matrix.LoadData($"{base}/ath_norm.csv")
+        Dim kegg As New Background With {
+            .clusters = LoadKEGG.ToArray
+        }
     End Sub
+
+    Private Iterator Function LoadKEGG() As IEnumerable(Of Cluster)
+        Using file = $"{base}/ath.db".Open(FileMode.Open, doClear:=False, [readOnly]:=True)
+            Using pack As New StreamPack(file, [readonly]:=True)
+
+
+
+            End Using
+        End Using
+    End Function
 End Module
