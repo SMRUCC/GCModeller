bf89909071eef56a9be563c77ef9dc2570c39259
xieguigang <xie.guigang@gcmodeller.org>
2023-02-18 17:17:03 +0800

    re-run gsva test

--- src/workbench/R#/demo/HTS/GSVA/norm.R
+++ src/workbench/R#/demo/HTS/GSVA/norm.R
@@ -1,7 +1,18 @@
+require(GCModeller);
+
+#' GCModeller DEG experiment analysis designer toolkit
+imports "sampleInfo" from "phenotype_kit";
+#' the gene expression matrix data toolkit
+imports "geneExpression" from "phenotype_kit";
+
 setwd(@dir);
 
 data = read.csv("../all_counts.csv", row.names = 1, check.names = FALSE);
-
+sampleinfo = read.sampleinfo(file = "../sampleinfo.csv",
+    tsv = FALSE,
+    exclude.groups = NULL,
+    id.makenames = FALSE);
+	
 for(sample in colnames(data)) {
 	v = data[, sample];
 	v = v / sum(v) * [10 ^8];
@@ -9,5 +20,10 @@ for(sample in colnames(data)) {
 	data[,sample] = v;
 }
 
-write.csv(data, file = "./ath_norm.csv", row.names = TRUE);
 writeLines(rownames(data), con = "./geneIds.txt");
+
+data = geneExpression::average(load.expr(data), sampleinfo);
+
+write.expr_matrix(expr = data, file = "./ath_norm.csv",
+    id = "geneID",
+    binary = FALSE);
