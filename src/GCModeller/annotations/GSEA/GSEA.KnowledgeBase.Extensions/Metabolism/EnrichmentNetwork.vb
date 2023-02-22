c8c54830b577e7da8ceee3d37189fb25647d5374
guigang xie <xie.guigang@gcmodeller.org>
2023-02-22 11:31:18 +0800

    calcRbc

--- src/GCModeller/annotations/GSEA/GSEA.KnowledgeBase.Extensions/Metabolism/EnrichmentNetwork.vb
+++ src/GCModeller/annotations/GSEA/GSEA.KnowledgeBase.Extensions/Metabolism/EnrichmentNetwork.vb
@@ -229,27 +229,5 @@ Namespace Metabolism
                 .Value = g
             }
         End Function
-
-        Private Function calcRbc(a As NamedValue(Of NetworkGraph)) As NamedValue(Of rbc)
-            Dim cnodes As Node() = a.Value.vertex _
-                .Where(Function(c)
-                           Return c.data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE) = "KEGG Compound"
-                       End Function) _
-                .ToArray
-            Dim cid = cnodes.Select(Function(c) c.label).ToArray
-            Dim rbcVal As Double() = cnodes _
-                .Select(Function(c)
-                            Return c.data(NamesOf.REFLECTION_ID_MAPPING_RELATIVE_BETWEENESS_CENTRALITY)
-                        End Function) _
-                .Select(AddressOf Val) _
-                .ToArray
-            Dim rbc As New rbc With {
-                .data = rbcVal,
-                .kegg_id = cid
-            }
-
-            Return New NamedValue(Of rbc)(a.Name, rbc)
-        End Function
-
     End Module
 End Namespace
--- src/GCModeller/annotations/GSEA/GSEA.KnowledgeBase.Extensions/Metabolism/Metpa/rbc.vb
+++ src/GCModeller/annotations/GSEA/GSEA.KnowledgeBase.Extensions/Metabolism/Metpa/rbc.vb
@@ -1,4 +1,8 @@
-﻿Namespace Metabolism.Metpa
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+Namespace Metabolism.Metpa
 
     Public Class rbc
 
@@ -11,5 +15,25 @@
 
         Public Property list As Dictionary(Of String, rbc)
 
+        Public Shared Function calcRbc(a As NamedValue(Of NetworkGraph)) As NamedValue(Of rbc)
+            Dim cnodes As Node() = a.Value.vertex _
+                .Where(Function(c)
+                           Return c.data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE) = "KEGG Compound"
+                       End Function) _
+                .ToArray
+            Dim cid = cnodes.Select(Function(c) c.label).ToArray
+            Dim rbcVal As Double() = cnodes _
+                .Select(Function(c)
+                            Return c.data(NamesOf.REFLECTION_ID_MAPPING_RELATIVE_BETWEENESS_CENTRALITY)
+                        End Function) _
+                .Select(AddressOf Val) _
+                .ToArray
+            Dim rbc As New rbc With {
+                .data = rbcVal,
+                .kegg_id = cid
+            }
+
+            Return New NamedValue(Of rbc)(a.Name, rbc)
+        End Function
     End Class
 End Namespace
