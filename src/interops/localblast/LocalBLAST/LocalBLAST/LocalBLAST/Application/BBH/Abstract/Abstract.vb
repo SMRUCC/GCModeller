4177453a115b349211e8a77dc62f9c8bb286e78b
guigang xie <xie.guigang@gcmodeller.org>
2023-02-22 09:07:57 +0800

    removes un-used .net 48 project

--- src/interops/localblast/LocalBLAST/LocalBLAST/LocalBLAST/Application/BBH/Abstract/Abstract.vb
+++ src/interops/localblast/LocalBLAST/LocalBLAST/LocalBLAST/Application/BBH/Abstract/Abstract.vb
@@ -1,51 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::4674e9c3dc7a01652339fe6d3c67719f, localblast\LocalBLAST\LocalBLAST\LocalBLAST\Application\BBH\Abstract\Abstract.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class I_BlastQueryHit
-    ' 
-    '         Properties: BBH_ID, HitName, isMatched, isSelfHit, QueryName
-    ' 
-    '         Function: ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class I_BlastQueryHit
+' 
+'         Properties: BBH_ID, HitName, isMatched, isSelfHit, QueryName
+' 
+'         Function: ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-#If netcore5 = 0 Then
+#If NET48 Then
 Imports System.Web.Script.Serialization
-#Else
-Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 #End If
 
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
@@ -122,7 +120,7 @@ Namespace LocalBLAST.Application.BBH.Abstract
         Public ReadOnly Property BBH_ID As String
             Get
                 Dim asc As String() = LinqAPI.Exec(Of String) <=
- _
+                                                                _
                     From s As String
                     In {Me.QueryName, Me.HitName}
                     Select str = s.ToUpper
