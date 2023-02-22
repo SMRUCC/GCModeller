b1c96b270372669a8ec11e2ddeef69978ae4f1cf
guigang xie <xie.guigang@gcmodeller.org>
2023-02-22 14:41:33 +0800

    demo script for create metpa model from the local pathway database file

--- src/workbench/pkg/test/metpa_demo.R
+++ src/workbench/pkg/test/metpa_demo.R
@@ -1,8 +1,18 @@
 imports "package_utils" from "devkit";
 
 require(HDS);
+require(JSON);
 
 package_utils::attach(`${@dir}/../`);
 
-let pathways = HDS::openStream(file = "D:\biodeep\biodeepdb_v3\KEGG\pathway\pack\hsa.hdspack", readonly = TRUE);
+const demo_file = "D:\biodeep\biodeepdb_v3\KEGG\pathway\pack\hsa.hdspack";
+
+let buf = HDS::openStream(file = demo_file, readonly = TRUE);
+let pathways = buf 
+|> HDS::files("/pathways/")
+|> sapply(file -> HDS::getData(buf, file) |> loadXml(typeof = "kegg_pathway"))
+;
+
 let metpa = metpa_background(pathways, taxonomy_name = NULL, raw = TRUE);
+
+print(json_encode(metpa));
