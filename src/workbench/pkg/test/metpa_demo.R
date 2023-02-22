30e617e0503fdcd5c06ca42f4f75527828b5f6df
guigang xie <xie.guigang@gcmodeller.org>
2023-02-22 15:19:34 +0800

    add xml help documents for the function parameters

--- src/workbench/pkg/test/metpa_demo.R
+++ src/workbench/pkg/test/metpa_demo.R
@@ -9,7 +9,7 @@ const demo_file = "D:\biodeep\biodeepdb_v3\KEGG\pathway\pack\hsa.hdspack";
 
 let buf = HDS::openStream(file = demo_file, readonly = TRUE);
 let pathways = buf 
-|> HDS::files("/pathways/")
+|> HDS::files("/pathways/", excludes_dir = TRUE)
 |> sapply(file -> HDS::getData(buf, file) |> loadXml(typeof = "kegg_pathway"))
 ;
 
