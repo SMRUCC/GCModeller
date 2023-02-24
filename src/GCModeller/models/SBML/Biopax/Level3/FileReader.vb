a6bb23c0000df6c3cdcb4949da9526d9c7bf83dd
xieguigang <xie.guigang@gcmodeller.org>
2023-02-24 21:53:58 +0800

    source file renames

--- src/GCModeller/models/SBML/Biopax/Level3/FileReader.vb
+++ src/GCModeller/models/SBML/Biopax/Level3/FileReader.vb
@@ -1,4 +1,6 @@
 ﻿
+Imports System.Reflection
+Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.MIME.application.xml
 
@@ -7,6 +9,25 @@ Namespace Level3
     Public Class FileReader
 
         ReadOnly xml As XmlElement
+
+        Shared ReadOnly schemaGraph As New Dictionary(Of String, PropertyInfo)
+
+        Shared Sub New()
+            Dim type As Type = GetType(File)
+            Dim properties As PropertyInfo() = type.GetProperties _
+                .Where(Function(p) p.CanWrite) _
+                .ToArray
+
+            For Each prop As PropertyInfo In properties
+                Dim tag As XmlElementAttribute = prop.GetCustomAttribute(Of XmlElementAttribute)
+
+                If tag Is Nothing OrElse tag.ElementName.StringEmpty Then
+                    Call schemaGraph.Add(prop.Name, prop)
+                Else
+                    Call schemaGraph.Add(tag.ElementName, prop)
+                End If
+            Next
+        End Sub
 
         Sub New(file As String)
             xml = XmlElement.ParseXmlText(file.SolveStream)
@@ -22,7 +43,21 @@ Namespace Level3
                               End Function)
             Dim file As New File
 
+            For Each field In schemaGraph
+                Dim array As XmlElement() = elements.TryGetValue(field.Key)
+
+                If array.IsNullOrEmpty Then
+                    Continue For
+                End If
+
+                If Not field.Value.PropertyType.IsArray Then
+
+                End If
+            Next
+
             Return file
         End Function
+
+
     End Class
 End Namespace
