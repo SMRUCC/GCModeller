d6609f964860cad73a74e53bca6a4c791cc2922c
guigang xie <xie.guigang@gcmodeller.org>
2023-02-24 18:02:11 +0800

    get element group

--- src/GCModeller/models/SBML/Biopax/Level3/FileReader.vb
+++ src/GCModeller/models/SBML/Biopax/Level3/FileReader.vb
@@ -1,4 +1,5 @@
 ﻿
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.MIME.application.xml
 
 Namespace Level3
@@ -12,6 +13,7 @@ Namespace Level3
         End Sub
 
         Public Function GetObject() As File
+            Dim elements = xml.elements.SafeQuery.GroupBy(Function(tag) tag.name).ToArray
 
         End Function
     End Class
