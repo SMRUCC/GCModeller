3220143d12054293d124f43e690e4f3c21eddcbc
xieguigang <xie.guigang@gcmodeller.org>
2023-02-25 15:10:05 +0800

    male improvements of the xml loader code

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/ComponentModel/DataSource/SchemaMaps/ObjectSchema.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/ComponentModel/DataSource/SchemaMaps/ObjectSchema.vb
@@ -1,63 +1,69 @@
 ﻿#Region "Microsoft.VisualBasic::e60b8ad39a90c810c090f46f60e62eea, sciBASIC#\mime\application%json\Serializer\ObjectSchema.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 126
-    '    Code Lines: 92
-    ' Comment Lines: 18
-    '   Blank Lines: 16
-    '     File Size: 4.48 KB
+' Summaries:
 
 
-    ' Class ObjectSchema
-    ' 
-    '     Constructor: (+2 Overloads) Sub New
-    '     Function: CreateSchema, FindInterfaceImpementations, GetSchema, Score, ToString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 126
+'    Code Lines: 92
+' Comment Lines: 18
+'   Blank Lines: 16
+'     File Size: 4.48 KB
+
+
+' Class ObjectSchema
+' 
+'     Constructor: (+2 Overloads) Sub New
+'     Function: CreateSchema, FindInterfaceImpementations, GetSchema, Score, ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Reflection
 Imports System.Runtime.Serialization
+Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Emit.Delegates
 Imports Microsoft.VisualBasic.Linq
 
 Namespace ComponentModel.DataSourceModel.SchemaMaps
+
+    Public Enum Serializations
+        JSON
+        XML
+    End Enum
 
     ''' <summary>
     ''' object schema provider for json/xml
@@ -75,6 +81,7 @@ Namespace ComponentModel.DataSourceModel.SchemaMaps
         Public ReadOnly raw As Type
 
         Public ReadOnly knownTypes As Type()
+        Public ReadOnly documentType As Serializations
 
         Private Sub New()
         End Sub
@@ -92,7 +99,8 @@ Namespace ComponentModel.DataSourceModel.SchemaMaps
                         keyType As Type,
                         valueType As Type,
                         raw As Type,
-                        knownTypes As Type())
+                        knownTypes As Type(),
+                        docTyp As Serializations)
 
             Me.addMethod = addMethod
             Me.isTable = isTable
@@ -101,22 +109,88 @@ Namespace ComponentModel.DataSourceModel.SchemaMaps
             Me.keyType = keyType
             Me.raw = raw
             Me.knownTypes = knownTypes
+            Me.documentType = docTyp
         End Sub
+
+        ''' <summary>
+        ''' just create a new and blank .net clr object
+        ''' </summary>
+        ''' <param name="parent"></param>
+        ''' <param name="docs"><see cref="Score"/></param>
+        ''' <returns></returns>
+        ''' <param name="schema">
+        ''' gets the actual object class type information from this parameters,
+        ''' if the input object type(myself/this/me/myclass) <see cref="raw"/> 
+        ''' type information is acutualy an interfacve type or abstract base 
+        ''' type or something could be variant
+        ''' </param>
+        ''' <remarks>
+        ''' this function is going to handling of create the clr object instance
+        ''' for the interface type, abstract base type or something
+        ''' </remarks>
+        Public Function Activate(parent As SoapGraph, docs As String(), Optional ByRef schema As SoapGraph = Nothing) As Object
+            Dim knownType As SoapGraph
+
+            If Not raw.IsInterface AndAlso Not raw Is GetType(Object) Then
+                Return Activator.CreateInstance(raw)
+            ElseIf raw.IsInterface Then
+                knownType = parent _
+                    .FindInterfaceImplementations(raw) _
+                    .OrderByDescending(Function(a) a.Score(docs)) _
+                    .FirstOrDefault
+
+                If knownType Is Nothing Then
+                    Throw New InvalidProgramException($"can not create object from an interface type: {raw.FullName}!")
+                End If
+            Else ' is object
+                knownType = parent.knownTypes _
+                    .Select(Function(t) SoapGraph.GetSchema(t, documentType)) _
+                    .OrderByDescending(Function(a) a.Score(docs)) _
+                    .FirstOrDefault
+
+                If knownType Is Nothing Then
+                    Throw New InvalidProgramException($"can not create object...")
+                End If
+            End If
+
+            schema = knownType
+
+            Return Activator.CreateInstance(knownType.raw)
+        End Function
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="obj">
+        ''' the parsed object member names from the document file, example as
+        ''' json field names or xml element names
+        ''' </param>
+        ''' <returns></returns>
+        Public Function Score(obj As IEnumerable(Of String)) As Integer
+            Dim hits As Integer = 0
+
+            For Each name As String In obj
+                If writers.ContainsKey(name) Then
+                    hits += 1
+                End If
+            Next
+
+            Return hits
+        End Function
 
         ''' <summary>
         ''' get (or cache a new schema graph object if not exists) a schema graph object
         ''' </summary>
         ''' <param name="type"></param>
         ''' <returns></returns>
-        Public Shared Function GetSchema(type As Type) As SoapGraph
-            Static cache As New Dictionary(Of Type, SoapGraph)
-            Return cache.ComputeIfAbsent(key:=type, lazyValue:=AddressOf CreateSchema)
+        Public Shared Function GetSchema(type As Type, Optional serializer As Serializations = Serializations.JSON) As SoapGraph
+            Dim key As String = $"<{serializer.ToString}>{type.FullName}"
+            Static cache As New Dictionary(Of String, SoapGraph)
+            Return cache.ComputeIfAbsent(key:=key, lazyValue:=Function() CreateSchema(type, serializer))
         End Function
 
-        Private Shared Function CreateSchema(schema As Type) As SoapGraph
-            Dim isTable As Boolean = schema.IsInheritsFrom(GetType(DictionaryBase)) OrElse schema.ImplementInterface(GetType(IDictionary))
-            Dim writers = schema.Schema(PropertyAccess.NotSure, PublicProperty, nonIndex:=True)
-            Dim addMethod As MethodInfo = schema.GetMethods _
+        Public Shared Function GetAddMethod(schema As Type) As MethodInfo
+            Return schema.GetMethods _
                 .Where(Function(m)
                            Dim params = m.GetParameters
 
@@ -126,6 +200,42 @@ Namespace ComponentModel.DataSourceModel.SchemaMaps
                                m.Name = "Add"
                        End Function) _
                 .FirstOrDefault
+        End Function
+
+        Public Shared Function GetJSONCLRWriters(schema As Type) As Dictionary(Of String, PropertyInfo)
+            Return schema.Schema(PropertyAccess.NotSure, PublicProperty, nonIndex:=True)
+        End Function
+
+        Public Shared Function GetXmlCLRWriters(schema As Type) As Dictionary(Of String, PropertyInfo)
+            Dim properties As PropertyInfo() = schema _
+                .GetProperties() _
+                .Where(Function(p) p.CanWrite AndAlso p.GetIndexParameters.IsNullOrEmpty) _
+                .ToArray
+            Dim name As String
+            Dim writers As New Dictionary(Of String, PropertyInfo)
+            Dim tagName As XmlElementAttribute
+
+            For Each prop As PropertyInfo In properties
+                name = Nothing
+                tagName = prop.GetCustomAttribute(Of XmlElementAttribute)
+
+                If Not tagName Is Nothing Then
+                    name = tagName.ElementName
+                End If
+                If name.StringEmpty Then
+                    name = prop.Name
+                End If
+
+                writers.Add(name, prop)
+            Next
+
+            Return writers
+        End Function
+
+        Private Shared Function CreateSchema(schema As Type, serializer As Serializations) As SoapGraph
+            Dim isTable As Boolean = schema.IsInheritsFrom(GetType(DictionaryBase)) OrElse schema.ImplementInterface(GetType(IDictionary))
+            Dim writers As Dictionary(Of String, PropertyInfo)
+            Dim addMethod As MethodInfo = GetAddMethod(schema)
             Dim keyType As Type = Nothing
             Dim valueType As Type = Nothing
 
@@ -141,7 +251,14 @@ Namespace ComponentModel.DataSourceModel.SchemaMaps
                 End With
             End If
 
-            ' for avoid a infinity loop that caused by the circlar reference
+            Select Case serializer
+                Case Serializations.JSON : writers = GetJSONCLRWriters(schema)
+                Case Serializations.XML : writers = GetXmlCLRWriters(schema)
+                Case Else
+                    Throw New NotImplementedException(serializer.ToString)
+            End Select
+
+            ' for avoid a infinity loop that caused by the circular reference
             ' we just get the types at here
             ' create object schema graph object at required
             Dim knownTypes As Type() = schema _
@@ -157,14 +274,20 @@ Namespace ComponentModel.DataSourceModel.SchemaMaps
                 isTable:=isTable,
                 valueType:=valueType,
                 keyType:=keyType,
-                knownTypes:=knownTypes
+                knownTypes:=knownTypes,
+                docTyp:=serializer
             )
         End Function
 
-        Public Iterator Function FindInterfaceImpementations(type As Type) As IEnumerable(Of SoapGraph)
+        ''' <summary>
+        ''' Find the implementations of the abstract interface define
+        ''' </summary>
+        ''' <param name="type"></param>
+        ''' <returns></returns>
+        Public Iterator Function FindInterfaceImplementations(type As Type) As IEnumerable(Of SoapGraph)
             For Each known As Type In knownTypes
                 If known.ImplementInterface(type) Then
-                    Yield SoapGraph.GetSchema(known)
+                    Yield SoapGraph.GetSchema(known, documentType)
                 End If
             Next
         End Function
--- src/runtime/sciBASIC#/mime/application%xml/XmlGeneric/GraphWriter.vb
+++ src/runtime/sciBASIC#/mime/application%xml/XmlGeneric/GraphWriter.vb
@@ -1,16 +1,68 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
+Imports Microsoft.VisualBasic.Linq
 
 Public Class GraphWriter
 
     ReadOnly graph As SoapGraph
 
     Sub New(type As Type)
-        graph = SoapGraph.GetSchema(type)
+        graph = SoapGraph.GetSchema(type, Serializations.XML)
     End Sub
 
     Public Function Load(xml As XmlElement) As Object
-
+        Return loadGraphTree(xml, graph)
     End Function
+
+    Private Shared Function loadGraphTree(xml As XmlElement, parent As SoapGraph) As Object
+        Dim members = xml.elements _
+           .SafeQuery _
+           .GroupBy(Function(xi) xi.name) _
+           .ToDictionary(Function(xi) xi.Key,
+                         Function(xi)
+                             Return xi.ToArray
+                         End Function)
+        Dim obj As Object = parent.Activate(parent:=parent, docs:=members.Keys.ToArray, schema:=parent)
+
+        For Each objVal In members
+            If parent.writers.ContainsKey(objVal.Key) Then
+                Call WriteValue(
+                    objKey:=objVal.Key,
+                    obj:=obj,
+                    docs:=objVal.Value,
+                    define:=parent.writers(objVal.Key)
+                )
+            Else
+                Call $"missing {objVal.Key} from schema {parent.ToString}".Warning
+            End If
+        Next
+
+        Return obj
+    End Function
+
+    Private Shared Sub WriteValue(objKey As String, obj As Object, docs As XmlElement(), define As PropertyInfo)
+        Dim value As Object
+
+        If docs.Length > 1 AndAlso Not define.PropertyType.IsArray Then
+            ' warning
+            Call $"{objKey}(array) -> {define.Name}(scalar) type mis-matched!".Warning
+
+            value = loadGraphTree(docs(Scan0), SoapGraph.GetSchema(define.PropertyType, Serializations.XML))
+        Else
+            Dim element As Type = define.PropertyType.GetElementType
+            Dim array As Array = Array.CreateInstance(element, docs.Length)
+            Dim elementGraph = SoapGraph.GetSchema(element, Serializations.XML)
+
+            For i As Integer = 0 To array.Length - 1
+                value = loadGraphTree(docs(i), elementGraph)
+                array.SetValue(value, i)
+            Next
+
+            value = array
+        End If
+
+        Call define.SetValue(obj, value)
+    End Sub
 
     Public Shared Function LoadXml(Of T)(xml As String) As T
         Dim doc As XmlElement = XmlElement.ParseXmlText(xml.SolveStream)
