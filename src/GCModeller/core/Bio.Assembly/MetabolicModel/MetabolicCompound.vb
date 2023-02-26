145dfb1ce5fd490585d45ba6e38ca183e8556765
xieguigang <xie.guigang@gcmodeller.org>
2023-02-26 22:35:00 +0800

    make improvements of the metabolic model

--- src/GCModeller/core/Bio.Assembly/MetabolicModel/MetabolicCompound.vb
+++ src/GCModeller/core/Bio.Assembly/MetabolicModel/MetabolicCompound.vb
@@ -1,11 +1,21 @@
 ﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+Imports SMRUCC.genomics.ComponentModel
+Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
 
 Namespace MetabolicModel
 
-    Public Class MetabolicCompound : Implements INamedValue
+    Public Class MetabolicCompound : Implements INamedValue, IMolecule
 
-        Public Property ID As String Implements IKeyedEntity(Of String).Key
+        Public Property id As String Implements IKeyedEntity(Of String).Key, IMolecule.EntryId
+        Public Property name As String Implements IMolecule.Name
+        Public Property formula As String Implements IMolecule.Formula
+        Public Property moleculeWeight As Double Implements IMolecule.Mass
+        Public Property xref As DBLink()
+
+        Public Overrides Function ToString() As String
+            Return name
+        End Function
 
     End Class
 End Namespace
--- src/GCModeller/core/Bio.Assembly/MetabolicModel/MetabolicPathway.vb
+++ src/GCModeller/core/Bio.Assembly/MetabolicModel/MetabolicPathway.vb
@@ -6,5 +6,9 @@ Namespace MetabolicModel
     Public Class MetabolicPathway : Implements INamedValue
 
         Public Property ID As String Implements IKeyedEntity(Of String).Key
+        Public Property name As String
+        Public Property metabolites As MetabolicCompound()
+        Public Property metabolicNetwork As MetabolicReaction()
+
     End Class
 End Namespace
