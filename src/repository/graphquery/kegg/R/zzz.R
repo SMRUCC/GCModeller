b4dfc8b1e1416c83dc620a7d8e47ce069fb5d06b
guigang xie <xie.guigang@gcmodeller.org>
2023-03-02 14:28:59 +0800

    renames R project

--- src/repository/graphquery/kegg/R/zzz.R
+++ src/repository/graphquery/kegg/R/zzz.R
@@ -1,10 +1,6 @@
 require(JSON);
 require(GCModeller);
 
-let .onLoad = function() {
-  print("Graphquery Toolkit for KEGG database.");
-  cat("\n");
-  print("get help about graphquery:");
-  print("https://github.com/xieguigang/sciBASIC/tree/master/Data/GraphQuery");
-  cat("\n\n");
+const .onLoad = function() {
+   print("KEGG API is a REST-style Application Programming Interface to the KEGG database resource.");
 }
