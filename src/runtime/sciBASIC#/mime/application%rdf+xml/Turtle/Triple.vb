7f205361953eb6d7dedc9230a461fcd32ae1ef30
xieguigang <gg.xie@bionovogene.com>
2023-03-21 17:03:49 +0800

    parse the ttl files

---
+++ src/runtime/sciBASIC#/mime/application%rdf+xml/Turtle/Triple.vb
@@ -0,0 +1,23 @@
+﻿Namespace Turtle
+
+    Public Class Triple
+
+        Public Property subject As String
+        Public Property relations As Relation()
+
+        Public Overrides Function ToString() As String
+            Return $"<{subject}> {relations.JoinBy(" ; ")}."
+        End Function
+
+    End Class
+
+    Public Class Relation
+
+        Public Property predicate As String
+        Public Property objs As String()
+
+        Public Overrides Function ToString() As String
+            Return $"<{predicate}> <{objs.JoinBy(" , ")}>"
+        End Function
+    End Class
+End Namespace
