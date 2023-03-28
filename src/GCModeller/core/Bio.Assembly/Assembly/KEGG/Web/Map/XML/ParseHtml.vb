f672500c9de5e6870663e7caf9120620dc629272
xieguigang <gg.xie@bionovogene.com>
2023-03-28 09:46:16 +0800

    make bugs fixed for the pathway map downloader

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/XML/ParseHtml.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/XML/ParseHtml.vb
@@ -1,52 +1,52 @@
 ﻿#Region "Microsoft.VisualBasic::399a350f1a606e540781906f38670a31, GCModeller\core\Bio.Assembly\Assembly\KEGG\Web\Map\XML\ParseHtml.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 79
-    '    Code Lines: 56
-    ' Comment Lines: 11
-    '   Blank Lines: 12
-    '     File Size: 3.01 KB
+' Summaries:
 
 
-    '     Module ParseHtmlExtensions
-    ' 
-    '         Function: GetEntryInfo, ParseHTML
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 79
+'    Code Lines: 56
+' Comment Lines: 11
+'   Blank Lines: 12
+'     File Size: 3.01 KB
+
+
+'     Module ParseHtmlExtensions
+' 
+'         Function: GetEntryInfo, ParseHTML
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -64,7 +64,7 @@ Namespace Assembly.KEGG.WebServices
 
     Module ParseHtmlExtensions
 
-        Const data$ = "<map name=""mapdata"">.+?</map>"
+        Const data$ = "<map .*name[=]""mapdata"".*>.+?</map>"
 
         '<html>
         '<!---
@@ -84,7 +84,7 @@ Namespace Assembly.KEGG.WebServices
             }
         End Function
 
-        Const mapImageURL$ = "<img src="".+?"" name=""pathwayimage"" usemap=""#mapdata"".+?/>"
+        Const mapImageURL$ = "<img src="".+?"" .*?id=""pathwayimage"".* usemap=""#mapdata"".+?/>"
 
         ''' <summary>
         ''' 
@@ -102,6 +102,7 @@ Namespace Assembly.KEGG.WebServices
                 .Take(areas.Length - 1) _
                 .Select(AddressOf Area.Parse) _
                 .ToArray
+            Dim desc As String = r.Match(html, "<div .*id[=]""description"".+?</div>", RegexICSng).Value.GetValue
 
             With "http://www.genome.jp/" & img.src
                 Call .DownloadFile(tmp)
@@ -124,7 +125,8 @@ Namespace Assembly.KEGG.WebServices
                 .shapes = shapes,
                 .id = info.Name,
                 .Name = info.Value,
-                .URL = url
+                .URL = url,
+                .description = desc
             }
         End Function
     End Module
