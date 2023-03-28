c14977c0f6cf1b7d07a6c673106563fda9ec4ce2
xieguigang <gg.xie@bionovogene.com>
2023-03-28 10:28:11 +0800

    Update update_kegg_maps.R

--- src/workbench/pkg/pipeline/update_kegg_maps.R
+++ src/workbench/pkg/pipeline/update_kegg_maps.R
@@ -3,4 +3,6 @@ require(GCModeller);
 #' Toolkit for process the kegg brite text file
 imports "brite" from "kegg_kit";
 
-kegg_maps = 
+kegg_maps = as.data.frame(brite::parse("br08901"));
+
+print(kegg_maps, max.print = 13);
