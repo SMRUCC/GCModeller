eeb93b20a8430f430c9da0b35ab91d40a009875b
xieguigang <xie.guigang@gcmodeller.org>
2023-03-29 09:23:49 +0800

    fix for the file path input

--- src/workbench/pkg/python/KEGGrender.py
+++ src/workbench/pkg/python/KEGGrender.py
@@ -23,6 +23,9 @@ def render(enrich_df,
         1. KEGG: the kegg pathway map id
         2. pathway_links: the kegg pathway map url for do kegg map image rendering
 
+        or else this parameter value could be a file path to the target kegg enrichment 
+        result table file.
+
     image: the gcmodeller docker image id
     outputdir: the directory path for output the kegg pathway map rendering result
     pathway_links: the data field name for get the kegg pathway map link url, default is 'pathway_links'
@@ -31,6 +34,12 @@ def render(enrich_df,
 
     enrich_file = "df_{}".format(random.randint(1000000, 10000000))
     enrich_file = "/tmp/{}.csv".format(enrich_file)
+
+    if type(enrich_df) == str:
+        enrich_file = enrich_df
+    else:
+        enrich_df.to_csv(enrich_file)
+
     args = {
         "map_id": map_id,
         "pathway_links": pathway_links,
@@ -40,7 +49,6 @@ def render(enrich_df,
     }
 
     # setup the r_lambda environment variables
-    enrich_df.to_csv(enrich_file)
     setup_renvironment(args, outputdir)
 
     RSCRIPT_HOST   = "/usr/local/bin/Rscript.dll"    
