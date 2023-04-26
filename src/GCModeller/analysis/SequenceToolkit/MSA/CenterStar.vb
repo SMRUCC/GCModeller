bed142183e0d40b5bcb6dcdf453eede2f74dc560
guigang xie <xie.guigang@gcmodeller.org>
2023-04-26 15:34:46 +0800

    make improvements of the centerstar algorithm module

---
+++ src/GCModeller/analysis/SequenceToolkit/MSA/CenterStar.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.DataMining
+Imports SMRUCC.genomics.SequenceModel.FASTA
+
+Public Class CenterStar
+
+    Dim algorithm As DynamicProgramming.CenterStar
+
+    Sub New(seqs As IEnumerable(Of String))
+        algorithm = New DynamicProgramming.CenterStar(seqs)
+    End Sub
+
+    Sub New(seqs As IEnumerable(Of FastaSeq))
+        algorithm = New DynamicProgramming.CenterStar(seqs.Select(Function(f) New NamedValue(Of String)(f.Title, f.SequenceData)))
+    End Sub
+
+    Public Function Compute(matrix As ScoreMatrix) As MSAOutput
+        Dim alignments As String() = Nothing
+        Dim cost As Double = algorithm.Compute(matrix Or ScoreMatrix.DefaultMatrix, alignments)
+        Dim output As New MSAOutput With {
+            .cost = cost,
+            .MSA = alignments,
+            .names = algorithm.NameList
+        }
+
+        Return output
+    End Function
+End Class
