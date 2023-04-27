47fa9739b6a51d2e2011a6f779750ac745c879e5
guigang xie <xie.guigang@gcmodeller.org>
2023-04-27 13:34:17 +0800

    FASTQ format is a text-based format for storing both a biological sequence (usually nucleotide sequence) and its corresponding quality scores. Both the sequence letter and quality score are each encoded with a single ASCII character for brevity. It was originally developed at the Wellcome Trust Sanger Institute to bundle a FASTA formatted sequence and its quality data, but has recently become the de facto standard for storing the output of high-throughput sequencing instruments such as the Illumina Genome Analyzer.

--- src/workbench/R#/rnaseq/FastQ.vb
+++ src/workbench/R#/rnaseq/FastQ.vb
@@ -7,9 +7,31 @@ Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 
+''' <summary>
+''' FastQ toolkit
+''' </summary>
+''' <remarks>
+''' FASTQ format Is a text-based format For storing both a biological sequence 
+''' (usually nucleotide sequence) And its corresponding quality scores. Both 
+''' the sequence letter And quality score are Each encoded With a Single ASCII 
+''' character For brevity. It was originally developed at the Wellcome Trust 
+''' Sanger Institute To bundle a FASTA formatted sequence And its quality data, 
+''' but has recently become the de facto standard For storing the output Of 
+''' high-throughput sequencing instruments such As the Illumina Genome 
+''' Analyzer.
+''' </remarks>
 <Package("FastQ")>
 Public Module FastQ
 
+    ''' <summary>
+    ''' In FASTQ files, quality scores are encoded into a compact form, 
+    ''' which uses only 1 byte per quality value. In this encoding, the 
+    ''' quality score is represented as the character with an ASCII 
+    ''' code equal to its value + 33.
+    ''' </summary>
+    ''' <param name="q"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
     <ExportAPI("quality_score")>
     <RApiReturn(GetType(Double))>
     Public Function GetQualityScore(q As Object, Optional env As Environment = Nothing) As Object
