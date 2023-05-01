d603571ac5bc58f2a7e56ebb92b411643a095fc4
xieguigang <xie.guigang@gcmodeller.org>
2023-05-02 01:29:46 +0800

    create the general sequence model

--- src/GCModeller/analysis/Motifs/MotifGraph/Builder.vb
+++ src/GCModeller/analysis/Motifs/MotifGraph/Builder.vb
@@ -1,20 +1,24 @@
-﻿Imports SMRUCC.genomics.SequenceModel.FASTA
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.SequenceModel.FASTA
 
 Public Module Builder
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function DNAGraph(seq As FastaSeq) As SequenceGraph
-
+        Return SequenceGraph(seq.SequenceData, SequenceModel.NT)
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function RNAGraph(seq As FastaSeq) As SequenceGraph
-
+        Return SequenceGraph(seq.SequenceData, SequenceModel.RNA)
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function PolypeptideGraph(seq As FastaSeq) As SequenceGraph
-
+        Return SequenceGraph(seq.SequenceData, SequenceModel.AA)
     End Function
 
-    Public Function SequenceGraph(seq As String, components As String) As SequenceGraph
+    Public Function SequenceGraph(seq As String, components As IEnumerable(Of Char)) As SequenceGraph
 
     End Function
 End Module
