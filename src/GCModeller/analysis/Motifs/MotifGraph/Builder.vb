e4c21ba1959705032fddf9209935517a32d18f2a
xieguigang <xie.guigang@gcmodeller.org>
2023-05-02 01:19:04 +0800

    Create Builder.vb

---
+++ src/GCModeller/analysis/Motifs/MotifGraph/Builder.vb
@@ -0,0 +1,16 @@
+﻿Imports SMRUCC.genomics.SequenceModel.FASTA
+
+Public Module Builder
+
+    Public Function DNAGraph(seq As FastaSeq) As SequenceGraph
+
+    End Function
+
+    Public Function RNAGraph(seq As FastaSeq) As SequenceGraph
+
+    End Function
+
+    Public Function PolypeptideGraph(seq As FastaSeq) As SequenceGraph
+
+    End Function
+End Module
