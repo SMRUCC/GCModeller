7db2d84ef924b809963d4c1c53487d250c467f8e
xieguigang <xie.guigang@gcmodeller.org>
2023-05-09 22:33:57 +0800

    how to implemenets the jquery liked html parser?

---
+++ src/workbench/pkg/R/regprecise/query.R
@@ -0,0 +1,6 @@
+imports "jQuery" from "webKit";
+
+const list.taxonomic_group = function() {
+    const page = jQuery::load('https://regprecise.lbl.gov/collections_tax.jsp');
+    const stattbl = page[".stattbl"];
+}
