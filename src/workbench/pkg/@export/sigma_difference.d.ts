1357860f9b8b27431f79c907b4f86ae8b144867f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-12 22:36:58 +0800

    build updates of the package documents

---
+++ src/workbench/pkg/@export/sigma_difference.d.ts
@@ -0,0 +1,21 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+// ref=seqtoolkit.SigmaDifference@seqtoolkit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+ * 
+*/
+declare namespace sigma_difference {
+   module genome {
+      /**
+       * 并行版本的计算函数
+       * 
+       * 
+        * @param genome -
+        * @param windowsSize 默认为1kb的长度
+        * 
+        * + default value Is ``1000``.
+      */
+      function sigma_diff(genome: object, compare: object, windowsSize?: object): object;
+   }
+}
---
+++ src/workbench/pkg/man/seqtoolkit/sigma_difference/sigma_difference.md
@@ -0,0 +1,5 @@
+# sigma_difference
+
+
+
++ [genome.sigma_diff](sigma_difference/genome.sigma_diff.1) 并行版本的计算函数
