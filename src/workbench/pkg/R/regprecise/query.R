d0f2cf616498308b38b79b293c9aa80571124604
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-13 01:29:04 +0800

    removes regprecise script

--- src/workbench/pkg/R/regprecise/query.R
+++
@@ -1,8 +0,0 @@
-imports "jQuery" from "webKit";
-
-#' Query the taxonomic list from RegPrecise database
-#' 
-const list.taxonomic_group = function() {
-    const page = jQuery::load('https://regprecise.lbl.gov/collections_tax.jsp');
-    const stattbl = page[".stattbl"];
-}
