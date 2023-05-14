888d4246299ce4ddbc5bc9a935738a7d08ed4e90
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-14 12:03:37 +0800

    add triple into model

--- src/GCModeller/analysis/Motifs/MotifGraph/SequenceGraph.vb
+++ src/GCModeller/analysis/Motifs/MotifGraph/SequenceGraph.vb
@@ -6,6 +6,7 @@ Public Class SequenceGraph
 
     Public Property Compositions As Dictionary(Of Char, Double)
     Public Property Graph As Dictionary(Of Char, Dictionary(Of Char, Double))
+    Public Property Triple As Dictionary(Of String, Double)
 
     Public Function GetVector(components As IReadOnlyCollection(Of Char)) As Double()
         Dim v As New List(Of Double)
@@ -16,6 +17,14 @@ Public Class SequenceGraph
         For Each key As Char In components
             g = Graph(key)
             v.AddRange(components.Select(Function(ci) g(ci)))
+        Next
+
+        For Each i As Char In components
+            For Each j As Char In components
+                For Each k As Char In components
+                    Call v.Add(Triple.TryGetValue(New String({i, j, k})))
+                Next
+            Next
         Next
 
         Return v.ToArray
