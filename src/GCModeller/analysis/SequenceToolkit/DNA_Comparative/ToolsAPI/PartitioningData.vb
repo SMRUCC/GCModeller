aaef37e7dcb24c1f58fe59d7ba4087837f12672e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-14 11:38:43 +0800

    removes un-used namespace reference

--- src/GCModeller/analysis/SequenceToolkit/DNA_Comparative/ToolsAPI/PartitioningData.vb
+++ src/GCModeller/analysis/SequenceToolkit/DNA_Comparative/ToolsAPI/PartitioningData.vb
@@ -1,58 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::3144bbbf9c7886ccecbe5217f651bf9d, GCModeller\analysis\SequenceToolkit\DNA_Comparative\ToolsAPI\PartitioningData.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 77
-    '    Code Lines: 40
-    ' Comment Lines: 29
-    '   Blank Lines: 8
-    '     File Size: 2.43 KB
+' Summaries:
 
 
-    ' Class PartitioningData
-    ' 
-    '     Properties: GC, GenomeID, Headers, Length, LociLeft
-    '                 LociRight, ORFList, PartitioningTag, SequenceData, Title
-    ' 
-    '     Function: ToFasta, ToString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 77
+'    Code Lines: 40
+' Comment Lines: 29
+'   Blank Lines: 8
+'     File Size: 2.43 KB
+
+
+' Class PartitioningData
+' 
+'     Properties: GC, GenomeID, Headers, Length, LociLeft
+'                 LociRight, ORFList, PartitioningTag, SequenceData, Title
+' 
+'     Function: ToFasta, ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
+Imports System.ComponentModel.DataAnnotations.Schema
 Imports SMRUCC.genomics.SequenceModel
 Imports SMRUCC.genomics.SequenceModel.FASTA
 
@@ -102,7 +102,7 @@ Public Class PartitioningData : Implements IAbstractFastaToken
         End Get
     End Property
 
-    Public ReadOnly Property Title As String Implements IAbstractFastaToken.Title
+    Public ReadOnly Property Title As String Implements IAbstractFastaToken.title
         Get
             Return String.Format("{0} ({1})", GenomeID, PartitioningTag)
         End Get
@@ -116,7 +116,7 @@ Public Class PartitioningData : Implements IAbstractFastaToken
     ''' <remarks></remarks>
     <Ignored> Public Property SequenceData As String Implements IPolymerSequenceModel.SequenceData
 
-    Public Property Headers As String() Implements IAbstractFastaToken.Headers
+    Public Property Headers As String() Implements IAbstractFastaToken.headers
 
     Public Overrides Function ToString() As String
         Return Title & ":  " & SequenceData
