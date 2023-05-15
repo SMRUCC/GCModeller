f39d201d20bf1ea804454979fb1efaac1019bd0d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-15 17:59:36 +0800

    add pack file for save the seeds data

---
+++ src/GCModeller/analysis/SequenceToolkit/MotifFinder/ScanFile.vb
@@ -0,0 +1,45 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.DataStorage.HDSPack
+Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Class ScanFile : Implements IDisposable
+
+    ReadOnly pack As StreamPack
+
+    Private disposedValue As Boolean
+
+    Sub New(file As Stream)
+        pack = New StreamPack(file)
+    End Sub
+
+    Public Sub AddSeed(name As String, seed As HSP)
+        Call pack.WriteText(seed.GetJson, fileName:=$"/seeds/{name.First}/{name}.json")
+    End Sub
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects)
+                Call pack.Dispose()
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            ' TODO: set large fields to null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: override finalizer only if 'Dispose(disposing As Boolean)' has code to free unmanaged resources
+    ' Protected Overrides Sub Finalize()
+    '     ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
+End Class
