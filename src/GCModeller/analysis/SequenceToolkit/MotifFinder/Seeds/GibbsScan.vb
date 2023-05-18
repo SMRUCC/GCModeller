5c30be89cb59e8000d7a2d0608f9e59af270cbc2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-18 20:59:18 +0800

    do motif finding via gibbs sampling?

---
+++ src/GCModeller/analysis/SequenceToolkit/MotifFinder/Seeds/GibbsScan.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic.Math.GibbsSampling
+Imports SMRUCC.genomics.SequenceModel.FASTA
+
+Public Class GibbsScan : Inherits SeedScanner
+
+    Public Sub New(param As PopulatorParameter, debug As Boolean)
+        MyBase.New(param, debug)
+    End Sub
+
+    Public Overrides Function GetSeeds(regions() As FastaSeq) As IEnumerable(Of HSP)
+        For i As Integer = param.minW To param.maxW
+            Dim gibbs As New Gibbs(regions.Select(Function(a) a.SequenceData).ToArray, i)
+            Dim find = gibbs.sample
+
+        Next
+    End Function
+End Class
