e88f8f1f189a8a10b082744683378142e028c541
guigang xie <xie.guigang@gcmodeller.org>
2023-05-30 13:13:23 +0800

    fix of the request  api error

--- src/workbench/pkg/R/ncbi/get_genbank.R
+++ src/workbench/pkg/R/ncbi/get_genbank.R
@@ -2,10 +2,20 @@ imports "GenBank" from "seqtoolkit";
 
 # https://www.ncbi.nlm.nih.gov/datasets/taxonomy/511145/
 
-const genbank_annotation_flags = ["GENOME_FASTA","GENOME_GFF","RNA_FASTA","CDS_FASTA","PROT_FASTA","SEQUENCE_REPORT"];
+const genbank_annotation_flags = ["GENOME_FASTA","GENOME_GFF","GENOME_GBFF","RNA_FASTA","CDS_FASTA","PROT_FASTA","SEQUENCE_REPORT"];
 const fetch_genbank_api = "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/%s/download?include_annotation_type=%s&filename=%s.zip";
 
-const fetch_genbank = function(accession_id, annotations = genbank_annotation_flags) {
+const fetch_genbank = function(accession_id, 
+                               annotations = [
+                                   "GENOME_GBFF",
+                                   "GENOME_FASTA",
+                                   "GENOME_GFF",
+                                   "RNA_FASTA",
+                                   "CDS_FASTA",
+                                   "PROT_FASTA",
+                                   "SEQUENCE_REPORT"
+                               ]) {
+
     const url = sprintf(fetch_genbank_api, accession_id, paste(annotations, ","), accession_id);
     const http.cache_dir = getOption("http.cache_dir") || stop("You should set of the 'http.cache_dir' option at first!");
     const key = md5(accession_id);
@@ -13,10 +23,11 @@ const fetch_genbank = function(accession_id, annotations = genbank_annotation_fl
     const temp = `${temp_dir}/ncbi_download.zip`;
     const gbff_file = `${temp_dir}/ncbi_dataset/data/${accession_id}/genomic.gbff`;
 
+    print(`fetch: ${url}`);
     # download the target ncbi dataset package
-    wget(url, save = temp);
+    wget(url, saveAs = temp);
     # and then extract the zip dataset package at temp location
-    unzip(temp);
+    unzip(temp, exdir = temp_dir);
 
     if (!file.exists(gbff_file)) {
         stop([
