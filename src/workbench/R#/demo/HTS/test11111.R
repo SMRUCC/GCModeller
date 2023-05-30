8d08bb100d4e50b142787711674ad4040b2e64c3
xieguigang <xie.guigang@gcmodeller.org>
2023-05-31 02:30:58 +0800

    run test success

--- src/workbench/R#/demo/HTS/test11111.R
+++ src/workbench/R#/demo/HTS/test11111.R
@@ -35,3 +35,38 @@ map[, "class"] = as.character(sapply(labels, function(lb) {
 }));
 
 write.csv(map ,file = "./counts_3d.csv", row.names = TRUE);
+
+require(ggplot);
+
+options(strict = FALSE);
+
+bitmap(file = `./counts_UMAP3d.png`, size = [4800, 3600]) {
+    
+	let data = map;
+	# data[, "class"] = `class_${rownames(data)}`;
+	
+	# colorset = {
+	# class_5:"red",
+	# class_0:"blue",
+	# class_4:"black",
+	# class_1:"yellow",
+	# class_9:"green",
+	# class_2:"skyblue",
+	# class_3:"lime",
+	# class_6:"orange",
+	# class_7:"steelblue",
+	# class_8:"brown"
+	# };
+	
+	print(unique(data[, "class"]));
+	
+	# create ggplot layers and tweaks via ggplot style options
+	ggplot(data, aes(x = "x", y = "y" , z = "z")
+    , padding = "padding:250px 500px 100px 100px;")
+	+ geom_point(aes(color = "class"), color = "paper", shape = "triangle", size = 80)
+	# + view_camera(angle = [31.5,65,125], fov = 100000)
+	+ ggtitle("Scatter UMAP 3D")
+	# + theme_default()
+	;
+
+}
