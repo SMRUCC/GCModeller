e145b7af7e932e3971bbbd1faf639cbbab7d06ff
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-03 03:11:54 +0800

    removes legacy .net framework project files

--- src/GCModeller/analysis/SequenceToolkit/SequencePatterns/Motif/MotifLog.vb
+++ src/GCModeller/analysis/SequenceToolkit/SequencePatterns/Motif/MotifLog.vb
@@ -1,62 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::3126d8237d66eeb00f22bbf93c182b1a, GCModeller\analysis\SequenceToolkit\SequencePatterns\Motif\MotifLog.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 69
-    '    Code Lines: 45
-    ' Comment Lines: 16
-    '   Blank Lines: 8
-    '     File Size: 2.06 KB
+' Summaries:
 
 
-    ' Class MotifLog
-    ' 
-    '     Properties: ATGDist, BiologicalProcess, Family, InPromoterRegion, Location
-    '                 Regulog, tag, tags, Taxonomy
-    ' 
-    '     Constructor: (+3 Overloads) Sub New
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 69
+'    Code Lines: 45
+' Comment Lines: 16
+'   Blank Lines: 8
+'     File Size: 2.06 KB
+
+
+' Class MotifLog
+' 
+'     Properties: ATGDist, BiologicalProcess, Family, InPromoterRegion, Location
+'                 Regulog, tag, tags, Taxonomy
+' 
+'     Constructor: (+3 Overloads) Sub New
+' 
+' /********************************************************************************/
 
 #End Region
 
-#If netcore5 = 0 Then
-Imports System.Web.Script.Serialization
-#Else
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
-#End If
 Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
 Imports Microsoft.VisualBasic.Math
 Imports SMRUCC.genomics.SequenceModel.NucleotideModels
