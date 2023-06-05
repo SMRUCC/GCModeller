d9eacdff4224da4291468f9e330efc7fb28571e3
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-05 22:02:54 +0800

    read biopax file for parse rhea database file

--- src/GCModeller/engine/vcellkit/Modeller/Enzymatic.vb
+++ src/GCModeller/engine/vcellkit/Modeller/Enzymatic.vb
@@ -60,6 +60,8 @@ Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports sbXML = SMRUCC.genomics.Model.SBML.Level3.XmlFile(Of SMRUCC.genomics.Data.SABIORK.SBML.SBMLReaction)
+Imports biopax = SMRUCC.genomics.Model.Biopax.Level3.File
+Imports SMRUCC.genomics.Model.Biopax.Level3
 
 ''' <summary>
 ''' enzymatic reaction network modeller
@@ -78,9 +80,24 @@ Module Enzymatic
         Return list
     End Function
 
+    ''' <summary>
+    ''' read the rhea database file
+    ''' </summary>
+    ''' <param name="file"></param>
+    ''' <returns></returns>
     <ExportAPI("read.rhea")>
     Public Function ParseRhea(file As String) As Object
-        Return TextParser.ParseReactions(file.ReadAllLines).DoCall(AddressOf pipeline.CreateFromPopulator)
+        If file.ExtensionSuffix("owl") Then
+            ' biopax reader
+            Dim xml As biopax = biopax.LoadDoc(file)
+            Dim loader As ResourceReader = ResourceReader.LoadResource(file:=xml)
+            Dim reactions = loader.GetAllReactions.ToArray
+
+            Return reactions
+        Else
+            Return TextParser.ParseReactions(file.ReadAllLines) _
+                .DoCall(AddressOf pipeline.CreateFromPopulator)
+        End If
     End Function
 
     <ExportAPI("open.rhea")>
