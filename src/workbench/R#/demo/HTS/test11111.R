648115cb05cbc2a2f2a2a62918499c179d0a0ef8
xieguigang <gg.xie@bionovogene.com>
2023-06-07 10:36:09 +0800

    configs for the plot data view

--- src/workbench/R#/demo/HTS/test11111.R
+++ src/workbench/R#/demo/HTS/test11111.R
@@ -12,7 +12,7 @@ let raw = geneExpression::load.expr(file = "./all_counts.csv");
 
 str(dims(raw));
 
-let sgt   = SGT(full = FALSE);
+let sgt   = SGT(full = FALSE,  kappa = 3);
 let pack  = encode.seqPack(raw, briefSet = TRUE);
 # let graph = as.seq_graph(pack);
 # let view = as.data.frame(graph);
@@ -21,8 +21,8 @@ let view = fit(sgt, seqs = pack, df = TRUE, par = FALSE);
 
 print(view);
 
-let manifold = umap(view, dimension = 3);
-let map = as.data.frame(manifold$umap, labels = manifold$labels, dimension = ["x","y","z"]);
+let manifold = umap(view, dimension = 5);
+let map = as.data.frame(manifold$umap, labels = manifold$labels, dimension = ["x","y","z","dim4","dim5"]);
 
 let labels = rownames(map);
 let groups = guess.sample_groups(labels, maxDepth = TRUE);
@@ -44,7 +44,7 @@ require(ggplot);
 
 options(strict = FALSE);
 
-bitmap(file = `./counts_UMAP3d.png`, size = [4800, 3600]) {
+bitmap(file = `./counts_UMAP3d.png`, size = [2800, 2100]) {
     
 	let data = map;
 	# data[, "class"] = `class_${rownames(data)}`;
@@ -70,6 +70,38 @@ bitmap(file = `./counts_UMAP3d.png`, size = [4800, 3600]) {
 	+ geom_point(aes(color = "class"), color = "paper", shape = "triangle", size = 80)
 	# + view_camera(angle = [31.5,65,125], fov = 100000)
 	+ ggtitle("Scatter UMAP 3D")
+	# + theme_default()
+	;
+
+}
+
+
+bitmap(file = `./counts_UMAP2d.png`, size = [2800, 2100]) {
+    
+	let data = map;
+	# data[, "class"] = `class_${rownames(data)}`;
+	
+	# colorset = {
+	# class_5:"red",
+	# class_0:"blue",
+	# class_4:"black",
+	# class_1:"yellow",
+	# class_9:"green",
+	# class_2:"skyblue",
+	# class_3:"lime",
+	# class_6:"orange",
+	# class_7:"steelblue",
+	# class_8:"brown"
+	# };
+	
+	print(unique(data[, "class"]));
+	
+	# create ggplot layers and tweaks via ggplot style options
+	ggplot(data, aes(x = "x", y = "y")
+    , padding = "padding:250px 500px 100px 100px;")
+	+ geom_point(aes(color = "class"), color = "paper", shape = "triangle", size = 80)
+	# + view_camera(angle = [31.5,65,125], fov = 100000)
+	+ ggtitle("Scatter UMAP 2D")
 	# + theme_default()
 	;
 
