f132ff93377dd36b3d08429e02623f59a3c3695f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-09 21:59:20 +0800

    add sbml model file tools api

---
+++ src/workbench/R#/biosystem/SBMLTools.vb
@@ -0,0 +1,17 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("SBML")>
+Module SBMLTools
+
+    ''' <summary>
+    ''' Read a sbml model file from a given file path
+    ''' </summary>
+    ''' <param name="file"></param>
+    ''' <returns></returns>
+    <ExportAPI("read.sbml")>
+    Public Function readSBML(file As String)
+
+    End Function
+End Module
