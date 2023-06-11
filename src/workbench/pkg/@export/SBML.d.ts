f8d2f6c7a87bb3bdb4500e5bf906da90b0d66568
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-11 14:00:56 +0800

    build updates of package export header files

--- src/workbench/pkg/@export/SBML.d.ts
+++ src/workbench/pkg/@export/SBML.d.ts
@@ -9,10 +9,21 @@
 declare namespace SBML {
    module extract {
       /**
+        * @param json default value Is ``false``.
         * @param env default value Is ``null``.
       */
-      function compartments(sbml: any, env?: object): any;
+      function compartments(sbml: any, json?: boolean, env?: object): any;
    }
+   /**
+     * @param json default value Is ``false``.
+     * @param env default value Is ``null``.
+   */
+   function extract_compounds(sbml: any, json?: boolean, env?: object): any;
+   /**
+     * @param json default value Is ``false``.
+     * @param env default value Is ``null``.
+   */
+   function extract_reactions(sbml: any, json?: boolean, env?: object): any;
    module read {
       /**
       */
--- src/workbench/pkg/man/biosystem/SBML/SBML.md
+++ src/workbench/pkg/man/biosystem/SBML/SBML.md
@@ -4,3 +4,5 @@
 
 + [read.sbml](SBML/read.sbml.1) 
 + [extract.compartments](SBML/extract.compartments.1) 
++ [extract_reactions](SBML/extract_reactions.1) 
++ [extract_compounds](SBML/extract_compounds.1) 
