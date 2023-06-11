a78bcb76d70b2c610c92a7985c9ba2c085f2ef76
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-11 13:07:02 +0800

    export list

--- src/workbench/R#/biosystem/SBMLTools.vb
+++ src/workbench/R#/biosystem/SBMLTools.vb
@@ -21,7 +21,9 @@ Module SBMLTools
     End Function
 
     <ExportAPI("extract.compartments")>
-    Public Function extract_compartments(sbml As Object, Optional env As Environment = Nothing) As Object
+    Public Function extract_compartments(sbml As Object,
+                                         Optional json As Boolean = False,
+                                         Optional env As Environment = Nothing) As Object
         If sbml Is Nothing Then
             Return Nothing
         End If
@@ -29,25 +31,44 @@ Module SBMLTools
         If TypeOf sbml Is Level3.XmlFile(Of Level3.Reaction) Then
             Dim xml As Level3.XmlFile(Of Level3.Reaction) = sbml
             Dim list = xml.model.listOfCompartments.SafeQuery.ToArray
-            Dim df As New dataframe With {
-                .columns = New Dictionary(Of String, Array),
-                .rownames = list _
-                    .Select(Function(c) c.id) _
-                    .ToArray
-            }
 
-            Call df.add("name", list.Select(Function(c) c.name))
-            Call df.add("is", list.Select(Function(c) If(c.annotation Is Nothing, "", c.annotation.GetIdMappings.Distinct.JoinBy("; "))))
-            Call df.add("type", list.Select(Function(c) c.sboTerm))
+            If json Then
+                Dim array As New list With {.slots = New Dictionary(Of String, Object)}
 
-            Return df
+                For Each loc As Level3.compartment In list
+                    array.slots(loc.id) = New list With {
+                        .slots = New Dictionary(Of String, Object) From {
+                            {"name", loc.name},
+                            {"is", If(loc.annotation Is Nothing, New String() {}, loc.annotation.GetIdMappings.Distinct.ToArray)},
+                            {"type", loc.sboTerm}
+                        }
+                    }
+                Next
+
+                Return array
+            Else
+                Dim df As New dataframe With {
+                    .columns = New Dictionary(Of String, Array),
+                    .rownames = list _
+                        .Select(Function(c) c.id) _
+                        .ToArray
+                }
+
+                Call df.add("name", list.Select(Function(c) c.name))
+                Call df.add("is", list.Select(Function(c) If(c.annotation Is Nothing, "", c.annotation.GetIdMappings.Distinct.JoinBy("; "))))
+                Call df.add("type", list.Select(Function(c) c.sboTerm))
+
+                Return df
+            End If
         Else
             Return Message.InCompatibleType(GetType(Level3.XmlFile(Of Level3.Reaction)), sbml.GetType, env)
         End If
     End Function
 
     <ExportAPI("extract_reactions")>
-    Public Function extract_reactions(sbml As Object, Optional env As Environment = Nothing) As Object
+    Public Function extract_reactions(sbml As Object,
+                                      Optional json As Boolean = False,
+                                      Optional env As Environment = Nothing) As Object
         If sbml Is Nothing Then
             Return Nothing
         End If
@@ -55,30 +76,54 @@ Module SBMLTools
         If TypeOf sbml Is Level3.XmlFile(Of Level3.Reaction) Then
             Dim xml As Level3.XmlFile(Of Level3.Reaction) = sbml
             Dim list = xml.model.listOfReactions.SafeQuery.ToArray
-            Dim df As New dataframe With {
-                .columns = New Dictionary(Of String, Array),
-                .rownames = list _
-                    .Select(Function(c) c.id) _
-                    .ToArray
-            }
 
-            Call df.add("name", list.Select(Function(c) c.name))
-            Call df.add("is", list.Select(Function(c) If(c.annotation Is Nothing, "", c.annotation.GetIdMappings.Distinct.JoinBy("; "))))
-            Call df.add("reversible", list.Select(Function(c) c.reversible))
-            Call df.add("fast", list.Select(Function(c) c.fast))
-            Call df.add("reactants", list.Select(Function(c) c.listOfReactants.Select(Function(a) $"{a.stoichiometry} {a.species}").JoinBy("; ")))
-            Call df.add("products", list.Select(Function(c) c.listOfProducts.Select(Function(a) $"{a.stoichiometry} {a.species}").JoinBy("; ")))
-            Call df.add("modifiers", list.Select(Function(c) c.listOfModifiers.Select(Function(a) a.species).JoinBy("; ")))
-            Call df.add("notes", list.Select(Function(c) c.notes.GetText))
+            If json Then
+                Dim array As New list With {.slots = New Dictionary(Of String, Object)}
 
-            Return df
+                For Each rxn As Level3.Reaction In list
+                    array.slots(rxn.id) = New list With {
+                        .slots = New Dictionary(Of String, Object) From {
+                            {"name", rxn.name},
+                            {"is", If(rxn.annotation Is Nothing, New String() {}, rxn.annotation.GetIdMappings.Distinct.ToArray)},
+                            {"reversible", rxn.reversible},
+                            {"fast", rxn.fast},
+                            {"reactants", rxn.listOfReactants.ToDictionary(Function(a) a.species, Function(a) a.stoichiometry)},
+                            {"products", rxn.listOfProducts.ToDictionary(Function(a) a.species, Function(a) a.stoichiometry)},
+                            {"modifiers", rxn.listOfModifiers.Select(Function(a) a.species).ToArray},
+                            {"notes", rxn.notes.GetText}
+                        }
+                    }
+                Next
+
+                Return array
+            Else
+                Dim df As New dataframe With {
+                    .columns = New Dictionary(Of String, Array),
+                    .rownames = list _
+                        .Select(Function(c) c.id) _
+                        .ToArray
+                }
+
+                Call df.add("name", list.Select(Function(c) c.name))
+                Call df.add("is", list.Select(Function(c) If(c.annotation Is Nothing, "", c.annotation.GetIdMappings.Distinct.JoinBy("; "))))
+                Call df.add("reversible", list.Select(Function(c) c.reversible))
+                Call df.add("fast", list.Select(Function(c) c.fast))
+                Call df.add("reactants", list.Select(Function(c) c.listOfReactants.Select(Function(a) $"{a.stoichiometry} {a.species}").JoinBy("; ")))
+                Call df.add("products", list.Select(Function(c) c.listOfProducts.Select(Function(a) $"{a.stoichiometry} {a.species}").JoinBy("; ")))
+                Call df.add("modifiers", list.Select(Function(c) c.listOfModifiers.Select(Function(a) a.species).JoinBy("; ")))
+                Call df.add("notes", list.Select(Function(c) c.notes.GetText))
+
+                Return df
+            End If
         Else
             Return Message.InCompatibleType(GetType(Level3.XmlFile(Of Level3.Reaction)), sbml.GetType, env)
         End If
     End Function
 
     <ExportAPI("extract_compounds")>
-    Public Function extract_compounds(sbml As Object, Optional env As Environment = Nothing) As Object
+    Public Function extract_compounds(sbml As Object,
+                                      Optional json As Boolean = False,
+                                      Optional env As Environment = Nothing) As Object
         If sbml Is Nothing Then
             Return Nothing
         End If
@@ -86,21 +131,41 @@ Module SBMLTools
         If TypeOf sbml Is Level3.XmlFile(Of Level3.Reaction) Then
             Dim xml As Level3.XmlFile(Of Level3.Reaction) = sbml
             Dim list = xml.model.listOfSpecies.ToArray
-            Dim df As New dataframe With {
-                .columns = New Dictionary(Of String, Array),
-                .rownames = list _
-                    .Select(Function(c) c.id) _
-                    .ToArray
-            }
 
-            Call df.add("name", list.Select(Function(c) c.name))
-            Call df.add("is", list.Select(Function(c) If(c.annotation Is Nothing, "", c.annotation.GetIdMappings.Distinct.JoinBy("; "))))
-            Call df.add("type", list.Select(Function(c) c.sboTerm))
-            Call df.add("components", list.Select(Function(c) If(c.annotation Is Nothing, "", c.annotation.GetIdComponents.Distinct.JoinBy("; "))))
-            Call df.add("homolog", list.Select(Function(c) If(c.annotation Is Nothing, "", c.annotation.GetIdHomolog.Distinct.JoinBy("; "))))
-            Call df.add("notes", list.Select(Function(c) c.notes.GetText.TrimNewLine))
+            If json Then
+                Dim array As New list With {.slots = New Dictionary(Of String, Object)}
 
-            Return df
+                For Each cpd As Level3.species In list
+                    array.slots(cpd.id) = New list With {
+                        .slots = New Dictionary(Of String, Object) From {
+                            {"name", cpd.name},
+                            {"is", If(cpd.annotation Is Nothing, New String() {}, cpd.annotation.GetIdMappings.Distinct.ToArray)},
+                            {"type", cpd.sboTerm},
+                            {"components", If(cpd.annotation Is Nothing, New String() {}, cpd.annotation.GetIdComponents.Distinct.ToArray)},
+                            {"homolog", If(cpd.annotation Is Nothing, New String() {}, cpd.annotation.GetIdHomolog.Distinct.ToArray)},
+                            {"notes", cpd.notes.GetText}
+                        }
+                    }
+                Next
+
+                Return array
+            Else
+                Dim df As New dataframe With {
+                    .columns = New Dictionary(Of String, Array),
+                    .rownames = list _
+                        .Select(Function(c) c.id) _
+                        .ToArray
+                }
+
+                Call df.add("name", list.Select(Function(c) c.name))
+                Call df.add("is", list.Select(Function(c) If(c.annotation Is Nothing, "", c.annotation.GetIdMappings.Distinct.JoinBy("; "))))
+                Call df.add("type", list.Select(Function(c) c.sboTerm))
+                Call df.add("components", list.Select(Function(c) If(c.annotation Is Nothing, "", c.annotation.GetIdComponents.Distinct.JoinBy("; "))))
+                Call df.add("homolog", list.Select(Function(c) If(c.annotation Is Nothing, "", c.annotation.GetIdHomolog.Distinct.JoinBy("; "))))
+                Call df.add("notes", list.Select(Function(c) c.notes.GetText.TrimNewLine))
+
+                Return df
+            End If
         Else
             Return Message.InCompatibleType(GetType(Level3.XmlFile(Of Level3.Reaction)), sbml.GetType, env)
         End If
