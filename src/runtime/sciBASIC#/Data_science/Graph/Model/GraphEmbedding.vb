12c3d32e968f5af95fb39067dd74afbd5b1e6e79
xieguigang <gg.xie@bionovogene.com>
2023-07-04 11:56:44 +0800

    how to make a general model for create graph embedding

---
+++ src/runtime/sciBASIC#/Data_science/Graph/Model/GraphEmbedding.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+
+''' <summary>
+''' Embedding of a graph object as numeric vector
+''' </summary>
+Public Module GraphEmbedding
+
+    <Extension>
+    Public Function ToVector(g) As Vector
+
+    End Function
+End Module
