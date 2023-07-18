f227d0db1e88b5d311aa819b14de5fcde1fdc99d
guigang xie <xie.guigang@gcmodeller.org>
2023-07-18 14:00:52 +0800

    fix for the language scanner for R ... symbol

--- src/workbench/R#/demo/HTS/design_test.R
+++ src/workbench/R#/demo/HTS/design_test.R
@@ -4,8 +4,9 @@ imports "sampleInfo" from "phenotype_kit";
 
 let new1 = sampleInfo::design(
     read.sampleinfo(`${@dir}/sampleInfo.csv`),
-    C = C6 + C9,
-    I = I56 + I59 + I86 + I89
+    "C vs TTTTT" = C6 + C9,
+    I = I56 + I59 + I86 + I89,
+    ... = I56 + I59
 );
 
 print(as.data.frame(new1));
