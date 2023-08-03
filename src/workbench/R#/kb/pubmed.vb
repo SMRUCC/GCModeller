9b797aa0692789250a60c5d51401dfd68bb83963
guigang xie <xie.guigang@gcmodeller.org>
2023-08-03 11:33:16 +0800

    deal with the possible null reference error

--- src/workbench/R#/kb/pubmed.vb
+++ src/workbench/R#/kb/pubmed.vb
@@ -14,12 +14,13 @@ Module pubmed
         Call Internal.Object.Converts.makeDataframe.addHandler(GetType(PubmedArticle()), AddressOf createArticleTable)
     End Sub
 
-    Public Function createArticleTable(articles As PubmedArticle(), args As list, env As Environment) As dataframe
+    Public Function createArticleTable(list As PubmedArticle(), args As list, env As Environment) As dataframe
         Dim df As New dataframe With {.columns = New Dictionary(Of String, Array)}
-        Call df.add("pubmed", articles.Select(Function(a) a.MedlineCitation.PMID.ID))
-        Call df.add("doi", articles.Select(Function(a) a.MedlineCitation.Article.ELocationID.SafeQuery.Where(Function(d) d.EIdType = "DOI").FirstOrDefault?.Value))
-        Call df.add("title", articles.Select(Function(a) a.MedlineCitation.Article.ArticleTitle))
-        Call df.add("abstract", articles.Select(Function(a) a.MedlineCitation.Article.Abstract.AbstractText.SafeQuery.Select(Function(d) d.Text).JoinBy(vbCrLf)))
+        Dim articles = list.Select(Function(a) a.MedlineCitation).ToArray
+        Call df.add("pubmed", articles.Select(Function(a) a.PMID?.ID))
+        Call df.add("doi", articles.Select(Function(a) a.Article.ELocationID.SafeQuery.Where(Function(d) d.EIdType = "DOI").FirstOrDefault?.Value))
+        Call df.add("title", articles.Select(Function(a) a.Article.ArticleTitle))
+        Call df.add("abstract", articles.Select(Function(a) a.Article.Abstract?.AbstractText.SafeQuery.Select(Function(d) d.Text).JoinBy(vbCrLf)))
         Return df
     End Function
 
