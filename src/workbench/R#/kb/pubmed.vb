43afb3777fe0bc080ceaebaa18410391d5247a2f
guigang xie <xie.guigang@gcmodeller.org>
2023-08-03 10:38:40 +0800

    parse the pubmed article data

---
+++ src/workbench/R#/kb/pubmed.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.PubMed
+Imports SMRUCC.Rsharp.Runtime.Interop
+Imports SMRUCC.Rsharp.Runtime.Vectorization
+
+<Package("pubmed")>
+Module pubmed
+
+    <ExportAPI("parse")>
+    Public Function ParsePubmed(<RRawVectorArgument> text As Object)
+        Return CLRVector.asCharacter(text) _
+            .Select(Function(si) PubMedServicesExtensions.ParseArticles(si)) _
+            .IteratesALL _
+            .ToArray
+    End Function
+End Module
---
+++ src/workbench/R#/kb/zzz.vb
@@ -0,0 +1,3 @@
+Public Class zzz
+
+End Class
