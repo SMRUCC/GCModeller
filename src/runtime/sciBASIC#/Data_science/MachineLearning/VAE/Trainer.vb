376002763948744a64e8fdd36fca1bab9e5f3e5a
guigang xie <xie.guigang@gcmodeller.org>
2023-08-08 14:13:00 +0800

    add source files

---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/VAE/Trainer.vb
@@ -0,0 +1,54 @@
+﻿Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports std = System.Math
+
+' // MNIST_DATA_LOCATION set by MNIST cmake config
+' std::cout << "MNIST data directory: " << MNIST_DATA_LOCATION << std::endl;
+
+' // Load MNIST data
+' mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t> dataset =
+'     mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>(MNIST_DATA_LOCATION);
+' // std::cout << dataset.test_images[10000].size() << std::endl;
+' Trainer<double, 28, 28> * mnist_vae_trainer = new Trainer<double, 28, 28>(); 
+' mnist_vae_trainer->train_vae(1000, dataset.training_images);
+' std::cout << "Nbr of training images = " << dataset.training_images.size() << std::endl;
+' std::cout << "Nbr of training labels = " << dataset.training_labels.size() << std::endl;
+' std::cout << "Nbr of test images = " << dataset.test_images.size() << std::endl;
+' std::cout << "Nbr of test labels = " << dataset.test_labels.size() << std::endl;
+
+Public Class Trainer
+
+    Dim loss_vector As Vector = Nothing
+    Dim vae As Vae
+
+    Sub New(N1 As Integer, N2 As Integer)
+        vae = New Vae(N1, N2)
+    End Sub
+
+    Public Sub train_vae(steps As Integer, dataset_input As Byte()())
+        Dim input As Double() = New Double(dataset_input(0).Length - 1) {}
+        Dim div As Double = 1 / 256
+
+        For i As Integer = 0 To std.Min(dataset_input.Length, steps) - 1
+            ' handle input
+            If dataset_input(i).Length = 0 Then
+                Continue For
+            End If
+
+            Dim mean As Double = 0
+
+            For j As Integer = 0 To dataset_input(i).Length - 1
+                mean += dataset_input(i)(j)
+                input(j) = dataset_input(i)(j)
+            Next
+            For j As Integer = 0 To input.Length - 1
+                input(j) -= mean
+                input(j) *= div
+            Next
+
+            ' update weights
+            vae.encode(input)
+            vae.decode
+            vae.update(input)
+        Next
+    End Sub
+End Class
