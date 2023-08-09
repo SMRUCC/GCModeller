6edc41010087fd47c9f1a7d4ceed86dba9eb030c
guigang xie <xie.guigang@gcmodeller.org>
2023-08-09 17:30:45 +0800

    implements a new VAE module

---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/DeepLearning/Layers/Linear.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
+
+Namespace Math
+
+    ''' <summary>
+    ''' implements torch.nn.Linear
+    ''' </summary>
+    Public Class Linear
+
+        Dim w As NumericMatrix
+        Dim b As Double = 0.0
+        Dim bias As Boolean
+
+        Sub New(in_features As Integer, out_features As Integer, Optional bias As Boolean = True)
+            Me.bias = bias
+            Me.w = New NumericMatrix(in_features, out_features)
+        End Sub
+
+        Public Function Fit(x As Vector) As Vector
+            Return w.DotMultiply(x) + b
+        End Function
+
+        Public Sub backward(loss As Vector)
+            w = w + loss
+
+            If bias Then
+                b += loss.Sum
+            End If
+        End Sub
+    End Class
+End Namespace
--- src/runtime/sciBASIC#/Data_science/MachineLearning/VAE/Decoder.vb
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/VAE/Decoder.vb
@@ -1,90 +1,25 @@
-﻿Imports Microsoft.VisualBasic.Math.LinearAlgebra
-Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
-Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
-Imports std = System.Math
+﻿Imports Microsoft.VisualBasic.MachineLearning.ComponentModel.Activations
+Imports Microsoft.VisualBasic.MachineLearning.Math
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
 
-Public MustInherit Class VAEEnc
+Public Class Decoder
 
-    Protected weights As NumericMatrix
-    Protected strides As Integer()
-    Protected dims As Integer()
-    Protected learning_rate As Double
+    Dim linear1 As Linear
+    Dim linear2 As Linear
 
-    Public Overridable ReadOnly Property loss As Double
-
-    ''' <summary>
-    ''' initialize weights with Gaussian distribution
-    ''' </summary>
-    Protected MustOverride Sub init_random()
-
-    Public MustOverride Sub update(output As Vector, input As Vector)
-    Public MustOverride Function get_output(input As Vector) As Vector
-
-End Class
-
-' N = 2800?
-
-Public Class Decoder : Inherits VAEEnc
-
-    Dim m_loss As Double
-
-    Public Overrides ReadOnly Property loss As Double
-        Get
-            Return m_loss
-        End Get
-    End Property
-
-    Sub New(N As Integer,
-            kernel As Integer(),
-            strides As Integer(),
-            Optional learning_rate As Double = 0.00001)
-
-        Me.weights = New NumericMatrix(kernel.First * kernel.Last, N)
-        Me.strides = New Integer() {1, 1}
-        Me.dims = New Integer() {kernel.First, kernel.Last}
-        Me.learning_rate = learning_rate
-
-        Call init_random()
+    Sub New(latent_dims As Integer)
+        linear1 = New Linear(latent_dims, 512)
+        linear2 = New Linear(512, 784)
     End Sub
 
-    Protected Overrides Sub init_random()
-        Dim dI As Integer = weights.Length
-        Dim width As Integer = weights.ColumnDimension
-
-        For i As Integer = 0 To dI - 1
-            weights(i) = Vector.norm(width, mu:=0, sigma:=1)
-        Next
-    End Sub
-
-    Public Overrides Sub update(output As Vector, input As Vector)
-        Dim outX As New NumericMatrix(output.Array.Split(input.Dim))
-        'Loss is the mean square between the real image and the generated image
-        Dim dI As Integer = input.Dim
-        Dim loss_div As Double = 2.0 / dI
-        Dim m As Double = learning_rate
-
-        outX = New NumericMatrix(outX.RowVectors.Select(Function(ov)
-                                                            Dim delta As Vector = loss_div * (ov - input) * input
-                                                            Return delta
-                                                        End Function))
-
-
-        ' weights += m * delta
-
-        m_loss = loss
-    End Sub
-
-    Public Overrides Function get_output(input As Vector) As Vector
-        ' matrix multiplication
-        Dim output = weights.DotMultiply(input)
-
-        ' leaky relu activation
-        For i As Integer = 0 To output.Dim - 1
-            If output(i) < 0 Then
-                output(i) = 0.001
-            End If
-        Next
-
-        Return output
+    Public Function forward(z As Vector) As Vector
+        z = ReLU.ReLU(linear1.Fit(z))
+        z = SigmoidFunction.Sigmoid(linear2.Fit(z))
+        Return z
     End Function
+
+    Public Sub backward(loss As Vector)
+        Call linear1.backward(loss)
+        Call linear2.backward(loss)
+    End Sub
 End Class
