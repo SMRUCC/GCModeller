ef07651bc50d3fb4c76b20b73cd60cae69708471
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-08-09 18:32:27 +0800

    try to resize the vector

--- src/runtime/sciBASIC#/Data_science/MachineLearning/VAE/Decoder.vb
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/VAE/Decoder.vb
@@ -1,15 +1,20 @@
 ﻿Imports Microsoft.VisualBasic.MachineLearning.ComponentModel.Activations
 Imports Microsoft.VisualBasic.MachineLearning.Math
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
 
 Public Class Decoder
 
     Dim linear1 As Linear
     Dim linear2 As Linear
+    Dim t1 As NumericMatrix
+    Dim t2 As NumericMatrix
 
     Sub New(latent_dims As Integer)
         linear1 = New Linear(latent_dims, 512)
         linear2 = New Linear(512, 784)
+        t1 = NumericMatrix.One(784, 512)
+        t2 = NumericMatrix.One(512, 512)
     End Sub
 
     Public Function forward(z As Vector) As Vector
@@ -19,7 +24,7 @@ Public Class Decoder
     End Function
 
     Public Sub backward(loss As Vector)
-        Call linear1.backward(loss)
-        Call linear2.backward(loss)
+        Call linear1.backward(t1.DotMultiply(loss))
+        Call linear2.backward(t2.DotMultiply(loss))
     End Sub
 End Class
