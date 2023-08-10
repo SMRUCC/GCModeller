816155d927af956f5bc2afe85034228aa2dd5e3a
guigang xie <xie.guigang@gcmodeller.org>
2023-08-10 16:15:34 +0800

    add code model for create GMM models

---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/VAE/GMM/Datum.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.DataMining.KMeans
+Imports Microsoft.VisualBasic.Math.Correlations
+
+Namespace GMM
+
+    Public Class Datum
+
+        Private m_val As  ClusterEntity
+        Private m_probs As Double()
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Sub New(value As Double, components As Integer)
+            Call Me.New({value}, components)
+        End Sub
+
+        Sub New(value As IEnumerable(Of Double), components As Integer)
+            Call Me.New(New ClusterEntity With {.entityVector = value.ToArray}, components)
+        End Sub
+
+        Sub New(value As ClusterEntity, components As Integer)
+            m_val = value
+            m_probs = New Double(components - 1) {}
+
+            For i = 0 To m_probs.Length - 1
+                m_probs(i) = 0.0
+            Next
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overridable Function val() As Double
+            Return m_val.entityVector.EuclideanDistance
+        End Function
+
+        Public Overridable Sub setProb(i As Integer, val As Double)
+            m_probs(i) = val
+        End Sub
+
+        Public Overridable Function getProb(i As Integer) As Double
+            Return m_probs(i)
+        End Function
+    End Class
+End Namespace
---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/VAE/GMM/Solver.vb
@@ -0,0 +1,37 @@
+﻿Imports Microsoft.VisualBasic.DataMining.KMeans
+Imports std = System.Math
+
+Namespace GMM
+
+    ''' <summary>
+    ''' GMM problem solver
+    ''' </summary>
+    Public Class Solver
+
+        Public Shared Function Predicts(ds As IEnumerable(Of ClusterEntity), Optional components As Integer = 3) As Mixture
+            Return Training(New Mixture(New DataSet(ds, components)))
+        End Function
+
+        Private Shared Function Training(mix As Mixture) As Mixture
+            mix.printStats()
+
+            Dim oldLog As Double = mix.logLike()
+            Dim newLog = oldLog - 100.0
+            Do
+                oldLog = newLog
+                mix.Expectation()
+                mix.Maximization()
+                newLog = mix.logLike()
+                Console.WriteLine(newLog)
+            Loop While newLog <> 0 AndAlso std.Abs(newLog - oldLog) > 0.00000000000001
+
+            mix.printStats()
+
+            Return mix
+        End Function
+
+        Public Shared Function Predicts(x As IEnumerable(Of Double), Optional components As Integer = 3) As Mixture
+            Return Training(New Mixture(New DataSet(x, components)))
+        End Function
+    End Class
+End Namespace
