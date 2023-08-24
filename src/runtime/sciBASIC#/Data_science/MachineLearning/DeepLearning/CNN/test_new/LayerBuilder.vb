41eba7ca98dd0335092d85124ff7edaae1ae0e11
guigang xie <xie.guigang@gcmodeller.org>
2023-08-24 10:12:49 +0800

    make code style improvements for the CNN code

---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/DeepLearning/CNN/test_new/LayerBuilder.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace CNN
+
+    Public Class LayerBuilder
+
+        ReadOnly m_layers As New List(Of Layer)
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Sub New(layer As Layer)
+            m_layers = New List(Of Layer) From {layer}
+        End Sub
+
+        Sub New()
+        End Sub
+
+        ''' <summary>
+        ''' add a new layer into current builder object
+        ''' </summary>
+        ''' <param name="layer"></param>
+        ''' <returns></returns>
+        Public Overridable Function addLayer(layer As Layer) As LayerBuilder
+            m_layers.Add(layer)
+            Return Me
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overrides Function ToString() As String
+            Return $"{m_layers.Count} CNN layers: {m_layers.JoinBy(" -> ")}"
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Narrowing Operator CType(lb As LayerBuilder) As List(Of Layer)
+            Return lb.m_layers.AsEnumerable.AsList
+        End Operator
+    End Class
+End Namespace
---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/DeepLearning/CNN/test_new/Size.vb
@@ -0,0 +1,35 @@
+﻿Namespace CNN
+
+    Public Class Size
+
+        Public ReadOnly x As Integer
+        Public ReadOnly y As Integer
+
+        Public Sub New(x As Integer, y As Integer)
+            Me.x = x
+            Me.y = y
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return $"size[x:{x}, y:{y}]"
+        End Function
+
+        Public Overridable Function divide(scaleSize As Size) As Size
+            Dim x As Integer = Me.x / scaleSize.x
+            Dim y As Integer = Me.y / scaleSize.y
+
+            If x * scaleSize.x <> Me.x OrElse y * scaleSize.y <> Me.y Then
+                Call $"{Me.ToString} is not matched with scale {scaleSize.ToString}?".Warning
+            End If
+
+            Return New Size(x, y)
+        End Function
+
+        Public Overridable Function subtract(size As Size, append As Integer) As Size
+            Dim x = Me.x - size.x + append
+            Dim y = Me.y - size.y + append
+
+            Return New Size(x, y)
+        End Function
+    End Class
+End Namespace
