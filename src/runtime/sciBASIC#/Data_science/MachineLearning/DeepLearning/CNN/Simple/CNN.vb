f16d65dcef0348c9e7849682c7361222a255adb8
guigang xie <xie.guigang@gcmodeller.org>
2023-08-24 12:28:15 +0800

    folder renames

---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/DeepLearning/CNN/Simple/CNN.vb
@@ -0,0 +1,319 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.MachineLearning.CNN.Util
+Imports Microsoft.VisualBasic.MachineLearning.ComponentModel.Activations
+Imports Microsoft.VisualBasic.MachineLearning.ComponentModel.StoreProcedure
+Imports layerTypes = Microsoft.VisualBasic.MachineLearning.Convolutional.LayerTypes
+
+Namespace CNN
+
+    Public Class CNN
+
+        Protected Friend ALPHA As Double = 0.85
+        Protected Friend LAMBDA As Double = 0
+
+        Private layers As List(Of Layer)
+        Private divide_batchSize As [Operator]
+        Private multiply_alpha As [Operator]
+        Private multiply_lambda As [Operator]
+
+        Dim log As Action(Of String) = AddressOf VBDebugger.EchoLine
+
+        Public ReadOnly Property batchSize As Integer
+        Public ReadOnly Property layerNum As Integer
+
+        Default Public ReadOnly Property Layer(i As Integer) As Layer
+            Get
+                Return layers(i)
+            End Get
+        End Property
+
+        Public Sub New(layerBuilder As LayerBuilder, batchSize As Integer)
+            Me.layers = layerBuilder
+            Me.layerNum = layers.Count
+            Me.batchSize = batchSize
+
+            Call setup(batchSize)
+            Call initPerator()
+        End Sub
+
+        Private Sub initPerator()
+            divide_batchSize = Function(value) value / _batchSize
+            multiply_alpha = Function(value) value * ALPHA
+            multiply_lambda = Function(value) value * (1 - LAMBDA * ALPHA)
+        End Sub
+
+        Public Function predict(record As SampleData) As Double()
+            Dim outputLayer = layers(layerNum - 1)
+            Dim mapNum = outputLayer.OutMapNum
+            Dim out = New Double(mapNum - 1) {}
+            Dim outmap As Double()()
+
+            Call forward(record)
+
+            For m = 0 To mapNum - 1
+                outmap = outputLayer.getMap(m)
+                out(m) = outmap(0)(0)
+            Next
+
+            Return out
+        End Function
+
+        Friend Function train(record As SampleData) As Boolean
+            forward(record)
+            Return backPropagation(record)
+        End Function
+
+        Private Function backPropagation(record As SampleData) As Boolean
+            Dim result = setOutLayerErrors(record)
+            setHiddenLayerErrors()
+            Return result
+        End Function
+
+        Friend Sub updateParas()
+            For l = 1 To layerNum - 1
+                Dim layer = layers(l)
+                Dim lastLayer = layers(l - 1)
+                Select Case layer.Type
+                    Case layerTypes.Convolution, layerTypes.Output
+                        updateKernels(layer, lastLayer)
+                        updateBias(layer, lastLayer)
+                    Case Else
+                End Select
+            Next
+        End Sub
+
+        Const start = 0
+
+        Private Sub updateBias(layer As Layer, lastLayer As Layer)
+            Dim errors = layer.Errors
+            Dim mapNum = layer.OutMapNum
+
+            For j = start To mapNum - 1
+                Dim [error] = Util.sum(errors, j)
+                Dim deltaBias = Util.sum([error]) / _batchSize
+                Dim bias = layer.getBias(j) + ALPHA * deltaBias
+
+                layer.setBias(j, bias)
+            Next
+        End Sub
+
+        Private Sub updateKernels(layer As Layer, lastLayer As Layer)
+            Dim mapNum = layer.OutMapNum
+            Dim lastMapNum = lastLayer.OutMapNum
+
+            For j = start To mapNum - 1
+                For i = 0 To lastMapNum - 1
+                    Dim deltaKernel As Double()() = Nothing
+
+                    For r = 0 To _batchSize - 1
+                        Dim [error] = layer.getError(r, j)
+                        If deltaKernel Is Nothing Then
+                            deltaKernel = Util.convnValid(lastLayer.getMap(r, i), [error])
+                        Else
+                            deltaKernel = Util.matrixOp(Util.convnValid(lastLayer.getMap(r, i), [error]), deltaKernel, Nothing, Nothing, Util.plus)
+                        End If
+                    Next
+
+                    deltaKernel = Util.matrixOp(deltaKernel, divide_batchSize)
+
+                    Dim kernel = layer.getKernel(i, j)
+                    deltaKernel = Util.matrixOp(kernel, deltaKernel, multiply_lambda, multiply_alpha, Util.plus)
+                    layer.setKernel(i, j, deltaKernel)
+                Next
+            Next
+
+        End Sub
+
+        Private Sub setHiddenLayerErrors()
+            For l = layerNum - 2 To 1 Step -1
+                Dim layer = layers(l)
+                Dim nextLayer = layers(l + 1)
+                Select Case layer.Type
+                    Case layerTypes.samp
+                        setSampErrors(layer, nextLayer)
+                    Case layerTypes.Convolution
+                        setConvErrors(layer, nextLayer)
+                    Case Else
+                End Select
+            Next
+        End Sub
+
+        Private Sub setSampErrors(layer As Layer, nextLayer As Layer)
+            Dim mapNum = layer.OutMapNum
+            Dim nextMapNum = nextLayer.OutMapNum
+
+            For i = start To mapNum - 1
+                Dim sum As Double()() = Nothing
+                For j = 0 To nextMapNum - 1
+                    Dim nextError = nextLayer.getError(j)
+                    Dim kernel = nextLayer.getKernel(i, j)
+
+                    If sum Is Nothing Then
+                        sum = Util.convnFull(nextError, Util.rot180(kernel))
+                    Else
+                        sum = Util.matrixOp(Util.convnFull(nextError, Util.rot180(kernel)), sum, Nothing, Nothing, Util.plus)
+                    End If
+                Next
+                layer.setError(i, sum)
+            Next
+
+        End Sub
+
+        Private Sub setConvErrors(layer As Layer, nextLayer As Layer)
+            Dim mapNum = layer.OutMapNum
+
+            For m = start To mapNum - 1
+                Dim scale = nextLayer.ScaleSize
+                Dim nextError = nextLayer.getError(m)
+                Dim map = layer.getMap(m)
+                Dim outMatrix = Util.matrixOp(map, Util.cloneMatrix(map), Nothing, Util.one_value, Util.multiply)
+                outMatrix = Util.matrixOp(outMatrix, Util.kronecker(nextError, scale), Nothing, Nothing, Util.multiply)
+                layer.setError(m, outMatrix)
+            Next
+
+        End Sub
+
+        Private Function setOutLayerErrors(record As SampleData) As Boolean
+            Dim outputLayer = layers(layerNum - 1)
+            Dim mapNum = outputLayer.OutMapNum
+            Dim target = New Double(mapNum - 1) {}
+            Dim outmaps = New Double(mapNum - 1) {}
+            For m = 0 To mapNum - 1
+                Dim outmap = outputLayer.getMap(m)
+                outmaps(m) = outmap(0)(0)
+
+            Next
+            Dim lable As Integer = record.labels(0)
+            target(lable) = 1
+
+            For m = 0 To mapNum - 1
+                outputLayer.setError(m, 0, 0, outmaps(m) * (1 - outmaps(m)) * (target(m) - outmaps(m)))
+            Next
+            Return lable = which.Max(outmaps)
+        End Function
+
+        Private Sub forward(record As SampleData)
+            Call setInLayerOutput(record)
+
+            For l = 1 To layers.Count - 1
+                Dim layer = layers(l)
+                Dim lastLayer = layers(l - 1)
+                Select Case layer.Type
+                    Case layerTypes.Convolution
+                        setConvOutput(layer, lastLayer)
+                    Case layerTypes.samp
+                        setSampOutput(layer, lastLayer)
+                    Case layerTypes.Output
+                        setConvOutput(layer, lastLayer)
+                    Case Else
+                End Select
+            Next
+        End Sub
+
+        Private Sub setInLayerOutput(value As SampleData)
+            Dim inputLayer = layers(0)
+            Dim mapSize = inputLayer.MapSize
+            Dim attr = value.features
+            If attr.Length <> mapSize.x * mapSize.y Then
+                Throw New Exception()
+            End If
+            For i = 0 To mapSize.x - 1
+                For j = 0 To mapSize.y - 1
+                    inputLayer.setMapValue(0, i, j, attr(mapSize.x * i + j))
+                Next
+            Next
+        End Sub
+
+        Private Sub setConvOutput(layer As Layer, lastLayer As Layer)
+            Dim mapNum = layer.OutMapNum
+            Dim lastMapNum = lastLayer.OutMapNum
+
+            For j = start To mapNum - 1
+                Dim sum As Double()() = Nothing
+                For i = 0 To lastMapNum - 1
+                    Dim lastMap = lastLayer.getMap(i)
+                    Dim kernel = layer.getKernel(i, j)
+                    If sum Is Nothing Then
+                        sum = Util.convnValid(lastMap, kernel)
+                    Else
+                        sum = Util.matrixOp(Util.convnValid(lastMap, kernel), sum, Nothing, Nothing, Util.plus)
+                    End If
+                Next
+
+                Dim bias = layer.getBias(j)
+
+                sum = Util.matrixOp(sum, Function(value) Sigmoid.doCall(value + bias))
+                layer.setMapValue(j, sum)
+            Next
+        End Sub
+
+        Private Sub setSampOutput(layer As Layer, lastLayer As Layer)
+            Dim lastMapNum = lastLayer.OutMapNum
+
+            For i = start To lastMapNum - 1
+                Dim lastMap = lastLayer.getMap(i)
+                Dim scaleSize = layer.ScaleSize
+                Dim sampMatrix = Util.scaleMatrix(lastMap, scaleSize)
+                layer.setMapValue(i, sampMatrix)
+            Next
+        End Sub
+
+        Public Overridable Sub setup(batchSize As Integer)
+            Dim inputLayer = layers(0)
+
+            inputLayer.initOutmaps(batchSize)
+            For i = 1 To layers.Count - 1
+                Dim layer = layers(i)
+                Dim frontLayer = layers(i - 1)
+                Dim frontMapNum = frontLayer.OutMapNum
+                Select Case layer.Type
+                    Case layerTypes.Input
+                    Case layerTypes.Convolution
+                        layer.MapSize = frontLayer.MapSize.subtract(layer.KernelSize, 1)
+                        layer.initKernel(frontMapNum)
+                        layer.initBias(frontMapNum)
+                        layer.initErros(batchSize)
+                        layer.initOutmaps(batchSize)
+                    Case layerTypes.samp
+                        layer.OutMapNum = frontMapNum
+                        layer.MapSize = frontLayer.MapSize.divide(layer.ScaleSize)
+                        layer.initErros(batchSize)
+                        layer.initOutmaps(batchSize)
+                    Case layerTypes.Output
+                        layer.initOutputKerkel(frontMapNum, frontLayer.MapSize)
+                        layer.initBias(frontMapNum)
+                        layer.initErros(batchSize)
+                        layer.initOutmaps(batchSize)
+                End Select
+            Next
+        End Sub
+
+        Public Overridable Sub saveModel(fileName As String)
+            'ObjectOutputStream oos = new ObjectOutputStream(new System.IO.FileStream(fileName, System.IO.FileMode.Create, System.IO.FileAccess.Write));
+            'oos.writeObject(this);
+            'oos.flush();
+            'oos.close();
+            Try
+            Catch e As IOException
+                Console.WriteLine(e.ToString())
+                Console.Write(e.StackTrace)
+            End Try
+
+        End Sub
+
+        Public Shared Function loadModel(fileName As String) As CNN
+            'ObjectInputStream @in = new ObjectInputStream(new System.IO.FileStream(fileName, System.IO.FileMode.Open, System.IO.FileAccess.Read));
+            'CNN cnn = (CNN) @in.readObject();
+            '@in.close();
+            'return cnn;
+            Try
+            Catch e As Exception
+                'e.printStackTrace();
+            End Try
+            Return Nothing
+        End Function
+    End Class
+
+End Namespace
---
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/DeepLearning/CNN/Simple/Layer.vb
@@ -0,0 +1,145 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.MachineLearning.Convolutional
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+
+Namespace CNN
+
+    Public Class Layer
+
+        Private m_kernel As Double()()()()
+        Private bias As Double()
+        Private outmaps As Double()()()()
+        Private m_errors As Double()()()()
+
+        Private Shared recordInBatch As Integer = 0
+
+        Public Overridable Property MapSize As Size
+        Public Overridable ReadOnly Property Type As LayerTypes
+        Public Overridable Property OutMapNum As Integer
+        Public Overridable ReadOnly Property KernelSize As Size
+        Public Overridable ReadOnly Property ScaleSize As Size
+
+        Public Overridable ReadOnly Property Maps As Double()()()()
+            Get
+                Return outmaps
+            End Get
+        End Property
+
+        Public Overridable ReadOnly Property Errors As Double()()()()
+            Get
+                Return m_errors
+            End Get
+        End Property
+
+        Public Overridable ReadOnly Property ClassNum As Integer = -1
+
+        Public Overridable ReadOnly Property Kernel As Double()()()()
+            Get
+                Return m_kernel
+            End Get
+        End Property
+
+        Private Sub New()
+
+        End Sub
+
+        Public Shared Sub prepareForNewBatch()
+            recordInBatch = 0
+        End Sub
+
+        Public Shared Sub prepareForNewRecord()
+            recordInBatch += 1
+        End Sub
+
+        Public Overridable Sub initKernel(frontMapNum As Integer)
+            m_kernel = RectangularArray.CubicMatrix(Of Double)(frontMapNum, _OutMapNum, _KernelSize.x, _KernelSize.y)
+
+            For i = 0 To frontMapNum - 1
+                For j = 0 To _OutMapNum - 1
+                    m_kernel(i)(j) = Util.randomMatrix(_KernelSize.x, _KernelSize.y, True)
+                Next
+            Next
+        End Sub
+
+        Public Overridable Sub initOutputKerkel(frontMapNum As Integer, size As Size)
+            _KernelSize = size
+            m_kernel = RectangularArray.CubicMatrix(Of Double)(frontMapNum, _OutMapNum, _KernelSize.x, _KernelSize.y)
+
+            For i = 0 To frontMapNum - 1
+                For j = 0 To _OutMapNum - 1
+                    m_kernel(i)(j) = Util.randomMatrix(_KernelSize.x, _KernelSize.y, False)
+                Next
+            Next
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overridable Sub initBias(frontMapNum As Integer)
+            bias = Vector.Zero(_OutMapNum)
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overridable Sub initOutmaps(batchSize As Integer)
+            outmaps = RectangularArray.CubicMatrix(Of Double)(batchSize, _OutMapNum, _MapSize.x, _MapSize.y)
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overridable Sub setMapValue(mapNo As Integer, mapX As Integer, mapY As Integer, value As Double)
+            outmaps(recordInBatch)(mapNo)(mapX)(mapY) = value
+        End Sub
+
+        Friend Shared count As Integer = 0
+
+        Public Overridable Sub setMapValue(mapNo As Integer, outMatrix As Double()())
+            ' Log.i(type.toString());
+            ' Util.printMatrix(outMatrix);
+            outmaps(recordInBatch)(mapNo) = outMatrix
+        End Sub
+
+        Public Overridable Function getMap(index As Integer) As Double()()
+            Return outmaps(recordInBatch)(index)
+        End Function
+
+        Public Overridable Function getKernel(i As Integer, j As Integer) As Double()()
+            Return m_kernel(i)(j)
+        End Function
+
+        Public Overridable Sub setError(mapNo As Integer, mapX As Integer, mapY As Integer, value As Double)
+            m_errors(recordInBatch)(mapNo)(mapX)(mapY) = value
+        End Sub
+
+        Public Overridable Sub setError(mapNo As Integer, matrix As Double()())
+            ' Log.i(type.toString());
+            ' Util.printMatrix(matrix);
+            m_errors(recordInBatch)(mapNo) = matrix
+        End Sub
+
+        Public Overridable Function getError(mapNo As Integer) As Double()()
+            Return m_errors(recordInBatch)(mapNo)
+        End Function
+
+        Public Overridable Sub initErros(batchSize As Integer)
+            m_errors = RectangularArray.CubicMatrix(Of Double)(batchSize, _OutMapNum, _MapSize.x, _MapSize.y)
+        End Sub
+
+        Public Overridable Sub setKernel(lastMapNo As Integer, mapNo As Integer, kernel As Double()())
+            m_kernel(lastMapNo)(mapNo) = kernel
+        End Sub
+
+        Public Overridable Function getBias(mapNo As Integer) As Double
+            Return bias(mapNo)
+        End Function
+
+        Public Overridable Sub setBias(mapNo As Integer, value As Double)
+            bias(mapNo) = value
+        End Sub
+
+        Public Overridable Function getError(recordId As Integer, mapNo As Integer) As Double()()
+            Return m_errors(recordId)(mapNo)
+        End Function
+
+        Public Overridable Function getMap(recordId As Integer, mapNo As Integer) As Double()()
+            Return outmaps(recordId)(mapNo)
+        End Function
+    End Class
+End Namespace
