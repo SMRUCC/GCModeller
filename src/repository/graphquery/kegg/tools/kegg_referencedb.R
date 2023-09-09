bfc6ba73543d0e18026cc4913ba315206847eb77
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-09-10 02:23:39 +0800

    enable cache

--- src/repository/graphquery/kegg/tools/kegg_referencedb.R
+++ src/repository/graphquery/kegg/tools/kegg_referencedb.R
@@ -6,11 +6,18 @@ imports "brite" from "kegg_kit";
 let brite = brite::parse("br08901");
 let df = as.data.frame(brite);
  
+const cache_dir = `${@dir}/cache.db`
+|> HDS::openStream(allowCreate = TRUE)
+|> http::http.cache()
+;
+
 print(df, max.print = 6);
 
 for(map in as.list(df, byrow = TRUE)) {
-    let pwy = kegg_api::kegg_pathway(`map${map$entry}`);
+    let pwy = kegg_api::kegg_pathway(`map${map$entry}`, cache = cache_dir);
 
     str(pwy);
     str(map);
+
+    stop();
 }
