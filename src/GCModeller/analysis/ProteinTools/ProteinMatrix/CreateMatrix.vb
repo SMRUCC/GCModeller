54383f6c13b6dd2e62eefb9adb2bf3e660ee738b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-09-10 15:33:27 +0800

    use the SGT tool?

---
+++ src/GCModeller/analysis/ProteinTools/ProteinMatrix/CreateMatrix.vb
@@ -0,0 +1,11 @@
+Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.SequenceModel.FASTA
+
+Public Module CreateMatrix
+
+    <Extension>
+    Public Function ToMatrix(prot As FastaSeq) As Double()()
+
+    End Function
+
+End Module
