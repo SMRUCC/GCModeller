a34db03ba49587e67f06dd8385bfe75a1d74f8d1
guigang xie <xie.guigang@gcmodeller.org>
2023-09-14 13:51:33 +0800

    split a large source function

--- src/GCModeller/models/Networks/KEGG/KEGGMap/PathwayMapNetwork.vb
+++ src/GCModeller/models/Networks/KEGG/KEGGMap/PathwayMapNetwork.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::2a33acef2a089ba5728ea2ae75ab2bc7, GCModeller\models\Networks\KEGG\KEGGMap\PathwayMapNetwork.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 87
-    '    Code Lines: 63
-    ' Comment Lines: 12
-    '   Blank Lines: 12
-    '     File Size: 3.33 KB
+' Summaries:
 
 
-    ' Module PathwayMapNetwork
-    ' 
-    '     Function: BuildModel
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 87
+'    Code Lines: 63
+' Comment Lines: 12
+'   Blank Lines: 12
+'     File Size: 3.33 KB
+
+
+' Module PathwayMapNetwork
+' 
+'     Function: BuildModel
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -75,58 +75,13 @@ Public Module PathwayMapNetwork
     ''' <returns></returns>
     Public Function BuildModel(br08901 As String) As NetworkGraph
         Dim g As New NetworkGraph
-        Dim node As Node
-        Dim nodeData As NodeData, edgeData As EdgeData
-        Dim pathwayMap As PathwayMap
 
         For Each xml As String In ls - l - r - "*.XML" <= br08901
-            pathwayMap = xml.LoadXml(Of PathwayMap)
-            ' 直接使用name作为键名会和cytoscape网络模型之中的name产生冲突
-            ' 所以下面的节点属性中
-            ' 使用pathway.name来存储代谢途径的名称
-            nodeData = New NodeData With {
-                .origID = pathwayMap.EntryId,
-                .label = pathwayMap.name,
-                .Properties = New Dictionary(Of String, String) From {
-                    {NamesOf.REFLECTION_ID_MAPPING_NODETYPE, pathwayMap.brite?.class},
-                    {"KO", pathwayMap.KEGGOrthology _
-                        .Terms _
-                        .SafeQuery _
-                        .Select(Function(x) x.name) _
-                        .JoinBy(PathwayMapNetwork.delimiter)},
-                    {"KO.counts", pathwayMap.KEGGOrthology.size}
-                }
-            }
-            node = New Node(pathwayMap.EntryId, nodeData)
-
-            Call g.AddNode(node, assignId:=True)
+            Call processPathwayNode(g, pathwayMap:=xml.LoadXml(Of PathwayMap))
         Next
 
         For Each a As Node In g.vertex
-            Dim KO As Index(Of String) = Strings.Split(a.data!KO, delimiter).Indexing
-
-            For Each b As Node In g.vertex.Where(Function(vi) vi.ID <> a.ID)
-                Dim kb = Strings.Split(b.data!KO, delimiter)
-                Dim n = kb.Where(Function(id) KO(id) > -1).AsList
-                Dim type$
-
-                If a.data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE) = b.data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE) Then
-                    type = "module internal"
-                Else
-                    type = "module outbounds"
-                End If
-
-                If Not n = 0 Then
-                    edgeData = New EdgeData With {
-                        .length = n.Count,
-                        .Properties = New Dictionary(Of String, String) From {
-                            {"intersets", n.JoinBy(delimiter)},
-                            {NamesOf.REFLECTION_ID_MAPPING_INTERACTION_TYPE, type}
-                        }
-                    }
-                    g.CreateEdge(g.GetElementByID(a.label), g.GetElementByID(b.label), n.Count, edgeData)
-                End If
-            Next
+            Call processPathwayEdgeLinks(a, g)
         Next
 
         Dim edges = g.graphEdges.ToArray
@@ -141,4 +96,59 @@ Public Module PathwayMapNetwork
 
         Return g
     End Function
+
+    Private Sub processPathwayEdgeLinks(a As Node, g As NetworkGraph)
+        Dim edgeData As EdgeData
+        Dim KO As Index(Of String) = Strings.Split(a.data!KO, delimiter).Indexing
+
+        For Each b As Node In g.vertex.Where(Function(vi) vi.ID <> a.ID)
+            Dim kb = Strings.Split(b.data!KO, delimiter)
+            Dim n = kb.Where(Function(id) KO(id) > -1).AsList
+            Dim type$
+
+            If a.data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE) = b.data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE) Then
+                type = "module internal"
+            Else
+                type = "module outbounds"
+            End If
+
+            If Not n = 0 Then
+                edgeData = New EdgeData With {
+                    .length = n.Count,
+                    .Properties = New Dictionary(Of String, String) From {
+                        {"intersets", n.JoinBy(delimiter)},
+                        {NamesOf.REFLECTION_ID_MAPPING_INTERACTION_TYPE, type}
+                    }
+                }
+                g.CreateEdge(
+                    u:=g.GetElementByID(a.label),
+                    v:=g.GetElementByID(b.label),
+                    weight:=n.Count,
+                    data:=edgeData
+                )
+            End If
+        Next
+    End Sub
+
+    Private Sub processPathwayNode(g As NetworkGraph, pathwayMap As PathwayMap)
+        ' 直接使用name作为键名会和cytoscape网络模型之中的name产生冲突
+        ' 所以下面的节点属性中
+        ' 使用pathway.name来存储代谢途径的名称
+        Dim nodeData = New NodeData With {
+            .origID = pathwayMap.EntryId,
+            .label = pathwayMap.name,
+            .Properties = New Dictionary(Of String, String) From {
+                {NamesOf.REFLECTION_ID_MAPPING_NODETYPE, pathwayMap.brite?.class},
+                {"KO", pathwayMap.KEGGOrthology _
+                    .Terms _
+                    .SafeQuery _
+                    .Select(Function(x) x.name) _
+                    .JoinBy(PathwayMapNetwork.delimiter)},
+                {"KO.counts", pathwayMap.KEGGOrthology.size}
+            }
+        }
+        Dim node = New Node(pathwayMap.EntryId, nodeData)
+
+        Call g.AddNode(node, assignId:=True)
+    End Sub
 End Module
