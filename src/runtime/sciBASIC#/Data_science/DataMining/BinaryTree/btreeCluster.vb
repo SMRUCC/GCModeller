1650f704fdaa6d0afe9aeba27583026fce4525c8
xieguigang <xie.guigang@gcmodeller.org>
2023-09-21 08:33:42 +0800

    how to conver result to matrix?

--- src/runtime/sciBASIC#/Data_science/DataMining/BinaryTree/btreeCluster.vb
+++ src/runtime/sciBASIC#/Data_science/DataMining/BinaryTree/btreeCluster.vb
@@ -1,59 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::f318b146e4efcd307636f7c99f1b62d0, sciBASIC#\Data_science\DataMining\BinaryTree\btreeCluster.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 37
-    '    Code Lines: 26
-    ' Comment Lines: 4
-    '   Blank Lines: 7
-    '     File Size: 1.27 KB
+' Summaries:
 
 
-    ' Class BTreeCluster
-    ' 
-    '     Properties: left, members, right, uuid
-    ' 
-    '     Function: (+2 Overloads) GetClusters, ToString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 37
+'    Code Lines: 26
+' Comment Lines: 4
+'   Blank Lines: 7
+'     File Size: 1.27 KB
+
+
+' Class BTreeCluster
+' 
+'     Properties: left, members, right, uuid
+' 
+'     Function: (+2 Overloads) GetClusters, ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Algorithm.BinaryTree
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 
+''' <summary>
+''' Clustering data via binary tree
+''' </summary>
 Public Class BTreeCluster : Implements INamedValue
 
     Public Property left As BTreeCluster
@@ -66,11 +70,13 @@ Public Class BTreeCluster : Implements INamedValue
     ''' </summary>
     ''' <returns></returns>
     Public Property members As String()
+    Public Property data As Dictionary(Of String, Object)
 
     Public Overrides Function ToString() As String
         Return $"[{uuid}] ({members.Length} members) {members.JoinBy(", ")}"
     End Function
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Shared Function GetClusters(btree As AVLTree(Of String, String)) As BTreeCluster
         Return GetClusters(btree.root)
     End Function
--- src/runtime/sciBASIC#/Data_science/Mathematica/Math/DataFrame/DataMatrix.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/DataFrame/DataMatrix.vb
@@ -1,59 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::6367c5bc07bb8fb1ea90ad1578b00851, sciBASIC#\Data_science\Mathematica\Math\DataFrame\DataMatrix.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 143
-    '    Code Lines: 111
-    ' Comment Lines: 9
-    '   Blank Lines: 23
-    '     File Size: 4.88 KB
+' Summaries:
 
 
-    ' Class DataMatrix
-    ' 
-    '     Properties: keys, size
-    ' 
-    '     Constructor: (+3 Overloads) Sub New
-    '     Function: GetVector, PopulateRowEntitys, PopulateRowObjects, PopulateRows, ToString
-    '               Visit
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 143
+'    Code Lines: 111
+' Comment Lines: 9
+'   Blank Lines: 23
+'     File Size: 4.88 KB
+
+
+' Class DataMatrix
+' 
+'     Properties: keys, size
+' 
+'     Constructor: (+3 Overloads) Sub New
+'     Function: GetVector, PopulateRowEntitys, PopulateRowObjects, PopulateRows, ToString
+'               Visit
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Data
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
@@ -160,6 +161,11 @@ Public Class DataMatrix : Implements IBucketVector
                                   End Function)
             }
         Next
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function GetVector(name As String) As Double()
+        Return names.Objects.Select(Function(a) Me(a, name)).ToArray
     End Function
 
     Public Iterator Function PopulateRowEntitys(Of DataSet As {New, INamedValue, DynamicPropertyBase(Of Double)})(propertyNames As String()) As IEnumerable(Of DataSet)
