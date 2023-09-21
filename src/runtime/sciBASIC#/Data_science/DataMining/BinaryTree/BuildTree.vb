e95010101f415cd28bd7d2aa9be9fe5fabf406c3
xieguigang <xie.guigang@gcmodeller.org>
2023-09-21 08:45:09 +0800

    make improvements of the tree buiulder

--- src/runtime/sciBASIC#/Data_science/DataMining/BinaryTree/BuildTree.vb
+++ src/runtime/sciBASIC#/Data_science/DataMining/BinaryTree/BuildTree.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::47c2d8ea59928d4307e38d10c2f05d73, sciBASIC#\Data_science\DataMining\BinaryTree\BuildTree.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 53
-    '    Code Lines: 43
-    ' Comment Lines: 0
-    '   Blank Lines: 10
-    '     File Size: 2.33 KB
+' Summaries:
 
 
-    ' Module BuildTree
-    ' 
-    '     Function: (+3 Overloads) BTreeCluster
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 53
+'    Code Lines: 43
+' Comment Lines: 0
+'   Blank Lines: 10
+'     File Size: 2.33 KB
+
+
+' Module BuildTree
+' 
+'     Function: (+3 Overloads) BTreeCluster
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -64,13 +64,14 @@ Public Module BuildTree
                                  Optional equals As Double = 0.9,
                                  Optional gt As Double = 0.7) As BTreeCluster
 
-        Dim btree As New AVLTree(Of String, String)(New Comparison(d, equals, gt).GetComparer, Function(str) str)
+        Dim align As New Comparison(d, equals, gt)
+        Dim btree As New AVLTree(Of String, String)(align.GetComparer, Function(str) str)
 
         For Each id As String In d.keys
             Call btree.Add(id, id, valueReplace:=False)
         Next
 
-        Return BTreeCluster.GetClusters(btree)
+        Return BTreeCluster.GetClusters(btree, align)
     End Function
 
     <Extension>
@@ -87,7 +88,7 @@ Public Module BuildTree
             Call btree.Add(id, id, valueReplace:=False)
         Next
 
-        Return BinaryTree.BTreeCluster.GetClusters(btree)
+        Return BinaryTree.BTreeCluster.GetClusters(btree, compares)
     End Function
 
     <Extension>
@@ -109,7 +110,7 @@ Public Module BuildTree
             Call btree.Add(id, id, valueReplace:=False)
         Next
 
-        Return BTreeCluster.GetClusters(btree)
+        Return BTreeCluster.GetClusters(btree, compares)
     End Function
 
     <Extension>
@@ -120,6 +121,6 @@ Public Module BuildTree
             Call btree.Add(id, id, valueReplace:=False)
         Next
 
-        Return BTreeCluster.GetClusters(btree)
+        Return BTreeCluster.GetClusters(btree, alignment)
     End Function
 End Module
