1412994d2d39a068802b2a2892cad2e4e72a87b9
谢桂纲 <gg.xie@bionovogene.com>
2023-10-16 15:19:23 +0800

    fix of the namespace imports error

--- src/GCModeller/models/Networks/KEGGGraphVisualizer/Styles/RenderStyle.vb
+++ src/GCModeller/models/Networks/KEGGGraphVisualizer/Styles/RenderStyle.vb
@@ -1,63 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::89f63e6405f47e6b468f3b8b5f243a40, GCModeller\models\Networks\KEGG\PathwayMaps\Styles\RenderStyle.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 97
-    '    Code Lines: 80
-    ' Comment Lines: 2
-    '   Blank Lines: 15
-    '     File Size: 4.16 KB
+' Summaries:
 
 
-    '     Class RenderStyle
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: createNodeTable, getNodeLayout
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 97
+'    Code Lines: 80
+' Comment Lines: 2
+'   Blank Lines: 15
+'     File Size: 4.16 KB
+
+
+'     Class RenderStyle
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: createNodeTable, getNodeLayout
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
 Imports System.Drawing.Drawing2D
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
-Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors.Scaler
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.BriteHEntry
 
 Namespace PathwayMaps.RenderStyles
