adcc36403d12b2012eb379ce461c6f8685804026
guigang xie <xie.guigang@gcmodeller.org>
2023-12-07 10:06:51 +0800

    fix of the symbol reference error

--- src/repository/graphquery/kegg/test/request_list.R
+++ src/repository/graphquery/kegg/test/request_list.R
@@ -10,8 +10,14 @@ let id_list = "./list.json"
 
 options(http.cache_dir = ?"--cache" || `${@dir}/.cache/`);
 
+const kegg_compounds = list();
+
 str(id_list);
 
 for(id in id_list$kegg) {
     str(id);
+    kegg_compounds[[id]] = kegg_compound(id, cache = "./.cache/");
 }
+
+
+repository::write.msgpack(unlist(kegg_compounds), file = "./kegg_parts.msgpack");
