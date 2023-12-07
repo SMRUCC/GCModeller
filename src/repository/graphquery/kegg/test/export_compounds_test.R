02f9e673f3b4b4983b1847b4d461a651c689d84e
guigang xie <xie.guigang@gcmodeller.org>
2023-12-07 10:45:12 +0800

    handling of get elekemntn collection from a group

--- src/repository/graphquery/kegg/test/export_compounds_test.R
+++ src/repository/graphquery/kegg/test/export_compounds_test.R
@@ -17,7 +17,17 @@ file_df = file_df[i, ];
 
 print(file_df);
 
+let unique_compounds = file_df$path;
+unique_compounds = groupBy(unique_compounds, f -> basename(f));
+
+str(unique_compounds);
+
+unique_compounds = sapply(unique_compounds, g -> first(g));
+
+print(unique_compounds);
+print(basename(unique_compounds));
+
 
 let load_compounds = lapply(file_df$path |> take(10), path -> HDS::getText(file, path) |> loadXml(typeof = "kegg_compound"));
 
-str(load_compounds);
+# str(load_compounds);
