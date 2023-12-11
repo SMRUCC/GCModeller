5bd6b5ffc77407005b4f63eebd1b8839c459f9ab
guigang xie <xie.guigang@gcmodeller.org>
2023-12-11 15:49:51 +0800

    make updates of the data files

--- src/GCModeller/analysis/Motifs/List of restriction enzyme cutting sites/update.R
+++ src/GCModeller/analysis/Motifs/List of restriction enzyme cutting sites/update.R
@@ -3,5 +3,29 @@ imports "http" from "webKit";
 setwd(@dir);
 
 let main = http::requests.get("https://en.wikipedia.org/wiki/List_of_restriction_enzyme_cutting_sites") |> http::content();
+let url = $'[/]wiki[/]List_of_restriction_enzyme_cutting_sites[^"]+';
 
-print(main);
+url = `https://en.wikipedia.org${url(main)}`;
+
+str(main);
+print(url);
+
+writeLines(main, con = "./index.txt");
+
+for(file in list.files("./txt")) {
+    file.remove(file);
+}
+
+for(link in url) {
+    const name = basename(link) |> gsub("List_of_restriction_enzyme_cutting_sites:", "");
+
+    print(name);
+    # stop();
+
+    link 
+    |> http::requests.get() 
+    |> http::content()
+    |> writeLines(
+        con = `./txt/${name}.txt`;
+    );
+}
