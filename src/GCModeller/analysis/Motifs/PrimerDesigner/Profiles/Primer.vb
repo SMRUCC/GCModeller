cc7480ff1dec89901abf08fbca3615448239556d
guigang xie <xie.guigang@gcmodeller.org>
2023-12-11 17:12:08 +0800

    split source file into m,ultiple parts

---
+++ src/GCModeller/analysis/Motifs/PrimerDesigner/Profiles/Primer.vb
@@ -0,0 +1,89 @@
+﻿Imports SMRUCC.genomics.ComponentModel.Loci
+Imports SMRUCC.genomics.SequenceModel
+Imports SMRUCC.genomics.SequenceModel.NucleotideModels
+
+Public Class Primer
+
+    Public ReadOnly Property GCDifference As Double
+        Get
+            Return Math.Abs(ForwardSequence.GC - ReversedSequence.GC)
+        End Get
+    End Property
+
+    Public ReadOnly Property TmDifference As Double
+        Get
+            Return Math.Abs(ForwardSequence.Tm - ReversedSequence.Tm)
+        End Get
+    End Property
+
+    Public ReadOnly Property Product As NucleotideLocation
+        Get
+            Return New NucleotideLocation With {
+                .Left = Reversed.right,
+                .Right = Forward.left
+            }
+        End Get
+    End Property
+
+    Public Property Forward As NucleotideLocation
+    Public Property Reversed As NucleotideLocation
+
+    Public Property SenseRestrictedSite As String
+    Public Property AntisenseRestrictedSite As String
+
+    ''' <summary>
+    ''' Fe - Fs
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public ReadOnly Property Alpha As Integer
+        Get
+            Return Forward.Ends - Forward.Start
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Rs - Fe
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public ReadOnly Property Beta As Integer
+        Get
+            Return Reversed.Start - Reversed.Ends
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Re - Rs
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public ReadOnly Property Gamma As Integer
+        Get
+            Return Reversed.Ends - Reversed.Start
+        End Get
+    End Property
+
+    Dim _Template As IPolymerSequenceModel
+    Dim _ReversedTemplate As IPolymerSequenceModel
+
+    Sub New(Template As NucleicAcid)
+        _Template = Template
+        _ReversedTemplate = Template.Complement
+    End Sub
+
+    Public ReadOnly Property ForwardSequence As NucleicAcid
+        Get
+            Return New NucleicAcid(SenseRestrictedSite & _Template.CutSequenceLinear(Forward.Start, Forward.Ends))
+        End Get
+    End Property
+
+    Public ReadOnly Property ReversedSequence As NucleicAcid
+        Get
+            Return New NucleicAcid(AntisenseRestrictedSite & _ReversedTemplate.CutSequenceLinear(Reversed.Start, Reversed.Ends))
+        End Get
+    End Property
+End Class
---
+++ src/GCModeller/analysis/Motifs/PrimerDesigner/Profiles/SearchProfile.vb
@@ -0,0 +1,13 @@
+﻿
+Public Class SearchProfile
+    Public Property MinLength As Integer
+    Public Property MaxLength As Integer
+    Public Property SenseRestricted As String
+    Public Property AntisenseRestricted As String
+    Public Property MinGC As Double
+    Public Property MaxGC As Double
+    Public Property MinTm As Double
+    Public Property MaxTm As Double
+    Public Property DeltaTm As Double
+    Public Property DeltaGC As Double
+End Class
