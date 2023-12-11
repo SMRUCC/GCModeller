375801c245f4c01347bb02494c2c20fa3e031c08
guigang xie <xie.guigang@gcmodeller.org>
2023-12-11 15:57:23 +0800

    parse the txt data via query

---
+++ src/GCModeller/analysis/Motifs/List of restriction enzyme cutting sites/parse_data.R
@@ -0,0 +1,13 @@
+imports ["Html", "graphquery"] from "webKit";
+
+require(JSON);
+
+setwd(@dir);
+
+const query = graphquery::parseQuery(readText("./parse_data.txt"));
+
+for(file in list.files("./txt")) {
+    const document = Html::parse(readText(file));
+    str(graphquery::query(document, query));
+}
+
