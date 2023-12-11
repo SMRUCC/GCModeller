238297b314be67e6ef7003a337b27a6949310edb
guigang xie <xie.guigang@gcmodeller.org>
2023-12-11 16:23:57 +0800

    export result table

--- src/GCModeller/analysis/Motifs/List of restriction enzyme cutting sites/parse_data.R
+++ src/GCModeller/analysis/Motifs/List of restriction enzyme cutting sites/parse_data.R
@@ -1,14 +1,29 @@
-imports ["Html", "graphquery"] from "webKit";
-
-require(JSON);
+imports "Html" from "webKit";
 
 setwd(@dir);
 
-const query = graphquery::parseQuery(readText("./parse_data.txt"));
-
 for(file in list.files("./txt")) {
-    const document = Html::parse(readText(file));
-    print(graphquery::query(document, query));
-    stop();
-}
+    let html = readText(file);
+    let data = Html::tables(html)
+    |> which(d -> ncol(d) > 4)
+    ;
+    let exports = NULL;
 
+    for(par in data) {
+        par = data.frame(
+            enzyme = par$Enzyme, 
+            pdb = par$"PDB code", 
+            organism = par$"Source", 
+            recognition = par$"Recognition sequence", 
+            cut = par$Cut, 
+            isoschizomers = par$Isoschizomers
+        );
+        offset = nrow(exports) + 1;
+        rownames(par) = offset:(offset + nrow(par) - 1);
+        exports = rbind(exports, par);
+    }
+
+    print(exports);
+
+    write.csv(exports, file = `./sites/${basename(file)}.csv`, row.names = FALSE);
+}
