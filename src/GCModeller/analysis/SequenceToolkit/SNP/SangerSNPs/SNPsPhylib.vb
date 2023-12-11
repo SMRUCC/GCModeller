53d2ad3fe492b224fedc4c26e43120639bc30ebd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-11 18:32:53 +0800

    fix of the missing component reference

--- src/GCModeller/analysis/SequenceToolkit/SNP/SangerSNPs/SNPsPhylib.vb
+++ src/GCModeller/analysis/SequenceToolkit/SNP/SangerSNPs/SNPsPhylib.vb
@@ -1,68 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::15019a0e6e7e374fd2476a5667a2e378, GCModeller\analysis\SequenceToolkit\SNP\SangerSNPs\SNPsPhylib.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 108
-    '    Code Lines: 59
-    ' Comment Lines: 30
-    '   Blank Lines: 19
-    '     File Size: 4.19 KB
+' Summaries:
 
 
-    '     Module SNPsPhylib
-    ' 
-    '         Function: PhylibOfSNPSites
-    ' 
-    '     Structure Phylip
-    ' 
-    '         Function: Doc, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 108
+'    Code Lines: 59
+' Comment Lines: 30
+'   Blank Lines: 19
+'     File Size: 4.19 KB
+
+
+'     Module SNPsPhylib
+' 
+'         Function: PhylibOfSNPSites
+' 
+'     Structure Phylip
+' 
+'         Function: Doc, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.IO
 Imports System.Text
-Imports Microsoft.VisualBasic
 Imports Microsoft.VisualBasic.Language
-Imports Microsoft.VisualBasic.Linq
-Imports Microsoft.VisualBasic.Serialization
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.genomics.SequenceModel.FASTA
-Imports FILE = System.IO.StreamWriter
 
 Namespace SangerSNPs
 
