9cfa8c9083473e9f0e81f4701537babd0aa701c5
guigang xie <xie.guigang@gcmodeller.org>
2023-12-12 09:22:46 +0800

    load test nt seq

--- src/GCModeller/analysis/Motifs/PrimerDesigner/test/Program.vb
+++ src/GCModeller/analysis/Motifs/PrimerDesigner/test/Program.vb
@@ -1,8 +1,11 @@
 Imports SMRUCC.genomics.Analysis.PrimerDesigner.Restriction_enzyme
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank
+Imports SMRUCC.genomics.SequenceModel.FASTA
 
 Module Program
     Sub Main(args As String())
         Dim list = WikiLoader.PullAll.ToArray
+        Dim plasmid As FastaSeq = GBFF.File.Load("D:\GCModeller\src\GCModeller\visualize\data\addgene-plasmid-100854-sequence-189713.gbk").Origin.ToFasta
 
         Pause()
     End Sub
