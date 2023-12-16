9647470f0aac76d39f53503c460f64ce5cc85fde
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-16 21:37:26 +0800

    make code style improvements

--- src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/SignalProcessing/COW/FunctionMatrix.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/SignalProcessing/COW/FunctionMatrix.vb
@@ -1,15 +1,15 @@
 ﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
-Imports Microsoft.VisualBasic.Parallel
 
 Namespace COW
 
     ''' <summary>
     ''' This class is used in dynamic programming algorithm of CowAlignment.cs
     ''' </summary>
-    Friend Class FunctionMatrix
+    Friend Class FunctionMatrix : Implements IDisposable
 
         Dim funcBeanMatrix As FunctionElement()()
         Dim dims As (row As Integer, col As Integer)
+        Private disposedValue As Boolean
 
         Default Public Property Item(rowPosition As Integer, columnPosition As Integer) As FunctionElement
             Get
@@ -26,7 +26,14 @@ Namespace COW
         End Sub
 
         Public Sub Initialize(segmentNumber As Integer, enabledLength As Integer)
-            Call New InitializeTask(Me).Run()
+            Dim mat = funcBeanMatrix
+
+            For i = 0 To segmentNumber
+                For j = 0 To enabledLength
+                    mat(i)(j) = New FunctionElement(Double.MinValue, 0)
+                Next
+            Next
+
             Me(segmentNumber, enabledLength).Score = 0
         End Sub
 
@@ -34,26 +41,34 @@ Namespace COW
             Return $"[{dims.row} x {dims.col}]"
         End Function
 
-        Private Class InitializeTask : Inherits VectorTask
-
-            ReadOnly mat As FunctionMatrix
-
-            Public Sub New(m As FunctionMatrix)
-                MyBase.New(m.funcBeanMatrix.Length)
-                Me.mat = m
-            End Sub
-
-            Protected Overrides Sub Solve(start As Integer, ends As Integer, cpu_id As Integer)
-                For i As Integer = start To ends
-                    Dim v As FunctionElement() = mat.funcBeanMatrix(i)
-
-                    For j As Integer = 0 To v.Length - 1
-                        v(j) = New FunctionElement(Double.MinValue, 0)
+        Protected Overridable Sub Dispose(disposing As Boolean)
+            If Not disposedValue Then
+                If disposing Then
+                    ' TODO: 释放托管状态(托管对象)
+                    For i As Integer = 0 To funcBeanMatrix.Length - 1
+                        Erase funcBeanMatrix(i)
                     Next
 
-                    mat.funcBeanMatrix(i) = v
-                Next
-            End Sub
-        End Class
+                    Erase funcBeanMatrix
+                End If
+
+                ' TODO: 释放未托管的资源(未托管的对象)并重写终结器
+                ' TODO: 将大型字段设置为 null
+                disposedValue = True
+            End If
+        End Sub
+
+        ' ' TODO: 仅当“Dispose(disposing As Boolean)”拥有用于释放未托管资源的代码时才替代终结器
+        ' Protected Overrides Sub Finalize()
+        '     ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+        '     Dispose(disposing:=False)
+        '     MyBase.Finalize()
+        ' End Sub
+
+        Public Sub Dispose() Implements IDisposable.Dispose
+            ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+            Dispose(disposing:=True)
+            GC.SuppressFinalize(Me)
+        End Sub
     End Class
 End Namespace
