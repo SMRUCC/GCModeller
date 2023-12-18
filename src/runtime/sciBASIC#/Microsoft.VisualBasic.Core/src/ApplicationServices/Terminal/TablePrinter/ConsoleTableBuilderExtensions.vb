b9ca96fa6cbab13ed2e0b0c995c4477050515d39
guigang xie <xie.guigang@gcmodeller.org>
2023-12-18 08:48:19 +0800

    make updates of the signal data processor

--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/ApplicationServices/Terminal/TablePrinter/ConsoleTableBuilderExtensions.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/ApplicationServices/Terminal/TablePrinter/ConsoleTableBuilderExtensions.vb
@@ -59,6 +59,7 @@
 Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal.TablePrinter.Flags
+Imports Microsoft.VisualBasic.Linq
 
 Namespace ApplicationServices.Terminal.TablePrinter
 
@@ -100,7 +101,7 @@ Namespace ApplicationServices.Terminal.TablePrinter
         <Extension()>
         Public Function AddRow(builder As ConsoleTableBuilder, ParamArray rowValues As Object()) As ConsoleTableBuilder
             If rowValues Is Nothing Then Return builder
-            builder.Rows.Add(New List(Of Object)(rowValues))
+            builder.Rows.Add(rowValues)
             Return builder
         End Function
 
@@ -247,7 +248,7 @@ Namespace ApplicationServices.Terminal.TablePrinter
         <Extension()>
         Public Function AddRow(builder As ConsoleTableBuilder, row As List(Of Object)) As ConsoleTableBuilder
             If row Is Nothing Then Return builder
-            builder.Rows.Add(row)
+            builder.Rows.Add(row.ToArray)
             Return builder
         End Function
 
@@ -261,7 +262,7 @@ Namespace ApplicationServices.Terminal.TablePrinter
         <Extension()>
         Public Function AddRow(builder As ConsoleTableBuilder, row As DataRow) As ConsoleTableBuilder
             If row Is Nothing Then Return builder
-            builder.Rows.Add(New List(Of Object)(row.ItemArray))
+            builder.Rows.Add(row.ItemArray)
             Return builder
         End Function
 
@@ -367,7 +368,7 @@ Namespace ApplicationServices.Terminal.TablePrinter
             Dim numberOfColumns = 0
 
             If builder.Rows.Any() Then
-                numberOfColumns = builder.Rows.Max(Function(x) x.Count)
+                numberOfColumns = builder.Rows.Max(Function(x) x.Length)
             Else
 
                 If builder.Column IsNot Nothing Then
@@ -401,12 +402,11 @@ Namespace ApplicationServices.Terminal.TablePrinter
 
             For i = 0 To builder.Rows.Count - 1
 
-                If builder.Rows(i).Count < numberOfColumns Then
-                    Dim missCount = numberOfColumns - builder.Rows(i).Count
+                If builder.Rows(i).Length < numberOfColumns Then
+                    Dim missCount = numberOfColumns - builder.Rows(i).Length
+                    Dim miss As Object() = New Object(missCount - 1) {}
 
-                    For j = 0 To missCount - 1
-                        builder.Rows(i).Add(Nothing)
-                    Next
+                    builder.Rows(i) = builder.Rows(i).JoinIterates(miss).ToArray
                 End If
             Next
 
@@ -433,12 +433,18 @@ Namespace ApplicationServices.Terminal.TablePrinter
                         row = New String(" "c, Console.WindowWidth - lines(i).RealLength(True)) & lines(i)
                 End Select
 
-                If i = 0 AndAlso Not String.IsNullOrEmpty(builder.TableTitle) AndAlso builder.TableTitle.Trim().Length <> 0 AndAlso Not builder.TableTitleColor.IsForegroundColorNull AndAlso builder.TitlePositionStartAt > 0 AndAlso builder.TitlePositionLength > 0 Then
+                If i = 0 AndAlso
+                    Not String.IsNullOrEmpty(builder.TableTitle) AndAlso
+                    builder.TableTitle.Trim().Length <> 0 AndAlso
+                    builder.TableTitleColor.ForegroundColor IsNot Nothing AndAlso
+                    builder.TitlePositionStartAt > 0 AndAlso
+                    builder.TitlePositionLength > 0 Then
+
                     Dim newTitlePositionStartAt = builder.TitlePositionStartAt + (row.Length - lines(i).Length)
                     Console.Write(row.Substring(0, newTitlePositionStartAt))
                     Console.ForegroundColor = builder.TableTitleColor.ForegroundColor
 
-                    If Not builder.TableTitleColor.IsBackgroundColorNull Then
+                    If Not builder.TableTitleColor.BackgroundColor Is Nothing Then
                         Console.BackgroundColor = builder.TableTitleColor.BackgroundColor
                     End If
 
