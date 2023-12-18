f34990f3f5325978bff36c953d0ed5bec94ed987
guigang xie <xie.guigang@gcmodeller.org>
2023-12-18 10:20:53 +0800

    add Dtw code

---
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/SignalProcessing/NDtw/Preprocessing/CentralizationPreprocessor.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace NDtw.Preprocessing
+
+    ''' <summary>
+    ''' f(x) = x - mean
+    ''' </summary>
+    Public Class CentralizationPreprocessor : Implements IPreprocessor
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function Preprocess(data As Double()) As Double() Implements IPreprocessor.Preprocess
+            Return SIMD.Subtract.f64_op_subtract_f64_scalar(data, data.Average)
+        End Function
+
+        Public Overrides Function ToString() As String Implements IPreprocessor.ToString
+            Return "Centralization"
+        End Function
+    End Class
+End Namespace
---
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/SignalProcessing/NDtw/Preprocessing/NonePreprocessor.vb
@@ -0,0 +1,24 @@
+﻿
+Namespace NDtw.Preprocessing
+
+    ''' <summary>
+    ''' signal data processor that do nothing
+    ''' 
+    ''' f(x) = x
+    ''' </summary>
+    Public Class NonePreprocessor : Implements IPreprocessor
+
+        ''' <summary>
+        ''' do nothing at here
+        ''' </summary>
+        ''' <param name="data"></param>
+        ''' <returns></returns>
+        Public Function Preprocess(data As Double()) As Double() Implements IPreprocessor.Preprocess
+            Return data
+        End Function
+
+        Public Overrides Function ToString() As String Implements IPreprocessor.ToString
+            Return "None"
+        End Function
+    End Class
+End Namespace
