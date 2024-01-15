d45e18563ca92817798a8c760502e6629f84785f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-15 14:13:59 +0800

    commit nothing

--- src/workbench/pkg/@export/WGCNA.d.ts
+++ src/workbench/pkg/@export/WGCNA.d.ts
@@ -1,18 +1,13 @@
 ﻿// export R# package module type define for javascript/typescript language
 //
-//    imports "WGCNA" from "phenotype_kit";
 //    imports "WGCNA" from "TRNtoolkit";
 //
-// ref=phenotype_kit.WGCNA@phenotype_kit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 // ref=TRNtoolkit.WGCNA@TRNtoolkit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
  * 
 */
 declare namespace WGCNA {
-   /**
-   */
-   function applyModuleColors(g: object, modules: object): any;
    /**
     * append protein iteration network based on the WGCNA weights.
     * 
@@ -25,17 +20,6 @@ declare namespace WGCNA {
      * + default value Is ``0.3``.
    */
    function interations(g: object, WGCNA: object, modules: object, threshold?: number): any;
-   module read {
-      /**
-        * @param prefix default value Is ``null``.
-      */
-      function modules(file: string, prefix?: string): any;
-      /**
-        * @param threshold default value Is ``0``.
-        * @param prefix default value Is ``null``.
-      */
-      function weightMatrix(file: string, threshold?: number, prefix?: string): object;
-   }
    /**
     * filter regulation network by WGCNA result weights
     * 
