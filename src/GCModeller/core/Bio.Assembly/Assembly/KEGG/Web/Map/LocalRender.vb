f617d417da49cb412df032b801941b88a237619e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-26 14:01:04 +0800

    split source file into multiple parts

--- src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/LocalRender.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/Web/Map/LocalRender.vb
@@ -56,7 +56,6 @@
 #End Region
 
 Imports System.Drawing
-Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Imaging
@@ -69,69 +68,6 @@ Imports Microsoft.VisualBasic.Scripting.Runtime
 Imports SMRUCC.genomics.Assembly.KEGG.WebServices.XML
 
 Namespace Assembly.KEGG.WebServices
-
-    Public Class MapHighlights
-
-        Public Property compounds As NamedValue(Of String)() = {}
-        Public Property genes As NamedValue(Of String)() = {}
-        Public Property proteins As NamedValue(Of String)() = {}
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Function PopulateAllHighlights() As IEnumerable(Of NamedValue(Of String))
-            Return compounds.JoinIterates(genes).JoinIterates(proteins)
-        End Function
-
-        Public Iterator Function GetGeneProteinTuples() As IEnumerable(Of NamedValue(Of (gene_color$, protein_color$)))
-            Dim geneSet = genes.GroupBy(Function(a) a.Name).ToDictionary(Function(a) a.Key, Function(a) a.First.Value)
-            Dim protSet = proteins.GroupBy(Function(a) a.Name).ToDictionary(Function(a) a.Key, Function(a) a.First.Value)
-            Dim unionIdSet As String() = geneSet.Keys.JoinIterates(protSet.Keys).Distinct.ToArray
-
-            For Each id As String In unionIdSet
-                If geneSet.ContainsKey(id) AndAlso protSet.ContainsKey(id) Then
-                    Yield New NamedValue(Of (gene_color As String, protein_color As String))(id, (geneSet(id), protSet(id)))
-                End If
-            Next
-        End Function
-
-        Public Shared Function CreateCompounds(list As IEnumerable(Of NamedValue(Of String))) As MapHighlights
-            Return New MapHighlights With {.compounds = list.ToArray}
-        End Function
-
-        Public Shared Function CreateGenes(list As IEnumerable(Of NamedValue(Of String))) As MapHighlights
-            Return New MapHighlights With {.genes = list.ToArray}
-        End Function
-
-        Public Shared Function CreateProteins(list As IEnumerable(Of NamedValue(Of String))) As MapHighlights
-            Return New MapHighlights With {.proteins = list.ToArray}
-        End Function
-
-        ''' <summary>
-        ''' check highlights automatically via kegg id prefix
-        ''' </summary>
-        ''' <param name="list"></param>
-        ''' <returns></returns>
-        ''' <remarks>
-        ''' can not determine the gene/proteins at here
-        ''' </remarks>
-        Public Shared Function CreateAuto(list As IEnumerable(Of NamedValue(Of String))) As MapHighlights
-            Dim compounds As New List(Of NamedValue(Of String))
-            Dim genes As New List(Of NamedValue(Of String))
-
-            For Each node As NamedValue(Of String) In list
-                If node.Name.IsPattern(KEGGCompoundIDPatterns) Then
-                    compounds.Add(node)
-                Else
-                    genes.Add(node)
-                End If
-            Next
-
-            Return New MapHighlights With {
-                .compounds = compounds.ToArray,
-                .genes = genes.ToArray
-            }
-        End Function
-
-    End Class
 
     ''' <summary>
     ''' KEGG pathway map local rendering engine
