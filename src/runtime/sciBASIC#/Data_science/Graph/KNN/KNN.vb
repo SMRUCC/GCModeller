8a2cc4700a3d5537f4a697f59ad601cd6554e57d
谢桂纲 <gg.xie@bionovogene.com>
2024-01-26 11:15:19 +0800

    move KNN module into graph method base project

---
+++ src/runtime/sciBASIC#/Data_science/Graph/KNN/KNN.vb
@@ -0,0 +1,62 @@
+﻿Imports Microsoft.VisualBasic.Data.GraphTheory.KdTree.ApproximateNearNeighbor
+Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
+
+Namespace KNearNeighbors
+
+    ''' <summary>
+    ''' KNN search handler for phenograph
+    ''' </summary>
+    Public Class KNN
+
+        ReadOnly score As ScoreMetric
+
+        ''' <summary>
+        ''' knn score cutoff
+        ''' </summary>
+        ReadOnly cutoff As Double
+
+        Sub New(metric As ScoreMetric, knn_cutoff As Double)
+            Me.score = metric
+            Me.cutoff = knn_cutoff
+
+            score.cutoff = knn_cutoff
+        End Sub
+
+        ''' <summary>
+        ''' the output keeps the same order as the given input <paramref name="data"/>
+        ''' </summary>
+        ''' <param name="data"></param>
+        ''' <param name="k"></param>
+        ''' <returns></returns>
+        Public Iterator Function FindNeighbors(data As GeneralMatrix, Optional k As Integer = 30) As IEnumerable(Of KNeighbors)
+            Dim matrix = data.PopulateVectors.ToArray
+            Dim knnQuery = matrix _
+                .AsParallel _
+                .Select(Function(v)
+                            Return FindNeighbors(v, matrix, k, score)
+                        End Function) _
+                .ToArray
+
+            For Each nn2 As (TagVector, w As Double)() In knnQuery
+                Dim index As Integer() = nn2.Select(Function(xi) xi.Item1.index).ToArray
+                Dim weights As Double() = nn2.Select(Function(xi) xi.w).ToArray
+
+                Yield New KNeighbors(index.Length, index, weights)
+            Next
+        End Function
+
+        Public Shared Function FindNeighbors(v As TagVector, matrix As TagVector(), k As Integer, score As ScoreMetric) As (TagVector, w As Double)()
+            Dim vec As Double() = v.vector.ToArray
+
+            Return matrix _
+                .Select(Function(i)
+                            Dim w As Double = score.eval(vec, i.vector)
+                            Return (i, w)
+                        End Function) _
+                .Where(Function(a) a.w > score.cutoff) _
+                .OrderByDescending(Function(a) a.w) _
+                .Take(k) _
+                .ToArray
+        End Function
+    End Class
+End Namespace
