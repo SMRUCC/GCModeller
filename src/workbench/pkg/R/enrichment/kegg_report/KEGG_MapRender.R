f96915f468454093d9f2683b22a64d5d5856621b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-26 14:14:02 +0800

    source file script renames

--- src/workbench/pkg/R/enrichment/kegg_report/KEGG_MapRender.R
+++ src/workbench/pkg/R/enrichment/kegg_report/KEGG_MapRender.R
@@ -8,11 +8,11 @@
 #'    file; or a file to the bundle pack of the kegg map xml files.
 #' 
 const KEGG_MapRender = function(enrich, 
-        map_id = "KEGG",
-        pathway_links = "pathway_links",
-        outputdir = "./",
-        min_objects = 0,
-        kegg_maps = NULL) {
+    map_id = "KEGG",
+    pathway_links = "pathway_links",
+    outputdir = "./",
+    min_objects = 0,
+    kegg_maps = NULL) {
 
     const KEGG_maps = GCModeller::kegg_maps(rawMaps = FALSE, repo = {
         print("inspect of the kegg_maps source:");
@@ -34,16 +34,18 @@ const KEGG_MapRender = function(enrich,
         );
     }
 
-    map_id = enrich[, map_id];
+    map_id        = enrich[, map_id];
     pathway_links = enrich[, pathway_links];
     pathway_links = as.list(pathway_links, names = map_id);
 
     str(pathway_links);
 
-    GCModeller::localRenderMap(KEGG_maps, pathwayList = pathway_links,
-                                compoundcolors = "red",
-                                gene_highights = "blue",
-                                outputdir      = outputdir,
-                                min_objects    = as.integer(min_objects)
-                            );
+    GCModeller::localRenderMap(
+        KEGG_maps, 
+        pathwayList    = pathway_links,
+        compoundcolors = "red",
+        gene_highights = "blue",
+        outputdir      = outputdir,
+        min_objects    = as.integer(min_objects)
+    );
 }
