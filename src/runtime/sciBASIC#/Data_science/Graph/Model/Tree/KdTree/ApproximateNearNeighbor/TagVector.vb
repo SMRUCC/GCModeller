45d1448306be5128bd9017e4cd033aeb6245821b
谢桂纲 <gg.xie@bionovogene.com>
2024-01-26 12:41:17 +0800

    make improvements of the data model improvements

--- src/runtime/sciBASIC#/Data_science/Graph/Model/Tree/KdTree/ApproximateNearNeighbor/TagVector.vb
+++ src/runtime/sciBASIC#/Data_science/Graph/Model/Tree/KdTree/ApproximateNearNeighbor/TagVector.vb
@@ -1,10 +1,28 @@
-﻿Namespace KdTree.ApproximateNearNeighbor
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.Math
 
-    Public Structure TagVector
+Namespace KdTree.ApproximateNearNeighbor
 
-        Dim index As Integer
-        Dim vector As Double()
-        Dim tag As String
+    ''' <summary>
+    ''' a matrix row data is a vector
+    ''' </summary>
+    Public Class TagVector : Implements INamedValue, IVector
+
+        ''' <summary>
+        ''' the row index inside the original matrix rows
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property index As Integer
+        ''' <summary>
+        ''' the vector row data
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property vector As Double() Implements IVector.Data
+        ''' <summary>
+        ''' maybe is the unique reference id tag
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property tag As String Implements INamedValue.Key
 
         Public ReadOnly Property size As Integer
             Get
@@ -16,5 +34,5 @@
             Return $"[{index}] {vector.Take(6).JoinBy(", ")}..."
         End Function
 
-    End Structure
+    End Class
 End Namespace
