5b52b445e80b8abcd51a33c1cd9fb20ebda9977f
谢桂纲 <gg.xie@bionovogene.com>
2024-01-26 10:48:36 +0800

    fix of the method reference

--- src/runtime/sciBASIC#/Data_science/Graph/Model/Tree/KdTree/ApproximateNearNeighbor/ApproximateNearNeighbor.vb
+++ src/runtime/sciBASIC#/Data_science/Graph/Model/Tree/KdTree/ApproximateNearNeighbor/ApproximateNearNeighbor.vb
@@ -158,20 +158,6 @@ Namespace KdTree.ApproximateNearNeighbor
         Private Function RowMetric(ncols As Integer) As KdNodeAccessor(Of TagVector)
             Return New VectorAccessor(ncols)
         End Function
-
-        ''' <summary>
-        ''' add new item with check duplicated item
-        ''' </summary>
-        ''' <typeparam name="T"></typeparam>
-        ''' <param name="list"></param>
-        ''' <param name="item"></param>
-        <Extension>
-        Friend Sub Push(Of T)(list As List(Of KdNodeHeapItem(Of T)), item As KdNodeHeapItem(Of T))
-            If list.IndexOf(item) = -1 Then
-                Call list.Add(item)
-            End If
-        End Sub
-
     End Module
 
 End Namespace
