2bb6dcf25df3452ced661e755ce6dfb100c9e6ee
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-26 14:25:15 +0800

    split source file into multiple parts

--- src/workbench/pkg/R/enrichment/kegg_report/localRenderMap.R
+++ src/workbench/pkg/R/enrichment/kegg_report/localRenderMap.R
@@ -71,24 +71,3 @@ const localRenderMap = function(KEGG_maps, pathwayList,
     }
 }
 
-const .safe_kegg_url_parser = function(url, compoundcolors, gene_highights) {
-    let URL_kegg = NULL;
-
-    try({
-        URL_kegg <- url |> report.utils::parseKeggUrl(
-            compound = compoundcolors, 
-            gene     = gene_highights
-        );
-    });
-
-    if (is.null(URL_kegg)) {
-        # error while parse the url string
-        warning(`error while parse the kegg map highlight url: "${toString(url)}"!`);
-        # generates a fake empty url object
-        URL_kegg = {
-            objects: list()
-        };
-    } 
-
-    URL_kegg;
-}
---
+++ src/workbench/pkg/R/enrichment/kegg_report/union_render.R
@@ -0,0 +1,5 @@
+#' visual plot of the multiple omics union data
+#' 
+const union_render = function(union_data, id = "KEGG", compound = "compound", gene = "gene", protein = "protein") {
+    
+}
