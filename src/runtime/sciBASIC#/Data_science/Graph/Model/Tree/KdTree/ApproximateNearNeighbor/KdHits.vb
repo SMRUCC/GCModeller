457df2d4b2ab74a6e81a97f7634dcc9003cf7d9d
谢桂纲 <gg.xie@bionovogene.com>
2024-01-26 10:46:29 +0800

    make improvements of the query hit result data

---
+++ src/runtime/sciBASIC#/Data_science/Graph/Model/Tree/KdTree/ApproximateNearNeighbor/KdHits.vb
@@ -0,0 +1,16 @@
+﻿Namespace KdTree.ApproximateNearNeighbor
+
+    Public Structure KdHits
+
+        Dim size As Integer
+        Dim indices As Integer()
+        Dim weights As Double()
+
+        Sub New(size As Integer, indices As Integer(), weights As Double())
+            Me.size = size
+            Me.indices = indices
+            Me.weights = weights
+        End Sub
+
+    End Structure
+End Namespace
