a38e0c3a44653c6080cba0a3412801f9145b02e8
谢桂纲 <gg.xie@bionovogene.com>
2024-01-26 10:44:16 +0800

    split source file into multiple parts

--- src/runtime/sciBASIC#/Data_science/Graph/Model/Tree/KdTree/ApproximateNearNeighbor/ApproximateNearNeighbor.vb
+++ src/runtime/sciBASIC#/Data_science/Graph/Model/Tree/KdTree/ApproximateNearNeighbor/ApproximateNearNeighbor.vb
@@ -68,12 +68,11 @@
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Linq
-Imports Microsoft.VisualBasic.Math.Correlations.DistanceMethods
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
 Imports Microsoft.VisualBasic.Parallel
 
-Namespace KdTree
+Namespace KdTree.ApproximateNearNeighbor
 
     ''' <summary>
     ''' K Nearest Neighbour Search
@@ -175,70 +174,4 @@ Namespace KdTree
 
     End Module
 
-    Public Structure TagVector
-
-        Dim index As Integer
-        Dim vector As Double()
-        Dim tag As String
-
-        Public ReadOnly Property size As Integer
-            Get
-                Return vector.Length
-            End Get
-        End Property
-
-        Public Overrides Function ToString() As String
-            Return $"[{index}] {vector.Take(6).JoinBy(", ")}..."
-        End Function
-
-    End Structure
-
-    Friend Class VectorAccessor : Inherits KdNodeAccessor(Of TagVector)
-
-        Dim dims As Dictionary(Of String, Integer)
-        Dim dimKeys As String()
-
-        ''' <summary>
-        ''' create an accessor for access the n-dimension vector
-        ''' </summary>
-        ''' <param name="m"></param>
-        Sub New(m As Integer)
-            dims = m _
-                .Sequence _
-                .ToDictionary(Function(k) k.ToString,
-                              Function(k)
-                                  Return k
-                              End Function)
-            dimKeys = dims.Keys.ToArray
-        End Sub
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overrides Sub setByDimensin(x As TagVector, dimName As String, value As Double)
-            x.vector(dims(dimName)) = value
-        End Sub
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overrides Function GetDimensions() As String()
-            Return dimKeys
-        End Function
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overrides Function metric(a As TagVector, b As TagVector) As Double
-            Return a.vector.EuclideanDistance(b.vector)
-        End Function
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overrides Function getByDimension(x As TagVector, dimName As String) As Double
-            Return x.vector(dims(dimName))
-        End Function
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overrides Function nodeIs(a As TagVector, b As TagVector) As Boolean
-            Return a.index = b.index
-        End Function
-
-        Public Overrides Function activate() As TagVector
-            Return New TagVector With {.vector = 0.0.Repeats(times:=dims.Count).ToArray, .index = -1}
-        End Function
-    End Class
 End Namespace
---
+++ src/runtime/sciBASIC#/Data_science/Graph/Model/Tree/KdTree/ApproximateNearNeighbor/TagVector.vb
@@ -0,0 +1,20 @@
+﻿Namespace KdTree.ApproximateNearNeighbor
+
+    Public Structure TagVector
+
+        Dim index As Integer
+        Dim vector As Double()
+        Dim tag As String
+
+        Public ReadOnly Property size As Integer
+            Get
+                Return vector.Length
+            End Get
+        End Property
+
+        Public Overrides Function ToString() As String
+            Return $"[{index}] {vector.Take(6).JoinBy(", ")}..."
+        End Function
+
+    End Structure
+End Namespace
