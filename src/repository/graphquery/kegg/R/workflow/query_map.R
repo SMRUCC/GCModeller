aba5a8ba9ee917aa365d44eaf4b4e910260b2905
谢桂纲 <gg.xie@bionovogene.com>
2024-02-22 09:07:53 +0800

    get query of kegg module data

--- src/repository/graphquery/kegg/R/workflow/query_map.R
+++ src/repository/graphquery/kegg/R/workflow/query_map.R
@@ -1,7 +1,8 @@
 const __query_map = function(map, cache_dir = "./") {
     const cache_fs = [cache_dir]::fs;
-    const pwy = kegg_api::kegg_pathway(`ko${map$entry}`, cache = cache_dir);
-    let dir = `/${map$class}/${map$category}/${map$entry} - ${map$name}/`;
+    const pwy      = kegg_api::kegg_pathway(`ko${map$entry}`, cache = cache_dir);
+    const dir      = `/${map$class}/${map$category}/${map$entry} - ${map$name}/`;
+    const modules  = as.data.frame([pwy]::modules);
 
     HDS::writeText(cache_fs, `${dir}/map.xml`, xml(pwy));
     HDS::flush(cache_fs);
@@ -12,36 +13,12 @@ const __query_map = function(map, cache_dir = "./") {
         fs_dir = dir, 
         cache_dir = cache_dir);
 
-    let modules = as.data.frame([pwy]::modules);
-
+    print("view list of the modules inside current pathway map:");
     print(modules, max.print = 6);
 
     for(mid in as.list(modules, byrow = TRUE)) {
-        let mod = kegg_api::kegg_module(mid$name, cache = cache_dir);
-        let mdir = `${dir}/modules/${mid$name} - ${mid$text}/`;
-
-        HDS::writeText(cache_fs, `${mdir}/module.xml`, xml(mod));
-        HDS::flush(cache_fs);
-
-        # str(mod);
-        let reactions = as.data.frame([mod]::reaction);
-
-        print(reactions, max.print = 6);
-
-        for(rid in as.list(reactions, byrow = TRUE)) {
-            let rxn = kegg_api::kegg_reaction(rid$name, cache = cache_dir);
-            let rfile = `${mdir}/reactions/${rid$name} - ${rid$text}.xml`;
-
-            HDS::writeText(cache_fs, rfile, xml(rxn));
-            HDS::flush(cache_fs);
-
-            # str(rxn);
-            # sleep(1);
-            # stop();
-        }
-
-        __query_compounds(as.data.frame([mod]::compound), 
-            fs_dir = mdir, 
+        __query_module(mid, 
+            fs_dir = dir, 
             cache_dir = cache_dir);
     }
 
