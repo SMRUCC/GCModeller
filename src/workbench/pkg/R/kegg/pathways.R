5fb2dda29f8fc3f95ae323d07c0cec8d8333896f
谢桂纲 <gg.xie@bionovogene.com>
2024-02-22 09:24:38 +0800

    add dev notes

--- src/workbench/pkg/R/kegg/pathways.R
+++ src/workbench/pkg/R/kegg/pathways.R
@@ -2,6 +2,12 @@
 #' 
 #' @details read from the internal resource data files
 #' 
+#' @return a dataframe object that contains the human pathway list, 
+#'   the data fields was includes this dataframe object:
+#' 
+#'   + ID: the pathway id
+#'   + Definition: the pathway name
+#' 
 const hsa_pathways = function() {
 	"data/hsa.txt" 
 	|> system.file(package = "GCModeller")
