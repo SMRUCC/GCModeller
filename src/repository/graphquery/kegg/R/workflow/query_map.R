29b6a4126c994d3a5e91b33b7aea69a7571bbe44
谢桂纲 <gg.xie@bionovogene.com>
2024-02-22 08:59:55 +0800

    split source file ijnto multiple parts

--- src/repository/graphquery/kegg/R/workflow/query_map.R
+++ src/repository/graphquery/kegg/R/workflow/query_map.R
@@ -59,30 +59,3 @@ const __query_map = function(map, cache_dir = "./") {
 
     invisible(NULL);
 }
-
-#' get query of the kegg compound
-#' 
-#' @param compounds a dataframe object that contains the a set of 
-#'    the kegg compounds for run data downloads, the two data fields: 
-#'    name and text should be includes in this dataframe object.
-#' @param cache_dir the cache filesystem object
-#' 
-const __query_compounds = function(compounds, fs_dir = "/", cache_dir = "./") {
-    const cache_fs = [cache_dir]::fs;
-    
-    print("view of the given kegg compound set:");
-    print(compounds, max.print = 6);
-
-    # compounds inside the pathway module
-    for(cid in as.list(compounds, byrow = TRUE)) {
-        let cpd = kegg_api::kegg_compound(cid$name, cache = cache_dir);
-        let fs_filepath = `${fs_dir}/compounds/${cid$name} - ${cid$text}.xml`;
-
-        print(fs_filepath);
-
-        HDS::writeText(cache_fs, fs_filepath, xml(cpd));
-        HDS::flush(cache_fs);
-    }
-
-    invisible(NULL);
-}
