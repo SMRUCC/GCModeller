5391ce2f68ed343f6e786976cbaf84a7db2b6050
谢桂纲 <gg.xie@bionovogene.com>
2024-02-22 08:48:15 +0800

    split source file into multiple parts

--- src/repository/graphquery/kegg/R/workflow/kegg_referencedb.R
+++ src/repository/graphquery/kegg/R/workflow/kegg_referencedb.R
@@ -1,6 +1,13 @@
-let brite = brite::parse("br08901");
+
+#' workflow function for create the kegg reference database
+#' 
+#' @param db_file a character vector of the database file its 
+#'    file path. for save the kegg data models. 
+#'  
+const kegg_referencedb = function(db_file = "./kegg.db") {
+    let brite = brite::parse("br08901");
 let df = as.data.frame(brite);
- 
+
 const cache_dir = `${@dir}/cache.db`
 |> HDS::openStream(allowCreate = TRUE, meta_size = 32*1024*1024)
 |> http::http.cache()
@@ -10,85 +17,9 @@ const cache_fs = [cache_dir]::fs;
 print(df, max.print = 6);
 
 for(map in as.list(df, byrow = TRUE)) {
-    let pwy = kegg_api::kegg_pathway(`ko${map$entry}`, cache = cache_dir);
-    let dir = `/${map$class}/${map$category}/${map$entry} - ${map$name}/`;
 
-    HDS::writeText(cache_fs, `${dir}/map.xml`, xml(pwy));
-    HDS::flush(cache_fs);
-
-    let compoundList = as.data.frame([pwy]::compound);
-
-    print(compoundList);
-
-    # compound inside pathway root
-    for(cid in as.list(compoundList, byrow = TRUE)) {
-        let cpd = kegg_api::kegg_compound(cid$name, cache = cache_dir);
-        let cfile = `${dir}/compounds/${cid$name} - ${cid$text}.xml`;
-
-        # str(cpd);
-        print(cfile);
-
-        HDS::writeText(cache_fs, cfile, xml(cpd));
-        HDS::flush(cache_fs);
-
-        # sleep(1);
-    }
-
-    let modules = as.data.frame([pwy]::modules);
-
-    print(modules, max.print = 6);
-
-    for(mid in as.list(modules, byrow = TRUE)) {
-        let mod = kegg_api::kegg_module(mid$name, cache = cache_dir);
-        let mdir = `${dir}/modules/${mid$name} - ${mid$text}/`;
-
-        HDS::writeText(cache_fs, `${mdir}/module.xml`, xml(mod));
-        HDS::flush(cache_fs);
-
-        # str(mod);
-        let reactions = as.data.frame([mod]::reaction);
-
-        print(reactions, max.print = 6);
-
-        for(rid in as.list(reactions, byrow = TRUE)) {
-            let rxn = kegg_api::kegg_reaction(rid$name, cache = cache_dir);
-            let rfile = `${mdir}/reactions/${rid$name} - ${rid$text}.xml`;
-
-            HDS::writeText(cache_fs, rfile, xml(rxn));
-            HDS::flush(cache_fs);
-
-            # str(rxn);
-            # sleep(1);
-            # stop();
-        }
-
-        let compounds = as.data.frame([mod]::compound);
-
-        print(compounds, max.print = 6);
-
-        # compounds inside the pathway module
-        for(cid in as.list(compounds, byrow = TRUE)) {
-            let cpd = kegg_api::kegg_compound(cid$name, cache = cache_dir);
-            let cfile = `${mdir}/compounds/${cid$name} - ${cid$text}.xml`;
-
-            # str(cpd);
-            print(cfile);
-
-            HDS::writeText(cache_fs, cfile, xml(cpd));
-            HDS::flush(cache_fs);
-
-            # sleep(1);
-        }
-
-        # stop();
-    }
-
-    # str(pwy);
-    # str(map);
-
-    # stop();
-
-    # sleep(1);
 }
 
 HDS::flush(cache_fs);
+}
+
