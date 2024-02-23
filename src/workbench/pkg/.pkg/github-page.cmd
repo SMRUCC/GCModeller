24f6a1d99407f5281bc3367e2960bbc3ea745f7a
谢桂纲 <gg.xie@bionovogene.com>
2024-02-23 11:16:25 +0800

    fix of the enum member orders

--- src/workbench/pkg/.pkg/github-page.cmd
+++ src/workbench/pkg/.pkg/github-page.cmd
@@ -1,5 +1,6 @@
 @echo off
 
 SET js_url="https://gcmodeller.org/lib/R_syntax.js"
+SET Rscript="\GCModeller\src\R-sharp\App\net6.0\Rscript.exe"
 
-Rscript --build /src ../  --skip-src-build --github-page %js_url%
+%Rscript% --build /src ../  --skip-src-build --github-page %js_url%
