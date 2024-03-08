6f48fb52eabd8c08fccdc9d05ee6af1e7d928241
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-08 19:24:57 +0800

    apply of the new markdown render engine

--- src/runtime/sciBASIC#/mime/text%markdown/Render/HtmlRender.vb
+++ src/runtime/sciBASIC#/mime/text%markdown/Render/HtmlRender.vb
@@ -19,4 +19,52 @@
     Public Overrides Function Document(text As String) As String
         Return text
     End Function
+
+    Public Overrides Function Header(text As String, level As Integer) As String
+        Return String.Format("<h{1}>{0}</h{1}>" & vbLf & vbLf, text, level)
+    End Function
+
+    Public Overrides Function CodeSpan(text As String) As String
+        Return {"<code>", text, "</code>"}.JoinBy("")
+    End Function
+
+    ''' <summary>
+    ''' &lt;hr />
+    ''' </summary>
+    ''' <returns></returns>
+    Public Overrides Function HorizontalLine() As String
+        Return "<hr />"
+    End Function
+
+    Public Overrides Function NewLine() As String
+        Return "<br />"
+    End Function
+
+    Public Overrides Function CodeBlock(code As String, lang As String) As String
+        Return String.Concat(vbLf & vbLf & $"<pre><code class=""{lang}"">", code, vbLf & "</code></pre>" & vbLf & vbLf)
+    End Function
+
+    Public Overrides Function Image(url As String, altText As String, title As String) As String
+        Dim result = String.Format("<img src=""{0}"" alt=""{1}""", url, altText)
+
+        If Not String.IsNullOrEmpty(title) Then
+            result &= String.Format(" title=""{0}""", title)
+        End If
+
+        result &= " />"
+
+        Return result
+    End Function
+
+    Public Overrides Function Bold(text As String) As String
+        Return $"<strong>{text}</strong>"
+    End Function
+
+    Public Overrides Function Italic(text As String) As String
+        Return $"<em>{text}</em>"
+    End Function
+
+    Public Overrides Function BlockQuote(text As String) As String
+        Return $"<blockquote>{text}</blockquote>"
+    End Function
 End Class
