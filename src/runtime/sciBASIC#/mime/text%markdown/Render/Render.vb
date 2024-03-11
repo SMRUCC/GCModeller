ef0cbe58faac6e66d12e5de74d739c772f14dc9c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-11 10:05:33 +0800

    make improvements of the new markdown document render

--- src/runtime/sciBASIC#/mime/text%markdown/Render/Render.vb
+++ src/runtime/sciBASIC#/mime/text%markdown/Render/Render.vb
@@ -2,13 +2,7 @@
 
 Public MustInherit Class Render
 
-    Protected markdown As MarkdownHTML
-
     Protected Shared ReadOnly _leadingWhitespace As New Regex("^[ ]*", RegexOptions.Compiled)
-
-    Friend Sub SetEngine(markdown As MarkdownHTML)
-        Me.markdown = markdown
-    End Sub
 
     Public MustOverride Function Paragraph(text As String, CreateParagraphs As Boolean) As String
     Public MustOverride Function Header(text As String, level As Integer) As String
@@ -18,5 +12,12 @@ Public MustInherit Class Render
     Public MustOverride Function HorizontalLine() As String
     Public MustOverride Function NewLine() As String
     Public MustOverride Function Image(url As String, altText As String, title As String) As String
+    Public MustOverride Function AnchorLink(url As String, text As String, title As String) As String
+    Public MustOverride Function Bold(text As String) As String
+    Public MustOverride Function Italic(text As String) As String
+    Public MustOverride Function Underline(text As String) As String
+    Public MustOverride Function BlockQuote(text As String) As String
+    Public MustOverride Function List(items As IEnumerable(Of String), orderList As Boolean) As String
+    Public MustOverride Function Table(head As String(), rows As IEnumerable(Of String())) As String
 
 End Class
