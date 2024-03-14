3efd4529fce22449bf1d281de26920aab75ba867
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-14 18:28:24 +0800

    configs run test

--- src/GCModeller/data/Reactome/test/Program.vb
+++ src/GCModeller/data/Reactome/test/Program.vb
@@ -1,7 +1,15 @@
-Imports System
+Imports SMRUCC.genomics.Data.Reactome
 
 Module Program
     Sub Main(args As String())
+        Call pathwayTree()
+
         Console.WriteLine("Hello World!")
+    End Sub
+
+    Sub pathwayTree()
+        Dim tree = Hierarchy.LoadInternal
+
+        Pause()
     End Sub
 End Module
