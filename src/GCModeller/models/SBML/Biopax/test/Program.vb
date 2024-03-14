07287e18910e1664363a271b3269df2c9ec3089c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-14 14:16:36 +0800

    configs for run test

--- src/GCModeller/models/SBML/Biopax/test/Program.vb
+++ src/GCModeller/models/SBML/Biopax/test/Program.vb
@@ -2,7 +2,16 @@ Imports SMRUCC.genomics.MetabolicModel
 Imports SMRUCC.genomics.Model.Biopax.Level3
 
 Module Program
+
     Sub Main(args As String())
+        Call graph_test()
+    End Sub
+
+    Sub graph_test()
+
+    End Sub
+
+    Sub readerTest()
         Dim docs = "D:\GCModeller\src\GCModeller\models\SBML\data\smpdb_PW000266.owl"
         Dim pathway = File.LoadDoc(docs)
         Dim loader = ResourceReader.LoadResource(pathway)
