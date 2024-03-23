4f22868668538a4105b4fb75d613513b4203db13
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-23 10:24:39 +0800

    add data types

---
+++ src/workbench/modules/Knowledge_base/Knowledge_base/MeSH/DataTypes.vb
@@ -0,0 +1,13 @@
+﻿Public Class XmlString
+
+    Public Property [String] As String
+
+End Class
+
+Public Class XmlDate
+
+    Public Property Year As Integer
+    Public Property Month As Integer
+    Public Property Day As Integer
+
+End Class
---
+++ src/workbench/modules/Knowledge_base/Knowledge_base/MeSH/DescriptorRecordSet.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Xml.Serialization
+
+''' <summary>
+''' the mesh Descriptor Record Set xml file
+''' </summary>
+''' <remarks>
+''' which could be download from the ncbi ftp website: 
+''' 
+''' https://nlmpubs.nlm.nih.gov/projects/mesh/MESH_FILES/xmlmesh/?_gl=1*jikpoo*_ga*MTQ4NzExODI0OS4xNjg3NDAyOTQ4*_ga_7147EPK006*MTcxMTE2MDE3Ny4xLjEuMTcxMTE2MDQzNC4wLjAuMA..*_ga_P1FPTH9PL4*MTcxMTE2MDE3Ny4xLjEuMTcxMTE2MDQzNC4wLjAuMA..
+''' </remarks>
+Public Class DescriptorRecordSet
+
+    <XmlAttribute> Public Property LanguageCode As String
+
+    <XmlElement>
+    Public Property DescriptorRecord As DescriptorRecord()
+
+End Class
+
+Public Class DescriptorRecord
+
+    <XmlAttribute>
+    Public Property DescriptorClass As Integer
+    Public Property DescriptorUI As String
+    Public Property DescriptorName As xmlstring
+
+End Class
