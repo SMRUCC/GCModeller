c01c6c027f6147cbf8d7a68be0b2698c6ff28c67
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-24 13:54:41 +0800

    fix of the namespace reference error

--- src/workbench/modules/workbench/pubmed.vb
+++ src/workbench/modules/workbench/pubmed.vb
@@ -1,59 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::2fccbf0bbf08557b7643b8af13693cbc, modules\workbench\pubmed.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 28
-    '    Code Lines: 22
-    ' Comment Lines: 0
-    '   Blank Lines: 6
-    '     File Size: 1.04 KB
+' Summaries:
 
 
-    ' Module pubmedTools
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: Parse, QueryKeyword, toTabular
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 28
+'    Code Lines: 22
+' Comment Lines: 0
+'   Blank Lines: 6
+'     File Size: 1.04 KB
+
+
+' Module pubmedTools
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: Parse, QueryKeyword, toTabular
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
-Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base
-Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.PubMed
+Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI
+Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI.PubMed
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 
