12773906aa150ef9eb9d88e9fdaf1b854c62d5b2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-26 16:50:55 +0800

    configs for run strimg test

--- src/workbench/modules/Knowledge_base/Knowledge_base/NLP/GeneName.vb
+++ src/workbench/modules/Knowledge_base/Knowledge_base/NLP/GeneName.vb
@@ -1,8 +1,10 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm.BinaryTree
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.csv.IO
-Imports Microsoft.VisualBasic.ComponentModel.Algorithm.BinaryTree
 Imports Microsoft.VisualBasic.DataMining.BinaryTree
+Imports Microsoft.VisualBasic.DataMining.DynamicProgramming
+Imports Microsoft.VisualBasic.DataMining.DynamicProgramming.NeedlemanWunsch
 
 Public Module GeneName
 
@@ -21,12 +23,23 @@ Public Module GeneName
 
     Private Class TextSimilar : Inherits ComparisonProvider
 
+        ReadOnly matrix As ScoreMatrix(Of Char)
+        ReadOnly symbol As GenericSymbol(Of Char) = GetGeneralCharSymbol()
+
         Sub New(cutoff As Double)
-            Call MyBase.New(cutoff, cutoff / 2)
+            MyBase.New(cutoff, cutoff / 2)
+            matrix = New ScoreMatrix(Of Char)(symbol)
         End Sub
 
         Public Overrides Function GetSimilarity(x As String, y As String) As Double
+            Dim gnw As New NeedlemanWunsch(Of Char)(x, y, matrix, symbol)
+            Dim best As GlobalAlign(Of Char) = gnw _
+                .Compute() _
+                .PopulateAlignments _
+                .OrderByDescending(Function(a) a.score) _
+                .FirstOrDefault
 
+            Throw New NotImplementedException
         End Function
 
         Public Overrides Function GetObject(id As String) As Object
