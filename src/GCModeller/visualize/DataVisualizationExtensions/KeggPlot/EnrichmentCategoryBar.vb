30b2a79d0e236f2aaf76ca4b5b24fd48b1d44f7a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-31 22:18:56 +0800

    plot module code

--- src/GCModeller/visualize/DataVisualizationExtensions/KeggPlot/EnrichmentCategoryBar.vb
+++ src/GCModeller/visualize/DataVisualizationExtensions/KeggPlot/EnrichmentCategoryBar.vb
@@ -1,3 +1,13 @@
-﻿Public Class ClassEnrichmentCategoryBar
+﻿Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
 
+Public Class ClassEnrichmentCategoryBar : Inherits Plot
+
+    Public Sub New(theme As Theme)
+        MyBase.New(theme)
+    End Sub
+
+    Protected Overrides Sub PlotInternal(ByRef g As Imaging.IGraphics, canvas As Imaging.Drawing2D.GraphicsRegion)
+        Throw New NotImplementedException()
+    End Sub
 End Class
