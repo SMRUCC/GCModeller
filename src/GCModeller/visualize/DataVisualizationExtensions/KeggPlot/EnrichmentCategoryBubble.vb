188f7b654e0fb56f95b550dbcc9ce653b826d355
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-02 14:31:34 +0800

    reference to the enrichment model

--- src/GCModeller/visualize/DataVisualizationExtensions/KeggPlot/EnrichmentCategoryBubble.vb
+++ src/GCModeller/visualize/DataVisualizationExtensions/KeggPlot/EnrichmentCategoryBubble.vb
@@ -1,13 +1,20 @@
 ﻿Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports SMRUCC.genomics.Analysis.HTS.GSEA
 
 Public Class EnrichmentCategoryBubble : Inherits Plot
 
-    Public Sub New(theme As Theme)
+    ReadOnly enrich As EnrichmentResult()
+
+    Public Sub New(enrich As IEnumerable(Of EnrichmentResult), theme As Theme)
         MyBase.New(theme)
+
+        Me.enrich = enrich.ToArray
     End Sub
 
-    Protected Overrides Sub PlotInternal(ByRef g As Imaging.IGraphics, canvas As Imaging.Drawing2D.GraphicsRegion)
+    Protected Overrides Sub PlotInternal(ByRef g As IGraphics, canvas As GraphicsRegion)
         Throw New NotImplementedException()
     End Sub
 End Class
