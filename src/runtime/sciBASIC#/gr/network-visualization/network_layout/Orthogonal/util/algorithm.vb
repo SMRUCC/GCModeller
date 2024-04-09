c4ada09c6bbfade66ee82c6da73491fc60407e56
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-09 09:22:45 +0800

    make improvements of the exact mass evaluation helper

--- src/runtime/sciBASIC#/gr/network-visualization/network_layout/Orthogonal/util/algorithm.vb
+++ src/runtime/sciBASIC#/gr/network-visualization/network_layout/Orthogonal/util/algorithm.vb
@@ -1,8 +1,13 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph.EdgeBundling
 Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts.Orthogonal.optimization
+Imports Microsoft.VisualBasic.Imaging.Math2D
+Imports Microsoft.VisualBasic.Linq
+Imports std = System.Math
 
 Namespace Orthogonal
 
@@ -10,7 +15,11 @@ Namespace Orthogonal
 
         <Extension>
         Public Function AsGraphMatrix(g As NetworkGraph) As Integer()()
-            Dim vlist As Node() = g.vertex.ToArray
+            Return g.AsGraphMatrix(vlist:=g.vertex.ToArray)
+        End Function
+
+        <Extension>
+        Private Function AsGraphMatrix(g As NetworkGraph, vlist As Node()) As Integer()()
             Dim graph As Integer()() = RectangularArray.Matrix(Of Integer)(vlist.Length, vlist.Length)
 
             For i As Integer = 0 To vlist.Length - 1
@@ -25,7 +34,11 @@ Namespace Orthogonal
                         Continue For
                     End If
 
-                    If g.GetEdges(v, node).Any Then
+                    ' 20240408 the graph matrix needs to be a un-directed graph
+                    ' the getEdges function is a kind of function for get directed edges set
+                    ' so we needs a tuple and reverse tuple combine for create a un-directed
+                    ' edge set
+                    If g.GetEdges(v, node).Any OrElse g.GetEdges(node, v).Any Then
                         vr(j) = 1
                     End If
                 Next
@@ -41,13 +54,49 @@ Namespace Orthogonal
                                  Optional simplify As Boolean = True,
                                  Optional fixNonOrthogonal As Boolean = True) As NetworkGraph
 
+            Dim vlist As Node() = g.vertex.ToArray
             Dim layout As OrthographicEmbeddingResult = g _
-                .AsGraphMatrix _
+                .AsGraphMatrix(vlist) _
                 .RunLayoutMatrix(
                     numberOfAttempts:=numberOfAttempts,
                     optimize:=optimize,
                     simplify:=simplify,
-                    fixNonOrthogonal:=fixNonOrthogonal)
+                    fixNonOrthogonal:=fixNonOrthogonal
+                )
+            Dim index As Integer
+            Dim u, v As Node
+
+            If layout Is Nothing Then
+                Throw New InvalidProgramException("run layout failure...")
+            End If
+
+            For i As Integer = 0 To layout.nodeIndexes.Length - 1
+                index = layout.nodeIndexes(i)
+
+                If index > -1 Then
+                    v = vlist(index)
+                    v.data.initialPostion = New FDGVector2(layout(i))
+                End If
+            Next
+
+            For Each edge As Edge In g.graphEdges
+                Dim pu = edge.U.data.initialPostion.Point2D
+                Dim pv = edge.V.data.initialPostion.Point2D
+
+                If std.Abs(pu.X - pv.X) < 0.01 OrElse std.Abs(pu.Y - pv.Y) < 0.01 Then
+                    ' u - v on a line
+                    ' no bends
+                Else
+                    ' needs a middle point in route
+                    If layout.find(pu.X, pv.Y) Then
+                        edge.data.bends = {XYMetaHandle.CreateVector(pu, pv, pu.X, pv.Y)}
+                    ElseIf layout.find(pv.X, pu.Y) Then
+                        edge.data.bends = {XYMetaHandle.CreateVector(pu, pv, pv.X, pu.Y)}
+                    Else
+                        ' no bends?
+                    End If
+                End If
+            Next
 
             Return g
         End Function
@@ -68,7 +117,7 @@ Namespace Orthogonal
                 Dim g As Integer()() = DisconnectedGraphs.subgraph(graph, nodeSubset)
                 Dim comparator As New SegmentLengthEmbeddingComparator()
 
-                For attempt = 0 To numberOfAttempts - 1
+                For attempt As Integer = 0 To numberOfAttempts - 1
                     Dim g_oe As OrthographicEmbeddingResult = OrthographicEmbedding.orthographicEmbedding(g, simplify, fixNonOrthogonal)
 
                     If g_oe Is Nothing Then
