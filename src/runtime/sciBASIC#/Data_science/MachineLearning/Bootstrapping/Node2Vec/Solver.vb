4b9accf8dc86c877c35fe2c38c10bce5df167f77
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-09 13:34:57 +0800

    make improvements of the graph model

--- src/runtime/sciBASIC#/Data_science/MachineLearning/Bootstrapping/Node2Vec/Solver.vb
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/Bootstrapping/Node2Vec/Solver.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Data.GraphTheory
 Imports Microsoft.VisualBasic.Data.NLP.Word2Vec
+Imports Microsoft.VisualBasic.Linq
 
 Namespace node2vec
 
@@ -8,6 +10,40 @@ Namespace node2vec
     ''' Created by freemso on 17-3-14.
     ''' </summary>
     Public Module Solver
+
+        ''' <summary>
+        ''' create graph from a dataframe?
+        ''' </summary>
+        ''' <param name="u"></param>
+        ''' <param name="v"></param>
+        ''' <param name="w"></param>
+        ''' <returns></returns>
+        Public Function CreateGraph(u As String(), v As String(), Optional w As Double() = Nothing) As Graph
+            Dim graph As New Graph
+
+            If u.TryCount <> v.TryCount Then
+                Throw New InvalidProgramException("the dimension size of the source node is mis-matched with the target nodes!")
+            End If
+            If w Is Nothing Then
+                w = 1.0.Replicate(u.Length).ToArray
+            End If
+
+            Dim idencoder As Index(Of String) = u.JoinIterates(v).Distinct.Indexing
+
+            For i As Integer = 0 To w.Length - 1
+                ' add the nodes to the graph
+                Dim node1 = graph.addNode(idencoder(u(i)), u(i))
+                Dim node2 = graph.addNode(idencoder(v(i)), v(i))
+                Dim weight As Double = w(i)
+
+                ' add the edge to the graph
+                Call graph.addEdge(node1, node2, weight)
+            Next
+
+            Call graph.preprocess()
+
+            Return graph
+        End Function
 
         ''' <summary>
         ''' 
