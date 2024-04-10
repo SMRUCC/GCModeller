aea8b2b7bba2e3714b57f91557f42d9685cf6bb0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-10 08:27:43 +0800

    make improvements of the graph vector embedder

--- src/runtime/sciBASIC#/Data_science/MachineLearning/Bootstrapping/Graph2Vec.vb
+++ src/runtime/sciBASIC#/Data_science/MachineLearning/Bootstrapping/Graph2Vec.vb
@@ -1,5 +1,6 @@
 ﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.GraphTheory
+Imports Microsoft.VisualBasic.Data.GraphTheory.SequenceGraphTransform
 Imports Microsoft.VisualBasic.Data.NLP.Word2Vec
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
@@ -8,11 +9,30 @@ Public Class Graph2Vec
 
     ReadOnly vocabulary As New Dictionary(Of String, Char)
     ReadOnly getVocabulary As Func(Of Vertex, String)
+    ReadOnly other As String
+
+    Const offset As Integer = 33
 
     Dim sgt As SequenceGraphTransform
 
-    Public Function Setup(terms As IEnumerable(Of String)) As Graph2Vec
+    Sub New(getVocabulary As Func(Of Vertex, String), Optional other As String = "*")
+        Me.other = other
+        Me.getVocabulary = getVocabulary
+    End Sub
 
+    Public Function Setup(terms As IEnumerable(Of String)) As Graph2Vec
+        For Each term As String In terms.JoinIterates({other})
+            Call vocabulary.Add(term, Chr(offset + vocabulary.Count))
+        Next
+
+        If vocabulary.Count >= (127 - offset) Then
+            Call VBDebugger.EchoLine("alphabet set too much chars, the embedding vector will be very very long!")
+        End If
+
+        sgt = New SequenceGraphTransform(mode:=Modes.Fast)
+        sgt.set_alphabets(vocabulary.Values.Select(Function(c) c.ToString).ToArray)
+
+        Return Me
     End Function
 
     Public Function GraphVector(g As Graph) As Double()
@@ -25,10 +45,17 @@ Public Class Graph2Vec
         Dim vertexSet = g.vertex.ToDictionary(Function(v) v.label)
         Dim chars As Char() = sort _
             .Select(Function(lb) getVocabulary(vertexSet(lb.name))) _
-            .Select(Function(key) vocabulary(key)) _
+            .Select(Function(key)
+                        If vocabulary.ContainsKey(key) Then
+                            Return vocabulary(key)
+                        Else
+                            Return vocabulary(other)
+                        End If
+                    End Function) _
             .ToArray
+        Dim vector As Double() = sgt.fitVector(New String(chars))
 
-
+        Return vector
     End Function
 
 End Class
