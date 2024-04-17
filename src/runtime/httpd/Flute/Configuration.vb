5705f887ae153323255cb92789fa86a447f1c312
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-17 17:18:34 +0800

    save description as ini comment text

--- src/runtime/httpd/Flute/Configuration.vb
+++ src/runtime/httpd/Flute/Configuration.vb
@@ -1,9 +1,14 @@
-﻿Imports Flute.Http.Core
+﻿Imports System.ComponentModel
+Imports Flute.Http.Core
 Imports Microsoft.VisualBasic.ComponentModel.Settings.Inf
 
+<ClassName("Configuration")>
 Public Class Configuration
 
+    <Description("a string for identify the http server backend.")>
     Public Property x_powered_by As String = HttpProcessor.VBS_platform
+
+    <Description("a logical value for turn the verbose echo of the debug message on.")>
     Public Property silent As Boolean = True
 
     ''' <summary>
