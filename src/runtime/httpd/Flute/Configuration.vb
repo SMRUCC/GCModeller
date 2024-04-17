6d355253c7d62b268b2debaf1caa3f746ab70c1a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-17 14:55:49 +0800

    engine configuration file?

--- src/runtime/httpd/Flute/Configuration.vb
+++ src/runtime/httpd/Flute/Configuration.vb
@@ -1,7 +1,20 @@
 ﻿Imports Flute.Http.Core
+Imports Microsoft.VisualBasic.ComponentModel.Settings.Inf
 
 Public Class Configuration
 
     Public Property x_powered_by As String = HttpProcessor.VBS_platform
+
+    Public Shared Function Load(inifile As String) As Configuration
+        If inifile.FileLength <= 0 Then
+            Return New Configuration
+        End If
+
+        Return ClassMapper.LoadIni(Of Configuration)(inifile)
+    End Function
+
+    Public Shared Function Save(settings As Configuration, inifile As String) As Boolean
+        Return ClassMapper.WriteClass(settings, inifile)
+    End Function
 
 End Class
