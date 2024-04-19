5e54c71fbf275ca36b784ded45cd9ee7cc7d07c0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-19 15:11:52 +0800

    add template compiler

--- src/runtime/httpd/src/FluteBuild/Program.vb
+++ src/runtime/httpd/src/FluteBuild/Program.vb
@@ -1,4 +1,5 @@
 Imports System.ComponentModel
+Imports Flute.Template
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 
@@ -12,6 +13,14 @@ Module Program
     <Description("Compile the html files from a collection of template source files.")>
     <Usage("/compile /view <directory_to_templates> /wwwroot <output_dir_for_html>")>
     Public Function Build(view As String, wwwroot As String, args As CommandLine) As Integer
+        Dim viewfiles As String() = view.EnumerateFiles("*.vbhtml").ToArray
 
+        For Each template As String In viewfiles
+            Call VBHtml _
+                .ReadHTML(template) _
+                .SaveTo(wwwroot & "/" & template.BaseName & ".html")
+        Next
+
+        Return 0
     End Function
 End Module
