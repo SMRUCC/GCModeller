7327ae226742a560e646e96875bfbeb2ee463228
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-19 16:03:22 +0800

    get includes document test success

--- src/runtime/httpd/src/WebCloud/SMRUCC.WebCloud.VBScript/Interpolate/Includes.vb
+++ src/runtime/httpd/src/WebCloud/SMRUCC.WebCloud.VBScript/Interpolate/Includes.vb
@@ -4,7 +4,15 @@ Namespace Interpolate
     Module IncludeInterpolate
 
         Public Sub FillIncludes(vbhtml As VBHtml)
+            Dim includes As String() = VBHtml.partialIncludes _
+                .Matches(vbhtml.ToString) _
+                .ToArray
 
+            ' each includes is also a new vbhtml template file
+            ' do render and get the html text as the value
+            For Each reference As String In includes
+
+            Next
         End Sub
     End Module
 End Namespace
