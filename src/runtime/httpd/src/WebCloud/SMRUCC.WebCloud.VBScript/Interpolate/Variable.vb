e258648eff46a8b4afd8d160784d48db7955813e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-19 15:24:30 +0800

    get variable names test sicvcess

--- src/runtime/httpd/src/WebCloud/SMRUCC.WebCloud.VBScript/Interpolate/Variable.vb
+++ src/runtime/httpd/src/WebCloud/SMRUCC.WebCloud.VBScript/Interpolate/Variable.vb
@@ -3,6 +3,16 @@
     Public Module VariableInterpolate
 
         Public Sub FillVariables(vbhtml As VBHtml)
+            Dim vars As String() = VBHtml.variable _
+                .Matches(vbhtml.ToString) _
+                .ToArray
+
+            For Each var As String In vars.OrderByDescending(Function(name) name.Length)
+                Call FillVariable(vbhtml, var)
+            Next
+        End Sub
+
+        Private Sub FillVariable(vbhtml As VBHtml, name As String)
 
         End Sub
     End Module
