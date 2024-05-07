28936ce36436d6dd2026a485ef2980fbda51613b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-07 19:09:17 +0800

    feather file format

---
+++ src/runtime/httpd/src/WebCloud/Flute.SessionManager/Extensions.vb
@@ -0,0 +1,22 @@
+﻿
+Imports System.Runtime.CompilerServices
+Imports Flute.Http.Configurations
+
+<HideModuleName>
+Public Module Extensions
+
+    Public Function Open(session_store As String, ssid As String) As SessionFile
+        Dim dir As String = $"{session_store}/{ssid.Substring(ssid.Length - 2, 2)}/{ssid.Substring(ssid.Length - 4, 3)}"
+        Dim keyfile As String = $"{dir}/{ssid}.keys"
+        Dim datafile As String = $"{dir}/{ssid}"
+        Dim file As New SessionFile(keyfile, datafile)
+
+        Return file
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function Open(ssid As String, config As Configuration) As SessionFile
+        Return SessionManager.Open(config.session.session_store, ssid)
+    End Function
+
+End Module
--- src/runtime/httpd/src/WebCloud/Flute.SessionManager/SessionFile.vb
+++ src/runtime/httpd/src/WebCloud/Flute.SessionManager/SessionFile.vb
@@ -1,51 +1,90 @@
 Imports System.IO
 Imports System.Text
 Imports Microsoft.VisualBasic.Data.IO
-Imports Microsoft.VisualBasic.Serialization
 
 Public Class SessionFile
 
-    ReadOnly file As String
+    ReadOnly keyfile As String
+    ReadOnly datafile As String
 
-    Sub New(filepath As String)
-        file = filepath
+    Sub New(keyfile As String, datafile As String)
+        Me.datafile = datafile
+        Me.keyfile = keyfile
 
-        If Not file.FileExists Then
-            Call New Byte() {}.FlushStream(file)
+        If Not Me.keyfile.FileExists Then
+            Call (New Byte() {}).FlushStream(Me.keyfile)
+            Call (New Byte() {}).FlushStream(Me.datafile)
         End If
     End Sub
 
     Public Function SaveKey(key As String, data As Byte()) As Boolean
         Dim lastBlock As BufferRegion = Nothing
-        Dim region As BufferRegion = SearchKey(key, lastBlock)
+        Dim offset As Long = 0
+        Dim region As BufferRegion = SearchKey(key, lastBlock, offset)
+
+        If lastBlock Is Nothing Then
+            lastBlock = New BufferRegion
+        End If
 
         If region Is Nothing Then
             ' append new region
-            Using s As New BinaryDataWriter(New FileStream(file, FileMode.Append), Encoding.ASCII)
+            Using s As New BinaryDataWriter(New FileStream(keyfile, FileMode.Append), Encoding.ASCII)
                 s.Write(key, BinaryStringFormat.ZeroTerminated)
+                s.Write(lastBlock.nextBlock)
                 s.Write(data.Length)
+                s.Flush()
+            End Using
+            Using s As New FileStream(datafile, FileMode.Open)
+                s.Seek(lastBlock.nextBlock, SeekOrigin.Begin)
                 s.Write(data)
                 s.Flush()
             End Using
         ElseIf data.Length = region.size Then
             ' overrides
-            Using s As New BinaryDataWriter(New FileStream(file, FileMode.Open), Encoding.ASCII)
+            Using s As New BinaryDataWriter(New FileStream(datafile, FileMode.Open), Encoding.ASCII)
                 s.Seek(region.position, SeekOrigin.Begin)
                 s.Write(data)
                 s.Flush()
             End Using
         ElseIf data.Length < region.size Then
             ' update region size and then overrides data
-            Using s As New BinaryDataWriter(New FileStream(file, FileMode.Open), Encoding.ASCII)
-                s.Seek(region.position - RawStream.INT32, SeekOrigin.Begin)
+            Using s As New BinaryDataWriter(New FileStream(keyfile, FileMode.Open), Encoding.ASCII)
+                s.Seek(offset + key.Length + 1 + 8, SeekOrigin.Begin)
                 s.Write(data.Length)
+                s.Flush()
+            End Using
+            Using s As New BinaryDataWriter(New FileStream(datafile, FileMode.Open), Encoding.ASCII)
+                s.Seek(region.position, SeekOrigin.Begin)
                 s.Write(data)
                 s.Flush()
             End Using
         Else
             ' erase the data, and write to new location
+            Dim dataOffset As Long
 
+            Using s As New BinaryDataWriter(New FileStream(datafile, FileMode.Open), Encoding.ASCII)
+                s.Seek(s.BaseStream.Length, SeekOrigin.Begin)
+                dataOffset = s.Position
+                s.Write(data)
+                s.Flush()
+            End Using
+            Using s As New BinaryDataWriter(New FileStream(keyfile, FileMode.Open), Encoding.ASCII)
+                s.Seek(offset + key.Length + 1, SeekOrigin.Begin)
+                s.Write(dataOffset)
+                s.Write(data.Length)
+                s.Flush()
+            End Using
         End If
+
+        Return True
+    End Function
+
+    Public Function SaveKey(key As String, data As Integer) As Boolean
+        Return SaveKey(key, BitConverter.GetBytes(data))
+    End Function
+
+    Public Function SaveKey(key As String, data As Double) As Boolean
+        Return SaveKey(key, BitConverter.GetBytes(data))
     End Function
 
     Public Function SaveKey(key As String, data As String) As Boolean
@@ -53,53 +92,80 @@ Public Class SessionFile
     End Function
 
     Public Function OpenKeyString(key As String) As String
-        Dim s As MemoryStream = OpenKey(key)
+        Dim s As Byte() = OpenKey(key)
 
         If s Is Nothing Then
             Return Nothing
         Else
-            Return Encoding.UTF8.GetString(s.ToArray)
+            Return Encoding.UTF8.GetString(s)
         End If
     End Function
 
-    Public Function OpenKey(key As String) As MemoryStream
+    Public Function OpenKeyInteger(key As String) As Integer
+        Dim s As Byte() = OpenKey(key)
+
+        If s Is Nothing Then
+            Return Nothing
+        Else
+            Return BitConverter.ToInt32(s, Scan0)
+        End If
+    End Function
+
+    Public Function OpenKeyDouble(key As String) As Double
+        Dim s As Byte() = OpenKey(key)
+
+        If s Is Nothing Then
+            Return Nothing
+        Else
+            Return BitConverter.ToDouble(s, Scan0)
+        End If
+    End Function
+
+    Public Function OpenKey(key As String) As Byte()
         Dim region As BufferRegion = SearchKey(key)
 
         If region Is Nothing Then
             Return Nothing
         Else
-            Using s As New FileStream(file, FileMode.Open)
+            Using s As New FileStream(datafile, FileMode.Open)
                 Dim load As Byte() = New Byte(region.size - 1) {}
 
                 Call s.Seek(region.position, SeekOrigin.Begin)
                 Call s.Read(load, Scan0, load.Length)
 
-                Return New MemoryStream(load)
+                Return load
             End Using
         End If
     End Function
 
     ''' <summary>
-    ''' [keyname => length,next]
+    ''' [keyname => offset,length]
     ''' </summary>
     ''' <param name="key"></param>
     ''' <returns></returns>
-    Public Function SearchKey(key As String, Optional ByRef lastBlock As BufferRegion = Nothing) As BufferRegion
-        Using s As New BinaryDataReader(New FileStream(file, FileMode.Open), Encoding.ASCII)
+    Public Function SearchKey(key As String,
+                              Optional ByRef lastBlock As BufferRegion = Nothing,
+                              Optional ByRef keyOffset As Long = 0) As BufferRegion
+
+        Using s As New BinaryDataReader(New FileStream(keyfile, FileMode.Open), Encoding.ASCII)
+            Dim skey As String
+            Dim start As Long
+            Dim len As Integer
+
             For i As Integer = 0 To 100000
-                Dim skey As String = s.ReadString(BinaryStringFormat.ZeroTerminated)
-                Dim len As Integer = s.ReadInt32
-                Dim start As Long = s.BaseStream.Position
+                If s.EndOfStream Then
+                    Exit For
+                Else
+                    skey = s.ReadString(BinaryStringFormat.ZeroTerminated)
+                    start = s.ReadInt64
+                    len = s.ReadInt32
+                End If
 
                 If skey = key Then
+                    keyOffset = s.Position - 8 - 4 - skey.Length - 1
                     Return New BufferRegion(start, len)
                 Else
                     lastBlock = New BufferRegion(start, len)
-                    s.Seek(lastBlock.nextBlock, SeekOrigin.Begin)
-
-                    If s.EndOfStream Then
-                        Exit For
-                    End If
                 End If
             Next
 
