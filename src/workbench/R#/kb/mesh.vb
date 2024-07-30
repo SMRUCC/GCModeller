325a95f698fe8f448259cc331c627f9bafccc741
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-30 11:32:49 +0800

    parse the tree as cluster

--- src/workbench/R#/kb/mesh.vb
+++ src/workbench/R#/kb/mesh.vb
@@ -54,8 +54,10 @@
 #End Region
 
 Imports System.IO
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.GraphTheory
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Analysis.HTS.GSEA
 Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI
@@ -173,12 +175,64 @@ Module meshTools
     ''' <param name="category"></param>
     ''' <returns></returns>
     <ExportAPI("mesh_background")>
-    Public Function convertMeshBackground(tree As Tree(Of MeSH.Tree.Term), category As MeshCategory) As Background
+    Public Function convertMeshBackground(tree As Tree(Of MeSH.Tree.Term), category As MeshCategory, Optional levels As Integer = 1) As Background
         Dim root_terms = tree.Childs.Values _
             .Where(Function(n)
                        Return n.Data.category.Contains(category)
                    End Function) _
             .ToArray
+        Dim clusters As New List(Of Cluster)
 
+        For Each root As Tree(Of MeSH.Tree.Term) In root_terms
+            For Each terms In root.ClusterTermByLevel(levels, category.Description & "->" & root.label)
+                Dim cluster As New Cluster With {
+                    .description = terms.category,
+                    .ID = terms.cluster.Data.accessionID,
+                    .names = terms.cluster.Data.term,
+                    .members = terms.cluster _
+                        .ClusterMembers _
+                        .ToArray
+                }
+            Next
+        Next
+
+        Return New Background With {
+            .build = Now,
+            .clusters = clusters.ToArray,
+            .id = category.ToString,
+            .name = category.Description,
+            .size = .clusters.BackgroundSize
+        }
+    End Function
+
+    <Extension>
+    Private Iterator Function ClusterMembers(root As Tree(Of MeSH.Tree.Term)) As IEnumerable(Of BackgroundGene)
+        Yield New BackgroundGene With {.name = root.label}
+
+        For Each node In root.Childs.SafeQuery.Select(Function(t) t.Value)
+            For Each term In node.ClusterMembers
+                Yield term
+            Next
+        Next
+    End Function
+
+    ''' <summary>
+    ''' get cluster term in specific level
+    ''' </summary>
+    ''' <param name="root"></param>
+    ''' <param name="level"></param>
+    ''' <returns></returns>
+    <Extension>
+    Private Iterator Function ClusterTermByLevel(root As Tree(Of MeSH.Tree.Term), level As Integer, label As String) As IEnumerable(Of (category As String, cluster As Tree(Of MeSH.Tree.Term)))
+        If level > 0 Then
+            For Each term In root.Childs.Values
+                For Each item In term.ClusterTermByLevel(level - 1, $"{label}->{term.label}")
+                    Yield item
+                Next
+            Next
+        Else
+            ' current tree node is a cluster member
+            Yield (label, root)
+        End If
     End Function
 End Module
