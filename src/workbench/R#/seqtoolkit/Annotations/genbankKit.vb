ca136f483e3beebfb45e65f8a2083a634a5cb52b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-04 20:38:25 +0800

    populate the featue data

--- src/workbench/R#/seqtoolkit/Annotations/genbankKit.vb
+++ src/workbench/R#/seqtoolkit/Annotations/genbankKit.vb
@@ -1,62 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::0fc8cf48b53f28d8fc7e56cc60cfab0d, R#\seqtoolkit\Annotations\genbankKit.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 419
-    '    Code Lines: 295 (70.41%)
-    ' Comment Lines: 70 (16.71%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 54 (12.89%)
-    '     File Size: 16.83 KB
+' Summaries:
 
 
-    ' Module genbankKit
-    ' 
-    '     Function: addFeature, addMeta, addproteinSeq, addRNAGene, asGenbank
-    '               createFeature, enumerateFeatures, featureMeta, getOrAddNtOrigin, getRNASeq
-    '               isPlasmidSource, keyNames, populateGenbanks, readGenbank, writeGenbank
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 419
+'    Code Lines: 295 (70.41%)
+' Comment Lines: 70 (16.71%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 54 (12.89%)
+'     File Size: 16.83 KB
+
+
+' Module genbankKit
+' 
+'     Function: addFeature, addMeta, addproteinSeq, addRNAGene, asGenbank
+'               createFeature, enumerateFeatures, featureMeta, getOrAddNtOrigin, getRNASeq
+'               isPlasmidSource, keyNames, populateGenbanks, readGenbank, writeGenbank
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Assembly.NCBI
@@ -310,19 +311,60 @@ Module genbankKit
             .ToArray
     End Function
 
+    ''' <summary>
+    ''' extract the feature metadata from a genbank clr feature object
+    ''' </summary>
+    ''' <param name="features"></param>
+    ''' <param name="attrName$"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
     <ExportAPI("featureMeta")>
     <RApiReturn(GetType(String))>
-    Public Function featureMeta(<RRawVectorArgument> features As Object, attrName$, Optional env As Environment = Nothing) As Object
-        Dim featureArray As pipeline = pipeline.TryCreatePipeline(Of Feature)(features, env)
+    Public Function featureMeta(<RRawVectorArgument> features As Object,
+                                Optional attrName$ = Nothing,
+                                Optional env As Environment = Nothing) As Object
+
+        Dim featureArray As pipeline = pipeline.TryCreatePipeline(Of gbffFeature)(features, env)
 
         If featureArray.isError Then
             Return featureArray.getError
         End If
 
-        Return featureArray _
-            .populates(Of Feature)(env) _
-            .Select(Function(feature) feature.Query(attrName)) _
+        Dim all As gbffFeature() = featureArray _
+            .populates(Of gbffFeature)(env) _
             .ToArray
+
+        If attrName.StringEmpty(, True) Then
+            ' populate all features
+            If all.Length = 1 Then
+                Return New list With {
+                    .slots = all(0).PairedValues _
+                        .GroupBy(Function(f) f.Name) _
+                        .ToDictionary(Function(f) f.Key,
+                                      Function(f)
+                                          Return CObj(f.Values)
+                                      End Function)
+                }
+            Else
+                Dim list As list = list.empty
+
+                For Each feature As gbffFeature In all
+                    list.add(feature.ToString, feature.PairedValues _
+                        .GroupBy(Function(f) f.Name) _
+                        .ToDictionary(Function(f) f.Key,
+                                      Function(f)
+                                          Return CObj(f.Values)
+                                      End Function))
+                Next
+
+                Return list
+            End If
+        Else
+            ' populate a specific feature metadata vector
+            Return all _
+                .Select(Function(feature) feature.Query(attrName)) _
+                .ToArray
+        End If
     End Function
 
     <ExportAPI("addMeta")>
