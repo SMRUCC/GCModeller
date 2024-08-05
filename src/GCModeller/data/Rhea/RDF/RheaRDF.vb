201dc209a9e817b487f0a3b29db199b157df82cb
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-05 20:44:41 +0800

    make updates of the rdf data model

---
+++ src/GCModeller/data/Rhea/RDF/RheaRDF.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic.MIME.application.rdf_xml
+
+''' <summary>
+''' 
+''' </summary>
+''' <remarks>
+''' https://ftp.expasy.org/databases/rhea/rdf/rhea.rdf.gz
+''' </remarks>
+Public Class RheaRDF : Inherits RDF(Of Description)
+
+    Public Const rh As String = "http://rdf.rhea-db.org/"
+
+    Sub New()
+        Call MyBase.New()
+        Call xmlns.Add("rh", rh)
+    End Sub
+End Class
