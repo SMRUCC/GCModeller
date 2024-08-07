9d940cbf189d81d4910d000378e2f0175ae1a4d2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-07 20:11:29 +0800

    split sopuirce file into multiple parts

--- src/GCModeller/data/Rhea/Reaction.vb
+++ src/GCModeller/data/Rhea/Reaction.vb
@@ -89,23 +89,3 @@ Public Class Reaction : Implements INamedValue
     End Function
 
 End Class
-
-Public Class SideCompound
-
-    <XmlAttribute>
-    Public Property side As String
-    Public Property compound As Compound
-
-    Sub New(side As String, compound As Compound)
-        _side = side
-        _compound = compound
-    End Sub
-
-    Sub New()
-    End Sub
-
-    Public Overrides Function ToString() As String
-        Return compound.ToString
-    End Function
-
-End Class
