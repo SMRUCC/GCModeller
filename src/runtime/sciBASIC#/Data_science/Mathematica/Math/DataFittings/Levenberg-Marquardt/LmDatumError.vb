f7d40e80411a4f6b65b62792c1548ef22c1962d5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-18 09:07:04 +0800

    add source files  for  LevenbergMarquardt

---
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/DataFittings/Levenberg-Marquardt/LmDatumError.vb
@@ -0,0 +1,59 @@
+﻿Namespace LevenbergMarquardt
+
+    ' Created by duy on 14/4/15.
+
+    ''' <summary>
+    ''' LmDatumError is an interface for evaluating error, Jacobian matrix and
+    ''' Hessian matrix of a single piece of observed data
+    ''' </summary>
+    Public Interface LmDatumError
+        ''' <summary>
+        ''' Gets the total number of observed data
+        ''' </summary>
+        ''' <returns> An integer which is the number of observed data </returns>
+        ReadOnly Property NumData As Integer
+
+        ''' <summary>
+        ''' Evaluates value of the error function for the k-th observed data that
+        ''' corresponds to the parameter vector
+        ''' </summary>
+        ''' <paramname="dataIdx"> The index of the input data </param>
+        ''' <paramname="optParams"> A vector of real values of parameters in the model
+        ''' @return </param>
+        Function eval(dataIdx As Integer, optParams As Double()) As Double
+
+        ''' <summary>
+        ''' Evaluates the Jacobian vector of the error function for the k-th observed
+        ''' data that corresponds to the parameter vector
+        ''' </summary>
+        ''' <paramname="dataIdx"> The index of the input data </param>
+        ''' <paramname="optParams"> A vector of real values of parameters in the model
+        ''' @return </param>
+        Function jacobian(dataIdx As Integer, optParams As Double()) As Double()
+
+        ''' <summary>
+        ''' Evaluates the Hessian matrix of the error function for the k-th observed
+        ''' data that corresponds to the parameter vector
+        ''' </summary>
+        ''' <paramname="dataIdx"> The index of the input data </param>
+        ''' <paramname="optParams"> A vector of real values of parameters in the model </param>
+        ''' <paramname="approxHessianFlg"> A boolean flag to indicate whether the Hessian
+        '''                         matrix can be approximated instead of having to be
+        '''                         computed exactly
+        ''' @return </param>
+        Function hessian(dataIdx As Integer, optParams As Double(), approxHessianFlg As Boolean) As Double()()
+
+        ''' <summary>
+        ''' Evaluates the Hessian matrix of the error function for the k-th observed
+        ''' data that corresponds to the parameter vector. The Hessian matrix is
+        ''' computed exactly
+        ''' </summary>
+        ''' <paramname="dataIdx"> </param>
+        ''' <paramname="optParams">
+        ''' @return </param>
+        Friend Function hessian(dataIdx As Integer, optParams As Double()) As Double()()
+        Return hessian(dataIdx, optParams, False)
+        End Function
+    End Interface
+
+End Namespace
---
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/DataFittings/Levenberg-Marquardt/LmModelError.vb
@@ -0,0 +1,48 @@
+﻿Namespace LevenbergMarquardt
+    ''' <summary>
+    ''' Created by duy on 20/1/15.
+    ''' </summary>
+    Public Interface LmModelError
+
+        ''' <summary>
+        ''' Evaluates the error function with input optimization parameter values
+        ''' </summary>
+        ''' <paramname="optParams"> A vector of real values of parameters used in optimizing
+        '''                  the error function </param>
+        ''' <returns> Double value of the error function </returns>
+        Function eval(optParams As Double()) As Double
+
+        ''' <summary>
+        ''' Computes the Jacobian vector of the error function with input
+        ''' optimization parameter values
+        ''' </summary>
+        ''' <paramname="optParams"> A vector of real values of parameters used in optimizing
+        '''                  the error function </param>
+        ''' <returns> Jacobian vector of the error function </returns>
+        Function jacobian(optParams As Double()) As Double()
+
+        ''' <summary>
+        ''' Computes the Hessian matrix of the error function with input
+        ''' optimization parameter values
+        ''' </summary>
+        ''' <paramname="optParams"> A vector of real values of parameters used in optimizing
+        '''                  the error function </param>
+        ''' <paramname="approxHessianFlg"> A boolean flag to indicate whether the Hessian
+        '''                         matrix can be approximated instead of having to be
+        '''                         computed exactly </param>
+        ''' <returns> Hessian matrix of the error function </returns>
+        Function hessian(optParams As Double(), approxHessianFlg As Boolean) As Double()()
+
+        ''' <summary>
+        ''' Computes the Hessian matrix of the error function with input
+        ''' optimization parameter values
+        ''' </summary>
+        ''' <paramname="optParams"> A vector of real values of parameters used in optimizing
+        '''                  the error function </param>
+        ''' <returns> The exact Hessian matrix of the error function </returns>
+        Friend Function hessian(optParams As Double()) As Double()()
+        Return hessian(optParams, False)
+        End Function
+    End Interface
+
+End Namespace
