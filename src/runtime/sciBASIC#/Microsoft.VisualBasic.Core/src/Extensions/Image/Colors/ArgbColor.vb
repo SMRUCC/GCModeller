4533f53e877d2027b1b1ea1b34b3862782f5a685
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-20 15:21:31 +0800

    needs a stanbdard color model

---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/Extensions/Image/Colors/ArgbColor.vb
@@ -0,0 +1,12 @@
+﻿
+Namespace Imaging
+
+    Public Structure ArgbColor
+
+        Dim A As Byte
+        Dim R As Byte
+        Dim G As Byte
+        Dim B As Byte
+
+    End Structure
+End Namespace
