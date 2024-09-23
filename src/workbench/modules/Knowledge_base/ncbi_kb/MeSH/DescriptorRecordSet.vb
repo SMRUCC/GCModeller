7a6452fa089142cb9226e2b17649ad0660af5bdc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-23 08:43:36 +0800

    add download link notes

--- src/workbench/modules/Knowledge_base/ncbi_kb/MeSH/DescriptorRecordSet.vb
+++ src/workbench/modules/Knowledge_base/ncbi_kb/MeSH/DescriptorRecordSet.vb
@@ -66,6 +66,9 @@ Namespace MeSH
     ''' <remarks>
     ''' which could be download from the ncbi ftp website: 
     ''' 
+    ''' https://nlmpubs.nlm.nih.gov/projects/mesh/MESH_FILES/xmlmesh/
+    ''' https://nlmpubs.nlm.nih.gov/projects/mesh/MESH_FILES/xmlmesh/desc2024.zip
+    ''' 
     ''' https://nlmpubs.nlm.nih.gov/projects/mesh/MESH_FILES/xmlmesh/?_gl=1*jikpoo*_ga*MTQ4NzExODI0OS4xNjg3NDAyOTQ4*_ga_7147EPK006*MTcxMTE2MDE3Ny4xLjEuMTcxMTE2MDQzNC4wLjAuMA..*_ga_P1FPTH9PL4*MTcxMTE2MDE3Ny4xLjEuMTcxMTE2MDQzNC4wLjAuMA..
     ''' </remarks>
     Public Class DescriptorRecordSet
