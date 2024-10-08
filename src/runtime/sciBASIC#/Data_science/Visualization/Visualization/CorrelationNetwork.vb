44c34160d67a413c77c6cf800ea129c15576e00f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-08 10:47:55 +0800

    rollback of the missing graphics builder function

--- src/runtime/sciBASIC#/Data_science/Visualization/Visualization/CorrelationNetwork.vb
+++ src/runtime/sciBASIC#/Data_science/Visualization/Visualization/CorrelationNetwork.vb
@@ -1,62 +1,64 @@
 ﻿#Region "Microsoft.VisualBasic::0e95d9a5015c4d93d66074350c8cf5e6, Data_science\Visualization\Visualization\CorrelationNetwork.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 121
-    '    Code Lines: 91 (75.21%)
-    ' Comment Lines: 11 (9.09%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 19 (15.70%)
-    '     File Size: 4.37 KB
+' Summaries:
 
 
-    ' Module CorrelationNetwork
-    ' 
-    '     Function: (+3 Overloads) BuildNetwork, HowStrong
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 121
+'    Code Lines: 91 (75.21%)
+' Comment Lines: 11 (9.09%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 19 (15.70%)
+'     File Size: 4.37 KB
+
+
+' Module CorrelationNetwork
+' 
+'     Function: (+3 Overloads) BuildNetwork, HowStrong
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Math.DataFrame
+Imports df = Microsoft.VisualBasic.Math.DataFrame.DataFrame
 Imports std = System.Math
 
 ''' <summary>
@@ -102,6 +104,67 @@ Public Module CorrelationNetwork
                     }
 
                     g.CreateEdge(id, partner, cor, linkdata)
+                End If
+            Next
+        Next
+
+        Return g
+    End Function
+
+    <Extension>
+    Public Function BuildNetwork(corDf As df, cutoff As Double) As NetworkGraph
+        Dim g As New NetworkGraph
+        Dim nodeData As NodeData
+        Dim linkdata As EdgeData
+        Dim cor As Double
+        Dim colnames As String() = corDf.featureNames
+        Dim rownames As String() = corDf.rownames
+        Dim rowOffset As Index(Of String) = rownames.Indexing
+
+        For Each id As String In colnames
+            nodeData = New NodeData With {
+                .origID = id,
+                .label = id,
+                .Properties = New Dictionary(Of String, String) From {
+                    {NamesOf.REFLECTION_ID_MAPPING_NODETYPE, "Feature"}
+                }
+            }
+            Call g.CreateNode(id, nodeData)
+        Next
+
+        For Each id As String In rownames
+            nodeData = New NodeData With {
+                .origID = id,
+                .label = id,
+                .Properties = New Dictionary(Of String, String) From {
+                    {NamesOf.REFLECTION_ID_MAPPING_NODETYPE, "Sample"}
+                }
+            }
+            Call g.CreateNode(id, nodeData)
+        Next
+
+        Dim uid As String
+        Dim prob As Double
+        Dim cols = corDf.features _
+            .ToDictionary(Function(a) a.Key,
+                          Function(a)
+                              Return a.Value.TryCast(Of Double)
+                          End Function)
+
+        For Each id As String In TqdmWrapper.Wrap(rownames)
+            For Each partner As String In colnames
+                cor = cols(partner)(rowOffset(id))
+                If std.Abs(cor) > cutoff Then
+                    uid$ = {partner, id}.OrderBy(Function(s) s).JoinBy(" - ")
+                    linkdata = New EdgeData With {
+                        .label = uid,
+                        .length = cor,
+                        .Properties = New Dictionary(Of String, String) From {
+                            {NamesOf.REFLECTION_ID_MAPPING_INTERACTION_TYPE, HowStrong(cor)},
+                            {"pvalue", prob}
+                        }
+                    }
+                    Call g.CreateEdge(id, partner, cor, linkdata)
                 End If
             Next
         Next
