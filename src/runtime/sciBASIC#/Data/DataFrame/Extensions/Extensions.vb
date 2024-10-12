0d176c15f2251a0b108a746a51154542dc4032da
谢桂纲 <gg.xie@bionovogene.com>
2024-10-12 10:07:00 +0800

    make bugs fixed for the data frame save

--- src/runtime/sciBASIC#/Data/DataFrame/Extensions/Extensions.vb
+++ src/runtime/sciBASIC#/Data/DataFrame/Extensions/Extensions.vb
@@ -354,6 +354,7 @@ Public Module Extensions
     Public Function SaveTable(data As IEnumerable, path$,
                               Optional encoding As Encoding = Nothing,
                               Optional type As Type = Nothing,
+                              Optional meta_blank As String = "",
                               Optional silent As Boolean = False) As Boolean
 
         If type Is Nothing Then
@@ -369,6 +370,7 @@ Public Module Extensions
             .doSave(objSource:=data,
                     typeDef:=type,
                     strict:=False,
+                    metaBlank:=meta_blank,
                     schemaOut:=Nothing) _
             .SaveDataFrame(
                 path:=path,
