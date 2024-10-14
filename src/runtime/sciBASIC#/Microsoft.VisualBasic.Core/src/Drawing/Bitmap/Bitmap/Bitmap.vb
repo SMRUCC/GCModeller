099a1bfce314016f811bc236ebd3758efae876e5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-14 14:38:10 +0800

    update new bitmap image reader/writer

---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/Drawing/Bitmap/Bitmap/Bitmap.vb
@@ -0,0 +1,205 @@
+Imports System
+Imports System.Collections.Generic
+Imports System.IO
+Imports System.Linq
+
+Namespace BmpSharp
+    Public Enum BitsPerPixelEnum As Integer
+        Monochrome = 1
+        Four = 4
+        Eight = 8
+        RBG16 = 16
+        RGB24 = 24
+        RGBA32 = 32
+    End Enum
+
+    ''' <summary>
+    ''' Number of bytes for specific Pixel format.
+    ''' </summary>
+    Public Enum BytesPerPixelEnum As Integer
+        RBG16 = 2
+        RGB24 = 3
+        RGBA32 = 4
+    End Enum
+
+    Public Enum CompressionMethod As Integer
+        BI_RGB = 0 ' none
+        BI_RLE8 = 1
+        BI_RLE4 = 2
+        BI_BITFIELDS = 3
+        BI_JPEG = 4
+        BI_PNG = 5
+        BI_ALPHABITFIELDS = 6
+        BI_CMYK = 11
+        BI_CMYKRLE8 = 12
+        BI_CMYKRLE4 = 13
+    End Enum
+
+    Public Class Bitmap
+        Public ReadOnly Property Width As Integer = 0
+        Public ReadOnly Property Height As Integer = 0
+        Public ReadOnly Property BitsPerPixelEnum As BitsPerPixelEnum
+
+        ''' <summary>
+        ''' BMP file must be aligned at 4 butes at the end of row
+        ''' </summary>
+        ''' <paramname="BitsPerPixelEnum"></param>
+        ''' <returns></returns>
+        Public ReadOnly Property BytesPerRow As Integer
+            Get
+                Return RequiredBytesPerRow(Width, BitsPerPixelEnum)
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' NOTE: we don't care for images that are less than 24 bits
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property BytesPerPixel As Integer
+            Get
+                Return CInt(BitsPerPixelEnum) / 8
+            End Get
+        End Property
+        Public ReadOnly Property PixelData As Byte()
+
+        ''' <summary>
+        ''' Get reversed order or rows.
+        ''' For Bitmap image, pixel rows are stored from bottom to top.
+        ''' So first row in bitmap file is lowest row in Image.
+        ''' </summary>
+        ''' <returns>Pixel data with reversed (fliped) rows</returns>
+        Public ReadOnly Property PixelDataFliped As Byte()
+            Get
+                Dim rowListData = New List(Of Byte())()
+                Dim totalRows = Height
+                Dim pixelsInRow = Width
+
+                For row = totalRows - 1 To 0 Step -1
+                    ' NOTE: this only works on images that are 8/24/32 bits per pixel
+                    Dim one_row = PixelData.Skip(row * Width * BytesPerPixel).Take(Width * BytesPerPixel).ToArray()
+                    rowListData.Add(one_row)
+                Next
+                Dim reversedBytes = rowListData.SelectMany(Function(row) row).ToArray()
+                Return reversedBytes
+            End Get
+        End Property
+
+        Public ReadOnly Property FileHeader As BitmapFileHeader
+        Public ReadOnly Property InfoHeaderBytes As Byte()
+
+        ''' <summary>
+        ''' Create new Bitmap object
+        ''' </summary>
+        ''' <paramname="width"></param>
+        ''' <paramname="height"></param>
+        ''' <paramname="pixelData"></param>
+        ''' <paramname="bitsPerPixel"></param>
+        Public Sub New(width As Integer, height As Integer, pixelData As Byte(), Optional bitsPerPixel As BitsPerPixelEnum = BitsPerPixelEnum.RGB24)
+            Me.Width = width
+            Me.Height = height
+			Me.PixelData = pixelData
+			Me.BitsPerPixelEnum = bitsPerPixel
+
+			Dim rawImageSize = BytesPerRow * height
+
+            ' Are we receiving proper byte[] size ?
+            If pixelData.Length <> width * height * BytesPerPixel Then Throw New ArgumentOutOfRangeException($"{NameOf(pixelData)} has invalid size.")
+
+
+            If bitsPerPixel = BitsPerPixelEnum.RGB24 Then InfoHeaderBytes = New BitmapInfoHeader(width, height, bitsPerPixel, rawImageSize).HeaderInfoBytes
+            If bitsPerPixel = BitsPerPixelEnum.RGBA32 Then
+                InfoHeaderBytes = New BitmapInfoHeaderRGBA(width, height, bitsPerPixel, rawImageSize).HeaderInfoBytes
+            End If
+
+            FileHeader = New BitmapFileHeader(width, height, bitsPerPixel, rawImageSize)
+        End Sub
+
+        ''' <summary>
+        ''' Get bitmap as byte aray for saving to file
+        ''' </summary>
+        ''' <paramname="flipped">Flip (reverse order of) rows. Bitmap pixel rows are stored from bottom to up as shown in image</param>
+        ''' <returns></returns>
+        Public Function GetBmpBytes(Optional flipped As Boolean = False) As Byte()
+            'var rawImageSize = BytesPerRow * Height;
+            'var buffer = new byte[BitmapFileHeader.BitmapFileHeaderSizeInBytes + rawImageSize];
+            'Buffer.BlockCopy( this.FileHeader.HeaderBytes, 0, buffer, 0, BitmapFileHeader.BitmapFileHeaderSizeInBytes );
+
+            'if (flipped) {
+            '	Buffer.BlockCopy( this.PixelDataFliped, 0, buffer, BitmapFileHeader.BitmapFileHeaderSizeInBytes, PixelData.Length );
+            '} else {
+            '	Buffer.BlockCopy( this.PixelData, 0, buffer, BitmapFileHeader.BitmapFileHeaderSizeInBytes, PixelData.Length );
+            '}
+            'return buffer;
+
+            Using stream = GetBmpStream(flipped)
+                Return stream.ToArray()
+            End Using
+        End Function
+
+        ''' <summary>
+        ''' Get bitmap as byte stream for saving to file
+        ''' </summary>
+        ''' <paramname="flipped">Flip (reverse order of) rows. Bitmap pixel rows are stored from bottom to up as shown in image</param>
+        ''' <returns></returns>
+        Public Function GetBmpStream(Optional fliped As Boolean = False) As MemoryStream
+            Dim rawImageSize = BytesPerRow * Height
+
+            'var stream = new System.IO.MemoryStream( BitmapFileHeader.BitmapFileHeaderSizeInBytes + (int) rawImageSize );
+            Dim stream = New MemoryStream(rawImageSize)
+
+            'using (var writer = new BinaryWriter( stream )) {
+            Dim writer = New BinaryWriter(stream)
+            writer.Write(FileHeader.HeaderBytes)
+            writer.Write(InfoHeaderBytes)
+            writer.Flush()
+            stream.Flush()
+
+            Dim paddingRequired = BytesPerRow <> Width * BytesPerPixel
+            Dim bytesToCopy = Width * BytesPerPixel
+            Dim pixData = If(fliped, PixelDataFliped, PixelData)
+
+            If paddingRequired Then
+                For counter = 0 To Height - 1
+                    Dim rowBuffer = New Byte(BytesPerRow - 1) {}
+                    Buffer.BlockCopy(src:=pixData, srcOffset:=counter * bytesToCopy, dst:=rowBuffer, dstOffset:=0, count:=bytesToCopy)
+                    writer.Write(rowBuffer)
+                Next
+            Else
+                writer.Write(pixData)
+            End If
+
+            stream.Position = 0
+
+            Return stream
+        End Function
+
+        ''' <summary>
+        ''' BMP file must be aligned at 4 bytes at the end of row
+        ''' </summary>
+        ''' <paramname="width">Image Width</param>
+        ''' <paramname="bitsPerPixel">Bits per pixel</param>
+        ''' <returns>How many bytes BMP requires per row</returns>
+        Public Shared Function RequiredBytesPerRow(width As Integer, bitsPerPixel As BitsPerPixelEnum) As Integer
+            Return CInt(Math.Ceiling(CDec(width * bitsPerPixel) / 32)) * 4
+        End Function
+
+        ''' <summary>
+        ''' Check if padding is required (extra bytes for a row).
+        ''' </summary>
+        ''' <paramname="width">Width of image</param>
+        ''' <paramname="bitsPerPixel">Bits per pixels to calculate actual byte requirement</param>
+        ''' <paramname="bytesPerRow">BMP required bytes per row</param>
+        ''' <returns>True/false if we need to allocate extra bytes (for BMP saving) for padding</returns>
+        Public Shared Function IsPaddingRequired(width As Integer, bitsPerPixel As BitsPerPixelEnum, bytesPerRow As Integer) As Boolean
+            Return bytesPerRow <> width * bitsPerPixel / 8
+        End Function
+
+        Private Class CSharpImpl
+            <Obsolete("Please refactor calling code to use normal throw statements")>
+            Shared Function __Throw(Of T)(ByVal e As Exception) As T
+                Throw e
+            End Function
+        End Class
+
+    End Class
+End Namespace
---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/Drawing/Bitmap/Bitmap/BitmapFileHelper.vb
@@ -0,0 +1,67 @@
+﻿Imports System
+Imports System.IO
+
+Namespace BmpSharp
+    Public Module BitmapFileHelper
+        Public Function ReadFileAsBitmap(fileName As String, Optional flipRows As Boolean = False) As Bitmap
+            If String.IsNullOrWhiteSpace(fileName) Then Throw New ArgumentNullException(NameOf(fileName))
+            If Not File.Exists(fileName) Then Throw New Exception($"File {fileName} not found")
+
+            Dim fileInfo = New FileInfo(fileName)
+            If fileInfo.Length <= BitmapFileHeader.BitmapFileHeaderSizeInBytes Then Throw New Exception($"Invalid file format. Size is too small.")
+
+            Using fileStream = File.OpenRead(fileName)
+                Using bReader = New SafeBinaryReader(fileStream)
+                    Dim headerBytes = bReader.ReadBytes(BitmapFileHeader.BitmapFileHeaderSizeInBytes)
+                    Dim fileHeader = BitmapFileHeader.GetHeaderFromBytes(headerBytes)
+
+                    If fileInfo.Length <> fileHeader.FileSize Then Throw New Exception($"File size is different than in header.")
+
+                    Dim dibHeaderSize = bReader.ReadInt32()
+                    fileStream.Seek(-4, SeekOrigin.Current)
+
+                    Dim infoHeader = BitmapInfoHeader.GetHeaderFromBytes(bReader.ReadBytes(dibHeaderSize))
+
+                    Dim width = infoHeader.Width
+                    Dim height = infoHeader.Height
+
+                    Dim bytesPerRow = BmpSharp.Bitmap.RequiredBytesPerRow(infoHeader.Width, infoHeader.BitsPerPixel)
+
+                    Dim bytesPerPixel = CInt(infoHeader.BitsPerPixel) / 8
+                    Dim paddingRequired = BmpSharp.Bitmap.IsPaddingRequired(infoHeader.Width, infoHeader.BitsPerPixel, bytesPerRow)
+                    Dim pixelData = New Byte(width * height * bytesPerPixel - 1) {}
+                    ' seek to location where pixel data is
+                    fileStream.Seek(fileHeader.PixelDataOffset, SeekOrigin.Begin)
+
+                    If paddingRequired Then
+                        Dim bytesToCopy = width * bytesPerPixel
+                        For counter = 0 To height - 1
+                            Dim rowBuffer = bReader.ReadBytes(bytesPerRow)
+                            Buffer.BlockCopy(src:=rowBuffer, srcOffset:=0, dst:=pixelData, dstOffset:=counter * bytesToCopy, count:=bytesToCopy)
+                        Next
+                    Else
+                        Dim rowBuffer = bReader.ReadBytes(pixelData.Length)
+                        rowBuffer.CopyTo(pixelData, 0)
+                    End If
+
+                    Dim bitmap = New Bitmap(width, height, pixelData:=pixelData, bitsPerPixel:=infoHeader.BitsPerPixel)
+                    Return bitmap
+
+                End Using
+            End Using
+        End Function
+
+        Public Sub SaveBitmapToFile(fileName As String, bitmap As Bitmap)
+            If bitmap Is Nothing Then Throw New ArgumentNullException(NameOf(bitmap))
+            If String.IsNullOrWhiteSpace(fileName) Then Throw New ArgumentNullException(NameOf(fileName))
+            Dim filePath = Path.GetDirectoryName(fileName)
+            If Not Directory.Exists(filePath) Then Throw New Exception($"Destination directory not found.")
+
+            Using fileStream = File.Create(fileName)
+                Using bmpStream = bitmap.GetBmpStream()
+                    bmpStream.CopyTo(fileStream, bufferSize:=16 * 1024)
+                End Using
+            End Using
+        End Sub
+    End Module
+End Namespace
