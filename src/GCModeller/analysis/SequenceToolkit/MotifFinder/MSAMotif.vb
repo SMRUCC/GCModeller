55f204d37aa87730bbbc2d69f5d6324361ca2035
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-19 16:01:40 +0800

    make improvements of the motif result model

--- src/GCModeller/analysis/SequenceToolkit/MotifFinder/MSAMotif.vb
+++ src/GCModeller/analysis/SequenceToolkit/MotifFinder/MSAMotif.vb
@@ -1,9 +1,19 @@
 ﻿Imports SMRUCC.genomics.Analysis.SequenceTools.MSA
+Imports Microsoft.VisualBasic.Math
 
 Public Class MSAMotif : Inherits MSAOutput
 
     Public Property start As Integer()
     Public Property countMatrix As Integer()()
     Public Property rowSum As Integer
+
+    Public Property p As Double()
+    Public Property q As Double()
+
+    Public ReadOnly Property score As Double()
+        Get
+            Return SIMD.Divide.f64_op_divide_f64(q, p)
+        End Get
+    End Property
 
 End Class
