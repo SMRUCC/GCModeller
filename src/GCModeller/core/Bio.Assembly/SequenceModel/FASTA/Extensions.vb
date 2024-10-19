9ebfb71f7204351420bd7aa58356715f7361f57c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-19 13:55:04 +0800

    helper function for write the sequence collection as fasta file

--- src/GCModeller/core/Bio.Assembly/SequenceModel/FASTA/Extensions.vb
+++ src/GCModeller/core/Bio.Assembly/SequenceModel/FASTA/Extensions.vb
@@ -1,58 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::788ab58e4d570be079a34dd23a672164, core\Bio.Assembly\SequenceModel\FASTA\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 32
-    '    Code Lines: 21 (65.62%)
-    ' Comment Lines: 6 (18.75%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 5 (15.62%)
-    '     File Size: 1.04 KB
+' Summaries:
 
 
-    '     Module Extensions
-    ' 
-    '         Function: Index
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 32
+'    Code Lines: 21 (65.62%)
+' Comment Lines: 6 (18.75%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 5 (15.62%)
+'     File Size: 1.04 KB
+
+
+'     Module Extensions
+' 
+'         Function: Index
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.IO
 Imports System.Runtime.CompilerServices
+Imports System.Text
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 
@@ -82,5 +84,33 @@ Namespace SequenceModel.FASTA
                 Return idx
             End If
         End Function
+
+        ''' <summary>
+        ''' Writes the sequences in FASTA format to a file with the given name </summary>
+        ''' <param name="sequences"> </param>
+        ''' <param name="filename"> </param>
+        ''' 
+        <Extension>
+        Public Sub writeFasta(sequences As IEnumerable(Of String), filename As String, Optional break As Integer = 80)
+            Using printWriter As New StreamWriter(filename.Open(FileMode.OpenOrCreate, doClear:=True))
+                Dim i As Integer = 0
+
+                For Each sequence As String In sequences
+                    Dim stringBuilder As New StringBuilder(sequence)
+                    i += 1
+
+                    Call Enumerable _
+                        .Range(0, sequence.Length / break) _
+                        .ForEach(Sub(j, o)
+                                     Call stringBuilder.Insert(j + break * (j + 1), vbLf)
+                                 End Sub)
+
+                    stringBuilder.Insert(0, String.Format("> sequence{0}" & vbLf, i))
+                    printWriter.WriteLine(stringBuilder.ToString())
+                Next
+
+                Call printWriter.Flush()
+            End Using
+        End Sub
     End Module
 End Namespace
