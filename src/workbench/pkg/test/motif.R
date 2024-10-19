af70424656e2e098bf8479ecfefa6185f88cb0b7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-19 16:48:29 +0800

    there is a bug about image drawing error

--- src/workbench/pkg/test/motif.R
+++ src/workbench/pkg/test/motif.R
@@ -6,12 +6,20 @@ imports "bioseq.fasta" from "seqtoolkit";
 let raw = read.fasta("G:\GCModeller\src\GCModeller\analysis\SequenceToolkit\data\Staphylococcaceae_LexA___Staphylococcaceae.fasta");
 let motif = gibbs_scan(raw, width = 18);
 
-pdf(file = "./LexA.pdf") {
+setwd(@dir);
+
+bitmap(file = "./LexA.png") {
     plot.seqLogo(motif, title = "LexA");
 }
+
+motif 
+|> JSON::json_encode()
+|> writeLines(con = "./LexA.json")
+;
 
 motif = as.data.frame(motif);
 motif = motif[order(motif$score),];
 
 print(motif);
 
+write.csv(motif, file = "./LexA.csv");
