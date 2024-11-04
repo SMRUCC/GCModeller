eeba00790034043d47791c541fc22854b75964bf
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-04 12:59:14 +0800

    still has type reference conflicts

--- src/runtime/sciBASIC#/gr/Drawing-net4.8/Utils.vb
+++ src/runtime/sciBASIC#/gr/Drawing-net4.8/Utils.vb
@@ -61,6 +61,13 @@ Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.BitmapImage
 Imports Microsoft.VisualBasic.Serialization.JSON
 
+#If NET8_0_OR_GREATER Then
+Imports Image = System.Drawing.Image
+Imports Bitmap = System.Drawing.Bitmap
+Imports SolidBrush = System.Drawing.SolidBrush
+Imports TextureBrush = System.Drawing.TextureBrush
+#End If
+
 Namespace Imaging.BitmapImage
 
     ''' <summary>
