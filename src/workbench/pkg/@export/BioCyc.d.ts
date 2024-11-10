c96a3dfb6e327dde25c10b05cdf48ad0ad914c44
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-10 13:06:15 +0800

    run package msbuild

--- src/workbench/pkg/@export/BioCyc.d.ts
+++ src/workbench/pkg/@export/BioCyc.d.ts
@@ -5,6 +5,7 @@
 // ref=annotationKit.BioCycRepository@annotationKit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
+ * The BioCyc database collection is an assortment of organism specific Pathway/Genome Databases (PGDBs) that provide reference to genome and metabolic pathway information for thousands of organisms.[1] As of July 2023, there were over 20,040 databases within BioCyc.[2] SRI International,[3] based in Menlo Park, California, maintains the BioCyc database family.
  * 
 */
 declare namespace BioCyc {
@@ -15,6 +16,13 @@ declare namespace BioCyc {
      * @param biocyc -
    */
    function createBackground(biocyc: object): object;
+   /**
+    * get external database cross reference id of current metabolite compound object
+    * 
+    * 
+     * @param meta -
+   */
+   function db_links(meta: object): any;
    /**
    */
    function formula(meta: object): string;
