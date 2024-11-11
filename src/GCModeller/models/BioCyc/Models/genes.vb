ce1acd1b081b86102ea4c5300bfdee3459b14a92
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-11 18:30:40 +0800

    creat gene model

---
+++ src/GCModeller/models/BioCyc/Models/genes.vb
@@ -0,0 +1,16 @@
+﻿
+Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
+
+<Xref("genes.dat")>
+Public Class genes : Inherits Model
+
+    <AttributeField("ACCESSION-1")>
+    Public Property accession1 As String
+    <AttributeField("ACCESSION-2")>
+    Public Property accession2 As String
+    <AttributeField("DBLINKS")>
+    Public Property db_xrefs As String()
+    <AttributeField("PRODUCT")>
+    Public Property product As String
+
+End Class
