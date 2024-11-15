e8f89284551dfa7a04a29e8a536a23127284020f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-15 22:37:14 +0800

    fix of the incorrect kinetics fix

--- src/GCModeller/engine/BootstrapLoader/MassLoader.vb
+++ src/GCModeller/engine/BootstrapLoader/MassLoader.vb
@@ -1,60 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::c106e51e42755d0ed993ca3fefeddfb1, engine\BootstrapLoader\MassLoader.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 57
-    '    Code Lines: 40 (70.18%)
-    ' Comment Lines: 7 (12.28%)
-    '    - Xml Docs: 57.14%
-    ' 
-    '   Blank Lines: 10 (17.54%)
-    '     File Size: 2.41 KB
+' Summaries:
 
 
-    '     Class MassLoader
-    ' 
-    '         Properties: massTable
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Sub: doMassLoadingOn
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 57
+'    Code Lines: 40 (70.18%)
+' Comment Lines: 7 (12.28%)
+'    - Xml Docs: 57.14%
+' 
+'   Blank Lines: 10 (17.54%)
+'     File Size: 2.41 KB
+
+
+'     Class MassLoader
+' 
+'         Properties: massTable
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Sub: doMassLoadingOn
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.BootstrapLoader.Engine
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Core
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.Model.Cellular
@@ -89,19 +90,6 @@ Namespace ModelLoader
                         Call massTable.AddNew(compound, MassRoles.compound)
                     End If
                 Next
-            Next
-
-            ' add missing kinetics factors at here?
-            For Each reaction As Reaction In cell.Phenotype.fluxes
-                If Not reaction.kinetics.paramVals.IsNullOrEmpty Then
-                    For Each factor As Object In reaction.kinetics.paramVals
-                        If TypeOf factor Is String Then
-                            If Not massTable.Exists(DirectCast(factor, String)) Then
-                                Call massTable.AddNew(DirectCast(factor, String), MassRoles.compound)
-                            End If
-                        End If
-                    Next
-                End If
             Next
 
             Dim complexID As String
