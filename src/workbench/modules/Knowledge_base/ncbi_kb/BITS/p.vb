4b2c2a6f613b35d03caed11ff28ac30e76421ec3
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-20 10:35:48 +0800

    split source file into multiple parts

---
+++ src/workbench/modules/Knowledge_base/ncbi_kb/BITS/p.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Xml.Serialization
+
+Namespace BITS
+    Public Class Paragraph
+
+        <XmlText> Public Property text As String
+
+        <XmlElement("related-object")>
+        Public Property related_object As RelatedObject()
+
+        <XmlElement> Public Property bold As String()
+
+        <XmlElement("ext-link")> Public Property links As ExtLink()
+        <XmlElement("italic")> Public Property italic As Italic()
+
+        Public Overrides Function ToString() As String
+            Return text
+        End Function
+
+    End Class
+
+    Public Class Italic
+
+        <XmlAttribute> Public Property toggle As String
+        <XmlText> Public Property text As String
+
+        Public Overrides Function ToString() As String
+            Return text
+        End Function
+
+    End Class
+
+    <XmlType("ext-link")>
+    Public Class ExtLink
+
+        <XmlAttribute("ext-link-type")> Public Property ext_link_type As String
+        <XmlAttribute("href", [Namespace]:=xlink)> Public Property href As String
+
+        Public Const xlink As String = "http://www.w3.org/1999/xlink"
+
+        <XmlText> Public Property text As String
+
+        Public Overrides Function ToString() As String
+            Return $"[{text}]({href})"
+        End Function
+    End Class
+
+    <XmlType("related-object")>
+    Public Class RelatedObject
+
+        <XmlAttribute("link-type")> Public Property link_type As String
+        <XmlAttribute("source-id")> Public Property source_id As String
+        <XmlAttribute("document-id")> Public Property document_id As String
+        <XmlAttribute("document-type")> Public Property document_type As String
+
+        Public Overrides Function ToString() As String
+            Return source_id
+        End Function
+
+    End Class
+End Namespace
---
+++ src/workbench/modules/Knowledge_base/ncbi_kb/BITS/table.vb
@@ -0,0 +1,76 @@
+﻿Imports System.Xml.Serialization
+
+Namespace BITS
+
+    Public Class TableWrap
+
+        <XmlAttribute> Public Property id As String
+        <XmlAttribute> Public Property orientation As String
+        <XmlAttribute> Public Property position As String
+
+        Public Property table As Table
+
+    End Class
+
+    Public Class Table
+
+        <XmlAttribute> Public Property frame As String
+        <XmlAttribute> Public Property rules As String
+
+        Public Property thead As THead
+        Public Property tbody As TBody
+
+    End Class
+
+    Public Class TBody
+
+        <XmlElement("tr")> Public Property tr As BodyRow()
+
+    End Class
+
+    Public Class THead
+
+        Public Property tr As HeaderRow
+
+        Public Overrides Function ToString() As String
+            Return tr.ToString
+        End Function
+
+    End Class
+
+    Public Class HeaderRow
+
+        <XmlElement("th")> Public Property header_cells As Cell()
+
+        Public Overrides Function ToString() As String
+            Return header_cells.Select(Function(th) th.ToString).GetJson
+        End Function
+
+    End Class
+
+    Public Class BodyRow
+
+        <XmlElement("td")> Public Property row_cells As Cell()
+
+        Public Overrides Function ToString() As String
+            Return row_cells.Select(Function(td) td.ToString).GetJson
+        End Function
+
+    End Class
+
+    Public Class Cell : Inherits Paragraph
+
+        <XmlAttribute> Public Property id As String
+        <XmlAttribute> Public Property valign As String
+        <XmlAttribute> Public Property align As String
+        <XmlAttribute> Public Property scope As String
+        <XmlAttribute> Public Property rowspan As String
+        <XmlAttribute> Public Property colspan As String
+        <XmlAttribute> Public Property headers As String
+
+        Public Overrides Function ToString() As String
+            Return text.ToString
+        End Function
+
+    End Class
+End Namespace
