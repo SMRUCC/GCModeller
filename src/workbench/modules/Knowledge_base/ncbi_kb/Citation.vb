9b2559e962847b66477a8a267d533ae52c0dd431
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-20 13:53:09 +0800

    add the citation data model

---
+++ src/workbench/modules/Knowledge_base/ncbi_kb/Citation.vb
@@ -0,0 +1,13 @@
+﻿Public Class Citation
+
+    Public Property authors As String()
+    Public Property title As String
+    Public Property journal As String
+    Public Property year As String
+    Public Property volume As String
+    Public Property fpage As String
+    Public Property lpage As String
+    Public Property doi As String
+    Public Property pubmed_id As UInteger
+
+End Class
