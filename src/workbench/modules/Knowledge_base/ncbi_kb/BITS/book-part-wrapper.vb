db807ae80a11a1d2d5fbb4513156e0087f439964
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-20 08:43:34 +0800

    create bits modelk

---
+++ src/workbench/modules/Knowledge_base/ncbi_kb/BITS/book-part-wrapper.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Xml.Serialization
+
+Namespace BITS
+
+    <XmlType("book-part-wrapper")>
+    Public Class BookPartWrapper
+
+        <XmlAttribute("id")> Public Property id As String
+        <XmlAttribute("content-type")> Public Property content_type As String
+        <XmlAttribute("from-where")> Public Property from_where As String
+        <XmlAttribute("dtd-version")> Public Property dtd_version As String
+
+    End Class
+End Namespace
