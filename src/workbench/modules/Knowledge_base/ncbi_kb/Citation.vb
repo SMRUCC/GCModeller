81f4e7e06ec453bc1bc9702c0b403dce3e78e849
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-20 14:02:11 +0800

    create abstract citation model

--- src/workbench/modules/Knowledge_base/ncbi_kb/Citation.vb
+++ src/workbench/modules/Knowledge_base/ncbi_kb/Citation.vb
@@ -10,4 +10,57 @@
     Public Property doi As String
     Public Property pubmed_id As UInteger
 
+    Public Overrides Function ToString() As String
+        Return nlm_cite()
+    End Function
+
+    ''' <summary>
+    ''' NLM
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function nlm_cite() As String
+        Dim authors = Me.authors.JoinBy(", ")
+        Return $"{authors}. {title}. {journal}. {year};{volume}:{fpage}-{lpage}. doi: {doi}. PMID: {pubmed_id}"
+    End Function
+
+    ''' <summary>
+    ''' AMA
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function ama_cite() As String
+        Dim authors = Me.authors.JoinBy(", ")
+        Return $"{authors}. {title}. {journal}. {year};{volume}:{fpage}-{lpage}. doi:{doi}"
+    End Function
+
+    ''' <summary>
+    ''' APA
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function apa_cite() As String
+        Dim authors As String
+
+        If Me.authors.Length = 1 Then
+            authors = Me.authors(0)
+        Else
+            authors = Me.authors.First & $", & {Me.authors.Last}"
+        End If
+
+        Return $"{authors}. ({year}). {title}. {journal}, {volume}, {fpage}-{lpage}. https://doi.org/{doi}"
+    End Function
+
+    ''' <summary>
+    ''' MLA
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function mla_cite() As String
+        Dim authors As String
+
+        If Me.authors.Length = 1 Then
+            authors = Me.authors(0)
+        Else
+            authors = Me.authors.First & $", and {Me.authors.Last}"
+        End If
+
+        Return $"{authors}. ""{title}."" {journal} vol. {volume}({year}): {fpage}-{lpage}. doi:{doi}"
+    End Function
 End Class
