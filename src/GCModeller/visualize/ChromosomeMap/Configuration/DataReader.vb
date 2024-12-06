8d47f88fc34f6a9bfa28371924d846dbb335ce40
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-06 22:41:27 +0800

    fix of the reference error

--- src/GCModeller/visualize/ChromosomeMap/Configuration/DataReader.vb
+++ src/GCModeller/visualize/ChromosomeMap/Configuration/DataReader.vb
@@ -1,72 +1,73 @@
 ﻿#Region "Microsoft.VisualBasic::f58d9e7c0fbbf6f52650a83790463ed1, visualize\ChromosomeMap\Configuration\DataReader.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 67
-    '    Code Lines: 35 (52.24%)
-    ' Comment Lines: 24 (35.82%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 8 (11.94%)
-    '     File Size: 3.02 KB
+' Summaries:
 
 
-    '     Class DataReader
-    ' 
-    ' 
-    '         Enum TextAlignment
-    ' 
-    '             Left, Middle, Right
-    ' 
-    ' 
-    ' 
-    '  
-    ' 
-    '     Properties: AddLegend, DefaultRNAColor, DeletionMutation, FLAG_HEIGHT, FlagLength
-    '                 FunctionAlignment, FunctionAnnotationFont, GeneObjectHeight, IntegrationMutant, LegendFont
-    '                 LineHeight, LineLength, LocusTagFont, Margin, NoneCogColor
-    '                 Resolution, ribosomalRNAColor, SavedFormat, SecondaryRuleFont, tRNAColor
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 67
+'    Code Lines: 35 (52.24%)
+' Comment Lines: 24 (35.82%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 8 (11.94%)
+'     File Size: 3.02 KB
+
+
+'     Class DataReader
+' 
+' 
+'         Enum TextAlignment
+' 
+'             Left, Middle, Right
+' 
+' 
+' 
+'  
+' 
+'     Properties: AddLegend, DefaultRNAColor, DeletionMutation, FLAG_HEIGHT, FlagLength
+'                 FunctionAlignment, FunctionAnnotationFont, GeneObjectHeight, IntegrationMutant, LegendFont
+'                 LineHeight, LineLength, LocusTagFont, Margin, NoneCogColor
+'                 Resolution, ribosomalRNAColor, SavedFormat, SecondaryRuleFont, tRNAColor
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
 Imports System.Drawing.Imaging
 Imports Microsoft.VisualBasic.Serialization
+Imports Microsoft.VisualBasic.Imaging
 
 #If NET48 Then
 Imports Pen = System.Drawing.Pen
@@ -154,6 +155,6 @@ Namespace Configuration
         Public Property NoneCogColor As Color
 #End Region
 
-        Public Property SavedFormat As ImageFormat
+        Public Property SavedFormat As ImageFormats
     End Class
 End Namespace
