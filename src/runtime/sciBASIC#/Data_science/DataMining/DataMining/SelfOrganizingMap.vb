276d48190a05ab1987ea4cb70cfa48cc0b090207
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-13 09:05:00 +0800

    make code style improvements

--- src/runtime/sciBASIC#/Data_science/DataMining/DataMining/SelfOrganizingMap.vb
+++ src/runtime/sciBASIC#/Data_science/DataMining/DataMining/SelfOrganizingMap.vb
@@ -2,43 +2,59 @@
 Imports Microsoft.VisualBasic.Math.Correlations
 Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
 
+''' <summary>
+''' SOM: Self-Organizing Map
+''' </summary>
 Public Class SelfOrganizingMap
 
-    Private ReadOnly numberOfNeurons As Integer
+    ReadOnly numberOfNeurons As Integer
+
+    ''' <summary>
+    ''' data channel depth
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property depth As Integer
 
     ''' <summary>
     ''' weight matrix
     ''' </summary>
-    Private neuronWeights As Double()()
+    Dim neuronWeights As Double()()
 
-    Public Sub New(numberOfNeurons As Integer)
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="numberOfNeurons">number of the neurons</param>
+    Public Sub New(numberOfNeurons As Integer, depth As Integer)
         Me.numberOfNeurons = numberOfNeurons
-        neuronWeights = RectangularArray.Matrix(Of Double)(numberOfNeurons, 3)
+        Me.depth = depth
+
+        neuronWeights = RectangularArray.Matrix(Of Double)(numberOfNeurons, depth)
     End Sub
 
-    Public Overridable Sub train(pixels As Double()())
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="pixels">dataset for run the training, data should be an rectangle array, with 2nd dimension size should be equals to <see cref="depth"/>.</param>
+    Public Overridable Sub train(pixels As Double()(), Optional learningRate As Double = 0.9, Optional numberOfIterations As Integer = 500)
         Dim numberOfPixels = pixels.Length
         Dim numberOfFeatures = pixels(0).Length
+        Dim globalMax As Double = Aggregate f In pixels Into Max(f.Max)
 
         ' Initialize neuron weights randomly
         Dim w = RectangularArray.Matrix(Of Double)(numberOfNeurons, numberOfFeatures)
-        For i = 0 To numberOfNeurons - 1
-            For j = 0 To numberOfFeatures - 1
-                w(i)(j) = randf.Next(256)
+        For i As Integer = 0 To numberOfNeurons - 1
+            For j As Integer = 0 To numberOfFeatures - 1
+                w(i)(j) = randf.Next(globalMax)
             Next
         Next
-
-        ' SOM training parameters
-        Dim learningRate = 0.9
-        Dim numberOfIterations = 500
 
         ' SOM training algorithm
         For iteration As Integer = 0 To numberOfIterations - 1
             ' Randomly shuffle the pixels
-            shufflePixels(pixels)
+            Call shufflePixels(pixels)
 
             ' Update neuron weights for each pixel
-            For i = 0 To numberOfPixels - 1
+            For i As Integer = 0 To numberOfPixels - 1
                 Dim pixel = pixels(i)
                 Dim nearestNeuron = findNearestNeuron(pixel, w)
 
@@ -54,7 +70,7 @@ Public Class SelfOrganizingMap
     End Sub
 
     Private Sub shufflePixels(ByRef pixels As Double()())
-        For i = pixels.Length - 1 To 1 Step -1
+        For i As Integer = pixels.Length - 1 To 1 Step -1
             Dim j = randf.Next(i + 1)
             Dim temp = pixels(i)
             pixels(i) = pixels(j)
@@ -81,6 +97,7 @@ Public Class SelfOrganizingMap
 
     Private Sub updateNeuronWeights(pixel As Double(), neuronWeightsMatrix As Double()(), nearestNeuron As Integer, learningRate As Double)
         Dim neuronWeights = neuronWeightsMatrix(nearestNeuron)
+
         For i As Integer = 0 To neuronWeights.Length - 1
             neuronWeights(i) += CInt(learningRate * (pixel(i) - neuronWeights(i)))
         Next
@@ -91,13 +108,12 @@ Public Class SelfOrganizingMap
     ''' </summary>
     ''' <param name="pixels"></param>
     ''' <returns></returns>
-    Public Overridable Function assignPixels(pixels As Double()()) As Integer()
+    Public Overridable Function ClusterId(pixels As Double()()) As Integer()
         Dim numberOfPixels = pixels.Length
         Dim assignedNeurons = New Integer(numberOfPixels - 1) {}
 
-        For i = 0 To numberOfPixels - 1
-            Dim pixel = pixels(i)
-            assignedNeurons(i) = findNearestNeuron(pixel, neuronWeights)
+        For i As Integer = 0 To numberOfPixels - 1
+            assignedNeurons(i) = findNearestNeuron(pixels(i), neuronWeights)
         Next
 
         Return assignedNeurons
