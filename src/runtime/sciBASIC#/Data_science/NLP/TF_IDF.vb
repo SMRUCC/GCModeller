abe0d1c89a4720073b139b32d4fc54e9d44d56d8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-20 16:41:53 +0800

    create test

--- src/runtime/sciBASIC#/Data_science/NLP/TF_IDF.vb
+++ src/runtime/sciBASIC#/Data_science/NLP/TF_IDF.vb
@@ -278,21 +278,4 @@ Public Class TF_IDF
         Next
         Return System.Math.Sqrt(sum)
     End Function
-
-
-    ''' <summary>
-    ''' Testing </summary>
-    ''' <param name="args">  </param>
-    Public Shared Sub Main(args As String())
-        Dim docs = New String() {"knowledge building needs innovative environments are better at helping their inhabitants explore the adjacent possible", "As a basis for evaluating explanations, creative knowledge building weight of evidence is a poor substitute for the first two criteria listed above.", "A public idea database makes every passing idea visible to everyone else in the organization and do creative work.", "questioning and various disturbances initiate cycles of innovation and creative organization knowledge.", "We need some way to ensure knowledge to spread among environments that any notes that are dropped are dropped."}
-
-        Dim tfIdf As TF_IDF = New TF_IDF(docs)
-        For i = 0 To tfIdf.docs.Count - 1
-            Console.Write(i + 1.ToString() & vbTab)
-            For j = 0 To tfIdf.docs.Count - 1
-                Console.Write(tfIdf.getSimilarity(i, j).ToString() & vbTab)
-            Next
-            Console.WriteLine()
-        Next
-    End Sub
 End Class
---
+++ src/runtime/sciBASIC#/Data_science/NLP/test/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic.Data.NLP
+
+Module Module1
+
+    Sub testtfidf()
+        Dim docs = New String() {"knowledge building needs innovative environments are better at helping their inhabitants explore the adjacent possible", "As a basis for evaluating explanations, creative knowledge building weight of evidence is a poor substitute for the first two criteria listed above.", "A public idea database makes every passing idea visible to everyone else in the organization and do creative work.", "questioning and various disturbances initiate cycles of innovation and creative organization knowledge.", "We need some way to ensure knowledge to spread among environments that any notes that are dropped are dropped."}
+
+        Dim tfIdf As TF_IDF = New TF_IDF(docs)
+        For i = 0 To tfIdf.docs.Count - 1
+            Console.Write(i + 1.ToString() & vbTab)
+            For j = 0 To tfIdf.docs.Count - 1
+                Console.Write(tfIdf.getSimilarity(i, j).ToString() & vbTab)
+            Next
+            Console.WriteLine()
+        Next
+    End Sub
+End Module
