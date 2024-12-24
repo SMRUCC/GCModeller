96b34f5f4084793528f14547b48698a7626eaaac
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-24 15:07:02 +0800

    create verbose debug progress for the correlation graph builder

--- src/GCModeller/annotations/WGCNA/WGCNA/Analysis.vb
+++ src/GCModeller/annotations/WGCNA/WGCNA/Analysis.vb
@@ -1,44 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::bf64092f3be5a477066e845bc5504004, annotations\WGCNA\WGCNA\Analysis.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Analysis
-    ' 
-    '     Function: createGraph, Run
-    ' 
-    ' /********************************************************************************/
+' Module Analysis
+' 
+'     Function: createGraph, Run
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.DataMining.HierarchicalClustering
@@ -99,11 +100,15 @@ Public Module Analysis
         Dim geneId As String() = samples.expression.Keys.ToArray
         Dim g As New NetworkGraph
 
+        Call VBDebugger.EchoLine("assign the gene id nodes.")
+
         For Each id As String In geneId
             Call g.CreateNode(id)
         Next
 
-        For i As Integer = 0 To geneId.Length - 1
+        Call VBDebugger.EchoLine("create links between the gene expression.")
+
+        For Each i As Integer In TqdmWrapper.Range(0, geneId.Length)
             For j As Integer = 0 To geneId.Length - 1
                 If i <> j AndAlso mat(i, j) <> 0.0 Then
                     Call g.AddEdge(geneId(i), geneId(j), weight:=mat(i, j))
