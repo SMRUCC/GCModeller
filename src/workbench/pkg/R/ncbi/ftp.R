e634ed9c48f9417d2b58db71f127665a610d667d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-04 17:50:58 +0800

    use wget for ftp download

--- src/workbench/pkg/R/ncbi/ftp.R
+++ src/workbench/pkg/R/ncbi/ftp.R
@@ -5,15 +5,17 @@
 const ncbi_assembly_ftp = function(ref, download_dir = "./", overrides = TRUE) {
     imports "ftp" from "webKit";
 
-    let ncbi = new ftp(server = "ftp.ncbi.nih.gov");
-    let url  = gsub([ref]::ftp_path, "https://ftp.ncbi.nlm.nih.gov", "");
+    # let ncbi = new ftp(server = "ftp.ncbi.nih.gov");
+    let url  = gsub([ref]::ftp_path, "https://ftp.ncbi.nlm.nih.gov/", "");
     let name = basename(url,TRUE);
-    let ftp_url = `${url}/${name}_genomic.gbff.gz`;
+    let ftp_url = `ftp://ftp.ncbi.nih.gov/${url}/${name}_genomic.gbff.gz`;
     let local_path = `${download_dir}/${basename(ftp_url,TRUE)}`;
 
     print(`ftp get: ${ftp_url}`);
+    dir.create(download_dir);
 
     if (overrides || (file.size(local_path) <= 0)) {
-        ncbi |> ftp.get(ftp_url, download_dir & "/");
+        # ncbi |> ftp.get(ftp_url, download_dir & "/");
+        @`wget "${ftp_url}" -O "${local_path}"`;
     }
 }
