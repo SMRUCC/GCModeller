135caa3ea80aa4e1348388fe3ede08ab59327960
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-04 16:53:40 +0800

    implements tools for run batch download

---
+++ src/workbench/pkg/tools/download_ncbi_genbank.R
@@ -0,0 +1,14 @@
+require(GCModeller);
+
+imports "NCBI" from "annotationKit";
+
+let index_file = ?"--index" || stop("a summary index file for download ncbi genbank is required!");
+let download_dir = ?"--save_dir" || "./ncbi_genbank_ftp";
+let index_data = NCBI::genome_assembly_index(index_file);
+
+for(let ref in index_data) {
+    let key = md5([ref]::assembly_accession);
+    let subdir = `${substr(key,2,4)}/${substr(key,8,11)}`;
+    
+    ncbi_assembly_ftp(ref, `${download_dir}/${subdir}`);
+}
