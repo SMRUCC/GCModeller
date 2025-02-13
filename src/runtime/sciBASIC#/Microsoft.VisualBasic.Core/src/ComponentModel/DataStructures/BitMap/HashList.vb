664f0b22ace65e010abdd7d1ddf49a814ee63ac0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-13 09:27:01 +0800

    how to make a fast hash list for fast removes and fast index of?

---
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/ComponentModel/DataStructures/BitMap/HashList.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic.Linq
+
+Namespace ComponentModel
+
+    Public Class HashList(Of T As IAddressOf) : Implements Enumeration(Of T)
+
+
+        Public Sub New()
+        End Sub
+
+        Public Iterator Function GenericEnumerator() As IEnumerator(Of T) Implements Enumeration(Of T).GenericEnumerator
+
+        End Function
+    End Class
+End Namespace
--- src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/ComponentModel/System.Collections.Generic/IndexOf.vb
+++ src/runtime/sciBASIC#/Microsoft.VisualBasic.Core/src/ComponentModel/System.Collections.Generic/IndexOf.vb
@@ -466,7 +466,7 @@ Namespace ComponentModel.Collection
         End Operator
 
         Public Iterator Function GetEnumerator() As IEnumerator(Of SeqValue(Of T)) Implements IEnumerable(Of SeqValue(Of T)).GetEnumerator
-            For Each o As SeqValue(Of T) In index
+            For Each o As SeqValue(Of T) In index.AsEnumerable
                 ' 20231227
                 ' handling of the delete operation result
                 If o.value Is Nothing AndAlso o.i = 0 Then
