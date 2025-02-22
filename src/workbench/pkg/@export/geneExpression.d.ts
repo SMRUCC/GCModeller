5e8c550f5bf5fbbe35d278979c972c2f625911fa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-22 17:56:19 +0800

    run msbuild

--- src/workbench/pkg/@export/geneExpression.d.ts
+++ src/workbench/pkg/@export/geneExpression.d.ts
@@ -404,7 +404,7 @@ declare namespace geneExpression {
      *  2. 'image' is a bitmap image that plot based on the object cluster patterns data.
      *  3. 'pdf' is a pdf image that could be edit
    */
-   function peakCMeans(matrix: object, nsize?: string, threshold?: number, fuzzification?: number, plotSize?: any, colorSet?: string, memberCutoff?: number, empty_shared?: object, max_cluster_shared?: object, xlab?: string, ylab?: string, top_members?: number, cluster_label_css?: string, legend_title_css?: string, legend_tick_css?: string, axis_tick_css?: string, axis_label_css?: string, env?: object): any;
+   function peakCMeans(matrix: object, nsize?: any, threshold?: number, fuzzification?: number, plotSize?: any, colorSet?: string, memberCutoff?: number, empty_shared?: object, max_cluster_shared?: object, xlab?: string, ylab?: string, top_members?: number, cluster_label_css?: string, legend_title_css?: string, legend_tick_css?: string, axis_tick_css?: string, axis_label_css?: string, env?: object): any;
    /**
     * read the cmeans expression pattern result from file
     * 
