06b900b3422fe4856a243d32f463792444197815
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-25 13:12:47 +0800

    config test reader

--- src/GCModeller/data/RCSB PDB/test/Module1.vb
+++ src/GCModeller/data/RCSB PDB/test/Module1.vb
@@ -1,7 +1,11 @@
-﻿Module Module1
+﻿Imports SMRUCC.genomics.Data.RCSB.PDB
+
+Module Module1
 
     Sub Main()
+        Dim pdb As PDB = PDB.Load("F:\complex.1.pdb")
 
+        Pause()
     End Sub
 
 End Module
