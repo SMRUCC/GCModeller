87fbd2450a943a2e1dbea242b2d876378c1066dc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-15 21:56:51 +0800

    fix of the missing comment text

--- src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/PostScript/SpatialLookup.vb
+++ src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/PostScript/SpatialLookup.vb
@@ -27,21 +27,23 @@ Namespace PostScript
             Dim endX As UInteger = HashX(pos.X + size.Width)
             Dim startY As UInteger = HashY(pos.Y)
             Dim endY As UInteger = HashY(pos.Y + size.Height)
+            Dim cX = CUInt((startX + endX) / 2)
+            Dim cY = CUInt((startY + endY) / 2)
 
-            For x As UInteger = startX To endX
-                If Not grid.ContainsKey(x) Then
-                    Call grid.Add(x, New Dictionary(Of UInteger, List(Of PSElement)))
+            For Each xy As UInteger() In New UInteger()() {
+                    New UInteger() {startX, startY}, New UInteger() {cX, startY}, New UInteger() {endX, startY},
+                    New UInteger() {startX, cY}, New UInteger() {cX, cY}, New UInteger() {endX, cY},
+                    New UInteger() {startX, endY}, New UInteger() {cX, endY}, New UInteger() {endX, endY}
+                }
+
+                If Not grid.ContainsKey(xy(0)) Then
+                    Call grid.Add(xy(0), New Dictionary(Of UInteger, List(Of PSElement)))
+                End If
+                If Not grid(xy(0)).ContainsKey(xy(1)) Then
+                    Call grid(xy(0)).Add(xy(1), New List(Of PSElement))
                 End If
 
-                Dim col = grid(key:=x)
-
-                For y As UInteger = startY To endY
-                    If Not col.ContainsKey(y) Then
-                        Call col.Add(y, New List(Of PSElement))
-                    End If
-
-                    Call col(key:=y).Add(shape)
-                Next
+                Call grid(xy(0))(xy(1)).Add(shape)
             Next
         End Sub
 
@@ -53,6 +55,25 @@ Namespace PostScript
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Private Function HashY(y As Single) As UInteger
             Return CUInt(std.Floor(y / gridSize.Height))
+        End Function
+
+        Public Function FindCommentShapeByPoint(x As Single, y As Single) As PSElement
+            Dim hX As UInteger = HashX(x)
+            Dim hY As UInteger = HashY(y)
+
+            If grid.ContainsKey(hX) Then
+                Dim col = grid(key:=hX)
+
+                If col.ContainsKey(hY) Then
+                    Return col(key:=hY) _
+                        .Where(Function(s)
+                                   Return Not s.comment.StringEmpty(, True)
+                               End Function) _
+                        .LastOrDefault
+                End If
+            End If
+
+            Return Nothing
         End Function
 
         Public Function FindShapeByPoint(x As Single, y As Single) As PSElement
