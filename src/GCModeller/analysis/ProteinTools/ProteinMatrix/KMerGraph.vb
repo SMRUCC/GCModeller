0511d7fececcb0dfc3dd0dc317af7562ee2a7c30
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-16 00:44:37 +0800

    create k-mer graph model

---
+++ src/GCModeller/analysis/ProteinTools/ProteinMatrix/KMerGraph.vb
@@ -0,0 +1,40 @@
+﻿Imports Microsoft.VisualBasic.Data.GraphTheory.Analysis.MorganFingerprint
+Imports Microsoft.VisualBasic.Data.GraphTheory.Network
+Imports Microsoft.VisualBasic.Math.HashMaps
+Imports SMRUCC.genomics.SequenceModel.FASTA
+
+Public Class KMerGraph : Implements MorganGraph(Of KmerNode, KmerEdge)
+
+    Public Property KMers As KmerNode() Implements MorganGraph(Of KmerNode, KmerEdge).Atoms
+    Public ReadOnly Property Graph As KmerEdge() Implements MorganGraph(Of KmerNode, KmerEdge).Graph
+
+    Public Shared Function FromSequence(seq As ISequenceProvider) As KMerGraph
+
+    End Function
+
+    Public Shared Function HashKMer(kmer As KSeq) As ULong
+        Dim hashcode As ULong = 0
+
+        For Each c As Char In kmer.Seq
+            hashcode = HashMap.HashCodePair(hashcode, CULng(Asc(c)))
+        Next
+
+        Return hashcode
+    End Function
+
+End Class
+
+Public Class KmerNode : Implements IMorganAtom
+
+    Public Property Index As Integer Implements IMorganAtom.Index
+    Public Property Code As ULong Implements IMorganAtom.Code
+    Public Property Type As String Implements IMorganAtom.Type
+
+End Class
+
+Public Class KmerEdge : Implements IndexEdge
+
+    Public Property U As Integer Implements IndexEdge.U
+    Public Property V As Integer Implements IndexEdge.V
+
+End Class
