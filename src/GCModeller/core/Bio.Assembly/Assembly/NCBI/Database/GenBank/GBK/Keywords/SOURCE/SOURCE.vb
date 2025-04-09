6534cfae76e207684b6e2693f9eb9d1f1f8563a0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-09 14:26:43 +0800

    set BIOM string

--- src/GCModeller/core/Bio.Assembly/Assembly/NCBI/Database/GenBank/GBK/Keywords/SOURCE/SOURCE.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/NCBI/Database/GenBank/GBK/Keywords/SOURCE/SOURCE.vb
@@ -1,60 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::75f7e4e05ec98d219e9e30935269adf5, core\Bio.Assembly\Assembly\NCBI\Database\GenBank\GBK\Keywords\SOURCE\SOURCE.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 38
-    '    Code Lines: 23 (60.53%)
-    ' Comment Lines: 7 (18.42%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 8 (21.05%)
-    '     File Size: 1.16 KB
+' Summaries:
 
 
-    '     Class SOURCE
-    ' 
-    '         Properties: OrganismHierarchy, SpeciesName
-    ' 
-    '         Function: GetTaxonomy, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 38
+'    Code Lines: 23 (60.53%)
+' Comment Lines: 7 (18.42%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 8 (21.05%)
+'     File Size: 1.16 KB
+
+
+'     Class SOURCE
+' 
+'         Properties: OrganismHierarchy, SpeciesName
+' 
+'         Function: GetTaxonomy, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.Metagenomics
 
 Namespace Assembly.NCBI.GenBank.GBFF.Keywords
 
@@ -69,6 +70,12 @@ Namespace Assembly.NCBI.GenBank.GBFF.Keywords
         ''' </summary>
         ''' <returns></returns>
         Public Property OrganismHierarchy As ORGANISM
+
+        Public ReadOnly Property BiomString As String
+            Get
+                Return BIOMTaxonomy.TaxonomyString(OrganismHierarchy.Lineage)
+            End Get
+        End Property
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Function GetTaxonomy() As Metagenomics.Taxonomy
