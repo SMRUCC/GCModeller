802df1a63fff7fede4d1b6de23176402a8e37e78
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-18 09:14:48 +0800

    try to make bugs about the source metadata parser

--- src/GCModeller/data/RCSB PDB/PDB/Keywords/Headers/MoleculeMetadata.vb
+++ src/GCModeller/data/RCSB PDB/PDB/Keywords/Headers/MoleculeMetadata.vb
@@ -1,74 +1,74 @@
 ﻿#Region "Microsoft.VisualBasic::c8b0f1fdcf87017aae45183028733eae, data\RCSB PDB\PDB\Keywords\Headers\MoleculeMetadata.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 125
-    '    Code Lines: 96 (76.80%)
-    ' Comment Lines: 1 (0.80%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 28 (22.40%)
-    '     File Size: 4.01 KB
+' Summaries:
 
 
-    '     Class MoleculeMetadata
-    ' 
-    '         Properties: Mols
-    ' 
-    '         Sub: Flush
-    ' 
-    '     Class Compound
-    ' 
-    '         Properties: Keyword
-    ' 
-    '         Function: Append
-    ' 
-    '     Class Properties
-    ' 
-    '         Properties: id, metadata
-    ' 
-    '         Sub: add
-    ' 
-    '     Class Source
-    ' 
-    '         Properties: Keyword
-    ' 
-    '         Function: Append
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 125
+'    Code Lines: 96 (76.80%)
+' Comment Lines: 1 (0.80%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 28 (22.40%)
+'     File Size: 4.01 KB
+
+
+'     Class MoleculeMetadata
+' 
+'         Properties: Mols
+' 
+'         Sub: Flush
+' 
+'     Class Compound
+' 
+'         Properties: Keyword
+' 
+'         Function: Append
+' 
+'     Class Properties
+' 
+'         Properties: id, metadata
+' 
+'         Sub: add
+' 
+'     Class Source
+' 
+'         Properties: Keyword
+' 
+'         Function: Append
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -90,26 +90,21 @@ Namespace Keywords
             Mols = New Dictionary(Of String, Properties)
 
             For Each line As String In lines
-                If line.Last <> ";"c Then
+                If InStr(line, ": ") = 0 Then
+                    ' line of metadata
+                    ' end of multiple line
                     If last Is Nothing Then
-                        last = line
+                        Call $"Invalid multiple line document({line}) for the metadata parser!".Warning
                     Else
                         last = last & " " & line.GetTagValue(" ", trim:=True).Value
                     End If
 
                     Continue For
-                ElseIf InStr(line, ": ") = 0 Then
-                    ' end of multiple line
-                    If last Is Nothing Then
-                        Call $"Invalid multiple line document({line}) for the metadata parser!".Warning
-                        Continue For
-                    Else
-                        last = last & " " & line.GetTagValue(" ", trim:=True).Value
-                        tag = last.GetTagValue(":", trim:=True)
-                        last = Nothing
-                    End If
+                ElseIf last Is Nothing Then
+                    Continue For
                 Else
-                    tag = line.GetTagValue(":", trim:=True)
+                    tag = last.GetTagValue(":", trim:=True)
+                    last = line
                 End If
 
                 If tag.Name.IsPattern("\d+ [^\s]+") Then
