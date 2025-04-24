f9b5a241973ce64582edea7d6eff40c852ba1f0c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-24 15:33:01 +0800

    make improvements of the frame detection object m,odels

--- src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/MachineVision/FrameData.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/MachineVision/FrameData.vb
@@ -1,14 +1,24 @@
 Imports System.Drawing
+Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Serialization.JSON
 
-Public Class FrameData
+Public Class FrameData : Implements Enumeration(Of Detection)
 
     <XmlAttribute> Public Property FrameID As Integer
     <XmlElement("Objects")>
     Public Property Detections As Detection()
 
+    Sub New()
+    End Sub
+
+    Sub New(id As Integer, detections As IEnumerable(Of Detection))
+        _FrameID = id
+        _Detections = detections.SafeQuery.ToArray
+    End Sub
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Overrides Function ToString() As String
         Return $"#{FrameID} " & Detections _
             .SafeQuery _
@@ -16,11 +26,17 @@ Public Class FrameData
             .GetJson
     End Function
 
+    Public Iterator Function GenericEnumerator() As IEnumerator(Of Detection) Implements Enumeration(Of Detection).GenericEnumerator
+        For Each obj As Detection In Detections.SafeQuery
+            Yield obj
+        Next
+    End Function
 End Class
 
 Public Class Detection
 
-    <XmlAttribute> Public Property ObjectID As String
+    <XmlAttribute>
+    Public Property ObjectID As String
     Public Property Position As PointF
 
     Public Overrides Function ToString() As String
