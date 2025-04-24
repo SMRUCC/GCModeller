fd1675ca12cb979fde28845bec9a4ca78bebcb9b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-24 15:20:29 +0800

    make improvements of the data model

--- src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/MachineVision/FrameData.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/MachineVision/FrameData.vb
@@ -1,13 +1,30 @@
 Imports System.Drawing
+Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Serialization.JSON
 
 Public Class FrameData
-    Public Property FrameID As Integer
-    Public Detections As New List(Of Detection)
+
+    <XmlAttribute> Public Property FrameID As Integer
+    <XmlElement("Objects")>
+    Public Property Detections As Detection()
+
+    Public Overrides Function ToString() As String
+        Return $"#{FrameID} " & Detections _
+            .SafeQuery _
+            .Select(Function(a) a.ObjectID) _
+            .GetJson
+    End Function
 
 End Class
 
 Public Class Detection
-    Public Property ObjectID As String
+
+    <XmlAttribute> Public Property ObjectID As String
     Public Property Position As PointF
+
+    Public Overrides Function ToString() As String
+        Return $"{ObjectID} [x:{Position.X}, y:{Position.Y}]"
+    End Function
 End Class
 
--- src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/MachineVision/Trajectory.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/MachineVision/Trajectory.vb
@@ -1,21 +1,32 @@
 ﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
 
+''' <summary>
+''' Trajectory of a object
+''' </summary>
 Public Class Trajectory
+
     Public ReadOnly Property TrajectoryID As Integer
-    Public positions As New List(Of PointF)
-
-    Public Sub New(id As Integer, initialDetection As Detection)
-        TrajectoryID = id
-        positions.Add(initialDetection.Position)
-    End Sub
-
-    Public Sub Update(detection As Detection)
-        positions.Add(detection.Position)
-    End Sub
+    Public ReadOnly Property positions As New List(Of PointF)
 
     Public ReadOnly Property LastPosition As PointF
         Get
             Return positions.Last()
         End Get
     End Property
+
+    Public Sub New(id As Integer, t0 As Detection)
+        TrajectoryID = id
+        positions.Add(t0.Position)
+    End Sub
+
+    ''' <summary>
+    ''' Add last position to the current object trajectory
+    ''' </summary>
+    ''' <param name="detection"></param>
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Sub Update(detection As Detection)
+        Call positions.Add(detection.Position)
+    End Sub
+
 End Class
