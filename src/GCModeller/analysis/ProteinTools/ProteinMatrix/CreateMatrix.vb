6b27d4fa4f88f83b962ecdfb57d8ad03330284cc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-18 10:34:02 +0800

    make general

--- src/GCModeller/analysis/ProteinTools/ProteinMatrix/CreateMatrix.vb
+++ src/GCModeller/analysis/ProteinTools/ProteinMatrix/CreateMatrix.vb
@@ -1,56 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::152ebaab3670da526bb7ac818ac7125e, analysis\ProteinTools\ProteinMatrix\CreateMatrix.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 49
-    '    Code Lines: 33 (67.35%)
-    ' Comment Lines: 7 (14.29%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 9 (18.37%)
-    '     File Size: 1.50 KB
+' Summaries:
 
 
-    ' Class CreateMatrix
-    ' 
-    '     Properties: dimension
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: ToMatrix, ToVector
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 49
+'    Code Lines: 33 (67.35%)
+' Comment Lines: 7 (14.29%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 9 (18.37%)
+'     File Size: 1.50 KB
+
+
+' Class CreateMatrix
+' 
+'     Properties: dimension
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: ToMatrix, ToVector
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -58,6 +58,8 @@ Imports System.Drawing
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.GraphTheory
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Scripting.Runtime
+Imports SMRUCC.genomics.SequenceModel
 Imports SMRUCC.genomics.SequenceModel.FASTA
 Imports SMRUCC.genomics.SequenceModel.Polypeptides
 
@@ -66,10 +68,10 @@ Imports SMRUCC.genomics.SequenceModel.Polypeptides
 ''' </summary>
 Public Class CreateMatrix
 
-    Dim sgt As SequenceGraphTransform
+    ReadOnly sgt As SequenceGraphTransform
 
     ''' <summary>
-    ''' get the dimension size of the generated protein matrix via the function <see cref="ToMatrix(FastaSeq)"/>
+    ''' get the dimension size of the generated biological sequence matrix via the function <see cref="ToMatrix(FastaSeq)"/>
     ''' </summary>
     ''' <returns>The CNN input size</returns>
     Public ReadOnly Property dimension As Size
@@ -79,16 +81,28 @@ Public Class CreateMatrix
         End Get
     End Property
 
-    Sub New()
-        Dim allChars As String() = AminoAcidObjUtility _
-            .AminoAcidLetters _
-            .JoinIterates("-") _
-            .Select(Function(c) c.ToString) _
-            .ToArray
+    Sub New(Optional mol As SeqTypes = SeqTypes.Protein)
+        Dim allChars As String() = GetChars(mol)
 
         sgt = New SequenceGraphTransform
         sgt.set_alphabets(allChars)
     End Sub
+
+    Private Shared Function GetChars(mol As SeqTypes) As String()
+        Select Case mol
+            Case SeqTypes.Protein
+                Return AminoAcidObjUtility _
+                    .AminoAcidLetters _
+                    .JoinIterates("-") _
+                    .AsCharacter _
+                    .ToArray
+            Case Else
+                Return mol.GetVector _
+                    .JoinIterates({"-"c, "N"c}) _
+                    .AsCharacter _
+                    .ToArray
+        End Select
+    End Function
 
     Public Function ToMatrix(prot As FastaSeq) As Double()()
         Dim v = sgt.fit(prot.SequenceData)
