6a92d32a50e551bfa05de1ae8d7fa9364f1e571a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-14 22:13:22 +0800

    Create MemoryDataSet.vb

---
+++ src/GCModeller/engine/BootstrapLoader/Engine/MemoryDataSet.vb
@@ -0,0 +1,25 @@
+﻿Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Engine
+
+Public Class MemoryDataSet : Implements IOmicsDataAdapter
+
+    Public ReadOnly Property mass As OmicsTuple(Of String()) Implements IOmicsDataAdapter.mass
+
+    Dim massRows As New List(Of Dictionary(Of String, Double))
+    Dim fluxRows As New List(Of Dictionary(Of String, Double))
+
+    Public Sub MassSnapshot(iteration As Integer, data As Dictionary(Of String, Double)) Implements IOmicsDataAdapter.MassSnapshot
+        massRows.Add(data)
+    End Sub
+
+    Public Sub FluxSnapshot(iteration As Integer, data As Dictionary(Of String, Double)) Implements IOmicsDataAdapter.FluxSnapshot
+        fluxRows.Add(data)
+    End Sub
+
+    Public Function getMassDataSet() As ICollection(Of Dictionary(Of String, Double))
+        Return massRows
+    End Function
+
+    Public Function getFluxDataSet() As ICollection(Of Dictionary(Of String, Double))
+        Return fluxRows
+    End Function
+End Class
