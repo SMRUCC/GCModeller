bd16bd7e25385b9194bee8d07aadfdcd9be87351
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-16 16:23:27 +0800

    Create GenomeCompiler.vb

---
+++ src/GCModeller/engine/Compiler/MarkupCompiler/BioCyc/GenomeCompiler.vb
@@ -0,0 +1,50 @@
+﻿
+Imports SMRUCC.genomics.Data.BioCyc
+Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
+
+Namespace MarkupCompiler.BioCyc
+
+    Public Class GenomeCompiler
+
+        ReadOnly biocyc As Workspace
+        ReadOnly geneIndex As Dictionary(Of String, genes)
+
+        Sub New(compiler As v2Compiler)
+            biocyc = compiler.biocyc
+            geneIndex = biocyc.genes.features.ToDictionary(Function(g) g.uniqueId)
+        End Sub
+
+        Public Function CreateReplicon() As replicon
+            Return New replicon With {
+                .genomeName = biocyc.species.commonName,
+                .isPlasmid = False,
+                .operons = CreateOperons.ToArray
+            }
+        End Function
+
+        Private Iterator Function CreateOperons() As IEnumerable(Of TranscriptUnit)
+            For Each operon As transunits In biocyc.transunits.features
+                Yield New TranscriptUnit With {
+                    .id = operon.uniqueId,
+                    .name = operon.commonName,
+                    .genes = GeneObjects(operon.components).ToArray,
+                    .note = operon.comment
+                }
+            Next
+        End Function
+
+        Private Iterator Function GeneObjects(list As IEnumerable(Of String)) As IEnumerable(Of gene)
+            For Each id As String In list
+                Dim data As genes = geneIndex(id)
+
+                Yield New gene With {
+                    .locus_tag = data.uniqueId,
+                    .product = data.product,
+                    .left = data.left,
+                    .right = data.right,
+                    .strand = data.direction.ToString
+                }
+            Next
+        End Function
+    End Class
+End Namespace
