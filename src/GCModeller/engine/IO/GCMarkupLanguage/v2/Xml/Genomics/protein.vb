36fe1c47868ca459dc20edd9112e7c78d3e7a499
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-16 20:04:50 +0800

    Create protein.vb

---
+++ src/GCModeller/engine/IO/GCMarkupLanguage/v2/Xml/Genomics/protein.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Xml.Serialization
+
+Namespace v2
+
+    Public Class protein
+
+        <XmlAttribute> Public Property protein_id As String
+
+        Public Property ligand As String()
+        Public Property peptide_chains As String()
+
+    End Class
+End Namespace
