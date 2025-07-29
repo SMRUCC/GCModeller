9d61c76d9063b0b71769db9d64676abf304b442c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-07-29 10:48:09 +0800

    configs of the database reader test

--- src/GCModeller/data/Rhea/test/Program.vb
+++ src/GCModeller/data/Rhea/test/Program.vb
@@ -1,7 +1,14 @@
 Imports System
+Imports SMRUCC.genomics.Data.Rhea
 
 Module Program
+
+    Const test_file = "E:\biodeep\biodeepdb_v3\rhea.rdf"
+
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim data As RheaRDF = RheaRDF.Load(test_file)
+        Dim reactions As Reaction() = data.GetReactions().ToArray
+
+        Pause()
     End Sub
 End Module
