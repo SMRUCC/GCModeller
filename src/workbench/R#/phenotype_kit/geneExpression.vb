1b526a9b6a1218f959824d3223ba10313da10416
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-18 16:33:45 +0800

    sort mad

--- src/workbench/R#/phenotype_kit/geneExpression.vb
+++ src/workbench/R#/phenotype_kit/geneExpression.vb
@@ -1,65 +1,65 @@
 ﻿#Region "Microsoft.VisualBasic::8acdbfb086a321475d9693d8d239ff21, R#\phenotype_kit\geneExpression.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 1471
-    '    Code Lines: 903 (61.39%)
-    ' Comment Lines: 432 (29.37%)
-    '    - Xml Docs: 94.68%
-    ' 
-    '   Blank Lines: 136 (9.25%)
-    '     File Size: 60.29 KB
+' Summaries:
 
 
-    ' Module geneExpression
-    ' 
-    '     Function: add_gauss, Aggregate, applyPCA, average, castGenericRows
-    '               cmeans, CMeans3D, CmeansPattern, createDEGModels, createVectorList
-    '               DEGclass, depDataTable, dimensionNotAgree, dims, exp
-    '               expDataTable, filter, filterNaN, filterZeroGenes, filterZeroSamples
-    '               geneId, GetCmeansPattern, GetCmeansPatternA, getFuzzyPatternMembers, getMatrixInformation
-    '               imputeMissing, joinSamples, loadExpression, loadFromDataFrame, loadFromGenericDataSet
-    '               loadMatrixView, log, matrixSummary, ranking, readBinaryMatrix
-    '               readPattern, relative, representatives, savePattern, setGeneIDs
-    '               setSampleIDs, setTag, setZero, splitCMeansClusters, toClusters
-    '               totalSumNorm, tr, Ttest, uniqueGeneId, writeMatrix
-    '               zscore
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 1471
+'    Code Lines: 903 (61.39%)
+' Comment Lines: 432 (29.37%)
+'    - Xml Docs: 94.68%
+' 
+'   Blank Lines: 136 (9.25%)
+'     File Size: 60.29 KB
+
+
+' Module geneExpression
+' 
+'     Function: add_gauss, Aggregate, applyPCA, average, castGenericRows
+'               cmeans, CMeans3D, CmeansPattern, createDEGModels, createVectorList
+'               DEGclass, depDataTable, dimensionNotAgree, dims, exp
+'               expDataTable, filter, filterNaN, filterZeroGenes, filterZeroSamples
+'               geneId, GetCmeansPattern, GetCmeansPatternA, getFuzzyPatternMembers, getMatrixInformation
+'               imputeMissing, joinSamples, loadExpression, loadFromDataFrame, loadFromGenericDataSet
+'               loadMatrixView, log, matrixSummary, ranking, readBinaryMatrix
+'               readPattern, relative, representatives, savePattern, setGeneIDs
+'               setSampleIDs, setTag, setZero, splitCMeansClusters, toClusters
+'               totalSumNorm, tr, Ttest, uniqueGeneId, writeMatrix
+'               zscore
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -816,6 +816,26 @@ Module geneExpression
         Next
 
         Return val
+    End Function
+
+    ''' <summary>
+    ''' take top n expression feature by rank expression MAD value desc
+    ''' </summary>
+    ''' <param name="x"></param>
+    ''' <param name="top"></param>
+    ''' <returns></returns>
+    <ExportAPI("sort_mad")>
+    Public Function sort_mad(x As Matrix, Optional top As Integer = 10000) As Matrix
+        Dim sort = x.expression _
+            .OrderByDescending(Function(xi) xi.MAD) _
+            .Take(top) _
+            .ToArray
+
+        Return New Matrix With {
+            .expression = sort,
+            .sampleID = x.sampleID,
+            .tag = $"sort_mad({x.tag})"
+        }
     End Function
 
     ''' <summary>
