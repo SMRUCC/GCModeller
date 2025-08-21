2ab16f21ad102441c053b44e40fa6965b1b0ac0f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-21 14:02:52 +0800

    impements of the limma algorithm

---
+++ src/GCModeller/analysis/HTS_matrix/Limma/Limma.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Data.Bootstrapping
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.Math.Statistics
+Imports Microsoft.VisualBasic.Math.Statistics.Linq
+Imports SMRUCC.genomics.GCModeller.Workbench.ExperimentDesigner
+
+Public Module Limma
+
+    <Extension>
+    Public Iterator Function LmFit(x As Matrix, design As DataAnalysis) As IEnumerable(Of DEGModel)
+        Dim control = x.IndexOf(design.control)  ' 0
+        Dim treat = x.IndexOf(design.experiment) ' 1
+        Dim xi As Double() = Replicate(0.0, control.Length) _
+            .JoinIterates(Replicate(1.0, treat.Length)) _
+            .ToArray
+
+        x = x.log(2)
+
+        Dim logFC As Double() = New Double(x.size - 1) {}
+        Dim residuals As Double()() = RectangularArray.Matrix(Of Double)(x.size, control.Length + treat.Length)
+        Dim i As Integer = 0
+
+        ' 拟合线性模型（逐基因）
+        For Each gene As DataFrameRow In x.expression
+            Dim y As Double() = gene(control).JoinIterates(gene(treat)).ToArray
+            Dim lm As FitResult = LeastSquares.LinearFit(xi, y)
+
+            logFC(i) = lm.Slope
+            residuals(i) = lm.Residuals
+
+            i += 1
+        Next
+
+        ' 经验贝叶斯方差调整
+        ' a vs b
+        Dim p As Integer = design.size
+        Dim df_residual = (control.Length + treat.Length) - p
+        Dim gene_vars As Double() = residuals _
+            .Select(Function(e) (New Vector(e) ^ 2).Sum / df_residual) _
+            .ToArray
+
+        Dim prior_var As Double = gene_vars.Median
+        Dim prior_df = 4  ' limma默认先验自由度
+        Dim shrunk_vars = (prior_df * prior_var + df_residual * New Vector(gene_vars)) / (prior_df + df_residual)
+        Dim shrunk_se = Vector.Sqrt(shrunk_vars)
+
+        ' t检验与p值
+        Dim t_stats = logFC / shrunk_se
+        Dim df_total = prior_df + df_residual
+        Dim t As Vector = -t_stats.Abs
+        Dim p_values = SIMD.Multiply.f64_scalar_op_multiply_f64(2, Hypothesis.t.Pvalue(t, df:=df_total, Hypothesis.Hypothesis.Less))
+
+        For offset As Integer = 0 To logFC.Length - 1
+            Yield New DEGModel With {
+                .logFC = logFC(offset),
+                .label = x(offset).geneID,
+                .pvalue = p_values(offset),
+                .[class] = If(.pvalue < 0.05, If(.logFC > 0, "up", "down"), "not sig")
+            }
+        Next
+    End Function
+End Module
--- src/workbench/modules/ExperimentDesigner/DEGModel.vb
+++ src/workbench/modules/ExperimentDesigner/DEGModel.vb
@@ -1,62 +1,69 @@
 ﻿#Region "Microsoft.VisualBasic::77b03c01b2df660112b639ed27255546, modules\ExperimentDesigner\DEGModel.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 27
-    '    Code Lines: 19 (70.37%)
-    ' Comment Lines: 4 (14.81%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 4 (14.81%)
-    '     File Size: 749 B
+' Summaries:
 
 
-    ' Class DEGModel
-    ' 
-    '     Properties: [class], label, logFC, pvalue, VIP
-    ' 
-    '     Constructor: (+2 Overloads) Sub New
-    '     Function: ToString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 27
+'    Code Lines: 19 (70.37%)
+' Comment Lines: 4 (14.81%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 4 (14.81%)
+'     File Size: 749 B
+
+
+' Class DEGModel
+' 
+'     Properties: [class], label, logFC, pvalue, VIP
+' 
+'     Constructor: (+2 Overloads) Sub New
+'     Function: ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
-Public Class DEGModel : Implements IDeg
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports std = System.Math
 
-    Public Property label$ Implements IDeg.label
+Public Class DEGModel : Implements IDeg, INamedValue, IReadOnlyId
+
+    ''' <summary>
+    ''' usually be the gene id
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property label$ Implements IDeg.label, INamedValue.Key, IReadOnlyId.Identity
     Public Property logFC# Implements IDeg.log2FC
     Public Property pvalue# Implements IDeg.pvalue
     ''' <summary>
@@ -65,6 +72,16 @@ Public Class DEGModel : Implements IDeg
     ''' <returns></returns>
     Public Property VIP As Double
     Public Property [class] As String
+
+    ''' <summary>
+    ''' -log10(p-value)
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property nLog10p As Double
+        Get
+            Return -std.Log10(pvalue)
+        End Get
+    End Property
 
     Sub New()
     End Sub
