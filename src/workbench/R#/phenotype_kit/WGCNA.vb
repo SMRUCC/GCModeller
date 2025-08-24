f65dfe2306eeb34c34d337e1f5ced02c211e187a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-24 15:18:17 +0800

    export node information

--- src/workbench/R#/phenotype_kit/WGCNA.vb
+++ src/workbench/R#/phenotype_kit/WGCNA.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::e4e38aabd98e19e760767e65622e4480, R#\phenotype_kit\WGCNA.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 122
-    '    Code Lines: 52 (42.62%)
-    ' Comment Lines: 63 (51.64%)
-    '    - Xml Docs: 82.54%
-    ' 
-    '   Blank Lines: 7 (5.74%)
-    '     File Size: 7.23 KB
+' Summaries:
 
 
-    ' Module WGCNA
-    ' 
-    '     Function: applyModuleColors, readModules, readWeightMatrix, runAnalysis
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 122
+'    Code Lines: 52 (42.62%)
+' Comment Lines: 63 (51.64%)
+'    - Xml Docs: 82.54%
+' 
+'   Blank Lines: 7 (5.74%)
+'     File Size: 7.23 KB
+
+
+' Module WGCNA
+' 
+'     Function: applyModuleColors, readModules, readWeightMatrix, runAnalysis
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -180,6 +180,26 @@ Module WGCNA
     <RApiReturn(GetType(NetworkGraph))>
     Public Function LoadTOMModuleGraph_call(edges As String, nodes As String, Optional threshold As Double = 0, Optional prefix$ = Nothing) As NetworkGraph
         Return LoadTOMModuleGraph(edges, nodes, threshold, prefix)
+    End Function
+
+    ''' <summary>
+    ''' export a dataframe of the node information with connectivity value 
+    ''' </summary>
+    ''' <param name="x"></param>
+    ''' <returns></returns>
+    <ExportAPI("connectivity")>
+    Public Function connectivitySummary(x As NetworkGraph) As Object
+        Dim nodes As Node() = x.vertex.ToArray
+        Dim df As New dataframe With {
+            .rownames = nodes.Select(Function(v) v.label).ToArray,
+            .columns = New Dictionary(Of String, Array)
+        }
+
+        Call df.add("connectivity", From v As Node In nodes Select v.directedVertex.connectivity)
+        Call df.add("degree", From v As Node In nodes Select v.directedVertex.inDegree + v.directedVertex.outDegree)
+        Call df.add("module", From v As Node In nodes Select v!module)
+
+        Return df
     End Function
 
     <ExportAPI("applyModuleColors")>
