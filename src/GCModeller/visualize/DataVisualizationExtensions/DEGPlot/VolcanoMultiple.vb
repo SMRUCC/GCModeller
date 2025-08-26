e95679584a977962b9612be26af0115ad8c6f624
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-26 20:53:02 +0800

    plot multiple group vocano

--- src/GCModeller/visualize/DataVisualizationExtensions/DEGPlot/VolcanoMultiple.vb
+++ src/GCModeller/visualize/DataVisualizationExtensions/DEGPlot/VolcanoMultiple.vb
@@ -52,14 +52,24 @@
 
 #End Region
 
+Imports System.Drawing
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Axis
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
 Imports Microsoft.VisualBasic.MIME.Html.CSS
 Imports Microsoft.VisualBasic.MIME.Html.Render
 Imports SMRUCC.genomics.GCModeller.Workbench.ExperimentDesigner
+Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
+Imports std = System.Math
+
+#If NET48 Then
+Imports SolidBrush = System.Drawing.SolidBrush
+#Else
+Imports SolidBrush = Microsoft.VisualBasic.Imaging.SolidBrush
+#End If
 
 ''' <summary>
 ''' volcano of multiple comparision analysis result
@@ -71,6 +81,9 @@ Public Class VolcanoMultiple : Inherits Plot
     ''' </summary>
     ReadOnly compares As NamedCollection(Of DEGModel)()
     ReadOnly deg_class As String
+    ReadOnly non_deg As New SolidBrush(Color.LightGray)
+    ReadOnly up_deg As New SolidBrush(Color.Red)
+    ReadOnly down_deg As New SolidBrush(Color.Blue)
 
     Public Sub New(compares As IEnumerable(Of NamedCollection(Of DEGModel)), deg_class As String, theme As Theme)
         MyBase.New(theme)
@@ -85,7 +98,40 @@ Public Class VolcanoMultiple : Inherits Plot
         Dim sumAll As Double = compares _
             .Select(Function(group) group.Count(Function(e) e.class = deg_class)) _
             .Sum
+        Dim maxLogFC As Double = compares _
+            .Select(Function(group) group.Max(Function(e) e.logFC)) _
+            .Max
+        Dim minLogFC As Double = compares _
+            .Select(Function(group) group.Min(Function(e) e.logFC)) _
+            .Min
+        Dim upTicks = {0, maxLogFC}.CreateAxisTicks
+        Dim downTicks = {0, -minLogFC}.CreateAxisTicks
+        Dim left As Double = plotRect.Left
+        Dim zero As Double = plotRect.Top + plotRect.Height / 2
+        Dim height As Double = plotRect.Height / 2
+        Dim upAxis = d3js.scale.linear.domain(upTicks).range(0, height)
+        Dim downAxis = d3js.scale.linear.domain(downTicks).range(0, height)
+        Dim radius As Single = theme.pointSize
 
+        For Each group As NamedCollection(Of DEGModel) In compares
+            Dim width As Double = plotRect.Width * group.Count(Function(e) e.class = deg_class)/sumAll
+            Dim maxlogp As Double = group.Max(Function(e) e.nLog10p)
+            Dim halfWidth As Double
 
+            For Each gene As DEGModel In group
+                Dim maxoffset As Double = randf.NextDouble(0, gene.nLog10p / maxlogp) * halfWidth
+                Dim x As Double = If(randf.NextDouble > 0.5, 1, -1) * halfWidth + left
+                Dim y As Double = If(gene.logFC > 0, upAxis, downAxis)(std.Abs(gene.logFC))
+                Dim sign As Double = If(gene.logFC > 0, 1, -1)
+
+                If gene.class <> deg_class Then
+                    Call g.DrawCircle(New PointF(x, zero + sign * y), radius, non_deg)
+                ElseIf gene.logFC > 0 Then
+                    Call g.DrawCircle(New PointF(x, zero + sign * y), radius, up_deg)
+                Else
+                    Call g.DrawCircle(New PointF(x, zero + sign * y), radius, down_deg)
+                End If
+            Next
+        Next
     End Sub
 End Class
