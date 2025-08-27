07240ab791c2843269f9be945b1053a79a0204c0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-28 00:15:32 +0800

    view lines

--- src/GCModeller/engine/Dynamics/Core/Flux/Extensions.vb
+++ src/GCModeller/engine/Dynamics/Core/Flux/Extensions.vb
@@ -1,58 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::e973e7a4d4912ff456d3fefff57647b9, engine\Dynamics\Core\Flux\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 37
-    '    Code Lines: 30 (81.08%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 7 (18.92%)
-    '     File Size: 1.28 KB
+' Summaries:
 
 
-    '     Module Extensions
-    ' 
-    '         Function: GetProducts, GetReactants, MassToString, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 37
+'    Code Lines: 30 (81.08%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 7 (18.92%)
+'     File Size: 1.28 KB
+
+
+'     Module Extensions
+' 
+'         Function: GetProducts, GetReactants, MassToString, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace Core
@@ -181,6 +182,12 @@ Namespace Core
 
         Public Overrides Function ToString() As String
             Return left.JoinBy(" + ") & " = " & right.JoinBy(" + ")
+        End Function
+
+        Public Iterator Function FactorIds() As IEnumerable(Of String)
+            For Each factor As VariableFactor In left.JoinIterates(right).JoinIterates(regulation)
+                Yield factor.id
+            Next
         End Function
 
     End Class
