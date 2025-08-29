3047d8d7ede68522d210fca0a1ae2ac449e7eb0d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-29 14:33:05 +0800

    fix ofthe debug

---
+++ src/runtime/sciBASIC#/Data_science/Mathematica/Math/Math/Scripting/R/base.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.Linq
+
+Namespace Scripting.BasicR
+
+    Public Module base
+
+        Public Function c(Of T)(ParamArray v As IEnumerable(Of T)()) As T()
+            Return v.IteratesALL.ToArray
+        End Function
+
+    End Module
+End Namespace
