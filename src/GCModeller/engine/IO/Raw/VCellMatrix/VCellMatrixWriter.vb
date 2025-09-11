9fef641b6b3f20a5303b2fd7b08d2e1470d74a2f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-12 02:23:50 +0800

    write tmp matrix

--- src/GCModeller/engine/IO/Raw/VCellMatrix/VCellMatrixWriter.vb
+++ src/GCModeller/engine/IO/Raw/VCellMatrix/VCellMatrixWriter.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.IO
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.DataStorage.HDSPack
 Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
 Imports Microsoft.VisualBasic.Serialization.JSON
@@ -18,23 +20,52 @@ Public Class VCellMatrixWriter : Implements IDisposable
         Dim moduleSet = pack.GetMoleculeIdList
         Dim moleculeSet = moduleSet.Where(Function(m) Not m.Key.EndsWith("-Flux")).ToArray
         Dim fluxSet = moduleSet.Where(Function(m) m.Key.EndsWith("-Flux")).ToArray
-        Dim moleculeExpr = SaveMatrix(pack, moleculeSet)
-        Dim fluxExpr = SaveMatrix(pack, fluxSet)
         Dim instance_id As Dictionary(Of String, Dictionary(Of String, String())) = pack _
             .GetStream _
             .ReadText("/dynamics/cellular_symbols.json") _
             .LoadJSON(Of Dictionary(Of String, Dictionary(Of String, String())))
 
         For Each compart_id As String In pack.comparts
-            Call s.Delete($"/matrix/{compart_id}/molecule.dat")
-            Call s.Delete($"/matrix/{compart_id}/flux.dat")
+            Dim objs = instance_id(compart_id)
 
+            For Each [module] As KeyValuePair(Of String, String()) In objs
+                Dim tmp As Double()() = SaveMatrix(pack, compart_id, [module])
+                Dim offset As Integer = 0
 
+                For Each name As String In [module].Value
+                    Dim path As String = $"/matrix/{compart_id}/{name}.vec"
+
+                    Call s.Delete(path)
+
+                    Using buf As New BinaryDataWriter(s.OpenBlock(path), byteOrder:=ByteOrder.BigEndian)
+                        Dim timeline As Double() = tmp.Select(Function(ti) ti(offset)).ToArray
+                        buf.Write(timeline)
+                        buf.Flush()
+                        buf.Close()
+                    End Using
+
+                    offset += 1
+                Next
+
+                Erase tmp
+            Next
         Next
     End Sub
 
-    Private Function SaveMatrix(pack As Reader, listSet As KeyValuePair(Of String, String())()) As Stream
+    Private Function SaveMatrix(pack As Reader, compartment_id As String, listSet As KeyValuePair(Of String, String())) As Double()()
+        Dim times As Double() = pack.AllTimePoints.ToArray
+        Dim resolve_name As String = listSet.Key
+        Dim mat As Double()() = RectangularArray.Matrix(Of Double)(times.Length, listSet.Value.Length)
 
+        For i As Integer = 0 To times.Length - 1
+            Dim ti As Double = times(i)
+            Dim list As BinaryDataReader = pack.GetFrameFile(resolve_name, compartment_id, ti)
+            Dim vec As Double() = list.ReadDoubles(listSet.Value.Length)
+
+            mat(i) = vec
+        Next
+
+        Return mat
     End Function
 
     Protected Overridable Sub Dispose(disposing As Boolean)
