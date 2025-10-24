c03c4e83970aa0066f7fa85f0ab9b9cb2005b3f3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-24 14:42:59 +0800

    rollback the algorithm code

--- src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/Drawing2D/Math2D/ConcaveHull/Extensions.vb
+++ src/runtime/sciBASIC#/gr/Microsoft.VisualBasic.Imaging/Drawing2D/Math2D/ConcaveHull/Extensions.vb
@@ -91,12 +91,13 @@ Namespace Drawing2D.Math2D.ConcaveHull
         ''' <returns></returns>
         <Extension>
         Public Function ConcaveHull(points As IEnumerable(Of PointF), Optional r# = -1) As PointF()
-            With New AlphaShapes2D(points)
+            With New BallConcave(points)
                 If r# <= 0 Then
-                    r# = .ComputeOptimalAlpha
+                    r# = .RecomandedRadius
                 End If
 
-                Return .ComputeAlphaShape(r).ToArray
+                Return .GetConcave_Ball(r) _
+                       .ToArray
             End With
         End Function
     End Module
