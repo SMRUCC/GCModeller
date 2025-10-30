c881f228c0eb9ef1a5ae9429e6e7a29bb2449e38
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-10-30 09:53:19 +0800

    make improvements of the piint descriptro model

--- src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/MachineVision/RANSAC/PointWithDescriptor.vb
+++ src/runtime/sciBASIC#/Data_science/Mathematica/SignalProcessing/MachineVision/RANSAC/PointWithDescriptor.vb
@@ -33,35 +33,39 @@ Public Structure PointWithDescriptor
     ''' </summary>
     Public Shared Function GenerateCandidateMatches(ByRef sourceDesc As PointWithDescriptor(), ByRef targetDesc As PointWithDescriptor()) As List(Of (source As PointF, target As PointF))
         Dim matches As New List(Of (source As PointF, target As PointF))()
+        Dim bar As Tqdm.ProgressBar = Nothing
 
         Call $"Generates a list of candidate matches by finding the nearest neighbor in descriptor space.".debug
         Call $"matrix size: [{sourceDesc.Length} x {targetDesc.Length}]".info
 
-        For Each sPt As PointWithDescriptor In Tqdm.Wrap(sourceDesc, wrap_console:=App.EnableTqdm)
-            Dim minDist As Double = Double.PositiveInfinity
-            Dim bestMatch As PointWithDescriptor
+        For Each sPt As PointWithDescriptor In Tqdm.Wrap(sourceDesc, bar:=bar, wrap_console:=App.EnableTqdm)
+            Dim q = targetDesc.AsParallel _
+                .Select(Function(tPt As PointWithDescriptor)
+                            ' Simple Euclidean distance in descriptor space (r, theta)
+                            ' We might want to weight angle more than distance, but this is a start.
+                            Dim dr = sPt.Descriptor.r - tPt.Descriptor.r
+                            Dim dtheta = sPt.Descriptor.theta - tPt.Descriptor.theta
+                            Dim pd As Double = If(sPt.properties.IsNullOrEmpty OrElse tPt.properties.IsNullOrEmpty, 0, sPt.properties.SquareDistance(tPt.properties))
 
-            For Each tPt As PointWithDescriptor In targetDesc
-                ' Simple Euclidean distance in descriptor space (r, theta)
-                ' We might want to weight angle more than distance, but this is a start.
-                Dim dr = sPt.Descriptor.r - tPt.Descriptor.r
-                Dim dtheta = sPt.Descriptor.theta - tPt.Descriptor.theta
-                Dim pd As Double = If(sPt.properties.IsNullOrEmpty OrElse tPt.properties.IsNullOrEmpty, 0, sPt.properties.SquareDistance(tPt.properties))
+                            ' Normalize angle difference
+                            While dtheta > std.PI : dtheta -= 2 * std.PI : End While
+                            While dtheta < -std.PI : dtheta += 2 * std.PI : End While
 
-                ' Normalize angle difference
-                While dtheta > std.PI : dtheta -= 2 * std.PI : End While
-                While dtheta < -std.PI : dtheta += 2 * std.PI : End While
+                            dr = dr ^ 2
+                            dtheta = dtheta ^ 2
 
-                Dim distSq = dr * dr + dtheta * dtheta + pd
+                            Dim distSq = dr + dtheta + pd * 0.5
 
-                If distSq < minDist Then
-                    minDist = distSq
-                    bestMatch = tPt
-                End If
-            Next
+                            Return (distSq, tPt, dr, dtheta, pd)
+                        End Function) _
+                .ToArray
+            Dim min = q.OrderBy(Function(a) a.distSq).First
+            Dim minDist As Double = min.distSq
+            Dim bestMatch As PointWithDescriptor = min.tPt
 
-            If minDist <> Double.PositiveInfinity Then
-                matches.Add((sPt.Pt, bestMatch.Pt))
+            If Not minDist.IsNaNImaginary Then
+                Call matches.Add((sPt.Pt, bestMatch.Pt))
+                Call bar.SetLabel($"({sPt.Pt.X},{sPt.Pt.Y}) = ({bestMatch.Pt.X},{bestMatch.Pt.Y}) [min sq_dist:{minDist}]")
             End If
         Next
 
@@ -97,6 +101,13 @@ Public Structure PointWithDescriptor
         Next
     End Function
 
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <typeparam name="T"></typeparam>
+    ''' <param name="shape"></param>
+    ''' <param name="getMetadata">should be normalized to [0,1]</param>
+    ''' <returns></returns>
     Public Shared Iterator Function ComputeDescriptors(Of T As Layout2D)(shape As IEnumerable(Of T), getMetadata As Func(Of T, Double())) As IEnumerable(Of PointWithDescriptor)
         Dim pool As T() = shape.SafeQuery.ToArray
 
@@ -122,5 +133,29 @@ Public Structure PointWithDescriptor
                 .properties = getMetadata(pool(i))
             }
         Next
+    End Function
+
+    Public Shared Function NormalizeProperties(points As PointWithDescriptor()) As PointWithDescriptor()
+        Dim max As Double() = New Double(points(0).properties.Length - 1) {}
+
+        For Each pt As PointWithDescriptor In points
+            Dim v As Double() = pt.properties
+
+            For i As Integer = 0 To v.Length - 1
+                If v(i) > max(i) Then
+                    max(i) = v(i)
+                End If
+            Next
+        Next
+
+        For i As Integer = 0 To points.Length - 1
+            points(i) = New PointWithDescriptor With {
+                .Descriptor = points(i).Descriptor,
+                .Pt = points(i).Pt,
+                .properties = SIMD.Divide.f64_op_divide_f64(points(i).properties, max)
+            }
+        Next
+
+        Return points
     End Function
 End Structure
