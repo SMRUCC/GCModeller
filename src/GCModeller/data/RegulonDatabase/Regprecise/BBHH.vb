6d4852402dc86faae89a71ace0e6006bb7c74878
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-06 14:39:07 +0800

    configs for run test

--- src/GCModeller/data/RegulonDatabase/Regprecise/BBHH.vb
+++ src/GCModeller/data/RegulonDatabase/Regprecise/BBHH.vb
@@ -1,65 +1,64 @@
 ﻿#Region "Microsoft.VisualBasic::b7e74bbfed0ac5f3bf31eff89964a099, data\RegulonDatabase\Regprecise\BBHH.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 48
-    '    Code Lines: 28 (58.33%)
-    ' Comment Lines: 14 (29.17%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 6 (12.50%)
-    '     File Size: 1.91 KB
+' Summaries:
 
 
-    '     Class bbhMappings
-    ' 
-    '         Properties: definition, Family, hit_name, Identities, Positive
-    '                     query_name, vimssId
-    ' 
-    '         Function: GetQueryMaps, ToString
-    ' 
-    '     Class FamilyHit
-    ' 
-    '         Properties: Family, HitName, QueryName
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 48
+'    Code Lines: 28 (58.33%)
+' Comment Lines: 14 (29.17%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 6 (12.50%)
+'     File Size: 1.91 KB
+
+
+'     Class bbhMappings
+' 
+'         Properties: definition, Family, hit_name, Identities, Positive
+'                     query_name, vimssId
+' 
+'         Function: GetQueryMaps, ToString
+' 
+'     Class FamilyHit
+' 
+'         Properties: Family, HitName, QueryName
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH.Abstract
 
 Namespace Regprecise
