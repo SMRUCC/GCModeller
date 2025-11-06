96484dd2b15ebbef4ea5e3d284edb3ee57b574d6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-11-06 23:25:36 +0800

    fix of the progrss bar object reference error

--- src/GCModeller/analysis/SequenceToolkit/SequencePatterns/Pattern/SSR/SSRSearch.vb
+++ src/GCModeller/analysis/SequenceToolkit/SequencePatterns/Pattern/SSR/SSRSearch.vb
@@ -1,70 +1,70 @@
 ﻿#Region "Microsoft.VisualBasic::a2b50e8950af39ffe721ed636b327e19, analysis\SequenceToolkit\SequencePatterns\Pattern\SSR\SSRSearch.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 207
-    '    Code Lines: 140 (67.63%)
-    ' Comment Lines: 29 (14.01%)
-    '    - Xml Docs: 62.07%
-    ' 
-    '   Blank Lines: 38 (18.36%)
-    '     File Size: 8.57 KB
+' Summaries:
 
 
-    ' Module SSRSearch
-    ' 
-    '     Properties: Parallel
-    ' 
-    '     Function: CompoundSSR, InterruptedSSR, PureSSR, SeedingInternal, SSR
-    ' 
-    '     Sub: MatchInternal
-    ' 
-    ' Structure SSR
-    ' 
-    '     Properties: Ends, RepeatUnit, Sequence, Start, Strand
-    ' 
-    '     Function: ToFasta, ToString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 207
+'    Code Lines: 140 (67.63%)
+' Comment Lines: 29 (14.01%)
+'    - Xml Docs: 62.07%
+' 
+'   Blank Lines: 38 (18.36%)
+'     File Size: 8.57 KB
+
+
+' Module SSRSearch
+' 
+'     Properties: Parallel
+' 
+'     Function: CompoundSSR, InterruptedSSR, PureSSR, SeedingInternal, SSR
+' 
+'     Sub: MatchInternal
+' 
+' Structure SSR
+' 
+'     Properties: Ends, RepeatUnit, Sequence, Start, Strand
+' 
+'     Function: ToFasta, ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal
-Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.ConsoleProgressBar
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
 Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Language
@@ -142,7 +142,7 @@ Public Module SSRSearch
                                         ' Do Nothing
                                     End Sub)
                 Else
-                    Using progress As New ProgressBar With {.Maximum = repeatUnit.Count}
+                    Using progress As New ConsoleProgressBar.ProgressBar With {.Maximum = repeatUnit.Count}
                         Call searchWork(report:=Sub(msg) progress.PerformStep(msg))
                     End Using
                 End If
