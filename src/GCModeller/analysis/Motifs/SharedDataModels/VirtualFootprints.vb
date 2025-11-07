fffaa8733e4f768bd44236c8273bb695413c123c
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-07 15:51:30 +0800

    fix of the missing components

--- src/GCModeller/analysis/Motifs/SharedDataModels/VirtualFootprints.vb
+++ src/GCModeller/analysis/Motifs/SharedDataModels/VirtualFootprints.vb
@@ -1,50 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::7dbeda0a7468b872f6221f6d508f23f6, analysis\Motifs\SharedDataModels\VirtualFootprints.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class VirtualFootprints
-    ' 
-    '         Properties: Distance, Ends, Length, MotifId, ORF
-    '                     ORFDirection, Sequence, Signature, Starts, Strand
-    ' 
-    '         Function: GetLociDescrib, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class VirtualFootprints
+' 
+'         Properties: Distance, Ends, Length, MotifId, ORF
+'                     ORFDirection, Sequence, Signature, Starts, Strand
+' 
+'         Function: GetLociDescrib, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Text.RegularExpressions
-Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
+Imports Microsoft.VisualBasic.Data.Framework.StorageProvider.Reflection
 Imports SMRUCC.genomics.ComponentModel.Loci
 Imports SMRUCC.genomics.ComponentModel.Loci.Abstract
 
