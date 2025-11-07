b72c3f40b68bf904c1faa06a863146c0b7e787c8
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-07 15:49:10 +0800

    fix of the namespace reference error

--- src/GCModeller/analysis/Motifs/SharedDataModels/RegulatesFootprints.vb
+++ src/GCModeller/analysis/Motifs/SharedDataModels/RegulatesFootprints.vb
@@ -1,54 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::0c05b1b4211b3404531200cef011b959, analysis\Motifs\SharedDataModels\RegulatesFootprints.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class RegulatesFootprints
-    ' 
-    '         Properties: [Class], Category, DoorId, InitX, InteractionType
-    '                     MotifFamily, MotifTrace, ORF, Pcc, Regulator
-    '                     RegulatorTrace, sPcc, StructGenes, tag, Type
-    '                     WGCNA
-    ' 
-    '         Function: __posUid, __posUidNonStrict, Equals, ToString, TraceUid
-    '                   TraceUidStrict
-    '         Operators: <>, =
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class RegulatesFootprints
+' 
+'         Properties: [Class], Category, DoorId, InitX, InteractionType
+'                     MotifFamily, MotifTrace, ORF, Pcc, Regulator
+'                     RegulatorTrace, sPcc, StructGenes, tag, Type
+'                     WGCNA
+' 
+'         Function: __posUid, __posUidNonStrict, Equals, ToString, TraceUid
+'                   TraceUidStrict
+'         Operators: <>, =
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
-Imports Microsoft.VisualBasic.Data.visualize.Network.Graph.Abstract
+Imports Microsoft.VisualBasic.Data.Framework.StorageProvider.Reflection
+Imports Microsoft.VisualBasic.Data.GraphTheory.Network
+Imports Microsoft.VisualBasic.Data.GraphTheory.SparseGraph
 Imports SMRUCC.genomics.ComponentModel.Loci.Abstract
 
 Namespace DocumentFormat
