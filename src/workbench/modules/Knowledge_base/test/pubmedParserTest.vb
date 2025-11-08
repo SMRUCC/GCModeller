d6aceb422f04401b3a6cd62dfb390880823f3e76
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-08 20:30:25 +0800

    test reader

--- src/workbench/modules/Knowledge_base/test/pubmedParserTest.vb
+++ src/workbench/modules/Knowledge_base/test/pubmedParserTest.vb
@@ -1,9 +1,17 @@
-﻿Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI
+﻿Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI
 
 Module pubmedParserTest
 
     Sub Main()
-        Dim articles = PlainTextParser.LoadArticles("C:\Users\Administrator\Downloads\pubmed-barleyspik-set.txt").ToArray
+        Dim articles = {"M:\project\20251010-wheat\释放数据20250927\pubmed\pubmed-Secalespik-set.txt",
+"M:\project\20251010-wheat\释放数据20250927\pubmed\pubmed-Triticumsp-set.txt",
+"M:\project\20251010-wheat\释放数据20250927\pubmed\pubmed-wheatspike-set.txt",
+"M:\project\20251010-wheat\释放数据20250927\pubmed\pubmed-Agropyrons-set.txt",
+"M:\project\20251010-wheat\释放数据20250927\pubmed\pubmed-barleyspik-set.txt",
+"M:\project\20251010-wheat\释放数据20250927\pubmed\pubmed-Hordeumspi-set.txt",
+"M:\project\20251010-wheat\释放数据20250927\pubmed\pubmed-Oryzaspike-set.txt",
+"M:\project\20251010-wheat\释放数据20250927\pubmed\pubmed-ricespiked-set.txt"}.Select(Function(file) PlainTextParser.LoadArticles(file)).IteratesALL.GroupBy(Function(a) a.PMID).Select(Function(a) a.First).ToArray
 
         Pause()
     End Sub
