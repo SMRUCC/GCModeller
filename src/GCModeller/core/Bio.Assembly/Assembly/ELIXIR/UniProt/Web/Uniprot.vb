994abc749f9a6758da190e4e6024d1ff6537f170
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-09 02:48:50 +0800

    export api for make query of the proteins from the uniprot rest api

--- src/GCModeller/core/Bio.Assembly/Assembly/ELIXIR/UniProt/Web/Uniprot.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/ELIXIR/UniProt/Web/Uniprot.vb
@@ -1,58 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::3a925d0938bcd6137137627a648d2f70, core\Bio.Assembly\Assembly\ELIXIR\UniProt\Web\Uniprot.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 54
-    '    Code Lines: 23 (42.59%)
-    ' Comment Lines: 24 (44.44%)
-    '    - Xml Docs: 83.33%
-    ' 
-    '   Blank Lines: 7 (12.96%)
-    '     File Size: 2.14 KB
+' Summaries:
 
 
-    '     Module WebServices
-    ' 
-    '         Function: CreateQuery, DownloadProtein, GetEntries
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 54
+'    Code Lines: 23 (42.59%)
+' Comment Lines: 24 (44.44%)
+'    - Xml Docs: 83.33%
+' 
+'   Blank Lines: 7 (12.96%)
+'     File Size: 2.14 KB
+
+
+'     Module WebServices
+' 
+'         Function: CreateQuery, DownloadProtein, GetEntries
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Microsoft.VisualBasic.Serialization.JSON
--- src/workbench/R#/annotationKit/UniProt.vb
+++ src/workbench/R#/annotationKit/UniProt.vb
@@ -1,59 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::c4c344af79f479d3055a1a01d63b2f82, R#\annotationKit\UniProt.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 57
-    '    Code Lines: 44 (77.19%)
-    ' Comment Lines: 6 (10.53%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 7 (12.28%)
-    '     File Size: 2.06 KB
+' Summaries:
 
 
-    ' Module UniProt
-    ' 
-    '     Function: Addnumbers, ExtractFasta, OpenOrCreateEnzymeSequencePack
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 57
+'    Code Lines: 44 (77.19%)
+' Comment Lines: 6 (10.53%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 7 (12.28%)
+'     File Size: 2.06 KB
+
+
+' Module UniProt
+' 
+'     Function: Addnumbers, ExtractFasta, OpenOrCreateEnzymeSequencePack
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Assembly.Uniprot.Web
 Imports SMRUCC.genomics.Assembly.Uniprot.XML
 Imports SMRUCC.genomics.Data
 Imports SMRUCC.genomics.SequenceModel.FASTA
@@ -89,7 +90,7 @@ Public Module UniProt
         If source Like GetType(Message) Then
             Return source.TryCast(Of Message)
         Else
-            For Each protein As entry In source.TryCast(Of IEnumerable(Of entry))
+            For Each protein As Entry In source.TryCast(Of IEnumerable(Of Entry))
                 Call pack.AddProtein(protein)
             Next
         End If
@@ -106,5 +107,15 @@ Public Module UniProt
     <ExportAPI("extract_fasta")>
     Public Function ExtractFasta(pack As ECNumberReader, Optional enzyme As Boolean = True) As FastaFile
         Return New FastaFile(pack.QueryFasta(enzymeQuery:=enzyme))
+    End Function
+
+    ''' <summary>
+    ''' make query of the proteins from the uniprot rest api
+    ''' </summary>
+    ''' <param name="q"></param>
+    ''' <returns></returns>
+    <ExportAPI("rest_query")>
+    Public Function uniprot_query(q As String) As RestQueryResult()
+        Return WebServices.CreateQuery(q)
     End Function
 End Module
