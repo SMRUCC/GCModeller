eaa8629c709ebf7d69e39ad94cf7952c1a5b5009
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-08 17:21:45 +0800

    configs fpr run test

--- src/workbench/modules/Knowledge_base/test/pubmedParserTest.vb
+++ src/workbench/modules/Knowledge_base/test/pubmedParserTest.vb
@@ -1,6 +1,10 @@
-﻿Module pubmedParserTest
+﻿Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI
+
+Module pubmedParserTest
 
     Sub Main()
+        Dim articles = PlainTextParser.LoadArticles("C:\Users\Administrator\Downloads\pubmed-barleyspik-set.txt").ToArray
 
+        Pause()
     End Sub
 End Module
