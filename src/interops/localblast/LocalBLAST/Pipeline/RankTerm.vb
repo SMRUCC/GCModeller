637af185c50a51c3fc8f0a6bdf957dcb3d9ae60d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-10 15:56:06 +0800

    make generic term assignment

--- src/interops/localblast/LocalBLAST/Pipeline/RankTerm.vb
+++ src/interops/localblast/LocalBLAST/Pipeline/RankTerm.vb
@@ -2,6 +2,7 @@
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
 Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.ComponentModel.Annotation
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
 
 Namespace Pipeline
@@ -27,7 +28,7 @@ Namespace Pipeline
                                                     Optional topBest As Boolean = True) As IEnumerable(Of RankTerm)
 
             For Each group As IGrouping(Of String, BestHit) In hits.SafeQuery.GroupBy(Function(a) a.QueryName)
-                With MeasureTopTerm(group, termMaps)
+                With MeasureTopTerm(group, AddressOf SingleBestHitScore, termMaps)
                     If topBest AndAlso .Any Then
                         Yield .First
                     Else
@@ -39,38 +40,57 @@ Namespace Pipeline
             Next
         End Function
 
-        Private Shared Function MeasureTopTerm(group As IGrouping(Of String, BestHit), termMaps As Dictionary(Of String, String)) As RankTerm()
-            Dim scores As NamedValue(Of Double)() = group _
-                .Where(Function(h) h.identities > 0 AndAlso h.HitName <> "HITS_NOT_FOUND") _
-                .Select(Function(h)
-                            Dim key As String = h.HitName.Split.First.Split("|"c).First
-                            Dim score As Double = (h.score + 1) * (h.identities + 1)
+        Public Shared Iterator Function RankTopTerm(hits As IEnumerable(Of IQueryHits),
+                                                    Optional termMaps As Dictionary(Of String, String) = Nothing,
+                                                    Optional topBest As Boolean = True) As IEnumerable(Of RankTerm)
 
-                            Return New NamedValue(Of Double)(key, score)
-                        End Function) _
-                .ToArray
-            Dim terms As RankTerm() = scores _
-                .GroupBy(Function(a)
-                             If termMaps Is Nothing Then
-                                 Return a.Name
-                             Else
-                                 Return termMaps(a.Name)
-                             End If
-                         End Function) _
-                .Select(Function(a)
-                            Dim scoreSet = a.UniqueNames.ToDictionary(Function(x) x.Name, Function(x) x.Value)
-                            Dim term As String = a.Key
+            For Each group As IGrouping(Of String, IQueryHits) In hits.SafeQuery.GroupBy(Function(a) a.queryName)
+                With MeasureTopTerm(group, AddressOf GenericHitIdentities, termMaps)
+                    If topBest AndAlso .Any Then
+                        Yield .First
+                    Else
+                        For Each term As RankTerm In .AsEnumerable
+                            Yield term
+                        Next
+                    End If
+                End With
+            Next
+        End Function
 
-                            Return (term, scoreSet)
-                        End Function) _
-                .Select(Function(a)
-                            Return New RankTerm With {
-                                .queryName = group.Key,
-                                .scores = a.scoreSet,
-                                .term = a.term
-                            }
-                        End Function) _
-                .OrderByDescending(Function(t) t.score) _
+        Private Shared Function SingleBestHitScore(h As BestHit) As Double
+            Return (h.score + 1) * (h.identities + 1)
+        End Function
+
+        Private Shared Function GenericHitIdentities(h As IQueryHits) As Double
+            Return h.identities
+        End Function
+
+        Private Shared Function ScoreQuery(Of T As IQueryHits)(group As IGrouping(Of String, T), eval As Func(Of T, Double)) As IEnumerable(Of NamedValue(Of Double))
+            Return From h As T
+                   In group
+                   Where h.identities > 0 AndAlso h.hitName <> "HITS_NOT_FOUND"
+                   Let key As String = h.hitName.Split.First.Split("|"c).First
+                   Let score As Double = eval(h)
+                   Select New NamedValue(Of Double)(key, score)
+        End Function
+
+        Private Shared Iterator Function MakeTerms(scores As IEnumerable(Of NamedValue(Of Double)), queryId As String, termMaps As Dictionary(Of String, String)) As IEnumerable(Of RankTerm)
+            For Each term As IGrouping(Of String, NamedValue(Of Double)) In scores.GroupBy(Function(a) If(termMaps Is Nothing, a.Name, termMaps.TryGetValue(a.Name, [default]:="Unknown")))
+                Dim scoreSet As Dictionary(Of String, Double) = term.UniqueNames.ToDictionary(Function(x) x.Name, Function(x) x.Value)
+                Dim termName As String = term.Key
+
+                Yield New RankTerm With {
+                    .queryName = queryId,
+                    .scores = scoreSet,
+                    .term = termName
+                }
+            Next
+        End Function
+
+        Private Shared Function MeasureTopTerm(Of T As IQueryHits)(group As IGrouping(Of String, T), eval As Func(Of T, Double), termMaps As Dictionary(Of String, String)) As RankTerm()
+            Dim scores As NamedValue(Of Double)() = ScoreQuery(group, eval).ToArray
+            Dim terms As RankTerm() = MakeTerms(scores, group.Key, termMaps) _
+                .OrderByDescending(Function(ti) ti.score) _
                 .ToArray
 
             Return terms
