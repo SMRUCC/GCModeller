e323e7ffc0848619c131802f6ee7effa6f0abd9b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-11 11:28:16 +0800

    run file reader test

--- src/repository/Bio.Repository/test/Module1.vb
+++ src/repository/Bio.Repository/test/Module1.vb
@@ -1,57 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::0606059fcf3e972aebbd8bb17a85669f, Bio.Repository\test\Module1.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 60
-    '    Code Lines: 42 (70.00%)
-    ' Comment Lines: 3 (5.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 15 (25.00%)
-    '     File Size: 1.68 KB
+' Summaries:
 
 
-    ' Module Module1
-    ' 
-    '     Sub: loadIndexTest, Main, read1, readTest, write1
-    '          writeTest
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 60
+'    Code Lines: 42 (70.00%)
+' Comment Lines: 3 (5.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 15 (25.00%)
+'     File Size: 1.68 KB
+
+
+' Module Module1
+' 
+'     Sub: loadIndexTest, Main, read1, readTest, write1
+'          writeTest
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
 Imports SMRUCC.genomics.Data
 Imports SMRUCC.genomics.Data.KEGG.Metabolism
@@ -67,7 +68,9 @@ Module Module1
     End Sub
 
     Sub loadIndexTest()
-        Dim index = FtpIndex.LoadIndex("K:\test\ASSEMBLY_REPORTS\assembly_summary_genbank.txt").ToArray
+        For Each asm In FtpIndex.LoadIndex("D:\datapool\assembly_summary_genbank.txt")
+            Call Console.WriteLine(asm.GetJson)
+        Next
 
         Pause()
     End Sub
