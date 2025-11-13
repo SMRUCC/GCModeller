e2c5a2a117a41758b9cf936dbfdb8eb59d4fa0f7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-14 04:51:04 +0800

    removes the protein id suffix

--- src/workbench/R#/seqtoolkit/Annotations/terms.vb
+++ src/workbench/R#/seqtoolkit/Annotations/terms.vb
@@ -1,60 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::384b5df159833b117e5acd310e689986, R#\seqtoolkit\Annotations\terms.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 147
-    '    Code Lines: 104 (70.75%)
-    ' Comment Lines: 22 (14.97%)
-    '    - Xml Docs: 95.45%
-    ' 
-    '   Blank Lines: 21 (14.29%)
-    '     File Size: 6.22 KB
+' Summaries:
 
 
-    ' Module terms
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: COGannotations, COGtable, geneNames, GOannotations, KOannotations
-    '               Pfamannotations, printIDSolver, readIdMappings, readMyvaCOG, saveIdMappings
-    '               Synonyms
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 147
+'    Code Lines: 104 (70.75%)
+' Comment Lines: 22 (14.97%)
+'    - Xml Docs: 95.45%
+' 
+'   Blank Lines: 21 (14.29%)
+'     File Size: 6.22 KB
+
+
+' Module terms
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: COGannotations, COGtable, geneNames, GOannotations, KOannotations
+'               Pfamannotations, printIDSolver, readIdMappings, readMyvaCOG, saveIdMappings
+'               Synonyms
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Text
+Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.Framework
 Imports Microsoft.VisualBasic.Linq
@@ -114,6 +115,26 @@ Module terms
         Call sb.AppendLine("...")
 
         Return sb.ToString
+    End Function
+
+    <ExportAPI("removes_proteinIDSuffix")>
+    Public Function removesProteinIDSuffix(<RRawVectorArgument> id As Object) As String()
+        Dim dbxref As String() = CLRVector.asCharacter(id)
+
+        If dbxref.IsNullOrEmpty Then
+            Return Nothing
+        End If
+
+        Const prot_idSuffix = "\.\d+$"
+
+        dbxref = dbxref _
+            .Select(Function(prot_id)
+                        Return Regex.Replace(prot_id, prot_idSuffix, String.Empty)
+                    End Function) _
+            .Distinct _
+            .ToArray
+
+        Return dbxref
     End Function
 
     ''' <summary>
