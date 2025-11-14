a39ffd5624cac94b9e625b3aad25e8ecda0e6cdf
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-14 20:22:19 +0800

    how to run demo test

--- src/GCModeller/analysis/SequenceToolkit/MotifFinder/Gibbs/Utils.vb
+++ src/GCModeller/analysis/SequenceToolkit/MotifFinder/Gibbs/Utils.vb
@@ -58,11 +58,11 @@ Public Class Utils
     Shared ReadOnly LOG_2 As Double = Math.Log(2)
 
     Friend Shared Function getSequenceFromPair(sequences As IList(Of KeyValuePair(Of String, Integer))) As IList(Of String)
-        Return sequences.[Select](Function(a) a.Key).ToList()
+        Return sequences.Select(Function(a) a.Key).ToList()
     End Function
 
     Friend Shared Function getSiteFromPair(sequences As IList(Of KeyValuePair(Of String, Integer))) As List(Of Integer)
-        Return sequences.[Select](Function(a) a.Value).ToList()
+        Return sequences.Select(Function(a) a.Value).ToList()
     End Function
 
     Public Shared Function indexOfBase(base As Char) As Integer
@@ -83,12 +83,12 @@ Public Class Utils
 
     Public Shared Function calcInformationContent(countSum As Double, counts As Integer()) As Double
         Dim probabilities As Double() = counts.[Select](Function(i) i / countSum).ToArray()
-        Return Enumerable.Select(Of Double, Global.System.[Double])(probabilities, CType(Function(p)
-                                                                                             If p = 0 Then
-                                                                                                 Return CDbl(0)
-                                                                                             Else
-                                                                                                 Return p * (Math.Log(p / 0.25) / LOG_2)
-                                                                                             End If
-                                                                                         End Function, Func(Of Double, Double))).Sum()
+        Return Enumerable.Select(probabilities, CType(Function(p)
+                                                          If p = 0 Then
+                                                              Return CDbl(0)
+                                                          Else
+                                                              Return p * (Math.Log(p / 0.25) / LOG_2)
+                                                          End If
+                                                      End Function, Func(Of Double, Double))).Sum()
     End Function
 End Class
