1ebd630b478d3ebfce4b96a309d54b3aadb8d28d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-17 22:33:30 +0800

    run package msbuild

---
+++ src/workbench/pkg/@export/QC.d.ts
@@ -0,0 +1,31 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "QC" from "rnaseq";
+//
+// ref=rnaseq.QC@rnaseq, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+ * 
+*/
+declare namespace QC {
+   /**
+     * @param quality default value Is ``20``.
+     * @param env default value Is ``null``.
+   */
+   function trim_low_quality(reads: any, quality?: object, env?: object): any;
+   /**
+    * trim the primers or adapters sequence at read header region
+    * 
+    * 
+     * @param reads -
+     * @param headers -
+     * @param exact_match 
+     * + default value Is ``true``.
+     * @param cutoff 
+     * + default value Is ``0.85``.
+     * @param env -
+     * 
+     * + default value Is ``null``.
+   */
+   function trim_reads_headers(reads: any, headers: any, exact_match?: boolean, cutoff?: number, env?: object): any;
+}
