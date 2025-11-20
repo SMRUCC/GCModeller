91d94a7c84a8e5ce3ac86fbe49b2fd0589658930
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-20 09:49:19 +0800

    Create Banner.vb

---
+++ src/GCModeller/engine/IO/GCMarkupLanguage/Banner.vb
@@ -0,0 +1,30 @@
+﻿Imports System.IO
+
+Public Module Banner
+
+    Public Const BannerInformation As String = "
+
+ Welcome To
+      _____  _____ __  __           _      _ _           
+     / ____|/ ____|  \/  |         | |    | | |          
+    | |  __| |    | \  / | ___   __| | ___| | | ___ _ __ 
+    | | |_ | |    | |\/| |/ _ \ / _` |/ _ \ | |/ _ \ '__|
+    | |__| | |____| |  | | (_) | (_| |  __/ | |  __/ |   
+     \_____|\_____|_|  |_|\___/ \__,_|\___|_|_|\___|_|   
+                                                      
+                      Virtual Cell Simulation Tool - 2025
+
+                         https://biocad.innovation.ac.cn/
+                         https://gcmodeller.org/
+
+"
+
+    Public Sub Print(console As TextWriter)
+        For Each line As String In Banner.BannerInformation.LineTokens
+            Call console.WriteLine(line)
+        Next
+
+        Call console.Flush()
+    End Sub
+
+End Module
