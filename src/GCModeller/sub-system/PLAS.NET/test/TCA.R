2faca0bd72bb48a9f835f82818d95c558abca3f7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-26 00:28:51 +0800

    run test

--- src/GCModeller/sub-system/PLAS.NET/test/TCA.R
+++ src/GCModeller/sub-system/PLAS.NET/test/TCA.R
@@ -11,38 +11,23 @@ imports "S.system" from "simulators";
 # Fumarate Hydration (Fumarase): Fumarate + H₂O → L-Malate
 # Malate Dehydrogenation: L-Malate + NAD⁺ → Oxaloacetate + NADH
 let TCA_cycle = [
-  # metabolite       synthesis                                                  consumption                
-  acetyl_coA      -> 0                                                        - (oxaloacetate ^ 0.5) * (acetyl_coA ^ 0.5),      
-  CO2             -> ((isocitrate ^ 0.5) * (NAD ^ 0.5)) + 
-                     ((a_ketoglutarate ^ 0.3) * (NAD ^ 0.3) * (coA_sh ^ 0.3)) - 0,                          
-  citrate         -> ((oxaloacetate^0.5) * (acetyl_coA^0.5))                  - (citrate),              
-  coA_sh          -> ((succinyl_coA^ 0.3) * ( Pi^0.3) * ( GDP^ 0.3))          - ((a_ketoglutarate ^ 0.3) * (NAD ^ 0.3) * (coA_sh ^ 0.3)),
-  FAD             -> 0                                                        - ((succinate ^ 0.5 ) * ( FAD ^ 0.5)),                
-  FADH2           -> ((succinate^0.5) * (FAD ^ 0.5))                          - 0,                 
-  fumarate        -> ((succinate ^ 0.5 ) *( FAD ^ 0.5))                       - ((fumarate ^0.5) * (H2O ^ 0.5)),                  
-  GDP             -> 0                                                        - ((succinyl_coA ^ 0.3) * (Pi ^ 0.3) * (GDP ^ 0.3)),
-  GTP             -> ((succinyl_coA ^ 0.5) *( Pi^0.5) *( GDP^0.5))            - 0,                    
-  H2O             -> 0                                                        - ((fumarate ^0.5) * (H2O ^ 0.5)),                     
-  isocitrate      -> (citrate)                                                - ((isocitrate ^ 0.5) * (NAD ^ 0.5)),               
-  l_malate        -> ((fumarate ^0.5) * (H2O ^ 0.5))                          - ((l_malate^0.5) * (NAD ^ 0.5)),                       
-  NAD             -> 0                                                        - (((isocitrate ^ 0.5) * (NAD ^ 0.5)) + 
-                                                                                ((a_ketoglutarate ^ 0.3) * (NAD ^ 0.3) * (coA_sh ^ 0.3)) +
-                                                                                ((l_malate^0.5) * (NAD ^ 0.5))),      
-  NADH            -> ((isocitrate^0.5) *( NAD^0.5)) + 
-                     ((a_ketoglutarate^0.3) *( NAD ^ 0.3) * (coA_sh ^ 0.3)) + 
-                     ((l_malate^0.5) * (NAD ^ 0.5))                           - 0,                         
-  oxaloacetate    -> ((l_malate^0.5) * (NAD ^ 0.5))                           - ((oxaloacetate^0.5) * (acetyl_coA^0.5)),          
-  Pi              -> 0                                                        - ((succinyl_coA ^ 0.3) * (Pi ^ 0.3) * (GDP ^ 0.3)),
-  succinate       -> ((succinyl_coA ^ 0.3) * (Pi ^ 0.3) * (GDP ^ 0.3))        - ((succinate ^ 0.5 ) *( FAD ^ 0.5)),   
-  succinyl_coA    -> ((a_ketoglutarate ^ 0.3) * (NAD ^ 0.3) * (coA_sh ^ 0.3)) - ((succinyl_coA ^ 0.3) * (Pi ^ 0.3) * (GDP ^ 0.3)),  
-  a_ketoglutarate -> ((isocitrate ^ 0.5) * (NAD ^ 0.5))                       - ((a_ketoglutarate ^ 0.3) * (NAD ^ 0.3) * (coA_sh ^ 0.3))
+  # metabolite       synthesis                                    consumption                                           
+  citrate         -> (oxaloacetate^0.5)                         - (citrate ^ 0.5),              
+  coA_sh          -> (succinyl_coA^ 0.5)                        - ((a_ketoglutarate ^ 0.5) * (coA_sh ^ 0.5)),                            
+  fumarate        -> (succinate ^ 0.5)                          - (fumarate ^0.5),                  
+  isocitrate      -> (citrate ^ 0.5)                            - (isocitrate ^ 0.5),               
+  l_malate        -> (fumarate ^0.5)                            - (l_malate^0.5),                      
+  oxaloacetate    -> (l_malate^0.5)                             - (oxaloacetate^0.5),          
+  succinate       -> (succinyl_coA ^ 0.5)                       - (succinate ^ 0.5 ) ,   
+  succinyl_coA    -> ((a_ketoglutarate ^ 0.5) * (coA_sh ^ 0.5)) - (succinyl_coA ^ 0.5),  
+  a_ketoglutarate -> (isocitrate ^ 0.5)                         - ((a_ketoglutarate ^ 0.5) * (coA_sh ^ 0.5))
 ];
 
 let metabolites = list(
-    acetyl_coA = 100, CO2 = 100, citrate = 100, coA_sh = 100,
-    FAD = 100, FADH2 = 100, fumarate = 100, GDP = 100, GTP = 100,
-    H2O = 100, isocitrate = 100, l_malate = 100,
-    NAD = 100, NADH = 100, oxaloacetate = 100, Pi = 100,
+    citrate = 100, coA_sh = 100,
+    fumarate = 100, 
+    isocitrate = 100, l_malate = 100,
+    oxaloacetate = 100, 
     succinate = 100, succinyl_coA = 100, a_ketoglutarate = 100
 );
 
@@ -52,6 +37,6 @@ using data.driver as snapshot(relative_work("TCA-cycle.csv"), symbols = names(me
     |> kernel(S.script("TCA cycle"),strict=FALSE)
     |> environment(metabolites)
     |> s.system(TCA_cycle)
-    |> run(ticks = 50, resolution = 0.1);
+    |> run(ticks = 250, resolution = 0.01);
 }
 
