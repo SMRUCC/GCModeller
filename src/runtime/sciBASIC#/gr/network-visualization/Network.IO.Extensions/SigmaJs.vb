5226251ce2b88279418ece650522e377315d4891
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-28 03:35:31 +0800

    create json model

---
+++ src/runtime/sciBASIC#/gr/network-visualization/Network.IO.Extensions/SigmaJs.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+''' <summary>
+''' Create json for sigma.js
+''' </summary>
+Public Module SigmaJs
+
+    <Extension>
+    Public Function AsGraphology(g As NetworkGraph) As graphology.graph
+
+    End Function
+
+End Module
