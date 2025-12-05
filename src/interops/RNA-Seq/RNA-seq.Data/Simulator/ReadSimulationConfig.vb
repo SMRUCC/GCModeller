a6d25bb57333d4691b2be96b319e3236e2641434
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-06 07:56:26 +0800

    split source file into multiple parts

---
+++ src/interops/RNA-Seq/RNA-seq.Data/Simulator/ReadSimulationConfig.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports SMRUCC.genomics.SequenceModel.NucleotideModels
+
+''' <summary>
+''' 用于配置模拟测序reads生成过程的参数。
+''' </summary>
+Public Class ReadSimulationConfig
+    ''' <summary>
+    ''' 包含所有参考基因组序列的FastaSeq数组。
+    ''' </summary>
+    Public Property Genomes As SimpleSegment()
+
+    ''' <summary>
+    ''' 需要生成的reads总数。
+    ''' </summary>
+    Public Property NumberOfReads As Integer
+
+    ''' <summary>
+    ''' 模拟reads的最小和最大长度。
+    ''' </summary>
+    Public Property ReadLengthRange As IntRange
+
+    ''' <summary>
+    ''' （可选）各基因组的相对丰度权重。Key为基因组的SequenceId，Value为权重。
+    ''' 如果未提供或某个基因组未在此字典中，则默认权重为1.0（均匀分布）。
+    ''' </summary>
+    Public Property GenomeAbundanceWeights As New Dictionary(Of String, Double)()
+
+    ''' <summary>
+    ''' （可选）各基因组上的热点区域。Key为基因组的SequenceId，Value为该基因组上的热点列表。
+    ''' 如果未提供或某个基因组未在此字典中，则认为该基因组没有热点，read在其上均匀分布。
+    ''' </summary>
+    Public Property RegionHotspots As New Dictionary(Of String, List(Of RegionHotspot))()
+End Class
---
+++ src/interops/RNA-Seq/RNA-seq.Data/Simulator/RegionHotspot.vb
@@ -0,0 +1,19 @@
+﻿''' <summary>
+''' 用于定义模拟测序区域的“热点”，即高丰度区域。
+''' </summary>
+Public Class RegionHotspot
+    ''' <summary>
+    ''' 热点区域的起始位置（0-based索引）。
+    ''' </summary>
+    Public Property Start As Integer
+
+    ''' <summary>
+    ''' 热点区域的结束位置（0-based索引）。
+    ''' </summary>
+    Public Property [End] As Integer
+
+    ''' <summary>
+    ''' 该热点的相对权重。权重越高，被选中的概率越大。
+    ''' </summary>
+    Public Property Weight As Double
+End Class
