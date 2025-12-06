e1fe8f1b8ef25cdc891d61eacedd2f2c39efa24e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-06 14:09:19 +0800

    Classifier code

---
+++ src/GCModeller/analysis/Metagenome/Metagenome/Kmers/Classifier/SequenceHit.vb
@@ -0,0 +1,22 @@
+﻿Namespace Kmers
+
+    Public Class SequenceHit : Inherits SequenceSource
+
+        Public Property reads_title As String
+        Public Property identities As Double
+        Public Property total As Double
+        Public Property score As Double
+        Public Property ratio As Double
+
+        Sub New()
+        End Sub
+
+        Sub New(info As SequenceSource)
+            id = info.id
+            ncbi_taxid = info.ncbi_taxid
+            name = info.name
+            accession_id = info.accession_id
+        End Sub
+
+    End Class
+End Namespace
