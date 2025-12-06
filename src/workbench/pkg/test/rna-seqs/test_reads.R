b64d4c7817b425a011749080eaa0192ef4577e84
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-06 08:22:19 +0800

    run test reads generator

--- src/workbench/pkg/test/rna-seqs/test_reads.R
+++ src/workbench/pkg/test/rna-seqs/test_reads.R
@@ -5,6 +5,6 @@ imports ["bioseq.fasta","GenBank"] from "seqtoolkit";
 
 let asm_source = GenBank::load_genbanks(list.files("D:\\datapool\\ncbi_genbank") |> take(100));
 let genomes = lapply(asm_source, gb -> GenBank::origin_fasta(gb));
-let reads_data = FastQ::simulate_reads(genomes, n= 1000);
+let reads_data = FastQ::simulate_reads(genomes, n= 100000);
 
 write.fastq(reads_data, file = "Z:/test.fq");
