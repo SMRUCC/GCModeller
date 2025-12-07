e1d4f2e88170d59a0a6ef00444373b476b797f40
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-07 21:04:24 +0800

    fix of the cast scalr value slots

--- src/workbench/pkg/test/rna-seqs/16s_predict_test.R
+++ src/workbench/pkg/test/rna-seqs/16s_predict_test.R
@@ -4,6 +4,9 @@ imports "OTU_table" from "metagenomics_kit";
 imports "microbiome" from "metagenomics_kit";
 
 let otu_table = read.OTUtable("M:\project\20250818-ML\20251201\rawdata\发送数据_20251114\微生物数据\数据2_OTU分类表已抽平20251106\OTU_Taxon_Depth_otu.full .xls", OTUTaxonAnalysis=TRUE);
-let PICRUSt = read_PICRUSt(file(J:\ko_13_5_precalculated.PICRUSt), open = "open");
+let PICRUSt = read_PICRUSt(file("J:\ko_13_5_precalculated.PICRUSt"), open = "open");
+let result = PICRUSt |> predict_metagenomes(otu_table) |> as.data.frame();
 
-PICRUSt |> predict_metagenomes(otu_table);
+print(result);
+
+write.csv(result, file = "Z:/predicts.csv");
