b071472a9fefbaec4aacf8ebebabc33a34040eb3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-12 01:29:14 +0800

    make code style improvements

---
+++ src/GCModeller/analysis/Metagenome/Metagenome/Kmers/KmerBloomFilter.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+
+Namespace Kmers
+
+    Public Class KmerBloomFilter
+
+        ReadOnly bloomFilter As BloomFilter
+
+    End Class
+
+End Namespace
