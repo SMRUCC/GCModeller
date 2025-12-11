5f8c5751cafe7d3a958c8665c97bcff06d54121f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-12 01:38:56 +0800

    how to save to file

--- src/GCModeller/analysis/Metagenome/Metagenome/Kmers/KmerBloomFilter.vb
+++ src/GCModeller/analysis/Metagenome/Metagenome/Kmers/KmerBloomFilter.vb
@@ -1,10 +1,29 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+Imports Microsoft.VisualBasic.Data.IO
 
 Namespace Kmers
 
     Public Class KmerBloomFilter
 
         ReadOnly bloomFilter As BloomFilter
+        ReadOnly k As Integer
+        ''' <summary>
+        ''' the genome name
+        ''' </summary>
+        ReadOnly name As String
+        ReadOnly ncbi_taxid As Integer
+
+        Public Shared Function LoadFromFile(file As Stream) As KmerBloomFilter
+            Dim bin As New BinaryDataReader(file, leaveOpen:=True) With {.ByteOrder = ByteOrder.LittleEndian}
+
+        End Function
+
+        Public Sub Save(file As Stream)
+            Dim bin As New BinaryDataWriter(file, leaveOpen:=True) With {.ByteOrder = ByteOrder.LittleEndian}
+
+
+        End Sub
 
     End Class
 
