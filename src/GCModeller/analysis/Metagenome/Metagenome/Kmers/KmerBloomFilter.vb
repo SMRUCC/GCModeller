10aa058aae30d291b805d18e0247fac6c3b331cf
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-12 01:47:01 +0800

    read/save of the kmer bloom filter model file

--- src/GCModeller/analysis/Metagenome/Metagenome/Kmers/KmerBloomFilter.vb
+++ src/GCModeller/analysis/Metagenome/Metagenome/Kmers/KmerBloomFilter.vb
@@ -8,6 +8,9 @@ Namespace Kmers
     Public Class KmerBloomFilter
 
         ReadOnly bloomFilter As BloomFilter
+        ''' <summary>
+        ''' the length of the k-mer
+        ''' </summary>
         ReadOnly k As Integer
         ''' <summary>
         ''' the genome name
@@ -17,9 +20,29 @@ Namespace Kmers
 
         Const magicNum As String = "kmer-bloom"
 
+        Sub New(k As Integer, name As String, ncbi_taxid As Integer, bloomFilter As BloomFilter)
+            Me.k = k
+            Me.name = name
+            Me.ncbi_taxid = ncbi_taxid
+            Me.bloomFilter = bloomFilter
+        End Sub
+
         Public Shared Function LoadFromFile(file As Stream) As KmerBloomFilter
             Dim bin As New BinaryDataReader(file, leaveOpen:=True) With {.ByteOrder = ByteOrder.LittleEndian}
+            Dim magic As String = Encoding.ASCII.GetString(bin.ReadBytes(magicNum.Length))
 
+            If magic <> magicNum Then
+                Throw New InvalidDataException("invalid magic number for the k-mer bloom filter model file!")
+            End If
+
+            Dim k As Integer = bin.ReadInt32
+            Dim taxid As Integer = bin.ReadInt32
+            Dim name As String = bin.ReadString
+            Dim pk As Integer = bin.ReadInt32
+            Dim bytes As Byte() = bin.ReadBytes(bin.ReadInt32 \ 8)
+            Dim bloom As New BloomFilter(bytes, pk)
+
+            Return New KmerBloomFilter(k, name, taxid, bloom)
         End Function
 
         Public Sub Save(file As Stream)
