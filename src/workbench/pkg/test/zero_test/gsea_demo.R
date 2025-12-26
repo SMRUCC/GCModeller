5fad6d8bbe2d89c4003322010c5cd659a0e27fff
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-26 11:34:02 +0800

    create expression data

---
+++ src/workbench/pkg/test/zero_test/gsea_demo.R
@@ -0,0 +1,15 @@
+require(GCModeller);
+
+setwd(@dir);
+
+imports ["geneExpression", "sampleInfo"] from "phenotype_kit";
+
+let expr = load.expr("expression_matrix.csv")
+|> filterZeroGenes() 
+|> impute_missing(by.features=TRUE) 
+|> totalSumNorm() 
+|> geneExpression::relative(median = TRUE)
+;
+let sample_info = sampleInfo::guess.sample_groups(matrix_info(expr)$sampleID, raw.list = FALSE);
+
+print(as.data.frame(sample_info));
