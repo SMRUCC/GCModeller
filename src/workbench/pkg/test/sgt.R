02701d0d48d8d9493cf7c54dcf6145f54325b1ff
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-30 07:30:00 +0800

    ' 20251230 pos_j = -1 ' W0(pos_i, pos_j) and WK(pos_i, pos_j) will throw index outside error ' try to avoid such error

--- src/workbench/pkg/test/sgt.R
+++ src/workbench/pkg/test/sgt.R
@@ -7,15 +7,15 @@ imports "clustering" from "MLkit";
 setwd(@dir);
 
 # get fasta sequence data
-let seqs = read.csv("./protein_classification.csv", row.names = 1, check.names = FALSE);
-let sgt  = seq_sgt(moltype = "prot", kappa = 3,lengthsensitive= TRUE );
+let seqs = read.csv("Z:\seq\protein_class.csv", row.names = NULL, check.names = FALSE);
+let sgt  = seq_sgt(moltype = "prot", kappa = 1,lengthsensitive= FALSE );
 # get sequence embedding result
 let vec  = seq_vector(sgt, as.fasta(seqs), as.dataframe = TRUE);
 
 # run data analysis on the generated embedding vectors
 # embedding the raw matrix from high dimensional space
 # into latent space
-let latent = umap(vec, dimension=6,numberOfNeighbors =32); 
+let latent = umap(vec, dimension=6,numberOfNeighbors =24, method = "SpectralCosine"); 
 # run clustering
 let clusters = kmeans(as.data.frame( latent$umap,labels = latent$labels), 
     centers = 3, 
@@ -29,7 +29,7 @@ message("the input sequence data is embedding as result:");
 str(latent);
 print(clusters, max.print = 13);
 
-bitmap(file = "protein_classification.png",width = 800, height = 600) {
+bitmap(file = "protein_classification.png", size = c(1200, 800)) {
     plot(clusters$dimension_1, clusters$dimension_2, 
         class = clusters$Cluster, 
         colors = "paper", 
