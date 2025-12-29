a83df12eae18b8052b98b6b5e46661da2ccbbace
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-29 16:53:02 +0800

    create network builder layout

--- src/interops/RNA-Seq/RNA-seq.Data/Graph/Builder.vb
+++ src/interops/RNA-Seq/RNA-seq.Data/Graph/Builder.vb
@@ -1,6 +1,17 @@
-﻿Namespace Graph
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports SMRUCC.genomics.SequenceModel.FQ
 
-    Public Module Builder
+Namespace Graph
 
-    End Module
+    Public MustInherit Class Builder
+
+        Sub New(reads As IEnumerable(Of FastQ))
+            Call ProcessReads(reads)
+        End Sub
+
+        Protected MustOverride Sub ProcessReads(reads As IEnumerable(Of FastQ))
+
+        Public MustOverride Function Create() As NetworkGraph
+
+    End Class
 End Namespace
