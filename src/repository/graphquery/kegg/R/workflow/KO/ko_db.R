b77b7da5ca08249c131b5cfc5baa705c7a3253e5
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-04 16:29:38 +0800

    Update ko_db.R

--- src/repository/graphquery/kegg/R/workflow/KO/ko_db.R
+++ src/repository/graphquery/kegg/R/workflow/KO/ko_db.R
@@ -7,15 +7,32 @@
 #' @param db a local repository connection to the target kegg genes database to save and cache the data which is download from
 #'    the kegg website via the kegg rest api. this parameter value could be a local directory or file connection to the archive file 
 #'    which has internal filesystem tree management, example as connection to a zip archive file, HDF5 archive file or the R# HDS pack file.
+#' @param seqtype the fasta sequence data type for download from the KEGG database, default is gene nucleotide sequence.
 #' 
 #' @return this function returns nothing
 #' 
-const ko_db = function(db = "./", species = NULL) {
+const ko_db = function(db = "./", species = NULL, seqtype = c("ntseq","aaseq")) {
     # config local cache dir for REnv::getHtml function
     options(http.cache_dir = "$ko_db.http_cache");
     # set global environment for the repository model
     set(globalenv(),"$ko_db.http_cache", db);
 
+    db |> ko_db_worker(species,seqtype= seqtype);
+}
+
+#' An internal function for implements create ko database
+#' 
+#' @param species a character vector of the kegg organism species code for make taxonomy range filter of the gene to downloads.
+#'    default is NULL means no filter. example as set species parameter to c("eco","hsa","mmu") means this function will just download
+#'    the ko gene sequence of taxonomy Escherichia coli, Homo sapiens, Mus musculus, ignores other orgniams
+#' 
+#' @param db a local repository connection to the target kegg genes database to save and cache the data which is download from
+#'    the kegg website via the kegg rest api. this parameter value could be a local directory or file connection to the archive file 
+#'    which has internal filesystem tree management, example as connection to a zip archive file, HDF5 archive file or the R# HDS pack file.
+#' 
+#' @return this function returns nothing
+#' 
+const ko_db_worker = function(db, species, seqtype = c("ntseq","aaseq")) {
     let ko_index = load_ko_index(db);
 
     print("load ko index table for make request data:");
@@ -26,53 +43,62 @@ const ko_db = function(db = "./", species = NULL) {
         print(species);
     }
 
-    for(ko in tqdm(as.list(ko_index,byrow=TRUE))) {
-        let ko_id = ko$ko;
-        let geneId_file = `/ko/${ko_id}.txt`;
-        let ko_genes = NULL;
+    for(ko in as.list(ko_index,byrow=TRUE)) {
+        db |> fetch_ko_data(ko_id = ko$ko, 
+                            species = species, 
+                            seqtype = seqtype
+        );
+    }
 
-        if (!file.exists(geneId_file, fs=db)) {
-            let ko_genes = REnv::getHtml(`https://rest.kegg.jp/link/genes/${ko_id}`, interval = 3, filetype = "txt");
+    invisible(NULL);
+}
 
-            # split each text line with white space
-            # ko:K00001	dme:Dmel_CG3481
-            # ko:K00001	der:6540581
-            # ko:K00001	dse:6611292
-            # ko:K00001	dsi:Dsimw501_GD23968
-            # ko:K00001	dya:Dyak_GE10683
-            #
-            # first column is the KO id
-            # use the second column as gene id
-            ko_genes = ko_genes |> textlines() |> strsplit("\s+");
-            ko_genes = ko_genes@{2};
+#' An internal function for download data for a specific KO ID
+const fetch_ko_data = function(db, ko_id, species, seqtype = c("ntseq","aaseq")) {
+    let geneId_file = `/ko/${ko_id}.txt`;
+    let ko_genes = NULL;
 
-            writeLines(ko_genes, con = file.allocate(geneId_file, fs = db));
-        }        
+    if (!file.exists(geneId_file, fs=db)) {
+        let ko_genes = REnv::getHtml(`https://rest.kegg.jp/link/genes/${ko_id}`, interval = 3, filetype = "txt");
 
-        # split the kegg gene id by use `:` symbol as delimiter
-        # eco:b0001
-        # first column is the kegg organism species code
-        # second column is the gene locus tag
-        ko_genes = readLines(file.allocate(geneId_file, fs = db));
-        ko_genes = strsplit(ko_genes,"[:]");
-        ko_genes = data.frame(species = ko_genes@{1}, gene_id = ko_genes@{2});
+        # split each text line with white space
+        # ko:K00001	dme:Dmel_CG3481
+        # ko:K00001	der:6540581
+        # ko:K00001	dse:6611292
+        # ko:K00001	dsi:Dsimw501_GD23968
+        # ko:K00001	dya:Dyak_GE10683
+        #
+        # first column is the KO id
+        # use the second column as gene id
+        ko_genes = ko_genes |> textlines() |> strsplit("\s+");
+        ko_genes = ko_genes@{2};
 
-        print("genes that belongs to KO group:");
-        print(ko_genes, max.print = 6);
+        writeLines(ko_genes, con = file.allocate(geneId_file, fs = db));
+    }        
 
-        if (length(species) > 0) {
-            let i = ko_genes$species in species;
+    # split the kegg gene id by use `:` symbol as delimiter
+    # eco:b0001
+    # first column is the kegg organism species code
+    # second column is the gene locus tag
+    ko_genes = readLines(file.allocate(geneId_file, fs = db));
+    ko_genes = strsplit(ko_genes,"[:]");
+    ko_genes = data.frame(species = ko_genes@{1}, gene_id = ko_genes@{2});
 
-            if (sum(i) > 0) {
-                ko_genes = ko_genes[i,];
-            } else {
-                ko_genes = NULL;
-            }            
-        }
+    print("genes that belongs to KO group:");
+    print(ko_genes, max.print = 6);
 
-        if (nrow(ko_genes) > 0) {
-            db |> download_koseqs(ko_id, ko_genes);
-        }
+    if (length(species) > 0) {
+        let i = ko_genes$species in species;
+
+        if (sum(i) > 0) {
+            ko_genes = ko_genes[i,];
+        } else {
+            ko_genes = NULL;
+        }            
+    }
+
+    if (nrow(ko_genes) > 0) {
+        db |> download_koseqs(ko_id, ko_genes,seqtype =seqtype );
     }
 
     invisible(NULL);
@@ -107,7 +133,7 @@ const download_koseqs = function(db, ko_id, ko_genes, seqtype = c("ntseq","aaseq
         str(ko_genes);
 
         for(block in ko_genes) {
-            let url = `https://rest.kegg.jp/get/${paste(block, sep="+")}/ntseq`;
+            let url = `https://rest.kegg.jp/get/${paste(block, sep="+")}/${seqtype}`;
             let seqs = REnv::getHtml(url, interval = 3, filetype = "txt");
             
             fasta = c(fasta, seqs);
