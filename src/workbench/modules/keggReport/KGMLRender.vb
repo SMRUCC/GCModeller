ad38cec3674efcdd0858472eb1b3101b6d13c2c3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-14 15:58:39 +0800

    add network graph model into kegg report helper

---
+++ src/workbench/modules/keggReport/KGMLRender.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports SMRUCC.genomics.Assembly.KEGG.WebServices.KGML
+
+Public Class KGMLRender
+
+    ReadOnly kgml As pathway
+    ReadOnly graph As NetworkGraph
+
+    Sub New(kgml As pathway)
+        Me.kgml = kgml
+        Me.graph = GetNetwork(kgml)
+    End Sub
+
+    Public Shared Function GetNetwork(pathway As pathway) As NetworkGraph
+
+    End Function
+
+End Class
---
+++ src/workbench/modules/keggReport/NodeRepresentation.vb
@@ -0,0 +1,7 @@
+﻿Imports Microsoft.VisualBasic.Imaging.Driver
+
+Public Class NodeRepresentation
+
+    Public Property images As Dictionary(Of String, IGraphicsData)
+
+End Class
