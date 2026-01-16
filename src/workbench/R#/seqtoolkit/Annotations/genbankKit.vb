852c9a0553aefa8822a4cb8952a7c0ea18661739
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-16 16:57:36 +0800

    check of the requirdd data fields

--- src/workbench/R#/seqtoolkit/Annotations/genbankKit.vb
+++ src/workbench/R#/seqtoolkit/Annotations/genbankKit.vb
@@ -1,56 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::ebd0f6501151744fbdff69bd0a551b9f, R#\seqtoolkit\Annotations\genbankKit.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 557
-    '    Code Lines: 382 (68.58%)
-    ' Comment Lines: 109 (19.57%)
-    '    - Xml Docs: 97.25%
-    ' 
-    '   Blank Lines: 66 (11.85%)
-    '     File Size: 21.64 KB
+' Summaries:
 
 
-    ' Module genbankKit
-    ' 
-    '     Function: accession_id, addFeature, addMeta, addproteinSeq, addRNAGene
-    '               asGenbank, create_tabular, createFeature, enumerateFeatures, featureMeta
-    '               getOrAddNtOrigin, getRNASeq, isPlasmidSource, keyNames, populateGenbanks
-    '               Populates, readGenbank, Taxon_Id, writeGenbank
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 557
+'    Code Lines: 382 (68.58%)
+' Comment Lines: 109 (19.57%)
+'    - Xml Docs: 97.25%
+' 
+'   Blank Lines: 66 (11.85%)
+'     File Size: 21.64 KB
+
+
+' Module genbankKit
+' 
+'     Function: accession_id, addFeature, addMeta, addproteinSeq, addRNAGene
+'               asGenbank, create_tabular, createFeature, enumerateFeatures, featureMeta
+'               getOrAddNtOrigin, getRNASeq, isPlasmidSource, keyNames, populateGenbanks
+'               Populates, readGenbank, Taxon_Id, writeGenbank
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -621,7 +621,13 @@ Module genbankKit
             Call template.SetDefaultKey("nucl_loc", gene.Location.ContiguousRegion.ToString)
             Call template.SetDefaultKey("locus_tag", $"locus_{++i}")
 
-            Call fastaFile.Add(gene.ToGeneFasta(template))
+            Dim fa As FastaSeq = gene.ToGeneFasta(template)
+
+            If required.IsNullOrEmpty Then
+                Call fastaFile.Add(fa)
+            ElseIf Not template.GetLastMissingKeys.Intersect(required).Any Then
+                Call fastaFile.Add(fa)
+            End If
         Next
 
         If unique_names Then
