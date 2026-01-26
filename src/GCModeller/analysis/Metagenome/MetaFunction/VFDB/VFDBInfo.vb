f5957c409a6745db6062af057a2f8e5f2c547e8b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-26 17:11:03 +0800

    add VFDB dataset table

---
+++ src/GCModeller/analysis/Metagenome/MetaFunction/VFDB/VFDBInfo.vb
@@ -0,0 +1,18 @@
+﻿Namespace VFDB
+
+    Public Class VFDBInfo
+
+        Public Property VFID As String
+        Public Property VF_Name As String
+        Public Property VF_FullName As String
+        Public Property Bacteria As String
+        Public Property VFCID As String
+        Public Property VFcategory As String
+        Public Property Characteristics As String
+        Public Property [Structure] As String
+        Public Property [Function] As String
+        Public Property Mechanism As String
+        Public Property Reference As String
+
+    End Class
+End Namespace
