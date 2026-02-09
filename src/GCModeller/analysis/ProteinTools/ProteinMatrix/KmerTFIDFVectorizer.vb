e1b87e330dcc971a902003f4542d114fbd636c46
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-09 22:06:39 +0800

    handling of the nucleotide sequence

--- src/GCModeller/analysis/ProteinTools/ProteinMatrix/KmerTFIDFVectorizer.vb
+++ src/GCModeller/analysis/ProteinTools/ProteinMatrix/KmerTFIDFVectorizer.vb
@@ -2,22 +2,27 @@
 Imports Microsoft.VisualBasic.Data.NLP
 Imports SMRUCC.genomics.SequenceModel
 Imports SMRUCC.genomics.SequenceModel.FASTA
+Imports SMRUCC.genomics.SequenceModel.NucleotideModels
 
 Public Class KmerTFIDFVectorizer
 
     ReadOnly vec As New TFIDF
-    ReadOnly kmers As String()
     ReadOnly k As Integer
     ReadOnly type As SeqTypes
 
     Sub New(Optional type As SeqTypes = SeqTypes.Protein, Optional k As Integer = 6)
         Me.k = k
-        ' Me.kmers = New KSeqCartesianProduct(type).KmerSeeds(k).ToArray
         Me.type = type
     End Sub
 
     Public Sub Add(seq As FastaSeq)
-        Call vec.Add(seq.Title, KSeq.KmerSpans(seq.SequenceData, k))
+        Dim chars As String = seq.SequenceData
+
+        If type <> SeqTypes.Protein Then
+            chars = NucleicAcid.Canonical(chars)
+        End If
+
+        Call vec.Add(seq.Title, KSeq.KmerSpans(chars, k))
     End Sub
 
     Public Sub AddRange(seqs As IEnumerable(Of FastaSeq))
