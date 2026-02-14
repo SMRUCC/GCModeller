8f740bd71519f678125aeb2cb4914322595dd93d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-15 07:47:38 +0800

    draw tree

--- src/GCModeller/visualize/DataVisualizationExtensions/UPGMATreeDrawer.vb
+++ src/GCModeller/visualize/DataVisualizationExtensions/UPGMATreeDrawer.vb
@@ -1,3 +1,19 @@
-﻿Public Class UPGMATreeDrawer
+﻿Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports SMRUCC.genomics.Analysis.Metagenome.UPGMATree
 
+Public Class UPGMATreeDrawer : Inherits Plot
+
+    ReadOnly tree As Taxa
+
+    Public Sub New(tree As Taxa, theme As Theme)
+        MyBase.New(theme)
+        Me.tree = tree
+    End Sub
+
+    Protected Overrides Sub PlotInternal(ByRef g As IGraphics, canvas As GraphicsRegion)
+        Call TreeDrawer.DrawCircularTree(g, tree, canvas.Size)
+    End Sub
 End Class
