dc9d74edffe000b6f134e902cb46c1483c38e4f3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-20 21:04:06 +0800

    add BrendaJSON model

---
+++ src/GCModeller/data/Rhea/Brenda/BrendaJSON.vb
@@ -0,0 +1,39 @@
+﻿Public Class BrendaJSON
+
+    Public Property release As String
+    Public Property version As String
+    Public Property data As Dictionary(Of String, BrendaEnzymeData)
+
+End Class
+
+Public Class BrendaEnzymeData
+
+    Public Property id As String
+    Public Property recommended_name As String
+    Public Property systematic_name As String
+    Public Property reaction As ValueData()
+    Public Property synonyms As ValueData()
+    Public Property protein As Dictionary(Of String, ProteinData)
+
+End Class
+
+Public Class ProteinData
+
+    Public Property id As String
+    Public Property organism As String
+    Public Property references As String()
+    Public Property comment As String
+    Public Property source As String
+    Public Property accessions As String()
+
+
+End Class
+
+Public Class ValueData
+
+    Public Property value As String
+    Public Property proteins As String()
+    Public Property references As String()
+    Public Property comment As String
+
+End Class
