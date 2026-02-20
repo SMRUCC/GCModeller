9bfa4ba16088c31542c43969125c8476540bd69e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-20 23:54:42 +0800

    read brenda enzyme json data file

--- src/workbench/R#/annotationKit/Rhea.vb
+++ src/workbench/R#/annotationKit/Rhea.vb
@@ -1,61 +1,66 @@
 ﻿#Region "Microsoft.VisualBasic::63b759e8670b42da4f65f275790c3b83, R#\annotationKit\Rhea.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 35
-    '    Code Lines: 14 (40.00%)
-    ' Comment Lines: 18 (51.43%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 3 (8.57%)
-    '     File Size: 1.24 KB
+' Summaries:
 
 
-    ' Module Rhea
-    ' 
-    '     Function: load_reactions, openRDF
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 35
+'    Code Lines: 14 (40.00%)
+' Comment Lines: 18 (51.43%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 3 (8.57%)
+'     File Size: 1.24 KB
+
+
+' Module Rhea
+' 
+'     Function: load_reactions, openRDF
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.IO
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.MIME.application.json
+Imports Microsoft.VisualBasic.MIME.application.json.Javascript
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.ComponentModel.EquaionModel
 Imports SMRUCC.genomics.Data.Rhea
 Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 
@@ -119,5 +124,31 @@ Module Rhea
     <ExportAPI("reactions")>
     Public Function load_reactions(rhea As RheaRDF) As Reaction()
         Return rhea.GetReactions.ToArray
+    End Function
+
+    <ExportAPI("load_brenda_enzymes")>
+    <RApiReturn(GetType(BrendaEnzymeData))>
+    Public Function load_brenda_enzymes(<RRawVectorArgument> file As Object, Optional env As Environment = Nothing) As Object
+        Dim is_file As Boolean = False
+        Dim s = SMRUCC.Rsharp.GetFileStream(file, IO.FileAccess.Read, env, is_filepath:=is_file)
+
+        If s Like GetType(Message) Then
+            Return s.TryCast(Of Message)
+        End If
+
+        Dim json As JsonObject = TryCast(JsonParser.OpenStream(s.TryCast(Of Stream), tqdm:=True), JsonObject)
+        Dim pull As IEnumerable(Of BrendaEnzymeData) = LoadEnzymes(data:=json!data)
+
+        Return pipeline.CreateFromPopulator(pull)
+    End Function
+
+    <Extension>
+    Private Iterator Function LoadEnzymes(data As JsonObject) As IEnumerable(Of BrendaEnzymeData)
+        For Each id As String In data.ObjectKeys
+            Dim enzyme_json As JsonObject = data(id)
+            Dim enzyme As BrendaEnzymeData = enzyme_json.CreateObject(Of BrendaEnzymeData)
+
+            Yield enzyme
+        Next
     End Function
 End Module
