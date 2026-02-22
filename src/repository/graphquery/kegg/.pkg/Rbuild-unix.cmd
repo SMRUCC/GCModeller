97d85fda8eec2681a5da7b4fc1d83f3edd99b67b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-22 22:13:55 +0800

    run package msbuyild

--- src/repository/graphquery/kegg/.pkg/Rbuild-unix.cmd
+++ src/repository/graphquery/kegg/.pkg/Rbuild-unix.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-SET R_HOME=../../../../R-sharp\App\net8.0
+SET R_HOME=../../../../R-sharp\App\net10.0
 SET Rscript="%R_HOME%\Rscript.exe"
 SET REnv="%R_HOME%\R#.exe"
 SET pkg=kegg_api.zip
