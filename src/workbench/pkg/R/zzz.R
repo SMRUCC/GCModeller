780dd3f721c913039e2d2577852c83cc0449d03b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-03 15:24:22 +0800

    Update zzz.R

--- src/workbench/pkg/R/zzz.R
+++ src/workbench/pkg/R/zzz.R
@@ -1,11 +1,13 @@
 
-const .onLoad = function() {
-    const y = year();
+const .onLoad = function(quietly=FALSE) {
+    let y = year();
 
-    cat("\n\n");
-    cat("  GCModeller: genomics CAD(Computer Assistant Design) Modeller System\n");
-    cat("                                author by: xie.guigang@gcmodeller.org\n");
-    cat("\n");
-    cat(`                           (c) ${y} | SMRUCC genomics - GuiLin, China\n`);
-    cat("\n\n");
+    if (!quietly) {
+        cat("\n\n");
+        cat("  GCModeller: genomics CAD(Computer Assistant Design) Modeller System\n");
+        cat("                                author by: xie.guigang@gcmodeller.org\n");
+        cat("\n");
+        cat(`                           (c) ${y} | SMRUCC genomics - GuiLin, China\n`);
+        cat("\n\n");
+    }
 }
