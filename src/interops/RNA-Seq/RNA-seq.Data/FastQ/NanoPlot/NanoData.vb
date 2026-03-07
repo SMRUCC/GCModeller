56c84ca583d90c141809c9b6d6d480e95ebd70ce
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-07 23:38:28 +0800

    Update NanoData.vb

--- src/interops/RNA-Seq/RNA-seq.Data/FastQ/NanoPlot/NanoData.vb
+++ src/interops/RNA-Seq/RNA-seq.Data/FastQ/NanoPlot/NanoData.vb
@@ -21,6 +21,11 @@
         Public Property TotalBases As Double
         Public Property MeanLength As Double
         Public Property MedianLength As Double
+        ''' <summary>
+        ''' 长度标准差
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property ReadLengthStdev As Double
         Public Property N50 As Double
         Public Property MinLength As Long
         Public Property MaxLength As Long
@@ -30,7 +35,24 @@
         Public Property MinQuality As Double
         Public Property MaxQuality As Double
 
-        ' 可以根据需要添加 Q20, Q7 等比例统计
+        ' 新增：Top 5 最长序列 (Item1=Length, Item2=Quality)
+        Public Property LongestReads As List(Of Tuple(Of Double, Double))
+
+        ' 新增：Top 5 质量最高序列 (Item1=Quality, Item2=Length)
+        Public Property HighestQualityReads As List(Of Tuple(Of Double, Double))
+
+        ' 新增：Q值阈值统计
+        Public Class QThresholdStats
+            Public Property Count As Long
+            Public Property Percentage As Double
+            Public Property TotalBasesMb As Double ' 以 Mb 为单位
+        End Class
+
+        Public Property Q10 As New QThresholdStats
+        Public Property Q15 As New QThresholdStats
+        Public Property Q20 As New QThresholdStats
+        Public Property Q25 As New QThresholdStats
+        Public Property Q30 As New QThresholdStats
     End Class
 
     ''' <summary>
