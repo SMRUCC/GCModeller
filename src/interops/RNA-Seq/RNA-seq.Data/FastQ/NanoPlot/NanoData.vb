993221b823d32054149d5a9e9328391c4e409bd0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-07 23:41:00 +0800

    export nano plot summary result

--- src/interops/RNA-Seq/RNA-seq.Data/FastQ/NanoPlot/NanoData.vb
+++ src/interops/RNA-Seq/RNA-seq.Data/FastQ/NanoPlot/NanoData.vb
@@ -35,17 +35,29 @@
         Public Property MinQuality As Double
         Public Property MaxQuality As Double
 
-        ' 新增：Top 5 最长序列 (Item1=Length, Item2=Quality)
+        ''' <summary>
+        ''' Top 5 最长序列 (Item1=Length, Item2=Quality)
+        ''' </summary>
+        ''' <returns></returns>
         Public Property LongestReads As List(Of Tuple(Of Double, Double))
 
-        ' 新增：Top 5 质量最高序列 (Item1=Quality, Item2=Length)
+        ''' <summary>
+        ''' Top 5 质量最高序列 (Item1=Quality, Item2=Length)
+        ''' </summary>
+        ''' <returns></returns>
         Public Property HighestQualityReads As List(Of Tuple(Of Double, Double))
 
-        ' 新增：Q值阈值统计
+        ''' <summary>
+        ''' Q值阈值统计
+        ''' </summary>
         Public Class QThresholdStats
             Public Property Count As Long
             Public Property Percentage As Double
-            Public Property TotalBasesMb As Double ' 以 Mb 为单位
+            ''' <summary>
+            ''' 以 Mb 为单位
+            ''' </summary>
+            ''' <returns></returns>
+            Public Property TotalBasesMb As Double
         End Class
 
         Public Property Q10 As New QThresholdStats
--- src/interops/RNA-Seq/RNA-seq.Data/FastQ/NanoPlot/NanoPlot.vb
+++ src/interops/RNA-Seq/RNA-seq.Data/FastQ/NanoPlot/NanoPlot.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.Imaging.LayoutModel
+﻿Imports System.Text
+Imports Microsoft.VisualBasic.Imaging.LayoutModel
 
 Namespace FQ.NanoPlot
 
@@ -9,6 +10,55 @@ Namespace FQ.NanoPlot
         Public Property QualHist As HistogramBin()
         Public Property ScatterData As Point2D()
 
+        Public Overrides Function ToString() As String
+            Dim sb As New StringBuilder
+            Call GetNanoPlotReport(sb)
+            Return sb.ToString
+        End Function
+
+        ''' <summary>
+        ''' 获取NanoPlot格式的文本报告
+        ''' </summary>
+        ''' <param name="sb"></param>
+        Public Sub GetNanoPlotReport(sb As StringBuilder)
+            Dim s = Me.Summary
+
+            sb.AppendLine("Metrics" & vbTab & "dataset")
+            sb.AppendLine($"number_of_reads{vbTab}{s.TotalReads}")
+            sb.AppendLine($"number_of_bases{vbTab}{s.TotalBases:F1}")
+            sb.AppendLine($"median_read_length{vbTab}{s.MedianLength:F1}")
+            sb.AppendLine($"mean_read_length{vbTab}{s.MeanLength:F1}")
+            sb.AppendLine($"read_length_stdev{vbTab}{s.ReadLengthStdev:F1}")
+            sb.AppendLine($"n50{vbTab}{s.N50:F1}")
+            sb.AppendLine($"mean_qual{vbTab}{s.MeanQuality:F1}")
+            sb.AppendLine($"median_qual{vbTab}{s.MedianQuality:F1}")
+
+            ' 输出 Longest Reads
+            ' 格式: longest_read_(with_Q):1 48062 (20.0)
+            If s.LongestReads IsNot Nothing Then
+                For i As Integer = 0 To Math.Min(s.LongestReads.Count - 1, 4)
+                    Dim item = s.LongestReads(i)
+                    sb.AppendLine($"longest_read_(with_Q):{i + 1}{vbTab}{CInt(item.Item1)} ({item.Item2:F1})")
+                Next
+            End If
+
+            ' 输出 Highest Quality Reads
+            ' 格式: highest_Q_read_(with_length):1 28.9 (3936)
+            If s.HighestQualityReads IsNot Nothing Then
+                For i As Integer = 0 To Math.Min(s.HighestQualityReads.Count - 1, 4)
+                    Dim item = s.HighestQualityReads(i)
+                    sb.AppendLine($"highest_Q_read_(with_length):{i + 1}{vbTab}{item.Item1:F1} ({CInt(item.Item2)})")
+                Next
+            End If
+
+            ' 输出 Q阈值统计
+            ' 格式: Reads >Q10: 37404 (99.9%) 114.6Mb
+            sb.AppendLine($"Reads >Q10:{vbTab}{s.Q10.Count} ({s.Q10.Percentage:F1}%) {s.Q10.TotalBasesMb:F1}Mb")
+            sb.AppendLine($"Reads >Q15:{vbTab}{s.Q15.Count} ({s.Q15.Percentage:F1}%) {s.Q15.TotalBasesMb:F1}Mb")
+            sb.AppendLine($"Reads >Q20:{vbTab}{s.Q20.Count} ({s.Q20.Percentage:F1}%) {s.Q20.TotalBasesMb:F1}Mb")
+            sb.AppendLine($"Reads >Q25:{vbTab}{s.Q25.Count} ({s.Q25.Percentage:F1}%) {s.Q25.TotalBasesMb:F1}Mb")
+            sb.AppendLine($"Reads >Q30:{vbTab}{s.Q30.Count} ({s.Q30.Percentage:F1}%) {s.Q30.TotalBasesMb:F1}Mb")
+        End Sub
     End Class
 
 End Namespace
