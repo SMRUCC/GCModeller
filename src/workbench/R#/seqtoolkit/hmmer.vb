949662d16c9307e71af277746cce26f9f918f2e3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-20 23:17:00 +0800

    parse the hmmer3 model

---
+++ src/workbench/R#/seqtoolkit/hmmer.vb
@@ -0,0 +1,13 @@
+﻿Imports HMMER3
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("hmmer")>
+Module hmmer
+
+    <ExportAPI("parse_hmmer_model")>
+    Public Function parse_hmmer_model(x As String) As ProfileHMM
+        Return HMMER3Parser.ParseContent(x.SolveStream)
+    End Function
+
+End Module
