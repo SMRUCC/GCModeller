46d16e7558f67f553869f481e944183df3ce24ba
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-21 00:24:50 +0800

    run package msbuild

---
+++ src/workbench/pkg/@export/hmmer.d.ts
@@ -0,0 +1,24 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "hmmer" from "seqtoolkit";
+//
+// ref=seqtoolkit.hmmer@seqtoolkit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+ * 
+*/
+declare namespace hmmer {
+   /**
+   */
+   function parse_hmmer_model(x: string): object;
+   /**
+    * Parse the kofamscan table output
+    * 
+    * 
+     * @param file -
+     * @param env -
+     * 
+     * + default value Is ``null``.
+   */
+   function parse_kofamscan(file: any, env?: object): object;
+}
---
+++ src/workbench/pkg/man/seqtoolkit/hmmer/hmmer.md
@@ -0,0 +1,6 @@
+﻿# hmmer
+
+
+
++ [parse_hmmer_model](hmmer/parse_hmmer_model.1) 
++ [parse_kofamscan](hmmer/parse_kofamscan.1) Parse the kofamscan table output
