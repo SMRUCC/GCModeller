0f94541bfa6655b95f02e18eac67ecd2f47ddbdd
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-21 21:56:44 +0800

    parse meme motif file

---
+++ src/workbench/R#/TRNtoolkit/Motifs.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Analysis.SequenceTools.SequencePatterns.Motif
+Imports SMRUCC.genomics.Interops.NBCR
+
+<Package("motif_tool")>
+Module MotifsTool
+
+    ''' <summary>
+    ''' read meme motif text file
+    ''' </summary>
+    ''' <param name="file">file path to the meme motif text file(*.meme)</param>
+    ''' <returns></returns>
+    <ExportAPI("read_meme")>
+    Public Function read_meme(file As String) As MotifPWM()
+        Return MEME_Suite.ParsePWMFile(file).ToArray
+    End Function
+
+End Module
