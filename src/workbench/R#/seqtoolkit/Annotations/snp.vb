92be0a9f122478b0b186b63798d33309d026c67f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-21 21:52:39 +0800

    add reference tot he snp scan tool

---
+++ src/workbench/R#/seqtoolkit/Annotations/snp.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Analysis.SequenceTools.SNP
+Imports SMRUCC.genomics.Analysis.SequenceTools.SNP.SangerSNPs
+Imports SMRUCC.genomics.SequenceModel.FASTA
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("snp_toolkit")>
+<RTypeExport("snp", GetType(SNP))>
+Module snpTools
+
+    <ExportAPI("snp_scan")>
+    Public Function snp_scan(nt As FastaFile,
+                             ref_index$,
+                             Optional pureMode As Boolean = False,
+                             Optional monomorphic As Boolean = False,
+                             Optional ByRef vcf_output_filename$ = Nothing) As SNPsAln
+
+        Return nt.ScanSNPs(ref_index, pureMode, monomorphic, vcf_output_filename)
+    End Function
+End Module
