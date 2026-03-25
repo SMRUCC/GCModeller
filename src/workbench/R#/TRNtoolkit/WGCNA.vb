2a5987f8f8b9218fa48ede8b641158c244664b20
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-25 16:36:46 +0800

    make evaluate of the correlation helper

--- src/workbench/R#/TRNtoolkit/WGCNA.vb
+++ src/workbench/R#/TRNtoolkit/WGCNA.vb
@@ -57,8 +57,15 @@ Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Analysis.RNA_Seq.RTools.WGCNA.Network
+Imports SMRUCC.genomics.Model.Network.Regulons
+Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop
 Imports any = Microsoft.VisualBasic.Scripting
+Imports HTSMatrix = SMRUCC.genomics.Analysis.HTS.DataFrame.Matrix
+Imports RInternal = SMRUCC.Rsharp.Runtime.Internal
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Vectorization
 
 <Package("WGCNA")>
 Module WGCNA
@@ -97,9 +104,9 @@ Module WGCNA
     ''' <returns></returns>
     <ExportAPI("interations")>
     Public Function CorrelationNetwork(g As NetworkGraph, WGCNA As WGCNAWeight, modules As list, Optional threshold As Double = 0.3) As Object
-        For Each conn As Weight In WGCNA.AsEnumerable.Where(Function(cn) cn.Weight >= threshold)
-            Dim u As Node = g.GetElementByID(conn.FromNode)
-            Dim v As Node = g.GetElementByID(conn.ToNode)
+        For Each conn As Weight In WGCNA.AsEnumerable.Where(Function(cn) cn.weight >= threshold)
+            Dim u As Node = g.GetElementByID(conn.fromNode)
+            Dim v As Node = g.GetElementByID(conn.toNode)
 
             If u Is Nothing OrElse v Is Nothing Then
                 Continue For
@@ -113,14 +120,68 @@ Module WGCNA
             data(NamesOf.REFLECTION_ID_MAPPING_INTERACTION_TYPE) = If(color1 = color2, color1, $"{color1}+{color2}")
 
             If edges.Length = 0 Then
-                g.CreateEdge(u, v, conn.Weight, data)
+                g.CreateEdge(u, v, conn.weight, data)
             Else
                 For Each link In edges
-                    link.weight += conn.Weight
+                    link.weight += conn.weight
                 Next
             End If
         Next
 
         Return g
+    End Function
+
+    <ExportAPI("expr_cor")>
+    <RApiReturn(GetType(LazyCorrelationMatrix), GetType(dataframe))>
+    Public Function expr_cor(expr As Object,
+                             <RRawVectorArgument(TypeCodes.string)> Optional id1 As Object = Nothing,
+                             <RRawVectorArgument(TypeCodes.string)> Optional id2 As Object = Nothing,
+                             Optional env As Environment = Nothing) As Object
+
+        Dim check_eval As Boolean = id1 IsNot Nothing AndAlso id2 IsNot Nothing
+
+        If check_eval Then
+            If Not TypeOf expr Is LazyCorrelationMatrix Then
+                Return RInternal.debug.stop("the given data expr object should be a correlation matrix object when id1 and id2 is presentes!", env)
+            End If
+
+            Dim cor As LazyCorrelationMatrix = DirectCast(expr, LazyCorrelationMatrix)
+            Dim idset1 As GetVectorElement = GetVectorElement.Create(Of String)(id1)
+            Dim idset2 As GetVectorElement = GetVectorElement.Create(Of String)(id2)
+
+            If Not GetVectorElement.DoesSizeMatch(idset1, idset2) Then
+                Return RInternal.debug.stop($"the dimension size of id1({idset1.size}) is not matched with the dimension size of id2({idset2.size})!", env)
+            End If
+
+            Dim eval As New dataframe With {
+               .columns = New Dictionary(Of String, Array)
+            }
+            Dim id1vec As New List(Of String)
+            Dim id2vec As New List(Of String)
+            Dim corvec As New List(Of Double)
+            Dim pvalvec As New List(Of Double)
+
+            For Each tuple In GetVectorElement.Zip(idset1, idset2)
+                Dim x As String = CStr(tuple.Item1)
+                Dim y As String = CStr(tuple.Item2)
+                Dim corResult = cor.Correlation(x, y)
+
+                Call id1vec.Add(x)
+                Call id2vec.Add(y)
+                Call corvec.Add(corResult.cor)
+                Call pvalvec.Add(corResult.pval)
+            Next
+
+            Call eval.add("id1", id1vec)
+            Call eval.add("id2", id2vec)
+            Call eval.add("cor", corvec)
+            Call eval.add("pval", pvalvec)
+
+            Return eval
+        ElseIf TypeOf expr Is HTSMatrix Then
+            Return New LazyCorrelationMatrix(DirectCast(expr, HTSMatrix))
+        Else
+            Return Message.InCompatibleType(GetType(HTSMatrix), expr.GetType, env)
+        End If
     End Function
 End Module
