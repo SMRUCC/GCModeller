0657da83c37471a82f1756ee89b5925b698c483f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-30 18:05:31 +0800

    split source file into multiple parts

--- src/GCModeller/analysis/SequenceToolkit/Pfam-HMM/PfamHMMScan/InterPro/interprodb.vb
+++ src/GCModeller/analysis/SequenceToolkit/Pfam-HMM/PfamHMMScan/InterPro/interprodb.vb
@@ -182,28 +182,6 @@ Namespace Interpro.Xml
         End Function
     End Class
 
-    <XmlType("interpro")>
-    Public Class Interpro
-        <XmlAttribute> Public Property id As String
-        <XmlAttribute> Public Property protein_count As Integer
-        <XmlAttribute> Public Property short_name As String
-        <XmlAttribute> Public Property type As String
-        Public Property name As String
-        Public Property abstract As String
-        Public Property pub_list As Publication()
-        Public Property parent_list As RelRef()
-        Public Property contains As RelRef()
-        Public Property found_in As RelRef()
-        Public Property member_list As DbXref()
-        Public Property external_doc_list As DbXref()
-        Public Property structure_db_links As DbXref()
-        Public Property taxonomy_distribution As TaxonData()
-        Public Property sec_list As SecAcc()
-
-        Public Overrides Function ToString() As String
-            Return $"[{type}] {short_name}   {name}"
-        End Function
-    End Class
 
 
     Public Class SecAcc
