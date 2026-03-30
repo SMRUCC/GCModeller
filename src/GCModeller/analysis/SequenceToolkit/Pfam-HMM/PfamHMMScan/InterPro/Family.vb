51c9b339758562098c94d1abbf4a17cadf9ab59d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-30 18:04:52 +0800

    removes obsolste codes

--- src/GCModeller/analysis/SequenceToolkit/Pfam-HMM/PfamHMMScan/InterPro/Family.vb
+++ src/GCModeller/analysis/SequenceToolkit/Pfam-HMM/PfamHMMScan/InterPro/Family.vb
@@ -1,44 +1,44 @@
 ﻿#Region "Microsoft.VisualBasic::44af93241d380cd687926499663ac19b, DataTools\Interpro\Xml\Family.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Family
-    ' 
-    '         Properties: Includes, Interpro, Name, Pfam
-    ' 
-    '         Function: CreateObject, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Family
+' 
+'         Properties: Includes, Interpro, Name, Pfam
+' 
+'         Function: CreateObject, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -50,6 +50,7 @@ Namespace Interpro.Xml
     ''' protein family data information
     ''' </summary>
     Public Class Family
+
         Public Property Interpro As String
         Public Property Name As String
         Public Property Pfam As String()
@@ -77,6 +78,15 @@ Namespace Interpro.Xml
                 .Pfam = Pfam,
                 .Includes = LQuery
             }
+        End Function
+
+        Public Shared Function BuildFamilies(DbXml As interprodb) As IEnumerable(Of Family)
+            Dim dict = DbXml.interpro.ToDictionary(Function(x) x.id)
+
+            Return From interpro
+                   In dict.AsParallel
+                   Where String.Equals(interpro.Value.type, "family", StringComparison.OrdinalIgnoreCase)
+                   Select Family.CreateObject(interpro.Value, dict)
         End Function
     End Class
 End Namespace
