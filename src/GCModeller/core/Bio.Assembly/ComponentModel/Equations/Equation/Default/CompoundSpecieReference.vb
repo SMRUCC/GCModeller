9f8fc7a21ca45463fa22b5d8e972a78cc019b914
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-02 03:48:01 +0800

    default models

---
+++ src/GCModeller/core/Bio.Assembly/ComponentModel/Equations/Equation/Default/CompoundSpecieReference.vb
@@ -0,0 +1,60 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ComponentModel.TagData
+
+Namespace ComponentModel.EquaionModel.DefaultTypes
+
+    ''' <summary>
+    ''' the compound model reference.
+    ''' </summary>
+    Public Class CompoundSpecieReference : Implements ICompoundSpecies
+
+        ''' <summary>
+        ''' 化学计量数
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlAttribute> Public Property Stoichiometry As Double Implements ICompoundSpecies.Stoichiometry
+        <XmlText> Public Property ID As String Implements ICompoundSpecies.Key
+        <XmlAttribute> Public Property Compartment As String
+
+        Sub New()
+        End Sub
+
+        Sub New(ref As ICompoundSpecies)
+            Stoichiometry = ref.Stoichiometry
+            ID = ref.Key
+        End Sub
+
+        Sub New(factor As Double, compound As String)
+            Stoichiometry = factor
+            ID = compound
+        End Sub
+
+        Sub New(factor As Double, compound As String, compart As String)
+            Me.Stoichiometry = factor
+            Me.ID = compound
+            Me.Compartment = compart
+        End Sub
+
+        Public Overloads Function Equals(b As ICompoundSpecies, strict As Boolean) As Boolean
+            Return Equivalence.Equals(Me, b, strict)
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function AsFactor() As FactorString(Of Double)
+            Return New FactorString(Of Double) With {
+                .factor = Stoichiometry,
+                .result = ID
+            }
+        End Function
+
+        Public Overrides Function ToString() As String
+            If Stoichiometry > 1 Then
+                Return String.Format("{0} {1}", Stoichiometry, ID)
+            Else
+                Return ID
+            End If
+        End Function
+    End Class
+
+End Namespace
--- src/GCModeller/core/Bio.Assembly/ComponentModel/Equations/Equation/Default/Equation.vb
+++ src/GCModeller/core/Bio.Assembly/ComponentModel/Equations/Equation/Default/Equation.vb
@@ -74,59 +74,6 @@ Imports Microsoft.VisualBasic.Linq
 Namespace ComponentModel.EquaionModel.DefaultTypes
 
     ''' <summary>
-    ''' the compound model reference.
-    ''' </summary>
-    Public Class CompoundSpecieReference : Implements ICompoundSpecies
-
-        ''' <summary>
-        ''' 化学计量数
-        ''' </summary>
-        ''' <returns></returns>
-        <XmlAttribute> Public Property Stoichiometry As Double Implements ICompoundSpecies.Stoichiometry
-        <XmlText> Public Property ID As String Implements ICompoundSpecies.Key
-        <XmlAttribute> Public Property Compartment As String
-
-        Sub New()
-        End Sub
-
-        Sub New(ref As ICompoundSpecies)
-            Stoichiometry = ref.Stoichiometry
-            ID = ref.Key
-        End Sub
-
-        Sub New(factor As Double, compound As String)
-            Stoichiometry = factor
-            ID = compound
-        End Sub
-
-        Sub New(factor As Double, compound As String, compart As String)
-            Me.Stoichiometry = factor
-            Me.ID = compound
-            Me.Compartment = compart
-        End Sub
-
-        Public Overloads Function Equals(b As ICompoundSpecies, strict As Boolean) As Boolean
-            Return Equivalence.Equals(Me, b, strict)
-        End Function
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Function AsFactor() As FactorString(Of Double)
-            Return New FactorString(Of Double) With {
-                .factor = Stoichiometry,
-                .result = ID
-            }
-        End Function
-
-        Public Overrides Function ToString() As String
-            If Stoichiometry > 1 Then
-                Return String.Format("{0} {1}", Stoichiometry, ID)
-            Else
-                Return ID
-            End If
-        End Function
-    End Class
-
-    ''' <summary>
     ''' 默认类型的反应表达式的数据结构，可以使用<see cref="EquationBuilder.CreateObject(String)"/>来进行构建
     ''' </summary>
     ''' <remarks></remarks>
