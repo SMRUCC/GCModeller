72b8f16f831756b9d0dd3564eca098ef4905cdcc
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-03 23:53:36 +0800

    Create ReadonlyStream.vb

---
+++ src/runtime/sciBASIC#/Data/BinaryData/BinaryData/Stream/ReadonlyStream.vb
@@ -0,0 +1,91 @@
+﻿#Region "Microsoft.VisualBasic::93f6a83769b7733e227e6108d2870de9, Data\BinaryData\SQLite3\Helpers\ReadonlyStream.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xie (genetics@smrucc.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2018 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+
+    ' /********************************************************************************/
+
+    ' Summaries:
+
+
+    ' Code Statistics:
+
+    '   Total Lines: 24
+    '    Code Lines: 20 (83.33%)
+    ' Comment Lines: 0 (0.00%)
+    '    - Xml Docs: 0.00%
+    ' 
+    '   Blank Lines: 4 (16.67%)
+    '     File Size: 701 B
+
+
+    '     Class ReadonlyStream
+    ' 
+    '         Properties: CanWrite
+    ' 
+    '         Sub: Flush, SetLength, Write
+    ' 
+    ' 
+    ' /********************************************************************************/
+
+#End Region
+
+Imports System.IO
+
+Public MustInherit Class ReadonlyStream : Inherits Stream
+
+    Public Overrides ReadOnly Property CanWrite() As Boolean
+        Get
+            Return False
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' do nothing at here 
+    ''' </summary>
+    Public Overrides Sub Flush()
+        Call "readonly stream not supports of flash stream data".warning
+    End Sub
+
+    ''' <summary>
+    ''' do nothing at here
+    ''' </summary>
+    ''' <param name="value"></param>
+    Public Overrides Sub SetLength(value As Long)
+        Call "readonly stream not supports of modify stream size, this action just be ignored".error
+    End Sub
+
+    ''' <summary>
+    ''' do nothing at here
+    ''' </summary>
+    ''' <param name="buffer"></param>
+    ''' <param name="offset"></param>
+    ''' <param name="count"></param>
+    Public Overrides Sub Write(buffer As Byte(), offset As Integer, count As Integer)
+        Call "readonly stream not supports of modify stream data, this action just be ignored".error
+    End Sub
+End Class
