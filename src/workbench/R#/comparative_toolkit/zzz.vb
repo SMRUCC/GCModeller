d16b752004433b188bc1eec5c240f412c1c68638
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-04 03:21:49 +0800

    move pangenome ananlysis tool to comparative toolkit project

---
+++ src/workbench/R#/comparative_toolkit/zzz.vb
@@ -0,0 +1,14 @@
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Assembly: RPackageModule>
+
+Public NotInheritable Class zzz
+
+    Shared Sub New()
+    End Sub
+
+    Public Shared Sub onLoad()
+
+    End Sub
+
+End Class
