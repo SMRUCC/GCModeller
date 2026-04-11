9e76b73e21ff22bff735e59251230439056c4d3b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-11 18:05:54 +0800

    make code style improvvements

--- src/GCModeller/core/Bio.Assembly/ComponentModel/Locus/Interfaces.vb
+++ src/GCModeller/core/Bio.Assembly/ComponentModel/Locus/Interfaces.vb
@@ -75,27 +75,31 @@
 Namespace ComponentModel.Loci.Abstract
 
     Public Interface ILocationSegment
+
         ''' <summary>
-        ''' Tag data on this location sequence segment.(当前的这个序列片段的标签信息)
+        ''' Tag data on this location sequence segment.
         ''' </summary>
         ''' <value></value>
         ''' <returns></returns>
-        ''' <remarks></remarks>
+        ''' <remarks>(当前的这个序列片段的标签信息)</remarks>
         ReadOnly Property UniqueId As String
         ''' <summary>
-        ''' The location value of this sequence segment.(这个序列片段的位置信息)
+        ''' The location value of this sequence segment.
         ''' </summary>
         ''' <value></value>
         ''' <returns></returns>
-        ''' <remarks></remarks>
+        ''' <remarks>(这个序列片段的位置信息)</remarks>
         ReadOnly Property Location As Location
+
     End Interface
 
     ''' <summary>
     ''' Abstract model with location data.
     ''' </summary>
     Public Interface IContig
+
         Property Location As NucleotideLocation
+
     End Interface
 
     Public Interface ILocationNucleotideSegment : Inherits ILocationSegment
@@ -121,6 +125,7 @@ Namespace ComponentModel.Loci.Abstract
         ''' <returns></returns>
         ''' <remarks></remarks>
         Property right As Integer
+
     End Interface
 
     ''' <summary>
@@ -129,11 +134,12 @@ Namespace ComponentModel.Loci.Abstract
     Public Interface ILoci
 
         ''' <summary>
-        ''' Left position of the loci site on sequence.(左端起始位点)
+        ''' Left position of the loci site on sequence.
         ''' </summary>
         ''' <value></value>
         ''' <returns></returns>
-        ''' <remarks></remarks>
+        ''' <remarks>(左端起始位点)</remarks>
         Property left As Integer
+
     End Interface
 End Namespace
