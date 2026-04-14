bf627e38c604e9f37ef1ae19e9591b7bb9581ac7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-14 22:40:01 +0800

    fix of the nam,espace referen ce error

--- src/GCModeller/models/SBML/Biopax/Level3/File.vb
+++ src/GCModeller/models/SBML/Biopax/Level3/File.vb
@@ -1,66 +1,66 @@
 ﻿#Region "Microsoft.VisualBasic::24c00dbb3d984195a7137884fac38728, models\SBML\Biopax\Level3\File.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 66
-    '    Code Lines: 51 (77.27%)
-    ' Comment Lines: 5 (7.58%)
-    '    - Xml Docs: 80.00%
-    ' 
-    '   Blank Lines: 10 (15.15%)
-    '     File Size: 3.47 KB
+' Summaries:
 
 
-    '     Class DescriptionData
-    ' 
-    ' 
-    ' 
-    '     Class File
-    ' 
-    '         Properties: base, BiochemicalPathwayStep, BiochemicalReaction, BioSource, Catalysis
-    '                     CellularLocationVocabulary, ChemicalStructure, Complex, FragmentFeature, Interaction
-    '                     OwlOntology, Pathway, PathwayStep, PhysicalEntity, Protein
-    '                     ProteinReference, Provenance, PublicationXref, RelationshipTypeVocabulary, RelationshipXref
-    '                     SequenceInterval, SequenceSite, SmallMoleculeReference, SmallMolecules, Stoichiometry
-    '                     Transport, TransportWithBiochemicalReaction, UnificationXref
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: LoadDoc
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 66
+'    Code Lines: 51 (77.27%)
+' Comment Lines: 5 (7.58%)
+'    - Xml Docs: 80.00%
+' 
+'   Blank Lines: 10 (15.15%)
+'     File Size: 3.47 KB
+
+
+'     Class DescriptionData
+' 
+' 
+' 
+'     Class File
+' 
+'         Properties: base, BiochemicalPathwayStep, BiochemicalReaction, BioSource, Catalysis
+'                     CellularLocationVocabulary, ChemicalStructure, Complex, FragmentFeature, Interaction
+'                     OwlOntology, Pathway, PathwayStep, PhysicalEntity, Protein
+'                     ProteinReference, Provenance, PublicationXref, RelationshipTypeVocabulary, RelationshipXref
+'                     SequenceInterval, SequenceSite, SmallMoleculeReference, SmallMolecules, Stoichiometry
+'                     Transport, TransportWithBiochemicalReaction, UnificationXref
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: LoadDoc
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -68,6 +68,7 @@ Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.MIME.application.rdf_xml
 Imports Microsoft.VisualBasic.MIME.application.xml
+Imports SMRUCC.genomics.Model.Biopax.Level3.Elements
 
 Namespace Level3
 
@@ -106,9 +107,9 @@ Namespace Level3
         <XmlElement> Public Property RelationshipXref As RelationshipXref()
         <XmlElement> Public Property RelationshipTypeVocabulary As RelationshipTypeVocabulary()
         <XmlElement> Public Property PublicationXref As PublicationXref()
-        <XmlElement> Public Property PhysicalEntity As PhysicalEntity()
+        <XmlElement> Public Property PhysicalEntity As physicalEntity()
         <XmlElement> Public Property PathwayStep As PathwayStep()
-        <XmlElement> Public Property Pathway As pathway()
+        <XmlElement> Public Property Pathway As Pathway()
         <XmlElement> Public Property BiochemicalPathwayStep As BiochemicalPathwayStep()
         <XmlElement> Public Property ChemicalStructure As ChemicalStructure()
         <XmlElement> Public Property Interaction As Interaction()
--- src/GCModeller/models/SBML/Biopax/Level3/ResourceReader.vb
+++ src/GCModeller/models/SBML/Biopax/Level3/ResourceReader.vb
@@ -1,55 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::2ae9bacd98c24f7248d193ea817d5782, models\SBML\Biopax\Level3\ResourceReader.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 154
-    '    Code Lines: 126 (81.82%)
-    ' Comment Lines: 4 (2.60%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 24 (15.58%)
-    '     File Size: 7.10 KB
+' Summaries:
 
 
-    '     Class ResourceReader
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: GetAllCompounds, GetAllReactions, GetCompoundResource, LoadResource
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 154
+'    Code Lines: 126 (81.82%)
+' Comment Lines: 4 (2.60%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 24 (15.58%)
+'     File Size: 7.10 KB
+
+
+'     Class ResourceReader
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: GetAllCompounds, GetAllReactions, GetCompoundResource, LoadResource
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -60,6 +60,7 @@ Imports Microsoft.VisualBasic.Text.Xml
 Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
 Imports SMRUCC.genomics.ComponentModel.EquaionModel.DefaultTypes
 Imports SMRUCC.genomics.MetabolicModel
+Imports SMRUCC.genomics.Model.Biopax.Level3.Elements
 
 Namespace Level3
 
@@ -94,7 +95,7 @@ Namespace Level3
                     stoichiometryValue = stoichiometry.stoichiometricCoefficient
                 End If
 
-                Dim cellularLocation As EntityProperties.term = Nothing
+                Dim cellularLocation As term = Nothing
 
                 If location IsNot Nothing Then
                     cellularLocation = cellularLocations(location).term
