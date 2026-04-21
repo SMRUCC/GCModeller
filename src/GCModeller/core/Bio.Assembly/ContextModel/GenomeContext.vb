6d71d92ccb72601172ab43775e22c635eea544c2
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-21 10:33:00 +0800

    add xml comment docs

--- src/GCModeller/core/Bio.Assembly/ContextModel/GenomeContext.vb
+++ src/GCModeller/core/Bio.Assembly/ContextModel/GenomeContext.vb
@@ -65,20 +65,47 @@ Imports SMRUCC.genomics.ComponentModel.Loci
 
 Namespace ContextModel
 
+    ''' <summary>
+    ''' 表示一个基因组上下文模型，用于在内存中高效地存储、索引和查询基因组特征（如基因）。
+    ''' </summary>
+    ''' <typeparam name="T">基因组特征的类型，必须实现 <see cref="IGeneBrief"/> 接口。</typeparam>
+    ''' <remarks>
+    ''' 此对象在初始化时会对基因按照物理位置和链方向进行排序和索引，
+    ''' 从而将区间查询的时间复杂度优化至 O(log N) 或通过提前跳出优化至近似 O(N)。
+    ''' <para>
+    ''' 注意：此类假设输入的基因组特征集合是基于同一条染色体或重叠群 的，
+    ''' 跨越不同染色体的特征不应放入同一个上下文实例中。
+    ''' </para>
+    ''' </remarks>
     Public Class GenomeContext(Of T As IGeneBrief) : Implements Enumeration(Of T)
 
+        ''' <summary>
+        ''' 正链基因集合。按照 <see cref="NucleotideLocation.Left"/> 升序排列。
+        ''' </summary>
         Dim plus As T()
+        ''' <summary>
+        ''' 负链基因集合。按照 <see cref="NucleotideLocation.Right"/> 降序排列（符合负链转录方向）。
+        ''' </summary>
         Dim minus As T()
         ''' <summary>
-        ''' 按照<see cref="NucleotideLocation.Left"/>从小到大排序的
+        ''' 所有基因的统一物理位置序列。严格按照 <see cref="NucleotideLocation.Left"/> 从小到大排序。
+        ''' 此数组是执行区间范围查询的基础，确保了空间局部性和提前跳出循环的正确性。
         ''' </summary>
         Dim sequence As T()
+        ''' <summary>
+        ''' 按照 <see cref="IGeneBrief.Feature"/> 关键字分组的基因字典，用于快速按名称检索。
+        ''' </summary>
         Dim featureTags As Dictionary(Of String, T())
         ''' <summary>
-        ''' The name of this genome
+        ''' 当前基因组上下文的名称（如染色体名称或菌株名称）。
         ''' </summary>
         Dim contextName$
 
+        ''' <summary>
+        ''' 通过物理位置索引获取基因组特征
+        ''' </summary>
+        ''' <param name="i">在 <see cref="sequence"/> 中的零基索引。</param>
+        ''' <returns>对应位置的特征对象。</returns>
         Default Public ReadOnly Property Feature(i As Integer) As T
             <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
@@ -86,6 +113,11 @@ Namespace ContextModel
             End Get
         End Property
 
+        ''' <summary>
+        ''' 通过特征名称获取基因组特征。如果存在同名特征，返回第一个匹配项。
+        ''' </summary>
+        ''' <param name="name">特征的唯一标识符或功能名称。</param>
+        ''' <returns>匹配的特征对象，如果未找到则返回类型的默认值 (Nothing)。</returns>
         Default Public ReadOnly Property Feature(name As String) As T
             Get
                 Return featureTags.TryGetValue(name).DefaultFirst
@@ -108,6 +140,10 @@ Namespace ContextModel
         ''' <returns></returns>
         Public ReadOnly Property size As Integer
 
+        ''' <summary>
+        ''' 获取当前基因组中所有不重复的特征标签名称。
+        ''' </summary>
+        ''' <returns>特征标签字符串数组。</returns>
         Public ReadOnly Property AllFeatureKeys As String()
             <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
@@ -115,6 +151,12 @@ Namespace ContextModel
             End Get
         End Property
 
+        ''' <summary>
+        ''' 使用基因组特征集合初始化 <see cref="GenomeContext(Of T)"/> 类的新实例。
+        ''' 此构造函数会执行数据预处理：按特征名分组、按链方向和物理坐标排序、计算基因组大小。
+        ''' </summary>
+        ''' <param name="genome">基因组特征的集合。</param>
+        ''' <param name="name">上下文名称，默认为 "unnamed"。</param>
         Sub New(genome As IEnumerable(Of T), Optional name$ = "unnamed")
             Dim source = genome.ToArray()
 
@@ -168,6 +210,44 @@ Namespace ContextModel
         End Function
 
         ''' <summary>
+        ''' 计算全基因组的平均基因长度
+        ''' </summary>
+        Public Function AverageGeneLength() As Double
+            If sequence.Length = 0 Then Return 0
+            Return sequence.Average(Function(g) g.Length)
+        End Function
+
+        ''' <summary>
+        ''' 计算全基因组的平均基因间距
+        ''' </summary>
+        Public Function AverageIntergenicDistance() As Double
+            If sequence.Length < 2 Then Return 0
+            Dim distances As New List(Of Integer)
+
+            For i As Integer = 0 To sequence.Length - 2
+                Dim curr = sequence(i)
+                Dim nxt = sequence(i + 1)
+                Dim dist = GetPhysicalDistance(curr.Location, nxt.Location)
+                distances.Add(dist)
+            Next
+
+            Return distances.Average()
+        End Function
+
+        ''' <summary>
+        ''' 获取指定滑动窗口内的基因密度 (基因数量 / 窗口大小)
+        ''' </summary>
+        ''' <param name="start">窗口起始</param>
+        ''' <param name="end">窗口结束</param>
+        ''' <returns></returns>
+        Public Function GeneDensity(start%, end%) As Double
+            Dim count = SelectByRange(start, [end]).Count()
+            Dim windowSize = [end] - start
+            If windowSize <= 0 Then Return 0
+            Return count / windowSize
+        End Function
+
+        ''' <summary>
         ''' 查找距离指定坐标位点一定半径范围内的所有feature
         ''' </summary>
         ''' <param name="position">基因组上的具体坐标点</param>
@@ -179,6 +259,150 @@ Namespace ContextModel
             Dim left = Math.Max(1, position - radius)
             Dim right = position + radius
             Return SelectByRange(left, right, strand)
+        End Function
+
+        ''' <summary>
+        ''' 获取指定基因在基因组物理位置上的左侧和右侧紧邻基因
+        ''' </summary>
+        ''' <param name="gene">目标基因</param>
+        ''' <param name="distanceLimit">最大允许的物理间距，超过此间距则认为不相邻，默认无限制</param>
+        ''' <returns>包含左侧邻居和右侧邻居的元组</returns>
+        Public Function GetPhysicalNeighbours(gene As T, Optional distanceLimit As Integer = Integer.MaxValue) As (LeftNeighbour As T, RightNeighbour As T)
+            ' 使用二分查找在 sequence 中快速定位当前基因的索引
+            Dim idx = Array.BinarySearch(sequence, gene, Comparer(Of T).Create(Function(a, b) a.Location.left.CompareTo(b.Location.left)))
+
+            If idx < 0 Then Return (Nothing, Nothing)
+
+            Dim leftNeighbour As T = Nothing
+            Dim rightNeighbour As T = Nothing
+
+            ' 寻找左侧邻居
+            If idx > 0 Then
+                Dim leftGene = sequence(idx - 1)
+                ' 检查间距是否在限制内
+                If gene.Location.left - leftGene.Location.right <= distanceLimit Then
+                    leftNeighbour = leftGene
+                End If
+            End If
+
+            ' 寻找右侧邻居
+            If idx < sequence.Length - 1 Then
+                Dim rightGene = sequence(idx + 1)
+                If rightGene.Location.left - gene.Location.right <= distanceLimit Then
+                    rightNeighbour = rightGene
+                End If
+            End If
+
+            Return (leftNeighbour, rightNeighbour)
+        End Function
+
+        ''' <summary>
+        ''' 获取指定基因在转录方向上的上游和下游紧邻基因
+        ''' </summary>
+        Public Function GetDirectionalNeighbours(gene As T, Optional distanceLimit As Integer = Integer.MaxValue) As (Upstream As T, Downstream As T)
+            Dim physical = GetPhysicalNeighbours(gene, distanceLimit)
+
+            If gene.Location.Strand = Strands.Forward Then
+                ' 正链：左侧是上游，右侧是下游
+                Return (physical.LeftNeighbour, physical.RightNeighbour)
+            ElseIf gene.Location.Strand = Strands.Reverse Then
+                ' 负链：右侧是上游，左侧是下游
+                Return (physical.RightNeighbour, physical.LeftNeighbour)
+            Else
+                ' 未知链：退化为物理左右
+                Return (physical.LeftNeighbour, physical.RightNeighbour)
+            End If
+        End Function
+
+        ''' <summary>
+        ''' 查找与指定位点存在重叠的所有基因
+        ''' </summary>
+        ''' <param name="loci">目标位点</param>
+        ''' <param name="overlapStrand">是否要求必须在同一条链上重叠</param>
+        ''' <returns></returns>
+        Public Iterator Function FindOverlaps(loci As NucleotideLocation, Optional overlapStrand As Boolean = False) As IEnumerable(Of T)
+            ' 因为 sequence 是按 Left 排序的，所以可以快速跳过不可能重叠的基因
+            For Each gene As T In sequence
+                ' 如果当前基因的左端点已经超过了目标位点的右端点，后续绝不可能重叠
+                If gene.Location.left > loci.right Then Exit For
+
+                ' 判断物理重叠
+                If gene.Location.right >= loci.left AndAlso gene.Location.left <= loci.right Then
+                    ' 判断是否需要链特异性
+                    If Not overlapStrand OrElse gene.Location.Strand = loci.Strand Then
+                        Yield gene
+                    End If
+                End If
+            Next
+        End Function
+
+        ''' <summary>
+        ''' 判断两个特征区间是否存在物理重叠
+        ''' </summary>
+        Public Function IsOverlapping(feature1 As String, feature2 As String) As Boolean
+            Dim f1 = GetByFeature(feature1)
+            Dim f2 = GetByFeature(feature2)
+
+            For Each g1 In f1
+                For Each g2 In f2
+                    If g1.Location.right >= g2.Location.left AndAlso g1.Location.left <= g2.Location.right Then
+                        Return True
+                    End If
+                Next
+            Next
+            Return False
+        End Function
+
+        ''' <summary>
+        ''' 计算两个位点的物理距离。如果重叠，返回0
+        ''' </summary>
+        Public Function GetPhysicalDistance(loci1 As NucleotideLocation, loci2 As NucleotideLocation) As Integer
+            ' 如果 loci1 在 loci2 左侧
+            If loci1.right <= loci2.left Then
+                Return loci2.left - loci1.right
+                ' 如果 loci1 在 loci2 右侧
+            ElseIf loci1.left >= loci2.right Then
+                Return loci1.left - loci2.right
+                ' 如果重叠
+            Else
+                Return 0
+            End If
+        End Function
+
+        ''' <summary>
+        ''' 计算两个基因特征之间的最短物理距离
+        ''' </summary>
+        Public Function GetFeatureDistance(feature1 As String, feature2 As String) As Integer
+            Dim f1 = GetByFeature(feature1)
+            Dim f2 = GetByFeature(feature2)
+
+            If f1.Length = 0 OrElse f2.Length = 0 Then Return Integer.MaxValue
+
+            ' 寻找两两组合中的最小距离
+            Return f1.SelectMany(Function(g1) f2.Select(Function(g2) GetPhysicalDistance(g1.Location, g2.Location))).Min()
+        End Function
+
+        ''' <summary>
+        ''' 根据基因产物的注释关键词模糊搜索基因
+        ''' </summary>
+        ''' <param name="keyword">搜索关键词</param>
+        ''' <param name="ignoreCase">是否忽略大小写，默认忽略</param>
+        ''' <returns></returns>
+        Public Iterator Function SearchByProduct(keyword As String, Optional ignoreCase As Boolean = True) As IEnumerable(Of T)
+            Dim comparison = If(ignoreCase, StringComparison.OrdinalIgnoreCase, StringComparison.Ordinal)
+
+            For Each gene As T In sequence
+                If Not gene.Product.StringEmpty AndAlso gene.Product.Contains(keyword, comparison) Then
+                    Yield gene
+                End If
+            Next
+        End Function
+
+        ''' <summary>
+        ''' 获取所有具有相同功能注释的基因集合
+        ''' </summary>
+        Public Function GetGenesByProduct(product As String, Optional ignoreCase As Boolean = True) As T()
+            Return SearchByProduct(product, ignoreCase).ToArray()
         End Function
 
         ''' <summary>
