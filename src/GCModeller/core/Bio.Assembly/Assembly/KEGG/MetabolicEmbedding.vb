cf4ccf82a2ed8890fd5a0892b18a025b5c8ff5e8
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-23 02:44:01 +0800

    how to make embedding

---
+++ src/GCModeller/core/Bio.Assembly/Assembly/KEGG/MetabolicEmbedding.vb
@@ -0,0 +1,20 @@
+﻿Imports SMRUCC.genomics.Assembly.KEGG.DBGET.BriteHEntry
+
+Namespace Assembly.KEGG
+
+    Public Class MetabolicEmbedding
+
+        ReadOnly maps As BriteTerm()
+
+        Sub New()
+            maps = BriteHText.Load_ko00001.Deflate("\d+").ToArray
+        End Sub
+
+        Public Function MakeVector(ec As IEnumerable(Of String)) As Dictionary(Of String, Double)
+            Dim vec As New Dictionary(Of String, Double)
+
+            Return vec
+        End Function
+
+    End Class
+End Namespace
