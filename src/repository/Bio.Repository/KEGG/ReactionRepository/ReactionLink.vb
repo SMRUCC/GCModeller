15894dea52daa79812f61effbc2e0450a4735b95
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-23 15:30:33 +0800

    fix of the namespace reference error

--- src/repository/Bio.Repository/KEGG/ReactionRepository/ReactionLink.vb
+++ src/repository/Bio.Repository/KEGG/ReactionRepository/ReactionLink.vb
@@ -1,55 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::771f06acd1126ffb469180eac87aac48, Bio.Repository\KEGG\ReactionRepository\ReactionLink.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 149
-    '    Code Lines: 117 (78.52%)
-    ' Comment Lines: 15 (10.07%)
-    '    - Xml Docs: 93.33%
-    ' 
-    '   Blank Lines: 17 (11.41%)
-    '     File Size: 8.52 KB
+' Summaries:
 
 
-    '     Class ReactionLink
-    ' 
-    '         Function: FromReactions, (+2 Overloads) FromRepository, PopulateAnyLinks, PopulateConversionLinks, populateLinks
-    '                   tuples
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 149
+'    Code Lines: 117 (78.52%)
+' Comment Lines: 15 (10.07%)
+'    - Xml Docs: 93.33%
+' 
+'   Blank Lines: 17 (11.41%)
+'     File Size: 8.52 KB
+
+
+'     Class ReactionLink
+' 
+'         Function: FromReactions, (+2 Overloads) FromRepository, PopulateAnyLinks, PopulateConversionLinks, populateLinks
+'                   tuples
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -59,6 +59,7 @@ Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language.UnixBash
 Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.Assembly.KEGG
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
 Imports SMRUCC.genomics.ComponentModel.EquaionModel.DefaultTypes
 
