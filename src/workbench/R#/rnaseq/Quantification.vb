729b4bf3551e25e6dd800cf76a917e678edbd60d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-24 09:51:15 +0800

    make gene expression data

--- src/workbench/R#/rnaseq/Quantification.vb
+++ src/workbench/R#/rnaseq/Quantification.vb
@@ -1,10 +1,15 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.Framework
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Analysis.HTS
 Imports SMRUCC.genomics.SequenceModel
 Imports SMRUCC.genomics.SequenceModel.GeneQuantification
 Imports SMRUCC.genomics.SequenceModel.SAM.featureCount
+Imports SMRUCC.Rsharp
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
+Imports Matrix = SMRUCC.genomics.Analysis.HTS.DataFrame.Matrix
 
 <Package("gene_quantification")>
 Module Quantification
@@ -25,5 +30,39 @@ Module Quantification
     <RApiReturn(GetType(GeneData))>
     Public Function read_genedata(file As String) As GeneData()
         Return file.LoadCsv(Of GeneData)(mute:=True)
+    End Function
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="counts">A collection of the gene <see cref="featureCounts"/> data.</param>
+    ''' <param name="env">The R environment.</param>
+    ''' <returns>A collection of gene expression data in TPM format.</returns>
+    <ExportAPI("convert_to_tpm")>
+    <RApiReturn(GetType(GeneSampleSet))>
+    Public Function convert_to_tpm(<RRawVectorArgument> counts As Object, Optional env As Environment = Nothing) As Object
+        Dim pull As pipeline = pipeline.TryCreatePipeline(Of featureCounts)(counts, env)
+
+        If pull.isError Then
+            Return pull.getError
+        End If
+
+        Return pull.populates(Of featureCounts)(env).MakeGeneExpressions.ToArray
+    End Function
+
+    <ExportAPI("expression_data")>
+    <RApiReturn(GetType(Matrix))>
+    Public Function expression_data(<RRawVectorArgument> sampledata As Object, Optional env As Environment = Nothing) As Object
+        Dim pull As pipeline = pipeline.TryCreatePipeline(Of GeneSampleSet)(sampledata, env)
+
+        If pull.isError Then
+            Return pull.getError
+        End If
+
+        Dim data As GeneSampleSet() = pull.populates(Of GeneSampleSet)(env).ToArray
+        Dim tpm As Matrix = data.TPMExpression
+        Dim fpkm As Matrix = data.FPKMExpression
+
+        Return New list(slot("tpm") = tpm, slot("fpkm") = fpkm)
     End Function
 End Module
