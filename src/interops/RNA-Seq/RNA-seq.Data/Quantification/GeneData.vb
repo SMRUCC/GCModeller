b1572440a822cdac581edc5d4aa022664c459691
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-24 08:36:10 +0800

    source file renames

---
+++ src/interops/RNA-Seq/RNA-seq.Data/Quantification/GeneData.vb
@@ -0,0 +1,20 @@
+﻿Imports SMRUCC.genomics.ComponentModel
+
+Namespace GeneQuantification
+
+    ''' <summary>
+    ''' gene abundance result
+    ''' </summary>
+    Public Class GeneData : Implements IExpressionValue
+
+        Public Property GeneID As String Implements IExpressionValue.Identity
+        Public Property Length As Double
+        Public Property RawCount As Double
+        Public Property RPK As Double
+        Public Property TPM As Double Implements IExpressionValue.ExpressionValue
+        Public Property FPKM As Double
+
+    End Class
+End Namespace
+
+
--- src/interops/RNA-Seq/RNA-seq.Data/SAM/IndexStats.vb
+++ src/interops/RNA-Seq/RNA-seq.Data/SAM/IndexStats.vb
@@ -65,20 +65,6 @@ Imports SMRUCC.genomics.ComponentModel
 Namespace SAM
 
     ''' <summary>
-    ''' gene abundance result
-    ''' </summary>
-    Public Class GeneData : Implements IExpressionValue
-
-        Public Property GeneID As String Implements IExpressionValue.Identity
-        Public Property Length As Double
-        Public Property RawCount As Double
-        Public Property RPK As Double
-        Public Property TPM As Double Implements IExpressionValue.ExpressionValue
-        Public Property FPKM As Double
-
-    End Class
-
-    ''' <summary>
     ''' A row of the samtool indexstats output
     ''' </summary>
     Public Class IndexStats
