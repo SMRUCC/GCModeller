070c25e996ea106205f80168fdab3cf24188dd91
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-24 12:27:18 +0800

    run package msbuild

--- src/workbench/pkg/test/rna-seqs/analysis/export_eggNOG.R
+++ src/workbench/pkg/test/rna-seqs/analysis/export_eggNOG.R
@@ -5,3 +5,5 @@ imports "ptf" from "annotationKit";
 let table = read_eggNOG("F:\datapool\20260312\20260424\emapper.annotations");
 
 print(as.data.frame(table));
+
+write.csv(as.data.frame(table), file = "F:\datapool\20260312\20260424/eggNOG.csv");
