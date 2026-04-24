209e571226b3c64c4a886cdf2d7514d7c7b2abca
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-24 08:22:48 +0800

    read_featureCounts

---
+++ src/workbench/R#/rnaseq/Quantification.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.SequenceModel
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("gene_quantification")>
+Module Quantification
+
+    <ExportAPI("read_featureCounts")>
+    <RApiReturn(GetType(featureCounts))>
+    Public Function read_featureCounts(file As String) As Object
+        Return featureCounts.ReadTable(file).ToArray
+    End Function
+
+End Module
