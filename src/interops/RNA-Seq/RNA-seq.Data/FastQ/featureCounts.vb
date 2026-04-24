51ed2dac09229a26aa679b5625c57c32d1865d96
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-24 08:09:57 +0800

    add feature counts table

---
+++ src/interops/RNA-Seq/RNA-seq.Data/FastQ/featureCounts.vb
@@ -0,0 +1,11 @@
+﻿Public Class featureCounts
+
+    Public Property Geneid As String
+    Public Property Chr As String
+    Public Property Start As UInteger
+    Public Property [End] As UInteger
+    Public Property Strand As String
+    Public Property Length As Integer
+    Public Property SampleCounts As Dictionary(Of String, Integer)
+
+End Class
