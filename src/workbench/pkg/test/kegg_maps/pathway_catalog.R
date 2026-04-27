d01d00634f368b98dc72bb0245e8329c7783cc05
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-27 15:38:52 +0800

    export data

---
+++ src/workbench/pkg/test/kegg_maps/pathway_catalog.R
@@ -0,0 +1,10 @@
+require(GCModeller);
+
+imports "brite" from "kegg_kit";
+
+let brite = brite::parse("ko00001");
+let df = brite.as.table(brite);
+
+print(df, max.print = 6);
+
+write.csv(df, file = relative_work( "kegg_pathway_brite.csv"));
