f7e6fc37c64b14892058c8bb6871ca182701c684
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-29 20:24:14 +0800

    removes obsolete source files

--- src/GCModeller/analysis/SequenceToolkit/SequenceAlignment/CDHit.vb
+++ src/GCModeller/analysis/SequenceToolkit/SequenceAlignment/CDHit.vb
@@ -195,26 +195,3 @@ Public Class CDHit
 
 End Class
 
-Public Class SimilarHit
-
-    Public Property SeqID As String
-    Public Property Similar As Dictionary(Of String, Double)
-
-    Public ReadOnly Property IsUniqued As Boolean
-        Get
-            Return Similar.IsNullOrEmpty
-        End Get
-    End Property
-
-    Public ReadOnly Property Size As Integer
-        Get
-            Return Similar.TryCount
-        End Get
-    End Property
-
-    Public Overrides Function ToString() As String
-        Return SeqID
-    End Function
-
-End Class
-
