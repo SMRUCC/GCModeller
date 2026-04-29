afafdf9b5e798239d926a40e4877a226207ca219
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-29 16:52:51 +0800

    extract of the data

--- src/GCModeller/analysis/OperonMapper/OperonPrediction/GenomeInfo.vb
+++ src/GCModeller/analysis/OperonMapper/OperonPrediction/GenomeInfo.vb
@@ -1,9 +1,15 @@
-﻿Namespace ContextModel
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank.GBFF.Keywords.FEATURES
+Imports SMRUCC.genomics.ComponentModel.Loci
+Imports SMRUCC.genomics.Metagenomics
+
+Namespace ContextModel
 
     ''' <summary>
     ''' 参考基因组信息结构，用于计算邻域保守性和系统发育距离
     ''' </summary>
-    Public Structure GenomeInfo
+    Public Class GenomeInfo
 
         ''' <summary>
         ''' 参考基因组的唯一标识符
@@ -27,5 +33,34 @@
         ''' </remarks>
         Public Property GenePositions As Dictionary(Of String, Integer)
 
-    End Structure
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+
+        Public Shared Function FromGenBank(gb As GBFF.File) As GenomeInfo
+            Dim genes As Feature() = gb.EnumerateGeneFeatures.ToArray
+            Dim tax As Taxonomy = gb.Source.GetTaxonomy
+            Dim geneLocs As New Dictionary(Of String, Integer)
+            Dim i As Integer = 1
+            Dim accId As String = gb.Accession.AccessionId
+
+            Static gene_key As String = FeatureQualifiers.locus_tag.ToString
+
+            For Each gene As Feature In genes
+                Dim id As String = If(gene.Query(FeatureQualifiers.locus_tag), $"{accId}{i}")
+                Dim loc As NucleotideLocation = gene.Location.ContiguousRegion
+                Dim left As Integer = loc.left
+
+                geneLocs(id) = left
+            Next
+
+            Return New GenomeInfo With {
+                .GeneCount = genes.Length,
+                .GenePositions = geneLocs,
+                .GenomeID = gb.Source.SpeciesName,
+                .Phylum = tax.phylum
+            }
+        End Function
+
+    End Class
 End Namespace
