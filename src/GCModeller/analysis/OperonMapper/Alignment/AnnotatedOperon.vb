df13ec8d5c24fd87bdea8d4d28da6073b6373a9b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-29 14:09:00 +0800

    OperonType

--- src/GCModeller/analysis/OperonMapper/Alignment/AnnotatedOperon.vb
+++ src/GCModeller/analysis/OperonMapper/Alignment/AnnotatedOperon.vb
@@ -65,14 +65,6 @@
 
 Imports Microsoft.VisualBasic.Serialization.JSON
 
-''' <summary>
-''' Operon的类型枚举
-''' </summary>
-Public Enum OperonType
-    Conserved  ' 保守的
-    Insertion  ' 插入突变
-    Deletion   ' 缺失突变
-End Enum
 
 ' 用于表示最终注释结果的Operon结构
 Public Class AnnotatedOperon
---
+++ src/GCModeller/analysis/OperonMapper/Alignment/OperonType.vb
@@ -0,0 +1,9 @@
+﻿
+''' <summary>
+''' Operon的类型枚举
+''' </summary>
+Public Enum OperonType
+    Conserved  ' 保守的
+    Insertion  ' 插入突变
+    Deletion   ' 缺失突变
+End Enum
