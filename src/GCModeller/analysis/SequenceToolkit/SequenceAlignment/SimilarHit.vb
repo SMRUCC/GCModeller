b52cc07e056155c9394a5176aeb7be7579f0f6e4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-29 20:22:38 +0800

    split soirce file into multiple parts

---
+++ src/GCModeller/analysis/SequenceToolkit/SequenceAlignment/SimilarHit.vb
@@ -0,0 +1,23 @@
+﻿
+Public Class SimilarHit
+
+    Public Property SeqID As String
+    Public Property Similar As Dictionary(Of String, Double)
+
+    Public ReadOnly Property IsUniqued As Boolean
+        Get
+            Return Similar.IsNullOrEmpty
+        End Get
+    End Property
+
+    Public ReadOnly Property Size As Integer
+        Get
+            Return Similar.TryCount
+        End Get
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return SeqID
+    End Function
+
+End Class
