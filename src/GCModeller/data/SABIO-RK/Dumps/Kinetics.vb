0aa0273864b3bbbaedbc6cf6637ce13b670ee4f4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-25 18:30:26 +0800

    try to make fixed of the data constructor error

--- src/GCModeller/data/SABIO-RK/Dumps/Kinetics.vb
+++ src/GCModeller/data/SABIO-RK/Dumps/Kinetics.vb
@@ -91,16 +91,7 @@ Imports SMRUCC.genomics.Data.SABIORK.SBML
 
 Namespace TabularDump
 
-    ''' <summary>
-    ''' Vmax 是指 最大反应速度。当 底物浓度 足够大时，体系中酶的活性中心达到饱和状态，其反应速度达到最大。由此可见，最大反应速度 Vmax 随 酶浓度 的变化而变化。
-    ''' kcat 指反应常数(Catalytic Constant)，Kcat 可以由公式计算得到： kcat = Vmax/[E]
-    '''    - [E] 指 酶浓度，由此可以说， kcat 表示了每单位时间内（秒）每摩尔的酶（或者说每摩尔的活性中心）能够把多少摩尔的底物转化成产物。
-    '''
-    ''' Km 俗称 米氏常数，以浓度做单位，米氏常数定义为 反应速度 达到 最大反应速度一半时 的底物浓度。 Km 可以反映出酶与底物的亲和力，Km越低，亲和力越大。
-    ''' Kcat/Km 称为 催化效率，常常以此来比较 不同的酶而同一底物， 或者 不同底物而同一种酶。
-    ''' </summary>
-    ''' <remarks></remarks>
-    Public Class EnzymeCatalystKineticLaw : Inherits SabiorkEntity
+    Public Class KineticLawData : Inherits SabiorkEntity
 
         ''' <summary>
         ''' list of compartment of current enzyme and kinetics
@@ -119,10 +110,24 @@ Namespace TabularDump
         Public Property PubMed As String()
         Public Property parameters As Dictionary(Of String, String)
         Public Property lambda As String
-        Public Property substrates As Dictionary(Of String, NamedCollection(Of String))
-        Public Property products As Dictionary(Of String, NamedCollection(Of String))
         Public Property uniprot_id As String()
         Public Property rhea_id As String
+
+    End Class
+
+    ''' <summary>
+    ''' Vmax 是指 最大反应速度。当 底物浓度 足够大时，体系中酶的活性中心达到饱和状态，其反应速度达到最大。由此可见，最大反应速度 Vmax 随 酶浓度 的变化而变化。
+    ''' kcat 指反应常数(Catalytic Constant)，Kcat 可以由公式计算得到： kcat = Vmax/[E]
+    '''    - [E] 指 酶浓度，由此可以说， kcat 表示了每单位时间内（秒）每摩尔的酶（或者说每摩尔的活性中心）能够把多少摩尔的底物转化成产物。
+    '''
+    ''' Km 俗称 米氏常数，以浓度做单位，米氏常数定义为 反应速度 达到 最大反应速度一半时 的底物浓度。 Km 可以反映出酶与底物的亲和力，Km越低，亲和力越大。
+    ''' Kcat/Km 称为 催化效率，常常以此来比较 不同的酶而同一底物， 或者 不同底物而同一种酶。
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class EnzymeCatalystKineticLaw : Inherits KineticLawData
+
+        Public Property substrates As Dictionary(Of String, NamedCollection(Of String))
+        Public Property products As Dictionary(Of String, NamedCollection(Of String))
 
         Public Overrides Function ToString() As String
             Return String.Format("({0}) {1}", enzyme.Values.GetJson, reaction)
