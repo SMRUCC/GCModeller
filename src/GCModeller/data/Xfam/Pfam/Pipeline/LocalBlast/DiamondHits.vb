f100aa2565dedba5e787cd72bbe196ae4c17bb50
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-27 22:10:59 +0800

    parse the diamond blastp hits

---
+++ src/GCModeller/data/Xfam/Pfam/Pipeline/LocalBlast/DiamondHits.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.Interops.NCBI.Extensions
+
+Namespace Pipeline.LocalBlast
+
+    Public Module DiamondHits
+
+        <Extension>
+        Public Iterator Function Parse(blastp As IEnumerable(Of DiamondAnnotation)) As IEnumerable(Of PfamHit)
+            For Each hit As DiamondAnnotation In blastp
+                Yield New PfamHit With {
+                    .description = hit.QseqId,
+                    .HitName = hit.QseqId,
+                    .QueryName = hit.SseqId,
+                    .start = hit.SStart,
+                    .ends = hit.SEnd,
+                    .evalue = hit.EValue,
+                    .identities = hit.Pident,
+                    .score = hit.BitScore
+                }
+            Next
+        End Function
+    End Module
+End Namespace
