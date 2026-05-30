ba9e863303fcbbf9caf7bc64e31ea89b7d5f3f35
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-31 01:14:41 +0800

    get samples names

--- src/workbench/modules/ExperimentDesigner/SampleIndex.vb
+++ src/workbench/modules/ExperimentDesigner/SampleIndex.vb
@@ -1,59 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::ead9f9bf2e8b4d35a5e3361a880d6b17, modules\ExperimentDesigner\SampleIndex.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 51
-    '    Code Lines: 41 (80.39%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 10 (19.61%)
-    '     File Size: 1.55 KB
+' Summaries:
 
 
-    ' Class SampleIndex
-    ' 
-    '     Properties: group_size, sampleInfo, size
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: GetSampleClass, GetSampleColor, GetSampleName
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 51
+'    Code Lines: 41 (80.39%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 10 (19.61%)
+'     File Size: 1.55 KB
+
+
+' Class SampleIndex
+' 
+'     Properties: group_size, sampleInfo, size
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: GetSampleClass, GetSampleColor, GetSampleName
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Linq
 
 Public Class SampleIndex
@@ -86,6 +87,7 @@ Public Class SampleIndex
         m_samplegroups = New DataAnalysis(m_sampleinfo)
     End Sub
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetSampleName(sample_id As IEnumerable(Of String)) As String()
         Return sample_id _
             .Select(Function(id) m_sampleindex(id).sample_name) _
