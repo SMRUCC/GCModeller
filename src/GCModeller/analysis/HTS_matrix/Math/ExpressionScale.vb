c2faf4023eab82880149880677cd285e00912b68
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-31 22:51:34 +0800

    relative scale

---
+++ src/GCModeller/analysis/HTS_matrix/Math/ExpressionScale.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Math.Statistics.Linq
+Imports std_vec = Microsoft.VisualBasic.Math.LinearAlgebra.Vector
+
+Public Module ExpressionScale
+
+    <Extension>
+    Public Function RelativeScale(gene As DataFrameRow, Optional median As Boolean = False) As DataFrameRow
+        Dim factor As Double = If(median, gene.experiments.Median, gene.experiments.Max)
+
+        If median AndAlso factor = 0.0 Then
+            Dim minmax As DoubleRange = gene.experiments
+
+            ' try to avoid divid zero
+            If minmax.Length = 0 Then
+                ' all zero
+                Return New DataFrameRow With {
+                    .geneID = gene.geneID,
+                    .experiments = gene.experiments.ToArray
+                }
+            Else
+                factor = minmax.Max / 2
+            End If
+        End If
+
+        Return New DataFrameRow With {
+            .geneID = gene.geneID,
+            .experiments = New std_vec(gene.experiments) / factor
+        }
+    End Function
+End Module
--- src/workbench/R#/phenotype_kit/geneExpression.vb
+++ src/workbench/R#/phenotype_kit/geneExpression.vb
@@ -1292,31 +1292,10 @@ Module geneExpression
             .sampleID = matrix.sampleID,
             .expression = matrix.expression _
                 .Select(Function(gene)
-                            Dim factor As Double = If(median,
-                                gene.experiments.Median,
-                                gene.experiments.Max)
-
-                            If median AndAlso factor = 0.0 Then
-                                Dim minmax As DoubleRange = gene.experiments
-
-                                ' try to avoid divid zero
-                                If minmax.Length = 0 Then
-                                    ' all zero
-                                    Return New DataFrameRow With {
-                                        .geneID = gene.geneID,
-                                        .experiments = gene.experiments.ToArray
-                                    }
-                                Else
-                                    factor = minmax.Max / 2
-                                End If
-                            End If
-
-                            Return New DataFrameRow With {
-                                .geneID = gene.geneID,
-                                .experiments = New std_vec(gene.experiments) / factor
-                            }
+                            Return gene.RelativeScale(median)
                         End Function) _
-                .ToArray
+                .ToArray,
+            .tag = $"relative_scaale({matrix.tag})"
         }
     End Function
 
