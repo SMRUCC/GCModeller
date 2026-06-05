6aefc175ae5ab19fb07c4e4815bd3cb07e2e1440
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-05 15:41:56 +0800

    Update sync_multiplegit.cmd

--- .github/sync_multiplegit.cmd
+++ .github/sync_multiplegit.cmd
@@ -2,7 +2,7 @@
 
 REM git remote remove local 
 REM git remote add local http://git.biodeep.cn/xieguigang/GCModeller.git
-REM git remote add local http://192.168.0.232:8848/xieguigang/GCModeller.git
+REM git remote add local http://192.168.3.48:8418/xieguigang/GCModeller.git
 
 git pull gitlink HEAD
 git pull gitee HEAD
