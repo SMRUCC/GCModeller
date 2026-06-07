d2c931cc4ca66ed180f96b4606e56db169a627b0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-07 17:45:32 +0800

    make improvements of the metabolic network math module

--- src/GCModeller/sub-system/CellPhenotype/MetabolicNetwork.vb
+++ src/GCModeller/sub-system/CellPhenotype/MetabolicNetwork.vb
@@ -3,10 +3,49 @@
 ''' </summary>
 Public Class MetabolicNetwork
 
-    Public Property NodeCount As Integer
     ''' <summary>
     ''' 邻接表：Adj(i) 存储从节点 i 出发的所有目标节点及权重
     ''' </summary>
-    Public Property Adjacency As List(Of List(Of (Target As Integer, Weight As Double)))
+    Public Property Adjacency As Dictionary(Of String, AdjacencyWeight())
+
+    Public ReadOnly Property NodeCount As Integer
+        Get
+            Return Adjacency.Count
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' 将代谢网络转换为行随机化转移矩阵 P
+    ''' </summary>
+    Public Function BuildRowStochasticMatrix() As Double(,)
+        Dim n As Integer = NodeCount
+        Dim P(n - 1, n - 1) As Double
+        Dim ids As String() = Adjacency.Keys.ToArray
+
+        For i As Integer = 0 To n - 1
+            Dim totalOutWeight As Double = 0.0
+            For Each edge As AdjacencyWeight In Adjacency(ids(i))
+                totalOutWeight += edge.Weight
+            Next
+
+            If totalOutWeight > 0 Then
+                For Each edge As AdjacencyWeight In Adjacency(ids(i))
+                    P(i, edge.Target) = edge.Weight / totalOutWeight
+                Next
+            Else
+                ' 无出边：自环（避免死端）
+                P(i, i) = 1.0
+            End If
+        Next
+
+        Return P
+    End Function
+
+End Class
+
+Public Class AdjacencyWeight
+
+    Public Property Target As String
+    Public Property Weight As Double
 
 End Class
--- src/GCModeller/sub-system/CellPhenotype/PPRSolver.vb
+++ src/GCModeller/sub-system/CellPhenotype/PPRSolver.vb
@@ -6,39 +6,13 @@
 Public Module PPRSolver
 
     ''' <summary>
-    ''' 将代谢网络转换为行随机化转移矩阵 P
-    ''' </summary>
-    Friend Function BuildRowStochasticMatrix(net As MetabolicNetwork) As Double(,)
-        Dim n As Integer = net.NodeCount
-        Dim P(n - 1, n - 1) As Double
-
-        For i As Integer = 0 To n - 1
-            Dim totalOutWeight As Double = 0.0
-            For Each edge In net.Adjacency(i)
-                totalOutWeight += edge.Weight
-            Next
-
-            If totalOutWeight > 0 Then
-                For Each edge In net.Adjacency(i)
-                    P(i, edge.Target) = edge.Weight / totalOutWeight
-                Next
-            Else
-                ' 无出边：自环（避免死端）
-                P(i, i) = 1.0
-            End If
-        Next
-
-        Return P
-    End Function
-
-    ''' <summary>
     ''' 计算封闭代谢网络的全局稳态浓度分布（质量守恒）
     ''' </summary>
     ''' <remarks>
     ''' 认为物质在 network 中永不消耗，只是不断循环反应，直到完全混合。封闭系统充分混合后的稳态只取决于网络结构。
     ''' </remarks>
     Public Function ComputeSteadyStateClosed(net As MetabolicNetwork, Optional totalMass As Double = 999, Optional maxItrs As Integer = 10000) As Double()
-        Dim P(,) As Double = BuildRowStochasticMatrix(net) ' 复用你原有的矩阵构建函数
+        Dim P(,) As Double = net.BuildRowStochasticMatrix()
         Dim n As Integer = P.GetLength(0)
         ' 幂迭代法求左特征向量 (平稳分布 pi^T = pi^T * P)
         Dim pi(n - 1) As Double
@@ -91,7 +65,7 @@ Public Module PPRSolver
     ''' 用途：同位素示踪 / 碳流 / 模拟 营养注入点
     ''' </remarks>
     Public Function ComputePPR(net As MetabolicNetwork, seedNode As Integer, Optional alpha As Double = 0.85, Optional tolerance As Double = 0.00000001) As Double()
-        Dim P(,) As Double = BuildRowStochasticMatrix(net)
+        Dim P(,) As Double = net.BuildRowStochasticMatrix
         Dim n As Integer = P.GetLength(0)
         Dim piPrev(n - 1) As Double
         Dim piCurr(n - 1) As Double
@@ -134,7 +108,7 @@ Public Module PPRSolver
     ''' 带 Drain 项的稳态解（简单 Jacobi 迭代）
     ''' </summary>
     Public Function SolveWithDrain(net As MetabolicNetwork, seedNode As Integer, drain() As Double, Optional alpha As Double = 0.85, Optional maxItrs As Integer = 10000) As Double()
-        Dim P(,) As Double = BuildRowStochasticMatrix(net)
+        Dim P(,) As Double = net.BuildRowStochasticMatrix
         Dim n As Integer = P.GetLength(0)
 
         Dim x(n - 1) As Double
