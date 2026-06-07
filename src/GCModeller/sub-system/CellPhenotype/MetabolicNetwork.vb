5412bc9741c22f539daebeada650a069cc3ee625
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-07 18:02:19 +0800

    make improvements of the matrix constructor based on the network model

--- src/GCModeller/sub-system/CellPhenotype/MetabolicNetwork.vb
+++ src/GCModeller/sub-system/CellPhenotype/MetabolicNetwork.vb
@@ -1,4 +1,7 @@
-﻿''' <summary>
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+''' <summary>
 ''' 代谢网络邻接表结构
 ''' </summary>
 Public Class MetabolicNetwork
@@ -7,6 +10,8 @@ Public Class MetabolicNetwork
     ''' 邻接表：Adj(i) 存储从节点 i 出发的所有目标节点及权重
     ''' </summary>
     Public Property Adjacency As Dictionary(Of String, AdjacencyWeight())
+
+    Public Property MetaIDs As String()
 
     Public ReadOnly Property NodeCount As Integer
         Get
@@ -20,7 +25,8 @@ Public Class MetabolicNetwork
     Public Function BuildRowStochasticMatrix() As Double(,)
         Dim n As Integer = NodeCount
         Dim P(n - 1, n - 1) As Double
-        Dim ids As String() = Adjacency.Keys.ToArray
+        Dim ids As String() = MetaIDs
+        Dim ordinal As Index(Of String) = ids.Indexing
 
         For i As Integer = 0 To n - 1
             Dim totalOutWeight As Double = 0.0
@@ -30,7 +36,7 @@ Public Class MetabolicNetwork
 
             If totalOutWeight > 0 Then
                 For Each edge As AdjacencyWeight In Adjacency(ids(i))
-                    P(i, edge.Target) = edge.Weight / totalOutWeight
+                    P(i, ordinal(edge.Target)) = edge.Weight / totalOutWeight
                 Next
             Else
                 ' 无出边：自环（避免死端）
@@ -39,6 +45,33 @@ Public Class MetabolicNetwork
         Next
 
         Return P
+    End Function
+
+    Public Shared Function Create(g As NetworkGraph) As MetabolicNetwork
+        Dim adj As New Dictionary(Of String, AdjacencyWeight())
+        Dim idList As New List(Of String)
+
+        ' G = {U->V}
+        ' current node v is U
+        ' adjacency of U->V
+        For Each v As Node In g.vertex
+            Call idList.Add(v.label)
+            Call adj.Add(v.label,
+                value:=v.adjacencies _
+                    .EnumerateAllEdges _
+                    .Select(Function(edge)
+                                Return New AdjacencyWeight With {
+                                    .Target = edge.V.label,
+                                    .Weight = edge.weight
+                                }
+                            End Function) _
+                    .ToArray)
+        Next
+
+        Return New MetabolicNetwork With {
+            .Adjacency = adj,
+            .MetaIDs = idList.ToArray
+        }
     End Function
 
 End Class
