ae9259b0be80d808801b4726748e9431554cde06
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-19 05:00:19 +0800

    add models

---
+++ src/GCModeller/engine/Cella/Gene.vb
@@ -0,0 +1,6 @@
+﻿Public Class Gene
+
+    Public Property Id As String
+    Public Property Ontology As String()
+
+End Class
---
+++ src/GCModeller/engine/Cella/Metabolite.vb
@@ -0,0 +1,11 @@
+﻿Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Core
+
+Public Class Metabolite : Inherits Factor
+
+    ''' <summary>
+    ''' metabolite chemical structure class
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Ontology As String
+
+End Class
