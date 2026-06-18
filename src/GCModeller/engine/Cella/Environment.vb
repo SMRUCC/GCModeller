aa63ff58a7aa3e128cd3fb281979e06a892467ce
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-19 04:58:11 +0800

    run ticks

--- src/GCModeller/engine/Cella/Environment.vb
+++ src/GCModeller/engine/Cella/Environment.vb
@@ -1,5 +1,30 @@
-﻿Public Class Environment
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
+
+Public Class Environment
 
     Public Property Space As Spot()()()
+
+    Dim itr As i32
+
+    Public Sub Tick()
+        Call Tick(++itr)
+    End Sub
+
+    Private Sub Tick(iteration As Integer)
+        For Each row In Space
+            For Each col In row
+                For Each spot In col
+                    Call spot.Tick(iteration)
+                Next
+            Next
+        Next
+    End Sub
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function GetAllCells() As IEnumerable(Of VirtualCella)
+        Return Space.IteratesALL.IteratesALL.SelectMany(Function(s) s.cells)
+    End Function
 
 End Class
--- src/GCModeller/engine/Cella/Spot.vb
+++ src/GCModeller/engine/Cella/Spot.vb
@@ -6,4 +6,12 @@ Public Class Spot
     Public Property cells As New List(Of VirtualCella)
     Public Property index As SpatialIndex3D
 
+    Public Sub Tick(iteration As Integer)
+        Call external.RunStep()
+
+        For Each cell As VirtualCella In cells
+            Call cell.RunStep(iteration)
+        Next
+    End Sub
+
 End Class
