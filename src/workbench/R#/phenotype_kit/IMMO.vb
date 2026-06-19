8827a9f05356454e6acd4c09bae9aecd4bb5c878
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-20 00:39:58 +0800

    export IMMO package api

---
+++ src/workbench/R#/phenotype_kit/IMMO.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Analysis.Microarray.IMMO
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+''' <summary>
+''' IMMO (Integration Model for Incomplete Multi-Omics)
+''' </summary>
+<Package("IMMO")>
+<RTypeExport("immo_pars", GetType(IMMOConfig))>
+Module IMMOTool
+
+End Module
