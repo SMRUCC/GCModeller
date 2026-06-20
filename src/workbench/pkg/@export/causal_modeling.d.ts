e58d67464433f17296e7e5d3928bc931f7581900
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-20 13:50:30 +0800

    run package msbuild

---
+++ src/workbench/pkg/@export/causal_modeling.d.ts
@@ -0,0 +1,39 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "causal_modeling" from "phenotype_kit";
+//
+// ref=phenotype_kit.causal_modeling@phenotype_kit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+*/
+declare namespace causal_modeling {
+   /**
+     * @param env default value Is ``null``.
+   */
+   function as_causalmodel(data: any, path: any, env?: object): object;
+   /**
+     * @param as_dataframe default value Is ``true``.
+   */
+   function effect_decomposition(sem_result: object, as_dataframe?: boolean): object|object;
+   /**
+     * @param as_dataframe default value Is ``true``.
+   */
+   function indirect_effect(sem_result: object, boot_result: object, as_dataframe?: boolean): object|object;
+   /**
+     * @param env default value Is ``null``.
+   */
+   function make_path(from: any, to: any, env?: object): object;
+   /**
+     * @param as_dataframe default value Is ``true``.
+   */
+   function path_coefficient(sem_result: object, as_dataframe?: boolean): object|object;
+   /**
+     * @param boot default value Is ``500``.
+     * @param env default value Is ``null``.
+   */
+   function sem(model: object, boot?: object, env?: object): object;
+   /**
+     * @param as_dataframe default value Is ``true``.
+   */
+   function significance_test(sem_result: object, boot_result: object, as_dataframe?: boolean): object|object;
+}
---
+++ src/workbench/pkg/man/phenotype_kit/causal_modeling/causal_modeling.md
@@ -0,0 +1,11 @@
+﻿# causal_modeling
+
+
+
++ [path_coefficient](causal_modeling/path_coefficient.1) 
++ [effect_decomposition](causal_modeling/effect_decomposition.1) 
++ [significance_test](causal_modeling/significance_test.1) 
++ [indirect_effect](causal_modeling/indirect_effect.1) 
++ [as_causalmodel](causal_modeling/as_causalmodel.1) 
++ [sem](causal_modeling/sem.1) 
++ [make_path](causal_modeling/make_path.1) 
