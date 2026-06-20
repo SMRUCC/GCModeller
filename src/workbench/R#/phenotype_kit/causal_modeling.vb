15a8c115b06de6ea954262527481f46c7c597d5b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-20 13:46:06 +0800

    make exports of the result table

--- src/workbench/R#/phenotype_kit/causal_modeling.vb
+++ src/workbench/R#/phenotype_kit/causal_modeling.vb
@@ -17,8 +17,90 @@ Module causal_modeling
 
     End Sub
 
-    Private Function castGraph()
+    <ExportAPI("path_coefficient")>
+    <RApiReturn(GetType(dataframe), GetType(PathCoefficient))>
+    Public Function path_coefficient(sem_result As SEMResult, Optional as_dataframe As Boolean = True) As Object
+        Dim pc As PathCoefficient() = PathCoefficient.FromResult(sem_result).ToArray
 
+        If as_dataframe Then
+            Dim df As New dataframe With {.columns = New Dictionary(Of String, Array)}
+
+            Call df.add(NameOf(PathCoefficient.fromName), From i As PathCoefficient In pc Select i.fromName)
+            Call df.add(NameOf(PathCoefficient.toName), From i As PathCoefficient In pc Select i.toName)
+            Call df.add(NameOf(PathCoefficient.coef), From i As PathCoefficient In pc Select i.coef)
+            Call df.add(NameOf(PathCoefficient.se), From i As PathCoefficient In pc Select i.se)
+            Call df.add(NameOf(PathCoefficient.t), From i As PathCoefficient In pc Select i.t)
+            Call df.add(NameOf(PathCoefficient.p), From i As PathCoefficient In pc Select i.p)
+            Call df.add(NameOf(PathCoefficient.sig), From i As PathCoefficient In pc Select i.sig)
+
+            Return df
+        Else
+            Return pc
+        End If
+    End Function
+
+    <ExportAPI("effect_decomposition")>
+    <RApiReturn(GetType(dataframe), GetType(EffectDecomposition))>
+    Public Function effect_decomposition(sem_result As SEMResult, Optional as_dataframe As Boolean = True) As Object
+        Dim ed As EffectDecomposition() = EffectDecomposition.FromResult(sem_result).ToArray
+
+        If as_dataframe Then
+            Dim df As New dataframe With {.columns = New Dictionary(Of String, Array)}
+
+            Call df.add(NameOf(EffectDecomposition.fromName), From i As EffectDecomposition In ed Select i.fromName)
+            Call df.add(NameOf(EffectDecomposition.toName), From i As EffectDecomposition In ed Select i.toName)
+            Call df.add(NameOf(EffectDecomposition.direct), From i As EffectDecomposition In ed Select i.direct)
+            Call df.add(NameOf(EffectDecomposition.indirect), From i As EffectDecomposition In ed Select i.indirect)
+            Call df.add(NameOf(EffectDecomposition.total), From i As EffectDecomposition In ed Select i.total)
+
+            Return df
+        Else
+            Return ed
+        End If
+    End Function
+
+    <ExportAPI("significance_test")>
+    <RApiReturn(GetType(dataframe), GetType(BootstrapSignificanceTest))>
+    Public Function significance_test(sem_result As SEMResult, boot_result As BootstrapResult, Optional as_dataframe As Boolean = True) As Object
+        Dim bs As BootstrapSignificanceTest() = BootstrapSignificanceTest.FromResult(sem_result, boot_result).ToArray
+
+        If as_dataframe Then
+            Dim df As New dataframe With {.columns = New Dictionary(Of String, Array)}
+
+            Call df.add(NameOf(BootstrapSignificanceTest.fromName), From i As BootstrapSignificanceTest In bs Select i.fromName)
+            Call df.add(NameOf(BootstrapSignificanceTest.toName), From i As BootstrapSignificanceTest In bs Select i.toName)
+            Call df.add(NameOf(BootstrapSignificanceTest.coef), From i As BootstrapSignificanceTest In bs Select i.coef)
+            Call df.add(NameOf(BootstrapSignificanceTest.bse), From i As BootstrapSignificanceTest In bs Select i.bse)
+            Call df.add(NameOf(BootstrapSignificanceTest.ci_lb), From i As BootstrapSignificanceTest In bs Select i.ci_lb)
+            Call df.add(NameOf(BootstrapSignificanceTest.ci_ub), From i As BootstrapSignificanceTest In bs Select i.ci_ub)
+            Call df.add(NameOf(BootstrapSignificanceTest.sig), From i As BootstrapSignificanceTest In bs Select i.sig)
+
+            Return df
+        Else
+            Return bs
+        End If
+    End Function
+
+    <ExportAPI("indirect_effect")>
+    <RApiReturn(GetType(dataframe), GetType(IndirectEffectBootstrap))>
+    Public Function indirect_effect(sem_result As SEMResult, boot_result As BootstrapResult, Optional as_dataframe As Boolean = True) As Object
+        Dim ib As IndirectEffectBootstrap() = IndirectEffectBootstrap.FromResult(sem_result, boot_result).ToArray
+
+        If as_dataframe Then
+            Dim df As New dataframe With {.columns = New Dictionary(Of String, Array)}
+
+            Call df.add(NameOf(IndirectEffectBootstrap.fromName), From i As IndirectEffectBootstrap In ib Select i.fromName)
+            Call df.add(NameOf(IndirectEffectBootstrap.toName), From i As IndirectEffectBootstrap In ib Select i.toName)
+            Call df.add(NameOf(IndirectEffectBootstrap.indirectEffect), From i As IndirectEffectBootstrap In ib Select i.indirectEffect)
+            Call df.add(NameOf(IndirectEffectBootstrap.bse), From i As IndirectEffectBootstrap In ib Select i.bse)
+            Call df.add(NameOf(IndirectEffectBootstrap.ci_lb), From i As IndirectEffectBootstrap In ib Select i.ci_lb)
+            Call df.add(NameOf(IndirectEffectBootstrap.ci_ub), From i As IndirectEffectBootstrap In ib Select i.ci_ub)
+            Call df.add(NameOf(IndirectEffectBootstrap.sig), From i As IndirectEffectBootstrap In ib Select i.sig)
+
+            Return df
+        Else
+            Return ib
+        End If
     End Function
 
     <ExportAPI("as_causalmodel")>
