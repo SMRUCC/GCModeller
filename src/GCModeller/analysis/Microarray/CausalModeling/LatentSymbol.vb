b9cc2d334b6ae760cd730006cee98b64c8c430ec
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-22 10:12:34 +0800

    make manifest symbol index

--- src/GCModeller/analysis/Microarray/CausalModeling/LatentSymbol.vb
+++ src/GCModeller/analysis/Microarray/CausalModeling/LatentSymbol.vb
@@ -1,10 +1,15 @@
-﻿Imports Microsoft.VisualBasic.Data.GraphTheory
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.GraphTheory
 
 Public Class LatentSymbol
 
     Public Property [class] As String
     Public Property latent As String
     Public Property manifest_id As String
+
+    Public Overrides Function ToString() As String
+        Return $"[{[class]}:{latent}] {manifest_id}"
+    End Function
 
     Public Shared Iterator Function MakeLatents(symbols As IEnumerable(Of LatentSymbol)) As IEnumerable(Of LatentDefinition)
         For Each cls_group As IGrouping(Of String, LatentSymbol) In symbols.GroupBy(Function(s) s.class)
@@ -20,7 +25,19 @@ Public Class LatentSymbol
     End Function
 
     Public Shared Iterator Function MakeFullPath(symbols As IEnumerable(Of LatentSymbol), from As String(), [to] As String()) As IEnumerable(Of SparseGraph.Edge)
+        Dim class_group As Dictionary(Of String, String()) = symbols _
+            .GroupBy(Function(s) s.class) _
+            .ToDictionary(Function(s) s.Key,
+                          Function(s)
+                              Return LatentSymbols(s)
+                          End Function)
 
+
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Private Shared Function LatentSymbols(g As IGrouping(Of String, LatentSymbol)) As String()
+        Return MakeLatents(g).Select(Function(s) s.varName).Distinct.ToArray
     End Function
 
 End Class
