4862cb2233207b6d7389e856e0400f260fe42b41
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-23 18:18:31 +0800

    split data model

--- src/GCModeller/analysis/Metagenome/MetaFunction/metaTraits/TraitAnnotation.vb
+++ src/GCModeller/analysis/Metagenome/MetaFunction/metaTraits/TraitAnnotation.vb
@@ -4,13 +4,8 @@ Imports Microsoft.VisualBasic.Data.Framework.StorageProvider.Reflection
 
 Namespace metaTraits
 
-    ''' <summary>
-    ''' table parser model for ncbi_species_summary_no_predictions.tsv
-    ''' </summary>
-    Public Class TraitAnnotation
+    Public Class TraitData
 
-        Public Property taxon_id As UInteger
-        Public Property taxon_name As String
         Public Property trait_name As String
         Public Property unit As String
         Public Property database_count As String
@@ -22,11 +17,22 @@ Namespace metaTraits
         Public Property median As String
         Public Property mean As String
         Public Property maximum As String
-        Public Property discrete_values As String()
-        Public Property databases As String()
+        <Collection("discrete_values", ";")> Public Property discrete_values As String()
+        <Collection("databases", ";")> Public Property databases As String()
         Public Property group_1 As String
         Public Property group_2 As String
-        Public Property ontology_ids As String()
+        <Collection("ontology_ids", ";")> Public Property ontology_ids As String()
+
+    End Class
+
+    ''' <summary>
+    ''' table parser model for ncbi_species_summary_no_predictions.tsv
+    ''' </summary>
+    Public Class TraitAnnotation : Inherits TraitData
+
+        Public Property taxon_id As UInteger
+        Public Property taxon_name As String
+
         <Collection("taxon_lineage", "|")> Public Property taxon_lineage As String()
 
         ''' <summary>
