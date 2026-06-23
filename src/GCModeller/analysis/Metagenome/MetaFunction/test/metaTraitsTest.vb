5f75a4c62effe2b361480443158dff500c23c0b0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-23 18:14:12 +0800

    configs for run demo test for read metaTraits

---
+++ src/GCModeller/analysis/Metagenome/MetaFunction/test/metaTraitsTest.vb
@@ -0,0 +1,10 @@
+﻿Imports SMRUCC.genomics.Analysis.Metagenome.MetaFunction.metaTraits
+
+Module metaTraitsTest
+
+    Sub readFiles()
+        Dim annos = TraitAnnotation.ParseTable("C:\Users\Administrator\Downloads\ncbi_species_summary_no_predictions.tsv").ToArray
+
+        Pause()
+    End Sub
+End Module
