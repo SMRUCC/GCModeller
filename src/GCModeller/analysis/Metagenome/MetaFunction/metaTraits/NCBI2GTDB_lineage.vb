e2af3b330e0d45a345b94d2b19257d00879bc15f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-23 17:42:10 +0800

    add taxonomy id mapping

---
+++ src/GCModeller/analysis/Metagenome/MetaFunction/metaTraits/NCBI2GTDB_lineage.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
+
+Namespace metaTraits
+
+    Public Class NCBI2GTDB_lineage
+
+        <Column("taxonID NCBI")> Public Property taxonID_NCBI As UInteger
+
+        <Column("taxonID GTDB")> Public Property taxonID_GTDB As UInteger
+
+    End Class
+End Namespace
