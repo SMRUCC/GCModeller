d0659b185ae3eb0b42bfa0963dc2f731916d2c11
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-25 06:07:08 +0800

    export svm api

--- src/workbench/R#/metagenomics_kit/metaTraits.vb
+++ src/workbench/R#/metagenomics_kit/metaTraits.vb
@@ -1,7 +1,10 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Math.Distributions
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Analysis.Metagenome.MetaFunction.metaTraits
 Imports SMRUCC.genomics.Analysis.Metagenome.MetaFunction.TraitarVB
+Imports SMRUCC.genomics.Analysis.Metagenome.MetaFunction.TraitarVB.Models
+Imports SMRUCC.genomics.Analysis.Metagenome.MetaFunction.TraitarVB.Modules
 Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Package("metaTraits")>
@@ -18,5 +21,15 @@ Module metaTraitsTool
         Dim modelLoader As ModelLoader = New ModelLoader(repo).LoadAll()
         Console.WriteLine("表型数: " & modelLoader.PhenotypeCount)
         Return modelLoader
+    End Function
+
+    <ExportAPI("make_predicts")>
+    Public Function make_predicts(models As ModelLoader, genome As GenomeSample) As Object
+        Dim voting As New Modules.EnsembleVoting()
+        Dim phenotypeModels = models.LoadPhenotypeSVM
+        ' 执行预测
+        Dim predictions As Dictionary(Of String, Modules.EnsembleVoting.VotingResult) = voting.PredictAllPhenotypes(phenotypeModels, genome.PhyleticProfile)
+
+        Return predictions
     End Function
 End Module
