a372c5204381bb90a3837b1a3b7153cc73590a06
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-25 10:12:50 +0800

    try to make exports of the result talbes

---
+++ src/GCModeller/analysis/Metagenome/MetaFunction/metaTraits/Traitar/KeyFamily.vb
@@ -0,0 +1,12 @@
+﻿Namespace TraitarVB
+
+    Public Class KeyFamily
+
+        Public Property PFamId As String
+        Public Property positive As Integer
+        Public Property w As Double
+        Public Property cor As Double
+        Public Property description As String
+
+    End Class
+End Namespace
---
+++ src/GCModeller/analysis/Metagenome/MetaFunction/metaTraits/Traitar/PredictionResult.vb
@@ -0,0 +1,12 @@
+﻿Namespace TraitarVB
+
+    Public Class PredictionResult
+
+        Public Property phenotypeId As String
+        Public Property accession As String
+        Public Property category As String
+        Public Property predict As String
+        Public Property confidence As Double
+
+    End Class
+End Namespace
