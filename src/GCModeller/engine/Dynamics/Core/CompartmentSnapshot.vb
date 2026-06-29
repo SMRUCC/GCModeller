b4ca4ec5ae4afd5584575517543f974e0121c8e0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-29 15:28:15 +0800

    Create CompartmentSnapshot.vb

---
+++ src/GCModeller/engine/Dynamics/Core/CompartmentSnapshot.vb
@@ -0,0 +1,10 @@
+﻿Namespace Core
+
+    Public Class CompartmentSnapshot
+
+        Public Property compart_id As String
+        Public Property snapshot As Dictionary(Of String, Double)
+
+    End Class
+
+End Namespace
