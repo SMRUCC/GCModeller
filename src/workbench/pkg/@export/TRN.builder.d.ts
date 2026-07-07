cea1586821f8ca50ad867d450d9306e4c0bb5814
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-07 11:45:43 +0800

    run package msbuild

--- src/workbench/pkg/@export/TRN.builder.d.ts
+++ src/workbench/pkg/@export/TRN.builder.d.ts
@@ -14,11 +14,12 @@ declare namespace TRN.builder {
      * @param identities_cutoff default value Is ``0.8``.
      * @param minW default value Is ``0.85``.
      * @param top default value Is ``3``.
+     * @param bg default value Is ``null``.
      * @param permutation default value Is ``2500``.
      * @param tqdm_bar default value Is ``true``.
      * @param env default value Is ``null``.
    */
-   function motif_search(db: object, search_regions: any, family?: any, identities_cutoff?: number, minW?: number, top?: object, permutation?: object, tqdm_bar?: boolean, env?: object): object;
+   function motif_search(db: object, search_regions: any, family?: any, identities_cutoff?: number, minW?: number, top?: object, bg?: object, permutation?: object, tqdm_bar?: boolean, env?: object): object;
    /**
      * @param env default value Is ``null``.
    */
