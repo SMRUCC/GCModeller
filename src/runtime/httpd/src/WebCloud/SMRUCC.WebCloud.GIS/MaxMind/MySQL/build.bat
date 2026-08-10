35a81fe58a7b67050da9f6705b175923f82dd370
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-11 03:04:15 +0800

    remlves obsolete project source files

--- src/runtime/httpd/src/WebCloud/SMRUCC.WebCloud.GIS/MaxMind/MySQL/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-Reflector --reflects /sql ./geolite2.sql /namespace MaxMind.geolite2 /split --language visualbasic 
-Reflector /MySQL.Markdown /sql ./geolite2.sql > "./geolite2-dev-docs.md"
