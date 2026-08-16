a2cac816fb0f846b5c695e8a74f3911c3389ab82
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-16 23:31:15 +0800

    fix of the namespace reference errror

--- src/GCModeller/core/Bio.Assembly/test/sequenceToolsTest.vb
+++ src/GCModeller/core/Bio.Assembly/test/sequenceToolsTest.vb
@@ -1,59 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::2542e098f9a63de60ea2660649edd7d1, core\Bio.Assembly\Test\sequenceToolsTest.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 16
-    '    Code Lines: 14 (87.50%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 2 (12.50%)
-    '     File Size: 744 B
+' Summaries:
 
 
-    ' Module sequenceToolsTest
-    ' 
-    '     Sub: Main1
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 16
+'    Code Lines: 14 (87.50%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 2 (12.50%)
+'     File Size: 744 B
+
+
+' Module sequenceToolsTest
+' 
+'     Sub: Main1
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports SMRUCC.genomics.ComponentModel.Loci
-Imports SMRUCC.genomics.SequenceModel
 Imports SMRUCC.genomics.SequenceModel.FASTA
+Imports SMRUCC.genomics.SequenceModel.Slicer
 
 Module sequenceToolsTest
     Sub Main1()
