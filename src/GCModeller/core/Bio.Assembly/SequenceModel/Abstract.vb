ea79e6effa36563f592c9566202609c40c1dc56b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-16 22:39:23 +0800

    split source file into multiple parts

--- src/GCModeller/core/Bio.Assembly/SequenceModel/Abstract.vb
+++ src/GCModeller/core/Bio.Assembly/SequenceModel/Abstract.vb
@@ -70,25 +70,6 @@ Imports System.Xml.Serialization
 
 Namespace SequenceModel
 
-    Public Enum SeqTypes As Integer
-        ''' <summary>
-        ''' the unknow sequence type
-        ''' </summary>
-        Generic = 0
-        ''' <summary>
-        ''' Deoxyribonucleotide - DNA(ATGC)
-        ''' </summary>
-        DNA
-        ''' <summary>
-        ''' Ribonucleotide - RNA(AUGC)
-        ''' </summary>
-        RNA
-        ''' <summary>
-        ''' Polypeptide
-        ''' </summary>
-        <Description("prot")> Protein
-    End Enum
-
     ''' <summary>
     ''' Sequence model for a macro biomolecule sequence.
     ''' </summary>
---
+++ src/GCModeller/core/Bio.Assembly/SequenceModel/SeqTypes.vb
@@ -0,0 +1,24 @@
+﻿
+Imports System.ComponentModel
+
+Namespace SequenceModel
+
+    Public Enum SeqTypes As Integer
+        ''' <summary>
+        ''' the unknow sequence type
+        ''' </summary>
+        Generic = 0
+        ''' <summary>
+        ''' Deoxyribonucleotide - DNA(ATGC)
+        ''' </summary>
+        DNA
+        ''' <summary>
+        ''' Ribonucleotide - RNA(AUGC)
+        ''' </summary>
+        RNA
+        ''' <summary>
+        ''' Polypeptide
+        ''' </summary>
+        <Description("prot")> Protein
+    End Enum
+End Namespace
