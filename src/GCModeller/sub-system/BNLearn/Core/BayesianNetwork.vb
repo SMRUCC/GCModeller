9722b6455108e0a8d867728c8b290644baf00514
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-27 05:36:17 +0800

    show verbose progress information

--- src/GCModeller/sub-system/BNLearn/Core/BayesianNetwork.vb
+++ src/GCModeller/sub-system/BNLearn/Core/BayesianNetwork.vb
@@ -1,70 +1,70 @@
 ﻿#Region "Microsoft.VisualBasic::328b8b16b2052a8520ec1e121444a069, sub-system\BNLearn\Core\BayesianNetwork.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 351
-    '    Code Lines: 228 (64.96%)
-    ' Comment Lines: 58 (16.52%)
-    '    - Xml Docs: 58.62%
-    ' 
-    '   Blank Lines: 65 (18.52%)
-    '     File Size: 12.76 KB
+' Summaries:
 
 
-    '     Class BnNode
-    ' 
-    '         Properties: Children, CPD, Index, Level, Name
-    '                     Parents
-    ' 
-    '         Function: ToString
-    ' 
-    '     Class BayesianNetwork
-    ' 
-    '         Properties: Adjacency, Blacklist, EdgeCount, NameToIndex, Nodes
-    '                     Whitelist
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    ' 
-    '         Function: AddEdge, AddNode, CloneStructure, GetEdges, GetNodeIndex
-    '                   HasEdge, IsDAG, MakeBlackIndex, TopologicalSort, ToString
-    '                   WouldCreateCycle
-    ' 
-    '         Sub: AddBlacklistEdge, AddWhitelistEdge, RemoveEdge, ResizeAdjacency
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 351
+'    Code Lines: 228 (64.96%)
+' Comment Lines: 58 (16.52%)
+'    - Xml Docs: 58.62%
+' 
+'   Blank Lines: 65 (18.52%)
+'     File Size: 12.76 KB
+
+
+'     Class BnNode
+' 
+'         Properties: Children, CPD, Index, Level, Name
+'                     Parents
+' 
+'         Function: ToString
+' 
+'     Class BayesianNetwork
+' 
+'         Properties: Adjacency, Blacklist, EdgeCount, NameToIndex, Nodes
+'                     Whitelist
+' 
+'         Constructor: (+2 Overloads) Sub New
+' 
+'         Function: AddEdge, AddNode, CloneStructure, GetEdges, GetNodeIndex
+'                   HasEdge, IsDAG, MakeBlackIndex, TopologicalSort, ToString
+'                   WouldCreateCycle
+' 
+'         Sub: AddBlacklistEdge, AddWhitelistEdge, RemoveEdge, ResizeAdjacency
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -79,6 +79,7 @@
 '   - 白名单/黑名单约束
 ' ============================================================
 
+Imports System.Runtime.CompilerServices
 Imports System.Text
 
 Namespace Core
@@ -273,8 +274,12 @@ Namespace Core
         ' ==================== 拓扑排序 ====================
 
         ''' <summary>获取拓扑排序（Kahn算法）</summary>
-        Public Function TopologicalSort() As Integer()
-            If Not _topoDirty AndAlso _topoOrder IsNot Nothing Then Return _topoOrder
+        Public Function TopologicalSort(<CallerMemberName> Optional callee As String = Nothing) As Integer()
+            If Not _topoDirty AndAlso _topoOrder IsNot Nothing Then
+                Return _topoOrder
+            Else
+                Call $"[{callee}] make network topological sort!".debug
+            End If
 
             Dim n As Integer = Nodes.Count
             Dim inDegree As Integer() = New Integer(n - 1) {}
--- src/GCModeller/sub-system/BNLearn/ParameterLearning/ParameterLearning.vb
+++ src/GCModeller/sub-system/BNLearn/ParameterLearning/ParameterLearning.vb
@@ -73,116 +73,124 @@ Namespace ParameterLearning
     ''' <summary>
     ''' 贝叶斯网络参数学习器
     ''' </summary>
-    Public Module BnParameterLearner
+    Public Class BnParameterLearner
+
+        Dim t0 As Date = Now
+        Dim nS As Integer = 0
+        Dim totalLL As Double = 0
+        Dim totalBIC As Double = 0
+        Dim sumR2 As Double = 0
+
+        Private Sub LearnRoot(ByRef cpd As Core.BnCPD, ByRef y As Double())
+            ' 无父节点：边际分布 N(μ, σ²)
+            Dim mean As Double = 0
+            For j = 0 To nS - 1
+                mean += y(j)
+            Next
+            mean /= nS
+
+            Dim rss As Double = 0
+            For j = 0 To nS - 1
+                rss += (y(j) - mean) ^ 2
+            Next
+
+            cpd.Intercept = mean
+            cpd.Coeffs = New Double() {}
+            cpd.ParentIndices = New Integer() {}
+            cpd.ResidualVariance = rss / nS
+            cpd.ResidualSD = Math.Sqrt(cpd.ResidualVariance)
+            cpd.RSquared = 0.0
+
+            ' 对数似然
+            If cpd.ResidualVariance > 0.000000000000001 Then
+                totalLL += -nS / 2.0 * Math.Log(2 * Math.PI * cpd.ResidualVariance) - rss / (2 * cpd.ResidualVariance)
+            End If
+
+            ' BIC
+            Dim k As Integer = 2  ' μ, σ²
+            totalBIC += -2 * totalLL + k * Math.Log(nS)
+        End Sub
+
+        Private Sub LearnRegression(ByRef cpd As Core.BnCPD, ByRef y As Double(), ByRef node As Core.BnNode, ByRef data As Core.GeneExpressionData)
+            ' 有父节点：线性回归 Xi = β0 + Σ βj·Paj + ε
+            Dim parentIndices As Integer() = node.Parents.ToArray()
+            Dim nP As Integer = parentIndices.Length
+
+            ' 构建设计矩阵
+            Dim X As Double(,) = New Double(nS - 1, nP) {}
+            For j = 0 To nS - 1
+                X(j, 0) = 1.0  ' 截距
+                For p = 0 To nP - 1
+                    X(j, p + 1) = data.Matrix(parentIndices(p), j)
+                Next
+            Next
+
+            ' 最小二乘法求解 β = (X'X)^(-1) X'y
+            Dim beta As Double() = OLS.Solve(X, y, nS, nP + 1)
+
+            ' 计算残差
+            Dim predicted As Double() = New Double(nS - 1) {}
+            Dim rss As Double = 0
+            For j = 0 To nS - 1
+                predicted(j) = beta(0)
+                For p = 0 To nP - 1
+                    predicted(j) += beta(p + 1) * X(j, p + 1)
+                Next
+                rss += (y(j) - predicted(j)) ^ 2
+            Next
+
+            ' 总平方和
+            Dim yMean As Double = y.Average()
+            Dim tss As Double = 0
+            For j = 0 To nS - 1
+                tss += (y(j) - yMean) ^ 2
+            Next
+
+            cpd.Intercept = beta(0)
+            cpd.Coeffs = New Double(nP - 1) {}
+            cpd.ParentIndices = parentIndices
+            For p = 0 To nP - 1
+                cpd.Coeffs(p) = beta(p + 1)
+            Next
+
+            cpd.ResidualVariance = rss / Math.Max(1, nS - nP - 1)
+            cpd.ResidualSD = Math.Sqrt(cpd.ResidualVariance)
+            cpd.RSquared = If(tss > 0, 1.0 - rss / tss, 0.0)
+
+            ' 对数似然
+            If cpd.ResidualVariance > 0.000000000000001 Then
+                totalLL += -nS / 2.0 * Math.Log(2 * Math.PI * cpd.ResidualVariance) - rss / (2 * cpd.ResidualVariance)
+            End If
+
+            ' BIC
+            Dim k As Integer = nP + 2  ' β0, β1...βp, σ²
+            Dim nodeBIC As Double = -2 * (-nS / 2.0 * Math.Log(2 * Math.PI * cpd.ResidualVariance) - rss / (2 * cpd.ResidualVariance)) + k * Math.Log(nS)
+            totalBIC += nodeBIC
+        End Sub
 
         ''' <summary>
         ''' 从数据学习网络参数
         ''' </summary>
         Public Function Learn(network As Core.BayesianNetwork, data As Core.GeneExpressionData) As ParameterLearningResult
-            Dim t0 As Date = Now
-            Dim nS As Integer = data.NSample
-            Dim totalLL As Double = 0
-            Dim totalBIC As Double = 0
-            Dim sumR2 As Double = 0
-
-            Call "make network topological sort!".debug
-
             ' 按拓扑排序依次拟合每个节点
             Dim topoOrder As Integer() = network.TopologicalSort()
 
             Call "run bnlearn network parameter learning...".debug
 
+            nS = data.NSample
+
             For Each nodeIdx As Integer In TqdmWrapper.Wrap(topoOrder)
                 Dim node As Core.BnNode = network.Nodes(nodeIdx)
-                Dim cpd As New Core.BnCPD()
-                cpd.NodeIndex = nodeIdx
-                cpd.NSamples = nS
-
+                Dim cpd As New Core.BnCPD() With {
+                    .NodeIndex = nodeIdx,
+                    .NSamples = nS
+                }
                 Dim y As Double() = data.GetGeneExpression(nodeIdx)
 
                 If node.Parents.Count = 0 Then
-                    ' 无父节点：边际分布 N(μ, σ²)
-                    Dim mean As Double = 0
-                    For j = 0 To nS - 1
-                        mean += y(j)
-                    Next
-                    mean /= nS
-
-                    Dim rss As Double = 0
-                    For j = 0 To nS - 1
-                        rss += (y(j) - mean) ^ 2
-                    Next
-
-                    cpd.Intercept = mean
-                    cpd.Coeffs = New Double() {}
-                    cpd.ParentIndices = New Integer() {}
-                    cpd.ResidualVariance = rss / nS
-                    cpd.ResidualSD = Math.Sqrt(cpd.ResidualVariance)
-                    cpd.RSquared = 0.0
-
-                    ' 对数似然
-                    If cpd.ResidualVariance > 0.000000000000001 Then
-                        totalLL += -nS / 2.0 * Math.Log(2 * Math.PI * cpd.ResidualVariance) - rss / (2 * cpd.ResidualVariance)
-                    End If
-
-                    ' BIC
-                    Dim k As Integer = 2  ' μ, σ²
-                    totalBIC += -2 * totalLL + k * Math.Log(nS)
+                    Call LearnRoot(cpd, y)
                 Else
-                    ' 有父节点：线性回归 Xi = β0 + Σ βj·Paj + ε
-                    Dim parentIndices As Integer() = node.Parents.ToArray()
-                    Dim nP As Integer = parentIndices.Length
-
-                    ' 构建设计矩阵
-                    Dim X As Double(,) = New Double(nS - 1, nP) {}
-                    For j = 0 To nS - 1
-                        X(j, 0) = 1.0  ' 截距
-                        For p = 0 To nP - 1
-                            X(j, p + 1) = data.Matrix(parentIndices(p), j)
-                        Next
-                    Next
-
-                    ' 最小二乘法求解 β = (X'X)^(-1) X'y
-                    Dim beta As Double() = OLS.Solve(X, y, nS, nP + 1)
-
-                    ' 计算残差
-                    Dim predicted As Double() = New Double(nS - 1) {}
-                    Dim rss As Double = 0
-                    For j = 0 To nS - 1
-                        predicted(j) = beta(0)
-                        For p = 0 To nP - 1
-                            predicted(j) += beta(p + 1) * X(j, p + 1)
-                        Next
-                        rss += (y(j) - predicted(j)) ^ 2
-                    Next
-
-                    ' 总平方和
-                    Dim yMean As Double = y.Average()
-                    Dim tss As Double = 0
-                    For j = 0 To nS - 1
-                        tss += (y(j) - yMean) ^ 2
-                    Next
-
-                    cpd.Intercept = beta(0)
-                    cpd.Coeffs = New Double(nP - 1) {}
-                    cpd.ParentIndices = parentIndices
-                    For p = 0 To nP - 1
-                        cpd.Coeffs(p) = beta(p + 1)
-                    Next
-
-                    cpd.ResidualVariance = rss / Math.Max(1, nS - nP - 1)
-                    cpd.ResidualSD = Math.Sqrt(cpd.ResidualVariance)
-                    cpd.RSquared = If(tss > 0, 1.0 - rss / tss, 0.0)
-
-                    ' 对数似然
-                    If cpd.ResidualVariance > 0.000000000000001 Then
-                        totalLL += -nS / 2.0 * Math.Log(2 * Math.PI * cpd.ResidualVariance) - rss / (2 * cpd.ResidualVariance)
-                    End If
-
-                    ' BIC
-                    Dim k As Integer = nP + 2  ' β0, β1...βp, σ²
-                    Dim nodeBIC As Double = -2 * (-nS / 2.0 * Math.Log(2 * Math.PI * cpd.ResidualVariance) - rss / (2 * cpd.ResidualVariance)) + k * Math.Log(nS)
-                    totalBIC += nodeBIC
+                    Call LearnRegression(cpd, y, node, data)
                 End If
 
                 node.CPD = cpd
@@ -197,7 +205,7 @@ Namespace ParameterLearning
                 .ElapsedMs = (Now - t0).TotalMilliseconds
             }
         End Function
-    End Module
+    End Class
 
 End Namespace
 
