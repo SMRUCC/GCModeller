55cf1d32499c27784101c69a1d35079b35cb2dec
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-29 02:17:56 +0800

    how to export filter gff feature as tabluar data

--- src/workbench/R#/seqtoolkit/Annotations/genomics.vb
+++ src/workbench/R#/seqtoolkit/Annotations/genomics.vb
@@ -1,59 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::77f82e6fbe753fca0ec1a7963aeaa4ca, R#\seqtoolkit\Annotations\genomics.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 258
-    '    Code Lines: 183 (70.93%)
-    ' Comment Lines: 43 (16.67%)
-    '    - Xml Docs: 95.35%
-    ' 
-    '   Blank Lines: 32 (12.40%)
-    '     File Size: 9.72 KB
+' Summaries:
 
 
-    ' Module genomics
-    ' 
-    '     Function: asPTT, asTable, extract_gff_seqs, genes, getUpstream
-    '               getUpStream, gff_features, operon_set, PTT2Dump, read_nucmer
-    '               readGff, readGtf, SourceFeatures, write_gff3, writePPTTabular
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 258
+'    Code Lines: 183 (70.93%)
+' Comment Lines: 43 (16.67%)
+'    - Xml Docs: 95.35%
+' 
+'   Blank Lines: 32 (12.40%)
+'     File Size: 9.72 KB
+
+
+' Module genomics
+' 
+'     Function: asPTT, asTable, extract_gff_seqs, genes, getUpstream
+'               getUpStream, gff_features, operon_set, PTT2Dump, read_nucmer
+'               readGff, readGtf, SourceFeatures, write_gff3, writePPTTabular
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
