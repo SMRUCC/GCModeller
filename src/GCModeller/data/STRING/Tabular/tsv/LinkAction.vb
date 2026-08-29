45129737c9ddb1fe3eb85a7071c3b2a206bc97de
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-30 02:54:13 +0800

    Remove old Tsv.vb from Tabular namespace
    本次改动删除了 `src/GCModeller/data/STRING/Tabular/tsv/Tsv.vb`文件，该文件是 STRING数据模块在命名空间重构过程中遗留的旧版本实现。
    
    该文件原本包含以下数据类型与解析逻辑：
    
    - `LinkAction`：解析 `9606.protein.actions.v10.txt`等蛋白质互作注释数据的类型。
    - `linksDetail`：解析 `9606.protein.links*.txt`系列蛋白质网络数据（含各通道子分数）的类型。
    - `entrez_gene_id_vs_string`：Entrez Gene ID与 STRING Locus ID之间的映射类型。
    
    删除后，这些类型将由重构后的新实现（位于调整后的目录结构与命名空间中）替代，避免新旧版本重复定义导致的歧义或冲突。此改动属于模块重组的一部分，需确认其它代码的引用已同步指向新的实现，以保证编译与运行正常。

---
+++ src/GCModeller/data/STRING/Tabular/tsv/LinkAction.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic.Text
+
+Namespace Tabular.Tsv
+
+    ''' <summary>
+    ''' interaction types for protein links.
+    ''' (从String-DB之中下载的蛋白质互作网络数据，例如：``9606.protein.actions.v10.txt``，
+    ''' 这个对象只是存在注释数据的互作关系，只是所有的互作关系之中研究比较明白的网络部分，
+    ''' 假若查看所有的网络数据在``9606.protein.links.v10.txt``文件之中)
+    ''' </summary>
+    Public Class LinkAction
+
+        Public Property item_id_a As String
+        Public Property item_id_b As String
+        Public Property mode As String
+        Public Property action As String
+        Public Property a_is_acting As String
+        Public Property score As String
+
+        Public Shared Iterator Function LoadText(path As String) As IEnumerable(Of LinkAction)
+            For Each line As String In path.IterateAllLines.Skip(1)
+                Dim tokens As String() = line.Split(ASCII.TAB)
+
+                Yield New LinkAction With {
+                    .item_id_a = tokens(0),
+                    .item_id_b = tokens(1),
+                    .mode = tokens(2),
+                    .action = tokens(3),
+                    .a_is_acting = tokens(4),
+                    .score = tokens(5)
+                }
+            Next
+        End Function
+    End Class
+End Namespace
---
+++ src/GCModeller/data/STRING/Tabular/tsv/entrez_gene_id_vs_string.vb
@@ -0,0 +1,103 @@
+﻿#Region "Microsoft.VisualBasic::f90cec9e8e004a9a70ca1df9736eec3f, data\STRING\Tsv.vb"
+
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+
+' /********************************************************************************/
+
+' Summaries:
+
+
+' Code Statistics:
+
+'   Total Lines: 256
+'    Code Lines: 192 (75.00%)
+' Comment Lines: 34 (13.28%)
+'    - Xml Docs: 94.12%
+' 
+'   Blank Lines: 30 (11.72%)
+'     File Size: 13.09 KB
+
+
+'     Class LinkAction
+' 
+'         Properties: a_is_acting, action, item_id_a, item_id_b, mode
+'                     score
+' 
+'         Function: LoadText
+' 
+'     Class linksDetail
+' 
+'         Properties: coexpression, coexpression_transferred, combined_score, cooccurence, database
+'                     database_transferred, experimental, experiments, experiments_transferred, fusion
+'                     homology, neighborhood, neighborhood_transferred, protein1, protein2
+'                     textmining, textmining_transferred
+' 
+'         Function: IteratesLinks, LoadFile, Selects, ToString
+' 
+'     Class entrez_gene_id_vs_string
+' 
+'         Properties: Entrez_Gene_ID, STRING_Locus_ID
+' 
+'         Function: BuildMaps, BuildMapsFromFile, ToString
+' 
+' 
+' /********************************************************************************/
+
+#End Region
+
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Data.Framework
+Imports Microsoft.VisualBasic.Data.Framework.StorageProvider.Reflection
+
+Namespace Tabular.Tsv
+
+    ''' <summary>
+    ''' separate identifier mapping files, for several frequently used name_spaces...
+    ''' </summary>
+    Public Class entrez_gene_id_vs_string
+
+        <Column("#Entrez_Gene_ID")> Public Property Entrez_Gene_ID As String
+        Public Property STRING_Locus_ID As String
+
+        Public Overrides Function ToString() As String
+            Return $"{Entrez_Gene_ID} <--> {STRING_Locus_ID}"
+        End Function
+
+        Public Shared Function BuildMapsFromFile(path As String, Optional tsv As Boolean = True) As Dictionary(Of String, String)
+            If tsv Then
+                Return BuildMaps(path.Imports(Of entrez_gene_id_vs_string)(vbTab))
+            Else
+                Return BuildMaps(path.LoadCsv(Of entrez_gene_id_vs_string))
+            End If
+        End Function
+
+        Public Shared Function BuildMaps(source As IEnumerable(Of entrez_gene_id_vs_string)) As Dictionary(Of String, String)
+            Return source.ToDictionary(Function(x) x.Entrez_Gene_ID, Function(x) x.STRING_Locus_ID)
+        End Function
+    End Class
+End Namespace
