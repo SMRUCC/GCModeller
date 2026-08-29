94eec8d4d3d7c58d531eb2a6c2b1162bee6a68c8
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-29 23:24:41 +0800

    Add GEARS model R# API to simulator workbench
    This change extends the `GEARS` package module in the GCModeller simulator workbench (`src/GCModeller/sub-system/simulators/gearsTools.vb`) with new R# export APIs for building and training GEARS models.
    
    Newly added export APIs:
    - `new`: creates a `GEARS` instance from a gene expression matrix, a `PriorNetwork`, and a `GEARSConfig`; the expression matrix is converted via `BnIO.ReadGeneExpressionMatrix`.
    - `training_set`: sets the training sample set, accepting control sample IDs as a raw character vector and perturbed samples as `SampleInfo` objects (streamed through a pipeline), then delegates to `SetTrainingSamples`.
    - `train`: runs model training through `Train()` and returns the trained `GEARS` instance.
    
    In addition, the module now registers a new R# type export `GEARS_opts` mapped to `GEARSConfig`, and adds the required `Imports` for BNLearn core/IO, GEARS, HTS DataFrame, ExperimentDesigner, and R# runtime infrastructure.
    
    This enables R# scripts running in the GCModeller workbench to construct, configure, and train GEARS models interactively, plugging the simulator's metabolic modelling workflow into the R# scripting environment.

--- src/GCModeller/sub-system/simulators/gearsTools.vb
+++ src/GCModeller/sub-system/simulators/gearsTools.vb
@@ -1,7 +1,57 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Analysis.BNLearn.Core
+Imports SMRUCC.genomics.Analysis.BNLearn.IO
+Imports SMRUCC.genomics.Analysis.GEARS
+Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+Imports SMRUCC.genomics.GCModeller.Workbench.ExperimentDesigner
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
+Imports SMRUCC.Rsharp.Runtime.Interop
+Imports SMRUCC.Rsharp.Runtime.Vectorization
 
 <Package("GEARS")>
+<RTypeExport("GEARS_opts", GetType(GEARSConfig))>
 Public Module gearsTools
+
+    <ExportAPI("new")>
+    Public Function create_GEARS(x As Matrix, prior As PriorNetwork, config As GEARSConfig) As GEARS
+        Dim exprData As GeneExpressionData = BnIO.ReadGeneExpressionMatrix(x)
+        Dim gears As New GEARS(exprData, prior, config)
+        Return gears
+    End Function
+
+    ''' <summary>
+    ''' Set the training sample set
+    ''' </summary>
+    ''' <param name="gears"></param>
+    ''' <param name="x"></param>
+    ''' <param name="controls"></param>
+    ''' <param name="perturbed"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("training_set")>
+    Public Function set_trainingSampleSet(gears As GEARS, x As Matrix,
+                                          <RRawVectorArgument(TypeCodes.string)> controls As Object,
+                                          <RRawVectorArgument(GetType(SampleInfo))> perturbed As Object,
+                                          Optional env As Environment = Nothing) As Object
+
+        Dim controlsId As String() = CLRVector.asCharacter(controls)
+        Dim perturbedSet As PipeIterator(Of SampleInfo) = pipeline.Stream(Of SampleInfo)(perturbed, env)
+
+        If perturbedSet.isError Then
+            Return perturbedSet.getError
+        End If
+
+        Return gears.SetTrainingSamples(x, controlsId, perturbedSet.ToArray)
+    End Function
+
+    <ExportAPI("train")>
+    Public Function train(gears As GEARS) As GEARS
+        Call gears.Train()
+        Return gears
+    End Function
 
 End Module
