2dbaaf1a9d8e24f8d70b3ed969ec110a0a2c218b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-30 02:46:57 +0800

    split source file into multiple parts

--- src/GCModeller/data/STRING/Tabular/Network.vb
+++ src/GCModeller/data/STRING/Tabular/Network.vb
@@ -61,52 +61,18 @@
 #End Region
 
 Imports System.Xml.Serialization
-Imports Microsoft.VisualBasic.Data.Framework.StorageProvider.Reflection
-Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
 Imports Microsoft.VisualBasic.Language
 
-Namespace SimpleCsv
-
-    ''' <summary>
-    ''' STRING网络的Csv数据文件存储
-    ''' </summary>
-    Public Class PitrNode : Inherits NetworkEdge
-
-        <XmlAttribute("Node_a")> <Column("fromNode")>
-        Public Overrides Property FromNode As String
-        <XmlAttribute("Node_b")> <Column("toNode")>
-        Public Overrides Property ToNode As String
-        <XmlAttribute("confidence")> <Column("confidence")>
-        Public Overrides Property value As Double
-
-        Public Overrides Function ToString() As String
-            Return $"{FromNode} <---> {ToNode}; {value}"
-        End Function
-
-        ''' <summary>
-        ''' 返回空字符串表示标识符不是这个节点边两端的实体对象
-        ''' </summary>
-        ''' <param name="NodeId"></param>
-        ''' <returns></returns>
-        Public Function GetInteractNode(NodeId As String) As String
-            If String.Equals(NodeId, FromNode) Then
-                Return ToNode
-            ElseIf String.Equals(NodeId, ToNode) Then
-                Return FromNode
-            Else
-                Return ""
-            End If
-        End Function
-    End Class
+Namespace Tabular
 
     <XmlRoot("Interaction_Network", Namespace:="http://code.google.com/p/genome-in-code/interaction_network/")>
     Public Class Network
 
-        <XmlElement("Node")> Public Property Nodes As SimpleCsv.PitrNode()
+        <XmlElement("Node")> Public Property Nodes As PitrNode()
 
         Public Function GetConfidence(Node1 As String, Node2 As String) As Double
             Dim LQuery As PitrNode = LinqAPI.DefaultFirst(Of PitrNode) <=
- _
+                                                                         _
                 From edge As PitrNode
                 In Nodes
                 Where edge.Contains(Node1) AndAlso
---
+++ src/GCModeller/data/STRING/Tabular/PitrNode.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Data.Framework.StorageProvider.Reflection
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+
+Namespace Tabular
+
+    ''' <summary>
+    ''' STRING网络的Csv数据文件存储
+    ''' </summary>
+    Public Class PitrNode : Inherits NetworkEdge
+
+        <XmlAttribute("Node_a"), Column("fromNode")>
+        Public Overrides Property FromNode As String
+        <XmlAttribute("Node_b")> <Column("toNode")>
+        Public Overrides Property ToNode As String
+        <XmlAttribute("confidence")> <Column("confidence")>
+        Public Overrides Property value As Double
+
+        Public Overrides Function ToString() As String
+            Return $"{FromNode} <---> {ToNode}; {value}"
+        End Function
+
+        ''' <summary>
+        ''' 返回空字符串表示标识符不是这个节点边两端的实体对象
+        ''' </summary>
+        ''' <param name="NodeId"></param>
+        ''' <returns></returns>
+        Public Function GetInteractNode(NodeId As String) As String
+            If String.Equals(NodeId, FromNode) Then
+                Return ToNode
+            ElseIf String.Equals(NodeId, ToNode) Then
+                Return FromNode
+            Else
+                Return ""
+            End If
+        End Function
+    End Class
+End Namespace
