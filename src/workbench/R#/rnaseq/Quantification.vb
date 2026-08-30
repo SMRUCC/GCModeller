6a73b3b06b63215dab13889fe80e1b3cb0edf45c
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-31 01:45:26 +0800

    Refactor gene quantification APIs: typed pipelines and docs
    本次编辑针对 R# rnaseq工作台中的 Quantification.vb做了类型强化、管道重构与文档补齐三方面改动。
    
    1.补充 XML文档注释：为 <Package("gene_quantification")>模块以及 read_featureCounts、counts_matrix、edgeR_tmm、read_genedata等导出 API添加了模块级与函数级注释；其中 convert_to_tpm原本为空的说明被完善为“将 feature counts数据归一化为 TPM表达值”，并保留了 counts、env、returns参数的详细释义。
    
    2.强类型化输入参数：counts_matrix、convert_to_tpm的 counts参数以及 expression_data的 sampledata参数，均由宽松的 RRawVectorArgument改为带类型约束的 RRawVectorArgument(GetType(featureCounts)) / RRawVectorArgument(GetType(GeneSampleSet))，使 R端传入的数据类型检查更严格、意图更明确。
    
    3.管道重构：将原先 pipeline.TryCreatePipeline(Of T)结合 populates(Of T)(env)的两步取数流程，简化为 pipeline.Stream(Of T)直接返回 PipeIterator(Of T)，并分别用 pull.FeatureCountMatrix、pull.MakeGeneExpressions直接产出结果，避免了对同一集合的重复枚举，代码更简洁、性能更优。
    
    整体影响：基因定量相关的 R# API使用方式不变，但内部实现更高效、类型更安全，且 API文档信息更完整，便于调用者理解各函数的用途与参数语义。

--- src/workbench/R#/rnaseq/Quantification.vb
+++ src/workbench/R#/rnaseq/Quantification.vb
@@ -66,25 +66,39 @@ Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports Matrix = SMRUCC.genomics.Analysis.HTS.DataFrame.Matrix
 
+''' <summary>
+''' gene expression quantify tools
+''' </summary>
 <Package("gene_quantification")>
 Module Quantification
 
+    ''' <summary>
+    ''' read feature counts tsv table file
+    ''' </summary>
+    ''' <param name="file"></param>
+    ''' <returns></returns>
     <ExportAPI("read_featureCounts")>
     <RApiReturn(GetType(featureCounts))>
     Public Function read_featureCounts(file As String) As Object
         Return featureCounts.ReadTable(file).ToArray
     End Function
 
+    ''' <summary>
+    ''' make assemble of the reads sample counts as a gene expression matrix object
+    ''' </summary>
+    ''' <param name="counts">feature counts data of multiple samples which is read via ``read_featureCounts`` api function</param>
+    ''' <param name="env"></param>
+    ''' <returns>the gene expression matrix object</returns>
     <ExportAPI("counts_matrix")>
     <RApiReturn(GetType(Matrix))>
-    Public Function as_countmatrix(<RRawVectorArgument> counts As Object, Optional env As Environment = Nothing) As Object
-        Dim pull As pipeline = pipeline.TryCreatePipeline(Of featureCounts)(counts, env)
+    Public Function as_countmatrix(<RRawVectorArgument(GetType(featureCounts))> counts As Object, Optional env As Environment = Nothing) As Object
+        Dim pull As PipeIterator(Of featureCounts) = pipeline.Stream(Of featureCounts)(counts, env)
 
         If pull.isError Then
             Return pull.getError
         End If
 
-        Return pull.populates(Of featureCounts)(env).FeatureCountMatrix
+        Return pull.FeatureCountMatrix
     End Function
 
     ''' <summary>
@@ -107,6 +121,13 @@ Module Quantification
         Return counts.EdgeRTMMNormalize(trimFractionM, trimFractionA)
     End Function
 
+    ''' <summary>
+    ''' get edgeR TMM factors
+    ''' </summary>
+    ''' <param name="countData"></param>
+    ''' <param name="trimFractionM"></param>
+    ''' <param name="trimFractionA"></param>
+    ''' <returns></returns>
     <ExportAPI("edgeR_tmm")>
     Public Function edgeR_tmm_factors(countData As Matrix, Optional trimFractionM As Double = 0.3, Optional trimFractionA As Double = 0.05) As Object
         Dim tmmFactors = countData.CalcTMMFactors(trimFractionA:=trimFractionA, trimFractionM:=trimFractionM)
@@ -125,6 +146,11 @@ Module Quantification
         Return GeneQuantification.ConvertCountsToTPM(IndexStats.Parse(file.OpenReadonly)).ToArray
     End Function
 
+    ''' <summary>
+    ''' read gene data csv table file
+    ''' </summary>
+    ''' <param name="file"></param>
+    ''' <returns></returns>
     <ExportAPI("read_genedata")>
     <RApiReturn(GetType(GeneData))>
     Public Function read_genedata(file As String) As GeneData()
@@ -132,33 +158,33 @@ Module Quantification
     End Function
 
     ''' <summary>
-    ''' 
+    ''' make normalize of the feature counts data as TPM expression value
     ''' </summary>
     ''' <param name="counts">A collection of the gene <see cref="featureCounts"/> data.</param>
     ''' <param name="env">The R environment.</param>
     ''' <returns>A collection of gene expression data in TPM format.</returns>
     <ExportAPI("convert_to_tpm")>
     <RApiReturn(GetType(GeneSampleSet))>
-    Public Function convert_to_tpm(<RRawVectorArgument> counts As Object, Optional env As Environment = Nothing) As Object
-        Dim pull As pipeline = pipeline.TryCreatePipeline(Of featureCounts)(counts, env)
+    Public Function convert_to_tpm(<RRawVectorArgument(GetType(featureCounts))> counts As Object, Optional env As Environment = Nothing) As Object
+        Dim pull As PipeIterator(Of featureCounts) = pipeline.Stream(Of featureCounts)(counts, env)
 
         If pull.isError Then
             Return pull.getError
         End If
 
-        Return pull.populates(Of featureCounts)(env).MakeGeneExpressions.ToArray
+        Return pull.MakeGeneExpressions.ToArray
     End Function
 
     <ExportAPI("expression_data")>
     <RApiReturn(GetType(Matrix))>
-    Public Function expression_data(<RRawVectorArgument> sampledata As Object, Optional env As Environment = Nothing) As Object
-        Dim pull As pipeline = pipeline.TryCreatePipeline(Of GeneSampleSet)(sampledata, env)
+    Public Function expression_data(<RRawVectorArgument(GetType(GeneSampleSet))> sampledata As Object, Optional env As Environment = Nothing) As Object
+        Dim pull As PipeIterator(Of GeneSampleSet) = pipeline.Stream(Of GeneSampleSet)(sampledata, env)
 
         If pull.isError Then
             Return pull.getError
         End If
 
-        Dim data As GeneSampleSet() = pull.populates(Of GeneSampleSet)(env).ToArray
+        Dim data As GeneSampleSet() = pull.ToArray
         Dim tpm As Matrix = data.TPMExpression
         Dim fpkm As Matrix = data.FPKMExpression
 
