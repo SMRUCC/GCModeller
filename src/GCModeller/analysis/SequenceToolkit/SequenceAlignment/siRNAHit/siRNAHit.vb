0427c31fc3a63855bdbe5cfa503b21f8065a7546
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-30 22:58:03 +0800

    Update siRNAHit.vb

--- src/GCModeller/analysis/SequenceToolkit/SequenceAlignment/siRNAHit/siRNAHit.vb
+++ src/GCModeller/analysis/SequenceToolkit/SequenceAlignment/siRNAHit/siRNAHit.vb
@@ -1,74 +1,75 @@
 ﻿#Region "Microsoft.VisualBasic::b346a2941b199bc851436a385f17537d, analysis\SequenceToolkit\SequenceAlignment\siRNAHit\siRNAHit.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 121
-    '    Code Lines: 62 (51.24%)
-    ' Comment Lines: 38 (31.40%)
-    '    - Xml Docs: 97.37%
-    ' 
-    '   Blank Lines: 21 (17.36%)
-    '     File Size: 5.40 KB
+' Summaries:
 
 
-    '     Class siRNAHit
-    ' 
-    '         Properties: Alignment, EndSite, Expectation, GapCount, Length
-    '                     miRNA, MismatchCount, Source, StartSite, Target
-    '                     TranslationInhibition, WobbleCount
-    ' 
-    '         Function: FromHSP, ToString
-    ' 
-    '     Interface IAccessibilityEvaluator
-    ' 
-    '         Function: UPE
-    ' 
-    '     Class DisabledAccessibility
-    ' 
-    '         Function: UPE
-    ' 
-    '     Interface miRNAMapper
-    ' 
-    '         Function: Run
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 121
+'    Code Lines: 62 (51.24%)
+' Comment Lines: 38 (31.40%)
+'    - Xml Docs: 97.37%
+' 
+'   Blank Lines: 21 (17.36%)
+'     File Size: 5.40 KB
+
+
+'     Class siRNAHit
+' 
+'         Properties: Alignment, EndSite, Expectation, GapCount, Length
+'                     miRNA, MismatchCount, Source, StartSite, Target
+'                     TranslationInhibition, WobbleCount
+' 
+'         Function: FromHSP, ToString
+' 
+'     Interface IAccessibilityEvaluator
+' 
+'         Function: UPE
+' 
+'     Class DisabledAccessibility
+' 
+'         Function: UPE
+' 
+'     Interface miRNAMapper
+' 
+'         Function: Run
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.DataMining.DynamicProgramming.SmithWaterman
 Imports SMRUCC.genomics.SequenceModel.FASTA
 
 Namespace siRNAHit
@@ -127,7 +128,7 @@ Namespace siRNAHit
         ''' <param name="mirna">miRNA 正向序列（用于 position 权重定位，非比对字符）</param>
         ''' <param name="hsp">最佳局部 HSP（seq1=miRNA rev-comp 片段, seq2=mRNA 片段）</param>
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Shared Function FromHSP(mirna As String, hsp As Microsoft.VisualBasic.DataMining.DynamicProgramming.SmithWaterman.LocalHSPMatch(Of Char), source As String) As siRNAHit
+        Public Shared Function FromHSP(mirna As String, hsp As LocalHSPMatch(Of Char), source As String) As siRNAHit
             Dim s1 As Char() = hsp.seq1
             Dim s2 As Char() = hsp.seq2
             Dim n As Integer = Math.Min(s1.Length, s2.Length)
