e08d2420697a87eb5dfb6367c10a504e83a8ce5a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-01 04:33:12 +0800

    run package msbuild

--- src/workbench/pkg/@export/Metaboliq.d.ts
+++ src/workbench/pkg/@export/Metaboliq.d.ts
@@ -8,3 +8,36 @@
  * Liquid In-silico Metabolic Network
  * 
 */
+declare namespace Metaboliq {
+   /**
+    * make liquid network training
+    * 
+    * 
+     * @param model -
+     * @param observed -
+     * @param enzymeSeries -
+     * @param boundarySeries -
+     * @param fluxTruth -
+     * @return get training loss @``T:SMRUCC.genomics.Analysis.Metaboliq.EpochLoss`` vector data via attr(x, "loss")
+   */
+   function fit(model: object, config: object, times: any, observed: object, enzymeSeries: object, boundarySeries: object, fluxTruth: object): object;
+   /**
+     * @param mode default value Is ``null``.
+     * @param solver default value Is ``'rk4'``.
+   */
+   function new(graph: object, mode?: object, solver?: string): object;
+   /**
+   */
+   function predict(model: object, h0: object, times: any, enzymeSeries: object, boundarySeries: object): object;
+   /**
+     * @param explicit_boundary default value Is ``null``.
+   */
+   function read_metabolic_graph(file: string, explicit_boundary?: any): object;
+   /**
+    * read metabolome/enzyme expression time serials data matrix
+    * 
+    * 
+     * @param file -
+   */
+   function read_timedata(file: string): object;
+}
--- src/workbench/pkg/man/biosystem/Metaboliq/Metaboliq.md
+++ src/workbench/pkg/man/biosystem/Metaboliq/Metaboliq.md
@@ -2,3 +2,8 @@
 
 Liquid In-silico Metabolic Network
 
++ [read_metabolic_graph](Metaboliq/read_metabolic_graph.1) 
++ [read_timedata](Metaboliq/read_timedata.1) read metabolome/enzyme expression time serials data matrix
++ [new](Metaboliq/new.1) 
++ [fit](Metaboliq/fit.1) make liquid network training
++ [predict](Metaboliq/predict.1) 
