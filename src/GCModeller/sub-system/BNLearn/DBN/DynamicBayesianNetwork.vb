0e7c1e4537dea146f7f0433039f0c2eb7d591148
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-01 21:34:46 +0800

    show progress for Initialize CPTs for all nodes

--- src/GCModeller/sub-system/BNLearn/DBN/DynamicBayesianNetwork.vb
+++ src/GCModeller/sub-system/BNLearn/DBN/DynamicBayesianNetwork.vb
@@ -1,64 +1,64 @@
 ﻿#Region "Microsoft.VisualBasic::92b6330268b82e3b1ab4dd055623c23f, sub-system\BNLearn\DBN\DynamicBayesianNetwork.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 995
-    '    Code Lines: 591 (59.40%)
-    ' Comment Lines: 256 (25.73%)
-    '    - Xml Docs: 56.64%
-    ' 
-    '   Blank Lines: 148 (14.87%)
-    '     File Size: 43.30 KB
+' Summaries:
 
 
-    '     Class DynamicBayesianNetwork
-    ' 
-    '         Properties: Config
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    ' 
-    '         Function: ComputeActivationScore, ComputeDefaultDistribution, ComputeExpectedRNARate, ComputeLogLikelihood, DiscretizeValue
-    '                   GetAllNodes, GetGeneNodes, GetMarginalDistribution, GetMetaboliteNodes, GetNode
-    '                   GetOperonGeneMapping, GetSummary, GetTFNodes, GetThresholds, PredictNextState
-    '                   StateToScore
-    ' 
-    '         Sub: BuildFromTopology, InitializeCPT, LearnParameters, LoadFromFile, ResetToTopologyPrior
-    '              SaveToFile, UpdateParametersOnline
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 995
+'    Code Lines: 591 (59.40%)
+' Comment Lines: 256 (25.73%)
+'    - Xml Docs: 56.64%
+' 
+'   Blank Lines: 148 (14.87%)
+'     File Size: 43.30 KB
+
+
+'     Class DynamicBayesianNetwork
+' 
+'         Properties: Config
+' 
+'         Constructor: (+2 Overloads) Sub New
+' 
+'         Function: ComputeActivationScore, ComputeDefaultDistribution, ComputeExpectedRNARate, ComputeLogLikelihood, DiscretizeValue
+'                   GetAllNodes, GetGeneNodes, GetMarginalDistribution, GetMetaboliteNodes, GetNode
+'                   GetOperonGeneMapping, GetSummary, GetTFNodes, GetThresholds, PredictNextState
+'                   StateToScore
+' 
+'         Sub: BuildFromTopology, InitializeCPT, LearnParameters, LoadFromFile, ResetToTopologyPrior
+'              SaveToFile, UpdateParametersOnline
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -96,6 +96,7 @@
 Imports System.Globalization
 Imports System.IO
 Imports System.Text
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar
 
 Namespace DBN
 
@@ -262,7 +263,7 @@ Namespace DBN
             Call "Step 3: Initialize CPTs for all nodes".debug
 
             ' --- Step 3: Initialize CPTs for all nodes ---
-            For Each node In _nodes.Values
+            For Each node In Tqdm.Wrap(_nodes.Values)
                 Call InitializeCPT(node)
             Next
 
