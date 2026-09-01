5eaea4b7ffbdf56b499ef4710b30d96a3e857886
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-01 17:47:04 +0800

    make matrix print

--- src/GCModeller/annotations/WGCNA/WGCNA/Algorithm/SoftLinear.vb
+++ src/GCModeller/annotations/WGCNA/WGCNA/Algorithm/SoftLinear.vb
@@ -1,59 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::7091a03d3423f54476bad123b4054d70, annotations\WGCNA\WGCNA\Algorithm\SoftLinear.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 23
-    '    Code Lines: 17 (73.91%)
-    ' Comment Lines: 3 (13.04%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 3 (13.04%)
-    '     File Size: 932 B
+' Summaries:
 
 
-    ' Module SoftLinear
-    ' 
-    '     Function: CreateLinear
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 23
+'    Code Lines: 17 (73.91%)
+' Comment Lines: 3 (13.04%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 3 (13.04%)
+'     File Size: 932 B
+
+
+' Module SoftLinear
+' 
+'     Function: CreateLinear
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.Data.Bootstrapping
-Imports Microsoft.VisualBasic.Math.Distributions
 Imports Microsoft.VisualBasic.Math.Distributions.BinBox
+Imports Microsoft.VisualBasic.Math.Distributions.Summary
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
 
 ''' <summary>
@@ -67,7 +67,7 @@ Module SoftLinear
             .Where(Function(b) b.size > 0) _
             .ToArray
         Dim bin As Vector = cut1.Select(Function(b) b.average).AsVector
-        Dim freq1 As Vector = 0.00000001 + New Vector(cut1.Select(Function(b) b.size)) / k.Dim
+        Dim freq1 As Vector = 0.00000001 + New Vector(cut1.Select(Function(b) CDbl(b.size))) / k.Dim
         Dim X As Vector = bin.Log(10)
         Dim Y As Vector = freq1.Log(10)
 
