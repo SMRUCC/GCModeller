c5707b658351becdf51dbbbef4cf09f48775c9b7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-08 14:54:01 +0800

    Remove unused imports from MetaCyc Proteins file
    本次改动对 MetaCyc数据文件解析模块中的 `Proteins.vb`做了小幅清理，核心内容有两处：
    
    -删除未使用的导入语句：移除了 `Imports SMRUCC.genomics.Assembly.MetaCyc.Schema.Reflection`与 `Imports Microsoft.VisualBasic`两行，二者在该文件剩余代码中均未再被引用；
    -顺带将文件头部自动生成的许可证/统计注释块统一缩进格式（由带缩进的块注释改为顶格 `'`注释），纯属格式规范化。
    
    改动动机是消除冗余引用、减少编译期不必要依赖，使文件自包含性更清晰。整个过程不涉及任何逻辑或行为变更，类的成员（`AttributeList`属性、`GetProteinComplexByComponent`等）均保持不变。

--- src/GCModeller/core/Bio.Assembly/Assembly/MetaCyc/File/AttributeValue DataFile/DataFiles/Files/Proteins.vb
+++ src/GCModeller/core/Bio.Assembly/Assembly/MetaCyc/File/AttributeValue DataFile/DataFiles/Files/Proteins.vb
@@ -1,61 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::9b05d478686a08a317323e98763895a1, core\Bio.Assembly\Assembly\MetaCyc\File\AttributeValue DataFile\DataFiles\Files\Proteins.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 61
-    '    Code Lines: 43 (70.49%)
-    ' Comment Lines: 12 (19.67%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 6 (9.84%)
-    '     File Size: 3.69 KB
+' Summaries:
 
 
-    '     Class Proteins
-    ' 
-    '         Properties: AttributeList
-    ' 
-    '         Function: GetProteinComplexByComponent, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 61
+'    Code Lines: 43 (70.49%)
+' Comment Lines: 12 (19.67%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 6 (9.84%)
+'     File Size: 3.69 KB
+
+
+'     Class Proteins
+' 
+'         Properties: AttributeList
+' 
+'         Function: GetProteinComplexByComponent, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports SMRUCC.genomics.Assembly.MetaCyc.Schema.Reflection
-Imports Microsoft.VisualBasic
 
 Namespace Assembly.MetaCyc.File.DataFiles
 
