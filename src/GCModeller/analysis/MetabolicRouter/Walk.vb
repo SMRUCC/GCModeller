f38d57bc612c7161f96b98c7b628c9ca6a8730c2
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-08 14:26:56 +0800

    walk network

---
+++ src/GCModeller/analysis/MetabolicRouter/Walk.vb
@@ -0,0 +1,15 @@
+﻿Imports SMRUCC.genomics.Analysis.RetroPath.Chem
+Imports SMRUCC.genomics.Analysis.RetroPath.Search
+
+Public Class Walk
+
+    Public ReadOnly Property opts As SearchOptions
+    Public ReadOnly Property w As ScoreWeights
+    Public ReadOnly Property rules As List(Of Rule)
+    Public ReadOnly Property sink As List(Of Tuple(Of String, String))
+
+
+    Public Function Search(targetSmiles As String)
+
+    End Function
+End Class
