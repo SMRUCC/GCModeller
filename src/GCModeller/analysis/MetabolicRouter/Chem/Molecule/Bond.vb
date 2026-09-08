4bc23775e7f48640f93d4ea9f68266e619e3df34
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-08 14:44:11 +0800

    move molecule source into molecue folder

---
+++ src/GCModeller/analysis/MetabolicRouter/Chem/Molecule/Bond.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic.Data.GraphTheory.Network
+
+Namespace Chem
+
+    Public Class Bond : Implements IndexEdge
+
+        Public Property a As Integer Implements IndexEdge.U
+        Public Property b As Integer Implements IndexEdge.V
+        Public Property order As Integer
+
+        Public Shared Widening Operator CType(tri As (Integer, Integer, Integer)) As Bond
+            Return New Bond With {.a = tri.Item1, .b = tri.Item2, .order = tri.Item3}
+        End Operator
+
+    End Class
+End Namespace
--- src/GCModeller/analysis/MetabolicRouter/Chem/Molecule/Molecule.vb
+++ src/GCModeller/analysis/MetabolicRouter/Chem/Molecule/Molecule.vb
@@ -9,21 +9,7 @@
 ' 价态模型：隐式 H = max(0, 价态 − Σ键级 − 显式H)；价态违规的应用被拒绝。
 ' ============================================================================
 
-Imports Microsoft.VisualBasic.Data.GraphTheory.Network
-
 Namespace Chem
-
-    Public Class Bond : Implements IndexEdge
-
-        Public Property a As Integer Implements IndexEdge.U
-        Public Property b As Integer Implements IndexEdge.V
-        Public Property order As Integer
-
-        Public Shared Widening Operator CType(tri As (Integer, Integer, Integer)) As Bond
-            Return New Bond With {.a = tri.Item1, .b = tri.Item2, .order = tri.Item3}
-        End Operator
-
-    End Class
 
     Public Class Molecule
 
