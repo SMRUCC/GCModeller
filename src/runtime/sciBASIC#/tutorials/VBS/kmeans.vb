8b00f3e4d07117a3a995b2133a38ba685e73a1fb
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-08 19:25:57 +0800

    run dynamics script file

---
+++ src/runtime/sciBASIC#/tutorials/VBS/kmeans.vb
@@ -0,0 +1,14 @@
+#include "Microsoft.VisualBasic.DataMining.Framework.dll"
+#include "Microsoft.VisualBasic.Data.Framework.dll"
+
+imports microsoft.visualbasic.data.framework.storageprovider
+imports microsoft.visualbasic.datamining.kmeans
+imports microsoft.visualbasic.data.framework
+
+dim file = "G:\GCModeller\src\R-sharp\REnv\data\bezdekIris.csv"
+dim k = 3
+
+dim dataset = DataFrameResolver.LoadDataSet(file).ToKMeansModels
+dim result = dataset.kmeans( expected := k).toarray
+
+call result.saveto("Z:/test.csv")
--- src/runtime/sciBASIC#/vs_solutions/VBS/Program.vb
+++ src/runtime/sciBASIC#/vs_solutions/VBS/Program.vb
@@ -1,7 +1,17 @@
-Imports System
+Imports Microsoft.VisualBasic.CommandLine
 
 Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-    End Sub
+
+    ''' <summary>
+    ''' vbs ./script.vb --arg1=xxx --arg2=xxx
+    ''' </summary>
+    ''' <param name="args"></param>
+    Public Function Main(args As String()) As Integer
+        Dim scriptFile As String = args(0)
+        Dim vbs As ScriptParseResult = VBScriptHost.Script.ParseScript(scriptFile)
+
+        Using script As ScriptRuntime = vbs.CompileScript
+            Return script.Run(args)
+        End Using
+    End Function
 End Module
