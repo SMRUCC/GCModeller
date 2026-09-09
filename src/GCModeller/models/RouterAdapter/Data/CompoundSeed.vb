f8b4bd4f6a7a8406f856e835b7e3b380bf8e4f3b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-10 02:48:35 +0800

    split source file into multiple parts

---
+++ src/GCModeller/models/RouterAdapter/Data/CompoundSeed.vb
@@ -0,0 +1,28 @@
+﻿
+''' <summary>
+''' 结构索引的输入种子：一个待索引的化合物。
+''' </summary>
+Public Class CompoundSeed
+
+    ''' <summary>化合物唯一标识。</summary>
+    Public Property Id As String
+
+    ''' <summary>原始 SMILES（未净化；为空即表示数据源未提供结构）。</summary>
+    Public Property Smiles As String
+
+    ''' <summary>显示名称（可选，供名称白名单与结果展示使用）。</summary>
+    Public Property Name As String
+
+    ''' <summary>同义名（可选）。</summary>
+    Public Property Synonyms As String()
+
+    ''' <summary>便捷构造。</summary>
+    Public Shared Function Create(id As String, smiles As String,
+                                  Optional name As String = Nothing,
+                                  Optional synonyms As String() = Nothing) As CompoundSeed
+        Return New CompoundSeed With {
+            .Id = id, .Smiles = smiles, .Name = name, .Synonyms = synonyms
+        }
+    End Function
+
+End Class
--- src/GCModeller/models/RouterAdapter/Data/SinkBuilder.vb
+++ src/GCModeller/models/RouterAdapter/Data/SinkBuilder.vb
@@ -22,13 +22,6 @@
 
 Imports System.Text
 
-Public Enum SinkModes
-    ''' <summary>核心中心代谢子集（枢纽代谢物 + 白名单 + 辅因子）</summary>
-    Core = 0
-    ''' <summary>全部有可用结构的化合物（查询时排除目标自身）</summary>
-    All = 1
-End Enum
-
 Public Module SinkBuilder
 
     ''' <summary>
