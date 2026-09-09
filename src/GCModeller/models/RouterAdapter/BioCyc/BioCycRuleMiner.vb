8c83ffb6eb157ab9fc940604f4f6298944d1ea81
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-09 17:28:27 +0800

    make improvements of the compound model

--- src/GCModeller/models/RouterAdapter/BioCyc/BioCycRuleMiner.vb
+++ src/GCModeller/models/RouterAdapter/BioCyc/BioCycRuleMiner.vb
@@ -26,24 +26,6 @@ Imports SMRUCC.genomics.ComponentModel.EquaionModel.DefaultTypes
 Imports SMRUCC.genomics.Data.BioCyc
 Imports SMRUCC.genomics.Data.BioCyc.Assembly.MetaCyc.Schema.Metabolism
 
-''' <summary>
-''' 一个可被 RetroPath 使用的化合物结构（净化后的 SMILES + 分子图）
-''' </summary>
-Public Class CompoundStructure
-
-    ''' <summary>BioCyc 的 compound frame id（UNIQUE-ID）</summary>
-    Public Property Id As String
-    ''' <summary>净化后的 SMILES（剔除立体标记之后的字符串）</summary>
-    Public Property Smiles As String
-    ''' <summary>该 SMILES 对应的分子图</summary>
-    Public Property Mol As Molecule
-
-    Public Overrides Function ToString() As String
-        Return $"{Id} ({Smiles})"
-    End Function
-
-End Class
-
 Public Module BioCycRuleMiner
 
     ''' <summary>
