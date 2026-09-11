551f173e7ded00a86c0d175d75caa5cfa9ea6880
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-12 04:46:19 +0800

    run package document msbuild

--- src/workbench/pkg/@export/circos.d.ts
+++ src/workbench/pkg/@export/circos.d.ts
@@ -5,15 +5,40 @@
 // ref=visualkit.circosTool@visualkit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
+ * 
 */
 declare namespace circos {
    /**
+     * @param env default value Is ``null``.
+   */
+   function blastn_mapping(source: any, chrs: any, env?: object): object;
+   /**
    */
    function default_identity_colors(default: string): object;
+   /**
+    * Creates the circos gene circle from the PTT database which is defined 
+    *  in the ``*.ptt/*.rnt`` file, and you can download this directory from 
+    *  the NCBI FTP website.
+    * 
+    * 
+     * @param PTT -
+     * @param COG -
+     * @param defaultColor -
+     * 
+     * + default value Is ``'blue'``.
+     * @param env 
+     * + default value Is ``null``.
+   */
+   function genome_circle(PTT: object, COG: any, defaultColor?: string, env?: object): object;
    /**
      * @param depth default value Is ``10``.
      * @param default default value Is ``'Brown'``.
      * @param mapName default value Is ``'Jet'``.
    */
    function identity_colors(min: number, max: number, depth?: object, default?: string, mapName?: string): object;
+   /**
+     * @param steps default value Is ``2048``.
+     * @param env default value Is ``null``.
+   */
+   function track_hits(source: any, karyotype: object, steps?: object, env?: object): object;
 }
--- src/workbench/pkg/man/visualkit/circos/circos.md
+++ src/workbench/pkg/man/visualkit/circos/circos.md
@@ -4,3 +4,6 @@
 
 + [default_identity_colors](circos/default_identity_colors.1) 
 + [identity_colors](circos/identity_colors.1) 
++ [blastn_mapping](circos/blastn_mapping.1) 
++ [track_hits](circos/track_hits.1) 
++ [genome_circle](circos/genome_circle.1) Creates the circos gene circle from the PTT database which is defined 
