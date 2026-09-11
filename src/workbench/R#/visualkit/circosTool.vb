7514ca37320eae492f5ac9b3787bd21cf0d173c3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-12 04:04:29 +0800

    export genomics context data plot for circos

--- src/workbench/R#/visualkit/circosTool.vb
+++ src/workbench/R#/visualkit/circosTool.vb
@@ -1,16 +1,24 @@
 ﻿
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank.TabularFormat
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.NtMapping
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.Pipeline.COG
 Imports SMRUCC.genomics.SequenceModel.FASTA
+Imports SMRUCC.genomics.Visualize
 Imports SMRUCC.genomics.Visualize.Circos
+Imports SMRUCC.genomics.Visualize.Circos.Configurations
 Imports SMRUCC.genomics.Visualize.Circos.Karyotype
+Imports SMRUCC.genomics.Visualize.Circos.Karyotype.GeneObjects
+Imports SMRUCC.genomics.Visualize.Circos.TrackDatas
 Imports SMRUCC.genomics.Visualize.Circos.TrackDatas.Highlights
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Package("circos")>
+<RTypeExport("circos", GetType(Circos))>
 Module circosTool
 
     <ExportAPI("default_identity_colors")>
@@ -35,6 +43,43 @@ Module circosTool
             Return chrNts.getError
         End If
 
-        Return DataExtensions.FromBlastnMappings(blastn, chrNts)
+        Return SMRUCC.genomics.Visualize.Circos.DataExtensions.FromBlastnMappings(blastn, chrNts)
+    End Function
+
+    <ExportAPI("track_hits")>
+    <RApiReturn(GetType(ValueTrackData))>
+    Public Function HitsTracks(source As Object,
+                            karyotype As SkeletonInfo,
+                            Optional steps% = 2048, Optional env As Environment = Nothing) As Object
+
+        Dim blastn As PipeIterator(Of BlastnMapping) = pipeline.Stream(Of BlastnMapping)(source, env)
+
+        If blastn.isError Then
+            Return blastn.getError
+        End If
+
+        Return blastn.Hits(karyotype, steps)
+    End Function
+
+    ''' <summary>
+    ''' Creates the circos gene circle from the PTT database which is defined 
+    ''' in the ``*.ptt/*.rnt`` file, and you can download this directory from 
+    ''' the NCBI FTP website.
+    ''' </summary>
+    ''' <param name="PTT"></param>
+    ''' <param name="COG"></param>
+    ''' <param name="defaultColor"></param>
+    ''' <returns></returns>
+    <ExportAPI("genome_circle")>
+    <RApiReturn(GetType(PTTMarks))>
+    Public Function genome_circle(PTT As PTTDbLoader, COG As Object, Optional defaultColor As String = "blue", Optional env As Environment = Nothing) As Object
+        Dim cogData As PipeIterator(Of MyvaCOG) = pipeline.Stream(Of MyvaCOG)(COG, env)
+
+        If cogData.isError Then
+            Return cogData.getError
+        End If
+
+        Dim data As New PTTMarks(PTT, cogData.ToArray, defaultColor)
+        Return data
     End Function
 End Module
