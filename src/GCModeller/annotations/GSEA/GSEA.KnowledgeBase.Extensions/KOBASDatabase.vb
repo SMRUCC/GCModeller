136dac1bc22e376c2a022c389f3e1d731f4c3061
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-12 01:48:12 +0800

    fix of the missing component reference

--- src/GCModeller/annotations/GSEA/GSEA.KnowledgeBase.Extensions/KOBASDatabase.vb
+++ src/GCModeller/annotations/GSEA/GSEA.KnowledgeBase.Extensions/KOBASDatabase.vb
@@ -1,57 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::741762f082cd3478e21410cb3b71f6b5, annotations\GSEA\GSEA.KnowledgeBase.Extensions\KOBASDatabase.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 8
-    '    Code Lines: 6 (75.00%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 2 (25.00%)
-    '     File Size: 246 B
+' Summaries:
 
 
-    ' Module KOBASDatabase
-    ' 
-    '     Function: ImportsKOBASSqlite3
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 8
+'    Code Lines: 6 (75.00%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 2 (25.00%)
+'     File Size: 246 B
+
+
+' Module KOBASDatabase
+' 
+'     Function: ImportsKOBASSqlite3
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Microsoft.VisualBasic.Data.IO.ManagedSqlite.Core
+Imports Microsoft.VisualBasic.Data.IO.ManagedSqlite
 
 Public Module KOBASDatabase
 
