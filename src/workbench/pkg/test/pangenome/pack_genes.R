ae0c38fda3036d9dc37c3b1fe0a10d9f8534b680
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-12 09:36:32 +0800

    Update pack_genes.R

--- src/workbench/pkg/test/pangenome/pack_genes.R
+++ src/workbench/pkg/test/pangenome/pack_genes.R
@@ -6,14 +6,17 @@ imports "bioseq.fasta" from "seqtoolkit";
 let src = ?"--genbank" || stop("no genbank source was provided!");
 let result = ?"--out" || file.path(src, "result");
 let gbff = load_genbanks( list.files(src,"*.gbff" ));
+let table = c();
 let get_proteins = function(gb) {
     protein_seqs(gb, title = "<locus_tag>.<gb_asm_id>");
 }
 
 let targets = open.fasta(file.path(result,"proteins.faa"), read=  FALSE);
 
-for(let gb in gbff) {
+for(let gb in gbff |> take(500)) {
+    table = c(table, as_tabular(gb));
     write.fasta(get_proteins(gb), file = targets, filter.empty = TRUE);
 }
 
+write.csv(table, file = file.path(result,"genes.csv"));
 close(targets);
