079e610fce481c664278fbb18b4f96102c4c1e44
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-13 04:27:45 +0800

    make improvements of the biocyc file parser

--- src/GCModeller/models/BioCyc/Reader/AttrDataCollection.vb
+++ src/GCModeller/models/BioCyc/Reader/AttrDataCollection.vb
@@ -128,6 +128,13 @@ Public Class AttrDataCollection(Of T As Model) : Implements Enumeration(Of T)
     Public Shared Function LoadFile(file As TextReader) As AttrDataCollection(Of T)
         Dim dataFile As AttrValDatFile = AttrValDatFile.ParseFile(file)
         Dim writer As ObjectWriter = ObjectWriter.LoadSchema(Of T)
+
+        ' 预热一次反序列化：反射相关的类型缓存是惰性初始化的共享状态，
+        ' 若直接在下面的 PLINQ 里并发首次访问，会偶发 NullReferenceException
+        If dataFile.features.Length > 0 Then
+            Call writer.Deserize(dataFile.features(Scan0).value)
+        End If
+
         Dim data As T() = (From a As SeqValue(Of FeatureElement)
                            In dataFile.features _
                                .SeqIterator _
