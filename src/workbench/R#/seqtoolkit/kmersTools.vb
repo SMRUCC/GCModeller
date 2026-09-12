1aef367d151050c33ada608ee50be4830794f14e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-13 00:40:04 +0800

    Update kmersTools.vb

--- src/workbench/R#/seqtoolkit/kmersTools.vb
+++ src/workbench/R#/seqtoolkit/kmersTools.vb
@@ -91,7 +91,7 @@ Imports SeqMatrix = SMRUCC.genomics.Analysis.HTS.DataFrame.Matrix
 '''   family table.
 ''' </remarks>
 <Package("kmers")>
-<RTypeExport("cdhit-family", GetType(SimilarHit()))>
+<RTypeExport("cdhit-family", GetType(SimilarHit))>
 Module kmersTools
 
     ''' <summary>
