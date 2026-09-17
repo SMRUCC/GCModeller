658e8af3ec52de7cd184ad69d781a3e660c183b1
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-17 15:46:33 +0800

    add comment docs

--- src/workbench/pkg/test/pangenome/pack_genes.R
+++ src/workbench/pkg/test/pangenome/pack_genes.R
@@ -3,22 +3,27 @@ require(GCModeller);
 imports "GenBank" from "seqtoolkit";
 imports "bioseq.fasta" from "seqtoolkit";
 
-let src = ?"--genbank" || stop("no genbank source was provided!");
-let result = ?"--out" || file.path(src, "result");
-let gbff = load_genbanks( list.files(src,"*.gbff" ), extract_genomics = TRUE);
-let table = c();
-let get_proteins = function(gb) {
-    protein_seqs(gb, title = "<locus_tag>.<gb_asm_id>");
-}
+# step 1: prepare annotation data set
 
+# R# ./pack_genes.R --genbank ./data
+
+let src     = ?"--genbank" || stop("no genbank source was provided!");
+let result  = ?"--out"     || file.path(src, "result");
+let takes_n = ?"--takes"   || 10000;
+let gbff    = load_genbanks( list.files(src,"*.gbff" ), extract_genomics = TRUE);
+let table   = c();
 let targets = open.fasta(file.path(result,"proteins.faa"), read=  FALSE);
 
-for(let gb in gbff |> take(1000)) {
+for(let gb in gbff |> take(takes_n)) {
     print(accession_id(gb));
 
+    # write protein sequence into the file stream
     table = c(table, as_tabular(gb));
-    write.fasta(get_proteins(gb), file = targets, filter.empty = TRUE);
+    write.fasta(protein_seqs(gb, title = "<locus_tag>.<gb_asm_id>"), 
+        file = targets, 
+        filter.empty = TRUE);
 }
 
+# export gene table and flush the protein sequence stream
 write.csv(table, file = file.path(result,"genes.csv"));
 close(targets);
