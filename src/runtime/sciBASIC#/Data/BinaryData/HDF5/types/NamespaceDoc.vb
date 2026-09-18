c1d556bf49d4d112f26344cc7f1b910ff89d247a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-18 16:04:59 +0800

    add namespace docs

---
+++ src/runtime/sciBASIC#/Data/BinaryData/HDF5/types/NamespaceDoc.vb
@@ -0,0 +1,9 @@
+Namespace type
+
+    ''' <summary>
+    ''' The HDF5 data type model: fixed point, floating point, string, enum and variable length
+    ''' data types.
+    ''' </summary>
+    Module NamespaceDoc
+    End Module
+End Namespace
