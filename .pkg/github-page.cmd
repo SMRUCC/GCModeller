4909b5509e1bb6088fa5419837a1b27cdc8c2273
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-23 03:52:56 +0800

    make config of the R# package msbuild script

--- .pkg/github-page.cmd
+++ .pkg/github-page.cmd
@@ -1,6 +1,7 @@
 @echo off
 
+set dotnet_ver=net10.0
 SET js_url="https://gcmodeller.org/lib/R_syntax.js"
-SET Rscript="\GCModeller\src\R-sharp\App\net8.0\Rscript.exe"
+SET Rscript="\GCModeller\src\R-sharp\App/%dotnet_ver%/Rscript.exe"
 
 %Rscript% --build /src ../  --skip-src-build --github-page %js_url%
